56df34b1f985ceda349c8523dde4905103868f00
zocha_sz <zofia.szczudlik@gmail.com>
2017-06-13 23:04:55 +0200

    14 passing

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,10 +1,33 @@
-var SortedList = function() {};
+var SortedList = function () {
+  this.length = 0;
+  this.list=[];
+};
 
-SortedList.prototype.add     = function(item) {}
-SortedList.prototype.get     = function(pos) {}
-SortedList.prototype.max     = function() {}
-SortedList.prototype.min     = function() {}
-SortedList.prototype.average = function() {}
-SortedList.prototype.sum     = function() {}
+SortedList.prototype.add     = function(item) {
+  this.list.push(item);
+  this.list.sort(function(a, b) {
+      return a - b;
+  });
+  this.length++;
+}
+SortedList.prototype.get     = function(pos) {
+  return this.list[pos-1];
+}
+SortedList.prototype.max     = function() {
+  return this.list[this.length-1];
+}
+SortedList.prototype.min     = function() {
+  return this.list[0];
+}
+SortedList.prototype.average = function() {
+  return this.sum()/this.length;
+}
+SortedList.prototype.sum     = function() {
+  var sum = 0;
+  for (var i=0; i<this.length; i++){
+    sum += this.list[i];
+  }
+  return sum;
+}
 
 module.exports = SortedList;
