bf4862c3eaa4945e0d000263f8566e87be9d2df7
ByronGBP <byrongbp@gmail.com>
2017-09-04 15:52:59 +0200

    donde

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,10 +1,45 @@
-var SortedList = function() {};
+var SortedList = function() {
+  this.length = 0;
+  this.list = [];
+};
 
-SortedList.prototype.add     = function(item) {}
-SortedList.prototype.get     = function(pos) {}
-SortedList.prototype.max     = function() {}
-SortedList.prototype.min     = function() {}
-SortedList.prototype.average = function() {}
-SortedList.prototype.sum     = function() {}
+SortedList.prototype.add = function(item) {
+  this.list.push(item);
+  this.list.sort(function(a,b){
+    if (a > b){return 1;}
+    else if (a < b){return -1;}
+    else{return 0;}
+  });
+  this.length++;
+};
+
+SortedList.prototype.get     = function(pos) {
+  return this.list[--pos];
+};
+
+SortedList.prototype.max = function() {
+    return this.list[this.length - 1];
+};
+
+SortedList.prototype.min = function() {
+  return this.list[0];
+};
+SortedList.prototype.average = function() {
+
+  var total = this.list.reduce(function(sum, value){
+    return sum + value;
+  }, 0);
+
+  return total/this.length;
+};
+SortedList.prototype.sum     = function() {
+
+  return this.list.reduce(function(sum, value){
+    return sum + value;
+  }, 0);
+};
+
+
+
 
 module.exports = SortedList;
