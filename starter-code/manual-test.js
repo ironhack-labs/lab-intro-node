3685148acc332a5f4021c26a75b64b874e1aea92
Philip Wisner <philipwisner@gmail.com>
2017-09-04 16:35:20 +0200

    done

---
+++ starter-code/manual-test.js
@@ -0,0 +1,10 @@
+var SortedList = require('./index.js');
+
+
+var newSortedList = new SortedList();
+
+newSortedList.add(2);
+newSortedList.add(3);
+newSortedList.add(1);
+
+console.log(newSortedList.get(0));
