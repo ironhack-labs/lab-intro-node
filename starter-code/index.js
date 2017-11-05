2eaee9c397090f45c5d66ed3a874b849a9fe8bee
Isak La Fleur <isak.engdahl@gmail.com>
2017-11-05 18:16:07 +0100

    final commit. All 14 tests passing.

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,10 +1,30 @@
-var SortedList = function() {};
+function SortedList() {
+  this.list = [];
+  this.length = 0;
+  this.length = this.list.length;
+}
 
-SortedList.prototype.add     = function(item) {}
-SortedList.prototype.get     = function(pos) {}
-SortedList.prototype.max     = function() {}
-SortedList.prototype.min     = function() {}
-SortedList.prototype.average = function() {}
-SortedList.prototype.sum     = function() {}
+SortedList.prototype.add = function(item) {
+  this.list.push(item);
+  this.list.sort((a, b) => a - b);
+  this.length += 1;
+};
+SortedList.prototype.get = function(pos) {
+  return this.list[pos - 1];
+};
+SortedList.prototype.max = function() {
+  return this.list[this.list.length - 1];
+};
+SortedList.prototype.min = function() {
+  return this.list[0];
+};
+SortedList.prototype.average = function() {
+  return this.list.length === 0 ? [] : this.sum() / this.list.length;
+};
+SortedList.prototype.sum = function() {
+  return this.list.length === 0
+    ? []
+    : this.list.reduce((sum, value) => sum + value, 0);
+};
 
 module.exports = SortedList;
