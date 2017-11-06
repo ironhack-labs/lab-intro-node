0e346e4234fbdf5f6c8e8350bc771ce18788e06f
Riftland <fran.mscr@gmail.com>
2017-11-06 12:50:47 +0100

    get

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,7 +1,16 @@
-var SortedList = function() {};
+var SortedList = function(){
+  this.length = [];
+};
 
-SortedList.prototype.add     = function(item) {}
-SortedList.prototype.get     = function(pos) {}
+SortedList.prototype.add = function(item) {
+  this.length.push(item);
+  this.length.sort((value1, value2) => {
+    return value1 - value2;
+  });
+}
+SortedList.prototype.get = function(pos) {
+  return this.length[pos];
+}
 SortedList.prototype.max     = function() {}
 SortedList.prototype.min     = function() {}
 SortedList.prototype.average = function() {}
