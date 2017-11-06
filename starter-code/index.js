1f7d2cd76bbd143af07ff65951e1e5fbe3c6eb1a
lacazeto <lucas.cazeto@gmail.com>
2017-11-06 15:52:47 +0100

    awfully explained exercise

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,7 +1,26 @@
-var SortedList = function() {};
+var SortedList = function() {
+  this.length = 0;
+  this.list = [];
+};
 
-SortedList.prototype.add     = function(item) {}
-SortedList.prototype.get     = function(pos) {}
+SortedList.prototype.add     = function(item) {
+    this.list.push(item);
+    this.length++;
+    this.list.sort(function(a,b){
+        return a -b;
+    });
+    }
+}
+SortedList.prototype.get     = function(pos) {
+    if(pos < this.list.length){
+        return this.list[pos];
+    }
+    else{
+        return "OutOfBounds";
+    }
+}
+
+
 SortedList.prototype.max     = function() {}
 SortedList.prototype.min     = function() {}
 SortedList.prototype.average = function() {}
