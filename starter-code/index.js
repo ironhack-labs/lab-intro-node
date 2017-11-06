e881b418aaa7d1668a00e371a31db193cdda75a5
Eduardo Rodés <eduardorodes@MacBook-Air-de-Eduardo.local>
2017-11-06 14:18:09 +0100

    done

--- starter-code/index.js
+++ starter-code/index.js
@@ -4,25 +4,32 @@ function SortedList () {
 
 SortedList.prototype.add = function(item) {
   this.length.push(item);
-  this.length.sort();
+  this.length.sort(function(a,b){return a-b});
 };
 
 SortedList.prototype.get = function(pos) {
-    return this.length[pos-1];
+  if(this.length.length < pos) return "OutOfBounds"
+  return this.length[pos-1];
 };
 
-SortedList.prototype.max     = function() {}
-SortedList.prototype.min     = function() {}
-SortedList.prototype.average = function() {}
-SortedList.prototype.sum     = function() {}
+SortedList.prototype.max  = function() {
+  if(this.length.length == 0) return 'EmptySortedList';
+  return Math.max(...this.length);
+};
+
+SortedList.prototype.min = function() {
+  if(this.length.length == 0) return 'EmptySortedList';
+  return Math.min(...this.length);
+};
+
+SortedList.prototype.average = function() {
+  if(this.length.length == 0) return 'EmptySortedList';
+  return (this.length.reduce((prev,next) => prev+next) / this.length.length);
+};
+
+SortedList.prototype.sum = function() {
+  if(this.length.length == 0) return 'EmptySortedList';
+  return this.length.reduce((prev,next) => prev+next);
+};
 
 module.exports = SortedList;
-
-
-// var sl = new SortedList;
-//
-// var foo = 10;
-// for(i=1; i<200; i++) {
-//   sl.add(foo*i);
-//   console.log(i + " " + sl.get(i));
-// };
