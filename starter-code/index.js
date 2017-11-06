868bc2d6ee5b6350025dcabee87ea1a32c3f51f7
alm00001 <alm00001@gmail.com>
2017-11-06 12:53:43 +0100

    First Commit

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,6 +1,18 @@
-var SortedList = function() {};
+var SortedList = function() {
+this.sortedList = [];
+this.length = 0;
+};
 
-SortedList.prototype.add     = function(item) {}
+SortedList.prototype.add = function(item) {
+this.length += 1;
+this.sortedList.push(item);
+this.sortedList.sort(function (a, b){
+return a - b;
+});
+return this.sortedList;
+};
+
+
 SortedList.prototype.get     = function(pos) {}
 SortedList.prototype.max     = function() {}
 SortedList.prototype.min     = function() {}
