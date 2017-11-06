60d7e0899398ede766c83a88d94aa2b619352e35
Rocio Garcia <gooblyn@gmail.com>
2017-11-06 16:43:58 +0100

    Finished

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,21 +1,40 @@
 function SortedList (){
-this.length = [];
+
 };
 
+SortedList.prototype= Object.create(Array.prototype);
+SortedList.prototype.constructor = SortedList;
+
 SortedList.prototype.add     = function(item) {
-   this.length.push(item);
-   this.length.sort();
+   this.push(item);
 };
 
 SortedList.prototype.get     = function(pos) {
- return this.length[pos-1];
+ return this.sort(function(a,b){return a - b;})[pos-1];
 };
 
 SortedList.prototype.max     = function() {
-
+  return Math.max(...this);
 };
-SortedList.prototype.min     = function() {};
-SortedList.prototype.average = function() {};
-SortedList.prototype.sum     = function() {};
+
+SortedList.prototype.min     = function() {
+  return Math.min(...this);
+};
+
+SortedList.prototype.average = function() {
+  function getSum(total, num) {
+    return total + num;
+  }
+  if (this.length === 0){return Error;}
+  return this.reduce(getSum)/this.length;
+};
+
+SortedList.prototype.sum     = function() {
+  function getSum(total, num) {
+    return total + num;
+  }
+  if (this.length === 0){return Error;}
+  return this.reduce(getSum);
+};
 
 module.exports = SortedList;
