d4991391e9a305093aad17b1b29cfae9fdfdbfc8
Abraham Castro <cucharadepalo@gmail.com>
2017-11-06 12:50:33 +0100

    9 test passing

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,7 +1,16 @@
-var SortedList = function() {};
+var SortedList = function() {
+  this.list = [];
+  this.length = this.list.length;
+};
 
-SortedList.prototype.add     = function(item) {}
-SortedList.prototype.get     = function(pos) {}
+SortedList.prototype.add  = function(item) {
+  this.list.push(item);
+  this.list.sort();
+  this.length++;
+}
+SortedList.prototype.get     = function(pos) {
+  return this.list[pos - 1];
+}
 SortedList.prototype.max     = function() {}
 SortedList.prototype.min     = function() {}
 SortedList.prototype.average = function() {}
