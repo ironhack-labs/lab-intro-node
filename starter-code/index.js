4dd1d14c34cb030fba96b09585b0eb5c32134e50
Rocio Garcia <gooblyn@gmail.com>
2017-11-06 12:57:15 +0100

    Done until function max

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,10 +1,21 @@
-var SortedList = function() {};
+function SortedList (){
+this.length = [];
+};
 
-SortedList.prototype.add     = function(item) {}
-SortedList.prototype.get     = function(pos) {}
-SortedList.prototype.max     = function() {}
-SortedList.prototype.min     = function() {}
-SortedList.prototype.average = function() {}
-SortedList.prototype.sum     = function() {}
+SortedList.prototype.add     = function(item) {
+   this.length.push(item);
+   this.length.sort();
+};
+
+SortedList.prototype.get     = function(pos) {
+ return this.length[pos-1];
+};
+
+SortedList.prototype.max     = function() {
+
+};
+SortedList.prototype.min     = function() {};
+SortedList.prototype.average = function() {};
+SortedList.prototype.sum     = function() {};
 
 module.exports = SortedList;
