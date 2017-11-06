40651014149091d34a3b530689c192c2570772d4
Yurema Rebollo <yuremarc@gmail.com>
2017-11-06 17:22:27 +0100

    Segundo commit

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,17 @@
 var SortedList = function() {
-  this.list = [];
-  this.length = this.list.length;
+  this.length = [];
 };
 
-SortedList.prototype.add     = function(item) {this.list.push(item);};
-SortedList.prototype.get     = function(pos) {};
-SortedList.prototype.max     = function() {};
+SortedList.prototype.add     = function(item) {
+  this.length.push(item);
+  this.length.sort();
+};
+SortedList.prototype.get     = function(pos) {
+  return this.length[pos-1];
+};
+SortedList.prototype.max     = function() {
+  return this.length.length == 0 ? console.log("EmptySortedList") : this.length[this.length.length - 1];
+};
 SortedList.prototype.min     = function() {};
 SortedList.prototype.average = function() {};
 SortedList.prototype.sum     = function() {};
