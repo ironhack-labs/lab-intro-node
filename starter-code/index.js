faf40e960c7db50446c5fab40953094e17535c29
J. Gaspar Prieto <jgprieto00@gmail.com>
2017-11-06 12:52:02 +0100

    failing 3 test

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,10 +1,23 @@
-var SortedList = function() {};
+var SortedList = function() {
+   this.length = [];
+    
+};
 
-SortedList.prototype.add     = function(item) {}
-SortedList.prototype.get     = function(pos) {}
-SortedList.prototype.max     = function() {}
-SortedList.prototype.min     = function() {}
+SortedList.prototype.add     = function(item) {
+    this.length.push(item);
+    this.length.sort();
+}
+SortedList.prototype.get     = function(pos) {
+    return this.length[pos - 1];
+}
+SortedList.prototype.max     = function() {
+    return this.length[this.length.length -1 ];
+}
+SortedList.prototype.min     = function() {
+    return this.length [0];
+}
 SortedList.prototype.average = function() {}
 SortedList.prototype.sum     = function() {}
 
 module.exports = SortedList;
+
