f56d482a8c5e455da11976c46cb07f61bff476a0
pablohk <pablusmmid@gmail.com>
2017-11-06 15:15:37 +0100

    up to add method

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,10 +1,21 @@
-var SortedList = function() {};
+var SortedList = function() {
+  this.length=[];
+};
 
-SortedList.prototype.add     = function(item) {}
-SortedList.prototype.get     = function(pos) {}
-SortedList.prototype.max     = function() {}
-SortedList.prototype.min     = function() {}
-SortedList.prototype.average = function() {}
-SortedList.prototype.sum     = function() {}
+SortedList.prototype.add     = function(item) {
+  this.length.push(item);
+  this.length.sort(function (a,b){
+    return a > b;
+  });
+};
+
+SortedList.prototype.get     = function(pos) {
+
+  return this.lenght[pos-1];
+};
+SortedList.prototype.max     = function() {};
+SortedList.prototype.min     = function() {};
+SortedList.prototype.average = function() {};
+SortedList.prototype.sum     = function() {};
 
 module.exports = SortedList;
