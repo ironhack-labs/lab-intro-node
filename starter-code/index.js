cc7bc96aa012a3eea4651d45f90e79118dbc9488
FranRom <maildefranz@gmail.com>
2017-11-06 12:51:01 +0100

    working on iteration 2

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,10 +1,30 @@
-var SortedList = function() {};
+var SortedList = function() {
+this.length = [];
+// this.sortedlist = [];
+};
 
-SortedList.prototype.add     = function(item) {}
-SortedList.prototype.get     = function(pos) {}
-SortedList.prototype.max     = function() {}
-SortedList.prototype.min     = function() {}
-SortedList.prototype.average = function() {}
-SortedList.prototype.sum     = function() {}
+
+SortedList.prototype.add     = function(a, b) {
+  this.length.push(a);
+
+  for(i=0; i < this.length; i++){
+  this.length.sort(function(a,b) {
+    if (a > b) {
+      return -1;
+    };
+    if (a < b) {
+      return 1;
+    };
+    return 0;
+
+  });
+ };
+ };
+
+SortedList.prototype.get     = function(pos) {};
+SortedList.prototype.max     = function() {};
+SortedList.prototype.min     = function() {};
+SortedList.prototype.average = function() {};
+SortedList.prototype.sum     = function() {};
 
 module.exports = SortedList;
