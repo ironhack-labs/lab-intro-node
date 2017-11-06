59a08966f03576ab9e94a026fd1f76196fb65a32
Yurema Rebollo <yuremarc@gmail.com>
2017-11-06 12:53:12 +0100

    first commit

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,10 +1,14 @@
-var SortedList = function() {};
+var SortedList = function() {
+  this.list = [];
+  this.length = this.list.length;
+};
 
-SortedList.prototype.add     = function(item) {}
-SortedList.prototype.get     = function(pos) {}
-SortedList.prototype.max     = function() {}
-SortedList.prototype.min     = function() {}
-SortedList.prototype.average = function() {}
-SortedList.prototype.sum     = function() {}
+SortedList.prototype.add     = function(item) {this.list.push(item);};
+SortedList.prototype.get     = function(pos) {};
+SortedList.prototype.max     = function() {};
+SortedList.prototype.min     = function() {};
+SortedList.prototype.average = function() {};
+SortedList.prototype.sum     = function() {};
+
 
 module.exports = SortedList;
