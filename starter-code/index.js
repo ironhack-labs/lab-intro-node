e847fbfc178621c905f6a104ad5c6f2c7c05b332
Jean Nothomb <jeannothomb@gmail.com>
2017-11-06 15:48:15 +0100

    done

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,10 +1,50 @@
-var SortedList = function() {};
+var SortedList = function() {
+  this.list = [];
+  this.length = 0;
+};
 
-SortedList.prototype.add     = function(item) {}
-SortedList.prototype.get     = function(pos) {}
-SortedList.prototype.max     = function() {}
-SortedList.prototype.min     = function() {}
-SortedList.prototype.average = function() {}
-SortedList.prototype.sum     = function() {}
+// EXERCISE START
+
+// SortedList.prototype.size = function() {
+//   return this.list.length;
+// };
+
+SortedList.prototype.add = function(item) {
+  this.list.push(item);
+  this.list.sort(function(a, b) {
+    return a - b;
+  });
+  this.length++;
+};
+
+SortedList.prototype.get = function(pos) {
+  return this.list[pos - 1];
+};
+
+
+SortedList.prototype.max = function() {
+  return this.list[this.list.length - 1];
+};
+
+SortedList.prototype.min = function() {
+  return this.list[0];
+};
+
+SortedList.prototype.average = function() {
+  var total = 0;
+  for (var ix = 0; ix < this.list.length; ix++) {
+    total += this.list[ix];
+  }
+  var average = total / this.list.length;
+  return average;
+};
+
+SortedList.prototype.sum = function() {
+  var total = 0;
+  for (var ix = 0; ix < this.list.length; ix++) {
+    total += this.list[ix];
+  }
+  return total;
+};
 
 module.exports = SortedList;
