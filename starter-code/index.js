f13fa00150a1a386205481d2b7406077db38e816
Abraham Castro <cucharadepalo@gmail.com>
2017-11-06 16:42:15 +0100

    Finished

--- starter-code/index.js
+++ starter-code/index.js
@@ -5,15 +5,31 @@ var SortedList = function() {
 
 SortedList.prototype.add  = function(item) {
   this.list.push(item);
-  this.list.sort();
+  this.list.sort(function(a,b) { return a-b});
   this.length++;
 }
 SortedList.prototype.get     = function(pos) {
-  return this.list[pos - 1];
+  return this.list.length < pos ? "OutOfBounds" : this.list[pos - 1];
 }
-SortedList.prototype.max     = function() {}
-SortedList.prototype.min     = function() {}
-SortedList.prototype.average = function() {}
-SortedList.prototype.sum     = function() {}
+SortedList.prototype.max = function() {
+  if(this.list.length == 0) return 'EmptySortedList';
+  return Math.max(...this.list);
+}
+SortedList.prototype.min = function() {
+  if(this.list.length == 0) return 'EmptySortedList';
+  return Math.min(...this.list);
+}
+SortedList.prototype.average = function() {
+  if(this.list.length == 0) return 'EmptySortedList';
+  let suma = this.sum();
+  return suma / this.length;
+}
+SortedList.prototype.sum     = function() {
+  if(this.list.length == 0) return 'EmptySortedList';
+  let suma = this.list.reduce(function(sum, e) {
+    return sum + e;
+  })
+  return suma;
+}
 
 module.exports = SortedList;
