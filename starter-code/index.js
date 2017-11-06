90ba8c6a228ff18c05e0b6a25efd45c9904d3528
Nikolaas Verlee <nikotomad@gmail.com>
2017-11-06 12:52:03 +0100

    done

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,10 +1,40 @@
-var SortedList = function() {};
+var SortedList = function() {
+  this.length = [];
+};
 
-SortedList.prototype.add     = function(item) {}
-SortedList.prototype.get     = function(pos) {}
-SortedList.prototype.max     = function() {}
-SortedList.prototype.min     = function() {}
-SortedList.prototype.average = function() {}
-SortedList.prototype.sum     = function() {}
+SortedList.prototype.add     = function(item) {
+  this.length.push(item);
+}
+
+SortedList.prototype.get     = function(pos) {
+  var asc = this.length.sort();
+  return asc[pos - 1];
+}
+
+SortedList.prototype.max     = function() {
+  try {
+    return Math.max.apply(null, this.length);
+  } catch (e) {
+    return 'EmptySortedList';
+  }
+}
+
+SortedList.prototype.min     = function() {
+  return Math.min.apply(null, this.length);
+}
+
+SortedList.prototype.average = function() {
+  // for(var i=0; i < this.length.length; i++) {
+  //   var sum += this.length[i];
+  //   var avg = sum / this.length.length;
+  // }
+  // return avg;
+}
+SortedList.prototype.sum     = function() {
+  // for(var i=0; i < this.length.length; i++) {
+  //   var sum += this.length[i];
+  // }
+  // return sum;
+}
 
 module.exports = SortedList;
