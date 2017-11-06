cc74ca8daf78415f381445fdafd1caabba17201b
Anyelina Moreno Robledo <anyelinamoreno@gmail.com>
2017-11-06 12:52:13 +0100

    MAD-Asier Cisneros-Angelina

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,7 +1,15 @@
-var SortedList = function() {};
+var SortedList = function() {
+  this.length = [];
+};
 
-SortedList.prototype.add     = function(item) {}
-SortedList.prototype.get     = function(pos) {}
+SortedList.prototype.add     = function(item) {
+  this.length.push(item);
+  this.length.sort();
+}
+SortedList.prototype.get     = function(pos) {
+  return this.length[pos - 1]
+}
+
 SortedList.prototype.max     = function() {}
 SortedList.prototype.min     = function() {}
 SortedList.prototype.average = function() {}
