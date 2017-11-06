32ce9d3611ac0f65ba14448fc0918930357fa9b5
Juan Vallejo-Nagera <juanvallejo-nagera@Juans-MacBook-Pro.local>
2017-11-06 12:51:32 +0100

    stuck in add

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,10 +1,16 @@
-var SortedList = function() {};
+var SortedList = function() {
+  this.length = [];
+  // this.length = this.list.length;
+};
 
-SortedList.prototype.add     = function(item) {}
-SortedList.prototype.get     = function(pos) {}
-SortedList.prototype.max     = function() {}
-SortedList.prototype.min     = function() {}
-SortedList.prototype.average = function() {}
-SortedList.prototype.sum     = function() {}
+SortedList.prototype.add     = function(item) {
+this.length.push(item);
+this.length.sort();
+};
+SortedList.prototype.get     = function(pos) {};
+SortedList.prototype.max     = function() {};
+SortedList.prototype.min     = function() {};
+SortedList.prototype.average = function() {};
+SortedList.prototype.sum     = function() {};
 
 module.exports = SortedList;
