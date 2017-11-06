bcee61d019176f3fba7fbee74dc79690ce0fe82e
Pablo Gutierrez Barcelo <pablo@gutierrezbarcelo.com>
2017-11-06 12:52:08 +0100

    Last iteration get

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,10 +1,34 @@
-var SortedList = function() {};
+var SortedList = function() {
+  this.length = [];
+};
 
-SortedList.prototype.add     = function(item) {}
-SortedList.prototype.get     = function(pos) {}
-SortedList.prototype.max     = function() {}
-SortedList.prototype.min     = function() {}
-SortedList.prototype.average = function() {}
+SortedList.prototype.add = function(item) {
+  this.length.push(item)
+  this.length.sort();
+}
+SortedList.prototype.get = function(pos) {
+  if (this.length.length === 0){
+    return "OutOfBounds";
+  } else {
+  return this.length[pos-1];
+}
+}
+SortedList.prototype.max = function() {
+  if (this.length.length === 0){
+    return [];
+  } else{
+  return Math.max(this.length);
+  }
+}
+SortedList.prototype.min = function() {
+  return Math.min(this.length);
+}
+SortedList.prototype.average = function() {
+
+}
 SortedList.prototype.sum     = function() {}
+SortedList.prototype.size    = function() {
+  return this.size();
+}
 
 module.exports = SortedList;
