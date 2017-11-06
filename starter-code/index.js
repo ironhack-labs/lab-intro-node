0ea0350469d22c921007f832f131ffe56b88a0b7
Sara Morales <saringa@gmail.com>
2017-11-06 16:07:23 +0100

    sara/dafne

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,10 +1,19 @@
-var SortedList = function() {};
+var SortedList = function() {
+  this.length = 0;
+  this.list = [];
+};
 
-SortedList.prototype.add     = function(item) {}
-SortedList.prototype.get     = function(pos) {}
-SortedList.prototype.max     = function() {}
-SortedList.prototype.min     = function() {}
+
+SortedList.prototype.add = function(item) {
+  this.list.push(item);
+  this.list.sort();
+  this.length++;
+};
+
+SortedList.prototype.get = function(pos) {}
+SortedList.prototype.max = function() {}
+SortedList.prototype.min = function() {}
 SortedList.prototype.average = function() {}
-SortedList.prototype.sum     = function() {}
+SortedList.prototype.sum = function() {}
 
 module.exports = SortedList;
