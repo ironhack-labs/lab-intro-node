3d980fb54473faa593f989ee9510c0c7522e6cf3
Eduardo Rodés <eduardorodes@MacBook-Air-de-Eduardo.local>
2017-11-06 12:53:21 +0100

    Trying to figure out why SortedLi #get(i) is failing using console.log

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,10 +1,28 @@
-var SortedList = function() {};
+function SortedList () {
+  this.length = [];
+};
 
-SortedList.prototype.add     = function(item) {}
-SortedList.prototype.get     = function(pos) {}
+SortedList.prototype.add = function(item) {
+  this.length.push(item);
+  this.length.sort();
+};
+
+SortedList.prototype.get = function(pos) {
+    return this.length[pos-1];
+};
+
 SortedList.prototype.max     = function() {}
 SortedList.prototype.min     = function() {}
 SortedList.prototype.average = function() {}
 SortedList.prototype.sum     = function() {}
 
 module.exports = SortedList;
+
+
+// var sl = new SortedList;
+//
+// var foo = 10;
+// for(i=1; i<200; i++) {
+//   sl.add(foo*i);
+//   console.log(i + " " + sl.get(i));
+// };
