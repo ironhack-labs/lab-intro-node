48783021bb096446d0db9f859361d0c5f2b33720
Shincero <mp2772@gmail.com>
2017-11-06 15:25:28 -0500

    initial commit

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,10 +1,30 @@
-var SortedList = function() {};
 
-SortedList.prototype.add     = function(item) {}
-SortedList.prototype.get     = function(pos) {}
-SortedList.prototype.max     = function() {}
-SortedList.prototype.min     = function() {}
-SortedList.prototype.average = function() {}
-SortedList.prototype.sum     = function() {}
+var SortedList = function() {
+
+};
+
+SortedList.prototype = function () {
+  this.list = [];
+};
+
+
+SortedList.prototype.add     = function(item) {
+  list.push(this.item);
+};
+SortedList.prototype.get     = function(pos) {
+
+};
+SortedList.prototype.max     = function() {
+
+};
+SortedList.prototype.min     = function() {
+
+};
+SortedList.prototype.average = function() {
+
+};
+SortedList.prototype.sum     = function() {
+
+};
 
 module.exports = SortedList;
