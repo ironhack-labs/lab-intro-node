849c20d8832b310fb8576de8edc031608d7854d6
Juan Vallejo-Nagera <juanvallejo-nagera@Juans-MacBook-Pro.local>
2017-11-06 17:30:00 +0100

    average left

--- starter-code/index.js
+++ starter-code/index.js
@@ -19,16 +19,17 @@ SortedList.prototype.min     = function() {
   return this.length[0];
 };
 SortedList.prototype.average = function() {
-  if(this.length.length!==0){
+  if(this.length.length>0){
   return this.sum/(this.length.length);
 }
 };
 SortedList.prototype.sum     = function() {
-  if(this.length.length!==0){
-  this.length.reduce(function(a,b){
-    return a + b;
-  });
-}
+  console.log(this.sum);
+  if(this.length.length>0){
+    return this.length.reduce(function(a,b){
+      return a + b;
+    });
+  }
 };
 
 module.exports = SortedList;
