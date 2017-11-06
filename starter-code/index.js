15bf292e4992bf6b4531e0ab3ab2da18b9dfc48f
Dafne <olca.dafne@gmail.com>
2017-11-06 19:59:03 +0100

    done

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,10 +1,45 @@
-var SortedList = function() {};
+"use strict";
 
-SortedList.prototype.add     = function(item) {}
-SortedList.prototype.get     = function(pos) {}
-SortedList.prototype.max     = function() {}
-SortedList.prototype.min     = function() {}
-SortedList.prototype.average = function() {}
-SortedList.prototype.sum     = function() {}
+var SortedList = function() {
+  this.length=0;
+  this.list=[];
+};
+
+SortedList.prototype.add = function(item) {
+  this.list.push(item);
+  this.length++;
+  if (this.length>1) {
+    this.list.sort(function(a, b) {
+      return a-b;
+    });
+  }
+};
+
+SortedList.prototype.get     = function(pos) {
+  return this.list[pos-1];
+};
+
+SortedList.prototype.max     = function() {
+  return this.list[this.list.length-1];
+};
+
+SortedList.prototype.min     = function() {
+  return this.list[0];
+};
+
+SortedList.prototype.average = function() {
+  var average = 0;
+  for (var i=0; i<=this.list.length-1; i++) {
+    average = average + this.list[i];
+  }
+  return average / this.list.length;
+};
+
+SortedList.prototype.sum     = function() {
+  var sum=0;
+  for (var i=0; i<=this.list.length; i++) {
+    sum += this.list[i];
+  }
+};
 
 module.exports = SortedList;
