0e14f1003aad45877596343b764dd529192f30c4
Antonella Briatore <antobria@gmail.com>
2017-12-24 12:19:08 +0100

    feat: Solo pasan 10 tests

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,10 +1,29 @@
-var SortedList = function() {};
-
-SortedList.prototype.add     = function(item) {}
-SortedList.prototype.get     = function(pos) {}
-SortedList.prototype.max     = function() {}
-SortedList.prototype.min     = function() {}
-SortedList.prototype.average = function() {}
-SortedList.prototype.sum     = function() {}
+class SortedList {
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
+  add (item){
+    this.items.push(item);
+    this.items = this.items.sort((a, b) => a - b);
+  }
+  get (pos){
+    return this.items[pos - 1];
+  }
+  max (){
+    return this.items[this.items.length];
+  }
+  min (){
+    return this.items[0];
+  }
+  average (){
+    let sum = this.items.reduce((a, b) => a += b);
+    return avg = sum / this.items.length;
+  }
+  sum (){
+    return sum = this.items.reduce((a, b) => a += b);
+  }
+}
+// no me salen todos los tests aprobados pero no sé donde está el error!//
 
 module.exports = SortedList;
