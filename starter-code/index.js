99a4c2da5876938e30a7a6a85ce963b6fc229740
Cristina Bertrand <cristina@11870.com>
2018-01-06 18:11:23 +0100

    feat: 10 passed test completed, until get method

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,7 +1,23 @@
-var SortedList = function() {};
+var SortedList = function() {
+  this.items = [];
+  this.length = this.items.length;
+};
 
-SortedList.prototype.add     = function(item) {}
-SortedList.prototype.get     = function(pos) {}
+SortedList.prototype.add     = function(item) {
+ this.items.push(item);
+ this.items.sort(function(a, b) {
+      return a - b;
+  });
+  this.length = this.items.length;
+   };
+
+SortedList.prototype.get     = function(pos) {
+  if (pos>this.length){
+    return "OutOfBounds";
+  } else {
+    return this.items[pos-1];
+  }
+}
 SortedList.prototype.max     = function() {}
 SortedList.prototype.min     = function() {}
 SortedList.prototype.average = function() {}
