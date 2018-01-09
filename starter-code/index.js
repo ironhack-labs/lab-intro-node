0e26704d97ed9512b4b37a45fd121b51a36848d2
Rak <lovetacirupeca@gmail.com>
2018-01-09 05:33:59 +0100

    feat: I think I forget all -.-

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,9 +1,36 @@
-var SortedList = function() {};
+// var SortedList = function() {};
 
-SortedList.prototype.add     = function(item) {}
-SortedList.prototype.get     = function(pos) {}
-SortedList.prototype.max     = function() {}
-SortedList.prototype.min     = function() {}
+class SortedList {
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
+
+add(x) {
+  this.items.push(x);
+  this.length.sort((a, b) => {
+    return a - b;
+  });
+}
+
+get() {
+
+}
+
+max() {
+  return Math.max(...this.items)
+}
+
+min() {
+  return this.items[0];
+}
+
+}
+
+// SortedList.prototype.add     = function(item) {}
+// SortedList.prototype.get     = function(pos) {}
+// SortedList.prototype.max     = function() {}
+// SortedList.prototype.min     = function() {}
 SortedList.prototype.average = function() {}
 SortedList.prototype.sum     = function() {}
 
