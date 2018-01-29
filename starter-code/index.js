49333567d1882b4d43fa6d932059d272005dee44
Jose Sanchez <joseansanher@gmail.com>
2018-01-29 17:28:02 +0100

    done

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,10 +1,28 @@
-var SortedList = function() {};
+class SortedList {
+    constructor() {
+        this.items = [];
+        this.length = 0;
+    }
+    add(x) {
+        this.items.push(x);
+        this.length++;
+        this.items.sort();
+    }
+    get(ith) {
+        return this.items[(ith - 1)];
+    }
+    max() {
+        return ((this.items.length)-1);
+    }
 
-SortedList.prototype.add     = function(item) {}
-SortedList.prototype.get     = function(pos) {}
-SortedList.prototype.max     = function() {}
-SortedList.prototype.min     = function() {}
-SortedList.prototype.average = function() {}
-SortedList.prototype.sum     = function() {}
+};
+
+// SortedList.prototype.add = function (item) { }
+
+//SortedList.prototype.get = function (pos) { }
+// SortedList.prototype.max = function () { }
+// SortedList.prototype.min = function () { }
+// SortedList.prototype.average = function () { }
+// SortedList.prototype.sum = function () { }
 
 module.exports = SortedList;
