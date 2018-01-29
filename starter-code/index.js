4a7137f2a9b4ab1d0fbe9ab991896331399ee433
Oscar Jorge <oscar.jorge.jorge@hotmail.com>
2018-01-29 17:56:18 +0100

    finito maybe

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,10 +1,54 @@
-var SortedList = function() {};
+var SortedList = function() {
+    this.items = [];
+    this.length = this.items.length;
+};
 
-SortedList.prototype.add     = function(item) {}
-SortedList.prototype.get     = function(pos) {}
+
+SortedList.prototype.add     = function(item) {
+    this.items.push(item);
+    this.length = this.items.length;
+    this.items.sort((a,b) => {
+        return a-b;
+    }) 
+}
+
+SortedList.prototype.get     = function(pos) {
+    return this.items[pos - 1];
+
+    
+}
 SortedList.prototype.max     = function() {}
 SortedList.prototype.min     = function() {}
 SortedList.prototype.average = function() {}
 SortedList.prototype.sum     = function() {}
 
+
+
+
 module.exports = SortedList;
+
+// class SortedList {
+//     constructor(items, length) {
+//         this.items = items;
+//         this.length = length;
+//     }
+
+//     add(item) {
+//         return this.items + this.item;
+//     }
+
+//     get(pos) {}
+
+//     max() {}
+
+//     min() {}
+
+//     average() {}
+
+//     sum() {}
+// }
+
+// module.exports = SortedList;
+
+// var list = new SortedList (items, length);
+
