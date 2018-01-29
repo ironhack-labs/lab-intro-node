d676452c61bb2e5721b16de8a30d86eb9374f0b6
Daniel Thurdekoos <danielkoos@gmail.com>
2018-01-29 18:03:37 +0100

    Finished

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,3 +1,5 @@
+/*
+
 var SortedList = function() {};
 
 SortedList.prototype.add     = function(item) {}
@@ -6,5 +8,52 @@ SortedList.prototype.max     = function() {}
 SortedList.prototype.min     = function() {}
 SortedList.prototype.average = function() {}
 SortedList.prototype.sum     = function() {}
+*/
+
+class SortedList{
+  constructor (){
+    this.item = [];
+    this.length = 0;
+
+  }
+
+  add(item){
+    this.item.push(item);
+    this.length = this.item.length;
+    this.item.sort((a, b) => a - b);
+  }
+
+  get(pos) {
+    
+     return this.item[pos-1];
+    
+  }
+
+  max(){
+    return Math.max(...this.item);
+  }
+
+  min(){
+    return Math.min(...this.item);
+  }
+
+  average(){
+    return this.sum()/this.item.length;
+  }
+  
+  sum(){
+    if(this.length === 0){
+      return "EmptySortedList";
+    }
+    return this.item.reduce((accumulator, currentValue) => accumulator + currentValue);
+  }
+
+
+}
+
+
+
+
 
 module.exports = SortedList;
+
