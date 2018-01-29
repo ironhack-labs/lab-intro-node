74fe9ad605c4a6d47f01c50bd60d1b45755cd20f
VladC <ciobanu.vlad@yahoo.com>
2018-01-29 17:57:45 +0100

    to easy

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,10 +1,18 @@
-var SortedList = function() {};
+// / SortedList.prototype.get = function (pos) {};
+// SortedList.prototype.max = function () {};
+// SortedList.prototype.min = function () {};
+// SortedList.prototype.average = function () {};
+// SortedList.prototype.sum = function () {};
 
-SortedList.prototype.add     = function(item) {}
-SortedList.prototype.get     = function(pos) {}
-SortedList.prototype.max     = function() {}
-SortedList.prototype.min     = function() {}
-SortedList.prototype.average = function() {}
-SortedList.prototype.sum     = function() {}
+class SortedList {
+  constructor () {
+    this.items = [];
+    this.length = this.items.length;
+  }
+  add (item) {
+    this.items.push(item);
+    this.length++;
+  }
+}
 
 module.exports = SortedList;
