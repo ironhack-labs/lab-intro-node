638984b1a5bec51ac8bdab498061f863a7425682
Pere Amorós <amorospere@gmail.com>
2018-01-29 17:57:43 +0100

    easy arrow function pls gg wp x 2

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,3 @@
-// var SortedList = function () {};
-// SortedList.prototype.add = function (item) {};
-// SortedList.prototype.get = function (pos) {};
-// SortedList.prototype.max = function () {};
-// SortedList.prototype.min = function () {};
-// SortedList.prototype.average = function () {};
-// SortedList.prototype.sum = function () {};
-
 class SortedList {
   constructor () {
     this.items = [];
