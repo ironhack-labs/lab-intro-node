f8b77608f82fa88216a443865ce993c96143d830
Pere Amorós <amorospere@gmail.com>
2018-01-29 17:57:01 +0100

    easy arrow function pls gg wp

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,10 +1,53 @@
-var SortedList = function() {};
+// var SortedList = function () {};
+// SortedList.prototype.add = function (item) {};
+// SortedList.prototype.get = function (pos) {};
+// SortedList.prototype.max = function () {};
+// SortedList.prototype.min = function () {};
+// SortedList.prototype.average = function () {};
+// SortedList.prototype.sum = function () {};
 
-SortedList.prototype.add     = function(item) {}
-SortedList.prototype.get     = function(pos) {}
-SortedList.prototype.max     = function() {}
-SortedList.prototype.min     = function() {}
-SortedList.prototype.average = function() {}
-SortedList.prototype.sum     = function() {}
+class SortedList {
+  constructor () {
+    this.items = [];
+    this.length = this.items.length;
+  }
+
+  add (item) {
+    this.items.push(item);
+    this.items.sort((a, b) => a - b);
+    this.length = this.items.length;
+  }
+
+  get (i) {
+    if (i <= this.length && i >= 0) {
+      return this.items[i - 1];
+    }
+  }
+
+  max () {
+    return Math.max(...this.items);
+  }
+
+  min () {
+    return Math.min(...this.items);
+  }
+
+  average () {
+    if (this.length === 0) {
+      return this.items;
+    } else {
+      const sum = this.items.reduce((acc, currentValue) => acc + currentValue);
+      return sum / this.length;
+    }
+  }
+
+  sum () {
+    if (this.length === 0) {
+      return this.items;
+    } else {
+      return this.items.reduce((acc, currentValue) => acc + currentValue);
+    }
+  }
+}
 
 module.exports = SortedList;
