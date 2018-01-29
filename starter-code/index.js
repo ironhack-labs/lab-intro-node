1561bfd1e7ec213f7efddb61f83e87d13e61c582
Adrián Chacón <adrianchaconmadueno@gmail.com>
2018-01-29 14:16:48 +0100

    Pair programming

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,10 +1,32 @@
-var SortedList = function() {};
-
-SortedList.prototype.add     = function(item) {}
-SortedList.prototype.get     = function(pos) {}
-SortedList.prototype.max     = function() {}
-SortedList.prototype.min     = function() {}
-SortedList.prototype.average = function() {}
-SortedList.prototype.sum     = function() {}
+class SortedList {
+    constructor() {
+        this.items = [];
+        this.length = 0
+    }
+    add(item) {
+        this.length++;
+        this.items.push(item);
+        this.items.sort((a, b) => (a - b));
+    }
+    get(pos, length) {
+        return this.items[pos - 1]
+    }
+    max() {
+        return this.items.sort()[this.length - 1]
+    }
+    min() {
+        return this.items.sort()[0]
+    }
+    average() {
+        if (this.length != 0) {
+            return ((this.items.reduce((e, acc) => e + acc)) / this.length)
+        }
+    }
+    sum() {
+        if (this.length != 0) {
+            return this.items.reduce((e, acc) => e + acc)
+        }
+    }
+};
 
 module.exports = SortedList;
