eff81e3fd76bc69baba764bac2630108f21a57fb
Alberto Soler Muñoz <albertosolermunoz@MacBook-Pro-de-Alberto.local>
2018-01-29 16:02:59 +0100

    terminado

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,10 +1,45 @@
-var SortedList = function() {};
+class SortedList {
+        constructor () {
+         this.items = [];
+         this.length = 0;
 
-SortedList.prototype.add     = function(item) {}
-SortedList.prototype.get     = function(pos) {}
-SortedList.prototype.max     = function() {}
-SortedList.prototype.min     = function() {}
-SortedList.prototype.average = function() {}
-SortedList.prototype.sum     = function() {}
+     }
+
+
+
+add(item) {
+        this.length++;
+        this.items.push(item);
+        this.items.sort((a, b) => {return a - b})
+
+
+}
+
+
+
+get(pos) {
+
+   return this.items[pos - 1];
+    
+}
+max() {
+    return this.items[this.length -1];
+};
+min() {
+    return this.items[0];
+};
+average () {
+    if (this.items.length !== 0){
+    return this.items.reduce((acc, e) => (acc + e)) / this.length;
+    }
+}
+sum() {
+    if (this.items.length !== 0){
+    return this.items.reduce((acc, e) => (acc + e));
+}
+}
+};
+
+
 
 module.exports = SortedList;
