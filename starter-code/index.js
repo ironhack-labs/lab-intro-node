3a9b0795cb5ecb294cf112210a318ee2d81ea403
Lucas Martin Calderon <lucasmartincalderon1@gmail.com>
2018-01-29 15:29:18 +0100

    All done!

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,42 @@
-var SortedList = function() {};
+// var SortedList = function() {};
 
-SortedList.prototype.add     = function(item) {}
-SortedList.prototype.get     = function(pos) {}
-SortedList.prototype.max     = function() {}
-SortedList.prototype.min     = function() {}
-SortedList.prototype.average = function() {}
-SortedList.prototype.sum     = function() {}
+// SortedList.prototype.add     = function(item) {}
+// SortedList.prototype.get     = function(pos) {}
+// SortedList.prototype.max     = function() {}
+// SortedList.prototype.min     = function() {}
+// SortedList.prototype.average = function() {}
+// SortedList.prototype.sum     = function() {}
 
+
+class SortedList {
+    constructor() {
+        this.items = [];
+        this.length = 0
+    }
+    add(item) {
+      this.items.push(item);
+      this.length++;
+      this.items.sort((a, b) => (a - b));
+    }
+    get (ind, length) {
+      return this.items[ind-1];
+    }
+    max () {
+      return this.items.sort((a, b) => (a - b))[this.length - 1];
+    }
+    min (){
+       return this.items.sort((a, b) => (a - b))[0];
+    }
+    average() {
+      if (this.length !== 0) {
+        return ((this.items.reduce((total, acc) => total + acc)) / this.length)
+      }
+    }
+    sum () {
+      if (this.length !== 0) {
+      return this.items.reduce((total, acc) => total + acc);
+    }
+  }
+}
 
 module.exports = SortedList;
