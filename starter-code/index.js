881376e615b84ed14f997ceb059d44af5a3c1ab2
Alvaro <echanovevaro@gmail.com>
2018-01-29 18:19:55 +0100

    terminado

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,10 +1,60 @@
-var SortedList = function() {};
+class SortedList {
+	constructor(...items) {
+		this.items = items;
+		this.length = items.length;
+	}
 
-SortedList.prototype.add     = function(item) {}
-SortedList.prototype.get     = function(pos) {}
-SortedList.prototype.max     = function() {}
-SortedList.prototype.min     = function() {}
-SortedList.prototype.average = function() {}
-SortedList.prototype.sum     = function() {}
+	add(...items) {
+		this.items = [...this.items, ...items];
+		this.items = this.items.sort((a, b) => {
+			return a - b;
+		});
+		this.length += items.length;
+	}
+
+	get(pos) {
+		if (pos - 1 < this.length) {
+			return this.items[pos - 1];
+		} else {
+			console.log("OutOfBounds");
+		}
+	}
+
+	max() {
+		if (this.length > 0) {
+			return this.get(this.length);
+		} else {
+			console.log("EmptySortedList");
+		}
+	}
+
+	min() {
+		if (this.length > 0) {
+			return this.get(1);
+		} else {
+			console.log("EmptySortedList");
+		}
+	}
+
+	average() {
+		if (this.length > 0) {
+			return Math.floor(this.sum() / this.length);
+		} else {
+			console.log("EmptySortedList");
+		}
+	}
+
+	sum() {
+		if (this.length > 0) {
+			return this.items.reduce((total, num) => {
+				return total + num;
+			}, 0);
+		} else {
+			console.log("EmptySortedList");
+		}
+	}
+}
 
 module.exports = SortedList;
+	
+
