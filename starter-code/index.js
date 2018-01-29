1c4c02a2233b646b679d4d686702aea43a191867
nachobaile <nachobaile@gmail.com>
2018-01-29 16:17:11 +0100

    siguiendo

--- starter-code/index.js
+++ starter-code/index.js
@@ -14,7 +14,41 @@ class SortedList {
     this.length = 0;
   }
   add(x){
-      
+    this.items.push(x);
+    this.items.sort(function(a,b){
+        return a-b;
+    })
+    this.length++;   
+  }
+  get(x){
+      return this.items[x-1]; 
+  }
+  max(){
+    if(this.length==0){
+          return "EmptySortedList";
+    } 
+    else{
+     this.items.reduce (function (a,b){
+       return Math.max(a,b);
+     })
+ }     
+  }
+  min(){
+    if(this.length==0){
+        return "EmptySortedList";
+  } 
+  else{
+   this.items.reduce (function (a,b){
+     return Math.min(a,b);
+   })
+}     
+
+  }
+  average(){
+
+  }
+  sum(){
+
   }
 }
 module.exports = SortedList;
