2b8286990f0c4ffd263fdd01470def5854da63f4
AlbertoSP <ALBERTOSANCHEZHTML@GMAIL.COM>
2018-01-29 15:34:52 +0100

    done, falta el último punto de todos

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,10 +1,65 @@
-var SortedList = function() {};
-
-SortedList.prototype.add     = function(item) {}
-SortedList.prototype.get     = function(pos) {}
-SortedList.prototype.max     = function() {}
-SortedList.prototype.min     = function() {}
-SortedList.prototype.average = function() {}
-SortedList.prototype.sum     = function() {}
-
-module.exports = SortedList;
+class SortedList {
+    constructor(items, length) {
+        this.items = [],
+            this.length = 0
+    }
+    add(item) {
+        this.length++;
+        this.items.push(item);
+        this.items.sort((a, b) => (a - b));
+ 
+    }
+    get(pos) {
+        return this.items[pos - 1];
+    }
+    max() {
+        return this.items.sort()[this.length - 1];
+    }
+    min() {
+        return this.items.sort()[0]
+    }
+    average() {
+        if (this.length != 0) {
+          return ((this.items.reduce((e, acc) => e + acc)) / this.length)
+        }
+    }
+    sum() {
+        class SortedList {
+    constructor(items, length) {
+        this.items = [],
+            this.length = 0
+    }
+    add(item) {
+        this.items.push(item);
+        this.length++;
+        this.items.sort((a, b) => (a - b));
+ 
+    }
+    get(pos) {
+        return this.items[pos - 1];
+    }
+    max() {
+        return this.items.sort()[this.length - 1];
+    }
+    min() {
+        return this.items.sort()[0]
+    }
+    average() {
+        if (EmptySortedList == false) {
+          return ((this.items.reduce((e, acc) => e + acc)) / this.length)
+        }
+    }
+    sum() {
+        if (EmptySortedList !=0) {
+            return this.items.reduce((acc,e) => acc + e ,0); 
+          }
+    }
+ };
+ 
+ module.exports = SortedList;
+ 
+    }
+ };
+ module.exports = SortedList;
+ 
+ 
