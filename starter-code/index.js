758e10d51fa141b8ccf2da9fefaff565e375ffc2
Jeremy Fox <jeremyfox@Jeremys-MacBook-Pro.local>
2018-02-12 12:42:38 -0500

    add my solutions to node intro exercise

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,10 +1,46 @@
-var SortedList = function() {};
+var SortedList = function(items, length) {
+    this.items = [];
+    this.length = 0;
+ };
+    
+ SortedList.prototype.add     = function(item) {
+    this.items.push(item);
+    this.items.sort((a,b)=>{
+        return a - b;
+      });
+    this.length = this.items.length;
+ };
+ 
+ SortedList.prototype.get     = function(pos) {
+    if(this.length === 0)
+        return;
+    return this.items[pos - 1];
+ }
+ SortedList.prototype.max     = function() {
+    if(this.length === 0)
+        return;
+    return this.items[this.length - 1];
+ }
+ SortedList.prototype.min     = function() {
+    if(this.length === 0)
+        return;
+    return this.items[0];
+ }
 
-SortedList.prototype.add     = function(item) {}
-SortedList.prototype.get     = function(pos) {}
-SortedList.prototype.max     = function() {}
-SortedList.prototype.min     = function() {}
-SortedList.prototype.average = function() {}
-SortedList.prototype.sum     = function() {}
 
-module.exports = SortedList;
+ SortedList.prototype.average = function() {
+    return this.sum() / this.items.length; 
+    
+ }
+
+
+ SortedList.prototype.sum     = function() {
+    if(this.length === 0)
+        return;
+    var sum = this.items.reduce(function(acc, item){
+        return acc + item;
+    })
+    return sum;
+ }
+ 
+ module.exports = SortedList;
