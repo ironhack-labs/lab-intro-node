2922c72f4ef5eb3d38578695551a3a4b7c570bf1
Scott Rowell <rowell.scott@gmail.com>
2018-02-12 11:45:39 -0500

    Done

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,10 +1,29 @@
-var SortedList = function() {};
+var SortedList = function() {
+  this.items =[];
+  this.length = this.items.length;
+};
 
-SortedList.prototype.add     = function(item) {}
-SortedList.prototype.get     = function(pos) {}
-SortedList.prototype.max     = function() {}
-SortedList.prototype.min     = function() {}
-SortedList.prototype.average = function() {}
-SortedList.prototype.sum     = function() {}
+SortedList.prototype.add     = function(item) {
+    this.items.push(item);
+    this.items.sort(function(a, b){return a-b})
+    this.length = this.items.length
+}
+SortedList.prototype.get     = function(pos) {
+    return this.items[pos-1];
+}
+SortedList.prototype.max     = function() {
+  return Math.max(...this.items)
+}
+SortedList.prototype.min     = function() {
+  return Math.min(...this.items)
+}
+SortedList.prototype.average = function() {
+  let sum = this.items.reduce((prev, curr) => prev + curr, 0)
+  return (sum / this.length)
+  
+}
+SortedList.prototype.sum     = function() {
+  return this.items.reduce((prev, curr) => prev + curr, 0)
+}
 
 module.exports = SortedList;
