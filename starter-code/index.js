0bf8e0f3099b5b393bc7b47feb11e2c5593d9511
Torre <torre.locascio@gmail.com>
2018-02-12 12:11:44 -0500

    done

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,10 +1,27 @@
-var SortedList = function() {};
+var SortedList = function() {
+this.items = [];
+this.length = 0;
 
-SortedList.prototype.add     = function(item) {}
-SortedList.prototype.get     = function(pos) {}
-SortedList.prototype.max     = function() {}
-SortedList.prototype.min     = function() {}
-SortedList.prototype.average = function() {}
-SortedList.prototype.sum     = function() {}
+};
+
+SortedList.prototype.add     = function(item) {this.items.push(item); this.length=this.length+1}
+SortedList.prototype.get     = function(pos) {this.items[pos]}
+SortedList.prototype.max     = function() {return Math.max(...this.items)}
+SortedList.prototype.min     = function() {return Math.min(...this.items)}
+SortedList.prototype.average = function() {var average = 0;var newSum =this.sum();
+  average=newSum/this.items.length;
+  return average
+}
+
+
+SortedList.prototype.sum     = function() {var sum = 0
+  for (var i = 0;i<this.items.length;i++){
+   
+ sum = sum + this.items[i];
+
+}
+return sum
+}
+
 
 module.exports = SortedList;
