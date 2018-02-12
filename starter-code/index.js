a212f626737c198f66875f577e9a1585387f2f41
Rakeem <thomas.rakeem.29@gmail.com>
2018-02-12 11:38:01 -0500

    done

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,10 +1,41 @@
-var SortedList = function() {};
+function SortedList() {
+  this.items = [];
+  this.length = 0;
+}
 
-SortedList.prototype.add     = function(item) {}
-SortedList.prototype.get     = function(pos) {}
-SortedList.prototype.max     = function() {}
-SortedList.prototype.min     = function() {}
-SortedList.prototype.average = function() {}
-SortedList.prototype.sum     = function() {}
+SortedList.prototype.add = function(item) {
+  this.items.push(item);
+  this.items.sort((a, b) => {
+    return a - b;
+  });
+  this.length = this.items.length;
+};
+SortedList.prototype.get = function(pos) {
+  return this.items[pos];
+};
+SortedList.prototype.max = function() {
+  let max = 0;
+  this.items.forEach(number => {
+    number > max ? (max = number) : number;
+  });
+  return max;
+};
+SortedList.prototype.min = function() {
+  let min = this.items[0];
+  this.items.forEach(number => {
+    number < min ? (min = number) : number;
+  });
+  return min;
+};
+SortedList.prototype.average = function() {
+  let sum = this.sum();
+  return sum / this.items.length;
+};
+SortedList.prototype.sum = function() {
+  let sum = this.items.reduce((previous, next) => {
+    return previous + next;
+  }, 0);
+  return sum;
+};
 
 module.exports = SortedList;
