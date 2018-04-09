431b4ee6d1b735148f6215bb0a4bfa3b83469d94
Sami Abderrezak <sami.abderre@gmail.com>
2018-04-09 15:31:04 +0200

    done

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,10 +1,49 @@
-var SortedList = function() {};
+var SortedList = function() {
+  this.items = [];
+  this.length = this.items.length;
+};
 
-SortedList.prototype.add     = function(item) {}
-SortedList.prototype.get     = function(pos) {}
-SortedList.prototype.max     = function() {}
-SortedList.prototype.min     = function() {}
-SortedList.prototype.average = function() {}
-SortedList.prototype.sum     = function() {}
+SortedList.prototype.add  = function (x) {
+  
+  this.items.push(x);
+  this.items = this.items.sort();
+  this.length = this.items.length;
+}
+
+SortedList.prototype.get = function(pos) {
+  return this.items[pos-1] ;
+  
+}
+
+SortedList.prototype.max  = function() {
+  
+  if (this.items === 0) {
+   return [];
+  
+  } else {
+     return Math.max.apply(Math, this.items)
+  }
+}
+
+
+SortedList.prototype.min     = function() {
+  if (this.items === 0) {
+    return [];
+} else {
+  return Math.min.apply(Math, this.items)
+}
+}
+SortedList.prototype.average = function() {
+
+}
+SortedList.prototype.sum     = function() {
+
+}
 
 module.exports = SortedList;
+
+var sortedList1 = new SortedList(); 
+sortedList1.add() 
+
+
+
