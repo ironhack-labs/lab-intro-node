cc7264034ef81e992d7263d0288fe6903434a0bc
Tanis Pan <e.pan.gar@gmail.com>
2018-04-09 16:51:42 +0200

    I4

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,24 +1,56 @@
-//const 
-
 class SortedList {
-  
+
   constructor(items, length){
-    let sorted = new SortedList([], 0);
-    /*this.items = items;
-    this.length = length;*/
+    this.items = [];
+    this.length = this.items.length;
   };
 
-  add(item) {
-    this.items.push(item)
-  };
   
 
+  add(x) {
+    this.items.push(x)
+    this.items.sort(function(a,b){
+      return a-b
+    });
+
+  };
+
+  get(pos) {
+      if(pos < 0 && pos > this.items.length){
+        return "Out of bounds";
+      } else {
+      return (this.items[pos]);
+      }
+  }
+
+  max() {
+    
+    return Math.max.apply(null,this.items);
+
+  }
+
+  min() {
+    return Math.min.apply(null,this.items);
+  }
+
+  average() {
+  return this.sum()/this.items.length;
+
+  }
+  sum() {
+    if (this.items.length>0){
+      var addition = 0;
+      for(i=0;i<this.items.length; i++){
+        addition+=this.items[i];
+      }
+      return addition;
+    }
+  }
 };
 
-SortedList.prototype.get     = function(pos) {}
-SortedList.prototype.max     = function() {}
-SortedList.prototype.min     = function() {}
-SortedList.prototype.average = function() {}
-SortedList.prototype.sum     = function() {}
+
+var element = new SortedList();
 
 module.exports = SortedList;
+
+
