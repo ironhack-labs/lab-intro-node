aa555077e58fae5fa81041a4e0c75b86764607a5
Samantha Ferreira <samanthamferr@gmail.com>
2018-04-09 15:52:50 +0200

    not done with excersise :(

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,9 +1,31 @@
-var SortedList = function() {};
+function SortedList(items,length) {
+    this.items = [];
+    this.length = this.items.length;
+};
 
-SortedList.prototype.add     = function(item) {}
-SortedList.prototype.get     = function(pos) {}
-SortedList.prototype.max     = function() {}
-SortedList.prototype.min     = function() {}
+// function get(nth){
+//     var nth;
+//     this.items[nth];
+// }
+
+SortedList.prototype.add = function(item) {
+    this.items.push(item);
+    this.length = this.items.length;
+    this.items.sort(function(a,b){
+        return a > b;
+    });
+}
+SortedList.prototype.get     = function(pos) {
+    this.item[pos];
+    this.length;
+}
+SortedList.prototype.max     = function() {
+    this.items[0];
+}
+SortedList.prototype.min     = function() {
+    var min = this.items.length -1;
+    this.items[min];
+}
 SortedList.prototype.average = function() {}
 SortedList.prototype.sum     = function() {}
 
