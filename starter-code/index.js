fa0469183d130a75ac1eac278064c72db0d5de9c
muflonex <muflonex@gmail.com>
2018-04-09 15:18:12 +0200

    Hecha la clase. Seguimos luchando con el Add

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,6 +1,24 @@
-var SortedList = function() {};
+class SortedList {
+  constructor(){
+    this.items = []
+    this.length = this.items.length
+  }
+  add(x){
+    this.items.push(x)
+    this.length = this.items.length
+  }
+};
 
-SortedList.prototype.add     = function(item) {}
+// SortedList.prototype.add     = function(x) {
+//   if(x < this.items[this.items.length-1]){
+//     this.items.unshift(x)
+//     this.length++
+//   }
+//   else{
+//     this.items.push(x)
+//     this.length++
+//   }
+// }
 SortedList.prototype.get     = function(pos) {}
 SortedList.prototype.max     = function() {}
 SortedList.prototype.min     = function() {}
