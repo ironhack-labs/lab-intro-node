0ed1c9bc85feb55dbbd7e36c76f75a8d092755a9
Yago Vega <yagofvv@gmail.com>
2018-04-09 16:05:36 +0200

    done maradone

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,15 +1,33 @@
 class SortedList {
-  constructor (){
+  constructor() {
     this.items = [];
     this.length = this.items.length;
   }
-  add (item) {
+  add(item) {
     this.items.push(item);
+    this.update();
+    this.sort();
   }
-  get(pos) {}
-  max() {}
-  min() {}
-  average() {}
-  sum() {}
+  get(pos) {
+    return this.items[pos-1];
+  }
+  max() {
+    return Math.max.apply(null, this.items);
+  }
+  min() {
+    return Math.min.apply(null, this.items);
+  }
+  average() {
+    return this.sum()/this.items.length;
+  }
+  sum() {
+    return this.items.reduce((acc, e) => acc += e, 0);
+  }
+  update() {
+    this.length = this.items.length;
+  }
+  sort() {
+    this.items.sort((a,b)=>a-b)
+  }
 }
 module.exports = SortedList;
