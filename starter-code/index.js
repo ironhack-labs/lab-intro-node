546ae95bca52ea882e48e8d209640bda6fd25f75
Luna Fidalgo <lunafidalgo@gmail.com>
2018-04-09 15:43:56 +0200

    DONE

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,10 +1,36 @@
-var SortedList = function() {};
+class SortedList {
+  constructor(items, length) {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-SortedList.prototype.add     = function(item) {}
-SortedList.prototype.get     = function(pos) {}
-SortedList.prototype.max     = function() {}
-SortedList.prototype.min     = function() {}
-SortedList.prototype.average = function() {}
-SortedList.prototype.sum     = function() {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a, b) => a - b);
+    this.length = this.items.length;
+  }
+
+  get(pos) {
+    return this.items[pos - 1];
+  }
+
+  max() {
+    return this.items[this.items.length - 1];
+  }
+  min() {
+    return this.items[0];
+  }
+  sum() {
+    if (this.length === 0) {
+      return new Error("EmptySortedList");
+    } else {
+      return this.items.reduce((acc, e) => acc + e);
+    }
+  }
+
+  average() {
+    return this.sum() / this.items.length;
+  }
+}
 
 module.exports = SortedList;
