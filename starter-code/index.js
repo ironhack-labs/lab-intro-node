a01dc7d96262e377c3981b802a06d93da41346f5
Patrycja <kucharskap@gmail.com>
2018-04-09 14:49:01 +0200

    done

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,10 +1,56 @@
-var SortedList = function() {};
+var SortedList = function() {
+    this.items= [];
+    this.length = this.items.length;
+};
 
-SortedList.prototype.add     = function(item) {}
-SortedList.prototype.get     = function(pos) {}
-SortedList.prototype.max     = function() {}
-SortedList.prototype.min     = function() {}
-SortedList.prototype.average = function() {}
-SortedList.prototype.sum     = function() {}
+var sl = new SortedList ();
+
 
 module.exports = SortedList;
+
+
+SortedList.prototype.add     = function(item) {
+    this.items.push(item);
+    this.items.sort(function(a,b){
+        return a-b;
+        });
+    this.length = this.items.length;
+}
+
+SortedList.prototype.get     = function(pos) {
+    return this.items[pos-1];
+}
+
+SortedList.prototype.max     = function() {
+    return this.items[this.items.length-1];
+}
+
+SortedList.prototype.min     = function() {
+    return this.items[0];
+}
+
+SortedList.prototype.sum     = function() {
+    if(this.items.length === 0){
+        return this.items;
+    }
+    var theSum = 0;
+    for(var i=0; i<this.items.length; i++){
+        theSum += this.items[i];
+    }
+    return theSum;
+}
+
+SortedList.prototype.average = function() {
+    if(this.items.length === 0){
+        return this.items;
+    }
+
+    var theSum = 0;
+    for(var i=0; i<this.items.length; i++){
+        theSum += this.items[i];
+    }
+    
+   var leng = this.items.length;
+
+   return theSum/leng;
+}
