2431389d5fec7315ace0efc71c41ba9fa592791f
Claudia Hernandez <hernandez.clauds@gmail.com>
2018-04-23 10:42:53 -0400

    WDFT-Claudia-Hernandez-Robert-Gelbman-lab-intro-node

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,10 +1,36 @@
-var SortedList = function() {};
-
-SortedList.prototype.add     = function(item) {}
-SortedList.prototype.get     = function(pos) {}
-SortedList.prototype.max     = function() {}
-SortedList.prototype.min     = function() {}
-SortedList.prototype.average = function() {}
-SortedList.prototype.sum     = function() {}
-
+var SortedList = function() {
+  this.items = [];
+  this.length = 0;
+};
+SortedList.prototype.add     = function(item) {
+  this.items.push(item);
+  this.items.sort(function(a, b) {
+    return a - b;
+  });
+  this.length = this.items.length;
+}
+SortedList.prototype.get     = function(pos) {
+  return this.items[pos - 1];
+}
+SortedList.prototype.max     = function() {
+  return Math.max.apply(null, this.items);
+}
+SortedList.prototype.min     = function() {
+  return Math.min.apply(null, this.items);
+}
+SortedList.prototype.average = function() {
+  var sum = 0;
+  for (i=0; i < this.items.length; i++){
+    sum += this.items[i];
+  }
+  return sum/this.items.length;
+}
+SortedList.prototype.sum     = function() {
+  var sum = 0;
+  for (i=0; i < this.items.length; i++){
+    sum += this.items[i];
+  }
+  return sum;
+}
 module.exports = SortedList;
+
