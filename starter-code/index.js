2328491f604cc8d54964a780a91091e01213036d
Nicholas Kreps <nicholas.kreps@gmail.com>
2018-05-30 22:56:19 -0400

    Constructor function step 1 done

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,10 +1,15 @@
-var SortedList = function() {};
+var SortedList = function() {
+  this.items=[];
+  this.length=this.items.length;
+};
 
-SortedList.prototype.add     = function(item) {}
-SortedList.prototype.get     = function(pos) {}
-SortedList.prototype.max     = function() {}
-SortedList.prototype.min     = function() {}
-SortedList.prototype.average = function() {}
-SortedList.prototype.sum     = function() {}
+SortedList.prototype.add     = function(item) {
+this.items.push(this.item);
+};
+SortedList.prototype.get     = function(pos) {};
+SortedList.prototype.max     = function() {};
+SortedList.prototype.min     = function() {};
+SortedList.prototype.average = function() {};
+SortedList.prototype.sum     = function() {};
 
 module.exports = SortedList;
