06736735123c1d9e9aeeed80ed6df26d646904c3
Teresa Plaz <teresaplaz@gmail.com>
2018-06-01 02:33:47 -0400

    done :)

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,10 +1,34 @@
-var SortedList = function() {};
+var SortedList = function() {
+  this.items = [];
+  this.length = this.items.length;
+};
 
-SortedList.prototype.add     = function(item) {}
-SortedList.prototype.get     = function(pos) {}
-SortedList.prototype.max     = function() {}
-SortedList.prototype.min     = function() {}
-SortedList.prototype.average = function() {}
-SortedList.prototype.sum     = function() {}
+SortedList.prototype.add = function(item) {
+  this.items.push(item);
+  this.items = this.items.sort((a, b) => a - b);
+  this.length = this.items.length;
+}
+
+SortedList.prototype.get     = function(pos) {return this.items[pos-1]}
+
+SortedList.prototype.max     = function() { return Math.max(...this.items)}
+
+SortedList.prototype.min     = function() { return Math.min(...this.items)}
+
+SortedList.prototype.average = function() { 
+  if (this.items.length === 0) {return 'EmptySortedList'}
+  else { return this.items.reduce( (prev,current)=>{ return prev + current })/this.items.length
+  } 
+}
+
+SortedList.prototype.sum     = function() {
+  if (this.items.length === 0) {return 'EmptySortedList'}
+  else {
+    return this.items.reduce( (prev,current)=>{ return prev + current })
+  }
+}
 
 module.exports = SortedList;
+
+
+sl = new SortedList;
