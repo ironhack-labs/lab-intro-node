be234d46bb973d03b117319552f7bee1ebfbad79
Mike Dougllas <mdougllas@gmail.com>
2018-06-01 18:56:08 -0400

    Started working on the exercise

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,7 +1,18 @@
-var SortedList = function() {};
+var SortedList = function() {
+  this.items = [];
+  this.length = this.items.length;
+};
 
-SortedList.prototype.add     = function(item) {}
-SortedList.prototype.get     = function(pos) {}
+SortedList.prototype.add     = function(item) {
+  this.items.push(item);
+  this.items.sort(function(a, b){
+    return a - b;
+  });
+  this.length = this.items.length;
+}
+SortedList.prototype.get     = function(pos) {
+  return this.items[pos - 1];
+}
 SortedList.prototype.max     = function() {}
 SortedList.prototype.min     = function() {}
 SortedList.prototype.average = function() {}
