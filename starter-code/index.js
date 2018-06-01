6acd9a9abd7b83fbb35cb6bf280b9c3102671bd1
ahmouda12 <ahmouda12@ufl.edu>
2018-06-01 14:30:15 -0400

    first test

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,10 +1,31 @@
-var SortedList = function() {};
+var SortedList = function() {
+  // this.items = items;
+  this.length = [];
+};
 
-SortedList.prototype.add     = function(item) {}
-SortedList.prototype.get     = function(pos) {}
-SortedList.prototype.max     = function() {}
-SortedList.prototype.min     = function() {}
-SortedList.prototype.average = function() {}
-SortedList.prototype.sum     = function() {}
+SortedList.prototype.add     = function(item) {
+  this.length.push(item);
+  // this.length.sort(function(a,b) {
+  //   return a - b;
+  // });
+};
+SortedList.prototype.get     = function(pos) {
+  return this.length[pos - 1];
+}
+SortedList.prototype.max     = function() {
+  return Math.max.apply(null, this.length);
+}
+SortedList.prototype.min     = function() {
+  return Math.min.apply(null, this.length);
+}
+SortedList.prototype.average = function() {
+  let sum = this.length.reduce(function(a,b){
+    return a+b}, 0);
+  return sum/this.length.length;
+}
+SortedList.prototype.sum     = function() {
+  return this.length.reduce(function(a,b){
+    return a+b}, 0);
+}
 
 module.exports = SortedList;
