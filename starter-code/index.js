8bc01db5e6aea51815318f6c3f4dd1eb23727248
Mike Dougllas <mdougllas@gmail.com>
2018-06-01 20:19:51 -0400

    MIA-WD-PT-Mike Dougllas

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,21 +1,24 @@
 var SortedList = function() {
   this.items = [];
   this.length = this.items.length;
-};
+}
 
 SortedList.prototype.add     = function(item) {
   this.items.push(item);
-  this.items.sort(function(a, b){
-    return a - b;
-  });
+  this.items.sort(function(a, b){return a - b});
   this.length = this.items.length;
 }
 SortedList.prototype.get     = function(pos) {
   return this.items[pos - 1];
 }
-SortedList.prototype.max     = function() {}
-SortedList.prototype.min     = function() {}
-SortedList.prototype.average = function() {}
-SortedList.prototype.sum     = function() {}
+SortedList.prototype.max     = function() {return Math.max(...this.items);}
+SortedList.prototype.min     = function() {return Math.min(...this.items);}
+SortedList.prototype.average = function() {
+  this.sum = this.items.reduce(function(a,b){return a+b;}, 0);
+  return this.sum/this.length;
+}
+SortedList.prototype.sum     = function() {
+  return this.items.reduce(function(a,b){return a+b;}, 0);
+}
 
 module.exports = SortedList;
