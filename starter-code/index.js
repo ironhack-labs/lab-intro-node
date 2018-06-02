7605e0007fe270f383c53bfe44781bab4fe34032
Mariah Dunn <mariahdunn@Mariahs-MacBook-Pro.local>
2018-06-02 13:01:31 -0400

    hw begun

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,7 +1,26 @@
-var SortedList = function() {};
+var SortedList = function() {
+  this.length = 0;
+  this.items = [];
+};
 
-SortedList.prototype.add     = function(item) {}
-SortedList.prototype.get     = function(pos) {}
+SortedList.prototype.add = function(item) {
+  this.items.push(item);
+  this.items.sort(function(a,b) {
+    return a-b;
+  });
+  this.length++;
+}
+
+let item1 = new SortedList();
+item1.add(8);
+
+console.log(item1)
+
+SortedList.prototype.get     = function(i) {
+  const got = items[i];
+  return got;
+
+}
 SortedList.prototype.max     = function() {}
 SortedList.prototype.min     = function() {}
 SortedList.prototype.average = function() {}
