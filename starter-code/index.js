a4600a741f9c7f28198e0a5159d33ce7a221a5c2
me@seanzamora.com <me@seanzamora.com>
2018-06-01 20:40:59 -0400

    Refractured Code. Using standard/built in js methods such as  Math and Reduce

--- starter-code/index.js
+++ starter-code/index.js
@@ -2,36 +2,21 @@ var SortedList = function() {
     this.items = [];
     this.length = 0;
 };
-
 SortedList.prototype.add  = function(item) {
-        this.length += 1;
         this.items.push(item);
         this.items = this.items.sort(function(a, b){return a-b});
+        this.length = this.items.length;
 }
 SortedList.prototype.get = function(pos) {
     return (this.items[pos-1] == undefined) ? false : this.items[pos-1];
 }
 SortedList.prototype.max     = function() {
     if(this.length == 0){return 'EmptyList';}
-    let max = 0;
-    this.items.forEach(i => {
-        if(i >= max){
-            max = i;
-        }
-    });
-    return max;
+    return Math.max(...this.items);
 }
 SortedList.prototype.min     = function() {
     if(this.length == 0){return 'EmptyList';}
-    let min = null;
-    this.items.forEach(i => {
-        if(min == null){
-            min = i;
-        }else if(i <= min){
-            min = i;
-        }
-    });
-    return min;
+    return Math.min(...this.items);
 }
 SortedList.prototype.average = function() {
     if(this.length == 0){return 'EmptySortedList';}
@@ -39,11 +24,7 @@ SortedList.prototype.average = function() {
 }
 SortedList.prototype.sum     = function() {
     if(this.length == 0){return 'EmptySortedList';}
-    let sum = 0;
-    this.items.forEach(i => {
-       sum += i;
-    });
-    return sum;
+    return this.items.reduce((a, b) =>a + b);
 }
 
 module.exports = SortedList;
