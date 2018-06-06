a23c0d47c403a05025fcb510520354f0e081e9c6
williamdonayre <william.donayre@outlook.com>
2018-06-05 22:01:04 -0400

    Working-Almost done

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,9 +1,28 @@
-var SortedList = function() {};
+var SortedList = function() {
+    this.items = [];
+    this.length = this.items.length;
+};
 
-SortedList.prototype.add     = function(item) {}
-SortedList.prototype.get     = function(pos) {}
-SortedList.prototype.max     = function() {}
-SortedList.prototype.min     = function() {}
+SortedList.prototype.add     = function(item) {
+    this.items.push(item);
+    this.length = this.items.length;
+    this.items.sort(function(a, b){
+         return a - b; });
+
+};
+SortedList.prototype.get     = function(pos) {
+     return this.items [pos - 1];
+
+};
+SortedList.prototype.max     = function() {
+    //since sorted take the last number in array
+    return this.items [this.items.length - 1 ];
+
+};
+SortedList.prototype.min     = function() {
+    //since sorted take first number in array
+    return this.items [0];
+}
 SortedList.prototype.average = function() {}
 SortedList.prototype.sum     = function() {}
 
