9225ef4f7e6b87aa2a94bf81030104b45d9a4011
Chris Manthey <calined@gmx.de>
2018-06-09 22:27:43 +0200

    first commit

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,10 +1,10 @@
-var SortedList = function() {};
+var SortedList = function () { };
 
-SortedList.prototype.add     = function(item) {}
-SortedList.prototype.get     = function(pos) {}
-SortedList.prototype.max     = function() {}
-SortedList.prototype.min     = function() {}
-SortedList.prototype.average = function() {}
-SortedList.prototype.sum     = function() {}
+SortedList.prototype.add = function (item) { }
+SortedList.prototype.get = function (pos) { }
+SortedList.prototype.max = function () { }
+SortedList.prototype.min = function () { }
+SortedList.prototype.average = function () { }
+SortedList.prototype.sum = function () { }
 
 module.exports = SortedList;
