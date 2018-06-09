e3ac052d25bee694ca037416b131611d2f587b15
Chris Manthey <calined@gmx.de>
2018-06-09 23:05:49 +0200

    get works

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,14 +1,32 @@
 class SortedList {
     constructor() {
-
+        this.items = [];
+        this.length = 0;
     }
 
-    add(item) { }
-    get(pos) { }
+    add(item) {
+        this.items.push(item);
+        this.sort();
+        this.updateLength();
+    }
+
+    get(pos) { return this.items[pos - 1]; }
+
     max() { }
+
     min() { }
+
     average() { }
+
     sum() { }
+
+    sort() {
+        this.items.sort(function (number1, number2) {
+            return number1 - number2;
+        });
+    }
+
+    updateLength() { this.length = this.items.length; }
 
 }
 
