5451efbee9501172110e234ea54c0351ef11bd38
williamdonayre <william.donayre@outlook.com>
2018-06-09 19:47:13 -0400

    done

--- starter-code/index.js
+++ starter-code/index.js
@@ -15,15 +15,28 @@ SortedList.prototype.get     = function(pos) {
 
 };
 SortedList.prototype.max     = function() {
-    //since sorted take the last number in array
+    //since sorted, take the last number in array
     return this.items [this.items.length - 1 ];
 
 };
 SortedList.prototype.min     = function() {
-    //since sorted take first number in array
+    //since sorted, take first number in array
     return this.items [0];
-}
-SortedList.prototype.average = function() {}
-SortedList.prototype.sum     = function() {}
+};
+
+SortedList.prototype.average = function() { 
+    var Sum = this.items.reduce(function(summed, currentValue){
+        return summed + currentValue;
+      },0);
+      return Sum/this.length;
+    };
+
+SortedList.prototype.sum = function() {
+    var Sum = this.items.reduce(function(summed, currentValue){
+        return summed + currentValue;
+      },0);
+      return Sum;
+    };
+
 
 module.exports = SortedList;
