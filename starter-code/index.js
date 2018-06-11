b3357479c4c7589326e7e611e230e71833bf2192
Juliet Urbina <julieturbina8@gmail.com>
2018-06-11 15:35:46 -0400

    Done

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,10 +1,61 @@
-var SortedList = function() {};
 
-SortedList.prototype.add     = function(item) {}
-SortedList.prototype.get     = function(pos) {}
-SortedList.prototype.max     = function() {}
-SortedList.prototype.min     = function() {}
-SortedList.prototype.average = function() {}
-SortedList.prototype.sum     = function() {}
 
-module.exports = SortedList;
+var SortedList = function () {
+this.items = [];
+this.length = this.items.length;
+};
+
+var list = new SortedList ();
+
+SortedList.prototype.add = function(item) {
+this.items.push(item);
+this.items.sort(function(element1, element2){
+return element1-element2;
+});
+this.length+=1;
+};
+
+
+SortedList.prototype.get = function (pos) {
+  if(this.length>0){
+  return this.item [pos-1];
+}
+return 0;
+};
+
+SortedList.prototype.max = function (){
+if(this.length>0){
+return this.items[this.length-1];
+}
+return 0;
+};
+
+
+SortedList.prototype.min = function (){
+if(this.length>0){
+return this.items[0];
+}
+return 0;
+};
+
+
+SortedList.prototype.average = function () {
+  if(this.length>0){
+  return this.sum()/this.length; 
+  }
+  return 0;
+};
+
+ 
+SortedList.prototype.sum = function () {
+    var sum = (accumulator, currentValue)=> accumulator+currentValue;
+    if(this.length>0){
+      return this.items.reduce(sum);
+    }
+    return 0;
+  };
+
+// module.exports = SortedList;
+// };
+
+
