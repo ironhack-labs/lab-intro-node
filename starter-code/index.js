bfe85e1c9ff76a37aea2512a15657ea3857fe0f4
Alma Alonso <alma.aa93@gmail.com>
2018-06-14 21:42:58 +0200

    wip

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,6 +1,17 @@
-var SortedList = function() {};
+var SortedList = function() {
+    this.items = [],
+    this.length = 0;
+};
 
-SortedList.prototype.add     = function(item) {}
+
+
+SortedList.prototype.add     = function(item) {
+    this.items.push(item);
+    this.length++;
+    this.items.sort(function(item1, item2){
+        return item1 - item2
+    })
+}
 SortedList.prototype.get     = function(pos) {}
 SortedList.prototype.max     = function() {}
 SortedList.prototype.min     = function() {}
