a1b58228d02d9fa645abfe952f522ceed31c6310
Alex González <alexgm@MacBook-Pro-de-Alex.local>
2018-06-14 21:39:30 +0200

    Mocha exercise

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,10 +1,38 @@
-var SortedList = function() {};
+var SortedList = function() {
+    this.items = [];
+    this.length = 0;
+};
 
-SortedList.prototype.add     = function(item) {}
-SortedList.prototype.get     = function(pos) {}
-SortedList.prototype.max     = function() {}
-SortedList.prototype.min     = function() {}
-SortedList.prototype.average = function() {}
-SortedList.prototype.sum     = function() {}
+SortedList.prototype.add = function(item) {
+    this.items.push(item);
+    this.length++;
+    this.items.sort(function(item1,item2){
+        return item1 - item2;
+    })
+}
+
+SortedList.prototype.get = function(pos) {
+    return this.items[pos - 1];
+}
+
+SortedList.prototype.max     = function() {
+    return this.items[this.length - 1];
+}
+
+SortedList.prototype.min     = function() {
+    return this.items[0];
+}
+
+SortedList.prototype.average = function() {
+    return this.items.reduce(function(acc, item , index , array){
+        return index < array.length - 1  ? acc + item : (acc + item)/ array.length
+    },0);
+}
+
+SortedList.prototype.sum  = function() {
+    return this.items.reduce(function(acc, item){
+        return acc + item;
+    },0);
+}
 
 module.exports = SortedList;
