92acf687bfcb7238fcb1cebea1399a78a85142b1
Georgina Pacey <paceygeorgina@gmail.com>
2018-06-15 20:51:20 +0200

    first two

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,10 +1,30 @@
-var SortedList = function() {};
+var SortedList = function(items = []) {
+    this.items = items;
+    this.length = items.length;
+};
 
-SortedList.prototype.add     = function(item) {}
-SortedList.prototype.get     = function(pos) {}
-SortedList.prototype.max     = function() {}
-SortedList.prototype.min     = function() {}
-SortedList.prototype.average = function() {}
-SortedList.prototype.sum     = function() {}
+SortedList.prototype.add     = function(item) {
+    
+    this.items.push(item);
+    this.length++;
+}
+SortedList.prototype.get     = function(pos) {
+
+}
+SortedList.prototype.max     = function() {
+
+}
+SortedList.prototype.min     = function() {
+
+}
+SortedList.prototype.average = function() {
+
+}
+SortedList.prototype.sum     = function() {
+
+}
+
+var list = new SortedList();
+list.add(1);
 
 module.exports = SortedList;
