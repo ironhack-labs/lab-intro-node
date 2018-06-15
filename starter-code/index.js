f5792ead95e379182a4131448e0ac6dafe34cee9
Alma Alonso <alma.aa93@gmail.com>
2018-06-15 18:38:32 +0200

    done

--- starter-code/index.js
+++ starter-code/index.js
@@ -12,10 +12,27 @@ SortedList.prototype.add     = function(item) {
         return item1 - item2
     })
 }
-SortedList.prototype.get     = function(pos) {}
-SortedList.prototype.max     = function() {}
-SortedList.prototype.min     = function() {}
-SortedList.prototype.average = function() {}
-SortedList.prototype.sum     = function() {}
+SortedList.prototype.get     = function(pos) {
+    return this.items[pos-1]
+}
+SortedList.prototype.max     = function() {
+    return this.items[this.items.length - 1];
+}
+SortedList.prototype.min     = function() {
+    return this.items[0]
+}
+SortedList.prototype.average = function() {
+    let sum = this.items.reduce(function(acc, currentValue){
+        return acc + currentValue;
+    }, 0);
+    let average = sum / this.items.length;
+    return average;
+}
+SortedList.prototype.sum     = function() {
+    let sum = this.items.reduce(function(acc, currentValue){
+        return acc + currentValue;
+    }, 0);
+    return sum;
+}
 
 module.exports = SortedList;
