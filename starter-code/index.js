2c64de5c916bcb24c95703100df262866ef9cbe6
Marta Soler <marta@MacBook-Air-de-Barrabes.local>
2018-06-16 16:58:16 +0200

    First iteration

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,7 +1,18 @@
-var SortedList = function() {};
+let SortedList = function() {
+    this.items = [];
+    this.length = this.items.length;
+};
 
-SortedList.prototype.add     = function(item) {}
-SortedList.prototype.get     = function(pos) {}
+SortedList.prototype.add = function(item) {
+    this.items.push(item)
+    this.items.sort(function(i1, i2) {
+        return i1 - i2;
+      });
+    this.length++;
+}
+SortedList.prototype.get = function(pos) {
+    return this.items[pos-1], this.length;
+}
 SortedList.prototype.max     = function() {}
 SortedList.prototype.min     = function() {}
 SortedList.prototype.average = function() {}
