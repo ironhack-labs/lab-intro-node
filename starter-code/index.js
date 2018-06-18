22ac8fe7fc04e99bef5545cf12187efb787c5cf7
Marta Soler <marta@MacBook-Air-de-Barrabes.local>
2018-06-18 21:11:39 +0200

    Second iteration

--- starter-code/index.js
+++ starter-code/index.js
@@ -4,18 +4,83 @@ let SortedList = function() {
 };
 
 SortedList.prototype.add = function(item) {
+    
     this.items.push(item)
-    this.items.sort(function(i1, i2) {
-        return i1 - i2;
-      });
+    this.items.sort();
+    
     this.length++;
 }
 SortedList.prototype.get = function(pos) {
-    return this.items[pos-1], this.length;
+    if(pos > this.items.length){
+        return this.OutOfBounds();
+    }
+    return this.items[pos-1];
 }
-SortedList.prototype.max     = function() {}
-SortedList.prototype.min     = function() {}
-SortedList.prototype.average = function() {}
-SortedList.prototype.sum     = function() {}
+SortedList.prototype.max = function() {
+    if (this.items === 0) {
+        return this.EmptyList();
+    } else {
+        var max = this.items[0];
+        this.items.forEach(function(e) {
+            if(e > max){
+                max = e;
+            }
+        }.bind(this));
+    }
+    return max; 
+}
+SortedList.prototype.min = function() {
+    if (this.items.length === 0) {
+        return this.EmptyList();
+    } else {
+        var min = this.items[0];
+        this.items.forEach(function(e) {
+            if(e < min){
+                min = e;
+            }
+        }.bind(this));
+    }
+    return min;
+}
+SortedList.prototype.average = function() {
+    if (this.items.length === 0) {
+        return this.EmptySortedList();
+    } else {
+        this.items.reduce(function(avg, e, i, arr) {
+            var average = 0;
+            if (i === this.items.length - 1) {
+                return average = ((avg + e) / this.items.length);
+            } else {
+                return average = avg + e;
+            }
+        }.bind(this), 0);
+        return average;
+    }
+}
+
+
+SortedList.prototype.sum = function() {
+    if (this.items.length === 0) {
+        return this.EmptySortedList();
+    } else {
+        return this.items.reduce(function(sum, e) {
+            sum += e;
+        }.bind(this), 0);
+    }
+}
+
+
+SortedList.prototype.OutOfBounds = function(){
+    return "Sorry, the position is out of bounds";
+}
+
+SortedList.prototype.EmptyList = function() {
+    return "The list is empty";
+
+}
+
+SortedList.prototype.EmptySortedList = function() {
+    return "The sorted list is empty";
+}
 
 module.exports = SortedList;
