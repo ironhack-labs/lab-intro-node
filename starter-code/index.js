00b2736cad954d631c42e7170f0b9d8a77d114a5
ednadv <edomingosv@gmail.com>
2018-06-21 16:43:38 +0200

    sin realizar test

--- starter-code/index.js
+++ starter-code/index.js
@@ -7,4 +7,42 @@ SortedList.prototype.min     = function() {}
 SortedList.prototype.average = function() {}
 SortedList.prototype.sum     = function() {}
 
+var SortedList = function(items = []){
+    this.items = items;
+    this.length = items.length;
+};
+SortedList.prototype.add = function(item) {
+    this.items.push(item);
+    this.length++;
+}
+
+SortedList.prototype.get = function(pos) {
+    return this.items[pos-1];
+};
+
+
+SortedList.prototype.max = function() {
+    return this.items[this.length -1];
+};
+
+
+SortedList.prototype.min = function(){
+    return this.items[0];
+
+};
+
+SortedList.prototype.average = function(){ 
+    return this.items.reduce(function(acc,elem){
+        return Index(acc+Index(elem.rate));
+    },0)/items.length).toFixed(2);
+};
+
+SortedList.prototype.sum = function(){
+    return this.items.reduce(function(acc,elem){
+        return acc+elem;
+    } ,0);
+    };
+
+
+
 module.exports = SortedList;
