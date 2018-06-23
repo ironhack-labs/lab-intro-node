a79384d9280d303fc8446f7153317bbeb4053389
MyrnaArredondo <tracia_ra@hotmail.com>
2018-06-22 21:59:24 -0500

    "Hecho en clase"

---
+++ starter-code/prueba.js
@@ -0,0 +1,12 @@
+const SortedList = require("./index.js")
+
+let Lista = new SortedList();
+
+Lista.add(10);
+Lista.add(20);
+console.log(Lista.get(2));
+console.log(Lista.sum());
+console.log(Lista.min());
+console.log(Lista.max());
+console.log(Lista.average(2));
+
