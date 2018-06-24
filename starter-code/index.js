787fca8eae2a1a0b1e7cff459e2d2dc25295560e
Alekz7 <ing.aragon.ipn@gmail.com>
2018-06-24 17:30:24 -0500

    MEXPT-ALEJANDRO_ARAGON_ALVAREZ

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,10 +1,33 @@
-var SortedList = function() {};
+var SortedList = function() {
+	this.items = [];
+	this.length = this.items.length;
+};
 
-SortedList.prototype.add     = function(item) {}
-SortedList.prototype.get     = function(pos) {}
-SortedList.prototype.max     = function() {}
-SortedList.prototype.min     = function() {}
-SortedList.prototype.average = function() {}
-SortedList.prototype.sum     = function() {}
+SortedList.prototype.add     = function(item) {
+	this.items.push(item);
+	this.items.sort(function(a,b){
+		return a-b;
+	});
+	this.length = this.items.length;
+}
+SortedList.prototype.get     = function(pos) {
+	return this.items[pos - 1];
+}
+SortedList.prototype.max     = function() {
+	return Math.max.apply(null, this.items);
+}
+SortedList.prototype.min     = function() {
+	return Math.min.apply(null, this.items);
+}
+SortedList.prototype.average = function() {
+	if (this.length == 0) throw new Error("EmptySortedList");
+	return this.sum()/this.length;
+}
+SortedList.prototype.sum     = function() {
+	if (this.length == 0) throw new Error("EmptySortedList");
+	return this.items.reduce(function(total,item){
+		return total + item;
+	})
+}
 
 module.exports = SortedList;
