4ba1f4dda2eaa9df485ee60a1cf20879e024c125
Jorge L <kolmax@gmail.com>
2018-07-02 13:45:01 +0200

    Finish the pair exercise

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,10 +1,33 @@
-var SortedList = function() {};
+var SortedList = function() {
+  this.items = []
+  this.length = this.items.length
+};
 
-SortedList.prototype.add     = function(item) {}
-SortedList.prototype.get     = function(pos) {}
-SortedList.prototype.max     = function() {}
-SortedList.prototype.min     = function() {}
-SortedList.prototype.average = function() {}
-SortedList.prototype.sum     = function() {}
+SortedList.prototype.add     = function(item) {
+  this.items.push(item)
+  this.length = this.items.length
+  this.items.sort(function (elemento1, elemento2) {
+    return elemento1 - elemento2
+  });
+};
+
+SortedList.prototype.get     = function(pos) {
+  return this.items[pos - 1]
+}
+
+SortedList.prototype.max     = function() {
+  return this.get(this.length)
+}
+SortedList.prototype.min     = function() {
+  return this.get(1)
+}
+SortedList.prototype.average = function() {
+  return this.sum() / this.length;
+}
+SortedList.prototype.sum     = function() {
+  return this.items.reduce(function(acc, e) {
+    return acc + e;
+  }, 0);
+}
 
 module.exports = SortedList;
