48f75765d01a9927ed3dad2f019b2d8599bbe4ff
Rodrigo Flores <rflor012@gmail.com>
2018-07-02 15:11:55 -0400

    working on MIN next

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,10 +1,44 @@
-var SortedList = function() {};
+/* jshint esversion: 6 */
 
-SortedList.prototype.add     = function(item) {}
-SortedList.prototype.get     = function(pos) {}
-SortedList.prototype.max     = function() {}
-SortedList.prototype.min     = function() {}
-SortedList.prototype.average = function() {}
-SortedList.prototype.sum     = function() {}
+class SortedList {
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
+
+  add(x){
+   this.items.push(x);
+   this.length = this.items.length;
+   this.items.sort(function(numA, numB) {
+     return numA - numB;
+   });
+  }
+
+  get(index){
+      return this.items[index-1];
+    }
+
+  max(items){
+    return Math.max(...this.items);
+  }
+
+}
+
+
+//   SortedList.prototype.max     = function() {};
+//   SortedList.prototype.min     = function() {};
+//   SortedList.prototype.average = function() {};
+//   SortedList.prototype.sum     = function() {};
+//
+
+var list = new SortedList();
+list.add(10);
+list.add(20);
+list.add(30);
+list.add(100);
+list.add(110);
+list.add(120);
+console.log(list.items);
+
 
 module.exports = SortedList;
