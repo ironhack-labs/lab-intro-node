8126fe1d1e5a92954eb2429e9464baeb56891109
Leismale <leismale@gmail.com>
2018-07-02 13:49:48 +0200

    Done

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,10 +1,33 @@
-var SortedList = function() {};
+var SortedList = function() {
+  this.items = [];
+  this.length = this.items.length;
+};
 
-SortedList.prototype.add     = function(item) {}
-SortedList.prototype.get     = function(pos) {}
-SortedList.prototype.max     = function() {}
-SortedList.prototype.min     = function() {}
-SortedList.prototype.average = function() {}
-SortedList.prototype.sum     = function() {}
+SortedList.prototype.add = function(x) {
+  this.items.push(x);
+  this.items.sort((a, b) => a - b);
+  this.length++;
+};
+
+SortedList.prototype.get = function(pos) {
+    return this.items[pos-1];
+};
+
+
+SortedList.prototype.max = function() {
+    return this.items[this.length-1];
+};
+
+SortedList.prototype.min = function() {
+    return this.items[0];
+};
+
+SortedList.prototype.average = function() {
+    return this.sum() / this.length;
+};
+
+SortedList.prototype.sum = function() {
+    return this.items.reduce((acc, e) => (acc + e),0);
+};
 
 module.exports = SortedList;
