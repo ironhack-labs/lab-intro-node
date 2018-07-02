aff297fd8247decb0837bf8321191988762807e5
Pablo Rezola <pablorezola20@yahoo.es>
2018-07-02 14:12:26 +0200

    done

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,10 +1,38 @@
-var SortedList = function() {};
+var SortedList = function() {
+this.items = [];
+this.length = 0;
 
-SortedList.prototype.add     = function(item) {}
-SortedList.prototype.get     = function(pos) {}
-SortedList.prototype.max     = function() {}
-SortedList.prototype.min     = function() {}
-SortedList.prototype.average = function() {}
-SortedList.prototype.sum     = function() {}
+
+
+};
+
+SortedList.prototype.add     = function(item) {
+
+    this.items.push(item);
+    this.items.sort((a,b) => a - b )  ;
+    this.length++;  
+
+
+
+};
+SortedList.prototype.get     = function(pos) {
+return this.items[pos-1];
+
+};
+SortedList.prototype.max     = function() {
+return this.items[this.length-1];
+
+
+};
+SortedList.prototype.min     = function() {
+    return this.items[0];
+};
+SortedList.prototype.average = function() {
+    return this.sum()/this.length;  
+    
+};
+SortedList.prototype.sum     = function() {
+    return this.items.reduce( (a,c) => {return a + c}, 0)
+};
 
 module.exports = SortedList;
