7488ba587ca7a39d3507d9e7bbbe6099c42cfe41
TomyTM <franciscotomasapariciobustos@gmail.com>
2018-07-02 13:45:01 +0200

    done

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,10 +1,28 @@
-var SortedList = function() {};
+var SortedList = function() {
+  this.items = [];
+  this.length = this.items.length;
+};
 
-SortedList.prototype.add     = function(item) {}
-SortedList.prototype.get     = function(pos) {}
-SortedList.prototype.max     = function() {}
-SortedList.prototype.min     = function() {}
-SortedList.prototype.average = function() {}
-SortedList.prototype.sum     = function() {}
+SortedList.prototype.add = function(item) {
+  this.items.push(item);
+  this.length = this.items.length;
+  this.items.sort((a, b) => a - b);
+};
+
+SortedList.prototype.get = function(pos) {
+  return this.items[pos - 1];
+};
+SortedList.prototype.max = function() {
+  return Math.max.apply(null, this.items);
+};
+SortedList.prototype.min = function() {
+  return Math.min.apply(null, this.items);
+};
+SortedList.prototype.average = function() {
+  return this.items.reduce((acc, e) => acc + e, 0) / this.length;
+};
+SortedList.prototype.sum = function() {
+  return this.items.reduce((acc, e) => acc + e, 0);
+};
 
 module.exports = SortedList;
