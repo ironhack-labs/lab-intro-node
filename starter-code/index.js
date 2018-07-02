f63465ff2beb69b0b0247d4bf01ccd6ea7e843f3
Andrey Barrantes <abarrantes@clavisco.com>
2018-07-02 15:09:57 -0400

    done

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,10 +1,41 @@
-var SortedList = function() {};
+class SortedList {
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-SortedList.prototype.add     = function(item) {}
-SortedList.prototype.get     = function(pos) {}
-SortedList.prototype.max     = function() {}
-SortedList.prototype.min     = function() {}
-SortedList.prototype.average = function() {}
-SortedList.prototype.sum     = function() {}
+  add(x) {
+    this.items.push(x);
+    this.items.sort((a, b) => a - b);
+    this.length = this.items.length;
+  }
+
+  get(nth) {
+    this.length = this.items.length;
+    return this.items[nth - 1];
+  }
+
+  max() {
+    return Math.max(...this.items);
+  }
+
+  min() {
+    return Math.min(...this.items);
+  }
+
+  average() {
+    if (this.items.length === 0) { return this.items.sort((a, b) => a - b); }
+    return this.sum() / this.items.length;
+  }
+
+  sum() {
+    if (this.items.length === 0) { return this.items.sort((a, b) => a - b); }
+    return this.items.reduce((sum, item) => sum + item);
+  }
+
+}
 
 module.exports = SortedList;
+
+
+
