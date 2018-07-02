a01545d644accc8e64f7edfdd443b6c340736675
Sarai Vasquez <svmarinez@hotmail.com>
2018-07-02 14:17:23 +0200

    first commit

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,7 +1,19 @@
-var SortedList = function() {};
+var SortedList = function() {
+    this.items = [];
+    this.length = 0;
+};
 
-SortedList.prototype.add     = function(item) {}
-SortedList.prototype.get     = function(pos) {}
+SortedList.prototype.add     = function(item) {
+    this.items.push(item);
+    this.items.sort(function(a, b) {
+        return a-b
+    });
+    this.length++;
+}
+
+SortedList.prototype.get     = function(pos) {
+    return this.items[pos-1];
+}
 SortedList.prototype.max     = function() {}
 SortedList.prototype.min     = function() {}
 SortedList.prototype.average = function() {}
