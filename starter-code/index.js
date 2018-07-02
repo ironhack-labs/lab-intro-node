0072c12df92221676ebe7ecb26717005331be61a
Sarai Vasquez <svmarinez@hotmail.com>
2018-07-02 15:58:51 +0200

    FINISHED

--- starter-code/index.js
+++ starter-code/index.js
@@ -14,9 +14,23 @@ SortedList.prototype.add     = function(item) {
 SortedList.prototype.get     = function(pos) {
     return this.items[pos-1];
 }
-SortedList.prototype.max     = function() {}
-SortedList.prototype.min     = function() {}
-SortedList.prototype.average = function() {}
-SortedList.prototype.sum     = function() {}
 
+SortedList.prototype.max     = function() {
+    return this.items[this.length-1];
+}
+SortedList.prototype.min     = function() {
+    return this.items[0];
+}
+SortedList.prototype.average = function() {
+    return this.sum()/this.length;
+}
+SortedList.prototype.sum     = function() {
+    if (this.items.length > 0){
+    return this.items.reduce(function(a, b) {
+        return a+b;
+        })
+    } else {
+        return [];
+    }
+}
 module.exports = SortedList;
