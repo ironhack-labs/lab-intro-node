0d3cbd643b5377c7d7c83125fd6a2ca37a108a16
Andrés Martin <monkeyandres@protonmail.com>
2018-07-02 13:32:43 +0200

    Finished

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,10 +1,34 @@
-var SortedList = function() {};
+class SortedList{
+    constructor() {
+        this.items = [];
+        this.length = 0;
+    }
 
-SortedList.prototype.add     = function(item) {}
-SortedList.prototype.get     = function(pos) {}
-SortedList.prototype.max     = function() {}
-SortedList.prototype.min     = function() {}
-SortedList.prototype.average = function() {}
-SortedList.prototype.sum     = function() {}
+    add(item) {
+        this.items.push(item);
+        this.items.sort((a, b) => a - b);
+        this.length++;
+    }
+
+    get(pos) {
+        return this.items[pos-1];
+    }
+
+    max() {
+        return this.items[this.length-1];
+    }
+
+    min() {
+        return this.items[0];
+    }
+
+    sum() {
+        return this.items.reduce((acc, e) => acc + e, 0);
+    }
+
+    average() {
+        return this.sum()/this.length;
+    }
+}
 
 module.exports = SortedList;
