a323c7833d012a7e8145867e2a924f47666869d0
Daniel <paseiro101@yahoo.com>
2018-07-02 21:28:00 -0400

    MIA Daniel Paseiro

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,10 +1,52 @@
-var SortedList = function() {};
+    // var SortedList = function() {};
 
-SortedList.prototype.add     = function(item) {}
-SortedList.prototype.get     = function(pos) {}
-SortedList.prototype.max     = function() {}
-SortedList.prototype.min     = function() {}
-SortedList.prototype.average = function() {}
-SortedList.prototype.sum     = function() {}
+    class SortedList{
+        constructor(items, length){
+            this.items =[];
+            this.length = this.items.length;
+        }
+        
+        // SortedList.prototype.add     = function(item) {}
+        
+        add(x){
+           this.items.push(x);
+           this.items.sort((a,b) => {
+              return a - b;
+           })
+           this.length++ 
+        };
+        get(pos){
+            return this.items[pos-1]
+          }
 
-module.exports = SortedList;
+          max(){
+            return Math.max(...this.items)  
+          }
+
+          min(){
+            return Math.min(...this.items)  
+          }
+
+          sum(){
+            return this.items.reduce((acc,current) => acc + current,0)
+          }
+
+          average(){
+            if(this.items.length === 0){
+                throw Error('EmptySortedList');
+            }  
+            return this.sum()/this.items.length;
+          }
+    
+    }
+
+        
+    // SortedList.prototype.get     = function(pos) {}
+    // SortedList.prototype.max     = function() {}
+    // SortedList.prototype.min     = function() {}
+    // SortedList.prototype.average = function() {}
+    // SortedList.prototype.sum     = function() {}
+
+    module.exports = SortedList;
+
+
