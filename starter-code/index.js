76ad756eba8878fe63d6125c44dcd6bce02f5506
Pere Busquet <perebusquet87gmail.com>
2018-07-04 11:27:45 +0200

    done

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,10 +1,34 @@
-var SortedList = function() {};
+var SortedList = function() {
+    this.length = 0;
+    this.items = [];
+};
 
-SortedList.prototype.add     = function(item) {}
-SortedList.prototype.get     = function(pos) {}
-SortedList.prototype.max     = function() {}
-SortedList.prototype.min     = function() {}
-SortedList.prototype.average = function() {}
-SortedList.prototype.sum     = function() {}
+SortedList.prototype.add = function(item) {
+    this.items.push(item);
+    this.length = this.items.length
+
+    this.items.sort((a,b) =>{
+        return a -b;
+    })
+    this.length++
+};
+SortedList.prototype.get     = function(pos) {
+    return this.items[pos -1]
+}
+SortedList.prototype.max     = function() {
+    return Math.max(this.items)
+}
+SortedList.prototype.min     = function() {
+    return Math.min(this.items)
+}
+SortedList.prototype.average = function() {
+    if(this.items.length === 0){
+        console.log("EmptySortedList");
+    }
+    return this.sum()/this.items.length;
+}
+SortedList.prototype.sum     = function() {
+    return this.items.reduce((acc,current)=>acc + current,0)
+}
 
 module.exports = SortedList;
