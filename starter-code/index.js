fd10adb2fe7cba057312470e812232f085b1b8ad
Oriol Hilari <orihil@icloud.com>
2018-07-05 18:25:07 +0200

    Finished

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,10 +1,34 @@
-var SortedList = function() {};
+var SortedList = function() {
+    this.items = [];
+    this.length = this.items.length;
+};
+  
+SortedList.prototype.add     = function(item) {
+    this.items.push(item);
+    this.items.sort(function(a, b){
+        return a - b;
+    });
+    this.length = this.items.length;
+}
 
-SortedList.prototype.add     = function(item) {}
-SortedList.prototype.get     = function(pos) {}
-SortedList.prototype.max     = function() {}
-SortedList.prototype.min     = function() {}
-SortedList.prototype.average = function() {}
-SortedList.prototype.sum     = function() {}
+SortedList.prototype.get     = function(pos) {
+    return this.items[pos-1];
+}
+
+SortedList.prototype.max     = function() {
+    return Math.max(...this.items);
+}
+
+SortedList.prototype.min     = function() {
+    return Math.min(...this.items);
+}
+SortedList.prototype.average = function() {
+    return this.sum() / this.items.length;
+}
+
+SortedList.prototype.sum     = function() {
+    return this.items.reduce((a, b) => a + b, 0);
+}
 
 module.exports = SortedList;
+
