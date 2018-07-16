69ef12efb6dfc2ab651f7eed2cb9be2774edc1cd
Rafa Gallego <rgdmax@gmail.com>
2018-07-16 16:30:10 +0200

    DONE

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,10 +1,62 @@
-var SortedList = function() {};
+var SortedList = function() {
+  this.items = [];
+  this.length = 0;
+};
 
-SortedList.prototype.add     = function(item) {}
-SortedList.prototype.get     = function(pos) {}
-SortedList.prototype.max     = function() {}
-SortedList.prototype.min     = function() {}
-SortedList.prototype.average = function() {}
-SortedList.prototype.sum     = function() {}
+SortedList.prototype.add     = function(item) {
+  
+  if (this.length === 0) {
+    this.items.push(item);
+  }
+  else {
+    this.items.push(item);
+    this.items.sort(function(a,b) {
+      return a - b;
+    });      
+  }
+  this.length++
+}
+
+SortedList.prototype.get     = function(pos) {
+
+  if (!this.items[pos - 1]) {
+    return "OutOfBounds";
+  }
+  else {
+    return this.items[pos - 1];
+  }  
+  
+}
+SortedList.prototype.max     = function() {
+  if (!this.items) {
+    return "EmptySortedList";
+  }
+  else {
+    return Math.max(...this.items);
+  }
+}
+SortedList.prototype.min     = function() {
+  if (!this.items) {
+    return "EmptySortedList";
+  }
+  else {
+    return Math.min(...this.items);
+  }
+}
+SortedList.prototype.average = function() {
+  if (!this.items) {
+    return "EmptySortedList";
+  }
+  else {    
+    return this.sum() / this.length;
+  }
+}
+SortedList.prototype.sum     = function() {
+  var accum = 0;
+  for (var i = 0; i < this.length; i++) {
+    accum += this.items[i];
+  }
+  return accum;
+}
 
 module.exports = SortedList;
