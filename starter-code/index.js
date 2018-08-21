a477a9500c5df9742d04cb1b878c390148b237e6
trong nhan nguyen <ntnhan30@gmail.com>
2018-08-21 18:25:36 +0200

    done

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,10 +1,53 @@
-var SortedList = function() {};
+class SortedList {
+    constructor() {
+        this.items = [];
+        this.length = 0;
+    }
 
-SortedList.prototype.add     = function(item) {}
-SortedList.prototype.get     = function(pos) {}
-SortedList.prototype.max     = function() {}
-SortedList.prototype.min     = function() {}
-SortedList.prototype.average = function() {}
-SortedList.prototype.sum     = function() {}
+    add(x) {
+        this.items.push(x);
+        this.length++;
+        this.items.sort((a, b) => {
+            return a - b
+        })
+    }
+
+    get(pos) {
+        return this.items[pos - 1];
+    }
+    max() {
+        if (this.length === 0) {
+            return this.items
+        }
+        let max = Math.max(...this.items)
+        return max
+    }
+    min() {
+        if (this.length === 0) {
+            return this.items
+        }
+        let min = Math.min(...this.items)
+        return min;
+
+    }
+    average() {
+        if (this.length === 0) {
+            return this.items
+        }
+
+        return this.sum() / this.length;
+    }
+
+    sum() {
+        if (this.length === 0) {
+            return this.items
+        }
+        let av = this.items.reduce((acc, num) => {
+            var sum = acc + num;
+            return sum
+        }, 0)
+        return av
+    }
+};
 
 module.exports = SortedList;
