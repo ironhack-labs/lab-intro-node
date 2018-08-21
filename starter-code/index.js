55f62cf28296a63c03f3ba39d6c7d1b1193ee352
katros <kasia.roszak@gmail.com>
2018-08-21 23:10:18 +0200

    done

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,10 +1,41 @@
-var SortedList = function() {};
+class SortedList {
+    constructor() {
+        this.items = [];
+        this.length = 0;
+    }
 
-SortedList.prototype.add     = function(item) {}
-SortedList.prototype.get     = function(pos) {}
-SortedList.prototype.max     = function() {}
-SortedList.prototype.min     = function() {}
-SortedList.prototype.average = function() {}
-SortedList.prototype.sum     = function() {}
+    add(x) {
+        this.items.push(x);
+        this.length++;
+
+        this.items.sort((a, b) => {
+            return a - b;
+        });
+    }
+
+    get(position) {
+        if (this.length === 0) return new Error('OutOfBounds');
+        else return this.items[position - 1];
+    }
+
+    max() {
+        return Math.max(...this.items);
+    }
+
+    min() {
+        return Math.min(...this.items);
+    }
+
+    average() {
+        if (this.length === 0) return new Error('EmptySortedList');
+        let average = this.sum() / this.length;
+        return average;
+    }
+
+    sum() {
+        let total = this.items.reduce((acc, cur) => acc + cur, 0);
+        return total;
+    }
+}
 
 module.exports = SortedList;
