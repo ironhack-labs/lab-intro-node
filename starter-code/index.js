5b3eb53411e5d5d5de95e33005fea8c2c45adf58
Nicole Jelonek <nicolejelonek@gmail.com>
2018-08-21 19:11:59 +0200

    BER Nicole done

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,10 +1,52 @@
-var SortedList = function() {};
+class SortedList {
+  constructor() {
+    this.items = []
+    this.length = 0;
+  };
 
-SortedList.prototype.add     = function(item) {}
-SortedList.prototype.get     = function(pos) {}
-SortedList.prototype.max     = function() {}
-SortedList.prototype.min     = function() {}
-SortedList.prototype.average = function() {}
-SortedList.prototype.sum     = function() {}
+  add(x) {
+    this.items.push(x);
+    this.items.sort((a, b) => a - b)
+    this.length++;
+  };
+
+  get(pos) {
+    return this.items[pos - 1];
+  };
+
+  max() {
+    if (this.length === 0) {
+      return this.items
+    }
+    let max = Math.max(...this.items)
+    return max
+  };
+
+  min() {
+    if (this.length === 0) {
+      return this.items
+    }
+    let min = Math.min(...this.items)
+    return min;
+  };
+
+  average() {
+    if (this.length === 0) {
+      return this.items
+    }
+    return this.sum() / this.length;
+  };
+
+  sum() {
+    if (this.length === 0) {
+      return this.items
+    }
+    let avrg = this.items.reduce((accelerator, number) => {
+      var sum = accelerator + number;
+      return sum
+    }, 0)
+    return avrg
+  }
+};
 
 module.exports = SortedList;
