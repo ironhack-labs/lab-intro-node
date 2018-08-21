bb08f7fdaf78bd8b20e1286ef6d01adafa46e27d
AlinaKr <mirgehendieideenaus@gmail.com>
2018-08-21 21:04:12 +0200

    BER-Alina Kraft

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,10 +1,60 @@
-var SortedList = function() {};
+class SortedList {
+    constructor() {
+        this.items = [];
+        this.length = 0;
+    }
 
-SortedList.prototype.add     = function(item) {}
-SortedList.prototype.get     = function(pos) {}
-SortedList.prototype.max     = function() {}
-SortedList.prototype.min     = function() {}
-SortedList.prototype.average = function() {}
-SortedList.prototype.sum     = function() {}
+    add(x) {
+        this.items.push(x);
+        this.items.sort((a, b) => a - b);
+        this.length++;
+    }
+
+    get(pos) {
+        return this.items[pos - 1];
+    }
+
+
+    max() {
+        if (this.length === 0) {
+            throw new Error("EmptySortedList");
+        }
+        return this.items[this.length - 1];
+    }
+
+    min() {
+        if (this.length === 0) {
+            throw new Error("EmptySortedList");
+        }
+        return this.items[0];
+    }
+
+    average() {
+        // if (this.length === 0) {
+        //     return new Error("EmptySortedList");
+        // }
+        // var sum = 0;
+        // this.forEach(function (el) {
+        //     sum += el;
+        // });
+        // return sum / this.length;
+        return this.sum() / this.length;
+    }
+
+    sum() {
+        if (this.length === 0) {
+            throw new Error("EmptySortedList");
+        }
+        else {
+            return this.items.reduce((sum, item) => sum + item);
+        }
+        // var sum = 0;
+        // this.forEach(function (el) {
+        //     sum += el;
+        // });
+        // return sum;
+    }
+
+};
 
 module.exports = SortedList;
