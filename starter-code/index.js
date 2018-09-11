99a8644d7c444d2fe51f1ea570868ac3a48c1caf
wergilia <weronika.zdrojewska@gmail.com>
2018-09-11 19:11:07 +0200

    Finnished

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,52 @@
 class SortedList {
-  constructor() {}
-  add(item) {}
-  get(pos) {}
-  max() {}
-  min() {}
-  average() {}
-  sum() {}
+  constructor() {
+    this.items = [];
+    this.length = 0;
+  }
+  
+  add(x) { 
+    this.items.push(x);
+    this.items.sort(function(a,b){
+      return a-b
+    });
+    this.length = this.items.length;
+  }
+  
+  get(nth) {
+    return this.items[nth-1];
+  }
+
+  max() {
+    if (this.length > 0) {
+      return this.items.pop();
+    } else {
+      throw new Error("EmptySortedList");
+    }
+  }
+
+  min() {
+    if (this.length > 0) {
+      return this.items.shift();
+    } else {
+      throw new Error("EmptySortedList");
+    }
+  }
+  
+  sum() {
+    if (this.length == 0) {
+      return 0
+    } else {
+     return this.items.reduce(((a, b) => a + b))
+    }    
+  }
+
+  average() {
+    if (this.length == 0) {
+      throw new Error("EmptySortedList");
+    } else {
+     return this.items.reduce(((a, b) => a + b))/this.length
+    }    
+  }
 };
 
 module.exports = SortedList;
