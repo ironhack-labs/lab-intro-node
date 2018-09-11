0dd948e3f1c9ad010f4301d735359a0e07c084d8
Francis Wardley <fwardley90@gmail.com>
2018-09-11 17:59:29 +0200

    done

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,43 @@
 class SortedList {
-  constructor() {}
-  add(item) {}
-  get(pos) {}
-  max() {}
-  min() {}
-  average() {}
-  sum() {}
+  constructor() {
+    this.items = [];
+    this.length = 0;
+  }
+
+  add(x) {
+    this.items.push(x);
+    this.items.sort((a, b) => a - b);
+    this.length++;
+  }
+
+  get(pos) {
+    return this.items[pos - 1];
+  }
+
+  max() {
+    if (this.length == 0) {
+      throw new Error("EmptySortedList")
+    } else {
+    return  Math.max(...this.items);
+    }
+  }
+  min() {
+    if (this.length == 0) {
+      throw new Error("EmptySortedList")
+    } else {
+    return  Math.min(...this.items);
+    }
+  }
+  average() {
+    if (this.length == 0) {
+      throw new Error("EmptySortedList")
+    } else {
+      return this.sum() / this.length;
+    }
+  }
+  sum() {
+    return this.length == 0 ? 0 : this.items.reduce((a, b) => a + b, 0);
+  }
 };
 
 module.exports = SortedList;
