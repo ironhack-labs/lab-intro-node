d12cc3c8d783f6d1d57d3a9604fd41d024d18fc1
pablotaboada <pablo.tabu@hotmail.com>
2018-09-11 19:17:25 +0200

    done

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,51 @@
 class SortedList {
-  constructor() {}
-  add(item) {}
-  get(pos) {}
-  max() {}
-  min() {}
-  average() {}
-  sum() {}
+  constructor() {
+    this.items= [];
+    this.length = this.items.length;
+  }
+  add(x) {
+
+    this.items.push(x) ;
+    this.items.sort(function(a,b){
+      return a-b;
+    });
+    this.length++;
+
+  }
+  get(pos) {
+    return this.items[pos-1];
+
+  }
+  max() {
+    if(this.items.length == 0){
+      throw new Error("EmptySortedList")
+    }
+    else{
+    
+   return Math.max(...this.items)
+    }
+   
+  }
+  min() {
+    if (this.items.length == 0){
+      throw new Error("EmptySortedList")
+    }
+    else{
+      return Math.min(...this.items)
+    }
+  }
+  average() {
+    if (this.items.length == 0){
+      throw new Error("EmptySortedList")
+    }
+    else {
+      return this.sum()/this.items.length;
+    }
+   
+  }
+  sum() {
+    return this.items.reduce((a,b) => a+b, 0)
+  }
 };
 
 module.exports = SortedList;
