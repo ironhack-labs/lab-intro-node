31a2ef7abc8596c6ba65de07731c2751b497bee9
Evenstarw1 <lauracorrallopez@gmail.com>
2018-09-11 19:06:44 +0200

    First commit

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,48 @@
 class SortedList {
-  constructor() {}
-  add(item) {}
-  get(pos) {}
-  max() {}
-  min() {}
-  average() {}
-  sum() {}
+  constructor() {
+    this.items= [];
+    this.length= 0;
+  }
+  add(x) {
+    this.items.push(x);
+    this.items.sort(function(a,b){
+      return a-b
+    });
+    this.length=this.items.length;
+  }
+  get(nth) {
+    return this.items[nth-1]
+
+  }
+  max() {
+    if (this.length == 0) {
+      throw new Error("EmptySortedList")
+    }
+    else {
+    return this.items.pop(); 
+    }
+  }
+  min() {
+    if (this.length == 0) {
+      throw new Error("EmptySortedList")
+    }
+    else {
+    return this.items.shift(); 
+    }
+  }
+  sum() {
+    if (this.length < 1){
+      return 0
+    } else {
+    return this.items.reduce(((a,b) => a + b));}
+  };
+
+  average() {
+    if (this.length < 1) {
+      throw new Error("EmptySortedList")
+    } else {
+    return this.items.reduce(((a,b) => a + b))/this.length;}
+  }
 };
 
 module.exports = SortedList;
