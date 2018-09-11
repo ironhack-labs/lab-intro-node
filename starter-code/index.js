abc008430afd00fbb653573fd3881467c8210454
Celia Arnelas <37745928+Carnelas@users.noreply.github.com>
2018-09-11 18:39:41 +0200

    [MAD DE] Celia Arnelas

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,47 @@
 class SortedList {
-  constructor() {}
-  add(item) {}
-  get(pos) {}
-  max() {}
-  min() {}
-  average() {}
-  sum() {}
+  constructor() {
+    this.items = [];
+    this.length = 0;
+  }
+  add(x) {
+    this.items.push(x);
+    this.items.sort(function(a,b){
+      return a-b;
+    });
+    this.length = this.items.length;
+  }
+  get(nth) {
+    return this.items[nth-1]
+  }
+  max() {
+    if (this.length >= 1){
+      return this.items.pop();
+    } else {
+      throw new Error ("EmptySortedList");
+    }
+  }
+  min() {
+    if (this.length >= 1){
+      return this.items.shift();
+    } else {
+      throw new Error ("EmptySortedList")
+    }
+  }
+  sum() {
+    if (this.length < 1){
+      return 0
+    } else {
+    return this.items.reduce((a,b) => a + b);}
+  };
+
+  average() {
+    if (this.length < 1){
+      throw new Error ("EmptySortedList")
+    } else {
+      return this.items.reduce((a,b) => a + b, 0) / this.length
+    }
+  }
+
 };
 
 module.exports = SortedList;
