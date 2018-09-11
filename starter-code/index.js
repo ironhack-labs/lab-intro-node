b5b2200d4fc060081ec0b6a605ed8204d79601d9
cesararmada <cesar.armada@gmail.com>
2018-09-11 19:02:10 +0200

    done

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,46 @@
 class SortedList {
-  constructor() {}
-  add(item) {}
-  get(pos) {}
-  max() {}
-  min() {}
-  average() {}
-  sum() {}
+  constructor() {
+    this.items = []
+    this.length = 0;
+  }
+  add(x) {
+    this.items.push(x);
+    this.items.sort(function (a, b) {
+      return a - b;
+    })
+    this.length = this.items.length;
+  }
+  get(pos) {
+    return this.items[pos - 1];
+  }
+  max() {
+    if (this.length == 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      return this.items.pop();
+    }
+  }
+  min() {
+    if (this.length == 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      return this.items.shift();
+    }
+  }
+  average() {
+    if (this.length === 0) {
+      throw new Error('EmptySortedList')
+    } else {
+      return this.sum() / this.length;
+    }
+  }
+  sum() {
+    if (this.length == 0) {
+      return 0
+    } else {
+      return this.items.reduce((a, b) => a + b, 0)
+    }
+  }
 };
 
 module.exports = SortedList;
