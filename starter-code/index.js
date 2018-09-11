08d33434321f92636db302b54bc2dac66e9723ab
David Sirit <davidsirit@yahoo.com>
2018-09-11 23:57:45 +0200

    some iteration done

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,51 @@
 class SortedList {
-  constructor() {}
-  add(item) {}
-  get(pos) {}
-  max() {}
-  min() {}
-  average() {}
-  sum() {}
-};
+  constructor() {
+    this.items = [];
+    this.length = 0;
+  }
+  add(item) {
+    this.items.push(item);
+    this.length = this.items.length;
+    this.items.sort((a, b) => {
+      return a - b;
+    });
+  }
+  get(pos) {
+    let element;
+    if (this.items[pos] === undefined) {
+      return "OutOfBounds";
+    } else {
+      element = this.items[pos-1];
+      return element;
+    }
+  }
+  max() {
+    try {
+      if (this.items.length === 0) throw ("EmptySortedList");
+      this.items.sort((a, b) => {
+        return b - a;
+      });
+      return this.items[0];
+    } catch (e) {
+      return;
+    }
+  }
+  min() {
+    this.items.sort((a, b) => {
+      return a - b;
+    });
+    return this.items[0];
+  }
+  average() {
+    return this.sum()/this.items.length;
+  }
+  sum() {
+    let sumElem = 0;
+    sumElem = this.items.reduce((acc, ele) =>{
+      return acc += ele;
+    },0);
+    return sumElem;
+  }
+}
 
 module.exports = SortedList;
