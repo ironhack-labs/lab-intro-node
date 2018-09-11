7c3ba62d857e1fad834af72dbb8cb671742ae908
Ruben Armendariz <rubenarmendarizcarretero@gmail.com>
2018-09-11 19:13:55 +0200

    DONE!!

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,50 @@
 class SortedList {
-  constructor() {}
-  add(item) {}
-  get(pos) {}
-  max() {}
-  min() {}
-  average() {}
-  sum() {}
-};
+  constructor() {
+    this.items = []
+    this.length= this.items.length;
+    
+  }
+  add(item) {
+  this.items.push(item);
+  this.items.sort(function(a,b){return a - b;});
+  this.length ++;
+  }
+  get(pos) {
+  return this.items[pos - 1];
+  }
+  max() {
+    if(this.items.length == 0) {
+    throw new Error("EmptySortedList")}
+    else {
+     return Math.max(...this.items);
+    }
+  }
+  min() {
+    if(this.items.length == 0) {
+      throw new Error("EmptySortedList")}
+      else {
+       return Math.min(...this.items);
+      }
+  }
+  average() {
+    if (this.items.length == 0) {
+      throw new Error("EmptySortedList")
+
+    }
+   else {
+      return this.sum()/this.length;
+
+    
+  }
+  }
+  sum() {
+    
+    return this.items = this.items.reduce((a, b) => a + b, 0);
+    
+  }
+    };
+  
+  
+
 
 module.exports = SortedList;
