d75d4a03a22e5eb5b0bc80fa3c254797daa27ca5
Luther <luther0211@hotmail.com>
2018-09-11 23:16:02 -0500

    casi terminado

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,10 +1,37 @@
 class SortedList {
-  constructor() {}
-  add(item) {}
-  get(pos) {}
-  max() {}
-  min() {}
+  constructor() {
+    this.items = []
+    this.length = 0
+  }
+  add(item) {
+
+    this.items.push(item)
+    this.length = this.items.length
+    this.items.sort((a,b) => a - b );
+    
+  }
+  get(pos) {
+    return this.items[pos-1]
+
+  }
+  max() {
+    if (this.items.length === 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      return this.items[this.items.length - 1];
+    }
+  }
+
+  min() {
+    if (this.items.length === 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      return this.items[0];
+    }
+  }
+
   average() {}
+  
   sum() {}
 };
 
