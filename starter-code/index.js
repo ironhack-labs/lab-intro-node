db855d7fc7566bdb7b79b4d65535a7ec5af4b478
Pedro Cruz <pedro.cruz@blackinntech.com>
2018-09-18 22:24:23 -0500

    Done

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,42 @@
 class SortedList {
-  constructor() {}
-  add(item) {}
-  get(pos) {}
-  max() {}
-  min() {}
-  average() {}
-  sum() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
+  add(item) {
+    this.items.push(item);
+    this.items.sort(function(a, b) {
+      return a - b;
+    });
+    this.length = this.items.length;
+  }
+  get(pos) {
+    return this.items[pos - 1];
+  }
+  max() {
+    if (this.items.length == 0) throw new Error("EmptySortedList");
+
+    return this.items[this.length - 1];
+  }
+  min() {
+    if (this.items.length == 0) throw new Error("EmptySortedList");
+
+    return this.items[0];
+  }
+  average() {
+    if (this.items.length == 0) throw new Error("EmptySortedList");
+    let total = this.sum();
+
+    return total / this.length;
+  }
+  sum() {
+    let total = 0;
+    this.items.forEach(element => {
+      total += element;
+    });
+
+    return total;
+  }
 };
 
 module.exports = SortedList;
