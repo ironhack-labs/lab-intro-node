b503afa3b21744aa84354d33b991b853e0db0e92
JuanXCV <jaconesab@agrodolores.es>
2018-10-09 15:44:17 +0200

    Pair programming Elsa y Juan

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,50 @@
 class SortedList {
-  constructor() {}
-  add(item) {}
-  get(pos) {}
-  max() {}
-  min() {}
-  average() {}
-  sum() {}
+  constructor() {
+    this.items = [];
+    this.length = 0;
+  }
+  add(item) {
+    this.items.push(item);
+    this.length = this.items.length;
+    this.items.sort((a,b) => a-b);
+  }
+  get(pos) {
+    return this.items[pos-1] 
+    this.length = this.items.length;
+  }
+  max() {
+    if(this.length>0) { 
+      return Math.max(...this.items) 
+    } else {
+      throw new Error("EmptySortedList")
+    }
+  }
+  min() {
+    if(this.length>0) { 
+      return Math.min(...this.items) 
+    } else {
+      throw new Error("EmptySortedList")
+    }
+  }
+  average() {
+
+    if(this.length>0) { 
+      return this.items.reduce((a,b) => {
+        return a+b ;
+      })/this.length
+    } else {
+      throw new Error("EmptySortedList")
+    }
+  }
+  sum() {
+    if(this.items.length>0){
+      return this.items.reduce((a,b) => {
+        return a+b ;
+      })
+    } else {
+      return 0;
+    }
+  }
 };
 
 module.exports = SortedList;
