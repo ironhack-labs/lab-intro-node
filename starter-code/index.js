9e5fb7d8032b91e7aa10a6f62bf5e7dbb3f670cf
Esaú JC <esau.jc@gmail.com>
2018-10-09 15:20:20 +0200

    Finished

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,53 @@
 class SortedList {
-  constructor() {}
-  add(item) {}
-  get(pos) {}
-  max() {}
-  min() {}
-  average() {}
-  sum() {}
+  constructor() {
+    this.items = [],
+    this.length = 0;
+  }
+  add(item) {
+    this.items.push(item);
+    this.length = this.items.length;
+    this.items.sort((a,b)=> a - b);
+  }
+  get(pos) {
+    if(this.length === 0){
+      throw new Error("OutOfBounds");
+    }else{
+      return this.items[pos-1];
+    }
+  }
+  max() {
+    if (this.length === 0){
+      throw new Error("EmptySortedList");
+    }else{
+      let sorted = this.items.sort((a,b) => b - a);
+      return sorted[0];
+    }
+  }
+  min() {
+    if (this.length === 0){
+      throw new Error("EmptySortedList");
+    }else{
+      let sorted = this.items.sort((a,b) => a - b);
+      return sorted[0];
+    }
+  }
+  average() {
+    if (this.length === 0){
+      throw new Error("EmptySortedList");
+    }else{
+      let average = (this.sum()/this.length);
+      return average;
+    }
+
+  }
+  sum() {
+    let resultado = 0;
+    if (this.length !== 0){
+      let suma = this.items.reduce((acc,current) => acc + current);
+       resultado = suma;
+    }
+    return resultado;
+  }
 };
 
 module.exports = SortedList;
