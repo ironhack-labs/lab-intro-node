f5360e55d55967bf0ca18757ba91973f6684814e
Anna Sch <anna-schaeffner@gmx.de>
2018-10-30 17:48:56 +0100

    Finished

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,42 @@
 class SortedList {
-  constructor() {}
-  add(item) {}
-  get(pos) {}
-  max() {}
-  min() {}
-  average() {}
-  sum() {}
+  constructor() {
+  this.items = [];
+  this.length = this.items.length;
+  };
+  add(item){
+    this.length++;
+    this.items.push(item)
+    this.items.sort(function(a,b){
+      return a-b;
+    });
+  }
+  get(pos){return this.items[pos-1]}
+  max() {
+    if (this.length === 0){
+      throw new Error("EmptySortedList")
+    }
+  else return Math.max(...this.items)
+}
+  min() {
+    if (this.length === 0){
+    throw new Error("EmptySortedList")
+  }
+  else return Math.min(...this.items)
+}
+  average() {
+    if (this.length === 0){
+      throw new Error("EmptySortedList")
+   }
+   else return this.sum() / this.length
+  }
+  sum() { 
+    if (this.length === 0){
+      return 0;
+    }
+    let sum = this.items.reduce((sum,x)=>sum + x);
+    return sum;
+  }
 };
+
 
 module.exports = SortedList;
