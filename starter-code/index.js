549cd339a4c81d323656125f28a91bd59474601b
Felicitas Deutges <deutges.ironhack@gmail.com>
2018-10-30 17:45:32 +0100

    Done

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,42 @@
 class SortedList {
-  constructor() {}
-  add(item) {}
-  get(pos) {}
-  max() {}
-  min() {}
-  average() {}
-  sum() {}
+  constructor() {
+    this.items = []
+    this.notSorted = []
+    this.length = 0
+  }
+  add(item) {
+    this.items.push(item)
+    this.items.sort((a, b) => a - b);
+  }
+  get(pos) {
+    return this.items[pos - 1]
+  }
+  max() {
+    if (this.length === 0) {
+      throw new Error("EmptySortedList")
+    }
+    else { return Math.max(...this.items) }
+  }
+  min() {
+    if (this.length === 0) {
+      throw new Error("EmptySortedList")
+    }
+    else { return Math.min(...this.items) }
+  }
+  average() {
+    if (this.length === 0) {
+      throw new Error("EmptySortedList")
+    }
+    else {
+      var sum = this.items.reduce((a, b) => a + b, 0);
+      var average = sum / this.length
+      return average
+    }
+  }
+  sum() {
+    var sum = this.items.reduce((a, b) => a + b, 0);
+    return sum;
+  }
 };
 
 module.exports = SortedList;
--- starter-code/test/test.js
+++ starter-code/test/test.js
@@ -4,26 +4,26 @@ var SortedList = require('../index.js')
 // Load the Chai Assertion Library
 var assert = require('assert');
 
-describe('SortedList', function() {
-  describe('Constructor', function() {
-    it('should create an empty SortedList', function() {
+describe('SortedList', function () {
+  describe('Constructor', function () {
+    it('should create an empty SortedList', function () {
       var sl = new SortedList();
       assert.equal(sl.length, 0);
       assert.equal(sl instanceof SortedList, true);
     });
   });
 
-  describe('#add(x)', function() {
+  describe('#add(x)', function () {
     var sl;
-    beforeEach(function(){
+    beforeEach(function () {
       sl = new SortedList();
     });
-    it('should add a single value to SortedList', function() {
+    it('should add a single value to SortedList', function () {
       assert.equal(sl.length, 0);
       sl.add(1);
       assert.equal(sl.length, 1);
     });
-    it('should add a third value to SortedList', function() {
+    it('should add a third value to SortedList', function () {
       sl.add(30);
       sl.add(20);
       sl.add(10);
@@ -31,13 +31,13 @@ describe('SortedList', function() {
     });
   });
 
-  describe('#get(i)', function() {
+  describe('#get(i)', function () {
     var sl;
-    beforeEach(function(){
+    beforeEach(function () {
       sl = new SortedList();
     });
 
-    it('should return an OutOfBounds exception if there is no element in that position', function() {
+    it('should return an OutOfBounds exception if there is no element in that position', function () {
       try {
         sl.get(20);
       } catch (e) {
@@ -47,27 +47,27 @@ describe('SortedList', function() {
       }
     });
 
-    it('should return the element in that position', function() {
+    it('should return the element in that position', function () {
       var foo = 10;
-      for(i=1; i<200; i++) {
-        sl.add(foo*i);
-        assert.equal(sl.get(i), foo*i);
+      for (i = 1; i < 200; i++) {
+        sl.add(foo * i);
+        assert.equal(sl.get(i), foo * i);
       }
     });
   });
 
-  describe('#add(x) and get(i)', function() {
+  describe('#add(x) and get(i)', function () {
     var sl;
-    beforeEach(function(){
+    beforeEach(function () {
       sl = new SortedList();
     });
-    it('should add a second value to SortedList, sorted', function() {
+    it('should add a second value to SortedList, sorted', function () {
       sl.add(20);
       sl.add(10);
       assert.equal(sl.get(1), 10);
       assert.equal(sl.get(2), 20);
     });
-    it('should add a third value to SortedList, sorted', function() {
+    it('should add a third value to SortedList, sorted', function () {
       sl.add(30);
       sl.add(20);
       sl.add(10);
@@ -77,24 +77,24 @@ describe('SortedList', function() {
     });
   });
 
-  describe('#max()', function() {
+  describe('#max()', function () {
     var sl;
-    beforeEach(function() {
+    beforeEach(function () {
       sl = new SortedList();
     });
 
-    it('should return an EmptySortedList exception if there is no element in the list', function() {
+    it('should return an EmptySortedList exception if there is no element in the list', function () {
       try {
         sl.max();
         // The next line should not be executed
-        assert.equal(true,false);
+        assert.equal(true, false);
       } catch (e) {
         assert.equal(e instanceof Error, true);
         assert.equal(e.message, 'EmptySortedList');
       }
     });
 
-    it('should return the max element in the list', function() {
+    it('should return the max element in the list', function () {
       sl.add(10);
       sl.add(20);
 
@@ -102,24 +102,24 @@ describe('SortedList', function() {
     });
   });
 
-  describe('#min()', function() {
+  describe('#min()', function () {
     var sl;
-    beforeEach(function() {
+    beforeEach(function () {
       sl = new SortedList();
     });
 
-    it('should return an EmptySortedList exception if there is no element in the list', function() {
+    it('should return an EmptySortedList exception if there is no element in the list', function () {
       try {
         sl.min();
         // The next line should not be executed
-        assert.equal(true,false);
+        assert.equal(true, false);
       } catch (e) {
         assert.equal(e instanceof Error, true);
         assert.equal(e.message, 'EmptySortedList');
       }
     });
 
-    it('should return the min element in the list', function() {
+    it('should return the min element in the list', function () {
       sl.add(10);
       sl.add(20);
 
@@ -127,17 +127,17 @@ describe('SortedList', function() {
     });
   });
 
-  describe('#sum()', function(){
+  describe('#sum()', function () {
     var sl;
-    beforeEach(function(){
+    beforeEach(function () {
       sl = new SortedList();
     });
 
-    it('should return 0 for an empty sorted list', function() {
+    it('should return 0 for an empty sorted list', function () {
       assert.equal(sl.sum(), 0);
     })
 
-    it('should add(sum) all elements of the array if there are elements in the list', function() {
+    it('should add(sum) all elements of the array if there are elements in the list', function () {
       sl.add(1);
       sl.add(2);
       sl.add(3);
@@ -145,30 +145,30 @@ describe('SortedList', function() {
     });
   });
 
-  describe('#average()', function() {
+  describe('#average()', function () {
     var sl;
-    beforeEach(function(){
+    beforeEach(function () {
       sl = new SortedList();
     });
 
-    it('should return an EmptySortedList exception if there are no elements', function() {
+    it('should return an EmptySortedList exception if there are no elements', function () {
       try {
         sl.average();
         // The next line should not be executed
-        assert.equal(true,false);
+        assert.equal(true, false);
       } catch (e) {
         assert.equal(e instanceof Error, true)
         assert.equal(e.message, "EmptySortedList")
       }
     })
 
-    it('should return the average of elements in the array', function() {
-      for(i=0; i<101; i++){
-        sl.add(i*2);
+    it('should return the average of elements in the array', function () {
+      for (i = 0; i < 101; i++) {
+        sl.add(i * 2);
       }
       assert.equal(sl.average(), 100);
     })
   });
 
-  
+
 });
