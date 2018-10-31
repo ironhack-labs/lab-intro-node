c1f12666ca1163f79af9d4be50acd0922f7f82de
aemartos <aemartos@gmail.com>
2018-10-31 12:04:32 +0100

    Finish exercise

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,41 @@
 class SortedList {
-  constructor() {}
-  add(item) {}
-  get(pos) {}
-  max() {}
-  min() {}
-  average() {}
-  sum() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
+  add(item) {
+    this.length++;
+    this.items.push(item);
+    this.items.sort((a, b) => a - b);
+  }
+  get(pos) {
+    return this.items[pos - 1];
+  }
+  max() {
+    if (this.length === 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      return Math.max(...this.items);
+    }
+  }
+  min() {
+    //this.length === 0 ? throw new Error("EmptySortedList") : return Math.min(...this.items);
+    if (this.length === 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      return Math.min(...this.items);
+    }
+  }
+  average() {
+    if (this.length === 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      return this.sum()/this.length;
+    }
+  }
+  sum() {
+    return this.items.reduce((a, b) => a + b, 0);
+  }
 };
 
 module.exports = SortedList;
