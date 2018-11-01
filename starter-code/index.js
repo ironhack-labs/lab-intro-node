cc306efe83d0ca5fbfb8286daa1e878aae5847c9
Alfredo Hinarejos <alfredo.hina@gmail.com>
2018-11-01 17:31:49 +0100

    me ha costado entender lo que pedía el ejercicio y no sé si es correcto iniciar this.length a 0 en el constructor y después ir sumando uno en cada add, pero es la única forma en la que me da todo OK

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,57 @@
 class SortedList {
-  constructor() {}
-  add(item) {}
-  get(pos) {}
-  max() {}
-  min() {}
-  average() {}
-  sum() {}
+  // constructor() {}
+  // add(item) {}
+  // get(pos) {}
+  // max() {}
+  // min() {}
+  // average() {}
+  // sum() {}
+
+  constructor() {
+    this.items = [];
+    this.length = 0;
+  }
+
+  add(item) {
+    this.length++;
+    this.items.push(item);
+    this.items.sort((a,b)=>a-b);
+  }
+
+  get(pos) {
+    return this.items[pos-1];
+  }
+
+  max() {
+    if (this.length === 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      return Math.max(...this.items);
+    }
+  }
+
+  min() {
+    if (this.length === 0) {
+      throw new Error("EmptySortedList")
+    } else {
+      return Math.min(...this.items);
+    }
+  }
+  
+    sum() {
+      return this.items.reduce(function(a, b) { return a + b; }, 0)
+    }
+
+  average() {
+    if (this.length === 0) {
+      throw new Error("EmptySortedList")
+    } else {
+      return this.sum() / this.length;
+    }
+  }
+
+
 };
+
 
 module.exports = SortedList;
