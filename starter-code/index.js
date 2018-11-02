f19cd12d2375cce84769ee1740668442f89906b9
Leticia Olábarri <leticiaolabarri@yahoo.es>
2018-11-02 11:37:03 +0100

    done

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,44 @@
 class SortedList {
-  constructor() {}
-  add(item) {}
-  get(pos) {}
-  max() {}
-  min() {}
-  average() {}
-  sum() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  };
+  add(item) {
+    this.length++;
+    this.items.push(item);
+    this.items.sort((a, b) => a - b)
+  };
+  get(pos) {
+    return this.items[pos - 1];
+  };
+  max() {
+    if (this.length === 0) {
+      throw new Error("EmptySortedList");
+    }
+    else {
+      return Math.max(...this.items);
+    };
+  };
+  min() {
+    if (this.length === 0) {
+      throw new Error("EmptySortedList");
+    }
+    else {
+      
+      return Math.min(...this.items);
+    };
+  };
+  average() {
+    if (this.length === 0) {
+      throw new Error("EmptySortedList");
+    }
+    else {
+      return this.items.reduce((a, b) => a + b, 0) / this.length;
+    };
+  };
+  sum() {
+    return this.items.reduce((a, b) => a + b, 0);
+  };
 };
 
 module.exports = SortedList;
