954627f19b34fafc2dc4f3479f1ad8b75e58887b
Teresa Romero <tromerolozano@gmail.com>
2018-11-02 17:56:23 +0100

    done

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,46 @@
 class SortedList {
-  constructor() {}
-  add(item) {}
-  get(pos) {}
-  max() {}
-  min() {}
-  average() {}
-  sum() {}
+  constructor() {
+    this.items=[];
+    this.length=this.items.length;
+  }
+  add(item) {
+    this.items.push(item);
+    this.length=this.items.length;
+    this.items.sort((a,b)=>a-b);
+  }
+  get(pos) {
+    return this.items[pos-1];
+  }
+  max() {
+    if(this.items.length===0){
+      throw new Error("EmptySortedList");
+    } else {
+      return Math.max(...this.items);
+    }
+  }
+  min() {
+    if(this.items.length===0){
+      throw new Error("EmptySortedList");
+    } else {
+      return Math.min(...this.items);
+    }
+
+  }
+  average() {
+    if(this.items.length===0){
+      throw new Error("EmptySortedList");
+    } else {
+      return this.items.reduce((a,b) => a + b, 0)/this.items.length;
+    }
+  }
+  sum() {
+    if(this.items.length===0){
+      return 0;
+    } else {
+      return this.items.reduce((a,b) => a + b, 0)
+    }
+  }
 };
 
 module.exports = SortedList;
+
