130c6e4a8c2287c6713eaeec9dd66a7baafe6dda
Cecile Daguin <gally999@gmail.com>
2018-11-05 18:32:18 +0100

    done

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,52 @@
+var assert = require('assert');
+
 class SortedList {
-  constructor() {}
-  add(item) {}
-  get(pos) {}
-  max() {}
-  min() {}
-  average() {}
-  sum() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  };
+  add(item) {
+    this.items.push(item);
+    this.length++;
+    this.items.sort(function (a, b) {
+      return a - b;
+    });
+  };
+  get(pos) {
+    return this.items[pos-1];
+  };
+  max() {
+    if (this.length === 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      return Math.max(...this.items);
+    }
+  };
+  min() {
+    if (this.length === 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      return Math.min(...this.items);
+    }
+  };
+  average() {
+    if (this.length === 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      return this.sum() / this.length;
+    }
+  }
+  sum() {
+    if (this.length === 0) {
+      return 0;
+    } else {
+      let temp = 0;
+      for (var i = 0; i < this.items.length; i++) {
+        temp += this.items[i];
+      }
+      return temp;
+    }
+  }
 };
 
 module.exports = SortedList;
