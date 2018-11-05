010d1bc47265c402d7fa45d01f4b1109c235969e
Gabriel Sicuto <gabrielsicuto@MacBook-Pro-de-Gabriel-2.local>
2018-11-05 19:27:42 -0200

    done

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,39 @@
 class SortedList {
-  constructor() {}
-  add(item) {}
-  get(pos) {}
-  max() {}
-  min() {}
-  average() {}
-  sum() {}
-};
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
+
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a, b) => a - b);
+    this.length = this.items.length;
+  }
+
+  get(pos) {
+    return this.items[pos - 1];
+  }
+
+  max() {
+    if (this.items.length === 0) throw new Error('EmptySortedList');
+    else return this.items[this.items.length - 1];
+  }
+
+  min() {
+    if (this.items.length === 0) throw new Error('EmptySortedList');
+    return this.items[0];
+  }
+
+  average() {
+    if (this.items.length === 0) throw new Error('EmptySortedList');
+    return this.sum() / this.items.length;
+  }
+
+  sum() {
+    if (this.items.length === 0) return 0;
+    return this.items.reduce((a, b) => a + b);
+  }
+}
+
 
 module.exports = SortedList;
