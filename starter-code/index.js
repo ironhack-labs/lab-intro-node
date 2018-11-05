f2863a83e78785fdff93acf0029e207d68635762
harvock <neolondres@outlook.com>
2018-11-05 20:16:11 -0200

    done

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,40 @@
 class SortedList {
-  constructor() {}
-  add(item) {}
-  get(pos) {}
-  max() {}
-  min() {}
-  average() {}
-  sum() {}
+  constructor(items, length) {
+    this.items = [];
+    this.length = this.items.length;
+  }
+  add(item) {
+    this.items.push(item);
+    this.length = this.items.length;
+    this.items.sort(function(a, b) {
+      return a - b;
+    });
+  }
+  get(pos) {
+    return this.items[pos -1];
+  }
+  max() {
+    if (this.items.length === 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      return this.items[this.length - 1];
+    }
+  }
+  min() {
+    if (this.length === 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      return this.items[0];
+    }
+  }
+  average() {
+    if(this.length>0){
+    return this.items.reduce((total, current) => total + current) / this.length;
+  } throw new Error("EmptySortedList");
+  };
+  sum() {
+    return this.items.reduce((total, current) => total + current, 0);
+  }
 };
 
 module.exports = SortedList;
