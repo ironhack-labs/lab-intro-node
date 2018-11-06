5aba1ddcf88f6490123a6799e3fb09e8a01b593d
Nicolas Pe <perrot.nic@gmail.com>
2018-11-06 12:14:22 +0100

    not finished

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,26 @@
 class SortedList {
-  constructor() {}
-  add(item) {}
+  constructor(){
+    this.items = [];
+    this.length = 0;
+  }
+
+
+  add(x) {
+    this.items.push(x);
+    this.length++;
+  }
+
   get(pos) {}
+
   max() {}
+
+
   min() {}
+
   average() {}
+
   sum() {}
-};
+
+}
 
 module.exports = SortedList;
