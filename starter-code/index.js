1d5efe946ac023188ca9da9f857e13e686c9be75
mehdilouraoui <louraouimehdi@MacBook-Pro-de-Mehdi-2.local>
2018-11-06 12:08:19 +0100

    done

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,49 @@
 class SortedList {
-  constructor() {}
-  add(item) {}
-  get(pos) {}
-  max() {}
-  min() {}
-  average() {}
-  sum() {}
+  constructor() {
+    this.items = [];
+    this.length = 0;
+  }
+  add(x) {
+    this.items.push(x);
+    this.items.sort( function (a, b) {
+      return a - b;
+    });
+    this.length++;
+  }
+
+  get(pos) {
+    return this.items[pos -1];
+  }
+
+  max() {
+    if (this.items.length === 0) {
+      throw new Error("EmptySortedList");
+    }
+    return this.items[this.items.length - 1];
+  }
+
+  min() {
+    if (this.items.length === 0) {
+      throw new Error("EmptySortedList");
+    }
+    return this.items[0];
+  }
+
+  sum() {
+    if (this.items.length === 0) {
+      return 0;
+    }
+    return this.items.reduce((acc, oneItem) => acc += oneItem, 0);
+  }
+
+  average() {
+    if (this.items.length === 0) {
+      throw new Error("EmptySortedList");
+    }
+   return (this.items.reduce((acc, oneItem) => acc += oneItem, 0)) / this.items.length;
+  }
 };
+
+
 
 module.exports = SortedList;
