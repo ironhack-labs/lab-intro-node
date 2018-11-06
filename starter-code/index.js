90ed4b360406d0c026fec0645445b84e3be07c84
stormamnc <amancioandre@gmail.com>
2018-11-06 09:17:23 -0200

    done with 1 error

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,57 @@
+/* eslint-disable no-unused-expressions */
 class SortedList {
-  constructor() {}
-  add(item) {}
-  get(pos) {}
-  max() {}
-  min() {}
-  average() {}
-  sum() {}
-};
+  constructor() {
+    this.items = [];
+    this.length = 0;
+  }
+
+  add(item) {
+    this.items.push(item)
+    this.items.sort();
+    this.length += 1;
+  }
+
+  get(pos) {
+    if (!this.items[pos - 1]) {
+      return 'OutOfBounds';
+    }
+    return this.items[pos - 1];
+  }
+
+  max() {
+    if (this.length === 0) {
+      throw new Error('EmptySortedList');
+    }
+    return this.items.sort()[this.length - 1];
+  }
+
+  min() {
+    if (this.length === 0) {
+      throw new Error('EmptySortedList');
+    }
+    return this.items.sort()[0];
+  }
+
+  average() {
+    if (this.length === 0) {
+      throw new Error('EmptySortedList');
+    }
+    return this.sum() / this.length;
+  }
+
+  sum() {
+    return this.items.reduce((acc, val) => { return acc + val }, 0);
+  }
+}
+
+const sl = new SortedList();
+
+sl.add(20);
+sl.add(10);
+sl.add(30);
+sl.add(20);
+sl.add(10);
+console.log(sl.items, sl.length, sl.get(1));
+
 
 module.exports = SortedList;
