c37e0781c7e8219ea4f44ffa8e701c9aa880519f
chloehd <chloehipeau@gmail.com>
2018-11-06 15:31:51 +0100

    Paris Chloe

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,23 @@
 class SortedList {
-  constructor() {}
-  add(item) {}
-  get(pos) {}
+  constructor() {
+    this.items = [];
+    this.length = 0;
+  }
+  add(item) {
+    this.items.push(item);
+    this.length++;
+    this.items = this.items.sort((itemA, itemB) => itemA - itemB);
+  }
+  get(pos) {
+    
+  }
+  
+  
   max() {}
   min() {}
   average() {}
   sum() {}
 };
+
 
 module.exports = SortedList;
