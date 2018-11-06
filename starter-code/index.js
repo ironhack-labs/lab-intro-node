b7e35bcf856292594048fdf021598da40e974e03
DoneDeal0 <ap.lanoe@outlook.com>
2018-11-06 12:13:20 +0100

    functions seem logic, but the script doesn't pass Mocha test

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,47 @@
 class SortedList {
-  constructor() {}
-  add(item) {}
-  get(pos) {}
-  max() {}
-  min() {}
-  average() {}
-  sum() {}
+  constructor() {
+    this.items  = [];
+    this.length = 0;
+  }
+  add(x){
+    this.items.push(x);
+    this.length++;
+  }
+
+  get(x){
+    if(this.items.length=== 0){
+      throw new Error("EmptySortedList")}
+    if(x > this.length){
+      throw new Error("OutOfBounds")}
+    return this.items[x];
+}
+
+  max(items){
+    if(this.items.length=== 0){
+      throw new Error("EmptySortedList")
+    }
+    return Math.max(...items);
+  }
+
+  min(items){
+    if(this.items.length=== 0){
+      throw new Error("EmptySortedList");
+    }
+    return Math.min(...items);
+  }
+
+  sum(items){
+    this.items.reduce(acc, curr)
+      return acc + curr;
+    }
+
+
+    average(){
+      sum(this.items)/this.items.length;
+    }
+
 };
 
 module.exports = SortedList;
+
+let newList = new SortedList();
