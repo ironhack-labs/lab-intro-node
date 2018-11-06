17ae9e7af371e34c76ea186f597e03b1858a10fb
JN Veigel <art.jnveigel@gmail.com>
2018-11-06 11:12:34 +0100

    initial version, working

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,60 @@
 class SortedList {
-  constructor() {}
-  add(item) {}
-  get(pos) {}
-  max() {}
-  min() {}
-  average() {}
-  sum() {}
+
+  constructor() {
+  	this.items = [];
+  	this.length = 0;
+  }
+
+  add(item) {
+  	this.items.push(item);
+  	this.length = this.items.length;
+
+  	this.items.sort(function(a, b) {
+	  return a - b;
+  	});
+  }
+
+  get(pos) {
+  	return this.items[pos-1];
+  }
+
+  max() {
+  	if (this.length === 0) {
+  		throw new Error("EmptySortedList");
+  	}
+  	else {
+  		return this.items[this.items.length -1];
+  	};
+  }
+
+  min() { 
+  	if (this.length === 0) {
+  		throw new Error("EmptySortedList");
+  	}
+  	else {
+  		return this.items[0];
+  	};
+  }
+
+  sum() {
+  	if (this.length === 0) {
+  		return 0;
+  	}
+  	else {
+  		let sum = 0;
+  		this.items.forEach( item => sum += item );
+  		return sum;
+  	};
+  }
+
+  average() {
+  	if (this.length === 0) {
+  		throw new Error("EmptySortedList");
+  	}
+  	else {
+  		return this.sum() / this.length;
+  	}
+  }
 };
 
 module.exports = SortedList;
