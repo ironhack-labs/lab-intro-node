dca8f9d45fb6fea7ee9165aae80c45f255739d9a
Romulo Franca <romulo2franca@gmail.com>
2018-11-05 23:15:00 -0200

    Commit Exercise

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,39 @@
 class SortedList {
-  constructor() {}
-  add(item) {}
-  get(pos) {}
-  max() {}
-  min() {}
-  average() {}
-  sum() {}
-};
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
+
+  add(item) {
+    this.items.push(item);
+    this.items.sort();
+    this.length = this.items.length;
+  }
+
+  get(pos) {
+    return this.items[pos - 1];
+  }
+
+  max() {
+    if (this.length === 0) throw new Error('EmptySortedList');
+    return this.items.slice(-1);
+  }
+
+  min() {
+    if (this.length === 0) throw new Error('EmptySortedList');
+    return this.items[0];
+  }
+
+  average() {
+    if (this.length === 0) throw new Error('EmptySortedList');
+    return (this.sum() / this.length);
+  }
+
+  sum() {
+    if (this.length === 0) return 0;
+    const sum = this.items.reduce((a, b) => a + b, 0);
+    return sum;
+  }
+}
 
 module.exports = SortedList;
