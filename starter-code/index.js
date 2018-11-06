1f0711b92ca1597d84c83ca7d9ea6c0c5b4ca158
Adèle Revert <revertadele@hotmail.fr>
2018-11-06 12:12:11 +0100

    done

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,60 @@
 class SortedList {
-  constructor() {}
-  add(item) {}
-  get(pos) {}
-  max() {}
-  min() {}
-  average() {}
-  sum() {}
+  constructor() {
+    this.items = [];
+    this.length = 0;
+    this.sum;
+    this.average;
+  }
+
+  add(x) {
+    this.length++;
+    this.items.push(x);
+    this.items.sort(function(a, b) {
+      return a - b;
+    });
+  }
+
+  get(pos) {
+    return this.items[pos];
+  }
+
+  max() {
+    if(this.length === 0){
+      throw new Error("EmptySortedList")
+    } else {
+      var max = Math.max( ...this.items);
+      return max;
+    }
+  }
+  min() {
+    if(this.length === 0){
+      throw new Error("EmptySortedList")
+    } else {
+      var min = Math.min( ...this.items);
+      return min;
+    }
+  }
+
+  average() {
+    if(this.length === 0){
+      throw new Error("EmptySortedList")
+    } else {
+      this.average = this.sum / this.items.length;
+      return this.average;
+    }
+  }
+
+  sum() {
+    if(this.length === 0){
+      return 0;
+    } else {
+      var add = (a, b) =>
+        a + b;
+        this.sum = this.items.reduce(add)
+        return this.sum;
+    }
+  }
 };
+
 
 module.exports = SortedList;
