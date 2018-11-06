18836b79c70d050a74cf6cca1674761ad53e9b93
Daniel Ferrari <danielferrari@live.com>
2018-11-06 00:40:51 -0200

    done

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,32 @@
 class SortedList {
-  constructor() {}
-  add(item) {}
-  get(pos) {}
-  max() {}
-  min() {}
-  average() {}
-  sum() {}
-};
+  constructor() {
+    this.items = [];
+    this.length = 0;
+  }
+
+
+  add(item) {
+    this.items.push(item);
+    this.length = this.items.length;
+    this.items.sort((a, b) => a - b);
+  }
+
+  get(pos) { return this.items[pos - 1]; }
+
+  max() { return this.items[this.length - 1]; }
+
+  min() { return this.items[0]; }
+
+  average() {
+    return this.sum() / this.length;
+  }
+
+  sum() {
+    if (this.length === 0) {
+      return 0;
+    }
+    return this.items.reduce((a, b) => a + b);
+  }
+}
 
 module.exports = SortedList;
