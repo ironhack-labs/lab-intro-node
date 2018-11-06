82b85fb00e6afefefe9481f9e544945a73723987
Mathis Peingnez <mathis.peingnez@gmail.com>
2018-11-06 11:03:49 +0100

    Mathis

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,62 @@
 class SortedList {
-  constructor() {}
-  add(item) {}
-  get(pos) {}
-  max() {}
-  min() {}
-  average() {}
-  sum() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
+
+  add(item) {
+    this.length++;
+    this.items.push(item);
+    this.items.sort( (a, b) => {
+     
+        return a - b;
+      
+    })
+  }
+  get(pos) {
+   return this.items[pos-1];
+  }
+
+
+  max() {
+    if (this.items.length === 0) {
+      throw new Error("EmptySortedList");
+    } else { 
+      return Math.max(...this.items);
+    }
+  }
+
+
+  min() {
+    if (this.items.length === 0) {
+      throw new Error("EmptySortedList");
+    } else { 
+      return Math.min(...this.items);
+    }
+  }
+
+  sum() {
+    if (this.items.length === 0) {
+      return 0;
+    } 
+    else { 
+      return this.items.reduce((accumulator, current) => {
+      return accumulator + current;
+    }, 0) 
+  }
+  }
+  
+  average() {
+    if (this.items.length === 0) {
+      throw new Error("EmptySortedList");
+    } 
+    else {
+      return (this.sum() / this.length);
+    }
+  }
+  
 };
 
 module.exports = SortedList;
+
+
