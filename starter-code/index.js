4b82caf44280b290ffff7e8e06dac987330d07d2
Adèle Revert <revertadele@hotmail.fr>
2018-11-11 17:27:18 +0100

    done

--- starter-code/index.js
+++ starter-code/index.js
@@ -15,46 +15,48 @@ class SortedList {
   }
 
   get(pos) {
-    return this.items[pos];
+    return this.items[pos - 1];
   }
 
   max() {
-    if(this.length === 0){
-      throw new Error("EmptySortedList")
+    if (this.length === 0) {
+      throw new Error("EmptySortedList");
     } else {
-      var max = Math.max( ...this.items);
+      var max = Math.max(...this.items);
       return max;
     }
   }
   min() {
-    if(this.length === 0){
-      throw new Error("EmptySortedList")
+    if (this.length === 0) {
+      throw new Error("EmptySortedList");
     } else {
-      var min = Math.min( ...this.items);
+      var min = Math.min(...this.items);
       return min;
     }
   }
 
   average() {
-    if(this.length === 0){
-      throw new Error("EmptySortedList")
+    if (this.length === 0) {
+      throw new Error("EmptySortedList");
     } else {
-      this.average = this.sum / this.items.length;
-      return this.average;
+      /*       return this.average = this.sum / this.items.length;
+       */
+
+      var add = (a, b) => a + b;
+      this.sum = this.items.reduce(add);
+      return this.sum / this.items.length;
     }
   }
 
   sum() {
-    if(this.length === 0){
+    if (this.length === 0) {
       return 0;
     } else {
-      var add = (a, b) =>
-        a + b;
-        this.sum = this.items.reduce(add)
-        return this.sum;
+      var add = (a, b) => a + b;
+      this.sum = this.items.reduce(add);
+      return this.sum;
     }
   }
-};
-
+}
 
 module.exports = SortedList;
