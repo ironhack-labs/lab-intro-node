1d6b5f4039478c5a2a765d2b1844479760caeae8
Alex McDaniel <amcdaniel186@gmail.com>
2018-11-11 02:23:30 -0500

    update1

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,47 @@
 class SortedList {
-  constructor() {}
+  constructor(items, length) {}
   add(item) {}
   get(pos) {}
   max() {}
   min() {}
   average() {}
   sum() {}
-};
+  
+  let items = [];
+  let length = items.length;
+  
+  function add(addValue) {
+    return x.push(items);
+  }
+  
+  function get(getValue) {
+    return items.nth-child();
+  }
+  
+  function maxV(maxValue) {
+    if (SortedList.length === 0) {
+      throw new Error("EmptySortedList")
+    } else {
+      return Math.max(SortedList);
+    }
+  }
 
-module.exports = SortedList;
+  function minV(minValue) {
+    if (SortedList.length === 0) {
+      throw new Error("EmptySortedList")
+    } else {
+      return Math.min(SortedList);
+    }
+  }
+
+  function sumV(sumValue) {
+    return SortedList.reduce(value);
+  }
+  
+  function averageV(avgValue) {
+    return function sumV() / SortedList.length; 
+  }
+
+};
+  module.exports = SortedList;
+  
