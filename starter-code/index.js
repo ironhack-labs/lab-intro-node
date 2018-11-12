da4c27f71c2f0daef6c0f4393bbbc0ebb34a7bd4
cocobilbao <cocobilbao@gmail.com>
2018-11-12 19:28:27 +0100

    9 passing

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,44 @@
 class SortedList {
-  constructor() {}
-  add(item) {}
-  get(pos) {}
-  max() {}
-  min() {}
-  average() {}
-  sum() {}
+  constructor() {
+    this.items = [];
+    this.length = 0;
+  }
+  add(item) {
+    this.items.push(item);
+    this.length ++;
+  }
+  get(pos) {
+    return this.items[pos -1];
+  }
+  max() {
+    if (this.length == 0){
+      throw new Error("EmptySortedList")
+    } else {
+     return this.items.length[-1];
+    }
+   
+  }
+  min() {
+    if (this.length == 0){
+      throw new Error("EmptySortedList")
+    } else {
+     return this.items.length[0];
+    }
+  }
+  average() {
+    if (this.items == 0){
+      throw new Error("EmptySortedList")
+    } else {
+     return this.max()/this.min;
+    }
+  }
+  sum() {
+    if (this.items == 0){
+      return 0;
+    } else {
+     
+    }
+  }
 };
 
 module.exports = SortedList;
