0d1a30a3ef63995acfa83666304127ea0110b660
Sonia Bravo <rinbra0@hotmail.com>
2018-11-12 18:53:25 +0100

    hecho

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,45 @@
 class SortedList {
-  constructor() {}
-  add(item) {}
-  get(pos) {}
-  max() {}
-  min() {}
-  average() {}
-  sum() {}
+  constructor() {
+   this.items = []
+   this.length = 0;
+  }
+  add(item) {
+    this.items.push(item);
+    this.items.sort(function(a,b){return a-b});
+    this.length+=1;
+  }
+  get(pos) {
+    return this.items[pos-1];
+  }
+  max() {
+    if (this.length == 0){
+      throw new Error("EmptySortedList");
+    }
+
+    return Math.max(...this.items);
+  }
+  min() {
+    if (this.length == 0){
+      throw new Error("EmptySortedList");
+    }
+
+    return Math.min(...this.items);
+  }
+  average() {
+    if (this.length == 0){
+      throw new Error("EmptySortedList");
+    }
+    else {
+   return this.sum()/this.length;}}
+  sum() {
+    if(this.length==0) {
+      return 0;
+
+    } else 
+  return this.items.reduce((total, value)=> total + value, 0)
+  
+  
+  }
 };
 
 module.exports = SortedList;
