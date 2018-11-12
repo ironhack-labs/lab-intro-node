2653daa94bad1adea8aedbf88fbefed187b7ba79
PomPom248 <sofiroggero@gmail.com>
2018-11-12 19:05:03 +0100

    [MAD DE]-Sofia Roggero

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,46 @@
 class SortedList {
-  constructor() {}
-  add(item) {}
-  get(pos) {}
-  max() {}
-  min() {}
-  average() {}
-  sum() {}
+  constructor() {
+    this.items = [];
+    this.length = 0;
+  }
+  add(item) {
+    this.items.push(item);
+    this.length ++;
+    this.items.sort(function(a,b){
+      return a-b;
+    })
+  }
+  
+  get(pos) {
+        return this.items[pos - 1]
+  }
+  max() {
+    if(this.items.length===0) throw new Error("EmptySortedList");
+    else {
+      return Math.max(...this.items)   
+    }
+  }
+  min() {
+    if(this.items.length===0) throw new Error("EmptySortedList");
+    else {
+      return Math.min(...this.items)   
+    }
+  }
+  average() {
+    if(this.items.length===0) throw new Error("EmptySortedList");
+    else {
+      return this.sum()/this.items.length; 
+    }
+  }
+  sum() {
+    if (this.items.length===0){
+      return 0
+    } else {
+      return this.items.reduce(function(acc,val){
+        return acc+val;
+      },0)
+    }
+  }
 };
 
 module.exports = SortedList;
