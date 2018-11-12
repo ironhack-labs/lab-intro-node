98f26e46d7a5e49bd78f9eaeb706e265bca4993c
Fer Perez Ayala <ferp@protonmail.ch>
2018-11-12 18:09:05 +0100

    Exercise complete.

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,34 @@
 class SortedList {
-  constructor() {}
-  add(item) {}
-  get(pos) {}
-  max() {}
-  min() {}
-  average() {}
-  sum() {}
+  constructor() {
+    this.items = []
+    this.length = this.items.length
+  }
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a, b) => { return a - b });
+    this.length = this.items.length;
+  }
+  get(pos) {
+    return this.items[pos - 1];
+  }
+  max() {
+    if (this.length === 0) { throw new Error("EmptySortedList")};
+    return this.items[this.length-1];
+  }
+  min() {
+    if (this.length === 0) { throw new Error("EmptySortedList")};
+    return this.items[0];
+   }
+  average() {
+    if (this.length === 0) { throw new Error("EmptySortedList")};
+    return this.sum()/this.length
+   }
+  sum() {
+    if (this.length === 0) {return 0};
+    return this.items.reduce((a,b) => {
+      return a + b
+    })
+   }
 };
 
 module.exports = SortedList;
