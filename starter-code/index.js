0fe756234474d9f9fdb2209e556f351bb5d87fe9
Javier Samper <jsamperarias@hotmail.com>
2018-11-12 18:13:00 +0100

    Iteración constructor

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,14 @@
 class SortedList {
-  constructor() {}
-  add(item) {}
-  get(pos) {}
-  max() {}
-  min() {}
-  average() {}
-  sum() {}
+    constructor() {
+        this.items = [];
+        this.length = 0;
+    }
+    add(item) {}
+    get(pos) {}
+    max() {}
+    min() {}
+    average() {}
+    sum() {}
 };
 
 module.exports = SortedList;
