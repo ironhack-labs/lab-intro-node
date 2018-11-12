fed2eb8227714932ae0018a52100cb249c3bf8b0
Rodrigo.Munoz <rodrigomunozga@gmail.com>
2018-11-12 20:57:22 +0100

    commit

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,58 @@
 class SortedList {
-  constructor() {}
-  add(item) {}
-  get(pos) {}
-  max() {}
-  min() {}
-  average() {}
-  sum() {}
+  constructor(items, length) {
+    this.items = [];
+    this.length = 0;
+  }
+  add(item) {
+    this.items.push(item);
+  }
+  get(pos) {
+
+    return this.items[pos-1];
+
+  }
+  max() {
+    
+    if (this.items.length <= 0){
+      throw new Error("EmptySortedList");
+    }else{
+      var max = Math.max(...this.items);
+      return max;
+    }
+
+
+  }
+  min() {
+    if(this.items.length <= 0){
+      throw new Error("EmptySortedList");
+    }else{
+      var min = Math.min(...this.items);
+      return min
+    }
+  }
+  average() {
+    if (this.items.length <= 0){
+      throw new Error("EmptySortedList");
+    }else{
+    
+      let ave = this.sum() / this.items.length;
+      return ave
+    }
+
+    
+  }
+  sum() {
+
+    if (this.items.length === 0){
+      return 0;
+    }else{
+      return this.items.reduce(function (ac,cu){
+          return ac+cu;
+      },0)
+    }
+  }
 };
 
 module.exports = SortedList;
+
+
