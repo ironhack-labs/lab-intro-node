77384f9c6096e5b84cff695fbd2a8d25c22fce07
David Diez <ddiezr@gmail.com>
2018-11-12 17:29:51 +0100

    First Iteration Finished exercise

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,43 @@
 class SortedList {
-  constructor() {}
-  add(item) {}
-  get(pos) {}
-  max() {}
-  min() {}
-  average() {}
-  sum() {}
+    constructor() {
+        this.items = [];
+        this.length = 0;
+    }
+    add(item) {
+        this.items.push(item);
+        this.items.sort(function(a, b){return a-b});
+        this.length ++;
+    }
+    get(pos) {
+       return  this.items[pos-1];
+    }
+    max() {
+        if (this.length === 0){
+            throw new Error("EmptySortedList")
+        }else{
+            return Math.max(...this.items);
+        }
+    }
+    min() {
+        if (this.length === 0){
+            throw new Error("EmptySortedList")
+        }else{
+            return Math.min(...this.items);
+        }
+    }
+    average() {
+        if (this.length === 0){
+            throw new Error("EmptySortedList")
+        }else{
+            return this.sum()/this.length;
+        }
+    }
+    sum() {
+
+       return  this.items.reduce(function(ac, value){
+          return ac + value;
+        },0);
+    }
 };
 
 module.exports = SortedList;
