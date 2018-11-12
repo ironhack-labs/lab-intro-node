c2d7d5c2de9456593f6ac5e0fe109a6230464c1c
Yared Santiago <yaredmyers@gmail.com>
2018-11-12 18:53:12 +0100

    ejercicio acabado

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,43 @@
 class SortedList {
-  constructor() {}
-  add(item) {}
-  get(pos) {}
-  max() {}
-  min() {}
-  average() {}
-  sum() {}
+  constructor() {
+    this.items = [];
+    this.length = 0;
+  }
+  
+  add(item) {
+    this.items.push(item);
+    this.items.sort(function(a, b){return a-b});
+    this.length += 1;
+  }
+  get(pos) {
+    return this.items[pos -1];
+  }
+  max() {
+    if (this.length <= 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      return this.items[this.length - 1];
+    }
+  }
+  min() {
+    if (this.length <= 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      return this.items[0];
+    }
+  }
+  average() {
+    if (this.length <= 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      return this.sum()/this.length;
+    }
+  }
+  sum() {
+    return this.items.reduce(function(add , value){
+      return add + value;
+    }, 0)
+  }
 };
 
 module.exports = SortedList;
