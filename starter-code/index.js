8fc16a9ba51a5732309a9d13ee91f0f8ec3f94f1
Javier Samper <jsamperarias@hotmail.com>
2018-11-12 18:51:36 +0100

    Ejercicio completo

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,14 +1,39 @@
 class SortedList {
     constructor() {
         this.items = [];
-        this.length = 0;
+        this.length = this.items.length;
     }
-    add(item) {}
-    get(pos) {}
-    max() {}
-    min() {}
-    average() {}
-    sum() {}
+    add(item) {
+        this.items.push(item);
+        this.items.sort(function(a, b) {
+            return a - b;
+        });
+        this.length += 1;
+
+
+    }
+    get(pos) {
+        return this.items[pos - 1];
+    }
+    max() {
+        if (this.items.length === 0) { throw new Error("EmptySortedList") };
+        return this.items[this.length - 1];
+
+    }
+    min() {
+        if (this.items.length === 0) { throw new Error("EmptySortedList") }
+        return this.items[0];
+    }
+    average() {
+        if (this.items.length === 0) { throw new Error("EmptySortedList") };
+        return this.sum() / this.length;
+    }
+    sum() {
+        if (this.items.length === 0) { return 0 };
+        return this.items.reduce(function(a, b) {
+            return a + b;
+        })
+    }
 };
 
 module.exports = SortedList;
