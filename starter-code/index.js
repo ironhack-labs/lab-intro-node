8f87534e7d7709b84fcc7aada1a3599cc4bb76e4
kovitx <sllonk@gmail.com>
2018-11-13 22:04:05 +0100

    refactor

--- starter-code/index.js
+++ starter-code/index.js
@@ -6,42 +6,30 @@ class SortedList {
   add(x) {
     this.items.push(x);
     this.length++;
-    this.items.sort(function(a, b) {
-      return a - b;
-    });
+    this.items.sort((a, b) => a -b);
   }
   get(pos) {
     return this.items[pos - 1];
   }
   max() {
-    if(this.length === 0) {
-      throw new Error("EmptySortedList");
-    } else {
-      return this.items[this.length - 1];
-    }
+    if(this.length === 0) throw new Error("EmptySortedList");
+    else return this.items[this.length - 1];
   }
   min() {
-    if(this.length === 0) {
-      throw new Error("EmptySortedList");
-    } else {
-      return this.items[0];
-    }
+    if(this.length === 0) throw new Error("EmptySortedList");
+    else return this.items[0];
   }
   average() {
-    if(this.length === 0) {
-      throw new Error("EmptySortedList");
-    } else {
-      return this.sum()/this.length;
-    }
+    if(this.length === 0) throw new Error("EmptySortedList");
+    else return this.sum() / this.length;
   }
   sum() {
-    if(this.length === 0) {
-      return 0;
-    } else {
+    if(this.length === 0) return 0;
+    else {
       const reducer = (accumulator, currentValue) => accumulator + currentValue;
       return this.items.reduce(reducer);
+    }
   }
-}
 };
 
 module.exports = SortedList;
