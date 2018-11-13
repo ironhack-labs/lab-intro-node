d742fe56b314460be22712cb3f985c1fb6a8e201
MarloMorgan <granatogi@gmail.com>
2018-11-13 13:05:10 +0100

    nah

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,8 +1,28 @@
 class SortedList {
-  constructor() {}
-  add(item) {}
-  get(pos) {}
-  max() {}
+  constructor() {
+    this.items = [],
+    this.length = this.items.length
+  }
+  
+  add(x) {
+      this.items.push(x)
+      this.length++;
+      this.items.sort((a, b) => a - b)
+  }
+
+  get(i) {
+    return this.items[i-1]
+  }
+
+  max() {
+    if (this.length === 0) { 
+    throw new Error("EmptySortedList")
+   
+  } else {
+    return this.items[this.length-1];
+  }
+}
+
   min() {}
   average() {}
   sum() {}
