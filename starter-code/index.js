a80e871c0892bceee97c88927ebe1725f2cf1216
adrigt00 <adriangomez@MacBook-Pro-de-Adrian.local>
2018-11-13 08:45:24 +0100

    terminado

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,53 @@
 class SortedList {
-  constructor() {}
-  add(item) {}
-  get(pos) {}
-  max() {}
-  min() {}
-  average() {}
-  sum() {}
+  constructor() {
+   this.items = [];
+   this.length = 0;
+  }
+  add(x) {
+    this.items.push(x);
+    this.items.sort(function(a, b) {
+      return a - b;
+    });
+    this.length++;
+    
+  }
+  get(pos) {
+   return this.items[pos-1];
+  }
+  max() {
+    if (this.length<1) {
+      throw new Error("EmptySortedList")
+    } else {
+   return this.items[this.length - 1]
+    }
+  }
+  min() {
+    if (this.length <1) {
+      throw new Error("EmptySortedList")
+    } else {
+    return this.items[0]
+    }
+  } 
+  
+  average() { 
+    if(this.length < 0){
+    throw new Error("EmptySortedList")
+  }else{
+    return this.sum(this.items)/this.length;
+}
+
+  }
+  sum() {
+   if (this.length > 0){
+     return this.items.reduce(function(a,b){
+     return a + b;
+     },0)
+   } else {
+     return 0;
+   }
+  }
 };
 
 module.exports = SortedList;
+var s1 = new SortedList()
+s1.add(5);
