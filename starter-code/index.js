c0b0eacbaf17361cb28a84ab37cedda392a191f0
lucas <cerfectus@gmail.com>
2018-11-13 19:31:27 -0600

    terminadou

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,25 @@
 class SortedList {
-  constructor() {}
-  add(item) {}
-  get(pos) {}
-  max() {}
-  min() {}
-  average() {}
-  sum() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
+  add(x) {
+    this.items.push(x);
+    this.length++;
+    this.items.sort((a,b) => a - b)
+
+  }
+  get(x) {return this.items[x-1]}
+  max() {return this.items[this.length-1]}
+  min() {return this.items[0]}
+  average() {
+    let average = this.items.reduce((acc, curr) => ,0);
+    return average;
+  }
+  sum() {
+    let total = this.items.reduce((acc, curr) => acc + curr, 0);
+    return total;
+  }
 };
 
 module.exports = SortedList;
