0572bc576e31e2a377be3a38e641c95cd9b87ed8
MefitHp <mefitDev@gmail.com>
2018-11-19 17:18:14 -0600

    Done

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,38 @@
 class SortedList {
-  constructor() {}
-  add(item) {}
-  get(pos) {}
-  max() {}
-  min() {}
-  average() {}
-  sum() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
+  add(item) {
+    this.length++
+    this.items.push(item)
+    return this.items.sort((a, b) => a - b)
+  }
+  get(pos) {
+    return this.items[pos - 1]
+  }
+  max() {
+    if (this.length === 0) {
+      throw new Error("EmptySortedList")
+    }
+    return this.items[this.length - 1]
+  }
+  min() {
+    if (this.length === 0) {
+      throw new Error("EmptySortedList")
+    }
+    return this.items[0]
+  }
+  average() {
+    if (this.length === 0) {
+      throw new Error("EmptySortedList")
+    }
+    return this.items.reduce((a, b) => a + b, 0) / this.length
+  }
+  sum() {
+    return this.length === 0 ? this.length : this.items.reduce((a, b) => a + b, 0)
+  }
+
 };
 
 module.exports = SortedList;
