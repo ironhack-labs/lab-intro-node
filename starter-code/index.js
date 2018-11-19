10c3638c7a34b03589074d48c00a73dbfa2c72f1
Carlos Rivera <carlos.rivera.cabrera@gmail.com>
2018-11-19 17:51:59 -0600

    done

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,49 @@
 class SortedList {
-  constructor() {}
-  add(item) {}
-  get(pos) {}
-  max() {}
-  min() {}
-  average() {}
-  sum() {}
+  constructor() {
+    this.items = []
+    this.length = 0
+  }
+  add(item) {
+    this.items.push(item)
+    this.items.sort((a, b) => a - b)
+    this.length++
+  }
+  get(pos) {
+    if(pos < 0 || pos > this.length){
+      return Error("OutOfBounds")
+    } else {
+      return this.items[pos-1]
+    }
+  }
+  max() {
+    if(this.length === 0){
+      return Error("EmptySortedList")
+    } else {
+      return this.items[this.length-1]
+    }
+  }
+  min() {
+    if(this.length === 0){
+      return Error("EmptySortedList")
+    } else {
+      return this.items[0]
+    }
+  }
+  average() {
+    if(this.length === 0){
+      return Error("EmptySortedList")
+    } else {
+      return this.sum()/this.length
+    }
+  }
+  sum() {
+    // const reducer = (accumulator, currentValue) => accumulator + currentValue;
+    if(this.length === 0){
+      return 0
+    } else {
+      return this.items.reduce((accumulator, currentValue) => accumulator + currentValue)
+    }
+  }
 };
 
 module.exports = SortedList;
