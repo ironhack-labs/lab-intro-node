4ca1ebb3d68a0742a9b81eea413a97452666651d
Rene Manzano <rene@Renes-MacBook.local>
2018-11-19 20:12:37 -0600

    Done

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,57 @@
 class SortedList {
-  constructor() {}
-  add(item) {}
-  get(pos) {}
-  max() {}
-  min() {}
-  average() {}
-  sum() {}
+  constructor(items=[]) {
+    this.SortedList = items;
+    this.length = this.SortedList.length;
+
+  }
+  add(item) {
+    this.SortedList.push(item);
+  }
+  get(pos) {
+    return this.SortedList[pos];
+  }
+  max() {
+    let ordenado = this.SortedList.sort(function(a, b){return a-b});
+    try{
+    
+      if(this.len===0) throw new Error("EmptySortedList")
+      let max = ordenado[this.len-1];
+      return max;
+      
+  
+    }catch(e){
+      return e.message;
+    }
+    
+  }
+  min() {
+    let ordenado = this.SortedList.sort(function(a, b){return a-b});
+    try{
+    
+      if(this.len===0) throw new Error("EmptySortedList")
+      let min = ordenado[0];
+      return min;
+      
+  
+    }catch(e){
+      return e.message;
+    }
+  }
+  average() {
+    let suma = 0
+    this.SortedList.forEach(element => {
+      suma +=element;
+      });
+    return suma/this.len
+  }
+  sum() {
+    let suma = 0
+    this.SortedList.forEach(element => {
+      suma +=element;
+      
+    });
+    return suma;
+  }
 };
 
 module.exports = SortedList;
