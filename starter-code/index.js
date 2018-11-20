cb3dd97c2ce42f35ff8d591cc6275e958d7b2547
conviertemx <ro@convierte.mx>
2018-11-19 18:50:20 -0600

    done

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,55 @@
 class SortedList {
-  constructor() {}
-  add(item) {}
-  get(pos) {}
-  max() {}
-  min() {}
-  average() {}
-  sum() {}
+  constructor() {
+    this.items = []
+    this.length = this.items.length
+  }
+  add(item) {
+    this.length++
+    this.items.push(item)
+    return this.items.sort((a,b)=>{
+      return a-b
+    })
+  }
+  get(pos) {
+    //console.log(this.items[pos-1])
+     return this.items[pos -1]
+  }
+
+  max() {
+    if(this.length === 0){
+      throw new Error("EmptySortedList")
+    }
+      return this.items[this.length-1]
+  }
+
+  min() {
+    if(this.length === 0){
+      throw new Error("EmptySortedList")
+    }
+      return this.items[0]
+  }
+  
+  average() {
+    if(this.length === 0){
+      throw new Error("EmptySortedList")
+    }
+      return this.items/[this.length]
+  }
+
+  sum() {
+    var suma = 0
+
+    if(this.length === 0){
+      return 0
+    }else {
+      this.items.forEach(function(element) {
+        suma += element
+     
+       });
+       return suma
+    }
+      
+  }
 };
 
 module.exports = SortedList;
