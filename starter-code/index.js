2401a1154e987712843f22ef32193953ec347a25
Pily <paila@live.com.mx>
2018-11-19 18:03:13 -0600

    Intro Node

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,56 @@
 class SortedList {
-  constructor() {}
-  add(item) {}
-  get(pos) {}
-  max() {}
-  min() {}
-  average() {}
-  sum() {}
+  constructor() {
+    this.items = []
+    this.length = 0
+  }
+  add(item) {
+    this.items.push(item)
+    this.items.sort((a,b)=> (a-b))
+    this.length = this.items.length
+  }
+  get(pos) {
+    return this.items[pos - 1]
+  }
+  max() {
+    if(this.length === 0){
+      throw new Error("EmptySortedList")
+    }else{
+      return Math.max(...this.items)
+    }
+    
+
+  }
+  min() {
+    if(this.length === 0){
+      throw new Error("EmptySortedList")
+    }else{
+      return Math.min(...this.items)
+    }
+    
+  }
+  average() {
+    if( this.length===0){
+      throw new Error("EmptySortedList")
+    }else{
+      var total = 0
+    for (var element of this.items){
+      total += element
+    }
+    
+    }
+
+    return total / this.length
+  }
+  sum() {
+    var total = 0
+    for (var element of this.items){
+      total += element
+    }
+
+    return total
+  }
 };
 
 module.exports = SortedList;
+
+
