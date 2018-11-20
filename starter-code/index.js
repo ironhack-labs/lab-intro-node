8bda30a6afb1d65f888e26c8af18976c85f49e9a
MontseOrtiz <montse_2908@hotmail.com>
2018-11-19 19:57:51 -0600

    listo

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,51 @@
 class SortedList {
-  constructor() {}
-  add(item) {}
-  get(pos) {}
-  max() {}
-  min() {}
-  average() {}
-  sum() {}
-};
+  constructor() {
+    this.items=[]
+    this.length = 0
+  }
+  add(x) {
+    this.items.push(x)
+    this.items.sort(function(a,b){
+      return a-b
+    })
+    
+    this.length++
+  }
+  get(pos) {
+    return this.items[pos-1]
+  }
+  max() {
+    if (this.items.length=== 0){
+      throw new Error("EmptySortedList")
+    }else {
+      return this.items[this.items.length-1]
+    }
+    
+  }
+  min() {
+    if (this.items.length=== 0){
+      throw new Error("EmptySortedList")
+    }else {
+      return this.items[0]
+    }
+  }
+  average() {
+    if (this.length>0){
+      let sum = this.items.reduce((a,b)=>a+b)
+      return sum/this.length
+    }else {
+      throw new Error("EmptySortedList")
+    }
+    
+  }
+  sum() {
+    if (this.length>0){
+      return this.items.reduce((a,b)=>a+b)
+    }else{
+      return 0
+      }
+    }
+  }
+// };
 
 module.exports = SortedList;
