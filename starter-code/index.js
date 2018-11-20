64dc6490394becc4c3d599cc34a0209926720e1a
Fermin Lopez <fermin.lopez96@gmail.com>
2018-11-19 23:57:31 -0600

    done

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,57 @@
 class SortedList {
-  constructor() {}
-  add(item) {}
-  get(pos) {}
-  max() {}
-  min() {}
-  average() {}
-  sum() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length
+  }
+  add(item) {
+    this.length++
+    this.items.push(item)
+    this.items.sort(function(a, b){
+     return (a - b) 
+    })
+  }
+  get(pos) {
+    if( pos < 1 || pos > this.length){
+      return new Error('OutofBounds')
+    }else{
+   return this.items[pos-1]
+    }
+  }
+  max() {
+    if(this.length === 0){
+      throw new Error('EmptySortedList')
+    }else{
+      return Math.max(...this.items)
+    }
+  }
+  min() {
+    if(this.length === 0){
+      throw new Error('EmptySortedList')
+    }else{
+      return Math.min(...this.items)
+    }
+  }
+  average() {
+    if(this.length === 0){
+      throw new Error('EmptySortedList')
+    }else{
+      let added = 0;
+      this.items.forEach(function(item){
+        return added += item;
+      })
+      return added/this.length;
+    }
+  }
+  sum() {
+    if(this.length === 0){
+      return 0
+    }else{
+    let itemsSum = 0;
+    this.items.forEach(function(x){
+      return itemsSum += x;
+    })
+  }
+  }
 };
 
 module.exports = SortedList;
