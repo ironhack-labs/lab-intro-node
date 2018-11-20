8fb605c0a94fca4447e0fc56528240aeac2a7d6f
Fernando Hernández Báez <Fernando1@MacBook-Pro-de-Fernando-6.local>
2018-11-19 18:33:16 -0600

    done

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,59 @@
 class SortedList {
-  constructor() {}
-  add(item) {}
-  get(pos) {}
-  max() {}
-  min() {}
-  average() {}
-  sum() {}
+  constructor() {
+    this.items=[]
+    this.length=this.items.length;
+  }
+
+  add(item) {
+  
+  this.length ++
+  this.items.push(item)
+  this.items.sort(function(a, b) {
+    return a - b
+  }
+    )
+  }
+
+
+  get(pos) {
+    if (pos < 1 || pos > this.items.length){
+      return new Error("OutOfBounds")
+    } else {
+    return this.items[pos - 1]
+  }}
+
+
+
+  
+  max() {
+    if ( this.length === 0 ){
+      throw new Error("EmptySortedList")
+    } 
+      return this.items[this.length - 1]
+
+  }
+  min() {
+    if ( this.length === 0 ){
+      throw new Error("EmptySortedList")
+    } return this.items[0]
+
+  }
+  average() {
+    if (this.length === 0) {
+      throw new Error("EmptySortedList")
+    }
+
+
+  return this.items.reduce((a, b) => a + b, 0) / this.length
+
+  }
+  sum() {
+    {
+      return this.length === 0 ? this.length : this.items.reduce((a, b) => a + b, 0)
+    }
+    
+
+  }
 };
 
 module.exports = SortedList;
