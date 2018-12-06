0bbe326ccdf098fdbc8fde3d66216ded1073ac56
Susanne Tünker <susanne.tuenker@hotmail.de>
2018-12-06 21:15:40 +0100

    done

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,43 @@
 class SortedList {
-  constructor() {}
-  add(item) {}
-  get(pos) {}
-  max() {}
-  min() {}
-  average() {}
-  sum() {}
+  constructor() {
+    this.items = [];
+    this.length = 0;
+  }
+  add(item) {
+     this.items.push(item);
+     this.length++;
+     this.items.sort((a,b) => a-b)
+     
+  }
+  get(pos) {
+    return this.items[pos-1];
+   
+  }
+  max() {
+    if (this.items.length === 0) {throw new Error("EmptySortedList")} else{
+      return this.items[this.items.length-1]
+    }
+  }
+
+  min() {
+    if (this.items.length === 0) {throw new Error("EmptySortedList")} else{
+      return this.items[0]
+    }
+
+
+  }
+  average() {
+    
+    if (this.items.length === 0) {throw new Error("EmptySortedList")} else{
+      return this.sum() / this.length;
+    }
+  }
+  sum() {
+      return this.items.reduce(function(acc,curr){
+      return acc + curr
+    },0) 
+  }
 };
 
 module.exports = SortedList;
+
