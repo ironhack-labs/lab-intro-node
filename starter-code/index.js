668e5f49719366220e61ae77aa48e341df94453b
Katharina Koenig <k.koenig1811@gmail.com>
2018-12-06 21:27:41 +0100

    Berlin-Nir-Katharina

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,8 +1,36 @@
 class SortedList {
-  constructor() {}
-  add(item) {}
-  get(pos) {}
-  max() {}
+  constructor() {
+    this.items = [];
+    this.length = 0;
+  }
+
+
+  add(item) {
+    this.items.push(item);
+    this.length++;
+    this.items.sort(function (a, b) {
+      return a - b;
+    });
+  }
+
+
+  get(pos) {
+
+    if (this.items[pos - 1] === null) {
+      throw new Error("error");
+    } else {
+      return this.items[pos - 1];
+    }
+  }
+  max() {
+    if (this.length === 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      return this.items[this.length - 1];
+    }
+  }
+
+
   min() {}
   average() {}
   sum() {}
