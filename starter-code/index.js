1f566ae104cd01e1684d02de22f68d4b767b1a1e
annearbor <betting.anne@gmail.com>
2018-12-06 21:49:36 +0100

    done

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,48 @@
 class SortedList {
-  constructor() {}
-  add(item) {}
-  get(pos) {}
-  max() {}
-  min() {}
-  average() {}
-  sum() {}
-};
+  constructor() {
+    this.length = 0;
+    this.items = [];
+  }
 
+  add(item) {
+    this.length++;
+    this.items.push(item);
+    this.items.sort((a, b) => a - b);
+  }
+
+  get(pos) {
+    return this.items[pos - 1];
+  }
+
+  max() {
+    if (this.length === 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      return this.items[this.length - 1];
+    }
+  }
+  min() {
+    if (this.length === 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      return this.items[0];
+    }
+  }
+  average() {
+    if (this.length === 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      return this.sum() / this.length;
+    }
+  }
+  sum() {
+    if (this.length === 0) {
+      return 0;
+    } else {
+      return this.items.reduce(function(acc, val) {
+        return acc + val;
+      });
+    }
+  }
+}
 module.exports = SortedList;
