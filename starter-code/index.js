baa8b99bbef830f05e09e3f98e35e0ac537903cb
hobglobin27 <hobglobin27@gmail.com>
2018-12-21 12:23:06 -0600

    Version completa Introduccion Node

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,40 @@
 class SortedList {
-  constructor() {}
-  add(item) {}
-  get(pos) {}
-  max() {}
-  min() {}
-  average() {}
-  sum() {}
+  constructor() {
+    this.items=[];
+    this.length=0;
+  }
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a,b)=>a-b);
+    this.length++;
+  }
+  get(pos){
+    return this.items[pos-1];
+  }
+  max() {
+    if(this.length===0)
+      throw new Error("EmptySortedList");
+    return this.items[this.length-1];
+  }
+  min() {
+    if(this.length===0)
+      throw new Error("EmptySortedList");
+    return this.items[0];
+  }
+  average() {
+    if(this.length===0)
+      throw new Error("EmptySortedList");
+    return this.sum()/this.length;
+  }
+  sum() {
+    let sum=0;
+    this.items.forEach((elem)=>sum+=elem);
+    return sum;
+  }
+  Error(mensaje) {
+   this.mensaje = mensaje;
+   this.nombre = "ExceptionSortedList";
+ }
 };
 
 module.exports = SortedList;
