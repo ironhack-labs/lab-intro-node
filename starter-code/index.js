9d434e2682240888f145f7a032b7d161a61d9a94
noedavidgonzalez <noedabid@gmail.com>
2018-12-20 22:15:46 -0600

    finish

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,50 @@
+function getSum(total, num) {
+  return total + Math.round(num);
+}
+
 class SortedList {
-  constructor() {}
-  add(item) {}
-  get(pos) {}
-  max() {}
-  min() {}
-  average() {}
-  sum() {}
+  constructor() {
+  	this.items=[];
+  	this.length = this.items.length;
+  }
+  add(item) {
+  	this.items.push(item);
+  	this.items.sort();
+  	this.length++; 
+  }
+  get(pos) { 
+  	if((pos-1)<=this.length)
+  		return (this.items[pos-1]);
+  	else throw new Error("OutOfBounds") 
+  }
+  max() {
+  	if(this.length==0) throw new Error("EmptySortedList");
+  	else return (Math.max( ...this.items ));
+  }
+  min() {
+  	if(this.length==0) throw new Error("EmptySortedList");
+  	else return (Math.min( ...this.items ));
+  }
+  average() {
+  	if(this.length==0) throw new Error("EmptySortedList");
+  	let total=0;
+  	this.items.forEach(function(item){
+  		total+=item;
+  	});
+  	
+  	return total/this.length;
+  }
+  sum() {
+  	if(this.length==0) return 0;
+  	let total=0;
+  	this.items.forEach(function(item){
+  		total+=item;
+  	});
+  	return total;
+  }
+
 };
+
+
 
 module.exports = SortedList;
