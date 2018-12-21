3449f8316030caaf4795ed0e2d43406d5480ab34
AntonioSalazar <antonio.salazar.emrsn@gmail.com>
2018-12-20 23:11:13 -0600

    work in progress

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,23 @@
 class SortedList {
-  constructor() {}
-  add(item) {}
-  get(pos) {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
+  add(item) {
+    this.items.push(item)
+    this.length++
+  }
+  get(pos) {
+    this.items = 
+    console.log(pos);
+  }
   max() {}
   min() {}
   average() {}
   sum() {}
 };
+
+var sl = new SortedList();
+
 
 module.exports = SortedList;
