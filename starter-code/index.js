84b6826b4528e0e484ddfccc4a264543e945ea4f
Salazar <antonio.salazar@oliverwyman.com>
2018-12-21 16:23:31 -0600

    only one iteration missing

--- starter-code/index.js
+++ starter-code/index.js
@@ -6,15 +6,49 @@ class SortedList {
   add(item) {
     this.items.push(item)
     this.length++
+    this.items.sort(function(a, b){
+      return a - b
+    })
   }
   get(pos) {
-    this.items = 
-    console.log(pos);
+    return pos = this.items[pos - 1]
   }
-  max() {}
-  min() {}
-  average() {}
-  sum() {}
+  max() {
+    if(this.items.length === 0){
+      throw new Error("EmptySortedList")
+    } else{
+      return this.items = Math.max(...this.items)
+    }
+    
+  }
+  min() {
+    if(this.items.length === 0){
+      throw new Error("EmptySortedList")
+    } else{
+      return this.item = Math.min(...this.items)
+    }
+  }
+  average() {
+    if (this.items.length === 0) {
+      throw new Error("EmptySortedList")
+    } else{
+      var tot = 0;
+      for (var index = 0; index < this.items.length; index++) {
+        tot += this.items[index];
+        var avg = tot / this.items.length
+        return avg
+      }
+      
+    }
+  }
+
+  sum() {
+    if(this.items.length === 0){
+      return 0
+    } else{
+      return this.items.reduce((a, b) => a + b)
+    }
+  }
 };
 
 var sl = new SortedList();
