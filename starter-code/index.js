348c9658c97b70e6bc028e17637001b4caf31253
Nicolas Castro <nicolascastro@me.com>
2019-01-12 15:36:03 -0500

    Lab Completed

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,37 @@
 class SortedList {
-  constructor() {}
-  add(item) {}
-  get(pos) {}
-  max() {}
-  min() {}
-  average() {}
-  sum() {}
+  constructor() {
+    this.items = [];
+    this.length = 0;
+  }
+  add(item) {
+    this.items.push(item);
+    this.length = this.items.length;
+    this.items.sort((a,b)=>a-b);
+  }
+  get(pos) {
+    return this.items[pos-1];
+  }
+  max() {
+    if(this.items.length > 0)
+      return this.items[this.length-1];
+    else 
+      throw new Error("EmptySortedList");
+  }
+  min() {
+    if(this.items.length > 0)
+      return this.items[0];
+    else 
+      throw new Error("EmptySortedList");
+  }
+  average() {
+    if(this.length)
+    return this.sum()/this.length;
+    else
+    throw new Error("EmptySortedList");
+  }
+  sum() {
+    return this.items.reduce((item, reducer)=>{return item+reducer},0)
+  }
 };
 
 module.exports = SortedList;
