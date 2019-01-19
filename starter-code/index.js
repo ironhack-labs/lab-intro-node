fcae261e69d3eaf4e330bd574d6943fcfe1d3c64
Rodrigo Yoshimoto <rodrigo.yoshimoto@gmail.com>
2019-01-19 17:20:54 -0200

    Node Practice

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,32 @@
 class SortedList {
-  constructor() {}
-  add(item) {}
-  get(pos) {}
-  max() {}
-  min() {}
-  average() {}
-  sum() {}
-};
+  constructor() {
+    this.items = [];
+    this.length = 0;
+  }
+  add(item) {
+    this.items.push(item);
+    this.length++;
+    this.items.sort((a,b) => a - b)
+  }
+  get(pos) {
+    return this.items[pos-1];
+  }
+  max() {
+    if (this.length === 0) throw new Error("EmptySortedList");
+    return this.get(this.length);
+  }
+
+  min() {
+    if (this.length === 0) throw new Error("EmptySortedList");
+    return this.get(1);
+  }
+  average() {
+    if (this.length === 0) throw new Error("EmptySortedList");
+    return this.sum()/this.length;
+  }
+  sum() {
+    return this.items.reduce((a,b) => a + b, 0);
+  }
+}
 
 module.exports = SortedList;
