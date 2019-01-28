5f25e9f60bb68b272e3e1dc2bbde42e61a9f4108
Didac Bigorda <didacbigorda@gmail.com>
2019-01-28 20:17:07 +0100

    First four tests passing

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,7 +1,19 @@
 class SortedList {
-  constructor() {}
-  add(item) {}
-  get(pos) {}
+  constructor() {
+    this.items = [];
+    this.length = 0;
+  }
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a,b) => a - b);
+    this.length++;
+  }
+  get(pos) {
+    if (pos > this.length) {
+      return new Error("OutOfBounds");
+    }
+    return this.items[pos - 1];
+  }
   max() {}
   min() {}
   average() {}
