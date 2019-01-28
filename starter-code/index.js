be676c3c4b49a4a846a9f3d153d7ce22fbdf1341
Bastien Tran <bastien.tran@gmail.com>
2019-01-28 19:11:03 +0100

    all good

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,56 @@
 class SortedList {
-  constructor() {}
-  add(item) {}
-  get(pos) {}
-  max() {}
-  min() {}
-  average() {}
-  sum() {}
-};
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
+
+  add(item) {
+    this.items.push(item);
+    this.items.sort(function(a, b) {
+      return a - b;
+    });
+    this.length = this.items.length;
+  }
+  get(pos) {
+    var nth = this.items[pos - 1];
+    return nth;
+  }
+  max() {
+    if (this.items.length === 0) {
+      throw new Error("EmptySortedList");
+    }
+    this.items.sort(function(a, b) {
+      return a - b;
+    });
+    return this.items[this.items.length - 1];
+  }
+  min() {
+    if (this.items.length === 0) {
+      throw new Error("EmptySortedList");
+    }
+    this.items.sort(function(a, b) {
+      return a - b;
+    });
+    return this.items[0];
+  }
+  sum() {
+    if (this.items.length === 0) {
+      return 0;
+    }
+    var summedArray = this.items.reduce(function(acc, oneItem) {
+      return acc + oneItem;
+    });
+    return summedArray;
+  }
+  average() {
+    if (this.items.length === 0) {
+      throw new Error("EmptySortedList");
+    }
+    var summedArray = this.items.reduce(function(acc, oneItem) {
+      return acc + oneItem;
+    });
+    return summedArray / this.items.length;
+  }
+}
 
 module.exports = SortedList;
