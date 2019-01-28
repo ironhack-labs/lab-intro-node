596101fb469402f943ff43b2af2474bfb944c842
dreazz <agustinsilveira.96@gmail.com>
2019-01-28 21:05:56 +0100

    11 passing 4 failing

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,49 @@
 class SortedList {
-  constructor() {}
-  add(item) {}
-  get(pos) {}
-  max() {}
-  min() {}
-  average() {}
-  sum() {}
-};
+  constructor() {
+    this.length = 0;
+    this.array = [];
+  }
+  add(item) {
+    this.array.push(item);
+    this.length++;
+    this.array.sort((a, b)=>{ return a+b })
+  }
+  get(pos) {
+    if (pos > this.length) {
+      return new Error("OutOfBounds");
+    }
+    return this.array[pos - 1];
+  }
+  max() {
+    if (this.length === 0) {
+      throw new Error("EmptySortedList");
+    }
+    return Math.max.apply(Math, this.array);
+  }
+  min() {
+    if (this.length === 0) {
+      throw new Error("EmptySortedList");
+    }
+    return Math.min.apply(Math, this.array);
+  }
+
+  average() {
+    if (this.length === 0) {
+      throw new Error("EmptySortedList");
+    }
+    
+    
+    
+  }
+  sum() {
+    if (this.length === 0) {
+      return 0;
+    }
+    
+    return this.array.forEach(element => {
+       let sumatory = sumatory + element
+     });
+  }
+}
 
 module.exports = SortedList;
