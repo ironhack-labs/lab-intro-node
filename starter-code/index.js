db031dfb970aeb18b6e63986b9b12efb1dc5359e
Henrique Mendes <henrique.gmendes@hotmail.com>
2019-01-28 17:48:20 -0200

    done

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,46 @@
 class SortedList {
-  constructor() {}
-  add(item) {}
-  get(pos) {}
-  max() {}
-  min() {}
-  average() {}
-  sum() {}
-};
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
+
+  add(items) {
+    this.items.push(items);
+    this.length = this.items.length;
+  }
+
+  get(pos) {
+    this.items.sort((a, b) => a - b);
+    return this.items[pos - 1];
+  }
+
+  max() {
+    if (this.items.length === 0) {
+      throw new Error('EmptySortedList');
+    }
+    return Math.max(...this.items);
+  }
+
+  min() {
+    if (this.items.length === 0) {
+      throw new Error('EmptySortedList');
+    }
+    return Math.min(...this.items);
+  }
+
+  average() {
+    if (this.items.length === 0) {
+      throw new Error('EmptySortedList');
+    }
+    return this.sum() / this.items.length;
+  }
+
+  sum() {
+    if (this.items.length === 0) {
+      return 0;
+    }
+    return this.items.reduce((a, b) => a + b);
+  }
+}
 
 module.exports = SortedList;
