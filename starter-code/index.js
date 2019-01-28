94540b09f1d77819e1ed9a5fe5a92b05328bb8b6
Miguelerja <miguelerja@gmail.com>
2019-01-28 18:41:08 +0100

    Add solutions to first 3 exercises

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,55 @@
 class SortedList {
-  constructor() {}
-  add(item) {}
-  get(pos) {}
-  max() {}
-  min() {}
-  average() {}
-  sum() {}
+  constructor() {
+    this.length = 0;
+    this.sortedList = [];
+  }
+
+  add(item) {
+    this.sortedList.push(item);
+    this.length = this.sortedList.length;
+  }
+
+  get(pos) {
+    if (pos > this.length) {
+      return new Error('OutOfBounds');
+    }
+    return this.sortedList[pos];
+  }
+
+  max() {
+    if (this.sortedList.length > 0) {
+      Math.max(this.sortedList);
+    } else {
+      throw new Error('EmptySortedList');
+
+    }
+  }
+
+  min() {
+    if (this.sortedList.length > 0) {
+      Math.min(this.sortedList);
+    } else {
+      throw new Error('EmptySortedList');
+
+    }
+  }
+
+  average() {
+    if (this.sortedList.length > 0) {
+      return this.sortedList.sum() / this.sortedList.length;
+    } else {
+      throw new Error('EmptySortedList');
+    }
+  }
+  
+  sum() {
+    if (this.sortedList.length > 0) {
+      return this.sortedList.reduce((acc, val) => {
+        return acc + val;
+      });
+    }
+    return 0;
+  }
 };
 
 module.exports = SortedList;
