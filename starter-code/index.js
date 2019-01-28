38d8c976df5efc095084a7ae0cfed944aa4d3a1f
Alexandre <alexandre.joly13@gmail.com>
2019-01-28 20:36:14 +0100

    Paris - Poeli & Alexandre

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,54 @@
 class SortedList {
-  constructor() {}
-  add(item) {}
-  get(pos) {}
-  max() {}
-  min() {}
-  average() {}
-  sum() {}
-};
+  constructor() {
+    this.items = [];
+    this.length = 0;
+  }
+  add(item) {
+    this.items.push(item);
+    this.length = this.items.length;
+    this.items.sort(function(a, b) {
+      return a - b;
+    });
+  }
+  get(pos) {
+    return this.items[pos - 1];
+  }
+  max() {
+    if (this.length === 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      return this.items[this.length - 1];
+    }
+  }
+  min() {
+    if (this.length === 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      return this.items[0];
+    }
+  }
+
+  sum() {
+    let sum = 0;
+    this.items.forEach(function(oneNumber) {
+      sum += oneNumber;
+    });
+    return sum;
+  }
+
+  average() {
+    if (this.length === 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      let sum = 0;
+      let average = 0;
+      this.items.forEach(function(oneNumber) {
+        sum += oneNumber;
+      });
+      average = sum / this.length;
+      return average;
+    }
+  }
+}
 
 module.exports = SortedList;
