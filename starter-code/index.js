f14355bb24a99bd0590b3bdcab17056474cf64c9
Malte <m.felmy@googlemail.com>
2019-01-28 13:57:57 +0100

    BER - Malte, submission

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,48 @@
 class SortedList {
-  constructor() {}
-  add(item) {}
-  get(pos) {}
-  max() {}
-  min() {}
-  average() {}
-  sum() {}
-};
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
+  add(item) {
+    this.items.push(item);
+    this.length = this.items.length;
+    this.items.sort((a, b) => (a - b));
+  }
+  get(pos) {
+    if (pos > this.length || pos === 0) {
+      throw new Error("OutOfBounds");
+    } else {
+      return this.items[pos - 1];
+    }
+  }
+  max() {
+    if (this.length === 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      return this.items[this.length - 1];
+    }
+  }
+  min() {
+    if (this.length === 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      return this.items[0];
+    }
+  }
+  average() {
+    if (this.length === 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      return this.sum() / this.length;
+    }
+  }
+  sum() {
+    if (this.length === 0) {
+      return 0;
+    } else {
+      return this.items.reduce((acc, val) => acc + val);
+    }
+  }
+}
 
 module.exports = SortedList;
