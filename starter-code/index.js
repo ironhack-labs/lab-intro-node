259634026167fd83112adc23c5adaa00df41369d
Yoshitsugu Nagao <yoshitsugunagao@gmail.com>
2019-01-28 19:21:53 +0100

    done

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,49 @@
 class SortedList {
-  constructor() {}
-  add(item) {}
-  get(pos) {}
-  max() {}
-  min() {}
-  average() {}
-  sum() {}
+  constructor() {
+    this.length = 0;
+    this.items = [];
+  }
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a,b) => a-b);
+    this.length++;
+  }
+  get(pos) {
+    if(pos > this.length) {
+      throw new Error("OutOfBounds")
+    }
+    return this.items[pos - 1];
+  }
+  max() {
+    if(this.length > 0) {
+      return Math.max(...this.items);
+    } else {
+      throw new Error("EmptySortedList");
+    }
+  }
+  min() {
+    if(this.length > 0) {
+      return Math.min(...this.items);
+    } else {
+      throw new Error("EmptySortedList");
+    }
+  }
+  sum() {
+    let result = 0;
+    if(this.length > 0) {
+      result = this.items.reduce(function(accu,num) {
+        return accu + num;
+      });
+    }
+    return result;
+  }
+  average() {
+    if(this.length > 0) {
+      return this.sum() / this.length;
+    } else {
+      throw new Error("EmptySortedList");
+    }
+  }
 };
 
 module.exports = SortedList;
