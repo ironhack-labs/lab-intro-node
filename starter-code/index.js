106f16ecbed769dc7b49b2658190de60f4ab2b12
Javier Aceña <j0nl1@MacBook-Pro-de-Javier.local>
2019-01-28 21:32:35 +0100

    Exercise Complete

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,35 @@
 class SortedList {
-  constructor() {}
-  add(item) {}
-  get(pos) {}
-  max() {}
-  min() {}
-  average() {}
-  sum() {}
+  constructor() {
+    this.items = []
+    this.length = this.items.length
+  }
+  add(item) {
+    this.items.push(item)
+    this.items.sort((a, b) => a - b)
+    this.length = this.items.length
+  }
+  get(pos) {
+    this.length = this.items.length
+    if(!this.items[pos-1]) {
+      return "OutOfBounds"
+    }
+    return this.items[pos-1]
+  }
+  max() {
+    if(!this.items.length) throw new Error("EmptySortedList")
+    return this.items[this.items.length - 1]
+  }
+  min() {
+    if(!this.items.length) throw new Error("EmptySortedList")
+    return this.items[0]
+  }
+  average() {
+    if(!this.items.length) throw new Error("EmptySortedList")
+    return this.items.reduce((a, b) => a + b)/this.items.length
+  }
+  sum() {
+    return (this.items.length == 0) ? 0 : this.items.reduce((a, b) => a + b)
+  }
 };
 
 module.exports = SortedList;
