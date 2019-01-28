68e7f12c51598dfc03d5ba0e6e1d4b1e9dbf0eb1
Bruno Dolce <brudolce@gmail.com>
2019-01-28 17:13:35 -0300

    done

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,50 @@
 class SortedList {
-  constructor() {}
-  add(item) {}
-  get(pos) {}
-  max() {}
-  min() {}
-  average() {}
-  sum() {}
+  constructor(items = []) {
+    this.items = items;
+    this.length = this.items.length;
+  }
+
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a, b) => a - b);
+    this.length = this.items.length;
+  }
+
+  get(pos) {
+    return this.items[pos - 1];
+  }
+
+  max() {
+    if (this.length === 0) {
+      throw new Error('EmptySortedList');
+    } else {
+      return this.items[this.length - 1];
+    }
+  }
+
+  min() {
+    if (this.length === 0) {
+      throw new Error('EmptySortedList');
+    } else {
+      return this.items[0];
+    }
+  }
+
+  sum() {
+    if (this.length === 0) {
+      return 0;
+    }
+    return this.items.reduce((a, b) => a + b);
+  }
+
+  average() {
+    if (this.length === 0) {
+      throw new Error('EmptySortedList');
+    } else {
+      return this.sum() / this.length;
+    }
+  }
+
 };
 
 module.exports = SortedList;
