e814dd0cbf068fb585c263b0464f4896048c043a
Marcos Tagliabue <marcostagliabue103@gmail.com>
2019-01-28 19:34:08 +0100

    done

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,47 @@
 class SortedList {
-  constructor() {}
-  add(item) {}
-  get(pos) {}
-  max() {}
-  min() {}
-  average() {}
-  sum() {}
+  constructor() {
+    this.length = 0;
+    this.items = [];
+  }
+  add(item) {
+    this.items.push(item);
+    this.length +=1;
+    this.items.sort((a,b) => a-b);
+  }
+  get(pos) {
+    return this.items[pos-1];
+  }
+  max() {
+    if (this.length === 0){
+      throw new Error("EmptySortedList");
+    } else {
+    return Math.max(...this.items);
+    }
+  }
+
+  min() {
+    if (this.length === 0){
+      throw new Error("EmptySortedList");
+    } else {
+    return Math.min(...this.items);
+    }
+  }
+
+  average() {
+    if (this.length === 0){
+      throw new Error("EmptySortedList");
+    } else {
+      return this.items.reduce((total, amount)=> total + amount)/this.length;
+    }
+  }
+
+  sum() {
+    if (this.length === 0){
+      return 0
+    } else {
+    return this.items.reduce((total, amount)=> total + amount)
+    }
+  }
 };
 
 module.exports = SortedList;
