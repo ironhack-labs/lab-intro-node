38ed1f69fdcb5581d452fb3c648767422853fcf0
Jorge <jlegazpegutierrez@gmail.com>
2019-01-28 22:04:54 +0100

    Resuelto

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,39 @@
 class SortedList {
-  constructor() {}
-  add(item) {}
-  get(pos) {}
-  max() {}
-  min() {}
-  average() {}
-  sum() {}
-};
-
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a, b) => {
+      return a - b;
+    });
+    this.length = this.items.length;
+  }
+  get(pos) {
+    return this.items[pos - 1];
+  }
+  max() {
+    if (this.items.length === 0) throw new Error("EmptySortedList");
+    return Math.max(...this.items);
+  }
+  min() {
+    if (this.items.length === 0) throw new Error("EmptySortedList");
+    return Math.min(...this.items);
+  }
+  average() {
+    if (this.items.length === 0) throw new Error("EmptySortedList");
+    return (
+      this.items.reduce(function(a, b) {
+        return a + b;
+      }) / this.items.length
+    );
+  }
+  sum() {
+    if (this.items.length === 0) return 0;
+    return this.items.reduce(function(a, b) {
+      return a + b;
+    });
+  }
+}
 module.exports = SortedList;
