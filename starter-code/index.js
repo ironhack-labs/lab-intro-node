c3aa1b7511743ee535b27ab1325bb464b9cd94e2
dreazz <agustinsilveira.96@gmail.com>
2019-01-28 21:37:04 +0100

    refactored code

--- starter-code/index.js
+++ starter-code/index.js
@@ -3,24 +3,29 @@ class SortedList {
     this.length = 0;
     this.items = [];
   }
+
   add(item) {
     this.items.push(item);
-    this.items.sort((a, b)=>{ return a - b })
+    this.items.sort((a, b) => {
+      return a - b;
+    });
     this.length++;
-    
   }
+
   get(pos) {
     if (pos > this.length) {
       return new Error("OutOfBounds");
     }
     return this.items[pos - 1];
   }
+
   max() {
     if (this.length === 0) {
       throw new Error("EmptySortedList");
     }
     return Math.max.apply(Math, this.items);
   }
+
   min() {
     if (this.length === 0) {
       throw new Error("EmptySortedList");
@@ -32,23 +37,21 @@ class SortedList {
     if (this.length === 0) {
       throw new Error("EmptySortedList");
     }
-    return this.items.reduce((total,currentNum)=>{
-         return total + currentNum
-         
-    })/ this.length
-    
-    
-    
+    return (
+      this.items.reduce((total, currentNum) => {
+        return total + currentNum;
+      }) / this.length
+    );
   }
+
   sum() {
     if (this.length === 0) {
       return 0;
     }
-    
-    return this.items.reduce((total,currentNum)=>{
-      return (total + currentNum);
 
-  })
+    return this.items.reduce((total, currentNum) => {
+      return total + currentNum;
+    });
   }
 }
 
