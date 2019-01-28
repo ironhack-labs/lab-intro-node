110cdcfc68c5718bbf9d2583dfec5077cc532228
jsanz24 <jesus240492@gmail.com>
2019-01-28 21:27:12 +0100

    Completo

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,34 @@
 class SortedList {
-  constructor() {}
-  add(item) {}
-  get(pos) {}
-  max() {}
-  min() {}
-  average() {}
-  sum() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
+  add(item) {
+    this.items.push(item);
+    this.length = this.items.length;
+    this.items.sort(function(a,b){
+      return a-b;
+    });
+  }
+  get(pos){
+    if(pos <= this.length) return this.items[pos-1];
+  }
+  max() {
+    if(this.length > 0) return Math.max.apply(null,this.items)
+    throw new Error("EmptySortedList")
+  }
+  min() {
+    if(this.length > 0) return Math.min.apply(null,this.items)
+    throw new Error("EmptySortedList")
+  }
+  average(){
+    if(this.length > 0) return this.sum()/this.length
+    throw new Error("EmptySortedList")
+  }
+  sum() {
+    return((this.length > 0) ? this.items.reduce((a,b) => {return a+b}) :  0)
+    //El asha me obligó a hacerlo con condicional ternario :(
+  }
 };
 
 module.exports = SortedList;
