3fa256dd19ad7c93d1ad871e3504d23e1a42c1f2
Nicolas Lacaze <nicolas.lacaze@gmail.com>
2019-01-28 20:17:21 +0100

    Complete exercise

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,59 @@
 class SortedList {
-  constructor() {}
-  add(item) {}
-  get(pos) {}
-  max() {}
-  min() {}
-  average() {}
-  sum() {}
+  constructor() {
+    this.items = [];
+    this.length = 0;
+  }
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a, b) => {
+      return a - b;
+    });
+    this.length++;
+  }
+  get(pos) {
+    
+    if (pos > this.items.length) {
+      throw new Error('OutOfBounds');
+    }
+    return this.items[pos - 1];
+    
+  }
+  max() {
+    if (this.length <= 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      return this.items.reduce((acc, current) => {
+        return Math.max(acc, current);
+      });
+    }
+  }
+  min() {
+    if (this.length <= 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      return this.items.reduce((acc, current) => {
+        return Math.min(acc, current);
+      });
+    }
+  }
+
+  average() {
+    if (this.length <= 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      return this.items.reduce((acc, current) => acc + current) / this.length;
+    }
+
+  }
+
+  sum() {
+    if (this.length <= 0) {
+      return 0;
+    } else {
+      return this.items.reduce((acc, current) => acc + current);
+    }
+  }
 };
 
 module.exports = SortedList;
+
