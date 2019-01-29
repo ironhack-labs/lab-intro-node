ab305b5dfb3d9bcca9ba1c1eb26293c3842bd104
Nira <niralonso@gmail.com>
2019-01-29 08:36:57 +0100

    .

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,54 @@
+
 class SortedList {
-  constructor() {}
-  add(item) {}
-  get(pos) {}
-  max() {}
-  min() {}
-  average() {}
-  sum() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
+  add(item) {
+     this.items.push(item);
+     this.length = this.items.length;
+     this.items.sort((a,b) => a-b);
+  }
+  get(pos) {
+     
+    return this.items[pos-1]
+
+  }
+  max() {
+    if(this.items.length === 0){
+      throw new Error("EmptySortedList")
+    }else{
+      return this.items[this.items.length-1]
+
+    }
+  }
+  min() {
+     if(this.items.length === 0){
+      throw new Error("EmptySortedList")
+    }else{
+      return this.items[0]
+
+    }
+  }
+  average() {
+    if(this.items.length === 0){
+      throw new Error("EmptySortedList")
+         }else{
+           return (this.items.reduce((ac,current) => ac + current))/this.items.length;
+       
+         }
+    
+
+  }
+  sum() {
+    if(this.items.length === 0){
+ return 0;
+    }else{
+      return this.items.reduce((ac,current) => ac + current)
+  
+    }
+    
+  }
 };
 
 module.exports = SortedList;
