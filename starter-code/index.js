94cbca8f1748faf95dae19ff4a6e4fb77775ea07
Dani Garcia <danielgarciamartinez1985@gmail.com>
2019-01-29 09:36:26 +0100

    Done

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,48 @@
 class SortedList {
-  constructor() {}
-  add(item) {}
-  get(pos) {}
-  max() {}
-  min() {}
-  average() {}
-  sum() {}
-};
+  constructor () {
+    this.items = [];
+    this.length = this.items.length;
+  }
+  add (item) {
+    this.items.push (item);
+    this.items.sort (function (a, b) {
+      return a - b;
+    });
+    this.length = this.items.length;
+  }
+  get (pos) {
+    return this.items[pos - 1];
+  }
+  max () {
+    if (this.items.length > 0) {
+      return Math.max.apply (null, this.items);
+    } else {
+      throw new Error ('EmptySortedList');
+    }
+  }
+  min () {
+    if (this.items.length > 0) {
+      return Math.min.apply (null, this.items);
+    } else {
+      throw new Error ('EmptySortedList');
+    }
+  }
+  average () {
+    if (this.items.length > 0) {
+      return this.sum () / this.items.length;
+    } else {
+      throw new Error ('EmptySortedList');
+    }
+  }
+  sum () {
+    if (this.items.length > 0) {
+      return this.items.reduce (function (a, b) {
+        return a + b;
+      });
+    } else {
+      return 0;
+    }
+  }
+}
 
 module.exports = SortedList;
