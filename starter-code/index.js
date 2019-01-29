7fe607b88644db857e015e5532b0a192da321f69
Ismael Jaouhar <ismael.jaouhar@gmail.com>
2019-01-29 09:05:41 +0100

    exercise

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,8 +1,25 @@
 class SortedList {
-  constructor() {}
-  add(item) {}
-  get(pos) {}
-  max() {}
+  constructor(items, length) {
+    this.items = [];
+    this.length = 0;
+  }
+  add(x) {
+    this.items.push(x);
+    this.items.sort((a,b) => a-b);
+    this.length++;
+  }
+  get(pos) {
+    return this.items[(pos - 1)];
+  }
+  max() {
+ 
+    if (this.length <= 0) {
+      throw new Error("EmptySortedList");
+    } else{
+      return this.items[this.items.length-1];
+    }
+ 
+  }
   min() {}
   average() {}
   sum() {}
