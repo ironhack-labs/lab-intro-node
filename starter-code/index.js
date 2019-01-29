4b75e8bb04b34c6a8d54900fe12b70ef7d795555
Samuel <bouaroua.samuel@gmail.com>
2019-01-29 07:57:55 +0100

    paris - christina et sam

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,50 @@
 class SortedList {
-  constructor() {}
-  add(item) {}
-  get(pos) {}
-  max() {}
-  min() {}
-  average() {}
-  sum() {}
-};
+	constructor() {
+		this.items = [];
+		this.length = this.items.length;
+	}
+
+	add(x) {
+		this.items.push(x);
+		this.items.sort((a, b) => a - b);
+		this.length = this.items.length;
+	}
+	get(pos) {
+		return this.items[pos - 1];
+	}
+	max() {
+		if (this.items.length === 0) {
+			throw new Error("EmptySortedList");
+		}
+
+		return Math.max(...this.items);
+	}
+	min() {
+		if (this.items.length === 0) {
+			throw new Error("EmptySortedList");
+		}
+
+		return Math.min(...this.items);
+	}
+
+	sum() {
+		if (this.items.length === 0) {
+			return 0;
+		} else {
+			return this.items.reduce((sum, current) => sum + current);
+		}
+	}
+
+	average() {
+		if (this.items.length === 0) {
+			throw new Error("EmptySortedList");
+		}
+
+		return this.sum() / this.items.length;
+	}
+}
 
 module.exports = SortedList;
+
+// COURSELINK sort one liners
+// https://stackoverflow.com/questions/979256/sorting-an-array-of-javascript-objects
