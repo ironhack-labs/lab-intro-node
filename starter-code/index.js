2b03989637f8a8d1d22f6bf7818d1b39e8e43fdf
Zenchy <alvaro.corral.soilan@gmail.com>
2019-01-29 08:47:42 +0100

    Done

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,41 @@
 class SortedList {
-  constructor() {}
-  add(item) {}
-  get(pos) {}
-  max() {}
-  min() {}
-  average() {}
-  sum() {}
+  constructor() 
+  {
+    this.items=[];
+    this.length=this.items.length;
+  }
+
+  add(item) 
+  {this.items.push(item)
+  this.length=this.items.length
+  this.items.sort((a,b)=>a-b)
+}
+
+  get(pos) 
+  {return this.items[pos-1]}
+
+  max() {
+    if(!this.items.length) throw new Error("EmptySortedList")
+    return this.items[this.items.length-1]
+  }
+  min() {
+    if(!this.items.length) throw new Error("EmptySortedList")
+    return this.items[0]
+  }
+  average() { 
+    if(!this.items.length) throw new Error("EmptySortedList")
+    
+    return this.items.reduce((a, b) => a + b)/this.items.length
+
+  }
+  sum() {
+    
+    if(!this.items.length) return 0
+    return this.items.reduce((a, b) => a + b)
+  }
 };
 
+
+
 module.exports = SortedList;
+
