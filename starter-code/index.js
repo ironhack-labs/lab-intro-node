35c5baaf0db2575a514850c266f14585d7ba4637
Patricia Martin <patricia_martin_4@hotmail.com>
2019-01-29 09:13:41 +0100

    First approach to Node

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,32 @@
-class SortedList {
-  constructor() {}
-  add(item) {}
-  get(pos) {}
-  max() {}
-  min() {}
-  average() {}
-  sum() {}
+class SortedList { 
+  constructor() {
+    this.items = [],
+    this.length = 0
+  }
+  add(item) {
+    this.items.push(item);
+    this.length = this.items.length;
+    
+  }
+  get(pos) {
+  if (pos <= this.length) return this.items[pos-1];
+
+  }
+  max() {
+
+  }
+  min() {
+
+  }
+  average() {
+
+  }
+  sum() {
+     
+  }
 };
 
 module.exports = SortedList;
+
+
+
