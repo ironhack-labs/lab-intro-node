56de01ea43b0a326ee0013886825a66c6130db3e
Jules Faivre <faivre.jules@gmail.com>
2019-01-29 09:46:42 +0100

    Tried

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,46 @@
 class SortedList {
-  constructor() {}
-  add(item) {}
-  get(pos) {}
-  max() {}
-  min() {}
-  average() {}
-  sum() {}
-};
+  constructor() {
+    this.items = [];
+    this.length = 0;
+  }
+  add(item) {
+    this.items.push(item);
+    this.items.sort();
+    this.length += 1;
+  }
+  get(pos) {
+    if (this.length === 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      return this.item[this.length - 1];
+    }
+  }
+  max() {
+    if (this.length === 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      return Math.max(this.items);
+    }
+  }
+  min() {
+    if (this.length === 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      return Math.min(this.items);
+    }
+  }
+  average() {
+    this.items.sum / this.length;
+  }
+  sum() {
+    this.items.reduce(function(acc, curr) {
+      return acc + curr;
+    }, 0);
+  }
+}
+
+let firstSortedList = new SortedList();
+
+console.log(firstSortedList);
 
 module.exports = SortedList;
