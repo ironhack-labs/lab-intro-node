b1add4040593aa86f4cc2fa0ce25606bf2334841
Mr.X <seniorito.x@icloud.com>
2019-01-29 09:35:58 +0100

    Done! ¯\_(ツ)_/¯

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,45 @@
 class SortedList {
-  constructor() {}
-  add(item) {}
-  get(pos) {}
-  max() {}
-  min() {}
-  average() {}
-  sum() {}
+  constructor() {
+    this.items = []
+    this.length = this.items.length
+  }
+  add(x) {
+    this.items.push(x)
+    this.items.sort((a, b) => {return a - b})
+    this.length = this.items.length
+  }
+  get(pos) {
+    return this.items[pos - 1]
+    //return this.items.indexOf(pos)
+  }
+  max() {
+    if (this.length > 0) return this.items[this.length - 1]
+    
+    throw new Error("EmptySortedList")
+  }
+  min() {
+    if (this.length > 0) return Math.min.apply(null, this.items)
+
+    throw new Error("EmptySortedList")
+  }
+  
+  sum() {
+    if (this.length > 0){
+      return this.items.reduce (function(acumulator , item){
+        return acumulator + item
+      })
+    }
+    return 0
+  }
+
+  average() {
+    if (this.length > 0){
+      return (this.items.reduce (function(acumulator , item){
+        return acumulator + item
+      }) / this.length)
+    }
+    throw new Error("EmptySortedList")
+  }
 };
 
 module.exports = SortedList;
