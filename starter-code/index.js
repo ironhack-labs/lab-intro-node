82639b07d38885cce7ab0883be61a1c46ed65965
ChristopheP <protin.christophe@gmail.com>
2019-02-07 06:41:35 +0100

    PTBCN1118-Christophe

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,40 @@
 class SortedList {
-  constructor() {}
-  add(item) {}
-  get(pos) {}
-  max() {}
-  min() {}
-  average() {}
-  sum() {}
+  constructor() {
+    this.items = [];
+    this.length =0;
+  }
+  add(x) {
+    this.items.push(x);
+    this.items.sort((a, b) =>  a-b);
+    this.length ++;
+  }
+  get(pos) {
+    return this.items[pos-1];
+  }
+  max() {
+    if (this.length === 0) {
+      throw new Error("EmptySortedList")
+    }
+    else {return Math.max(...this.items)}
+  }
+  min() {
+    if (this.length === 0) {
+      throw new Error("EmptySortedList")
+    }
+    else {return Math.min(...this.items)}
+  }
+  average() {
+    if (this.length === 0) {
+      throw new Error("EmptySortedList")
+    }
+    else {return this.items.reduce((accum, current) => accum + current)/this.length}
+  }
+  sum() {
+    if (this.length === 0) {
+      return 0;
+    }
+    else {return this.items.reduce((accum, current) => accum + current)}
+  }
 };
 
 module.exports = SortedList;
