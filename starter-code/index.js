79e57512a138dcf895413f480e055bf52110f5fc
Manuel Garaboa <mannygaraboa@gmail.com>
2019-02-11 12:02:27 -0500

    Node Intro Lab w/ Victoria Hickam

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,62 @@
-class SortedList {
-  constructor() {}
-  add(item) {}
-  get(pos) {}
-  max() {}
-  min() {}
-  average() {}
-  sum() {}
-};
+class SortedList 
+{
+  constructor() 
+  {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
+  add(x) 
+  {
+    this.items.push(x);
+    this.length = this.items.length;
+    this.items.sort(function(a, b){return a - b});
+  }
+
+  get(i) 
+  {
+    return this.items[i];
+  }
+
+  max() 
+  {
+    if(this.items.length == 0)
+    {
+      throw new Error("EmptySortedList")
+    }
+    else
+    {
+      return this.items[this.items.length - 1];
+    }
+  }
+
+  min() 
+  {
+    if(this.items.length == 0)
+    {
+      throw new Error("EmptySortedList")
+    }
+    else
+    {
+      return this.items[0];
+    }
+  }
+
+  average() 
+  {
+    if(this.items.length == 0)
+    {
+      throw new Error("EmptySortedList")
+    }
+    else
+    {
+      return this.items.reduce((a, b) => a + b, 0) / this.items.length;
+    }
+  }
+
+  sum() 
+  {
+    return this.items.reduce((a, b) => a + b, 0);
+  }
+};
 module.exports = SortedList;
