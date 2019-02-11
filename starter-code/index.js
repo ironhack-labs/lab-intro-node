df08943d7393a2e8aaa2c844d970b7763d19b9ab
Victoria Hickam <vhickam@gmail.com>
2019-02-11 12:01:53 -0500

    done

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,45 @@
 class SortedList {
-  constructor() {}
-  add(item) {}
-  get(pos) {}
-  max() {}
-  min() {}
-  average() {}
-  sum() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
+  add(x) {
+    this.items.push(x);
+    this.items.sort((a,b)=> (a-b));
+    this.length = this.items.length;
+  }
+  get(i) {
+    //this.items.sort();
+   return this.items[i];
+  
+  }
+  max() {
+    if(this.items.length == 0){
+      throw new Error("EmptySortedList");
+    }
+    else{
+      return this.items[this.items.length-1];
+    }
+  }
+  min() {
+    if(this.items.length == 0){
+      throw new Error("EmptySortedList");
+    }
+    else {
+      return this.items[0];
+    }
+  }
+  average() {
+    if(this.items.length == 0){
+      throw new Error("EmptySortedList");
+    }
+    else{
+      return (this.items.reduce((a, b) => a + b, 0))/this.items.length;
+    }
+  }
+  sum() {
+    return this.items.reduce((a, b) => a + b, 0);
+  }
 };
 
 module.exports = SortedList;
