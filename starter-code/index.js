02cc20615c22ef5d801ee157bd5c923357b768c0
baltasar torres garcia <baltasartorresgarcia@MacBook-Pro-de-amelia.local>
2019-02-25 20:06:33 +0100

    done

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,54 @@
 class SortedList {
-  constructor() {}
-  add(item) {}
-  get(pos) {}
-  max() {}
-  min() {}
-  average() {}
-  sum() {}
-};
+  constructor() {
+    this.items = []
+    this.length = this.items.length
+  }
+  
+  add(item) {
+  this.items.push(item)
+  this.length = this.items.length
+  this.items.sort(function(a,b){
+    return a - b
+  })
+  }
+  get(pos) {
+    return this.items[pos -1]
+  }
+  
+  max() {
+    if (this.length === 0) {
+      throw new Error("EmptySortedList")
+    }
 
+    let ult = this.items[this.items.length - 1]
+    return ult
+
+  }
+
+  
+  min() {
+    if (this.length === 0) {
+      throw new Error("EmptySortedList")
+    }
+    
+    return this.items[0]
+  }
+  
+  average() {
+    if (this.length === 0) {
+      throw new Error("EmptySortedList")
+    }
+    return this.sum() / this.items.length
+  }
+  
+  sum() {
+if(this.length === 0){
+  return 0
+  }
+return this.items.reduce(function(a,b){
+return a + b
+})
+  }
+};
+new SortedList
 module.exports = SortedList;
