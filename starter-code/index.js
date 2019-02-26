9b4f43a5cecd9ed4015ef492be00e9b14ba2dc52
Abel Colmenares Napolitano <abelpronet@gmail.com>
2019-02-26 08:32:31 +0100

    done

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,43 @@
 class SortedList {
-  constructor() {}
-  add(item) {}
-  get(pos) {}
-  max() {}
-  min() {}
-  average() {}
-  sum() {}
+    constructor(items, length) {
+        this.items = [];
+        this.length = this.items.length;
+    }
+    add(item) {
+        this.items.push(item);
+        this.length = this.items.length;
+        this.items.sort((a, b) => a - b)
+    }
+    get(pos) {
+        return this.items[pos - 1]
+    }
+    max() {
+        if (this.items[this.items.length - 1]) return this.items[this.items.length - 1]
+        else throw new Error("EmptySortedList")
+    }
+    min() {
+        if (this.items.length === 0) {
+            throw new Error("EmptySortedList");
+        } else {
+            return this.items[0];
+        }
+    }
+    average() {
+
+        if (this.items.length === 0) {
+            throw new Error("EmptySortedList");
+        } else {
+            return this.sum() / this.items.length;
+        }
+
+    }
+    sum() {
+        if (this.items.length === 0) {
+            return 0;
+        } else {
+            return this.items.reduce((accumulator, currenValue) => accumulator + currenValue)
+        }
+    }
 };
 
 module.exports = SortedList;
