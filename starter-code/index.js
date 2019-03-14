bbf0306c299f9dda9888b446a91f478f19d423b1
Fedde <feddetilma@hotmail.com>
2019-03-14 20:55:15 +0100

    complete

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,58 @@
 class SortedList {
-  constructor() {}
-  add(item) {}
-  get(pos) {}
-  max() {}
-  min() {}
-  average() {}
-  sum() {}
+  constructor() {
+    this.items = [];
+    //this.length = this.items.length; // <-- will set a static length (0)
+  }
+  get length() { // this will add an option to do test.length which will equal this.items.length
+    return this.items.length;
+  }
+  add(x) {
+    this.items.push(x);
+    this.sort();
+  }
+  get(pos) {
+    return this.items[pos];
+  }
+  sort() {
+    this.items.sort((a, b) => { return a - b });
+  }
+  max() {
+    if (this.length == 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      return Math.max(...this.items);
+    };
+  };
+  min() {
+    if (this.length == 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      return Math.min(...this.items);
+    }
+  }
+  average() {
+    if (this.length == 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      return this.sum() / this.length;
+    }
+  }
+  sum() {
+    if (this.length == 0) {
+      return 0;
+    } else {
+      return this.items.reduce((incr, total) => {
+        return total + incr;
+      });
+    }
+  };
 };
 
-module.exports = SortedList;
+var test = new SortedList;
+test.add(20);
+test.add(10);
+test.add(21);
+test.add(11);
+console.log(test);
+console.log(test.max());
+module.exports = SortedList; 
