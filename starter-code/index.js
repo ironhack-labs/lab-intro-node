1b3bd3961bc993bf9451e4a75c48526b6fd093f3
amperry <alexp3y@gmail.com>
2019-03-14 20:42:45 +0100

    Done

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,47 @@
 class SortedList {
-  constructor() {}
-  add(item) {}
-  get(pos) {}
-  max() {}
-  min() {}
-  average() {}
-  sum() {}
+  constructor() {
+    this.items = [],
+    this.length = this.items.length;
+  }
+
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a, b) => a - b);
+    this.length++;
+  }
+  
+  get(pos) {
+    pos--;
+    return this.items[pos];
+  }
+  
+  max() {
+    if (this.length == 0) {
+      throw new Error('EmptySortedList');
+    } else {
+      return this.items[this.length - 1];
+    }
+  }
+  
+  min() {
+    if (this.length == 0) {
+      throw new Error('EmptySortedList');
+    } else {
+      return this.items[0];
+    }
+  }
+  
+  average() {
+    if (this.length == 0) {
+      throw new Error('EmptySortedList');
+    } else {
+      return this.sum() / this.length;
+    }
+  }
+  
+  sum() {
+    return (this.length == 0) ? 0 : this.items.reduce((acc, val) => acc + val);
+  }
 };
 
 module.exports = SortedList;
