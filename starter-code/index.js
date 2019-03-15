bddc1af482e7c550b6866abd4627115474df7b2a
Mariana López <marianaglp15@gmail.com>
2019-03-15 01:22:08 -0600

    almost

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,40 @@
 class SortedList {
-  constructor() {}
-  add(item) {}
-  get(pos) {}
-  max() {}
-  min() {}
-  average() {}
-  sum() {}
+  constructor() {
+    this.items = [];
+    this.length = 0;
+  }
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a, b) => {
+      return a - b;
+    });
+    this.length++;
+  }
+  get(pos) {
+    return this.items[pos - 1];
+  }
+  max() {
+    if (this.items.length === 0) {
+      throw new Error("EmptySortedList");
+    }
+    return this.items[this.items.length - 1];
+  }
+  min() {
+    if (this.length === 0){
+      throw new Error("EmptySortedList")
+    }
+    return this.items[0];
+  }
+  average() {
+    if(this.length === 0) {
+      throw new Error ("EmptySortedList");
+    }
+    return (this.sum() / this.length);
+  }
+  sum() {
+    if ((this.length == 0)) return 0;
+    return this.items.reduce((suma, num) => suma + num, 0);
+  }
 };
 
 module.exports = SortedList;
