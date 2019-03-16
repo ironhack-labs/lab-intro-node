429067917b58a79860a8d8083294028018b9f798
Merle Jansen <merlevanjansen@gmail.com>
2019-03-16 23:10:00 +0100

    upload

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,66 @@
-class SortedList {
-  constructor() {}
-  add(item) {}
-  get(pos) {}
-  max() {}
-  min() {}
-  average() {}
-  sum() {}
+class SortedList  {
+  constructor() {
+    this.items = []; 
+  }
+
+  get length() {
+    return this.items.length
+  }
+
+  add(item) { 
+    return this.items.push(item)
+   }
+
+  get(pos) {
+   return  this.items[pos-1]
+  }
+
+  max() {
+    try {
+      return Math.max(...this.items);
+    }
+    catch(error) {
+      throw new Error("EmptySortedList");
+    }
+  }
+
+  min() {
+    try {
+      return Math.min(...this.items);
+    }
+    catch(error) {
+      throw new Error("EmptySortedList");
+    }
+  }
+  
+  
+  
+  average() {
+   try {
+     reurn (sum()/arguments.length)
+   }
+   catch(error) {
+    throw new Error("EmptySortedList");
+   }
+  }
+
+
+  sum() {
+    if(arguments.length >0){
+      let sum = 0;
+      for(let i = 0; i < arguments.length; i++) {
+        sum += arguments[i];
+      }
+      return sum;
+    } else {
+    return 0
+    }
+
+ 
+    
 };
 
+
+
+}
 module.exports = SortedList;
