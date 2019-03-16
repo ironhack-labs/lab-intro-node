b91520ecaf9c99eb1fa386a4a48b5c1051d6b83e
Jelena Jukic <jelena.dj.pg@gmail.com>
2019-03-16 11:47:52 +0100

    done

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,73 @@
 class SortedList {
-  constructor() {}
-  add(item) {}
-  get(pos) {}
-  max() {}
-  min() {}
-  average() {}
-  sum() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
+  add(item) {
+    this.items.push(item);
+    this.length++;
+    this.items.sort((a,b)=>a-b);
+  }
+
+  get(pos) {
+    
+    if (this.length >pos) {
+        
+        return this.items[pos];
+    }else {
+      throw new Error ("OutOfBounds");
+    }
+
+  }
+  max() {
+
+    if (this.length == 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      let max = Math.max(...this.items);
+      return max;
+    }
+
+  }
+  min() {
+
+    if (this.length == 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      return Math.min(...this.items);
+    }
+
+    // if (this.length == 0) throw new Error("EmptySortedList");
+
+
+    // return Math.min(...this.items);
+  }
+
+
+  average() {
+    let avr;
+    if (this.sum() == 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      avr = this.sum() / this.length;
+      return avr;
+    }
+  }
+
+
+  sum() {
+    let sum;
+    if (this.length == 0) {
+      sum = 0;
+    } else {
+      const reducer = (accumulator, currentValue) => accumulator + currentValue;
+      sum = this.items.reduce(reducer);
+    }
+
+    return sum;
+  }
+
+  
 };
 
 module.exports = SortedList;
