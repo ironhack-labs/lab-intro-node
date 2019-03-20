1b849db97b87a98e27ba5c8462f4b6fcb7796430
RedBalloon24 <worldtraveler757@gmail.com>
2019-03-20 21:25:56 +0100

    1st commit

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,26 @@
 class SortedList {
-  constructor() {}
-  add(item) {}
-  get(pos) {}
-  max() {}
-  min() {}
-  average() {}
-  sum() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
+  add(item) {
+    this.items.push(item);
+  }
+  get(pos) {
+
+  }
+  max() {
+
+  }
+  min() {
+
+  }
+  average() {
+    
+  }
+  sum() {
+
+  }
 };
 
 module.exports = SortedList;
