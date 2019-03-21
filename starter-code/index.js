a10413fbbdf7b87c2089e2cb05e034952cd67935
Antonio Méndez <antonio.mendez.martin89@gmail.com>
2019-03-21 21:30:44 +0100

    finish

--- starter-code/index.js
+++ starter-code/index.js
@@ -5,13 +5,40 @@ class SortedList {
   }
   add(item) {
     this.item.push(item);
+    this.item.sort ((a,b) => a - b);
     this.length = this.item.length
   }
-  get(pos) {}
-  max() {}
-  min() {}
-  average() {}
-  sum() {}
+  get(pos) {
+    return this.item[pos]
+  }
+  max() {
+    if (this.item.length === 0) {
+      throw new Error("EmptySortedList")
+    } else {
+      return Math.max(...this.item)
+    }
+  }
+  min() {
+    if (this.item.length === 0) {
+      throw new Error("EmptySortedList")
+    } else {
+      return Math.min(...this.item)
+    }    
+  }
+  average() {
+    if (this.item.length === 0) {
+      throw new Error("EmptySortedList")
+    } else {
+      return this.item.reduce((acc, it) => (acc+=it), 0)/this.item.length
+    }    
+  }
+  sum() {
+    if (this.item.length === 0) {
+      return 0
+    } else {
+      return this.item.reduce((acc, it) => acc+=it, 0)
+    }    
+  }
 };
 
 module.exports = SortedList;
