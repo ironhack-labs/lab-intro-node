7afb84dd0078c9782944407a45c23e2d98030448
Javi L <jlopezro@gmail.com>
2019-03-21 09:05:51 +0100

    first iteration done

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,6 +1,17 @@
 class SortedList {
-  constructor() {}
-  add(item) {}
+  constructor() {
+    this.items = [],
+    this.length = this.items.length
+  };
+
+  //The add(x) method will add x to the items array.
+
+  add(item) {
+    // item = 'x';
+    // this.items.push(item);
+    let items = [item];
+  }
+  
   get(pos) {}
   max() {}
   min() {}
