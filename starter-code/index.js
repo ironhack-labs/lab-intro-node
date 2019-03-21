bc2e5f65bf931640f0bf63d5ba68a92fddb627f4
David Gallego Gomez <davizs@hotmail.com>
2019-03-21 19:43:33 +0100

    Ejercicio terminado

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,43 @@
 class SortedList {
-  constructor() {}
-  add(item) {}
-  get(pos) {}
-  max() {}
-  min() {}
-  average() {}
-  sum() {}
+  constructor() {
+    this.items = [];
+    this.length = 0;
+  }
+  
+  add(item) {
+    this.items.push(item);
+    this.items.sort(function(a, b) { return a - b; });
+    this.length++;
+  }
+  get(pos) {
+    if (pos < this.length) return this.items[pos];
+  }
+  max() {
+    if (this.length === 0) { throw new Error('EmptySortedList'); }
+    else {
+      return Math.max(...this.items);
+    }
+  }
+  min() {
+    if (this.length === 0) { throw new Error('EmptySortedList'); }
+    else {
+      return Math.min(...this.items);
+    }
+  }
+  average() {
+    if (this.length === 0) { throw new Error('EmptySortedList'); }
+    else {
+      return ( this.sum() / this.length );
+    }
+  }
+  sum() {
+    if (this.length === 0) { return 0; }
+    else {
+      return this.items.reduce(function(valorAnterior, valorActual){
+        return valorAnterior + valorActual;
+      });
+    }
+  }
 };
 
 module.exports = SortedList;
