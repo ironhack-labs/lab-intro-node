fc5e5c36b494d7765cb6665f2f6c4b5f3696222a
Patricia Delgado <patricia.delgado.morcillo@gmail.com>
2019-03-21 21:27:37 +0100

    sum function fixed

--- starter-code/index.js
+++ starter-code/index.js
@@ -5,14 +5,42 @@ class SortedList {
   }
   add(item) {
     this.items.push(item);
+    this.items.sort((a , b) => a-b);
     this.length = this.items.length;
   }
   get(pos) {
     return this.items[pos];
+
   }
-  max() {}
-  min() {}
+  max() {
+    if(this.items.length === 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      return Math.max(...this.items);
+    }
+  }
+  min() {
+    if(this.items.length === 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      return Math.min(...this.items);
+    }
+  }
   average() {}
-  sum() {}
+
+  sum() {
+    if(this.items.length === 0){
+      return 0;
+    } else {
+     return this.items.reduce((acc, a) => acc + a);
+    } 
+  }
+
+  //this.items.reduce(add);
+/* const sum = [1, 2, 3].reduce(add);
+
+function add(accumulator, a) {
+    return accumulator + a;
+} */
 };
 module.exports = SortedList;
