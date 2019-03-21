ed1f9c19ef89cd26ab303d4e704e9c04014ef645
Fernando Lacaba <fernando.lacaba.herrera@gmail.com>
2019-03-21 17:46:00 +0100

    Done

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,51 @@
 class SortedList {
-  constructor() {}
-  add(item) {}
-  get(pos) {}
-  max() {}
-  min() {}
-  average() {}
-  sum() {}
+  constructor() {
+    this.items = []
+    this.length = 0
+  }
+
+  add(item) {
+    this.items.push(item)
+    this.items.sort((a,b)=> a-b)
+    this.length++
+  }
+
+  get(pos) {
+    pos--;
+    if (this.length === 0) {
+      throw new Error('OutOfBounds')
+    }
+     
+    return this.items[pos]
+  }
+
+  max() {
+    if (this.length === 0) {
+      throw new Error('EmptySortedList')
+    }
+     
+    return Math.max(...this.items)
+  }
+
+  min() {
+    if (this.length === 0) {
+      throw new Error('EmptySortedList')
+    }
+     
+    return Math.min(...this.items)
+  }
+
+  average() {
+    if (this.length === 0) {
+      throw new Error('EmptySortedList')
+    }
+     
+    return this.sum()/this.length
+  }
+
+  sum() { 
+    return this.items.reduce((a,b)=>a+b,0)
+  }
 };
 
-module.exports = SortedList;
+const a = new SortedList();
