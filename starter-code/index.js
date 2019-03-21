83e2f884a4886f463d875ba9d1c96a15f5324ea8
Javi L <jlopezro@gmail.com>
2019-03-21 20:30:23 +0100

    add done

--- starter-code/index.js
+++ starter-code/index.js
@@ -6,11 +6,12 @@ class SortedList {
 
   //The add(x) method will add x to the items array.
 
-  add(item) {
-    // item = 'x';
-    // this.items.push(item);
-    let items = [item];
-  }
+add(item) {
+
+this.items.push(item);
+this.items.sort((a,b)=> a-b);
+this.length = this.items.length;
+  };
   
   get(pos) {}
   max() {}
