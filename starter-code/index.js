cc0ad5062ec1a5e831722f72374338a2d08e091a
Javi L <jlopezro@gmail.com>
2019-03-22 23:34:23 +0100

    done

--- starter-code/index.js
+++ starter-code/index.js
@@ -4,20 +4,51 @@ class SortedList {
     this.length = this.items.length
   };
 
-  //The add(x) method will add x to the items array.
-
 add(item) {
 
 this.items.push(item);
 this.items.sort((a,b)=> a-b);
 this.length = this.items.length;
+
   };
   
-  get(pos) {}
-  max() {}
-  min() {}
-  average() {}
-  sum() {}
+  get(pos) {
+    if (this.items.length === 0) {
+      throw new Error("OutOfBounds");
+    }
+           return this.items[pos];
+      }
+      
+  max() {
+    if (this.items.length === 0) {
+      throw new Error("EmptySortedList") 
+    }
+  return Math.max(...this.items);
+
+  }
+
+  min() {
+    if (this.items.length === 0) {
+      throw new Error("EmptySortedList");
+    }
+    return this.min = Math.min(...this.items);
+  }
+
+  average() {
+    if (this.items.length === 0) {
+      throw new Error("EmptySortedList");
+    }
+    return this.items.reduce((a, b) => a + b) / this.items.length;  
+  }
+  
+  sum() {
+    if (this.items.length === 0){
+      return 0;
+    } else {
+   return  this.sum = this.items.reduce((a, b) => a + b, 0);
+    }
+  }
+  
 };
 
 module.exports = SortedList;
