f0e8b514b6b41e8c5c688941b525c6a53cdd3a91
Santiago Iglesias <iglesias.arozena@gmail.com>
2019-03-24 14:55:50 +0100

    constructor, add, get

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,7 +1,39 @@
 class SortedList {
-  constructor() {}
-  add(item) {}
-  get(pos) {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
+  add(item) {
+    this.item = item;
+    if (this.items === []) {
+      this.items.push(item);
+    }
+    else {
+      if (this.items[this.length - 1] > this.item) {
+        this.items.sort((a, b) => a - b)
+      }
+      else {
+        this.items.push(item);
+      }
+    }
+    this.length++;
+  }
+  get(pos) {
+    pos = this.items.indexOf(this.item);
+    this.pos = pos;
+    
+    
+    return this.items[this.pos];
+
+     
+  //let pos = this.items.indexOf(item);
+  //this.pos = pos; 
+
+  //this.pos = this.length - 1;
+  //return this.items[this.pos]
+
+  //this.pos = this.items.indexOf(item)
+  }
   max() {}
   min() {}
   average() {}
