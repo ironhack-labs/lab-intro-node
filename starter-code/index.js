2a507babd87eaccd1faceae8d04ec309d83b0af5
Josue Valencia <josue.valencia@domo.ec>
2019-03-24 12:51:26 +0100

    complete

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,51 @@
 class SortedList {
-  constructor() {}
-  add(item) {}
-  get(pos) {}
-  max() {}
-  min() {}
-  average() {}
-  sum() {}
+  constructor() {
+    this.items = []
+    this.length = this.items.length
+  }
+  add(item) {
+    this.items = [...this.items, item]
+    this.items.sort((a,b)=> a-b)
+    this.length = this.items.length
+  }
+  get(pos) {
+    pos--    
+    try {
+      return this.items.length  ? this.items[pos] : (void 0).throwError()
+    }
+    catch(e) {
+      throw new Error('OutOfBounds')
+    }
+  }
+  max() {
+    try {
+      return this.items.length  ? Math.max(...this.items) : (void 0).throwError()
+    }
+    catch(e) {
+      throw new Error('EmptySortedList')
+    }
+  }
+  min() {
+    try {
+      return this.items.length  ? Math.min(...this.items) : (void 0).throwError()
+    }
+    catch(e) {
+      throw new Error('EmptySortedList')
+    }
+  }
+  average() {
+    try {
+      return   this.items.length
+             ? this.items.reduce((t, n)=> (t + n))/this.items.length
+             : (void 0).throwError()
+    }
+    catch(e) {
+      throw new Error('EmptySortedList')
+    }
+  }
+  sum() {
+    return this.items.length ? this.items.reduce((t, n)=> (t + n)) : 0
+  }
 };
 
 module.exports = SortedList;
