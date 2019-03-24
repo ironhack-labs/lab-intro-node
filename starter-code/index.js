716fc4cb385ee02fb53f7b4f28174d2d093de9ba
Santiago Iglesias <iglesias.arozena@gmail.com>
2019-03-24 15:19:36 +0100

    constructor, add, get OK

--- starter-code/index.js
+++ starter-code/index.js
@@ -5,28 +5,36 @@ class SortedList {
   }
   add(item) {
     this.item = item;
-    if (this.items === []) {
-      this.items.push(item);
-    }
-    else {
-      if (this.items[this.length - 1] > this.item) {
-        this.items.sort((a, b) => a - b)
-      }
-      else {
-        this.items.push(item);
-      }
-    }
+    this.items.push(item);
     this.length++;
   }
   get(pos) {
-    pos = this.items.indexOf(this.item);
-    this.pos = pos;
-    
-    
-    return this.items[this.pos];
-
-     
-  //let pos = this.items.indexOf(item);
+    this.pos = this.items.indexOf(pos);
+    return this.items[this.length -1];
+  
+//    
+//    add(item) {
+//      this.item = item;
+//      if (this.items === []) {
+//        this.items.push(item);
+//      }
+//      else {
+//        if (this.items[this.length - 1] > this.item) {
+//          this.items.sort((a, b) => a - b)
+//        }
+//        else {
+//          this.items.push(item);
+//        }
+//      }
+//      this.length++;
+//    }
+//    get(pos) {
+//      pos = this.items.indexOf(this.item);
+//      this.pos = pos;
+//      
+//      
+//      return this.items[this.pos];    
+//  //let pos = this.items.indexOf(item);
   //this.pos = pos; 
 
   //this.pos = this.length - 1;
