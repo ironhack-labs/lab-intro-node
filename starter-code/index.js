3085ae171204adc3e4396301c52ed64e4bdf4515
Victor Garcia <v.garcia.mestre@gmail.com>
2019-03-27 18:25:31 +0100

    Commit with 12 passing and 3 failing

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,47 @@
 class SortedList {
-  constructor() {}
-  add(item) {}
-  get(pos) {}
-  max() {}
-  min() {}
-  average() {}
-  sum() {}
-};
+  constructor(){
+    this.items = [];
+    this.length= this.items.length;
+  }
 
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a, b) => a - b);
+    this.length = this.items.length;
+  }
+  
+  get(pos) {
+    return this.items[pos];
+
+  }
+  max() {
+    if (this.items.length === 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      return Math.max(...this.items);
+    }
+  }
+  min() {
+    if (this.items.length === 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      return Math.min(...this.items);
+    }
+  }
+  sum() {
+    if (this.items.length === 0) {
+      return 0;
+    } else {
+      return this.items.reduce((acc, a) => acc + a);
+    }
+  }
+
+  average() {
+    if (this.items.length === 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      return this.items.reduce((acc, a) => acc + a) / this.items.length;
+    }
+  }
+};
 module.exports = SortedList;
