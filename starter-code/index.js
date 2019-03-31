0cebd36d675317190bc4e6726b16fba397672ff4
Wilkor Almeida <wilkor.almeida@globalhitss.com.br>
2019-03-31 14:31:51 -0300

    exercicio concluido

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,46 @@
 class SortedList {
-  constructor() {}
-  add(item) {}
-  get(pos) {}
-  max() {}
-  min() {}
-  average() {}
-  sum() {}
+  constructor(items, length) {
+    this.items = [];
+    this.length = this.items.length;
+  }
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a , b) => a-b);
+    this.length=this.items.length;
+  }
+  get(pos) {
+    return this.items[pos];
+  }
+  max() {
+    if(this.items.length===0) {
+      throw new Error("EmptySortedList");
+    } else {
+      return Math.max(...this.items);
+    }
+  }
+  min() {
+    if(this.items.length===0) {
+      throw new Error("EmptySortedList");
+    } else {
+      return Math.min(...this.items);
+    }
+
+  }
+  average() {
+    if(this.items.length===0) {
+      throw new Error("EmptySortedList");
+    } else {
+      return this.items.reduce((acu, it) => acu + it) / this.items.length;
+    }
+  }
+  sum() {
+    if(this.items.length===0){
+      return 0;
+    } else {
+     return this.items.reduce((acu, it) => acu + it);
+    } 
+  }
 };
 
 module.exports = SortedList;
+let sortedList = new SortedList();
