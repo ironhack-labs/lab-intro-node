c4d9977afbcfba2ce163187410e56d1950807f1e
Marcelo Oliveira <marbmo@gmail.com>
2019-04-01 11:03:16 -0300

    Exercício feito, deu um problema que se eu mudasse o código um teste passava e outro não, se eu mudava pra arrumar o que estava dando erro, o que estava certo originalmente começa a dar erro

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,57 @@
 class SortedList {
-  constructor() {}
-  add(item) {}
-  get(pos) {}
-  max() {}
-  min() {}
-  average() {}
-  sum() {}
-};
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
+
+  add(item) {
+    this.items.push(item);
+    this.length = this.items.length;
+    this.items.sort((a, b) => {
+      return a - b;
+    });
+  }
+
+  get(pos) {
+    return this.items[pos - 1];
+  }
+
+  max() {
+    if (this.length === 0) {
+      throw new Error('EmptySortedList');
+    }
+
+    const highestNumber = Math.max(...this.items);
+
+    return highestNumber;
+  }
+
+  min() {
+    if (this.length === 0) {
+      throw new Error('EmptySortedList');
+    }
+
+    const lowestNumber = Math.min(...this.items);
+
+    return lowestNumber;
+  }
+
+  average() {
+    if (this.length === 0) {
+      throw new Error('EmptySortedList');
+    }
+
+    return this.sum() / this.length;
+  }
+
+  sum() {
+    if (this.length === 0) {
+      return 0;
+    }
+
+    const sum = this.items.reduce((acc, cv) => acc + cv);
+    return sum;
+  }
+}
 
 module.exports = SortedList;
