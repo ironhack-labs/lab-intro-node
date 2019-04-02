9fbb1a14de44e0932ca6bd1ead5525abca0cf945
Diogo Pires Oliveira <diogopiresoliveira@Diogos-MacBook-Air.local>
2019-04-02 00:59:38 -0300

    completed lab

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,41 @@
 class SortedList {
-  constructor() {}
-  add(item) {}
-  get(pos) {}
-  max() {}
-  min() {}
-  average() {}
-  sum() {}
-};
+  constructor() {
+    this.items = [];
+    this.length = 0;
+  }
+
+  add(item) {
+    this.items.push(item);
+    // eslint-disable-next-line no-plusplus
+    this.length++;
+    this.items.sort((a, b) => a - b);
+  }
+
+  get(pos) {
+    return this.items[pos - 1];
+  }
+
+  max() {
+    if (this.length === 0) throw Error('EmptySortedList');
+    return this.get(this.length);
+  }
+
+  min() {
+    if (this.length === 0) throw Error('EmptySortedList');
+    return this.get(1);
+  }
+
+  average() {
+    if (this.length === 0) throw Error('EmptySortedList');
+    return this.sum() / this.length;
+  }
+
+  sum() {
+    if (this.length === 0) {
+      return 0;
+    }
+    return this.items.reduce((a, b) => a + b);
+  }
+}
 
 module.exports = SortedList;
