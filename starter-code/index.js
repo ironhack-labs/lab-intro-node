2a46e555d29b683b19cc0c23e078d6753960f6eb
laura <lauraabad83@gmail.com>
2019-04-08 22:12:36 +0200

    a test is missing

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,58 @@
 class SortedList {
-  constructor() {}
-  add(item) {}
-  get(pos) {}
-  max() {}
-  min() {}
-  average() {}
-  sum() {}
+    constructor() {
+        this.items = [];
+        this.length = this.items.length;
+    }
+    add(item) {
+        this.items.push(item);
+        this.length = this.items.length;
+        this.items.sort(function(a, b) { return a - b });
+
+    }
+    get(pos) {
+
+        if (this.length > pos) {
+            return this.items[pos];
+        } else {
+            throw new Error("OutOfBounds");
+        }
+
+
+    }
+    max() {
+        if (this.length === 0) {
+            throw new Error("EmptySortedList")
+        } else {
+            return this.items[this.length - 1];
+        }
+    }
+    min() {
+        if (this.length === 0) {
+            throw new Error("EmptySortedList")
+        } else {
+            return this.items[0];
+        }
+    }
+    average() {
+        if (this.length === 0) {
+            throw new Error("EmptySortedList")
+        } else {
+            let avg = this.sum() / this.items.length;
+            return avg;
+        }
+    }
+
+    sum() {
+        this.sum = 0;
+        if (this.length === 0) return 0;
+        else {
+            for (let i = 0; i < this.length; i++) {
+                this.sum += this.items[i];
+
+            }
+            return this.sum
+        }
+    }
 };
 
 module.exports = SortedList;
