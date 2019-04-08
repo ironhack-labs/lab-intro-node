9db14d9eccd3e48055af6cd618eb0477ba20d2ac
raveBolognesa <rafael.arrieta.gar@gmail.com>
2019-04-08 18:33:49 +0200

    last comit

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,32 @@
 class SortedList {
-  constructor() {}
-  add(item) {}
-  get(pos) {}
-  max() {}
-  min() {}
-  average() {}
-  sum() {}
+  constructor() {
+    this.items= []
+    this.length= 0;
+  }
+  add(item) {
+    this.items.push(item)
+    this.length= this.items.length
+  }
+  get(pos) {
+    return this.items[pos]
+  }
+  max() {
+    if(this.items.length ===0){throw new Error("EmptySortedList")}
+    return Math.max(...this.items)
+  }
+  min() {
+    if(this.items.length ===0){throw new Error("EmptySortedList")}
+    return Math.min(...this.items)
+  }
+  average() {
+    if(this.items.length ===0){throw new Error("EmptySortedList")}
+    return this.sum()/this.items.length
+  }
+  sum() {
+    if(this.items.length ===0){return 0}
+   return this.items.reduce((x,i)=>x+i)
+  }
 };
+var sl = new SortedList()
 
 module.exports = SortedList;
