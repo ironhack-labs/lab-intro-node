5d2699d5e5f01664e0f5b92ba6c79e482fd15bb2
Raúl Contreras <raulc98@gmail.com>
2019-04-08 18:09:33 +0200

    Only fails one test

--- starter-code/index.js
+++ starter-code/index.js
@@ -12,21 +12,23 @@ class SortedList {
     this._update();
   }
   get(pos) {
-    if(this.items[pos] === undefined) throw new Error('OutOfBounds');
-    return this.items[pos];
+    if(typeof this.items[pos-1] === 'undefined') throw new Error('OutOfBounds');
+    return this.items[pos-1];
   }
   max() {
     if(this.length === 0) throw new Error("EmptySortedList");
-
+    return Math.max(...this.items);
   }
   min() {
     if(this.length === 0) throw new Error("EmptySortedList");
+    return Math.min(...this.items);
   }
   average() {
     if(this.length === 0) throw new Error("EmptySortedList");
+    return this.items.reduce((a, b) => a+b, 0) / this.length;
   }
   sum() {
-  
+    return this.items.reduce((a, b)=> a+b, 0);
   }
 }
 
