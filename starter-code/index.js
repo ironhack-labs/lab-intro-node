3bf7b24c29401781368ccbf1511504a7a4767672
Luca Manfredi <luca.manfredi1987@gmail.com>
2019-04-08 18:34:17 +0200

    done

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,38 @@
 class SortedList {
-  constructor() {}
-  add(item) {}
-  get(pos) {}
-  max() {}
-  min() {}
-  average() {}
-  sum() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
+
+  add(item) {
+    this.items.push(item);
+    this.length = this.items.length;
+    this.items.sort();
+  }
+  get(pos) {
+    return this.items[pos-1];
+  }
+  max() {
+    if(this.items.length == 0){
+      throw new Error("EmptySortedList");
+    }
+    return this.items.reduce((a, b) =>  Math.max(a, b));
+  }
+  min() {
+    if(this.items.length == 0){
+      throw new Error("EmptySortedList");
+    }
+    return this.items.reduce((a, b) => Math.min(a, b));
+  }
+  average() {
+    if(this.items.length == 0){
+      throw new Error("EmptySortedList");
+    }
+    return this.items.reduce((a,b) => (a + b)) / this.items.length ;
+  }
+  sum() {
+    return this.items.reduce((a,b) => (a + b), 0); 
+  }
 };
 
 module.exports = SortedList;
