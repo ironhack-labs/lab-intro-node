41007362f09c4ed0b74cd2c3fea50250995988df
Raúl Contreras <raulc98@gmail.com>
2019-04-08 14:56:51 +0200

    Initial commit

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,33 @@
 class SortedList {
-  constructor() {}
-  add(item) {}
-  get(pos) {}
-  max() {}
-  min() {}
-  average() {}
-  sum() {}
-};
+  constructor() {
+    this.items = [];
+    this._update();
+  }
+  _update(){
+    this.length = this.items.length;
+  }
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a, b) => a-b);
+    this._update();
+  }
+  get(pos) {
+    if(this.items[pos] === undefined) throw new Error('OutOfBounds');
+    return this.items[pos];
+  }
+  max() {
+    if(this.length === 0) throw new Error("EmptySortedList");
+
+  }
+  min() {
+    if(this.length === 0) throw new Error("EmptySortedList");
+  }
+  average() {
+    if(this.length === 0) throw new Error("EmptySortedList");
+  }
+  sum() {
+  
+  }
+}
 
 module.exports = SortedList;
