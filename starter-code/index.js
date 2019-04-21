8b3f92bf17d773fe7b3d2bfeb0d03794ac8b70cb
Arthur <arthur_sbd@hotmail.com>
2019-04-20 22:16:37 -0300

    finish

--- starter-code/index.js
+++ starter-code/index.js
@@ -10,12 +10,21 @@ class SortedList {
     return this.item[pos];
   }
   max() {
+    if(this.length === 0) {
+      throw new Error('EmptySortedList');
+    }
     return Math.max.apply(null, this.item);
   }
   min() {
+    if(this.length === 0) {
+      throw new Error('EmptySortedList');
+    }
     return Math.min.apply(null, this.item)
   }
   average() {
+    if(this.length === 0) {
+      throw new Error('EmptySortedList');
+    }
     let sumo = 0;
     for (let i = 0; i < this.item.length; i += 1) {
       sumo += this.item[i];
