34b1326c0af14d9db77b2c06a269162720b69e14
Christian <christiankhawam@outlook.com>
2019-04-23 08:23:15 -0400

    initial commit

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,52 @@
 class SortedList {
-  constructor() {}
-  add(item) {}
-  get(pos) {}
-  max() {}
-  min() {}
-  average() {}
-  sum() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length
+  }
+  add(item) {
+    this.items.push(item)
+    this.items = this.items.sort()
+    this.length += 1
+
+  }
+  get(pos) {
+    return this.items[pos];
+  }
+
+  max() {
+    if (this.items.length === 0) {
+      throw new Error("EmptySortedList")
+    }
+    return Math.max(...this.items)
+  }
+
+
+  min() {
+    if (this.items.length === 0) {
+      throw new Error("EmptySortedList")
+    }
+    return Math.min(...this.items)
+  }
+
+
+  average() {
+    if (this.items.length === 0) {
+      throw new Error('EmptySortedList');
+    } else {
+      const arrSum = this.items.reduce((acc, b) => acc + b);
+      const averageArray = arrSum / this.items.length;
+      return averageArray;
+    }
+
+  }
+  sum() {
+    if (this.items.length === 0) {
+      return 0;
+    } else {
+      const arrSum = this.items.reduce((acc, b) => acc + b);
+      return arrSum;
+    }
+  }
 };
 
 module.exports = SortedList;
