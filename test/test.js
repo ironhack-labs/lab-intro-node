183e3d275fd48e1edce7e8cf48a96fb9b336d3e7
tawebbcn <ta-web-bcn@ironhack.com>
2019-04-24 12:51:36 +0200

    Fixed test error

--- test/test.js
+++ test/test.js
@@ -51,7 +51,7 @@ describe('SortedList', function() {
       var foo = 10;
       for(let i=0; i<200; i++) {
         sl.add(foo*i);
-        assert.equal(sl.get(i), foo*i);
+        assert.equal(sl.get(i+1), foo*i);
       }
     });
   });
