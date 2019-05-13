dc069b8f968115c1ca32cddeab17d0e5c352696a
Noah Wang Teng <NoahRiseUp@Noahs-MacBook-Pro.local>
2019-05-13 19:16:22 +0200

    finished

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,45 @@
 class SortedList {
-  constructor() {}
-  add(item) {}
-  get(pos) {}
-  max() {}
-  min() {}
-  average() {}
-  sum() {}
+  constructor(items, length) {
+    this.items = []
+    this.length = this.items.length
+
+  }
+  add(item) {
+    this.items.push(item)
+    this.items.sort((a, b) => a - b)
+    this.length = this.items.length
+  }
+  get(pos) {
+    return this.items[pos - 1]
+  }
+  max() {
+    if (this.length > 0) {
+      return Math.max(...this.items) // Porque los 3 puntos
+    } else {
+      throw new Error("EmptySortedList")
+    }
+  }
+  min() {
+    if (this.length > 0) {
+      return Math.min(...this.items) // Porque los 3 puntos
+    } else {
+      throw new Error("EmptySortedList")
+    }
+  }
+  average() {
+    if (this.length > 0) {
+      return this.items.reduce((a, b) => a + b) / this.length
+    } else {
+      throw new Error("EmptySortedList")
+    }
+  }
+  sum() {
+    if (this.length > 0) {
+      return this.items.reduce((a, b) => a + b)
+    } else {
+      return 0
+    }
+  }
 };
 
 module.exports = SortedList;
