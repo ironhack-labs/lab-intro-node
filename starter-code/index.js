501b1466b02d41d3c4ed5c693d4173adc1b6667b
Paula Filipe <pac.tic.es@gmail.com>
2019-05-13 23:38:00 +0200

    All tests passed

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,45 @@
 class SortedList {
-  constructor() {}
-  add(item) {}
-  get(pos) {}
-  max() {}
-  min() {}
-  average() {}
-  sum() {}
+  constructor() {
+    this.items = [];
+    this.length = 0;
+  }
+  add(item) {
+    this.items.push(item);
+    this.length++;
+    this.items.sort(function (a, b) {
+      return a - b;
+    });
+
+  }
+  get(pos) {
+    //You should also provide a length property that gives the length of the list.
+
+    return this.items[pos - 1];
+  }
+  max() {
+    if (this.length == 0)
+      throw new Error('EmptySortedList');
+    else
+      return Math.max(...this.items);
+  }
+  min() {
+    if (this.length == 0)
+      throw new Error('EmptySortedList');
+    else
+      return Math.min(...this.items);
+  }
+  average() {
+    if (this.length == 0)
+      throw new Error('EmptySortedList');
+    else
+      return this.items.reduce((a, b) => a + b) / this.length;
+  }
+  sum() {
+    if (this.length == 0)
+      return 0;
+    else
+      return this.items.reduce((a, b) => a + b);
+  }
 };
 
 module.exports = SortedList;
