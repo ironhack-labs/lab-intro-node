f5ba6839881a05f6c39653f379c79720835420b0
“Anthony <anthonym.gs@gmail.com>
2019-05-14 08:39:51 +0200

    finished

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,42 @@
 class SortedList {
-  constructor() {}
-  add(item) {}
-  get(pos) {}
-  max() {}
-  min() {}
-  average() {}
-  sum() {}
+  constructor() {
+    this.items = [];
+    this.length = 0;
+  }
+  add(item) {
+    this.items.push(item);
+    this.items.sort((x, y) => x - y)
+    this.length++
+  }
+  get(pos) {
+    return this.items[pos - 1];
+  }
+  max() {
+    if (this.length === 0) {
+      throw new Error("EmptySortedList")
+    }
+    return this.items[this.length - 1]
+  }
+  min() {
+    if (this.length === 0) {
+      throw new Error("EmptySortedList")
+    }
+    return this.items[0]
+  }
+  average() {
+    if (this.length === 0) {
+      throw new Error("EmptySortedList")
+    }
+    return this.sum() / this.length;
+
+  }
+  sum() {
+    let result = 0;
+    for (let i = 0; i < this.length; i++) {
+      result += this.items[i]
+    }
+    return result;
+  }
 };
 
 module.exports = SortedList;
