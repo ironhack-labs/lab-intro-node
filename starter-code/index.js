1eca71b49469cdd467acc6658f60e3310f847f4e
root <root@JORGE.localdomain>
2019-05-14 13:55:57 +0200

    Dayli_Exercice_4-Week_1-Day_INTRO_NODE

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,20 @@
 class SortedList {
-  constructor() {}
-  add(item) {}
-  get(pos) {}
+  constructor(items, length) {
+    this.items = [];
+    this.length = this.items.length;
+  }
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a, b) => a - b);
+    this.length = this.items.length;
+  }
+  get(pos) {
+    return this.items[pos - 1];
+  }
   max() {}
   min() {}
   average() {}
   sum() {}
-};
+}
 
 module.exports = SortedList;
