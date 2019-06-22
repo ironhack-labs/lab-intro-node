cfbd0ebae8cd1459f17b241191938f3b6d949aa5
YerimMG <yeral_z2000@hotmail.com>
2019-06-22 16:01:11 -0500

    Tarea

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,55 @@
+'use strict';
+
 class SortedList {
-  constructor() {}
-  add(item) {}
-  get(pos) {}
-  max() {}
-  min() {}
-  average() {}
-  sum() {}
+  constructor() {
+    this.items  = [];
+    this.length = this.items.length;   
+  }
+
+
+
+
+
+  add(item) {
+    this.items.push(item);
+    this.items.sort(function (a,b){
+      return a-b;
+    })
+    this.length = this.items.length;   
+  }
+  get(pos) {
+    return this.items[pos - 1]
+  }
+
+
+  max() {
+  if (this.items.length === 0){
+    throw new Error("EmptySortedList");
+  }
+  return Math.max.apply(null, this.items)
+  }
+
+
+  min() {
+      if (this.items.length === 0){
+    throw new Error("EmptySortedList");
+  }
+  return Math.min.apply(null, this.items)
+  }
+  
+  average() {
+    if (this.items.length === 0) {
+        throw new Error("EmptySortedList");
+      }
+    return this.sum() / this.length;
+
+  }
+  sum() {
+    if (this.items.length === 0) {
+      return 0;
+    }
+    return this.items.reduce((total, current) => total += current);
+  }  
 };
 
 module.exports = SortedList;
