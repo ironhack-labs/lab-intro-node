793506af8644f4ef5f3627128c6d6f3b1b364cb2
LuisOrihuela <33643227+LuisOrihuela@users.noreply.github.com>
2019-06-22 15:52:00 -0500

    Update index.js

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,43 @@
 class SortedList {
-  constructor() {}
-  add(item) {}
-  get(pos) {}
-  max() {}
-  min() {}
-  average() {}
-  sum() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a, b)=>a-b);
+    this.length = this.items.length;
+    return this.length;
+  }
+  get(pos) {
+    return this.items[pos-1];
+  }
+  max() {
+    if(this.items.length === 0){
+      throw new Error('EmptySortedList');
+    }
+    return Math.max.apply(null, this.items);
+  }
+
+  min() {
+    if(this.items.length === 0){
+      throw new Error('EmptySortedList');
+    }
+    return Math.min.apply(null, this.items);
+  }
+  average() {
+    if(this.items.length === 0){
+      throw new Error('EmptySortedList');
+    }
+    return this.sum() / this.length;
+    
+  }
+  sum() {
+    if(this.length === 0){
+      return 0;
+    }
+    return this.items.reduce(function(a, b) { return a + b; });
+  }
 };
 
 module.exports = SortedList;
