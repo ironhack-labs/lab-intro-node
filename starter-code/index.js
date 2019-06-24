6afb0b200cb2c5cf51ba653645e78197532dff67
anwittw <git@andrewittwer.de>
2019-06-24 20:10:31 +0100

    Done

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,44 @@
 class SortedList {
-  constructor() {}
-  add(item) {}
-  get(pos) {}
-  max() {}
-  min() {}
-  average() {}
-  sum() {}
-};
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
+  add(x) {
+    this.items.push(x);
+    this.length = this.items.length;
+  }
+  get(pos) {
+    this.items.sort()
+    return this.items[pos-1];
+  }
+  max() {
+    if (this.items.length === 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      return this.items.reduce((acc, cv) => (cv > acc ? cv : acc));
+    }
+  }
+  min() {
+    if (this.items.length === 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      return this.items.reduce((acc, cv) => (cv < acc ? cv : acc));
+    }
+  }
+  average() {
+    if (this.items.length === 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      return this.items.reduce((acc, cv) => acc + cv) / this.length;
+    }
+  }
 
+  sum() {
+    if (this.items.length === 0) {
+      return 0;
+    } else {
+      return this.items.reduce((acc, cv) => acc + cv);
+    }
+  }
+}
 module.exports = SortedList;
