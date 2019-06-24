3294a41c056ec2dc10bb7c62e1d1f2db2c42e524
rubander <50637691+rubander@users.noreply.github.com>
2019-06-24 19:09:38 +0200

    Update index.js

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,34 @@
 class SortedList {
-  constructor() {}
-  add(item) {}
-  get(pos) {}
-  max() {}
-  min() {}
-  average() {}
-  sum() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length
+  }
+  add(item) {
+    return this.items.push(item)
+  }
+  get(pos) {
+    return this.items[pos]
+    
+  }
+  max() {
+
+    if (this.items.length = 0) {
+      throw new Error("EmptySortedList")}
+  }
+  min() {
+
+    if (this.items.length = 0) {
+    throw new Error("EmptySortedList")}
+
+  }
+  average() {
+
+    if (this.items.length = 0) {
+      throw new Error("EmptySortedList")}
+  }
+  sum() {
+
+  }
 };
 
 module.exports = SortedList;
