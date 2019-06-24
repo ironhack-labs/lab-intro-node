b60b970835e3a307ff6e2bc62a5bc512b35030d4
geguiluz <memo.eguiluz@gmail.com>
2019-06-24 13:20:26 -0500

    Ejercicio de clase terminado

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,42 @@
 class SortedList {
-  constructor() {}
-  add(item) {}
-  get(pos) {}
-  max() {}
-  min() {}
-  average() {}
-  sum() {}
-};
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a,b) => a-b);
+    this.length = this.items.length;
+  }
+  get(pos) {
+    return this.items[pos];
+  }
+  max() {
+    if (this.items.length === 0){
+      throw new Error('EmptySortedList');
+    }
+    return Math.max.apply(null, this.items);
+  }
+  min() {
+    if (this.items.length === 0){
+      throw new Error('EmptySortedList');
+    }
+    return Math.min.apply(null, this.items);
+  }
+  average() {
+    if (this.items.length === 0){
+      throw new Error('EmptySortedList');
+    }
+    return this.items.reduce((total, current) => total  += current)/this.length;
+  }
+    
+    sum() {
+      if (this.items.length === 0){
+        return 0
+      }
+    return this.items.reduce((total, current) => total  += current);
+      
+  }
+}
 
 module.exports = SortedList;
