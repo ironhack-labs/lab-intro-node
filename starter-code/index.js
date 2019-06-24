0bda82b143b74cc0c192e1d37e67ebe8c5de6bb4
Miriloper <miriam.mad.ml@gmail.com>
2019-06-24 20:12:13 +0200

    Finish Daily Intro node

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,48 @@
 class SortedList {
-  constructor() {}
-  add(item) {}
-  get(pos) {}
-  max() {}
-  min() {}
-  average() {}
-  sum() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+    
+  }
+  add(item) {
+    this.items.push(item);
+    this.length = this.items.length;
+    this.items.sort((a,b) => (a-b));
+  }
+
+  get(pos) {
+    this.length = this.items.length
+    return this.items[pos-1];
+    
+  }
+
+  max() {
+    if (this.items.length == 0) {
+      throw new Error("EmptySortedList");
+    }
+    else return Math.max(...this.items)
+    
+  }
+
+  min() {
+    if (this.items.length == 0) {
+      throw new Error("EmptySortedList");
+    }
+    else return Math.min(...this.items)
+    
+  }
+
+  sum() {
+    return this.items.reduce((acum, sumElem) => acum + sumElem, 0);
+  }
+
+  average() {
+    if (this.items.length == 0) {
+      throw new Error("EmptySortedList");
+    }
+    else return this.sum()/this.length;
+  }
+
 };
 
 module.exports = SortedList;
