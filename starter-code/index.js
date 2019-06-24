c41252cb1b3334d3787c431c0f946032cec7da15
Vanesa Puerta <Vanesa@MacBook-Air-de-Vanesa.local>
2019-06-24 19:53:56 +0200

    done

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,47 @@
 class SortedList {
-  constructor() {}
-  add(item) {}
-  get(pos) {}
-  max() {}
-  min() {}
-  average() {}
-  sum() {}
+  constructor() {
+    this.items = []
+    this.length = this.items.length
+  }
+  add(item) {
+    this.items.push(item)
+    this.length = this.items.length
+    this.items.sort((a,b) => a-b)
+  }
+  get(pos) {
+    return this.items[pos-1]
+  }
+  max() {
+    
+    if (!this.items.length == 0 ){
+      return Math.max(...this.items)
+    }else {
+      throw new Error("EmptySortedList")
+    }
+  }
+  min() {
+    if (!this.items.length == 0 ){
+      return Math.min(...this.items)
+    }else {
+      throw new Error("EmptySortedList")
+    }
+  }
+  average() {
+    if (!this.items.length == 0){
+      return this.sum()/this.length
+    }else {
+    throw new Error("EmptySortedList")    
+  }
+
+  }
+  sum() {
+    let sum = (a,b) => a+b;
+    if (!this.items.length == 0){
+      return this.items.reduce(sum)
+    }else {
+      return 0
+    }
+  }   
 };
 
 module.exports = SortedList;
