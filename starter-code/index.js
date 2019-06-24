f39a94b7cce7ef5012b73de168868ddc78c2a1b1
rubander <ralexanderv89@gmail.com>
2019-06-24 19:53:06 +0200

    well...

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,34 +1,44 @@
 class SortedList {
   constructor() {
     this.items = [];
-    this.length = this.items.length
+    this.length = this.items.length;
   }
   add(item) {
-    return this.items.push(item)
+    this.items.push(item);
+    this.length = this.items.length;
+    this.items.sort((a, b) => a - b);
   }
   get(pos) {
-    return this.items[pos]
-    
+    return this.items[pos - 1];
   }
   max() {
-
-    if (this.items.length = 0) {
-      throw new Error("EmptySortedList")}
+    if (this.length == 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      return Math.max(...this.items);
+    }
   }
   min() {
-
-    if (this.items.length = 0) {
-    throw new Error("EmptySortedList")}
-
+    if (this.length == 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      return Math.min(...this.items);
+    }
   }
   average() {
-
-    if (this.items.length = 0) {
-      throw new Error("EmptySortedList")}
+    if (this.length ==   0) {
+      throw new Error("EmptySortedList");
+    } else {
+    } return this.sum() /this.length
   }
   sum() {
-
+    if (this.items.length == 0) {
+      return 0
+    } else {
+      let sum = (a, b) => a + b;
+      return this.items.reduce(sum);
+    }
   }
-};
+}
 
 module.exports = SortedList;
