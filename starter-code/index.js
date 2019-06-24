c31b5141aeb66fefa6d60554df181c4dcdb5395a
Gabriel B <gabelmo92@hotmail.com>
2019-06-24 19:27:58 +0200

    refactor

--- starter-code/index.js
+++ starter-code/index.js
@@ -12,28 +12,28 @@ class SortedList {
     return this.items[pos - 1];
   }
   max() {
-    if (!this.items.length == 0) {
+    if (!this.length == 0) {
       return Math.max(...this.items);
     } else {
       throw new Error("EmptySortedList");
     }
   }
   min() {
-    if (!this.items.length == 0) {
+    if (!this.length == 0) {
       return Math.min(...this.items);
     } else {
       throw new Error("EmptySortedList");
     }
   }
   average() {
-    if (this.items.length == 0) {
+    if (this.length == 0) {
       throw new Error("EmptySortedList");
     } else {
-      return this.sum() / this.items.length;
+      return this.sum() / this.length;
     }
   }
   sum() {
-    if (!this.items.length == 0) {
+    if (!this.length == 0) {
       let suma = (a, b) => a + b;
       return this.items.reduce(suma);
     } else {
