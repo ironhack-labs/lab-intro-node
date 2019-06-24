06e8f9856b77e90e80de778f9a5aeea1b7858b7c
MicaelZV <micaelzv@gmail.com>
2019-06-24 20:13:54 +0200

    intro node

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,41 @@
 class SortedList {
-  constructor() {}
-  add(item) {}
-  get(pos) {}
-  max() {}
-  min() {}
-  average() {}
-  sum() {}
+  constructor() {
+      this.items = [];
+      this.length = this.items.length;
+  }
+  add(item) {
+      this.items.push(item)
+      this.length = this.items.length;
+      this.items.sort((a, b) => a - b)
+  }
+  get(position) {
+      return this.items[position - 1]
+  }
+  max() {
+      if (this.length == 0) {
+          throw new Error("EmptySortedList")
+      } else {
+          return Math.max(...this.items)
+      }
+  }
+  min() {
+      if (this.length == 0) {
+          throw new Error("EmptySortedList")
+      } else {
+          return Math.min(...this.items)
+      }
+  }
+  average() {
+      if (this.length == 0) {
+          throw new Error("EmptySortedList")
+      } else {
+          return this.sum() / this.length
+      }
+
+  }
+  sum() {
+      return this.items.reduce((previous, current) => previous + current, 0)
+  }
 };
 
 module.exports = SortedList;
