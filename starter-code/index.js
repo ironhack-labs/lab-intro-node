efc5863756932b8134e24c712ec187b6a7dacbe7
William B <william.benn11@gmail.com>
2019-07-01 16:06:03 -0400

    MIA-FTWD William Bennett

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,79 @@
 class SortedList {
-  constructor() {}
-  add(item) {}
-  get(pos) {}
-  max() {}
-  min() {}
-  average() {}
-  sum() {}
-};
+  constructor() {
+
+    this.items = [];
+    this.length = this.items.length;
+
+  }
+  add(item) {
+
+    this.items.push(item);
+    this.items.sort();
+    this.length=this.items.length;
+  
+    }
+
+ 
+    get(pos) {
+ 
+      if(!this.items[pos-1]){
+        throw new Error('OutOfBounds');
+      }else{
+          return this.items[pos-1];
+      }
+     }
+    
+  max() {
+
+    if(this.length ===0){
+      throw new Error('EmptySortedList');
+      }
+      else{
+       return Math.max(...this.items);
+      }
+    }
+
+  min(){
+   
+    if(this.length ===0){
+    throw new Error('EmptySortedList');
+    }
+    else{
+     return Math.min(...this.items);
+    }
+  }
+
+  average() {
+
+    if(this.items.length ===0){
+      throw new Error('EmptySortedList');
+    }
+else{
+
+  let total = this.sum();
+
+   return total/this.length;
+
+}
+  }
+
+  sum() {
+
+    let sum =0;
+
+    if(this.items.length ===0){
+      return sum;
+    }
+
+else{
+    for (let i = 0; i < this.items.length; i++) {
+      sum += this.items[i]
+
+    }
+    return sum;
+  }
+  }
+
+}
 
 module.exports = SortedList;
