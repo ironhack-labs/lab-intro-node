94925dad6af30b0335c30f47e0077e720052b029
Uira Oliveira <uira.sampaio@hotmail.com>
2019-07-01 18:47:04 -0300

    done

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,43 @@
 class SortedList {
-  constructor() {}
-  add(item) {}
-  get(pos) {}
-  max() {}
-  min() {}
-  average() {}
-  sum() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
+
+  add(x) {
+    this.items.push(x);
+    this.items = this.items.sort((a, b) => a - b);
+    this.length = this.items.length;
+  }
+
+  get(pos) {
+    // if (pos > this.length || pos <= 0) return 0;
+
+    return this.items[pos - 1];
+  }
+
+  max() {
+    if (this.length === 0) throw Error('EmptySortedList');
+
+    return Math.max(...this.items);
+  }
+
+  min() {
+    if (this.length === 0) throw Error('EmptySortedList');
+
+    return Math.min(...this.items);
+  }
+
+  sum() {
+    if (this.length === 0) return 0;
+    return this.items.reduce((acc, cv) => acc + cv, 0);
+  }
+
+  average() {
+    if (this.length === 0) throw Error('EmptySortedList');
+    return this.sum() / this.length;
+  }
+
 };
 
 module.exports = SortedList;
