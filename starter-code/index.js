b0287f633510cd88be2919981a718698684a5334
Renan Soares <renan.soares.francisco@gmail.com>
2019-07-01 20:30:24 -0300

    complete

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,69 @@
+let highest = 0;
+
 class SortedList {
-  constructor() {}
-  add(item) {}
-  get(pos) {}
-  max() {}
-  min() {}
-  average() {}
-  sum() {}
-};
+  constructor() {
+    this.items = [];
+    this.length = 0;
+  }
+
+  add(item) {
+    this.items.push(item);
+    this.length += 1;
+    this.items.sort((a, b) => a - b);
+  }
+
+  get(pos) {
+    return this.items[pos - 1];
+  }
+
+  max() {
+    if (this.items == 0) {
+      throw new Error('EmptySortedList');
+    } else {
+      this.items.forEach(num => {
+        if (num > highest) {
+          highest = num;
+        }
+      });
+      return highest;
+    }
+  }
+
+  min() {
+    if (this.items == 0) {
+      throw new Error('EmptySortedList');
+    } else {
+      let lowest = highest;
+      this.items.forEach(num => {
+        if (num < lowest) {
+          lowest = num;
+        }
+      });
+      return lowest;
+    }
+  }
+
+  average() {
+    if (this.length === 0) {
+      throw new Error('EmptySortedList');
+    } else {
+      let sum = this.items.reduce((total, num) => {
+        return total + num;
+      });
+      return sum / this.length;
+    }
+  }
+
+  sum() {
+    if (this.length === 0) {
+      return 0;
+    } else {
+      let sum = this.items.reduce((total, num) => {
+        return total + num;
+      });
+      return sum;
+    }
+  }
+}
 
 module.exports = SortedList;
