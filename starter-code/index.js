77b7a65625dd0e647d1e6da23eb958c2504bf0c2
yi-ting Chiang <tarynchiang@gmail.com>
2019-07-01 16:57:35 -0400

    done

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,54 @@
 class SortedList {
-  constructor() {}
-  add(item) {}
-  get(pos) {}
-  max() {}
-  min() {}
-  average() {}
-  sum() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
+  add(item) {
+    this.items.push(item);
+    this.length = this.items.length;
+    this.items.sort(function(a,b){
+      if(a>b){
+        return 1;
+      }else if(a < b){
+        return -1;
+      }
+      return 0;
+    });
+  }
+  get(pos) {
+      return this.items[pos-1];
+  }
+  max() {
+    if(this.items.length ===0){
+      throw new Error("EmptySortedList")
+    }else{
+      return this.items[this.length-1];
+    }
+  }
+  min() {
+    if(this.items.length ===0){
+      throw new Error("EmptySortedList")
+    }else{
+      return this.items[0];
+    }
+  }
+  average() {
+    if(this.items.length ===0){
+      throw new Error("EmptySortedList")
+    }else{
+      return this.sum()/this.length;
+    }
+  }
+  sum() {
+    if(this.items.length ===0){
+      return 0;
+    }else{
+      let sum = this.items.reduce(function(a,b){
+        return a+b;
+      },0)
+      return sum;
+    }
+  }
 };
 
-module.exports = SortedList;
+module.exports =  SortedList;
