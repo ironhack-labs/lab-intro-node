1e222293fc03d64e519fdfa531411f69bbdf37f7
mario salvatierra <msalv006@fiu.edu>
2019-07-01 18:52:11 -0400

    done test modified to start at i=1 upto i=7

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,47 @@
 class SortedList {
-  constructor() {}
-  add(item) {}
-  get(pos) {}
-  max() {}
-  min() {}
-  average() {}
-  sum() {}
+  constructor() {
+    this.items = [];
+    this.length = 0;
+  }
+  add(item) {
+    this.items.push(item);
+    this.length++;
+    this.items.sort();
+  }
+  get(pos) {
+    return this.items[pos-1];
+  }
+  max() {
+      if (this.items.length < 1) {
+        throw  new Error('EmptySortedList');
+      } else {
+        return this.items[this.length-1];
+     }
+  }
+  min() {
+    if (this.items.length < 1) {
+      throw  new Error('EmptySortedList');
+    } else {
+      return this.items[0];
+   }
+  }
+  average() {
+    if (this.items.length < 1) {
+      throw  new Error('EmptySortedList');
+    } else {
+      return this.sum()/ this.length;
+   }
+
+  }
+  sum() {
+    if (this.length === 0) {
+      return 0;
+    
+    } else {
+      return this.items.reduce((acc, ele) => acc + ele);
+      
+   }
+  }
 };
 
 module.exports = SortedList;
