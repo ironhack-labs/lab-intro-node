c9a429d8ac15e7c35fb846d49563f8c306e577f2
arstrel <arstrel@gmail.com>
2019-07-01 16:36:12 -0400

    All pass except for get(i)

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,34 @@
 class SortedList {
-  constructor() {}
-  add(item) {}
-  get(pos) {}
-  max() {}
-  min() {}
-  average() {}
-  sum() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
+  add(item) {
+    this.items.push(item);
+    this.length = this.items.length;
+    this.items.sort();
+  }
+  get(pos) {
+    
+    return this.items[pos -1];
+  }
+  max() {
+    if(this.items.length == 0) {throw new Error("EmptySortedList")}
+    return this.items[this.items.length-1]
+  }
+  min() {
+    if(this.items.length == 0) {throw new Error("EmptySortedList")}
+    return this.items[0];
+  }
+  average() {
+    if(this.items.length == 0) {throw new Error("EmptySortedList")}
+    return this.sum() / this.length
+  }
+  sum() {
+    return this.items.reduce((acc, val) => {
+      return acc+val
+    },0)
+  }
 };
 
 module.exports = SortedList;
