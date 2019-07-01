16f4bfb61688d2e457f0f0001703dc6377f75271
Taluama Velludo <taluama@gmail.com>
2019-07-01 18:39:51 -0400

    done

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,51 @@
 class SortedList {
-  constructor() {}
-  add(item) {}
-  get(pos) {}
-  max() {}
-  min() {}
-  average() {}
-  sum() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
+
+  add(x) {
+    this.items.push(x);
+    this.length = this.items.length;
+    this.items.sort(function(a, b){return a-b});
+  }
+
+  get(pos) {
+    return this.items[pos - 1];
+  }
+
+  max() {
+    if (this.length == 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      return Math.max(...this.items);
+    }
+  }
+  
+  min() {
+    if (this.length == 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      return Math.min(...this.items);
+    }
+  }
+  
+  average(x) {
+    if (this.length == 0) {
+      throw new Error("EmptySortedList");
+    } else {
+    return this.sum(x)/this.length
+    }
+  }
+  
+  sum() {
+    if (this.length == 0) {
+      return 0;
+    } else {
+      return this.items.reduce(function(a,b){return a + b}, 0);
+    }
+  }
+
 };
 
 module.exports = SortedList;
