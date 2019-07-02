9568cf05d7a7532cd0ea651e90e75ccdd02f29e9
fredericonti <fred@conticonti.com>
2019-07-01 22:35:55 -0300

    My commit

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,47 @@
 class SortedList {
-  constructor() {}
-  add(item) {}
-  get(pos) {}
-  max() {}
-  min() {}
-  average() {}
-  sum() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
+
+  add(item) {
+    this.items.push(item);
+    this.length = this.items.length;
+    this.items.sort((a, b) => a - b);
+  }
+
+  get(pos) {
+    return this.items[pos - 1];
+  }
+
+  max() {
+    if (this.items.length === 0) {
+      throw new Error("EmptySortedList")
+    } else {
+      return Math.max(...this.items);
+    }
+  }
+
+  min() {
+    if (this.items.length === 0) {
+      throw new Error("EmptySortedList")
+    } else {
+      return Math.min(...this.items);
+    }
+  }
+
+  average() {
+
+    if (this.items.length === 0) {
+      throw new Error("EmptySortedList")
+    } else {
+      return this.sum() / this.items.length;
+    }
+
+  }
+  sum() {
+    return this.items.reduce((a, b) => a + b, 0)
+  }
 };
 
 module.exports = SortedList;
