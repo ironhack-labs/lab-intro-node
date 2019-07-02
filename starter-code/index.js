053ef60ee10a4538878c1cbe6c8939b13355617e
Gabriela Dias <gabrieladias.souza@hotmail.com>
2019-07-02 09:52:08 -0300

    Done

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,44 @@
+/* eslint-disable no-unused-expressions */
 class SortedList {
-  constructor() {}
-  add(item) {}
-  get(pos) {}
-  max() {}
-  min() {}
-  average() {}
-  sum() {}
-};
+  constructor() {
+    this.items = [];
+    this.length = 0;
+  }
+
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a, b) => a - b);
+    this.length = this.items.length;
+  }
+
+  get(pos) {
+    return this.items[pos];
+  }
+
+  max() {
+    if (this.items.length === 0) {
+      throw new Error('EmptySortedList')
+    }
+    return Math.max(...this.items);
+  }
+
+  min() {
+    if (this.items.length === 0) {
+      throw new Error('EmptySortedList');
+    }
+    return Math.min(...this.items);
+  }
+
+  sum() {
+    return this.items.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+  }
+
+  average() {
+    if (this.items.length === 0) {
+      throw new Error('EmptySortedList');
+    }
+    return this.sum() / this.length;
+  }
+}
 
 module.exports = SortedList;
