3c923fb073b183da46f24356d5078d52e7f3722e
Victor Levy <victorlevygp@gmail.com>
2019-07-02 09:51:18 -0300

    done

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,37 @@
 class SortedList {
-  constructor() {}
-  add(item) {}
-  get(pos) {}
-  max() {}
-  min() {}
-  average() {}
-  sum() {}
-};
+  constructor() {
+    this.items = [];
+    this.length = 0;
+  }
+
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a, b) => a - b);
+    this.length = this.items.length;
+  }
+
+  get(pos) {
+    return this.items[pos];
+  }
+
+  max() {
+    if (this.length === 0) throw new Error('EmptySortedList');
+    return this.items[this.length - 1];
+  }
+
+  min() {
+    if (this.length === 0) throw new Error('EmptySortedList');
+    return this.items[0];
+  }
+
+  average() {
+    if (this.length === 0) throw new Error('EmptySortedList');
+    return this.sum() / this.length;
+  }
+
+  sum() {
+    return this.items.reduce((total, num) => total + num, 0);
+  }
+}
 
 module.exports = SortedList;
