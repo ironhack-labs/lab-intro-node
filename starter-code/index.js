757461388efad1173164eb7092e9c8f17f720117
Serena Dominguez <serenadominguez@gmail.com>
2019-07-01 21:50:24 -0400

    lab

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,16 @@
 class SortedList {
-  constructor() {}
-  add(item) {}
+  constructor() {
+    this.items = [];
+    this.length = 0;
+  }
+  add(items) {
+    this.items.push(items);
+    this.length = this.items.length;
+  }
   get(pos) {}
   max() {}
   min() {}
   average() {}
   sum() {}
-};
-
+}
 module.exports = SortedList;
