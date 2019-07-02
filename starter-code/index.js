630ff51574c92c36f786717c7ae4877d0dea84b7
Layza Floriano <layzafloriano@gmail.com>
2019-07-02 09:50:55 -0300

    done

--- starter-code/index.js
+++ starter-code/index.js
@@ -5,8 +5,9 @@ class SortedList {
   }
 
   add(item) {
-    this.items.unshift(item);
-    this.length += 1;
+    this.items.push(item);
+    this.items.sort((a, b) => a - b);
+    this.length = this.items.length;
   }
 
   get(pos) {
@@ -14,28 +15,28 @@ class SortedList {
   }
 
   max() {
-    if (this.length <= 0) {
+    if (this.length === 0) {
       throw new Error('EmptySortedList');
     }
     return Math.max(...this.items);
   }
 
   min() {
-    if (this.length <= 0) {
+    if (this.length === 0) {
       throw new Error('EmptySortedList');
     }
     return Math.min(...this.items);
   }
 
   average() {
-    if (this.length <= 0) {
+    if (this.length === 0) {
       throw new Error('EmptySortedList');
     }
     return this.sum() / this.length;
   }
 
   sum() {
-    if (this.length <= 0) {
+    if (this.length === 0) {
       return 0;
     }
     return this.items.reduce((sum, num) => sum + num, 0);
