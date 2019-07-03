4dad1bf945dde637357b2a17c6099a130ec2223d
Serena Dominguez <serenadominguez@gmail.com>
2019-07-02 20:45:44 -0400

    progress

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,16 +1,37 @@
 class SortedList {
   constructor() {
     this.items = [];
-    this.length = 0;
+    this.length = this.items.length;
   }
   add(items) {
     this.items.push(items);
     this.length = this.items.length;
+    this.items = this.items.sort();
   }
-  get(pos) {}
-  max() {}
-  min() {}
-  average() {}
-  sum() {}
+  get(pos) {
+    // this.length = this.items.length;
+    return this.items[pos];
+  }
+  max() {
+    this.items.max();
+    if (this.items === 0) {
+      throw new Error("EmptySortedList");
+    }
+  }
+  min() {
+    this.items.min();
+    if (this.items === 0) {
+      throw new Error("EmptySortedList");
+    }
+  }
+  average() {
+    return this.items.averge();
+    if (this.items === 0) {
+      throw new Error("EmptySortedList");
+    }
+  }
+  sum() {
+    return this.items.sum();
+  }
 }
 module.exports = SortedList;
