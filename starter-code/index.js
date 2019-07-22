9c8d039fc982ccd6d71df28159448abe0398345b
Iváne <iromcan1984@gmail.com>
2019-07-22 19:40:09 +0200

    primer commit

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,40 @@
 class SortedList {
-    constructor() {}
-    add(item) {}
-    get(pos) {}
-    max() {}
-    min() {}
-    average() {}
-    sum() {}
-  };
-  
-  module.exports = SortedList;
+  constructor() {
+    this.items = [],
+      this.length = 0
+  }
+  add(item) {
+    this.length++
+    this.items.push(item)
+    this.items.sort((a, b) => a - b)
+  }
+  get(pos) {
+    if (pos > this.length - 1) {
+      throw new Error("OutOfBounds")
+    } else return this.items[pos]
+  }
+  max() {
+    if (this.length == 0) {
+      throw new Error("EmptySortedList")
+    } else return Math.max(...this.items)
+  }
+  min() {
+    if (this.length == 0) {
+      throw new Error("EmptySortedList")
+    } else return Math.min(...this.items)
+  }
+  average() {
+    if (this.length == 0) {
+      throw new Error("EmptySortedList")
+    } else return this.items.reduce((a, b) => a + b) / this.length
+  }
+
+  sum() {
+    if (this.length == 0) {
+      return 0
+    } else return this.items.reduce((a, b) => a + b)
+  }
+
+};
+
+module.exports = SortedList;
