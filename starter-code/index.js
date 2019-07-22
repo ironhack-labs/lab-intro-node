d5a3e5978aba4efd01617cc51e18c2859030e834
Maria Pelaez <maria.shecodes@gmail.com>
2019-07-22 23:49:04 +0200

    done

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,47 @@
 class SortedList {
-    constructor() {}
-    add(item) {}
-    get(pos) {}
-    max() {}
-    min() {}
-    average() {}
-    sum() {}
+    constructor() {
+      this.items = []
+      this.length = this.items.length
+    }
+    add(x) {
+      this.items.push(x)
+      this.items.sort((a, b) => a - b)
+      this.length = this.items.length
+    }
+    get(pos) {
+      return this.items[pos]
+
+    }
+    max() {
+      if(this.items.length >= 1) {
+       return (Math.max(...this.items))
+      } else {
+        throw new Error("EmptySortedList")
+      }
+    }
+    min() {
+      if(this.items.length >= 1) {
+       return (Math.min(...this.items))
+      } else {
+        throw new Error("EmptySortedList")
+      }
+    }
+    sum() {
+      if(this.items.length == 0) {
+        return 0
+      } else {
+      return this.items.reduce(function(a,b) {
+        return a + b}, 0)
+      }
+    }
+    average() {
+      if(this.items.length == 0) {
+        throw new Error("EmptySortedList")
+      } else {
+      return (this.sum()/this.items.length)
+      }
+    }
   };
   
   module.exports = SortedList;
+
