91a28898889b2c7d9814b85cbf2970e5c10d53b9
ebgester <ebg.ester@gmail.com>
2019-07-23 00:05:20 +0200

    Done

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,54 @@
 class SortedList {
-    constructor() {}
-    add(item) {}
-    get(pos) {}
-    max() {}
-    min() {}
-    average() {}
-    sum() {}
+    constructor() {
+      this.items = []
+      this.length = this.items.length
+    }
+    add(item) {
+      this.items.push(item)
+      this.items.sort((a,b) => a-b)
+      this.length = this.items.length
+      
+    }
+    get(pos) {
+      
+      return this.items[pos]
+      
+    }
+    max() {
+      if (this.items.length != 0) {
+        let nMax = Math.max(...this.items)
+        
+        return nMax
+      } else {
+        throw new Error("EmptySortedList")
+      }
+    }
+    min() {
+      if (this.items.length == 0) {
+        throw new Error("EmptySortedList")
+      } else {
+        let nMin = Math.min(...this.items)
+        return nMin
+      }
+    }
+    average() {
+      if (this.items.length == 0) {
+        throw new Error("EmptySortedList")
+      } else {
+
+        return this.sum()/this.items.length
+      }
+    }
+    sum() {
+      if (this.length == 0) {
+        return 0
+      } else {
+        let total = 0
+        total += this.items.reduce(function(a,b){return a+b})
+
+        return total
+      }
+    }
   };
   
   module.exports = SortedList;
