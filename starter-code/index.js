8ec85114fadf955d488a56cf46a87266a7850078
medinajr <jrmedinpa@gmail.com>
2019-07-23 08:10:07 +0200

    done

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,78 @@
 class SortedList {
-    constructor() {}
-    add(item) {}
-    get(pos) {}
-    max() {}
-    min() {}
-    average() {}
-    sum() {}
-  };
-  
-  module.exports = SortedList;
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
+  add(item) {
+    this.items.push(item);
+    this.length++;
+    this.items.sort((a, b) => a - b);
+  }
+  get(pos) {
+    return this.items[pos];
+  }
+  max() {
+    let x = 0;
+    let sum = 0;
+
+    if (this.items.length > 0) {
+      let sum = this.items[x];
+
+      while (x < this.items.length) {
+        if (sum < this.items[x]) {
+          sum = this.items[x];
+        }
+        x++;
+      }
+      return sum;
+    } else {
+      throw new Error("EmptySortedList");
+    }
+  }
+  min() {
+    let x = 0;
+
+    let sum = this.items[x];
+    if (this.items.length > 0) {
+      while (x < this.items.length) {
+        if (this.items[x] < sum) {
+          sum = this.items[x];
+        }
+        x++;
+      }
+      return sum;
+    } else {
+      throw new Error("EmptySortedList");
+    }
+  }
+  average() {
+    let x = 0;
+    let sum = 0;
+    if (this.items.length > 0) {
+      while (x < this.items.length) {
+        sum += this.items[x];
+
+        x++;
+      }
+      return sum / this.items.length;
+    } else {
+      throw new Error("EmptySortedList");
+    }
+  }
+  sum() {
+    let x = 0;
+    let sum = 0;
+    if (this.items.length > 0) {
+      while (x < this.items.length) {
+        sum += this.items[x];
+
+        x++;
+      }
+      return sum;
+    } else {
+      return 0;
+    }
+  }
+}
+
+module.exports = SortedList;
