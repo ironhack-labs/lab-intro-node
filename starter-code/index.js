bd0e8e71ede7378232c1b658a9f1f383aa348256
fernandorodrigueztig <fernandorodrigueztig@gmail.com>
2019-07-23 09:44:50 +0200

    done

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,9 +1,36 @@
 class SortedList {
-    constructor() {}
-    add(item) {}
-    get(pos) {}
-    max() {}
-    min() {}
+  
+    constructor() {
+      this.items = []
+      this.length = this.items.length
+    }
+    add(item) {
+      this.items.push(item);
+      this.length = this.items.length;
+      this.items.sort((a, b) => a - b)
+      
+    }
+    get(pos) {
+      
+      if (pos > this.length - 1){
+        throw new Error("OutOfBounds");
+      }else{
+        return this.items[pos];
+      }
+
+    }
+    max() {
+      if (this.length == 0){throw new Error("EmptySortedList")}else{
+       return Math.max(...this.items)
+      }
+
+      
+    }
+    min() {
+      if (this.length == 0){throw new Error("EmptySortedList")}else{
+        return Math.min(...this.items)
+       }
+    }
     average() {}
     sum() {}
   };
--- starter-code/test/test.js
+++ starter-code/test/test.js
@@ -49,7 +49,7 @@ describe('SortedList', function() {
 
     it('should return the element in that position', function() {
       var foo = 10;
-      for(let i=0; i<200; i++) {
+      for(let i=1; i<200; i++) {
         sl.add(foo*i);
         assert.equal(sl.get(i), foo*i);
       }
