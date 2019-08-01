fbb24e9122edc7db1661ca2362b74b69060f0fd5
Nelly Gonzalez-Vinas <nramos4510@gmail.com>
2019-08-01 18:23:11 -0400

    updated lab

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,67 @@
 class SortedList {
-    constructor() {}
-    add(item) {}
-    get(pos) {}
-    max() {}
-    min() {}
-    average() {}
-    sum() {}
+    constructor() {
+      this.items = [];
+      this.length = 0;
+
+
+    }
+    add(item) {
+      this.items.push(item);
+      this.length++;
+      this.items.sort();
+    }
+
+    get(pos) {
+      if (this.length = 0) {
+        throw new Error('OutOfBounds');
+      } else {
+      return this.items[pos];
+      } 
+    }
+
+    max() {
+      if (this.length == 0) {
+        throw new Error("EmptySortedList");
+      } else {
+        return this.items.reduce(function(a,b) {
+        Math.max(a,b)
+        });
+      }
+    }
+
+    min() {
+      if (this.length == 0) {
+        throw new Error("EmptySortedList");
+      } else {
+        return this.items.reduce(function(a,b) {
+        Math.min(a,b)
+        });
+      }
+      
+    }
+    average() {
+      if (this.length == 0) {
+        throw new Error("EmptySortedList");
+      } else {
+        return this.items.reduce(function(a,b){
+          return a + b 
+        }, 0) / this.items.length;
+      }
+
+    }
+
+    sum() {
+      if (this.length == 0) {
+        return 0;
+      } else {
+        return this.items.reduce(function(a,b){
+          return a + b
+        }, 0);
+      }
+      
+    }
+  
+  
   };
   
   module.exports = SortedList;
