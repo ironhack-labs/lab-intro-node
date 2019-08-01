6e80c24146dc4d5b0ee4677654bd36f06da17dd3
Ant-Quanique Dancy <antdancy95@gmail.com>
2019-07-31 22:04:51 -0400

    added the constructor, get, add, max, and min methods

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,9 +1,37 @@
 class SortedList {
-    constructor() {}
-    add(item) {}
-    get(pos) {}
-    max() {}
-    min() {}
+    constructor() {
+      this.items = [];
+      this.length = 0;
+    }
+    add(item){
+       this.items.push(item);
+       this.length++;
+      
+    }
+    get(pos){
+      if(pos > this.length){
+        return new Error("The position is Out of Bounds")
+        
+      } else{
+        return this.items[pos];
+      }
+      
+    }
+    max(){
+      if(this.length === 0){
+        throw new Error("EmptySortedList")
+      } else {
+        return Math.max(...this.items)
+      }
+
+    }
+    min(){
+      if(this.length === 0){
+        throw new Error("EmptySortedList")
+      } else {
+        return Math.min(...this.items)
+      }
+    }
     average() {}
     sum() {}
   };
