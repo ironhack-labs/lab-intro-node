036a9c7859d2fdaca0d5bc13b3489613557e8519
Dusty Garon <dustygaron@hotmail.com>
2019-07-31 23:48:37 -0400

    in progress

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,47 @@
 class SortedList {
-    constructor() {}
-    add(item) {}
-    get(pos) {}
-    max() {}
-    min() {}
-    average() {}
-    sum() {}
-  };
-  
-  module.exports = SortedList;
+
+  constructor() {
+    this.items = []
+    this.length = 0
+  }
+
+  add(item) {
+    this.items.push(item)
+  }
+
+  get(pos) {
+    if (pos > this.length) {
+      throw new Error("OutOfBounds")
+    } else {
+      return this.items[pos]
+    }
+  }
+
+  max() {
+    if (this.items.length === 0) {
+      throw new Error("EmptySortedList")
+    } else {
+      // return this.items[this.length - 1]
+      return Math.max(...this.items)
+    }
+  }
+
+  min() {
+    if (this.items.length === 0) {
+      throw new Error("EmptySortedList")
+    } else {
+      return Math.min(...this.items)
+    }
+  }
+
+  average() {
+    return this.items.reduce((total, currentNum) => total + currentNum) / this.items.length
+  }
+
+  sum() {
+    return this.items.reduce((total, currentNum) => total + currentNum)
+  }
+
+};
+
+module.exports = SortedList;
