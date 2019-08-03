e8e2fdd4b2de15109cf8a17dcef8e76193449a25
Alejandro Salas <asalas.wd@outlook.com>
2019-08-02 22:40:53 -0400

    add method 'add' and 'get'

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,30 @@
 class SortedList {
-    constructor() {}
-    add(item) {}
-    get(pos) {}
+    constructor() {
+      this.items = [];
+      this.length = this.items.length;
+    }
+    add(item) {
+      this.items.push(item);
+      this.length = this.items.length;
+    }
+    get(pos) {
+      if (pos > this.length){
+        console.log('The selected position does not exist');
+      } else {
+      return this.items[pos];
+      }
+    }
     max() {}
     min() {}
     average() {}
     sum() {}
   };
+
+  let list = new SortedList();
+  list.add('x');
+  list.add('20')
+  let gotten = list.get(0);
+  console.log(gotten);
+  console.log(list);
   
   module.exports = SortedList;
