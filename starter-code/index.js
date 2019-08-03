3cfa22fba37ae67cbdaa1d00cf6e657c6a19bd55
Randolph Amaya <randolphjr7@gmail.com>
2019-08-02 23:02:58 -0400

    done

--- starter-code/index.js
+++ starter-code/index.js
@@ -5,17 +5,19 @@ class SortedList {
       this.length = 0;
     }
 
+    // add a single value to array, then sort
     add(item) {
       this.items.push(item);
       this.items.sort((a,b) => a-b);
       this.length ++;
     }
 
+    // get number at position index of array
     get(pos) {
       return this.items[pos];
     }
 
-
+    // get biggest number
     max() {
       if(this.length ===0){
         throw new Error("EmptySortedList");
@@ -24,6 +26,7 @@ class SortedList {
       return big;
     }
 
+    // get smallest number
     min() {
       if(this.length === 0){
         throw new Error("EmptySortedList");
@@ -31,6 +34,7 @@ class SortedList {
       return this.items[0];
     }
 
+    // get average; calls sum then divide by length of array
     average() {
       if(this.length === 0){
         throw new Error("EmptySortedList");
@@ -38,12 +42,11 @@ class SortedList {
       return this.sum()/this.length
     }
 
+    // get sum
     sum() {
-
       if(this.length === 0){
         return 0;
       }
-
       return this.items.reduce((a, b) => a + b, 0);
     }
 
