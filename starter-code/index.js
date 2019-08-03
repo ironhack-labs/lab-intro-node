cc6963f271d573d8b6226292a704fb02e068d560
Randolph Amaya <randolphjr7@gmail.com>
2019-08-02 22:56:31 -0400

    MIA PTWD Randolph Amaya

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,52 @@
 class SortedList {
-    constructor() {}
-    add(item) {}
-    get(pos) {}
-    max() {}
-    min() {}
-    average() {}
-    sum() {}
+
+    constructor() {
+      this.items = [];
+      this.length = 0;
+    }
+
+    add(item) {
+      this.items.push(item);
+      this.items.sort((a,b) => a-b);
+      this.length ++;
+    }
+
+    get(pos) {
+      return this.items[pos];
+    }
+
+
+    max() {
+      if(this.length ===0){
+        throw new Error("EmptySortedList");
+      }
+      let big = Math.max(...this.items);
+      return big;
+    }
+
+    min() {
+      if(this.length === 0){
+        throw new Error("EmptySortedList");
+      }
+      return this.items[0];
+    }
+
+    average() {
+      if(this.length === 0){
+        throw new Error("EmptySortedList");
+      }
+      return this.sum()/this.length
+    }
+
+    sum() {
+
+      if(this.length === 0){
+        return 0;
+      }
+
+      return this.items.reduce((a, b) => a + b, 0);
+    }
+
   };
   
   module.exports = SortedList;
