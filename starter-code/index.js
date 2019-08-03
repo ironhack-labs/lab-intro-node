93e51425298e250b9408969c78f4e0691acdef67
Alejandro Salas <asalas.wd@outlook.com>
2019-08-02 23:30:06 -0400

    complete all methods and pass test

--- starter-code/index.js
+++ starter-code/index.js
@@ -5,26 +5,68 @@ class SortedList {
     }
     add(item) {
       this.items.push(item);
+      this.items.sort((a,b) => { 
+        return a - b
+      });
       this.length = this.items.length;
     }
     get(pos) {
-      if (pos > this.length){
-        console.log('The selected position does not exist');
+      if (pos >= this.length){
+        throw new Error("OutOfBounds");
       } else {
       return this.items[pos];
       }
     }
-    max() {}
-    min() {}
-    average() {}
-    sum() {}
+    max() {
+      if (this.length > 0){
+        let theMax = 0;
+        this.items.forEach((item) => {
+          if (item > theMax){
+            theMax = item;
+          }          
+        });
+        return theMax;
+      } else {
+        throw new Error("EmptySortedList");
+      }
+    }
+    min() {
+      if (this.length > 0){
+        let theMin = this.items[0];
+        this.items.forEach((item) => {
+          if (item < theMin){
+            theMin = item;
+          }          
+        });
+        return theMin;
+      } else {
+        throw new Error("EmptySortedList");
+      }
+    }
+    average() {
+      let theAverage;
+      if (this.length === 0){
+        throw new Error("EmptySortedList");
+      } else {
+        let theSum = this.items.reduce((accumulator,currentValue) => {
+          return accumulator + currentValue;
+        });
+        theAverage = theSum/this.length;
+      }
+      return theAverage;
+    }
+    sum() {
+      let theSum;
+      if (this.length === 0){
+        theSum = 0;
+      } else {
+        theSum = this.items.reduce((accumulator,currentValue) => {
+          return accumulator + currentValue;
+        });
+        }
+      return theSum;  
+    }
   };
 
-  let list = new SortedList();
-  list.add('x');
-  list.add('20')
-  let gotten = list.get(0);
-  console.log(gotten);
-  console.log(list);
   
   module.exports = SortedList;
