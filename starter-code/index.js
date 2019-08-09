612f77447c7ea22cf387e60ba38efa90567a11ef
danyamashita <dan.yamashita@gmail.com>
2019-08-08 22:31:02 -0300

    initial commit

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,54 @@
 class SortedList {
-    constructor() {}
-    add(item) {}
-    get(pos) {}
-    max() {}
-    min() {}
-    average() {}
-    sum() {}
+    constructor() {
+      this.items = [],
+      this.length = this.items.length
+    }
+    add(item) {
+      this.items.push(item)
+      this.length += 1
+      this.items.sort((a, b)=>a-b)
+    }
+    get(pos) {
+      if(pos > this.length){
+        return 'OutOfBounds'
+      }
+      return this.items[pos]
+    }
+    max() {
+      if(this.length===0){
+        throw new Error("EmptySortedList")
+      }
+      return this.items[this.items.length-1]
+    }
+    min() {
+      if(this.length===0){
+        throw new Error("EmptySortedList")
+      }
+      return this.items[0]
+    }
+    sum() {
+      if(this.length===0){
+        return 0
+      }
+      return this.items.reduce((acumulator, element) => acumulator + element)
+    }
+    average() {
+      if(!this.length){
+        throw new Error("EmptySortedList")
+      }
+      return this.sum()/this.length
+    }
   };
   
   module.exports = SortedList;
+
+  const teste = new SortedList();
+  console.log(teste.items)
+  teste.add(10)
+  console.log(teste.items)
+  teste.add(30)
+  teste.add(80)
+  console.log(teste.items)
+  console.log(teste.average())
+
+  // console.log(teste.max())
