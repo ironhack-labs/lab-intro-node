9fc321e36d06e60bc95f96d3d1508cb0314a6adf
Fernando Yto <fernando.toshioyto@gmail.com>
2019-08-08 22:35:50 -0300

    lab done

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,49 @@
 class SortedList {
-    constructor() {}
-    add(item) {}
-    get(pos) {}
-    max() {}
-    min() {}
-    average() {}
-    sum() {}
-  };
-  
-  module.exports = SortedList;
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
+
+  add(item) {
+    this.items.push(item);
+    this.length += 1;
+    this.items.sort((a, b) => a - b);
+  }
+
+  get(pos) {
+    return this.items[pos];
+  }
+
+  max() {
+    if (this.length === 0) {
+      throw new Error('EmptySortedList');
+    } else {
+      return this.items[this.length - 1];
+    }
+  }
+
+  min() {
+    if (this.length === 0) {
+      throw new Error('EmptySortedList');
+    } else {
+      return this.items[0];
+    }
+  }
+
+  average() {
+    if (this.length === 0) {
+      throw new Error('EmptySortedList');
+    }
+    // eslint-disable-next-line max-len
+    return this.items.reduce((accumulator, currentValue) => accumulator + currentValue, 0) / this.length;
+  }
+
+  sum() {
+    if (this.length === 0) {
+      return 0;
+    }
+    return this.items.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+  }
+}
+
+module.exports = SortedList;
