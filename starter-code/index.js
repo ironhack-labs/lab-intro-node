c89c366328c8165e2a3be5f7a29242ad711e7604
Roberto Camacho <camachof.roberto@gmail.com>
2019-08-09 12:54:39 -0300

    -Done

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,44 @@
 class SortedList {
-    constructor() {}
-    add(item) {}
-    get(pos) {}
-    max() {}
-    min() {}
-    average() {}
-    sum() {}
+    constructor() {
+      this.items = [];
+      this.length = this.items.length;
+    }
+    add(item) {
+      this.items.push(item);
+      this.length = this.items.length;
+      this.items.sort((a,b) => a - b);
+    }
+    get(pos) {
+      if(pos > this.items.length){
+        throw new Error("OutOfBounds");
+      }else{
+      return this.items[pos];
+      }
+    }
+    max() {
+      if(this.length == 0){
+        throw new Error("EmptySortedList");
+      }else{
+        return Math.max(...this.items)
+      }
+    }
+    min() {
+      if(this.length == 0){
+        throw new Error("EmptySortedList");
+      }else{
+        return Math.min(...this.items)
+      }
+    }
+    average() {
+      if(this.length == 0){
+        throw new Error("EmptySortedList");
+      }else{
+      return this.sum()/this.length
+      }
+    }
+    sum() {
+      return this.length == 0 ? 0 : this.items.reduce((accum, curr) => accum + curr)
+    }
   };
   
   module.exports = SortedList;
