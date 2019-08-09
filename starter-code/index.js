59b0a02b804000af2d2e5aae0d843888b633f933
Rafael Bueno <rafaelbuenolink@gmail.com>
2019-08-08 22:31:41 -0300

    done

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,56 @@
 class SortedList {
-    constructor() {}
-    add(item) {}
-    get(pos) {}
-    max() {}
-    min() {}
-    average() {}
-    sum() {}
-  };
-  
-  module.exports = SortedList;
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
+
+  add(x) {
+    this.items.push(x);
+    this.items.sort((a, b) => a - b);
+    this.length = this.items.length;
+  }
+  get(pos) {
+    return this.items[pos];
+  }
+  max() {
+    if (this.items.length === 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      return Math.max(...this.items);
+    }
+  }
+  min() {
+    if (this.items.length === 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      return Math.min(...this.items);
+    }
+  }
+  average() {
+    if (this.items.length === 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      const sumArr = this.items.reduce((acc, el) => {
+        acc += el;
+        return acc;
+      }, 0);
+      return sumArr / this.items.length;
+    }
+  }
+  sum() {
+    if (this.items.length === 0) {
+      return 0;
+    } else {
+      const sumArr = this.items.reduce((acc, el) => {
+        acc += el;
+        return acc;
+      }, 0);
+
+      return sumArr;
+    }
+  }
+}
+
+module.exports = SortedList;
+
+theSortedList = new SortedList();
