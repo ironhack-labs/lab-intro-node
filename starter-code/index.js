94da17b7eead69243d54e6a6a7fa192da75f6a0d
Pedro Chiossi <pchiossi@gmail.com>
2019-08-08 22:01:25 -0300

    finished lab

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,53 @@
 class SortedList {
-    constructor() {}
-    add(item) {}
-    get(pos) {}
-    max() {}
-    min() {}
-    average() {}
-    sum() {}
-  };
-  
-  module.exports = SortedList;
+
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
+
+  add(x) {
+    this.items.push(x);
+    this.items.sort((a, b) => a - b);
+    this.length = this.items.length;
+  }
+
+  get(pos) {
+    try {
+      return this.items[pos];
+    } catch (error) {
+      return `Error: ${error} `;
+    }
+  }
+
+  max() {
+    if (this.length === 0) {
+      throw new Error('EmptySortedList');
+    }
+    return Math.max(...this.items);
+  }
+
+  min() {
+    if (this.length === 0) {
+      throw new Error('EmptySortedList');
+    }
+    console.log(...this.items);
+    return Math.min(...this.items);
+  }
+
+  average() {
+    if (this.length === 0) {
+      throw new Error('EmptySortedList');
+    }
+    return this.sum() / this.length;
+  }
+
+  sum() {
+    if (this.length === 0) {
+      return 0;
+    }
+    return this.items.reduce((sum, value) => sum + value);
+  }
+}
+
+sl = new SortedList();
+module.exports = SortedList;
