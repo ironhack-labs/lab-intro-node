ed3a9d352d44c3b2770fa047570ce9907726671d
luanperosa <luan.perosa@hotmail.com>
2019-08-08 22:35:20 -0300

    testado até o teste de add() com get()

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,8 +1,25 @@
 class SortedList {
-    constructor() {}
-    add(item) {}
-    get(pos) {}
-    max() {}
+    constructor(items, length) {
+      this.items = [];
+      this.length = this.items.length;
+    }
+    add(item) {
+      this.items.push(item);
+      this.length = this.items.length;
+      
+    }
+    get(pos) {
+      if(pos > this.items.length) {
+        throw new Error('OutOfBounds');
+      }
+      return this.items[(pos)];
+    }
+    max() {
+      if (this.items == ''){
+        throw new Error("EmptySortedList");
+      }
+      return this.items.slice(-1);
+    }
     min() {}
     average() {}
     sum() {}
