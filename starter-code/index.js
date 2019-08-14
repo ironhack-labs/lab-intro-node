fbc8ccd19d8bdd2c14af09fbfed1428e74b5f333
Jose Delacruz <josedelacruz@Joses-iMac.attlocal.net>
2019-08-13 22:20:57 -0400

    done

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,43 @@
 class SortedList {
-    constructor() {}
-    add(item) {}
-    get(pos) {}
-    max() {}
-    min() {}
-    average() {}
-    sum() {}
+    constructor() {
+      this.items = [];
+      this.length = this.items.length;
+    }
+    add(item) {
+      this.items.push(item);
+      this.items.sort((a, b) => a - b);
+      this.length ++;
+    }
+    get(pos) {
+      if(!this.length || pos > this.length){
+        throw Error("OutOfBounds");
+      }
+      return this.items[pos];
+    }
+    max() {
+      if(!this.length){
+        throw Error("EmptySortedList");
+      }
+      return Math.max(... this.items);
+    }
+    min() {
+      if(!this.length){
+        throw Error("EmptySortedList");
+      }
+      return this.items[0];
+    }
+    average() {
+      if(!this.length){
+        throw Error("EmptySortedList");
+      }
+      return this.sum()/this.length;
+    }
+    sum() {
+      if(!this.length){
+        return 0;
+      }
+      return this.items.reduce((total, currentValue) => total + currentValue );
+    }
   };
   
   module.exports = SortedList;
