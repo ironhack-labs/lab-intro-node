918f3e3d66ce1afb7dd4288f03b5dc03397e6993
Elena Piaggio <elenapiaggio@gmail.com>
2019-08-18 13:33:59 +0200

    iniital commit

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,6 +1,13 @@
 class SortedList {
-    constructor() {}
-    add(item) {}
+    constructor(items = []) {
+      this.items = items;
+      this.length = this.items.length;
+
+    }
+    add(item) {
+      this.items.push(item);
+      this.length = this.items.length;
+    }
     get(pos) {}
     max() {}
     min() {}
