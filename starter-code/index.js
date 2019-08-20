37623155f414d254431d7d746c360ab173fa7c80
Dayana Mick <dayanamick@gmail.com>
2019-08-20 21:30:14 +0200

    done

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,46 @@
 class SortedList {
-    constructor() {}
-    add(item) {}
-    get(pos) {}
-    max() {}
-    min() {}
-    average() {}
-    sum() {}
-  };
-  
-  module.exports = SortedList;
+  constructor() {
+    this.items = []
+    this.length = 0
+  }
+  add(item) {
+    this.items.push(item)
+    this.length++
+    this.items.sort((a, b) => a - b)
+  }
+
+  get(pos) {
+    return this.items[pos]
+  }
+  max() {
+    if (this.items.length === 0) {
+      throw new Error("EmptySortedList")
+    } else
+      return this.items[this.items.length - 1]
+  }
+  min() {
+    if (this.items.length === 0) {
+      throw new Error("EmptySortedList")
+    } else
+      return this.items[0]
+  }
+  average() {
+    if (this.items.length === 0) {
+      throw new Error("EmptySortedList")
+    } else {
+      const reducer = (accumulator, currentValue) => accumulator + currentValue;
+      return this.items.reduce(reducer) / this.items.length
+    }
+  }
+
+  sum() {
+    if (this.items.length === 0) {
+      return 0
+    } else {
+      const reducer = (accumulator, currentValue) => accumulator + currentValue;
+      return this.items.reduce(reducer)
+    }
+  }
+};
+
+module.exports = SortedList;
