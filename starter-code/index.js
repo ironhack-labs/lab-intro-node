2ebba247f0f3411bfefb5e9b97465d3c64c79bb4
fairouz <fairouzkoehlerwest@gmail.com>
2019-08-21 02:04:30 +0200

    first commit

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,57 @@
 class SortedList {
-    constructor() {}
-    add(item) {}
-    get(pos) {}
-    max() {}
-    min() {}
-    average() {}
-    sum() {}
-  };
-  
-  module.exports = SortedList;
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
+
+
+  add(item) {
+    this.items.push(item);
+    this.length++;
+
+    this.items.sort((a, b) => a - b);
+  }
+
+  get(pos) {
+    return this.items[pos];
+  }
+
+  max() {
+    if (this.length === 0) {
+      return 0 //??
+    } else {
+      return this.items[this.items.length - 1];
+    }
+  }
+
+  min() {
+    if (this.length === 0) {
+      return 0 //??
+    } else {
+      return this.items[0];
+    }
+  }
+
+  average() {
+    if (this.length === 0) {
+      return 0 //??
+    } else {
+      let total = (acc, curr) => acc + curr;
+      return this.items.reduce(total) / this.length;
+
+    }
+  }
+
+  sum() {
+    if (this.length === 0) {
+      return 0
+    } else {
+      let total = (acc, curr) => acc + curr;
+      return this.items.reduce(total);
+
+    }
+
+  }
+};
+
+module.exports = SortedList;
