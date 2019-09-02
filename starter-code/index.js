88ddc3ff91179a7d60bfecc2fa49f5f6a07ceb75
Tiago Silva <tiagosilva94@outlook.com>
2019-09-02 18:17:09 +0100

    Add lab finished

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,78 @@
 class SortedList {
-    constructor() {}
-    add(item) {}
-    get(pos) {}
-    max() {}
-    min() {}
-    average() {}
-    sum() {}
-  };
-  
+    constructor(item, length) {
+      this.item = [];
+      this.length = this.item.length;
+    }
+
+    add(x) {
+      this.item.push(x);
+      this.item.sort((a, b) => {
+        if(a>b){
+          return 1;
+        }else if(a<b){
+          return -1;
+        }else{
+          return 0;
+        }
+      });
+      this.length = this.item.length;
+    }
+
+    get(pos) {
+      if(pos<=this.item.length){
+        return this.item[pos];
+      }
+    }
+    
+    max() {
+      let max=-Infinity;
+      if(this.item.length!==0){
+        for(let i=0;i<this.item.length;i++){
+          if(this.item[i]>max?max=this.item[i]:max);
+        }
+        return max;
+      }else{
+        throw new Error("EmptySortedList");
+      }
+    }
+
+    min() {
+      let min=+Infinity;
+      if(this.item.length!==0){
+        for(let i=0;i<this.item.length;i++){
+          if(this.item[i]<min?min=this.item[i]:min);
+        }
+        return min;
+      }else{
+        throw new Error("EmptySortedList");
+      }
+    }
+    
+  average() {
+    let avg = 0; 
+    let sum = 0;
+    if(this.item.length!==0){
+      for (let i = 0; i < this.item.length; i++) {
+        sum+=this.item[i];
+      }
+      avg = sum / this.item.length;
+      return avg;
+    }else{
+      throw new Error("EmptySortedList");
+    }
+  }
+
+  sum() {
+    let sum=0;
+      if(this.item.length!==0){
+        for(let i=0;i<this.item.length;i++){
+          sum+=this.item[i];
+        }
+        return sum;
+      }else{
+        return 0;
+      }
+    }
+}
   module.exports = SortedList;
+
