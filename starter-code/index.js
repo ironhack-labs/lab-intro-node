fae8e54745faa2863613e61e60b3a21c98916df8
Sonia Meller <mellersonia@gmail.com>
2019-09-02 18:12:04 +0100

    done

--- starter-code/index.js
+++ starter-code/index.js
@@ -5,32 +5,52 @@ class SortedList {
   }
   add(item) {
     this.items.push(item);
-      this.items.sort((a,b) => a-b);
+    this.items.sort((a, b) => a - b);
     this.length = this.items.length;
   }
   get(pos) {
-    if (pos > this.items.lenght || pos < -1) throw new Error("OutOfBounds");
-    else {
-      this.items.sort((a,b) => a-b);
+    if (pos > this.items.lenght || pos < -1) {
+      throw new Error("OutOfBounds");
+    } else {
+      this.items.sort((a, b) => a - b);
       return this.items[pos];
     }
   }
+
   max() {
-    if (this.items.length === 0) throw new Error("EmptySortedList")
-    else return Math.max(...this.items)
+    if (this.items.length === 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      return this.items.reduce((a, b) => {
+        if (typeof a === "undefined") return b
+        else return a > b ? a : b;
+      });
+    }
+    // --- Using rest spread operator ---
+    // else return Math.max(...this.items);
   }
 
   min() {
-    if (this.items.length === 0) throw new Error("EmptySortedList")
-    else return Math.min(...this.items)
+    if (this.items.length === 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      return this.items.reduce((a, b) => {
+        if (typeof a === "undefined") return b
+        else return a < b ? a : b;
+      });
+    }
+    // --- Using rest spread operator ---
+    // else return Math.min(...this.items);
   }
+
   sum() {
-    if (this.items.length === 0) return 0
-    else return this.items.reduce((a,b)=> a+b )
+    if (this.items.length === 0) return 0;
+    else return this.items.reduce((a, b) => a + b);
   }
+
   average() {
-    if (this.items.length === 0) throw new Error("EmptySortedList")
-    else return this.sum()/this.items.length;
+    if (this.items.length === 0) throw new Error("EmptySortedList");
+    else return this.sum() / this.items.length;
   }
 }
 
