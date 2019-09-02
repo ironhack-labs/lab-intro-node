6709b4eb8d6059873bd8cc44c4e5ed4ef1381b8f
Sam Ettinger <ettingersam@gmail.com>
2019-09-02 16:55:44 +0100

    All tests passing

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,48 @@
 class SortedList {
-    constructor() {}
-    add(item) {}
-    get(pos) {}
-    max() {}
-    min() {}
-    average() {}
-    sum() {}
-  };
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
+
+  sortfunc(x, y) {
+    return x-y;
+  }
+
+  add(item) {
+    // Push item to end of this.items
+    this.items.push(item);
+    // Sort this.items
+    this.items.sort(this.sortfunc);
+    // Increment this.length
+    this.length = this.items.length;
+  }
+
+  get(pos) {
+    return this.items[pos];
+  }
+
+  max() {
+    if (this.length === 0) throw new Error("EmptySortedList");
+    else return this.items[this.length-1];
+  }
+
+  min() {
+    if (this.length === 0) throw new Error("EmptySortedList");
+    else return this.items[0];
+  }
   
-  module.exports = SortedList;
+  sum() {
+    let sum = 0;
+    for (let i=0; i<this.length; i++) {
+      sum += this.items[i];
+    }
+    return sum;
+  }
+
+  average() {
+    if (this.length === 0) throw new Error("EmptySortedList");
+    else return this.sum()/this.length;
+  }
+};
+
+module.exports = SortedList;
