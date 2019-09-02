37036a7f690ff5490137765225764c3fe4fa6d17
Brandy McArthur <bdm.mcarthur@gmail.com>
2019-09-02 17:44:28 +0100

    Completed exercise

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,62 @@
 class SortedList {
-    constructor() {}
-    add(item) {}
-    get(pos) {}
-    max() {}
-    min() {}
-    average() {}
-    sum() {}
-  };
+    constructor() {
+      this.items = [];
+      this.length = this.items.length;
+    }
+    add(item) {
+      this.items.push(item);
+      this.length = this.items.length
+      this.items.sort(function (a, b) {
+        if (a > b) {
+          return 1;
+        } else if (a < b) {
+          return -1;
+        } else {
+          return 0;
+        }
+      });
+    }
+
+    get(pos) {
+      return this.items[pos]
+    }
+
+    max() {
+      if (this.length === 0){
+        throw new Error("EmptySortedList")
+      }
+
+      else {
+        return this.items[this.length-1]
+      }
+    }
+    min() {
+      if (this.length === 0){
+        throw new Error("EmptySortedList")
+      }
+
+      else {
+        return this.items[0]
+      }
+    }
+    sum() {
+      if(this.length === 0){
+        return 0
+      }
+      else {
+        return this.items.reduce(function(a,b){
+          return a + b
+        }, 0);
+      }
+    }
+    average() {
+      if(this.length === 0){
+        throw new Error("EmptySortedList")
+      }
+      else {
+        return this.items.reduce((a,b) => a + b, 0) / this.length
+      }
+    }
+  }
   
   module.exports = SortedList;
