f45ce48a4da47d1869e2ab32f8558ec3621e119b
Prelhaz <54009534+Prelhaz@users.noreply.github.com>
2019-09-02 18:03:51 +0100

    Filipe and Guilherme Lab finished

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,54 @@
 class SortedList {
-    constructor() {}
-    add(item) {}
-    get(pos) {}
-    max() {}
-    min() {}
-    average() {}
-    sum() {}
+    constructor(length, items) {
+      this.items = [];
+      this.length = this.items.length;
+    }
+
+    add(item) {
+      this.items.push(item);
+      this.items.sort((a,b) => a > b ? 1 : a < b ? -1 : 0);
+      this.length = this.items.length;
+    }
+
+    get(pos) {
+      return this.items[pos];
+    }
+
+    max() {
+      if (this.items.length === 0) {
+        throw new Error("EmptySortedList");
+      } else {
+        return this.items[this.length-1];
+      }
+    }
+
+    min() {
+      if (this.items.length === 0) {
+        throw new Error("EmptySortedList");
+      } else {
+        return this.items[0];
+      }
+    }
+
+    average() {
+      if (this.items.length === 0) {
+        throw new Error("EmptySortedList");
+      } else {
+        let average = this.sum()/this.length;
+        return average;
+      }
+    }
+
+    sum() {
+      if (this.items.length === 0) {
+        return 0;
+      } else {
+        let sum = 0;
+        for (let i = 0; i < this.length; i++) {
+          sum = sum + this.items[i];
+        }
+        return sum;
+    }
   };
-  
-  module.exports = SortedList;
+}
+ module.exports = SortedList;
