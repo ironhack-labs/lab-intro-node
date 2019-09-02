df4d96f0eff2259540e16bf159e553f7785466b8
Paula Brinkhege <paula.brinkhege@gmail.com>
2019-09-02 18:43:13 +0100

    first draft pair programming with daniel s.

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,57 @@
 class SortedList {
-    constructor() {}
-    add(item) {}
-    get(pos) {}
-    max() {}
-    min() {}
-    average() {}
-    sum() {}
+    constructor(item, pos) {
+      this.items = [];
+      this.length = this.items.length;
+
+    }
+    add(item) {
+      this.items.push(item);
+      this.items.sort(function(a, b){return a > b ? 1 : a < b ? -1 : 0});
+      this.length = this.items.length; 
+    }
+
+    get(pos) {
+      return this.items[pos]
+    }
+
+    max() {
+      if (this.items.length === 0) {
+        throw new Error("EmptySortedList");
+      } else {
+        return this.items[this.items.length -1]
+      }
+    }
+
+    min() {
+      if (this.items.length === 0) {
+        throw new Error("EmptySortedList");
+      } else {
+        return this.items[0]
+      }
+    }
+
+    sum() {
+      if (this.items.length === 0) {
+        return 0;
+      } else {
+        return this.items.reduce(function(accumulator, currentValue) {
+          return accumulator + currentValue;
+        }) ;
+      }
+
+    }
+
+    average() {
+      if (this.items.length === 0) {
+        throw new Error("EmptySortedList")
+      } else {
+        return this.sum() /this.items.length  
+      }
+    }
+
   };
-  
+
   module.exports = SortedList;
+
+
+  
