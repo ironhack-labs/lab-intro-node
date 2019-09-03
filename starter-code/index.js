1132c47cb0d8ae6d3f323c435a3d487b2ff9ccf9
Ali Kucukavci <ali91kavci@outlook.dk>
2019-09-03 11:49:02 +0200

    First commit

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,41 @@
 class SortedList {
-    constructor() {}
-    add(item) {}
-    get(pos) {}
-    max() {}
-    min() {}
-    average() {}
-    sum() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+    }
+  add(x) {
+    this.items.push(x)
+    this.items.sort((a,b)=>a-b);
+    this.length = this.items.length;
+  }
+  
+  get(pos) {
+    return this.items[pos]
+    
+    }
+  max() {
+    if (this.items.length ===0){ throw new Error("EmptySortedList") }
+      return Math.max(...this.items)
+    }
+  min() {
+    if (this.items.length ===0){ throw new Error("EmptySortedList") }
+    return Math.min(...this.items)
+    }
+  average() {
+    if (this.items.length === 0) { throw new Error("EmptySortedList") }
+    let sum = this.items.reduce((a, b) => { return a + b })
+    let aver = sum / this.items.length
+    return aver
+    // if (this.items.length ===0){ return 0 }
+    // return this.items.reduce((a, b) => {return a +b})
+    }
+  sum() {
+    if (this.items.length ===0 ){ return 0 }
+    return this.items.reduce((a, b) => {return a +b})
+    }
   };
   
+
+
+
   module.exports = SortedList;
