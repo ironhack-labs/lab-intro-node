70d9c705e456a60902a896a30f6fa215a2fbb35c
Ninette <ninette.adhikari@gmail.com>
2019-09-03 11:23:54 +0200

    done

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,55 @@
 class SortedList {
-    constructor() {}
-    add(item) {}
-    get(pos) {}
-    max() {}
-    min() {}
-    average() {}
-    sum() {}
-  };
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
+
+sort(a,b){
+return a-b;
+}
+
+  add(item) {
+    this.items.push(item)
+    this.items.sort(this.sort)
+    this.length=this.items.length
+
+  }
+  get(pos) {
+    return this.items[pos];
+  }
+  max() {
+    if(this.length<=0){
+      throw new Error("EmptySortedList")
+    }else{
+    return Math.max(...this.items);
+    }
+
+    // try {
+    //   return Math.max(this.items);
+    // } catch (this.items<=0) {
+    //   throw new Error("EmptySortedList")
+    // }
+  }
+  min() {
+    if(this.length<=0){
+      throw new Error("EmptySortedList")
+    }else{
+    return Math.min(...this.items);
+    }
+  }
   
-  module.exports = SortedList;
+  sum() {
+    return this.items.reduce(function(acc, value)
+    {return acc+value
+    },0)
+}
+average() {
+  if(this.length<=0){
+    throw new Error("EmptySortedList")
+  }else{
+ return this.sum()/this.length
+  }
+}
+}
+
+module.exports = SortedList;
