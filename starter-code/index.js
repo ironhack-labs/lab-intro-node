39a05e3df210678dd889e8ab94fa14404a77ed6d
Keighley McFarland <mcfarland.ke@gmail.com>
2019-09-03 11:21:30 +0200

    done

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,37 @@
 class SortedList {
-    constructor() {}
-    add(item) {}
-    get(pos) {}
-    max() {}
-    min() {}
-    average() {}
-    sum() {}
-  };
-  
-  module.exports = SortedList;
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a, b) => a - b);
+    this.length = this.items.length;
+  }
+  get(pos) {
+    return this.items[pos];
+  }
+  max() {
+    if (!this.length) throw new Error("EmptySortedList");
+    return this.items[this.length - 1];
+  }
+  min() {
+    if (!this.length) throw new Error("EmptySortedList");
+    return this.items[0];
+  }
+  average() {
+    if (!this.length) throw new Error("EmptySortedList");
+    const sum = this.items.reduce((accumulator, value) => {
+      return accumulator + value;
+    }, 0);
+    return sum / this.items.length;
+  }
+  sum() {
+    const sum = this.items.reduce((accumulator, value) => {
+      return accumulator + value;
+    }, 0);
+    return sum;
+  }
+}
+
+module.exports = SortedList;
