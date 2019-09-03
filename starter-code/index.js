947cea4c050569106a3b56c2f8c853837dc7d7ea
Noriko Takizawa <norikotakizawa@Norikos-MacBook-Pro.local>
2019-09-03 12:08:32 +0200

    done

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,45 @@
 class SortedList {
-    constructor() {}
-    add(item) {}
-    get(pos) {}
-    max() {}
-    min() {}
-    average() {}
-    sum() {}
+  constructor(items, length) {
+    this.items = [];
+    this.length = this.items.length;
+
+  }
+  add(item) {
+    this.items.push(item);
+    this.length = this.items.length;
+
+    this.items.sort((a, b) => {
+      return a - b
+    });
+  }
+  get(pos) {
+    // if (pos > this.items.length)
+    //   throw new Error('OutOfBounds');
+    return this.items[pos];
+
+  }
+  max() {
+    if (this.items.length === 0)
+      throw new Error("EmptySortedList");
+    return this.items[this.items.length - 1];
+  }
+  min() {
+    if (this.items.length === 0)
+      throw new Error("EmptySortedList");
+    return this.items[0];
+  }
+  sum() {
+    if (this.items.length === 0) return 0;
+    return this.items.reduce((acc, el) => acc + el)
   };
-  
-  module.exports = SortedList;
+
+  average() {
+    if (this.items.length === 0)
+      throw new Error("EmptySortedList");
+    return this.sum() / this.items.length;
+
+  }
+
+};
+
+module.exports = SortedList;
