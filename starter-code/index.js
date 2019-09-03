1c98f728d1377e5db3b9d9d62f2676c95daaf839
KaterynaStadnyk <kateryna.stad@gmail.com>
2019-09-03 13:43:02 +0200

    done

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,35 @@
 class SortedList {
-    constructor() {}
-    add(item) {}
-    get(pos) {}
-    max() {}
-    min() {}
-    average() {}
-    sum() {}
+    constructor() {
+      this.items = [];
+      this.length = this.items.length;
+    }
+    add(x) {
+      this.items.push(x);
+      this.items.sort((a,b)=>a-b);
+      this.length = this.items.length;
+    }
+    get(pos) {
+      return this.items[pos]; 
+    }
+    max() {
+      if (this.items.length === 0) throw new Error("EmptySortedList"); 
+      return this.items[this.length-1]; 
+    }
+    min() {
+      if (this.items.length === 0) throw new Error("EmptySortedList"); 
+      return this.items[0]; 
+    }
+    average() {
+      if(this.items.length === 0) throw new Error("EmptySortedList");
+      return this.items.reduce((accumulator, currentValue) => accumulator + currentValue, 0) / this.items.length;
+    }
+    sum() {
+      if(this.items.length === 0) return 0; 
+      return this.items.reduce((accumulator, currentValue) => accumulator + currentValue, 0)
+    }
   };
   
   module.exports = SortedList;
+  let sl = new SortedList(); 
+  //sl.add("wasser"); 
+  
