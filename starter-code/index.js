6b1bb3096d35dee0c7c6fe5097bab4802f9eb956
Jan Redmann <janredmann1@gmail.com>
2019-09-03 11:43:51 +0200

    done

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,44 @@
 class SortedList {
-    constructor() {}
-    add(item) {}
-    get(pos) {}
-    max() {}
-    min() {}
-    average() {}
-    sum() {}
+    constructor() {
+      this.length = 0;
+      this.items = []; 
+    }
+    add(item) {
+      this.items.push(item);
+      this.items.sort((a, b) => a - b);
+      this.length++;
+    }
+    get (pos) {
+      if (this.items[pos - 1] === null) {
+        throw new Error("Out of bounds");
+      }
+      return this.items[pos];
+    }
+    max() {
+      this.isListEmpty();
+      return Math.max(...this.items);
+    }
+    min() {
+      this.isListEmpty();
+      return Math.min(...this.items);
+    }
+    average() {
+      this.isListEmpty();
+      return this.sum() / this.length; 
+    }
+    sum() {
+      return this.items.reduce((a, b) => a + b, 0);
+    }
+    isListEmpty() {
+      if (this.items.length === 0) {
+        throw new Error("EmptySortedList");
+      }
+    }
   };
+
+  const sl = new SortedList();
+  console.log(sl.length);
+  sl.add("a");
+  console.log(sl.get(0));
   
   module.exports = SortedList;
