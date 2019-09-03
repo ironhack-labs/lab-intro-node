3e813edf7a8398092ea0cf7f01db062050a431e2
SarahHouben <sarahhouben@outlook.com>
2019-09-03 11:49:06 +0200

    done

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,46 @@
 class SortedList {
-    constructor() {}
-    add(item) {}
-    get(pos) {}
-    max() {}
-    min() {}
-    average() {}
-    sum() {}
-  };
-  
-  module.exports = SortedList;
+  constructor() {
+    this.items = [];
+    this.length = this.items.length
+  }
+  add(item) {
+    this.items.push(item)
+    this.length = this.items.length
+    this.items.sort((a, b) => a - b)
+  }
+  get(pos) {
+    return this.items[pos]
+  }
+  max() {
+    if (this.length === 0) {
+      throw new Error("EmptySortedList")
+    } else {
+      return Math.max(...this.items)
+    }
+  }
+  min() {
+    if (this.length === 0) {
+      throw Error("EmptySortedList")
+    } else {
+      return Math.min(...this.items)
+    }
+  }
+  average() {
+    if (this.length === 0) {
+      throw Error("EmptySortedList")
+    } else {
+      return this.sum() / this.length
+    }
+  }
+  sum() {
+    if (this.length === 0) {
+      return 0
+    } else {
+      return this.items.reduce(function (accumulator, value) {
+        return accumulator + value;
+      }, 0)
+    }
+  }
+};
+
+module.exports = SortedList;
