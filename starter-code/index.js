e79c1afe4ec7211c4da6ae003335c4c6035f106e
Harinath Vutla <harinathvutla428@gmail.com>
2019-09-03 13:52:52 +0200

    first commit

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,32 @@
 class SortedList {
-    constructor() {}
-    add(item) {}
-    get(pos) {}
-    max() {}
-    min() {}
-    average() {}
-    sum() {}
-  };
-  
-  module.exports = SortedList;
+  constructor(items, length) {
+    this.items = [];
+    this.length = 0;
+  }
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a, b) => a - b);
+    this.length = this.items.length;
+  }
+  get(pos) {
+    if (pos > this.items.length - 1 || pos < 0) throw new Error("OutOfBounds");
+    else return this.items[pos];
+  }
+  max() {
+    if (this.items.length === 0) throw new Error("EmptySortedList");
+    else return this.items[this.length - 1];
+  }
+  min() {
+    if (this.items.length === 0) throw new Error("EmptySortedList");
+    else return this.items[0];
+  }
+  average() {
+    if (this.items.length === 0) throw new Error("EmptySortedList");
+    else return this.sum() / this.items.length;
+  }
+  sum() {
+    return this.items.reduce((acc, i) => acc + i, 0);
+  }
+}
+
+module.exports = SortedList;
