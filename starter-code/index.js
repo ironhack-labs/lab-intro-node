dd121277df7512de106696e0c8f0a99199d34cd9
beneditaalmeida <benedita@MacBook-Pro-de-Benedita.local>
2019-09-04 17:57:22 +0100

    missing last exercise

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,10 +1,42 @@
 class SortedList {
-    constructor() {}
-    add(item) {}
-    get(pos) {}
-    max() {}
-    min() {}
-    average() {}
+    constructor(item, length, pos) {
+      this.items = [];
+      this.length = this.items.length;
+      
+    }
+
+    add(item) {
+      this.items.push(item)
+      this.items.sort(function(a,b){return a>b ? 1 : a<b ? -1 : 0;});
+      this.length = this.items.length;
+    }
+
+    get(pos) {
+          return this.items[pos];
+        }
+
+
+    max() {
+      if(this.items.length === 0){
+        throw new Error ('EmptySortedList')
+      }else {
+        return this.items[this.items.length -1]
+      }
+    }
+    min() {
+      if (this.items.length === 0){
+        throw new Error ('EmptySortedList')
+      }else {
+        return this.items [0]
+      }
+    }
+    average() {
+      if (this.items.length === 0){
+        throw new Error ('EmptySortedList')
+      }else {
+        return this.items / this.items.length
+      }
+    }
     sum() {}
   };
   
