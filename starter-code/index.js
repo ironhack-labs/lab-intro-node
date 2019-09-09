c3438ceeb5685dc737cac1b1d75c7e7e72e4873e
Lorena Poza Díaz <lorenapozadiaz@MacLPD.local>
2019-09-09 19:21:00 +0200

    Done

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,49 @@
 class SortedList {
-    constructor() {}
-    add(item) {}
-    get(pos) {}
-    max() {}
-    min() {}
-    average() {}
-    sum() {}
+    constructor() {
+      this.items = [];
+      this.length = 0;
+    }
+    add(x) {
+      this.items.push(x);
+      this.length = this.items.length;
+      this.items.sort(function(a,b){return a - b;});
+    }
+    get(i) {
+      if (i > this.length || i < 0) {
+        throw new Error('OutOfBounds');
+      } else {
+        return this.items[i];
+      }
+    }
+    max() {
+      if (this.length === 0 ){
+         throw new Error('EmptySortedList');
+      } else {
+       return Math.max(...this.items);
+      }
+    }
+    min() {
+      if (this.length === 0 ){
+        throw new Error('EmptySortedList');
+     } else {
+      return Math.min(...this.items);
+     }
+    }
+    average() {
+      if (this.length === 0 ){
+        throw new Error('EmptySortedList');
+      } else {return this.sum(this.items)/this.length;
+      }
+    }
+
+    sum() {
+      if (this.length === 0 ){
+        return 0;
+     } else {
+        return this.items.reduce(function(accumulator, value){ return accumulator + value;
+      },0);
+     }
+    }
   };
   
   module.exports = SortedList;
