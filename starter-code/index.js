043fc06860656590b94cbbb38bcd481d6fcac4ad
Brais Vidal <brais.vidal.rodriguez@gmail.com>
2019-09-09 19:16:12 +0200

    all done

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,45 @@
 class SortedList {
-    constructor() {}
-    add(item) {}
-    get(pos) {}
-    max() {}
-    min() {}
-    average() {}
-    sum() {}
+    constructor() {
+      this.items = [],
+      this.length = 0
+    }
+    add(item) {
+      this.items.push(item);
+      this.items.sort((a, b) => a - b);
+      this.length++
+    }
+    get(pos) {
+      if (pos > this.length || pos < 0) {
+        throw new Error("OutOfBounds")
+      } else {
+        return this.items[pos]
+      }
+    }
+    max() {
+      if (this.length === 0) {
+        throw new Error("EmptySortedList")
+      } else { 
+        return Math.max(...this.items)
+      }
+    } 
+
+    min() {
+      if (this.length === 0) {
+        throw new Error("EmptySortedList");
+      } else {
+        return Math.min(...this.items);
+      }
+    }
+    average() {
+      if (this.items.length === 0) {
+        throw new Error("EmptySortedList");
+      } else {
+        return this.sum() / this.items.length;
+      }
+    }
+    sum() {
+      return (this.items.length === 0) ? 0 : this.items.reduce((acc, num) => acc + num, 0);
+    }
   };
   
   module.exports = SortedList;
