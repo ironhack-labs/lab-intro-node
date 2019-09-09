351cb49b33689bce3cf70ad2e7f9c0323b738423
Maximiliano Sussini <maxsus60@gmail.com>
2019-09-09 19:27:59 +0200

    done

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,35 @@
 class SortedList {
-    constructor() {}
-    add(item) {}
-    get(pos) {}
-    max() {}
-    min() {}
-    average() {}
-    sum() {}
+    constructor() {
+      this.items = [],
+      this.length = 0
+    }
+    
+    add(item) {
+      this.items.push(item);
+      this.length++;
+      this.items.sort((a, b) => a - b);
+    }
+    get(pos) {
+      if(pos > this.length || pos < 0) throw new Error("OutOfBounds");
+      return this.items[pos];      
+    }
+    max() {
+      if (this.length === 0) throw new Error("EmptySortedList")
+      return Math.max(...this.items);
+    }
+
+    min() {
+      if (this.length === 0) throw new Error("EmptySortedList")
+      return Math.min(...this.items);
+    }
+    average() {
+      if (this.length === 0) throw new Error("EmptySortedList");
+      return this.sum(this.items) / this.items.length;
+    }
+
+    sum() {
+      return this.length === 0 ? 0 : this.items.reduce((ac, cv) => ac + cv, 0);
+    }
   };
   
   module.exports = SortedList;
