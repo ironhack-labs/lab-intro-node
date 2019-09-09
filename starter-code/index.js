5f3c9d7189ef1ef55db3f3ca64782873f1ef5771
José Luis Valderrama Regueira <pepe.valderrama.regueira@gmail.com>
2019-09-09 18:55:59 +0200

    Done

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,62 @@
 class SortedList {
-    constructor() {}
-    add(item) {}
-    get(pos) {}
-    max() {}
-    min() {}
-    average() {}
-    sum() {}
-  };
-  
-  module.exports = SortedList;
+  constructor() {
+    this.items = [];
+    this.length = 0;
+  }
+
+  add(item) {
+    for (let i = 0; i < this.items.length; i++) {
+      if (item < this.items[i]) {
+        this.items.splice(i, 0, item);
+        this.length += 1;
+        return;
+      }
+    }
+
+    this.items.push(item);
+    this.length += 1;
+  }
+
+  get(pos) {
+    try {
+      return this.items[pos];
+    } catch (Err) {
+      throw new Error("OutOfBounds");
+    }
+  }
+
+  isEmpty() {
+    return this.items.length;
+  }
+
+  max() {
+    if (this.isEmpty()) {
+      return this.items[this.length - 1];
+    } else {
+      throw new Error("EmptySortedList");
+    }
+  }
+
+  min() {
+    if (!this.items.length) throw new Error("EmptySortedList");
+    return this.items[0];
+  }
+
+  average() {
+    if (this.isEmpty()) {
+      return (
+        this.items.reduce((num1, num2) => num1 + num2, 0) / this.items.length
+      );
+    } else {
+      throw new Error("EmptySortedList");
+    }
+  }
+
+  sum() {
+    if (!this.items) return 0;
+
+    return this.items.reduce((num1, num2) => num1 + num2, 0);
+  }
+}
+
+module.exports = SortedList;
