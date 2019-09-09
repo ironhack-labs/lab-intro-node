02be680bcb137f0c4f0f31059c3f936e21a2eaed
JesusSP <jesus.saints@gmail.com>
2019-09-09 19:26:41 +0200

    Ejercicios Terminados

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,48 @@
 class SortedList {
-    constructor() {}
-    add(item) {}
-    get(pos) {}
-    max() {}
-    min() {}
-    average() {}
-    sum() {}
+    constructor() {
+      this.items = [];
+      this.length = 0;
+    }
+    add(item) {
+      this.items.push(item);
+      this.items.sort((x, y) => x - y);
+      this.length++;
+    }
+    get(pos) {
+      if(pos > this.length || pos < 0){
+        throw new Error (`OutOfBounds`)
+      } else {
+        return this.items[pos];
+      }
+    }
+    max() {
+      if (this.items.length === 0) {
+        throw new Error("EmptySortedList")
+      } else {
+        return Math.max(...this.items)
+      }
+    }  
+    min() {
+      if (this.items.length === 0) {
+        throw new Error("EmptySortedList")
+      } else {
+        return Math.min(...this.items)
+      }
+    }
+    average() {
+      if (this.items.length === 0) {
+        throw new Error("EmptySortedList")
+      } else {
+      return this.items.reduce((acc, cu) => acc + cu) / this.items.length
+      }
+    }
+    sum() {
+      if (this.items.length === 0) {
+        return 0;
+      } else {
+      return this.items.reduce((acc, cu) => acc + cu)
+      }
+    }
   };
   
   module.exports = SortedList;
