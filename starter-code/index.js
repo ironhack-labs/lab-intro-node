034af00dc482fd8d541d55b4d6ce653e6c9796c9
Unai Bolivar <ujbolivar@gmail.com>
2019-09-09 22:20:58 +0200

    first iteration

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,36 @@
 class SortedList {
-    constructor() {}
-    add(item) {}
-    get(pos) {}
-    max() {}
-    min() {}
-    average() {}
-    sum() {}
+    constructor() {
+    this.items = [];
+    this.length = this.items.length;
+    }
+    add(item) {
+      this.items.push(item);
+      this.length = this.items.length;
+    }
+    get(pos) {
+      if (pos > this.items.length) {
+        TypeError('OutOfBounds')} else {this.items.indexOf(pos)};
+    }
+    max() {
+      if (this.items.length === 0) {
+        TypeError("EmptySortedList");
+      } else {Math.max(...this.items)
+      }
+    }
+    min() {
+      if (this.sortedList.length === 0) {
+        TypeError("EmptySortedList");
+      } else {Math.min(...this.items);}
+    }
+    average() {
+      return this.sum(this.items) / this.items.length;
+      }
+    sum() {
+      this.items.reduce(sumFunc);
+
+      function sumFunc(ac, cu) {
+        return ac+cu;
+    }
   };
   
   module.exports = SortedList;
