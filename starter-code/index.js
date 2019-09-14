6ba73ef2410d15084ca4adb39a44de578ba7c4e6
elwiiman <emmanuel.sanro@gmail.com>
2019-09-13 22:12:09 -0500

    First commit, some errors yet

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,47 @@
 class SortedList {
-    constructor() {}
-    add(item) {}
-    get(pos) {}
-    max() {}
-    min() {}
-    average() {}
-    sum() {}
-  };
-  
-  module.exports = SortedList;
+  constructor() {
+    this.items = [];
+    this.length = 0;
+  }
+  add(item) {
+    this.items.push(item);
+    this.length = this.items.length;
+  }
+  get(pos) {
+    if (pos > this.items.length || this.items.length == 0) {
+      throw new RangeError("OutOfBounds");
+    } else {
+      return this.items[pos];
+    }
+  }
+  max() {
+    if (this.items == 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      return Math.max(...this.items);
+    }
+  }
+  min() {
+    if (this.items == 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      return Math.min(...this.items);
+    }
+  }
+
+  sum() {
+    if (this.items == 0) return 0;
+    return this.items.reduce((total, currentValue) => {
+      return total + currentValue;
+    });
+  }
+  average() {
+    if (this.items == 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      return this.sum / this.length;
+    }
+  }
+}
+
+module.exports = SortedList;
