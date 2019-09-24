89c1376db95334808e4f1ef7d7872b8432f26477
bruno <brunno-dev@gmail.com>
2019-09-24 08:03:29 -0300

    Update index.js

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,31 @@
 class SortedList {
-    constructor() {}
-    add(item) {}
-    get(pos) {}
-    max() {}
-    min() {}
-    average() {}
-    sum() {}
-  };
-  
-  module.exports = SortedList;
+  constructor() {
+    this.item = [];
+    this.length = this.item.length;
+  }
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a, b) => a - b);
+    this.length = this.items.length;
+    
+  }
+  get(pos) {
+    if (pos > this.items.length - 1 || pos < 0) throw new Error("OutOfBounds");
+    else return this.items[pos];
+  }
+  max() {
+    if (this.items.length === 0) throw new Error("EmptySortedList");
+    else return this.items[this.length - 1];
+  }
+  min() {
+
+  }
+  average() {
+
+  }
+  sum() {
+
+  }
+};
+
+module.exports = SortedList;
