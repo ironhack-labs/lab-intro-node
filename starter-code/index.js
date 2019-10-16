81dd246899735af89c1607b3713bc744343c6b87
Gabriel Zsigmond <gaazsig@gmail.com>
2019-10-16 12:55:53 -0300

    need to add item in correct order

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,48 @@
 class SortedList {
-    constructor() {}
-    add(item) {}
-    get(pos) {}
-    max() {}
-    min() {}
-    average() {}
-    sum() {}
-  };
-  
-  module.exports = SortedList;
+  constructor(items, length) {
+    this.items = [];
+    this.length = 0;
+  }
+  add(item) {
+    this.items.push(item);
+    this.length++;
+  }
+  get(pos) {
+    if (this.length - 1 < pos) {
+      throw new Error("OutOfBounds");
+    }
+    return this.items[pos];
+  }
+  max() {
+    if (this.length) {
+      return Math.max(...this.items);
+    } else {
+      throw new Error("EmptySortedList");
+    }
+  }
+  min() {
+    if (this.length) {
+      return Math.min(...this.items);
+    } else {
+      throw new Error("EmptySortedList");
+    }
+  }
+  average() {
+    if (!this.length) throw new Error("EmptySortedList");
+    let s = this.sum();
+    return s / this.length;
+  }
+  sum() {
+    if (!this.length) return 0;
+    let s = 0;
+    // return this.items.reduce((a, b) => {
+    //   a + b, 0;
+    // });
+    this.items.forEach(e => {
+      s += e;
+    });
+    return s;
+  }
+}
+
+module.exports = SortedList;
