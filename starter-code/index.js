7212db17f438239395bc14e3493d45d16547fb37
marciano <mcnvrodrigues@gmail.com>
2019-10-16 22:28:15 -0300

    inicial

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,9 +1,33 @@
 class SortedList {
-    constructor() {}
-    add(item) {}
-    get(pos) {}
-    max() {}
-    min() {}
+    constructor() {
+      this.items = [];
+      this.length = this.items.length;
+    }
+    add(item) {
+      this.items.push(item);
+      this.length++;
+    }
+    get(pos) {
+      this.items.sort(function (a, b){
+        return a - b;
+      })
+
+      return this.items[pos];
+    }
+    max() {
+      if(this.length == 0){
+        throw new Error("EmptySortedList");
+      }else{
+        return Math.max(...this.items);
+      }
+    }
+    min() {
+      if(this.length == 0){
+        throw new Error("EmptySortedList")
+      }else{
+        return Math.min(...this.items);
+      }
+    }
     average() {}
     sum() {}
   };
