6c0540a7163d6d5aeddd3701683b26f250854373
nasciba <amandanasciba@gmail.com>
2019-10-17 14:06:55 -0300

    test "should add a third value" to complete

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,78 @@
 class SortedList {
-    constructor() {}
-    add(item) {}
-    get(pos) {}
-    max() {}
-    min() {}
-    average() {}
-    sum() {}
-  };
-  
-  module.exports = SortedList;
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
+  add(item) {
+    this.items.push(item);
+    this.items.sort(function (a, b) {
+      if (a > b) {
+        return 1;
+      }
+      if (a < b) {
+        return -1;
+      }
+      if (a === b) {
+        return 0;
+      }
+    });
+    this.length += 1;
+
+  }
+
+  get(pos) {
+    if (pos < (this.length - 1) && pos > 0) {
+      throw new Error("OutOfBounds");
+    }
+    else {
+      return this.items[pos];
+    }
+  }
+
+  max() {
+    if (this.length === 0) {
+      throw new Error("EmptySortedList");
+    }
+    else {
+      return this.items.reduce(function (a, b) {
+        return Math.max(a, b);
+      })
+    }
+  }
+  min() {
+    if (this.length === 0) {
+      throw new Error("EmptySortedList");
+    }
+    else {
+      return Math.min(...this.items)
+    }
+  }
+
+  average() {
+    if (this.length === 0) {
+      throw new Error("EmptySortedList");
+    }
+    else{
+      let sum = 0;
+      for (let i = 0; i < this.length; i += 1) {
+        sum += this.items[i];
+      }
+      return sum / this.length;
+    }
+  }
+  sum() {
+    if (this.length === 0) {
+      return 0;
+    }
+    else {
+      let sum = 0;
+      for (let i = 0; i < this.length; i += 1) {
+        sum += this.items[i];
+      }
+      return sum;
+    }
+  }
+};
+
+
+module.exports = SortedList;
