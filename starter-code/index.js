60d7cfe70fa550a7bf5640818f8642c3ef3c6d8e
Solomonrp <matheusrp97@hotmail.com>
2019-10-17 01:36:44 -0300

    Feito

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,64 @@
 class SortedList {
-    constructor() {}
-    add(item) {}
-    get(pos) {}
-    max() {}
-    min() {}
-    average() {}
-    sum() {}
+    constructor() {
+      {
+        this.items = [],
+        this.length = this.items.length;
+      }
+    }
+    add(item) {
+      
+      this.items.push(item);
+      this.items.sort((a,b)=>{
+        return a - b;
+      });
+      this.length +=1;
+      
+    }
+    get(pos) {
+      if(pos > this.length){
+        return 'OutOfBounds';
+      }
+      let position = this.items[pos];
+      return position;
+    }
+    max() {
+      if(this.length == 0){
+        throw new Error('EmptySortedList');
+      }
+      let max = this.items.reduce((a,b)=>{
+        return Math.max(a, b);
+      })
+
+      return max;
+    }
+    min() {
+      if(this.length == 0){
+        throw new Error('EmptySortedList');
+      }
+      let min = this.items.reduce((a,b)=>{
+        return Math.min(a, b);
+      })
+      return min;
+    }
+    average() {
+      if(this.items.length === 0){
+        throw new Error('EmptySortedList');
+      }
+      let average = this.items.reduce((acumulador,atual)=>{
+        return acumulador + atual;
+      })
+
+      return average / this.items.length
+    }
+    sum() {
+      if(this.items.length === 0){
+        return 0;
+      }
+      let sum = this.items.reduce((acumulador,atual)=>{
+        return acumulador + atual;
+      })
+      return sum;
+    }
   };
   
   module.exports = SortedList;
