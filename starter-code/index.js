74987568aa90df120c00db83796f6b5b473cba69
Victor Nevola <victornevolapn@gmail.com>
2019-10-17 10:48:08 -0300

    Iniciado, feito até metodo min()

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,9 +1,39 @@
 class SortedList {
-    constructor() {}
-    add(item) {}
-    get(pos) {}
-    max() {}
-    min() {}
+    constructor() {
+      this.items = [],
+      this.length = this.items.length
+    };
+    add(item) {
+      this.items.push(item);
+      this.length = this.items.length;
+
+      return this.items.sort((a, b)=>{
+          return a - b;
+      }); 
+    }
+    get(pos) {
+        return this.items[pos];
+    }
+    max() {
+      try{
+        var max = this.items.reduce(function(a, b) {
+          return Math.max(a, b);
+      });
+      return max;
+    } catch(error){
+      throw new Error("EmptySortedList");
+      };
+  };
+    min() {
+      try{
+        var min = this.items.reduce(function(a, b) {
+          return Math.min(a, b);
+      });
+      return min;
+    } catch(error){
+      throw new Error("EmptySortedList");
+      };
+  };
     average() {}
     sum() {}
   };
