e8f9a2ba28c6d9845cbe0aa27e4c459301713387
Cristina Rodríguez <cristina.rd.90@gmail.com>
2019-10-22 20:42:16 +0200

    pass all test

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,51 @@
 class SortedList {
-    constructor() {}
-    add(item) {}
-    get(pos) {}
-    max() {}
-    min() {}
-    average() {}
-    sum() {}
+    constructor(items, length) {
+      this.items = []
+    }
+
+    get length () {
+      return this.items.length;
+    }
+
+    add(item) {
+      this.items.push(item)
+      this.items.sort(function(a,b) {return a-b})
+    }
+    
+    get(pos) {
+      if (pos >= this.length) {
+        return Error("OutOfBounds")
+      }
+      else {return this.items[pos]}
+    }
+
+    max() {
+      if (this.length > 0) {
+        return this.items[this.length - 1]
+      }
+      else {throw new Error("EmptySortedList")}
+    }
+
+    min() {
+      if (this.length > 0) {
+        return this.items[0]
+      }
+      else {throw new Error("EmptySortedList")}
+    }
+
+    average() {
+      if (this.length > 0) {
+        return (this.items.reduce((acc, e) => acc + e, 0)) / this.length
+      }
+      else {throw new Error("EmptySortedList")}
+    }
+
+    sum() {
+      if (this.length > 0) {
+        return this.items.reduce((acc, e) => acc + e, 0)
+      }
+      else {return 0}
+    }
   };
   
   module.exports = SortedList;
