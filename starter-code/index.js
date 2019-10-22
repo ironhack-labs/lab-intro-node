6007fdfd1c927b3d096fb929ca56f76e206bc5d9
Jeremy Mendoza <jeremy.mendoza@decathlon.com>
2019-10-22 21:01:32 +0200

    webptmad0819 - Jeremy Mendoza

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,47 @@
 class SortedList {
-    constructor() {}
-    add(item) {}
-    get(pos) {}
-    max() {}
-    min() {}
-    average() {}
-    sum() {}
+    constructor(items, length) {
+      this.items = []
+    }
+
+    get length(){
+      return this.items.length
+    }
+
+    add(item) {
+      this.items = [...this.items, item]
+      this.items.sort((a, b) => a - b)
+    }
+    get(pos) {
+      if(pos > this.items.length || pos < 0) {
+        throw new Error("OutOfBounds")
+      }
+      return this.items[pos]
+    }
+    max() {
+      if(!this.length) {
+        throw new Error("EmptySortedList")
+      }
+      return Math.max(...this.items)    
+    }
+    min() {
+      if(!this.length) {
+        throw new Error("EmptySortedList")
+      }
+      return Math.min(...this.items)
+    }
+    average() {
+      if(!this.length) {
+        throw new Error("EmptySortedList")
+      }
+      return this.items.reduce((acc, item) => {
+        return acc + item
+      } ,0) / this.items.length
+    }
+    sum() {
+      return !this.items.length ? 0 : this.items.reduce((acc, item) => {
+        return acc + item
+      } ,0)
+    }
   };
   
   module.exports = SortedList;
