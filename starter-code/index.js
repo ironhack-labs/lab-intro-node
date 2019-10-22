2462a78721f029522f4cd2c5f025b17e1b04a498
root <root@LAPTOP-TCKDMSCV.localdomain>
2019-10-22 20:45:09 +0200

    done

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,56 @@
 class SortedList {
-    constructor() {}
-    add(item) {}
-    get(pos) {}
-    max() {}
-    min() {}
-    average() {}
-    sum() {}
+    constructor() {
+      this.items = [];
+    }
+
+    get length() {
+      return this.items.length
+    }
+
+    add(item) {
+      this.items[this.length] = item;
+      this.items = this.items.sort((a,b)=> {return a-b})
+    }
+
+    get(pos) {
+      if( pos <= this.length -1) {
+        return this.items[pos]
+      } else {
+        throw new Error("OutOfBounds")
+      }
+      
+    }
+
+    max() {
+      const max = 0
+      if (this.length === 0) {
+        throw new Error("EmptySortedList")
+      } else {
+        return this.items[this.length-1]
+      }
+    }
+    min() {
+      if (this.length === 0) {
+        throw new Error("EmptySortedList")
+      } else {
+        return this.items[0];
+      }
+    }
+    average() {
+      if( this.length === 0) {
+        throw new Error("EmptySortedList")
+      } else {
+        return this.items.reduce((acc,num) => {
+          return acc + num
+        }, 0)/this.length
+      }
+    }
+    sum() {
+        return this.items.reduce((acc,num) => {
+          return acc + num
+        }, 0)
+      
+    }
   };
   
   module.exports = SortedList;
