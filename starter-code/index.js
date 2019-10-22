5110351b19e47990552931df8cbf8fc424ff93aa
Maria Teresa Barbany Biescas <mt.barbany@gmail.com>
2019-10-22 20:56:00 +0200

    done

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,53 @@
 class SortedList {
-    constructor() {}
-    add(item) {}
-    get(pos) {}
-    max() {}
-    min() {}
-    average() {}
-    sum() {}
-  };
+    constructor() {
+      this.items = []
+    }
+
+    get length(){
+      return this.items.length
+    }
+    add(item) {
+      this.items = [...this.items,item]
+      this.items.sort(function(a, b){return a-b})
+    }
+    get(pos) {
+      try {
+        return this.items[pos]
+      } catch (e) {
+        throw new Error('OutOfBounds')
+      }
+    }
+    max() {
+      if (this.length === 0 ){
+        throw new Error("EmptySortedList")
+      } else {
+        return this.items[this.length-1]
+      }
+    }
+    min() {
+      if (this.length === 0 ){
+        throw new Error("EmptySortedList")
+      } else {
+        return this.items[0]
+      }
+    }
+    average() {
+      if (this.length === 0 ){
+        throw new Error("EmptySortedList")
+      } 
+      
+      return this.sum()/this.length
+      
+    }
+    sum() {
+      if (this.length === 0 ){
+        return 0
+      } else {
+        return this.items.reduce((acc,c) =>  acc+c, 0)
+        
+      }
+    }
+  }
+
   
   module.exports = SortedList;
