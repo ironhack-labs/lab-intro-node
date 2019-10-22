11dda59588280084771b2bee8bbc00ad9b89ff74
SuperRey <juanma@modelical.com>
2019-10-22 19:54:33 +0200

    done

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,48 @@
 class SortedList {
-    constructor() {}
-    add(item) {}
-    get(pos) {}
-    max() {}
-    min() {}
-    average() {}
-    sum() {}
+    constructor() {
+      this.items =[]
+      this.length = 0
+    }
+    add(item) {
+      this.items.push(item)
+      this.items = this.items.sort((a,b)=>a-b)
+      this.length = this.items.length
+    }
+    get(pos) {
+      if(pos >= this.length){
+        throw new Error("OutOfBounds")
+      }else{
+        return this.items[pos]
+      }
+    }
+    max() {
+      if(this.length === 0){
+        throw new Error("EmptySortedList")
+      }else{
+        return this.items[this.length -1]
+      }
+    }
+    min() {
+      if(this.length === 0){
+        throw new Error("EmptySortedList")
+      }else{
+        return this.items[0]
+      }      
+    }
+    average() {
+      if( this.length === 0){
+        throw new Error("EmptySortedList")
+      } else{   
+        return this.sum()/ this.length
+      }   
+    }
+    sum() {
+      if( this.length === 0){
+        return 0
+      } else{
+        return this.items.reduce((attr,e) => attr+e,0)
+      }
+    }
   };
   
   module.exports = SortedList;
