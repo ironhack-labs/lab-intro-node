51c6ef60dd75b9911ae726c31f00606c30594c91
Gudurix <gudurix85@gmail.com>
2019-10-22 21:12:53 +0200

    done

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,33 @@
 class SortedList {
-    constructor() {}
-    add(item) {}
-    get(pos) {}
-    max() {}
-    min() {}
-    average() {}
-    sum() {}
+    constructor() {
+      this.items = []           
+    }
+    get length(){
+      return this.items.length
+    }    
+    add(item) {
+      this.items.push(item)
+      this.items.sort((a,b) => a - b)
+    }
+    get(pos) {
+      if (pos >= this.length) throw new Error ("OutOfBounds")
+      return this.items[pos]      
+    }
+    max() {
+      if (this.length === 0) throw new Error("EmptySortedList")
+      return this.items[this.length - 1]
+    }
+    min() {
+      if (this.length === 0) throw new Error("EmptySortedList")
+      return this.items[0]
+    }
+    average() {
+      if (this.length <= 0) throw new Error("EmptySortedList")
+      return this.sum()/this.length
+    }
+    sum() {
+      return this.length <= 0 ? 0 : this.items.reduce((a,b) => a+b, 0)      
+    }
   };
   
   module.exports = SortedList;
