96994a20eb58c97c4fe5793d1fb14d17b948a554
Elizabeth Lombardi <elizabeth.lombardi7@gmail.com>
2019-10-27 19:26:42 +0100

    Alll Done

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,60 @@
 class SortedList {
-    constructor() {}
-    add(item) {}
-    get(pos) {}
-    max() {}
-    min() {}
-    average() {}
-    sum() {}
+    constructor() {
+      this.item = [];
+    }
+
+    get length(){
+      return this.item.length
+    }
+
+    add(item) {
+     this.item.push(item);
+     this.item.sort((a, b) => a - b)
+    }
+
+    get(pos) {
+      if(pos > this.item.length){
+        throw new Error("OutOfBounds")
+      }
+      return this.item[pos]
+    }
+
+    max() {
+      if(this.item.length === 0){
+        throw new Error("EmptySortedList")
+      }
+      return this.item.reduce((a, b) => {
+        return Math.max(a, b);
+      });
+    }
+
+    min() {
+      if(this.item.length === 0){
+        throw new Error("EmptySortedList")
+      }
+      return this.item.reduce((a, b) => {
+        return Math.min(a, b);
+      });
+    }
+
+    average() {
+      if(this.item.length === 0){
+        throw new Error("EmptySortedList")
+      }
+
+      return this.sum() / this.item.length
+    }
+
+    sum() {
+      if(this.item.length === 0){
+        return 0
+      }
+      return this.item.reduce((a, b) => {
+        return a + b
+      })
+    }
   };
   
-  module.exports = SortedList;
+const sl = new SortedList();
+
+module.exports = SortedList;
