0a5111c71544325f14719758c2bf8c8a5920d6cc
sandrabosk <sandraboskovic@hotmail.com>
2019-11-05 14:32:26 -0500

    update tests, solve OutOfBounds issue, turn to es6

--- starter-code/test/test.js
+++ starter-code/test/test.js
@@ -1,173 +1,167 @@
-// Require the file to be tested
-var SortedList = require('../index.js')
-
+// Require the file which will be tested
+const SortedList = require('../index.js');
 // Load the Chai Assertion Library
-var assert = require('assert');
+const assert = require('assert');
 
-describe('SortedList', function() {
-  describe('Constructor', function() {
-    it('should create an empty SortedList', function() {
-      var sl = new SortedList();
-      assert.equal(sl.length, 0);
+describe('SortedList', () => {
+  describe('Constructor', () => {
+    it('should have items and length properties', () => {
+      const sl = new SortedList();
       assert.equal(sl instanceof SortedList, true);
+      assert.equal(sl.length, 0);
+      assert(Array.isArray(sl.items))
     });
   });
 
-  describe('#add(x)', function() {
-    var sl;
-    beforeEach(function(){
+  describe('#add(x)', () => {
+    let sl;
+    beforeEach(() => {
       sl = new SortedList();
     });
-    it('should add a single value to SortedList', function() {
+    it('should add a single value to SortedList', () => {
       assert.equal(sl.length, 0);
       sl.add(1);
       assert.equal(sl.length, 1);
     });
-    it('should add a third value to SortedList', function() {
+    it('should add a third value to SortedList', () => {
       sl.add(30);
       sl.add(20);
       sl.add(10);
-      assert.equal(sl.length, 3);
+      assert.equal(sl.items.length, 3);
     });
   });
 
-  describe('#get(i)', function() {
-    var sl;
-    beforeEach(function(){
+  describe('#get(i)', () => {
+    let sl;
+    beforeEach(() => {
       sl = new SortedList();
+      for(let i=0; i<3; i++) {
+        sl.add(i);
+      }
+    })
+    
+    it('should return the element in that position', () => {
+      assert.equal(sl.get(2), 2);
     });
 
-    it('should return an OutOfBounds exception if there is no element in that position', function() {
-      try {
-        sl.get(20);
-      } catch (e) {
-        assert.equal(e instanceof Error, true)
-        assert.equal(e.message, "OutOfBounds")
-        assert.throws(sl.get, Error, '/OutOfBounds/');
-      }
+    it('should return an OutOfBounds exception if there is no element in that position', () => {
+      assert.throws(() => {
+        if (sl.get(4) === 'undefined') {
+          throw new Error('OutOfBounds');
+        }
+      }, Error)
     });
 
-    it('should return the element in that position', function() {
-      var foo = 10;
-      for(let i=0; i<200; i++) {
-        sl.add(foo*i);
-        assert.equal(sl.get(i), foo*i);
-      }
-    });
+   
   });
 
-  describe('#add(x) and get(i)', function() {
-    var sl;
-    beforeEach(function(){
+
+  // I really don't understand why we have to specify this?
+  // describe('#add(x) and get(i)', function() {
+  //   var sl;
+  //   beforeEach(function(){
+  //     sl = new SortedList();
+  //   });
+  //   it('should add a second value to SortedList, sorted', function() {
+  //     sl.add(20);
+  //     sl.add(10);
+  //     assert.equal(sl.get(0), 10);
+  //     assert.equal(sl.get(1), 20);
+  //   });
+  //   it('should add a third value to SortedList, sorted', function() {
+  //     sl.add(30);
+  //     sl.add(20);
+  //     sl.add(10);
+  //     assert.equal(sl.get(0), 10);
+  //     assert.equal(sl.get(1), 20);
+  //     assert.equal(sl.get(2), 30);
+  //   });
+  // });
+
+  describe('#max()', () => {
+    let sl;
+    beforeEach(() => {
       sl = new SortedList();
     });
-    it('should add a second value to SortedList, sorted', function() {
-      sl.add(20);
-      sl.add(10);
-      assert.equal(sl.get(0), 10);
-      assert.equal(sl.get(1), 20);
-    });
-    it('should add a third value to SortedList, sorted', function() {
-      sl.add(30);
-      sl.add(20);
-      sl.add(10);
-      assert.equal(sl.get(0), 10);
-      assert.equal(sl.get(1), 20);
-      assert.equal(sl.get(2), 30);
-    });
-  });
 
-  describe('#max()', function() {
-    var sl;
-    beforeEach(function() {
-      sl = new SortedList();
-    });
-
-    it('should return an EmptySortedList exception if there is no element in the list', function() {
-      try {
-        sl.max();
-        // The next line should not be executed
-        assert.equal(true,false);
-      } catch (e) {
-        assert.equal(e instanceof Error, true);
-        assert.equal(e.message, 'EmptySortedList');
-      }
-    });
-
-    it('should return the max element in the list', function() {
+    it('should return the max (highest) value in the array', () => {
       sl.add(10);
       sl.add(20);
 
       assert.equal(sl.max(), 20);
     });
+
+    it('should return an EmptySortedList exception if there is no elements in the array', () => {
+       assert.throws(() => {
+        if (sl.max([]) === 'undefined') {
+          throw new Error('EmptySortedList');
+        }
+      }, Error)
+    });
   });
 
-  describe('#min()', function() {
-    var sl;
-    beforeEach(function() {
+  describe('#min()', () => {
+    let sl;
+    beforeEach(() => {
       sl = new SortedList();
     });
 
-    it('should return an EmptySortedList exception if there is no element in the list', function() {
-      try {
-        sl.min();
-        // The next line should not be executed
-        assert.equal(true,false);
-      } catch (e) {
-        assert.equal(e instanceof Error, true);
-        assert.equal(e.message, 'EmptySortedList');
-      }
-    });
-
-    it('should return the min element in the list', function() {
+    it('should return the min (lowest) value in the array', () => {
       sl.add(10);
       sl.add(20);
-
       assert.equal(sl.min(), 10);
+    });
+
+    it('should return an EmptySortedList exception if there is no elements in the array', () => {
+      assert.throws(() => {
+        if (sl.min([]) === 'undefined') {
+          throw new Error('EmptySortedList');
+        }
+      }, Error)
     });
   });
 
-  describe('#sum()', function(){
-    var sl;
-    beforeEach(function(){
+  describe('#sum()', () => {
+    let sl;
+    beforeEach(() => {
       sl = new SortedList();
     });
 
-    it('should return 0 for an empty sorted list', function() {
-      assert.equal(sl.sum(), 0);
-    })
-
-    it('should add(sum) all elements of the array if there are elements in the list', function() {
+    it('should return sum of all elements in the array', () => {
       sl.add(1);
       sl.add(2);
       sl.add(3);
       assert.equal(sl.sum(), 6);
     });
+
+    it('should return 0 for an empty sorted list', () => {
+      assert.equal(sl.sum(0), 0);
+    })
+
   });
 
-  describe('#average()', function() {
-    var sl;
-    beforeEach(function(){
+  describe('#avg()', () => {
+    let sl;
+    beforeEach(() => {
       sl = new SortedList();
     });
 
-    it('should return an EmptySortedList exception if there are no elements', function() {
-      try {
-        sl.average();
-        // The next line should not be executed
-        assert.equal(true,false);
-      } catch (e) {
-        assert.equal(e instanceof Error, true)
-        assert.equal(e.message, "EmptySortedList")
-      }
+    it('should return the average of elements in the array', () => {
+      sl.add(1)
+      sl.add(2)
+      sl.add(3)
+      assert.equal(sl.avg(), 2);
     })
 
-    it('should return the average of elements in the array', function() {
-      for(i=0; i<101; i++){
-        sl.add(i*2);
-      }
-      assert.equal(sl.average(), 100);
+    it('should return an EmptySortedList exception if there are no elements', () => {
+      assert.throws(() => {
+        if (sl.avg([]) === 'undefined') {
+          throw new Error('EmptySortedList');
+        }
+      }, Error)
     })
+
+    
   });
 
   
