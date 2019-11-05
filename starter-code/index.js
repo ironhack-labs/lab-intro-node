a1fc04809cb91a6882ed088c10610ecf8787eaff
camille <camille.grison@gmail.com>
2019-11-05 22:27:31 +0100

    all passed with mocha

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,50 @@
 class SortedList {
-    constructor() {}
-    add(item) {}
-    get(pos) {}
-    max() {}
-    min() {}
-    average() {}
-    sum() {}
-  };
-  
-  module.exports = SortedList;
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
+  add(item) {
+    this.items.push(item);
+    this.length++; // mocha renvoie 1 : (sl.length, 1) donc m.à j. length aussi
+    this.items.sort((a, b) => a - b);
+  }
+  get(pos) {
+    if (pos > this.length) {
+      throw new Error("OutOfBounds");
+    } else {
+      return this.items[pos];
+    }
+  }
+  max() {
+    if (this.length == 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      return this.items.reduce((a, b) => Math.max(a, b));
+    }
+  }
+  min() {
+    if (this.length == 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      return this.items.reduce((a, b) => Math.min(a, b));
+    }
+  }
+  sum() {
+    return this.items.reduce((total, item) => total + item, 0);
+    // OR:
+    //let total = 0;
+    // this.items.forEach(item => {
+    //   total += item;
+    // });
+    // return total;
+  }
+
+  average() {
+    if (this.length == 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      return this.sum() / this.length;
+    }
+  }
+}
+module.exports = SortedList;
