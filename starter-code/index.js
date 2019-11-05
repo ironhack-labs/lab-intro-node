114815e2365324512d575ede66a7388c80177178
Davide Bernocchi <55143818+davideberno@users.noreply.github.com>
2019-11-05 14:04:31 +0100

    done

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,45 @@
 class SortedList {
-    constructor() {}
-    add(item) {}
-    get(pos) {}
-    max() {}
-    min() {}
-    average() {}
-    sum() {}
-  };
-  
-  module.exports = SortedList;
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a, b) => {
+      return a - b;
+    });
+    this.length = this.items.length;
+  }
+  get(pos) {
+    if (pos > this.length) {
+      throw new Error("OutOfBounds");
+    }
+    return this.items[pos];
+  }
+  max() {
+    if (this.items.length === 0) {
+      throw new Error("EmptySortedList");
+    }
+    return Math.max(...this.items);
+  }
+  min() {
+    if (this.items.length === 0) {
+      throw new Error("EmptySortedList");
+    }
+    return Math.min(...this.items);
+  }
+  sum() {
+    if (this.length === 0) {
+      return 0;
+    }
+    return this.items.reduce((a, b) => a + b);
+  }
+  average() {
+    if (this.length === 0) {
+      throw new Error("EmptySortedList");
+    }
+    return this.sum() / this.length;
+  }
+}
+
+module.exports = SortedList;
