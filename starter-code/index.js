2ed18b355a1f7669d917140e70ee9ca3c9b8a276
Noemie Nakab <noemie.nakab@gmail.com>
2019-11-06 14:15:32 +0100

    mission last 2 iterations

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,29 @@
 class SortedList {
-    constructor() {}
-    add(item) {}
-    get(pos) {}
-    max() {}
-    min() {}
-    average() {}
-    sum() {}
-  };
-  
-  module.exports = SortedList;
+  constructor() {
+    this.items = [];
+    this.length = 0;
+  }
+  add(item) {
+    this.length = this.items.push(item);
+    this.items.sort(function(a, b) {
+      return a - b;
+    });
+  }
+  get(pos) {
+    return this.items[pos];
+  }
+  max() {
+    if (this.length === 0) {
+      throw new Error("EmptySortedList");
+    } else return Math.max(...this.items);
+  }
+  min() {
+    if (this.length === 0) {
+      throw new Error("EmptySortedList");
+    } else return Math.min(...this.items);
+  }
+  sum() {}
+  average() {}
+}
+
+module.exports = SortedList;
