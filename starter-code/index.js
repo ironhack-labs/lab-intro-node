d066855226caa69f1710aa0e3c99cdd0c86aa69a
Mawee33 <mariegreiner33@gmail.com>
2019-11-10 19:48:17 +0100

    only done untill the add()

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,25 @@
 class SortedList {
-    constructor() {}
-    add(item) {}
-    get(pos) {}
+    constructor() {
+      this.items = [];
+      this.length = 0;
+    }
+    add(item) {
+    this.items.push(item);
+    this.length++;
+    }
+    get(pos) {
+      var pos = 
+    if (condition) {
+      
+    }
+      }
+   
+
     max() {}
     min() {}
     average() {}
     sum() {}
   };
   
+
   module.exports = SortedList;
