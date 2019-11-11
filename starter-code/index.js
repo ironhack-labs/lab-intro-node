46ddd56253ec03590c60b1da4a3e54cdb29e5fce
keidu <guille.kfb@gmail.com>
2019-11-11 18:59:32 +0100

    subiendo ejercicio

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,50 @@
 class SortedList {
-    constructor() {}
-    add(item) {}
-    get(pos) {}
-    max() {}
-    min() {}
-    average() {}
-    sum() {}
-  };
-  
-  module.exports = SortedList;
+  constructor() {
+    this.items = []
+    this.length = this.items.length
+  }
+  add(item) {
+    this.items.push(item)
+    this.length = this.items.length
+  }
+  get(pos) {
+    this.items.sort((a, b) => a - b)
+
+    if (this.items.length < pos) {
+      throw new Error("OutOfBounds")
+    }
+    return this.items[pos]
+
+  }
+  max() {
+    if (this.items.length == 0) {
+      throw new Error("EmptySortedList")
+    } else {
+      return Math.max(...this.items)
+    }
+  }
+
+  min() {
+    if (this.items.length == 0) {
+      throw new Error("EmptySortedList")
+    } else {
+      return Math.min(...this.items)
+    }
+  }
+  average() {
+    if (this.items.length == 0) {
+      throw new Error("EmptySortedList")
+    } else {
+      return this.sum() / this.items.length
+    }
+  }
+  sum() {
+    let sum = 0
+    for (let i = 0; i < this.items.length; i++) {
+      sum += this.items[i]
+    } return sum
+  }
+}
+;
+
+module.exports = SortedList;
