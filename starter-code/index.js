416b2e81bd51f0f2f0679fdcda1becdda3cbb75e
Sonia Cáceres Benítez <soniacaceresbenitez@MacBook-Air-de-Sonia.local>
2019-11-11 19:11:44 +0100

    done

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,49 @@
 class SortedList {
-    constructor() {}
-    add(item) {}
-    get(pos) {}
-    max() {}
-    min() {}
-    average() {}
-    sum() {}
-  };
-  
-  module.exports = SortedList;
+  constructor() {
+    this.items = []
+    this.length = 0
+  }
+
+  add(item) {
+    this.items.push(item);
+    this.length = this.items.length
+    this.items.sort((a, b) => a - b)
+  }
+
+
+  get(pos) {
+    if (this.items[pos] == null) { throw new Error("OutOfBounds") }
+    return this.items[pos]
+  }
+
+
+
+
+  max() {
+    if (this.items.length <= 0) { throw new Error("EmptySortedList") }
+    return Math.max.apply(0, this.items)
+  }
+
+
+  min() {
+    if (this.items.length <= 0) { throw new Error("EmptySortedList") }
+    return Math.min.apply(0, this.items)
+  }
+
+
+  average() {
+    if (this.items.length <= 0) { throw new Error("EmptySortedList") }
+    return (this.items.reduce((ac, cu) => ac + cu, 0)) / this.items.length
+  }
+
+
+  sum() {
+    if (this.items.length <= 0) { return 0 }
+    return (this.items.reduce((ac, cu) => ac + cu, 0))
+  }
+
+
+
+};
+
+module.exports = SortedList;
