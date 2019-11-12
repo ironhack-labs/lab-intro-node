5ee51b1f67d782334bd0992a0821fd3c58c21f9f
Gonçalo Regalado <gf.regalado@gmail.com>
2019-11-12 22:01:15 +0000

    finished, no issues

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,43 @@
 class SortedList {
-    constructor() {}
-    add(item) {}
-    get(pos) {}
-    max() {}
-    min() {}
-    average() {}
-    sum() {}
-  };
-  
-  module.exports = SortedList;
+  constructor() {
+    this.items = [];
+    this.length = 0;
+  }
+
+  add(item) {
+    this.items.push(item);
+    this.length++;
+    this.items.sort(function(a, b) {
+      return a - b;
+    });
+  }
+
+  get(pos) {
+    return this.items[pos];
+  }
+
+  max() {
+    if (this.length === 0) throw new Error("EmptySortedList");
+    else return this.items[this.length - 1];
+  }
+
+  min() {
+    if (this.length === 0) throw new Error("EmptySortedList");
+    else return this.items[0];
+  }
+
+  average() {
+    if (this.length === 0) throw new Error("EmptySortedList");
+    else return this.items.reduce((a, b) => a + b, 0) / this.length;
+  }
+
+  sum() {
+    if (this.length === 0) return 0;
+    else return this.items.reduce((a, b) => a + b, 0);
+  }
+}
+
+// let theList = new SortedList();
+module.exports = SortedList;
+
+//
