0b10472d4f598568d6b37eaacf06087f8f916479
stefan hranek <hranekstefan@gmail.com>
2019-11-13 15:12:23 +0100

    BCN-WEBDEV-FT-1019-Stefan Hranek & Boris Vogt

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,59 @@
+
 class SortedList {
-    constructor() {}
-    add(item) {}
-    get(pos) {}
-    max() {}
-    min() {}
-    average() {}
-    sum() {}
+    constructor() {
+      this.items = [];
+      this.length = this.items.length;
+    }
+
+    add(item) {
+      this.items.push(item);
+      this.length++;
+      this.items = this.items.sort(function(a, b){return a - b});
+    }
+
+    get(pos) {
+      if (pos <= this.length-1) {
+        return this.items[pos];
+      } else {
+        throw new Error("OutOfBounds");
+      }
+    }
+
+    max() {
+
+      if (this.length > 0) {
+        return Math.max(...this.items);
+      }
+      else {
+        throw new Error("EmptySortedList");
+      }
+    }
+
+
+    min() {
+    
+      if (this.length > 0) {
+        return Math.min(...this.items);
+      }
+      else {
+        throw new Error("EmptySortedList");
+      }
+    }
+
+    
+    sum() {
+      return this.items.reduce((a,b) => a+b,0);
+    }
+
+
+    average() {
+      if (this.length > 0) {
+        return (this.items.reduce((a,b) => a+b,0) / this.length);
+      }
+      else {
+        throw new Error("EmptySortedList");
+      }
+    }
   };
   
   module.exports = SortedList;
