8843806da60dde5c8b344ad27f7629b0031561ad
caprosset <capucinerosset@gmail.com>
2019-11-13 14:21:47 +0100

    Exercice

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,81 @@
 class SortedList {
-    constructor() {}
-    add(item) {}
-    get(pos) {}
-    max() {}
-    min() {}
-    average() {}
-    sum() {}
+    constructor() {
+      this.items = [];
+      this.length = 0;
+    }
+  
+    add(item) {
+      this.items.push(item);
+      this.length++;
+      this.items.sort( (a,b) => a - b );
+    }
+
+    get(pos) {
+      try {
+        return this.items[pos]; 
+      }
+      catch(error) {
+        if (pos > this.length) {
+          throw new Error('OutOfBounds');
+        }
+      }
+    }
+
+    max() {
+      if (this.length>0) {
+        return Math.max(...this.items);
+      } else if(this.items = [])  {
+        throw new Error("EmptySortedList");
+      }
+
+      // try {
+      //   return Math.max(...this.items);
+      // } catch(e) {
+      //     throw new Error("EmptySortedList");
+        
+      // }
+    }
+      
+    min() {
+      if (this.length>0) {
+        return Math.min(...this.items);
+      } else if(this.items = [])  {
+        throw new Error("EmptySortedList");
+      }
+
+      // try {
+      //   return Math.min(...this.items);
+      // } catch(error) {
+      //   if(this.items = []) {
+      //    throw new Error("EmptySortedList");
+      //   }
+      // }
+    }
+
+
+
+    average() {
+      try {
+        return this.items.reduce( (acc, ele) => acc + ele ) / this.length;
+      } catch(error) {
+        if(this.items = []) {
+          throw new Error("EmptySortedList");
+        }
+      }
+    }
+
+    sum() { 
+      try {
+        return this.items.reduce( (acc, ele) => acc + ele );
+      } catch(error) {
+        if (this.items = []) {
+          return 0;
+        }
+      }
+    }
+
   };
   
+
+
   module.exports = SortedList;
