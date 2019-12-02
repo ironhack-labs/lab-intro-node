130eeb466251a5edbe2159ca04ccce444b865ddd
Shahriyar <shahryar102@yahoo.com>
2019-12-02 12:40:28 -0500

    add function

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,32 @@
 class SortedList {
   constructor() {
     this.items = [];
-    this.length = this.items.length;
+    this.length = 0;
   }
 
-  add(item) {}
-
+  add(item) {
+    if (this.length!==0){
+      for (let i=0;i<this.length;i++){
+        if (item>this.items[i]) {
+          if (i<this.length-1){
+          continue;}
+          else{
+            this.items.push(item)
+          }
+        }
+        else {
+          this.items.splice(i,0,item)
+          break;
+          
+        }
+        
+      }
+    }
+    else{
+      this.items.push(item)
+    }
+    this.length++;
+  }
   get(pos) {}
 
   max() {}
