ec52f4dac0bce527b6ffe83cffddb434b3b5d7e4
avrahm <me@avrahm.com>
2019-12-02 12:51:24 -0500

    completed mocha

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,17 +1,64 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    (this.items = []), (this.length = this.items.length);
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    let sortedItems = this.items.sort((a, b) => {
+      return a - b;
+    });
+    this.length = sortedItems.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if ((pos = this.items[pos])) {
+      return this.items[pos];
+    } else {
+      throw new Error("OutOfBounds");
+    }
+  }
 
-  max() {}
+  max() {
+    let max = this.items.sort((a, b) => {
+      return b - a;
+    });
+    if (max.length > 0) {
+      return this.items[0];
+    } else {
+      throw new Error("EmptySortedList");
+    }
+  }
 
-  min() {}
+  min() {
+    let min = this.items.sort((a, b) => {
+      return a - b;
+    });
+    if (min.length > 0) {
+      return this.items[0];
+    } else {
+      throw new Error("EmptySortedList");
+    }
+  }
 
-  sum() {}
+  sum() {
+    if (this.items.length > 0) {
+      let sum = this.items.reduce(function(a, b) {
+        return a + b;
+      });
+      return sum;
+    } else return 0;
+  }
 
-  avg() {}
+  avg() {
+    if (this.items.length > 0) {
+      let avg = this.sum() / this.items.length;
+
+      return avg;
+    } else {
+      throw new Error("EmptySortedList");
+    }
+  }
 }
 
 module.exports = SortedList;
