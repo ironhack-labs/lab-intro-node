675cd27fb3a784e234c17b5cf45ffe3e420c5a24
kikibres <kristinabressler@yahoo.com>
2019-12-02 12:34:46 -0500

    completed

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,17 +1,52 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items =[];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.length = this.items.length;
+    this.items.sort(function(a, b){return a - b});
+  }
 
-  get(pos) {}
+  get(pos) {
+    if(pos > this.items.length) {
+      throw new Error("OutOfBounds");
+    } else {
+      return this.items[pos];
+    }
+  }
 
-  max() {}
+  max() {
+    if (this.length == 0) {
+      throw new Error("EmptySortedList");
+    } 
+    return Math.max(...this.items);
+  
+  }
 
-  min() {}
+  min() {
+    if (this.length == 0) {
+      throw new Error("EmptySortedList");
+    } 
+    return Math.min(...this.items);
+  }
 
-  sum() {}
+  sum() {
+    if (this.length == 0) {
+      return 0;
+    }
+    return this.items.reduce((a, b) => a + b);
+  }
 
-  avg() {}
+  avg() {
+    if (this.length == 0) {
+      throw new Error("EmptySortedList");
+    } 
+    let sum = this.sum();
+    return sum/this.length;
+  }
 }
 
 module.exports = SortedList;
