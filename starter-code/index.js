ce501678eecc13d37562309e373e95c299193b71
gmsy <gmsy@gmsy.gmsy>
2019-12-02 16:46:18 -0500

    Completed constructor() and add()

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,7 +1,14 @@
 class SortedList {
-  constructor() {}
+  constructor(items, length) {
+    this.items = [];
+    this.length = 0;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a, b) => a - b);
+    this.length = this.items.length;
+  }
 
   get(pos) {}
 
