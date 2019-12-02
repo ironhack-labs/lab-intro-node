99ccf49682d4eca2cc062594c3122ad0f462dd87
saruit0105 <saruits2@gmail.com>
2019-12-02 12:52:00 -0500

    done

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,17 +1,52 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    (this.items = []), (this.length = this.items.length);
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a, b) => (a > b ? 1 : -1));
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos > this.items.length) {
+      throw new Error("OutOfBounds");
+    }
+    return this.items[pos];
+  }
 
-  max() {}
+  max() {
+    if (this.length === 0) {
+      throw new Error("EmptySortedList");
+    }
+    let sortedArr = this.items.sort((a, b) => (a > b ? -1 : 1));
+    return sortedArr[0];
+  }
 
-  min() {}
+  min() {
+    if (this.length === 0) {
+      throw new Error("EmptySortedList");
+    }
+    let sortedArr = this.items.sort((a, b) => (a > b ? 1 : -1));
+    return sortedArr[0];
+  }
 
-  sum() {}
+  sum() {
+    if (this.length === 0) {
+      return 0;
+    }
+    let summedNums = this.items.reduce((total, acc) => total + acc);
+    return summedNums;
+  }
 
-  avg() {}
+  avg() {
+    if (this.length === 0) {
+      throw new Error("EmptySortedList");
+    }
+    let avgNums = this.sum() / this.length;
+    return avgNums;
+  }
 }
 
 module.exports = SortedList;
