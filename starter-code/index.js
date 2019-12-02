e8f781dab8895aa8b2b7965873840c6d1cad5613
Vitalii Agapov <vitaly.agapov@outlook.com>
2019-12-02 12:32:25 -0500

    done

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,17 +1,59 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length
+  }
 
-  add(item) {}
 
-  get(pos) {}
+  add(item) {
+    this.items.push(item)
+    this.items.sort(function (a, b) {
+      return a - b
+    })
+    this.length = this.items.length;
+  }
 
-  max() {}
 
-  min() {}
+  get(pos) {
+    if (pos > this.length - 1 || pos < 0) {
+      throw new Error('OutOfBounds');
+    } else {
+      return this.items[pos]
+    }
+  }
+  max() {
+    if (this.length == 0) {
+      throw new Error('EmptySortedList')
+    } else {
+      return this.items[this.length - 1]
+    }
+  }
 
-  sum() {}
+  min() {
+    if (this.length == 0) {
+      throw new Error('EmptySortedList')
+    } else {
+      return this.items[0]
+    }
+  }
 
-  avg() {}
+
+  sum() {
+    if (this.length == 0) {
+      return 0
+    } else {
+      return this.items.reduce((a, b) => a + b, 0)
+    }
+  }
+
+  avg() {
+    if (this.length == 0) {
+      throw new Error('EmptySortedList')
+    } else {
+      return this.items.reduce((a, b) => a + b, 0) / this.items.length
+    }
+  }
 }
+
 
 module.exports = SortedList;
