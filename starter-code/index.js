f0fc25ec6643ffe509fe47e87566a37b75140c95
gdel21 <56038834+gdel21@users.noreply.github.com>
2019-12-02 12:47:46 -0500

    final

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,17 +1,53 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = []
+    this.length = 0
+  }
 
-  add(item) {}
+  add(item) {
+    let tempList = this.items
+    tempList.push(item)
+    this.length = tempList.length
+    this.items = tempList.sort((a,b)=>{return a-b})
+  }
 
-  get(pos) {}
+  get(pos) {
+    if(pos > this.length - 1 || pos < 0){
+      throw new Error("OutOfBounds");
+    }
+    else{
+      return this.items.indexOf(pos)
+    }
+  }
 
-  max() {}
+  max() {
+    if(this.length == 0){
+      throw new Error("EmptySortedList");
+    }
+    return this.items[this.length-1]
+  }
 
-  min() {}
+  min() {
+    if(this.length === 0){
+      throw new Error("EmptySortedList")
+    }
+    return this.items[0]
+  }
 
-  sum() {}
+  sum() {
+    let sum = 0;
+    for(let i = 0; i < this.length; i++){
+      sum += this.items[i]
+    }
+    return sum
+  }
 
-  avg() {}
+  avg() {
+    if(this.length === 0){
+      throw new Error("EmptySortedList")
+    }
+    return this.sum()/this.length
+  }
 }
 
 module.exports = SortedList;
