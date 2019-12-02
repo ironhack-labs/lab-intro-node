6a512cea45f48db9403d41270c2f309fbecdf7ee
Daphne <daphne.feresin@gmail.com>
2019-12-02 12:38:44 -0500

    done

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,17 +1,51 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items=[],
+    this.length=0
+  }
 
-  add(item) {}
+  add(item) {
+  this.items.push(item);
+  this.items.sort(function(a, b) {
+    return a - b;
+  });
+  this.length = this.items.length
+}
 
-  get(pos) {}
+  get(pos) {
+    if(pos > this.items.length){
+      throw new Error("OutOfBounds");
+    }
+    return this.items[pos]
+  }
 
-  max() {}
+  max() {
+    if(this.items.length === 0){
+      throw new Error("EmptySortedList");
+    }
+    return Math.max(...this.items)
+  }
 
-  min() {}
+  min() {
+    if(this.items.length === 0){
+      throw new Error("EmptySortedList");
+    }
+    return Math.min(...this.items)
+  }
 
-  sum() {}
+  sum() {
+    let numSum = this.items.reduce((a, b) => a + b, 0)
+    return numSum
+  }
 
-  avg() {}
+  avg() {
+    if(this.items.length === 0){
+      throw new Error("EmptySortedList");
+    }
+    return this.sum() / this.length
+  }
 }
 
 module.exports = SortedList;
+
+
