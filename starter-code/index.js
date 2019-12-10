48a7234f5607acbbc60dabe56688969c81cafdab
Strapak, Sergej <strapak@mediamarktsaturn.com>
2019-12-10 12:08:26 +0100

    Finished

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,17 +1,62 @@
+
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.length = this.items.length;
+    this.items = this.items.sort(function(a,b){
+      return  a-b;
+    });
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos > this.items.length){
+      throw new Error("OutOfBounds");
+    }
+    else return(this.items[pos]);
+  }
 
-  max() {}
+  max() {
+    var maxVal = Math.max(...this.items);
+    if (this.items.length == 0){
+      throw new Error("EmptySortedList");
+    }
+    return maxVal;
+    
+  }
 
-  min() {}
+  min() {
+    var minVal = Math.min(...this.items);
+    if (this.items.length == 0){
+      throw new Error("EmptySortedList");
+    }
+    return minVal;
+  }
 
-  sum() {}
+  sum() {
+    if (this.items.length == 0){
+      return 0;
+    }
+    else{
+    var sum = this.items.reduce(function(a,b){return a+b});
+    return sum;
+    }
+  }
+  
 
-  avg() {}
+
+  avg() {
+    if (this.items.length == 0){
+      throw new Error("EmptySortedList");
+    }
+    else{
+      return this.sum(this.items)/this.items.length
+    }
+  }
 }
 
 module.exports = SortedList;
