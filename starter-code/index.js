b685eb12fe2560704c236caae5d92d8fb0ca43a4
kilianal <kilianal@mediamarktsaturn.com>
2019-12-10 11:57:29 +0100

    alles erledigt

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,17 +1,74 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items=[];
+    this.length=this.items.length;
 
-  add(item) {}
+  }
 
-  get(pos) {}
+  add(item) {
+    this.items.push(item);
+    this.length=this.items.length;
+    this.items=this.items.sort(function(a,b){
+      return a-b;
+    })
+  }
 
-  max() {}
+  get(pos) {
+    if(pos>this.length)
+    {throw new Error("OutOfBounds")}
+    else{
+      return this.items[pos]
+    }
+    
 
-  min() {}
+  }
+  
+  max() {
+    if(this.length<=0){
+      throw new Error("EmptySortedList");
+    }
+    else{
+    return Math.max(...this.items);
+    }
 
-  sum() {}
 
-  avg() {}
+  }
+
+  min() {
+    if(this.length<=0){
+      throw new Error("EmptySortedList");
+    }
+    else{
+    return Math.min(...this.items);
+    }
+
+
+  }
+
+  sum() {
+    if(this.length<=0){
+      return 0;
+    }
+    else{
+    return this.items.reduce(function(acc, val){
+      return acc+val;
+    });
+    }
+
+
+  }
+
+  avg() {
+    if(this.length<=0){
+      throw new Error("EmptySortedList");
+    }
+    else{
+    return this.sum()/this.length;
+    }
+
+
+
+  }
 }
 
 module.exports = SortedList;
