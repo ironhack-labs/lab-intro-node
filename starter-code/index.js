4998a438448f59c0ab0bceef0267407fcde97b5e
Bialas <bialaska@media-saturn.com>
2019-12-10 13:57:50 +0100

    exercise finished

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,17 +1,52 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item)
+    this.length++
+    this.items.sort(function(a,b){
+      return a - b;
+    });
+  }
 
-  get(pos) {}
+  get(pos) {
+    if(pos >= this.length){
+      throw new Error("OutOfBounds");
+    }
+    return this.items[pos];
+  }
 
-  max() {}
+  max() {
+    if(this.length === 0){
+      throw new Error("EmptySortedList");
+    }
+    return this.items[this.length-1];
+  }
 
-  min() {}
+  min() {
+    if(this.length === 0){
+      throw new Error("EmptySortedList");
+    }
+    return this.items[0];
+  }
 
-  sum() {}
+  sum() {
+    let sum = this.items.reduce(function(accumulator, item){
+      return accumulator + item
+    },0)
+    return sum
+  }
 
-  avg() {}
+  avg() {
+    if(this.length === 0){
+      throw new Error("EmptySortedList");
+    }
+    return this.sum()/this.length;  
+    
+  }
 }
 
 module.exports = SortedList;
