e68c97afd0ce4b82b62417067db529ad955cde71
Malte Schenkling <schenkling@mediamarktsaturn.com>
2019-12-10 11:33:06 +0100

    final

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,17 +1,68 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort(function(a, b){return a-b});
+    this.length++;
+    console.log(this.items);
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos <= this.length-1) {
+      return this.items[pos];
+    }
+    else {
+      throw new Error("OutOfBounds");
+    }
+  }
 
-  max() {}
+  max() {
+    if (this.length != 0) {
+      return this.items[this.length-1];
+    }
+    else {
+      throw new Error("EmptySortedList");
+    }
+  }
 
-  min() {}
+  min() {
+    if (this.length != 0) {
+      return this.items[0];
+    }
+    else {
+      throw new Error("EmptySortedList");
+    }
+  }
 
-  sum() {}
+  sum() {
+    let arrSum = 0;
+    if (this.length != 0) {
+      for (let item of this.items) {
+        arrSum += Number(item);
+      }
+      return arrSum;
+    }
+    else {
+      return 0;
+    }
+  }
 
-  avg() {}
+  avg() {
+    if (this.length != 0) {
+      return this.sum()/this.length;
+    }
+    else {
+      throw new Error("EmptySortedList");
+    }
+  }
 }
 
 module.exports = SortedList;
+
+let mySortedList = new SortedList();
+mySortedList.add(2);
+console.log();
