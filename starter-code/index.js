b0c53e92b204eb62f8b178812334fd86e117cf3f
Zahn <zahnk@media-saturn.com>
2019-12-10 11:54:34 +0100

    done

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,17 +1,43 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length
+  }
+    
+  add(item) {
+    this.items.push(item);
+    this.length = this.items.length;
+    this.items = this.items.sort(function(a, b){return a-b});
+    
+  }
 
-  add(item) {}
+  get(pos) {
+    if (pos<this.items.length) return this.items[pos];
+    else throw new Error("OutOfBounds");
+  }
 
-  get(pos) {}
+  max() {
+    if (this.items.length === 0) throw new Error("EmptySortedList")
+    else return Math.max(...this.items);
+  }
 
-  max() {}
+  min() {
+    if (this.items.length === 0) throw new Error("EmptySortedList")
+    //(else return this.item[0];
+    else return Math.min(...this.items);
+  }
 
-  min() {}
+  sum() {
+    return this.items.reduce((a, b) => a + b, 0);
+  }
 
-  sum() {}
-
-  avg() {}
+  avg() {
+    const arrAvg = this.sum() / this.length;
+    if (this.length === 0) throw new Error("EmptySortedList");
+    else return arrAvg;
+  }
 }
 
 module.exports = SortedList;
+
+let sortListInst = new SortedList;
