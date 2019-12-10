6bfa619671bf2da0223babaf9fbac35581312114
MSHSusanneV <voglsus@mediamarktsaturn.com>
2019-12-10 13:17:37 +0100

    Intro to Node done

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,17 +1,60 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
+  add(item) {
+      this.items.push(item);
+      this.length = this.items.length;
+      this.items.sort(function(a, b) {
+        return a - b;
+      });
+  }
 
-  add(item) {}
+  get(pos) {
+    if (pos >= this.length) {
+     throw new Error("OutOfBounds");
+    }
+    return this.items[pos];
+  }
 
-  get(pos) {}
+  max() {
+    this.items.sort(function(a, b) {
+      return a - b;
+    });
+    if (this.items.length === 0){
+      throw new Error("EmptySortedList");
+    }
+    return this.items[this.items.length-1];
+  }
 
-  max() {}
+  min() {
+    if (this.items.length === 0){
+      throw new Error("EmptySortedList");
+    }
+    return this.items[0];
+  }
 
-  min() {}
+  sum() {
+    let sum = 0;
+    for (let i = 0; i < this.length; i++) {
+    if (this.length === 0) {
+      return 0;
+    }
+    sum = sum + this.items[i];
+    }
+    return sum;
+  }
 
-  sum() {}
-
-  avg() {}
+  avg() {
+    let sum = 0;
+    this.length = this.items.length;
+    if (this.length === 0) {
+      throw new Error("EmptySortedList");
+      } 
+    else 
+      return this.sum()/this.length;
+  }  
 }
 
 module.exports = SortedList;
