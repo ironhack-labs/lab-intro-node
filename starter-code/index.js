2d8eb2bb1c3b24aaa372b16b0a17ebf4e665d1f0
pyrrhos-msits <menzelf@mediamarktsaturn.com>
2019-12-10 13:19:21 +0100

    Letzter Stand Menzel

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,32 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.length ++;
+    this.items.sort((a,b)=>a-b);
+  }
 
-  get(pos) {}
+  get(pos) {
+  if (pos < this.length){
+    let item = this.items[pos]
+       return item;
+  }else{
+  throw new Error("OutOfBounds");
+  }
+  }
+  
 
-  max() {}
+  max() {
+    if (this.length !== 0){
+   return Math.max(...this.items);
+  }else{
+    throw new Error("EmptySortedList");
+  }
+}
 
   min() {}
 
