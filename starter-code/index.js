373f8e77bbce7f43a13326130b72bd607eec8c0d
Yunnuenmontes <yunnuenmontesym@gmail.com>
2019-12-11 00:33:42 -0600

    done

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,17 +1,54 @@
 class SortedList {
-  constructor() {}
+  constructor(items, length) {
+    this.items =[];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.length++;
+    this.items.push(item);
+    function compare(a,b){return a - b;}
+    this.items.sort(compare);
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos > this.items.length){throw new Error("OutOfBounds");
+  }else {
+    return this.items[pos];
+  }
 
-  max() {}
+  }
 
-  min() {}
+  max() {
+    if(this.length == 0) {
+      throw new Error("EmptySortedList");
+    }else{
+      return Math.max(...this.items);
+    }
+  }
 
-  sum() {}
+  min() {
+    if(this.length == 0){
+      throw new Error ("EmptySortedList");
+    }else {
+      return Math.min(...this.items);
+    }
+  }
 
-  avg() {}
+  sum() {
+    if(this.length == 0){
+      return 0;
+    }else{
+       return this.items.reduce((a,b) => a+b);
+    }
+  }
+
+  avg() {
+    if(this.length == 0){
+      throw new Error ("EmptySortedList");
+    }else{
+      return this.items.reduce((a,b) => (a+b))/this.items.length;
+    }
 }
 
 module.exports = SortedList;
