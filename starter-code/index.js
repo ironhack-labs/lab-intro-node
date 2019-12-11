6e9feca3c94929781b8e84c2a3bc803ce4e1708d
Ardash <56517452+ardache@users.noreply.github.com>
2019-12-10 21:58:15 -0600

    done

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,17 +1,55 @@
+
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = []
+    this.length = this.items.length
+  }
 
-  add(item) {}
+  add(item) {
 
-  get(pos) {}
+    this.items.push(item)
+    this.length += 1
+    this.items.sort ( function (item1, item2) { return item1 - item2})
 
-  max() {}
+  }
 
-  min() {}
+  get(pos) {
+    if (pos > this.length){
+      throw new Error("OutOfBounds");
+    } 
+      return this.items[pos]
+    
+  }
 
-  sum() {}
+  max() {
+    if (this.length == 0) {
+      throw new Error("EmptySortedList");
+    }
+    return this.items[this.length-1]
+  }
 
-  avg() {}
+  min() {
+    if (this.length == 0) {
+      throw new Error("EmptySortedList");
+    }
+    return this.items[0]
+  }
+
+  sum() {
+    let acu = 0
+    this.items.forEach (num => acu +=  num)
+    return acu
+  }
+
+
+  avg() {
+    if (this.length == 0) {
+      throw new Error("EmptySortedList");
+    }
+    let acu = 0
+    this.items.forEach (num => acu +=  num)
+    return acu/this.length
+  }
 }
 
 module.exports = SortedList;
