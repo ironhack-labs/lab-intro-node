37eff7438a111f6303e81e89848bdbf254664001
robbcorrea <51095085+RobbCorrea@users.noreply.github.com>
2019-12-10 21:45:44 -0600

    done

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,17 +1,54 @@
 class SortedList {
-  constructor() {}
+  constructor(items, length) {
+    this.items=[];
+    this.length=this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a,b) => (a-b));
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos>this.length){
+      throw new Error("OutOfBounds");
+    }else{
+      return this.items[pos];
+    }
+  }
 
-  max() {}
+  max() {
+    if (!this.length){
+      throw new Error("EmptySortedList");
+    }else{
+      return Math.max(...this.items);
+    }
+  }
 
-  min() {}
+  min() {
+    if (!this.length){
+      throw new Error("EmptySortedList");
+    }else{
+      return Math.min(...this.items);
+    }
+  }
 
-  sum() {}
+  sum() {
+    if (!this.length){
+      return 0;
+    }else{
+      return this.items.reduce((a,b) => a+b);
+    }
+  }
 
-  avg() {}
+  avg() {
+    if (!this.length){
+      throw new Error("EmptySortedList");
+    }else{
+      return this.items.reduce((a,b) => (a+b))/this.items.length;
+    }
+  }
 }
 
 module.exports = SortedList;
