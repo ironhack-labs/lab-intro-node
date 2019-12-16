7b2408bdef87e7bd57f91d68df0bb10eab5e1665
Fernando Lima Borrelli <flborrelli@gmail.com>
2019-12-16 18:06:13 -0300

    Done

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,17 +1,64 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.length = this.items.push(item);
+    this.items.sort((a, b) => a - b);
+  }
 
-  get(pos) {}
+  get(pos) {
+    if(pos > this.length - 1){
+      throw new Error("OutOfBounds");
+    }
+    else {
+      return this.items[pos];
+    }
+  }
 
-  max() {}
+  max() {
+    if(this.length < 1){
+      throw new Error("EmptySortedList");
+    }
+    else{
+      return this.items[this.length - 1];
+    }
+    
+  }
 
-  min() {}
+  min() {
+    if(this.length < 1){
+      throw new Error("EmptySortedList");
+    }
+    else{
+      return this.items[0];
+    }
+    
+  }
 
-  sum() {}
+  sum() {
+    if(this.length < 1){
+      return 0;
+    }
+    else {
+      let sumOfArr = 0;
+      this.items.forEach(e => sumOfArr += e);
+      return sumOfArr;
+    }
+  }
 
-  avg() {}
+  avg() {
+    if(this.length < 1){
+      throw new Error("EmptySortedList");
+    }
+    else {
+      let sumOfArr = 0;
+      this.items.forEach(e => sumOfArr += e);
+      return sumOfArr / this.length;
+    }
+  }
 }
 
 module.exports = SortedList;
