0eb0659cb0b98244ed1a14ec37e0dc2696249a99
Fabio SantAnna <sgfabio@yahoo.com>
2019-12-16 18:54:53 -0300

    done

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,17 +1,58 @@
 class SortedList {
-  constructor() {}
+  constructor ( items , length ) {
+    this.items = [];
+    this.length = 0;
+  }
 
-  add(item) {}
+  itemsSort() {
+    this.items.sort((a, b) => a - b);
+  }
 
-  get(pos) {}
+  add(item) {
+    this.length = this.items.push(item);
+    this.items.sort((a, b) => a - b);
+  }
 
-  max() {}
+  get(pos) {
+    if ( pos > this.items.length - 1 || pos < 0 ){ 
+      throw new Error("OutOfBounds");
+    }
+    return this.items[pos];
+  }
 
-  min() {}
+  max() {
+    this.items.sort(function(a, b) {
+      return a - b;})
+    if ( this.length === 0 ) {
+      throw new Error("EmptySortedList"); 
+    }
+    return this.items[this.items.length - 1];
 
-  sum() {}
+  }
 
-  avg() {}
+  min() {
+    this.items.sort(function(a, b) {
+      return a - b;})
+    if ( this.length === 0 ) {
+      throw new Error("EmptySortedList"); 
+    }
+    return this.items[0];
+  }
+
+  sum() {
+    // const reducer = (acc, e) => acc + e;
+    if ( this.length === 0 ) {
+      return 0; 
+    }
+    return this.items.reduce((acc, e) => acc + e);
+  }
+
+  avg() {
+    if ( this.length === 0 ) {
+      throw new Error("EmptySortedList"); 
+    }
+    return (this.items.reduce((acc, e) => acc + e)) / this.items.length;
+  }
 }
 
 module.exports = SortedList;
