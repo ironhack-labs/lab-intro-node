7cfb4fc9a5afb7b3abcc05e110482a24e4d90a08
Alex Chivescu <alexchivescu@gmail.com>
2019-12-16 18:06:05 -0300

    done em horario de gente normal

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,17 +1,58 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.length = this.items.push(item);
+    this.items.sort((a, b) => a - b);
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (this.length - 1 < pos) {
+      throw new Error("OutOfBounds");
+    } else {
+      return this.items[pos];
+    }
+  }
 
-  max() {}
+  max() {
+    if (this.length === 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      return this.items[this.length - 1];
+    }
+  }
 
-  min() {}
+  min() {
+    if (this.length === 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      return this.items[0];
+    }
+  }
 
-  sum() {}
+  sum() {
 
-  avg() {}
+    if (this.length === 0) {
+      return 0;
+    } else {
+      let sum = 0;
+      this.items.forEach(e => sum += e);
+      return sum;
+    }
+  }
+
+  avg() {
+    if (this.length === 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      let sum = 0;
+      this.items.forEach(e => sum += e);
+      return sum / this.length;
+    }
+  }
 }
 
 module.exports = SortedList;
