b0203e2335a7ba70982725ff61cb7c9afe295b3a
Maxwell Martins Lima Monteiro <maxwba@MacBook-Pro-de-Maxwell.local>
2019-12-16 22:31:11 -0300

    Done without 4 item

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,17 +1,69 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item)
+    this.items.sort()
+    this.length = this.items.length;
 
-  get(pos) {}
+  }
 
-  max() {}
+  get(pos) {
 
-  min() {}
+    if (this.items[pos] == undefined) {
+      throw new Error("OutOfBounds");
+    }else {
+      return this.items[pos]
+    }
+  }
 
-  sum() {}
+  max() {
+    if (this.items.length === 0) {
+      throw new Error("EmptySortedList");
+    }else {
+      return Math.max.apply(null, this.items);
+    }
+  }
 
-  avg() {}
+  min() {
+    if (this.items.length === 0) {
+      throw new Error("EmptySortedList");
+    }else {
+      return Math.min.apply(null, this.items);
+    }
+  }
+
+  sum() {
+    if (this.items.length === 0) {
+      return 0;
+    }else {
+      return this.items.reduce((acc,curr) => acc += curr)
+    }
+  }
+
+  avg() {
+    if (this.items.length === 0) {
+       throw new Error("EmptySortedList");
+    }else {
+      let sum = 0;
+      sum = this.items.reduce((acc,curr) => {
+        return acc += curr
+      })
+      return sum / (this.items.length)
+    }
+  }
 }
 
+
 module.exports = SortedList;
+
+// const newArr = new SortedList();
+
+// newArr.add(1)
+// newArr.add(1)
+// newArr.add(4)
+
+// console.log(newArr.get())
