afc881b41642106670d66c810c91abc50772240f
Ignacio Garcia Barrero <igarciabarrero@outlook.com>
2020-01-11 13:32:42 +0100

    Completed Tests

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,17 +1,64 @@
 class SortedList {
-  constructor() {}
+  constructor(items, length) {
 
-  add(item) {}
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  add(item) {
 
-  max() {}
 
-  min() {}
 
-  sum() {}
+    this.length++;
+    this.items.push(item)
+    this.items.sort(function (a, b) {
 
-  avg() {}
+
+      return a - b;
+
+    })
+  }
+
+  get(pos) {
+
+    if (pos > this.length) {
+      throw new Error("OutOfBounds");
+
+    }
+
+    return this.items[pos]
+  }
+
+  max() {
+    if (this.length === 0) {
+      throw new Error("EmptySortedList");
+    }
+    return Math.max(...this.items)
+  }
+
+  min() {
+    if (this.length === 0) {
+      throw new Error("EmptySortedList");
+    }
+    return Math.min(...this.items)
+  }
+
+  sum() {
+    if (this.length === 0) {
+      return 0;
+    }
+    return this.items.reduce((a, b) => a + b, 0)
+
+  }
+
+  avg() {
+
+    if (this.length === 0) {
+      throw new Error("EmptySortedList");
+    }
+    return this.sum() / this.length;
+
+  }
 }
 
 module.exports = SortedList;
