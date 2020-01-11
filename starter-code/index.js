c66188c8d8ddc33d350668904f7a642601828492
Luismi Feijoo <luism.feijoo26@gmail.com>
2020-01-11 12:53:14 +0100

    Format code

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,42 +1,48 @@
 class SortedList {
   constructor() {
     this.items = [];
-    this.length = this.items.length
+    this.length = this.items.length;
   }
 
   add(item) {
     this.items.push(item);
-    this.items.sort( (a,b) => a-b)
-    this.length = this.items.length
+    this.items.sort((a, b) => a - b);
+    this.length = this.items.length;
   }
 
   get(pos) {
     if (this.items[pos]) {
-      return this.items[pos]
-    } else {throw new Error('OutOfBounds')}
-   }
+      return this.items[pos];
+    } else {
+      throw new Error("OutOfBounds");
+    }
+  }
 
   max() {
     if (this.items.length > 0) {
-      return Math.max(...this.items)
-    } else {throw new Error("EmptySortedList")}
+      return Math.max(...this.items);
+    } else {
+      throw new Error("EmptySortedList");
+    }
   }
 
   min() {
     if (this.items.length > 0) {
-      return Math.min(...this.items)
-    } else {throw new Error("EmptySortedList")}
+      return Math.min(...this.items);
+    } else {
+      throw new Error("EmptySortedList");
+    }
   }
 
   sum() {
-    return this.items.reduce((a, b) => a + b, 0)
+    return this.items.reduce((a, b) => a + b, 0);
   }
 
   avg() {
-    if(this.length !== 0) {
-      return this.sum()/this.length
+    if (this.length !== 0) {
+      return this.sum() / this.length;
     } else {
-      throw new Error ("EmptySortedList")
+      throw new Error("EmptySortedList");
     }
   }
 }
