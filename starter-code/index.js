d4866a0f1a02d4e84797e2a6f9f6b20de28dafe6
pelinski <teresapelinski@gmail.com>
2020-01-11 12:57:36 +0100

    finished

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,20 +1,46 @@
 class SortedList {
   constructor() {
-    items: [];
-    length: items.length;
+    this.items = [];
+    this.length = this.items.length;
   }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a,b) => a-b);
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos>this.length) {throw new Error("OutOfBounds");}
+    return this.items[pos]
+  }
 
-  max() {}
+  max() {
+    if (this.length == 0) {throw new Error("EmptySortedList");}
+    let max = 0;
+    this.items.forEach(e => { if (e>max) {max = e;}})
+    return max;
+  }
 
-  min() {}
+  min() {
+    if (this.length == 0) {throw new Error("EmptySortedList");}
+    let min = this.max();
+    this.items.forEach(e => { if (e<min) {min = e;}})
+    return min;
+  }
 
-  sum() {}
+  sum() {
+    let sum = 0;
+    this.items.forEach(e => {
+      sum += e;
+    })
+    return sum;
+  }
 
-  avg() {}
+  avg() {
+    if (this.length == 0) {throw new Error("EmptySortedList");}
+    return this.sum()/this.length;
+  }
 }
 
 module.exports = SortedList;
