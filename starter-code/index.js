d1a6697eca0a082782741df5d45aea7a893e9455
Victor Chamizo <victor.chamizo96@gmail.com>
2020-01-11 13:23:24 +0100

    Add full exersice

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,17 +1,42 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = 0;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((n1, n2) => {
+      return n1 - n2;
+    });
+    this.length++;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (this.length < pos) throw new Error("OutOfBounds");
+    else return this.items[pos];
+  }
 
-  max() {}
+  max() {
+    if (this.length === 0) throw new Error("EmptySortedList");
+    return this.items[this.length - 1];
+  }
 
-  min() {}
+  min() {
+    if (this.length === 0) throw new Error("EmptySortedList");
+    return this.items[0];
+  }
 
-  sum() {}
+  sum() {
+    return this.items.reduce((acc, value) => {
+      return value + acc;
+    }, 0);
+  }
 
-  avg() {}
+  avg() {
+    if (this.length === 0) throw new Error("EmptySortedList");
+    return this.sum() / this.length;
+  }
 }
 
 module.exports = SortedList;
