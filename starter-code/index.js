cc847ba46a74a63a0b996719291df202bce25964
Borja del Amo <borjadao@gmail.com>
2020-01-12 18:26:27 +0100

    [MAD-PT PP] Borja del Amo & Leonardo Silva Pavao

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,17 +1,50 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = 0;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a, b) => a - b);
+    this.length = this.items;
 
-  get(pos) {}
+  }
 
-  max() {}
+  get(pos) {
+    if (pos > this.items.length) {
+      throw new Error("OutOfBounds");
+    }
+    return this.items[pos];
 
-  min() {}
+  }
 
-  sum() {}
+  max() {
+    if (this.length === 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      return this.items[this.length - 1];
+    }
+  }
 
-  avg() {}
+}
+
+min() {
+  if (this.length === 0) {
+    throw new Error("EmptySortedList");
+  } else {
+    return this.items[0];
+  }
+}
+
+sum() {
+  return this.items.reduce((a, b) => a + b, 0);
+}
+
+avg() {
+  if (this.length == 0) { throw new Error("EmptySortedList"); }
+  return this.sum() / this.length;
+}
 }
 
 module.exports = SortedList;
