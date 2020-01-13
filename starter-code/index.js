64e3bd2f7451d02fc52465870c6ec798b54ad29b
Ashrafzhon Kurbonaliev <alforsii@gmail.com>
2020-01-13 11:16:31 -0500

    add 1 to 5 iterations

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,13 +1,42 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    return this.items.sort((a, b) => a - b);
+  }
 
-  get(pos) {}
+  //with arguments
+  // add(item) {
+  //   return [...arguments].sort((a, b) => a - b);
+  // }
 
-  max() {}
+  get(pos) {
+    if (this.items[pos] !== undefined) {
+      return this.items[pos];
+    } else {
+      throw new Error('OutOfBounds');
+    }
+  }
 
-  min() {}
+  max() {
+    if (this.items !== []) {
+      return Math.max(...this.items);
+    } else {
+      throw new Error('EmptySortedList');
+    }
+  }
+
+  min() {
+    if (this.items !== []) {
+      return Math.min(...this.items);
+    } else {
+      throw new Error('EmptySortedList');
+    }
+  }
 
   sum() {}
 
@@ -15,3 +44,12 @@ class SortedList {
 }
 
 module.exports = SortedList;
+const test = new SortedList();
+console.log(test.add(5));
+console.log(test.add(2));
+console.log(test.add(7));
+console.log(test.items);
+// console.log(test.add(1, 2, 7, 4, 6, 24, 5));
+console.log(test.get(1));
+console.log(test.max());
+console.log(test.min());
