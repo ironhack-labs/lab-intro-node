0fe7512285632277b4203ed00737516160774f59
Delvis <deapud@gmail.com>
2020-01-14 12:10:59 -0500

    add lab with mocha test

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,17 +1,50 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items =[];
+    this.length=0;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a,b)=>{
+      return a-b;
+    });
+    this.length++;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if(pos>=this.length || pos<0)
+      throw new Error("OutOfBounds");    
+    return this.items[pos];
+  }
 
-  max() {}
+  max() {
+    if(this.length===0)
+    throw new Error("EmptySortedList");
+    return Math.max(...this.items);
+  }
 
-  min() {}
+  min() {
+    if(this.length===0)
+    throw new Error("EmptySortedList");
+    return Math.min(...this.items);
 
-  sum() {}
+  }
 
-  avg() {}
+  sum() {
+    if(this.length===0)
+    return 0;
+    return this.items.reduce((acc,val)=>{
+      return acc+val;
+    },0)
+    
+  }
+
+  avg() {
+    if(this.length==0)
+    throw new Error("EmptySortedList")
+    return (this.sum()/this.length).toFixed(2);
+  }
 }
 
 module.exports = SortedList;
