cbb40de84fdc0cfec88bf47ce8d20b3e558a36aa
Kayla Martinez <kaylamartinez1201@gmail.com>
2020-01-14 23:14:35 -0500

    Iteration 4 complete

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,9 +1,23 @@
+//sorted list of numbers in ascending order
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a,b) => a - b);
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if(this.length < pos){
+      throw new Error("OutOfBounds");
+    }else{
+      return this.items[pos];
+    }
+  }
 
   max() {}
 
