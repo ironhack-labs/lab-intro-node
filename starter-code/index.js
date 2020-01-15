84ee5127bb296cbd5019017ae61d92db9396e385
Veronica Donoso <veronicadonoso1506@gmail.com>
2020-01-14 20:27:33 -0500

    Add method

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,12 +1,18 @@
 class SortedList {
-  constructor() {}
+  constructor(items, length) {
+    this.items = [],
+    this.length = this.items.length
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a, b) => a - b);
+    return this.items
+  }
 
   get(pos) {}
 
   max() {}
-
   min() {}
 
   sum() {}
