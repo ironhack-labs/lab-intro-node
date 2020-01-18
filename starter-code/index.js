38c4e72fd5f685dd01641c8a386fd97c67b33bd6
Veronica Donoso <veronicadonoso1506@gmail.com>
2020-01-18 10:56:38 -0500

    max method and min method

--- starter-code/index.js
+++ starter-code/index.js
@@ -10,14 +10,35 @@ class SortedList {
     return this.items
   }
 
-  get(pos) {}
+  get(pos) {
+    this.items.sort((a, b) => a - b);
+    if (pos >= 0 && pos < this.length) {
+    return this.items[pos]
+     } else {
+       return "OutOfBounds";
+     }
+  }
 
-  max() {}
-  min() {}
+  max() {
+    if (this.length === 0) {
+      return "EmptySortedList";
+    } else {
+      this.items.sort((a, b) => a - b);
+      return this.items[this.length-1]
+    }
+  }
+  min() {
+    if (this.length === 0) {
+      return "EmptySortedList";
+    } else {
+      this.items.sort((a, b) => a - b);
+      return this.items[0]
+    }
+  }
 
   sum() {}
 
   avg() {}
 }
 
-module.exports = SortedList;
+// module.exports = SortedList;
