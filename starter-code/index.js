495549f3f487868e34e548874977d342d0e3c9a8
Veronica Donoso <veronicadonoso1506@gmail.com>
2020-01-25 12:26:54 -0500

    Add last 3 methods

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,44 +1,54 @@
 class SortedList {
-  constructor(items, length) {
+  constructor() {
     this.items = [],
     this.length = this.items.length
   }
 
   add(item) {
-    this.items.push(item);
-    this.items.sort((a, b) => a - b);
+    if (item !== undefined) {
+      this.items.push(item)
+      this.length = this.items.length
+      this.items.sort((a, b) => a - b);
+    }
     return this.items
   }
 
   get(pos) {
-    this.items.sort((a, b) => a - b);
-    if (pos >= 0 && pos < this.length) {
-    return this.items[pos]
-     } else {
-       return "OutOfBounds";
+    if (this.length < pos) {
+      throw new Error('OutOfBounds');
+     }
+     else{
+       return this.items[pos];
      }
   }
 
   max() {
     if (this.length === 0) {
-      return "EmptySortedList";
-    } else {
-      this.items.sort((a, b) => a - b);
-      return this.items[this.length-1]
+      throw new Error ("EmptySortedList");
+    } 
+    else {
+      return Math.max(...this.items)
     }
   }
+
   min() {
     if (this.length === 0) {
-      return "EmptySortedList";
+      throw new Error ("EmptySortedList");
     } else {
-      this.items.sort((a, b) => a - b);
-      return this.items[0]
+      return Math.min(...this.items)
     }
   }
 
-  sum() {}
+  sum() {
+    return this.items.reduce((acc, val) => acc + val, 0)
+  }
 
-  avg() {}
+  avg() {
+    if (this.length === 0) {
+      throw new Error ("EmptySortedList");
+    } 
+    return this.sum()/this.length
+  }
 }
 
-// module.exports = SortedList;
+module.exports = SortedList;
