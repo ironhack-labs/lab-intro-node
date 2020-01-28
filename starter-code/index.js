56457df2b077dd4975006714a59d3123d40bd864
root <root@eCiwan.localdomain>
2020-01-28 12:56:43 +0100

    done

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,17 +1,55 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.length = this.items.length;
+    this.items.sort((a, b) => a - b);
+  }
 
-  get(pos) {}
+  get(pos) {
+    let item = this.items[pos];
+    if (pos > this.length) {
+      throw new Error('OutOfBounds');
+    }
+    return item;
+  }
 
-  max() {}
+  max() {
+    if (this.items == 0) {
+      throw new Error('EmptySortedList');
+    }
+    return this.items[this.length - 1];
+  }
 
-  min() {}
+  min() {
+    if (this.items == 0) {
+      throw new Error('EmptySortedList');
+    }
+    return this.items[0];
+  }
 
-  sum() {}
+  sum() {
+    let total = 0;
+    for (let i = 0; i < this.length; i++) {
+      total += this.items[i];
+    }
+    return total;
+  }
 
-  avg() {}
+  avg() {
+    let total = 0;
+    if (this.items == 0) {
+      throw new Error('EmptySortedList');
+    }
+    for (let i = 0; i < this.length; i++) {
+      total += this.items[i];
+    }
+    return total / this.length;
+  }
 }
 
 module.exports = SortedList;
