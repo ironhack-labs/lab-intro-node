240413aa8447b60588ba60129246ae6f6b640b54
akups <appiah-lindberg@outlook.com>
2020-01-28 12:17:17 +0100

    almost done

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,17 +1,57 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a, b) => {
+      return a - b;
+    });
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos > this.items.length) {
+      throw new Error("OutOfBounds");
+    }
+    return this.items[pos];
+  }
 
-  max() {}
+  max() {
+    if (this.items.length > 0) {
+      return Math.max(...this.items);
+    } else {
+      throw new Error(`EmptySortedList`);
+    }
+  }
 
-  min() {}
+  min() {
+    if (this.items.length > 0) {
+      return Math.min(...this.items);
+    } else {
+      throw new Error(`EmptySortedList`);
+    }
+  }
 
-  sum() {}
+  sum() {
+    if (this.items.length > 0) {
+      return this.items.reduce((a, b) => {
+        return (a += b);
+        0;
+      });
+    } else {
+      return 0;
+    }
+  }
 
-  avg() {}
+  avg() {
+    if (this.items.length > 0) {
+      return sum(this.items) / this.items.length;
+    } else {
+      throw new Error(`EmptySortedList`);
+    }
+  }
 }
-
 module.exports = SortedList;
