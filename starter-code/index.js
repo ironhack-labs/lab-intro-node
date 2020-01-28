c3c0365588d086ec4c9a497ad0acdf0593701b46
Ozhan Yilmaz <ozhanyilmaz@gmail.com>
2020-01-28 11:22:16 +0100

    done

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,17 +1,45 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a, b) => a - b);
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos < this.length && pos >= 0) {
+      return this.items[pos];
+    } else throw new Error("OutOfBounds");
+  }
 
-  max() {}
+  max() {
+    if ((this.length === 0)) {
+      throw new Error("EmptySortedList");
+    } else return Math.max(...this.items);
+  }
 
-  min() {}
+  min() {
+    if((this.length === 0)) {
+      throw new Error("EmptySortedList");
+    } else return Math.min(...this.items);
+  }
 
-  sum() {}
+  sum() {
+    return this.items.reduce((acc, val) => {
+       return acc + val;}, 0);
+  }
 
-  avg() {}
+  avg() {
+    if((this.length === 0)) {
+      throw new Error("EmptySortedList")
+    } else
+    // return this.items.reduce((acc, val) => acc + val) / this.length;
+    return this.sum() / this.length;
+  }
 }
 
 module.exports = SortedList;
