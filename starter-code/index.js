4b9a560b5f4d20e975b901753fa65015a3bf113a
MngSebastian <m.sebi97@yahoo.com>
2020-01-28 20:46:29 +0100

    done

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,13 +1,47 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.length = this.items.length;
+    function compareFunction(a, b) {
+      if (a > b) {
+        return 1;
+      } else if (b > a) {
+        return -1;
+      } else {
+        return 0;
+      }
+    }
+    this.items.sort(compareFunction);
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (this.items[pos]) {
+      // return OutOfBounds;
+      return this.items[pos];
+    }
+    throw new Error("OutOfBounds");
+  }
 
-  max() {}
+  max() {
+    //this if might be wrong
+    if (this.items.length === 0) {
+      throw new Error("EmptySortedList");
+    }
+    return Math.max(...this.items);
+  }
 
-  min() {}
+  min() {
+    //this if might also be wrong
+    if (this.items.length === 0) {
+      throw new Error("EmptySortedList");
+    }
+    return Math.min(...this.items);
+  }
 
   sum() {}
 
