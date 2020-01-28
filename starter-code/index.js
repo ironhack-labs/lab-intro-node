a0c54103b28e81f774aec7ef91cf5dca2f040c5b
Marcel <marcel.riek@googlemail.com>
2020-01-28 11:36:51 +0100

    done

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,17 +1,57 @@
 class SortedList {
-  constructor() {}
+  constructor(items, length) {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+  this.items.push(item);
+  this.length = this.items.length;
+  this.items.sort(function (a, b) {
+    return a - b
+  })
 
-  get(pos) {}
+  }
 
-  max() {}
+  get(pos) {
+    if (pos > this.length ) {
+      throw new Error("OutOfBounds");
+    } else {
+      return this.items[pos];
+    } 
+  }
 
-  min() {}
+  max() {
+    if (this.length === 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      return Math.max(...this.items)
+  }
+}
 
-  sum() {}
+  min() {
+    if (this.length === 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      return Math.min(...this.items);
+    }
+  }
 
-  avg() {}
+  sum() {
+    if (this.length === 0) {
+      return 0;
+    } else {
+     return this.items.reduce((acc, val) => acc + val)
+    }
+  }
+
+  avg() {
+    if (this.length === 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      return (this.items.reduce((acc, val) => acc + val)) / this.items.length;
+    }
+  }
 }
 
 module.exports = SortedList;
