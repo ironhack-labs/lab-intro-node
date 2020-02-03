c42d15986c82b9761f1d79bdeb229787cc59c33c
Roberto Marrero <robertomarrerop@hotmail.com>
2020-02-03 18:38:30 +0100

    done

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,17 +1,62 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = 0;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item)
+    this.items.sort(function (a, b) {
+      return a - b;
+    })
+    this.length = this.items.length
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (this.items.length >= pos) {
+      return this.items[pos]
+    } else {
+      throw new Error("OutOfBounds")
+    }
 
-  max() {}
+  }
 
-  min() {}
+  max() {
+    if (this.items.length === 0) {
+      throw new Error("EmptySortedList")
+    } else {
+      let numbers = this.items
+      return Math.max(...numbers)
+    }
+  }
 
-  sum() {}
+  min() {
+    if (this.items.length === 0) {
+      throw new Error("EmptySortedList")
+    } else {
+      let numbers = this.items
+      return Math.min(...numbers)
+    }
 
-  avg() {}
+  }
+
+  sum() {
+    if (this.items.length != 0) {
+      let sum = this.items.reduce((a, c) => a + c)
+      return sum
+    } else {
+      return 0
+    }
+  }
+
+  avg() {
+    if (this.items.length != 0) {
+      let avg = this.items.reduce((a, c) => a + c) / this.items.length
+      return avg
+    } else {
+      throw new Error("EmptySortedList")
+    }
+  }
 }
 
 module.exports = SortedList;
