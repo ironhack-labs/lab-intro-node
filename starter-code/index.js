d19f9282dcff428f5af79f69cbfbe614f01b7525
fernandocomet <fernandocomet@gmail.com>
2020-02-03 23:20:14 +0100

    Subiendo

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,17 +1,67 @@
 class SortedList {
-  constructor() {}
+  //The task here is to create a class that maintains a sorted list of numbers in ascending order.
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    //you should also make sure that the length property gets updated accordingly when new items are added to the list.
+    this.items.sort((a,b) => a - b); 
+    this.items.push(item);
+    this.items.sort((a,b) => a - b); 
+    this.length = this.items.length;
 
-  get(pos) {}
+    //¿A qué se refieren con "should add a value while keeping the list sorted ?????"
+    //También tengo que aprender jeroglíficos además de Javascript? 
+    //if(notclearinstructions){frustration++;}
+    //En serio, parece que lo ha escrito un indio navajo (nada contra los indios navajos, Dios los bendiga)
+    //¡Viva el pueblo navajo! 
+  }
 
-  max() {}
+  get(pos) {
+    if (pos > this.length){
+      throw new Error("OutOfBounds");
+    }else{
+      return this.items[pos]
+    }
+  }
 
-  min() {}
+  max() {
+    if(this.length === 0){
+      throw new Error("EmptySortedList");
+    }
+    else{
+      return this.items[this.items.length-1]
+    }
+  }
 
-  sum() {}
+  min() {
+    if(this.length === 0){
+      throw new Error("EmptySortedList");
+    }
+    else{
+      return this.items[0]
+    }
+  }
 
-  avg() {}
+  sum() {
+    if(this.length === 0){
+      return 0
+    }else{
+      let total = items.reduce((sum, elem) => sum + elem)
+      //Non capito
+    }
+  }
+
+  avg() {
+    if(this.length === 0){
+      throw new Error("EmptySortedList");
+    }else{
+      let avg = this.sum/this.length;
+      return avg
+    }
+  }
 }
 
 module.exports = SortedList;
