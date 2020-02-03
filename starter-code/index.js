31b5979a5603c227df689187bc5ba6e5428bf7cf
Sergio Collado <sergiocollado95@gmail.com>
2020-02-03 18:00:24 +0100

    Done

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,17 +1,54 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = []
+    this.length = 0
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item)
+    this.length++
+    this.items.sort((a, b) => a < b ? -1 : a > b ? 1 : 0)
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos >= this.length) {
+      throw new Error("OutOfBounds")
+    } else {
+      return this.items[pos]
+    }
+  }
 
-  max() {}
+  max() {
+    if (this.length === 0) {
+      throw new Error("EmptySortedList")
+    } else {
+      return this.items[this.length - 1]
+    }
+  }
 
-  min() {}
+  min() {
+    if (this.length === 0) {
+      throw new Error("EmptySortedList")
+    } else {
+      return this.items[0]
+    }
+  }
 
-  sum() {}
+  sum() {
+    if (this.length === 0) {
+      return 0
+    } else {
+      return this.items.reduce((acc, item) => acc + item)
+    }
+  }
 
-  avg() {}
+  avg() {
+    if (this.length === 0) {
+      throw new Error("EmptySortedList")
+    } else {
+      return this.items.reduce((acc, item) => acc + item) / this.items.length
+    }
+  }
 }
 
 module.exports = SortedList;
