d9440b90525a4380d8fe543372c832c31971a016
CrisSugar <aguacero18@hotmail.com>
2020-02-03 23:21:24 +0100

    done

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,17 +1,60 @@
 class SortedList {
-  constructor() {}
+  constructor(items,length) {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+  
+    this.items.push(item);
+    this.items.sort(function(a,b){
+    
+        return a-b;
+    });
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (this.items[pos]!= undefined){
+      return this.items[pos]
+    }
+    else{
+      throw new Error("OutOfBounds");
+    }
+  }
 
-  max() {}
+  max() {
+    if (this.items.length === 0){
+      
+      throw new Error("EmptySortedList");
+    }
+    else {
+      return Math.max(...this.items);
+    }
+  }
 
-  min() {}
+  min() {
+    if (this.items.length === 0){
+      throw new Error ("EmptySortedList")
+    }
+    else{
+      return Math.min(...this.items)
+    }
+  }
 
-  sum() {}
+  sum() {
+    return this.items.reduce((ac,item) => { return ac + item},0 )
+  }
 
-  avg() {}
+  avg() {
+    if (this.items.length === 0){
+      throw new Error ("EmptySortedList")
+    }
+    else{
+    let sum = this.items.reduce((ac,item) => { return ac + item},0 )
+    return sum / this.items.length
+    }
+  }
 }
 
 module.exports = SortedList;
