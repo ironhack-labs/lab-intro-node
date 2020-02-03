d6204766544b528e6b1c1cbc58904e5edd0ab033
almonasterio <a.monasterio.a@gmail.com>
2020-02-03 19:50:48 +0100

    done

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,17 +1,56 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = []
+    this.length =this.items.length
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a,b) => (a-b));
+    this.length= this.items.length;
 
-  get(pos) {}
+  }
 
-  max() {}
+  get(pos) {
+    if (pos>= this.items.length) {
+      throw new Error("OutOfBounds");
+    } else {
+      return this.items[pos];
+    }
+  }
 
-  min() {}
+  max() {
+    if (this.items.length === 0) {
+      throw new Error("EmptySortedList")
+    } else {
+      return this.items[this.items.length -1]
+    }
+  }
 
-  sum() {}
+  min() {
+    if (this.items.length === 0) {
+      throw new Error("EmptySortedList")
+    } else {
+      return this.items[0]
+    }
+  }
 
-  avg() {}
+  sum() {
+     if (this.items.length === 0) {
+       return 0
+     } else {
+       return this.items.reduce((ac,cv)=> (ac+cv))
+     }
+     }
+
+
+  avg() {
+     if (this.items.length === 0) {
+       throw new Error("EmptySortedList")
+     } else {
+       return (this.items.reduce((ac, cv) => (ac + cv))/this.items.length)
+     }
+  }
 }
 
 module.exports = SortedList;
