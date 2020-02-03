a3d4b890da521f70a15f420000a4540a92675e6b
Andres Atehortua <anderson17colombia@hotmail.com>
2020-02-03 18:12:08 +0100

    Hecho

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,17 +1,52 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = []
+    this.length = this.items.length
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item)
+    this.length = this.items.length
+    this.items.sort((a, b) => a - b)
+  }
+  get(pos) {
+    if (this.items[pos])
+      return this.items[pos]
+    else
+      throw new Error("OutOfBounds")
+  }
 
-  get(pos) {}
+  max() {
+    if (this.length > 0)
+      return Math.max(...this.items)
+    else
+      throw new Error("EmptySortedList")
+  }
 
-  max() {}
+  min() {
+    if (this.length > 0)
+      return Math.min(...this.items)
+    else
+      throw new Error("EmptySortedList")
+  }
 
-  min() {}
+  sum() {
+    if (this.length > 0) {
 
-  sum() {}
+      let sum = this.items.reduce((acc, currentValue) => acc + currentValue)
+      return sum
+    } else
+      return 0
+  }
 
-  avg() {}
+  avg() {
+    if (this.length > 0) {
+      let sum = this.items.reduce((acc, currentValue) => acc + currentValue)
+      let avg = sum / this.length
+      return avg
+    } else
+      throw new Error("EmptySortedList")
+  }
 }
 
 module.exports = SortedList;
