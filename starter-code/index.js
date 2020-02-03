b63ca2698b86a7b0714eb09be9d880e99b01e776
Ruben Calzacorta <calzacorta.ruben@gmail.com>
2020-02-03 19:03:37 +0100

    daily listo

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,17 +1,59 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = []
+    this.length = this.items.length
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item)
+    this.length = this.items.length
+    this.items.sort((a,b) => a-b)
+  }
 
-  get(pos) {}
+  get(pos) {
 
-  max() {}
+    if(this.items[pos]){
 
-  min() {}
+    return this.items[pos]
+    
+    }else{
+      
+    throw new Error("OutOfBounds") 
 
-  sum() {}
+    } 
 
-  avg() {}
+  }
+
+  max() {
+
+    if(this.items[0]){
+      return this.items[this.items.length -1]
+    }else{
+      throw new Error("EmptySortedList")
+    }
+
+  }
+
+  min() {
+
+    if(this.items[0]){
+      return this.items[0]
+    }else{
+      throw new Error("EmptySortedList")
+    }
+  }
+
+  sum() { return this.items[0] ? this.items.reduce((acc, elm) => acc + elm) : 0 }
+
+  avg() {
+
+    if(this.items[0]){
+      return (this.items.reduce((acc, elm) => acc + elm)/this.length)
+    }else{
+      throw new Error("EmptySortedList")
+    }
+
+  }
 }
 
 module.exports = SortedList;
