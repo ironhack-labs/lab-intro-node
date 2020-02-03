0f160169abafad5185ba6f6a714b161cc4b10190
armetegithub <armomomete@protonmail.com>
2020-02-03 19:57:01 +0100

    Terminado

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,17 +1,86 @@
 class SortedList {
-  constructor() {}
+  constructor() {
 
-  add(item) {}
+    this.items = [];
+    this.length = this.items.length;
+    
 
-  get(pos) {}
+  }
 
-  max() {}
+  add(item) {
 
-  min() {}
+    this.items.push(item);
+    this.items.sort(function (a, b){
+      return a - b;
 
-  sum() {}
+    });
+    this.length = this.items.length;
 
-  avg() {}
+}
+    
+
+  get(pos){
+
+    if(this.items[pos] != undefined){
+
+     return this.items[pos]
+    }else{
+
+      throw new Error("OutOfBounds");
+    }
+
+
+  }
+
+  max() {
+
+    if(this.items.length === 0){
+      throw new Error("EmptySortedList");
+    }else{
+      
+      return Math.max(...this.items);
+      
+    }
+
+  }
+
+  min() {
+
+    if(this.items.length === 0){
+      throw new Error("EmptySortedList");
+    }else{
+      
+      return Math.min(...this.items);
+      
+    }
+
+  }
+
+
+  sum() {
+
+    if(this.items.length === 0){
+      return 0;
+    }else{
+  const reducer = (accumulator, currentValue) => accumulator + currentValue;
+
+    return this.items.reduce(reducer);
+    }
+  }
+
+  avg() {
+
+    if(this.items.length === 0){
+      throw new Error("EmptySortedList");
+    }else{
+  const reducer = (accumulator, currentValue) => accumulator + currentValue;
+
+  let sum = this.items.reduce(reducer);
+  
+  return sum / this.items.length;
+
+    }
+  }
 }
 
 module.exports = SortedList;
