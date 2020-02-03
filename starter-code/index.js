16d1beb31265c8a897ecaa6d484279f3b85d08ff
Carolina Guzman <carolinaguzman76@gmail.com>
2020-02-03 22:03:08 +0100

    Terminado

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,17 +1,57 @@
 class SortedList {
-  constructor() {}
+  constructor(items, length) {
+    this.items = []
+    this.length = this.items.length
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item)
+    this.items = this.items.sort(function(a,b) {return a - b})
+    this.length = this.items.length
+    
+  }
 
-  get(pos) {}
+  get(pos) {
+    if(pos > this.length) {
+      throw new Error("OutOfBounds")
+    } else if(pos <= 0) {
+      throw new Error("OutOfBounds")
+    } else {
+      return this.items[pos]
+    }
+  }
 
-  max() {}
+  max() {
+    if(this.length == 0) {
+      throw new Error("EmptySortedList")
+    } else {
+      return this.items[this.length - 1]
+    }
+  }
 
-  min() {}
+  min() {
+    if(this.length == 0) {
+      throw new Error("EmptySortedList")
+    } else {
+      return this.items[0]
+    }
+  }
 
-  sum() {}
+  sum() {
+    if(this.length == 0) {
+      return 0
+    } else {
+      return this.items.reduce((acc, elm) => acc + elm)
+    }
+  }
 
-  avg() {}
+  avg() {
+    if(this.length == 0) {
+      throw new Error("EmptySortedList")
+    } else {
+      return this.sum() / this.items.length
+    }
+  }
 }
 
 module.exports = SortedList;
