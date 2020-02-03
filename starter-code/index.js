780b58b5c14a836f223613244200b51471c9c12a
FrankyMolina <fjmolinatorrejon@gmail.com>
2020-02-03 20:06:08 +0100

    done

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,17 +1,56 @@
 class SortedList {
-  constructor() {}
+  constructor() {
 
-  add(item) {}
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a, b) => a - b);
+    this.length = this.items.length;
+  }
 
-  max() {}
+  get(pos) {
+    if (pos >= this.items.length) {
+      throw new Error('OutOfBounds');
+    } else {
+      return this.items[pos];
+    }
 
-  min() {}
+  }
 
-  sum() {}
+  max() {
+    if (this.length === 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      return Math.max(...this.items);
+    }
+  }
 
-  avg() {}
+  min() {
+    if (this.length === 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      return Math.min(...this.items);
+    }
+  }
+
+  sum() { 
+    if (this.length === 0 ) {
+      return this.items; 
+    } else {
+      return this.items.reduce((acc, cu) => acc += cu);
+    }
+  }
+
+  avg() { 
+    if (this.length === 0 ) {
+      throw new Error("EmptySortedList"); 
+    } else {
+      return this.items.reduce((acc, cu) => acc += cu) / this.length;
+    }
+  }
 }
 
 module.exports = SortedList;
