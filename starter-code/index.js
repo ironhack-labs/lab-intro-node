7b91940417216f007c982af7d0abcf60f3461198
Rafael Bollit <rafa.bollit@gmail.com>
2020-02-04 09:24:37 +0100

    donete

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,17 +1,65 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = 0;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item)
+    this.items.sort(function (a, b) {
 
-  get(pos) {}
+      return a - b
+    })
 
-  max() {}
+    this.length = this.items.length
 
-  min() {}
 
-  sum() {}
+  }
 
-  avg() {}
+  get(pos) {
+    if (this.items.length > pos) {
+      return this.items[pos]
+    }
+    else
+      throw new Error("OutOfBounds");
+  }
+  max() {
+    if (this.items.length > 0) {
+      return this.items[this.items.length - 1]
+    }
+
+    else {
+      throw new Error("EmptySortedList");
+    }
+  }
+
+  min() {
+
+    if (this.items.length > 0) {
+      return this.items[0]
+    }
+
+    else {
+      throw new Error("EmptySortedList");
+    }
+  }
+
+  sum() {
+    if (this.items.length > 0) {
+      let suma = this.items.reduce((acc, idx) => acc + idx)
+      return suma
+    }
+    else { return 0 }
+  }
+
+  avg() {
+    if (this.items.length > 0) {
+      let suma = this.items.reduce((acc, idx) => acc + idx)
+      return suma / this.items.length
+    }
+    else throw new Error("EmptySortedList");
+  }
 }
+
 
 module.exports = SortedList;
