61f3d502c06c1a36c3ea1b226ac667eb9eaf75be
federicogiorgino <federicogiorgino@protonmail.com>
2020-02-05 15:09:19 +0100

    Done

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,17 +1,61 @@
 class SortedList {
-  constructor() {}
+  constructor(items, length) {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a, b) => a - b);
+    this.length += 1;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos > this.items.length) {
+      throw new Error("OutOfBounds");
+    } else {
+      return this.items[pos];
+    }
+  }
 
-  max() {}
+  max() {
+    if (this.length === 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      this.items.sort((a, b) => b - a);
+      return this.items[0];
+    }
+  }
 
-  min() {}
+  min() {
+    if (this.length === 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      this.items.sort((a, b) => a - b);
+      return this.items[0];
+    }
+  }
 
-  sum() {}
+  sum() {
+    let sum = 0;
+    for (const el of this.items) {
+      sum += el;
+    }
+    return sum;
+  }
 
-  avg() {}
+  avg() {
+    if (this.length === 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      let sum = 0;
+      for (const el of this.items) {
+        sum += el;
+      }
+      return sum / this.length;
+      // return avg;
+    }
+  }
 }
 
 module.exports = SortedList;
