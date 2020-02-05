60aa110f84dfa2d1a3112bab2ec4d748ade96068
rodmartinezmedina <rodmartinezmedina@gmail.com>
2020-02-05 13:55:40 +0100

    Rodrigo & David finished

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,17 +1,75 @@
 class SortedList {
-  constructor() {}
+  constructor(items) {
+    this.items = [];
+    this.length = 0;
+    // this.length = this.items.length;
+  }
 
-  add(item) {}
 
-  get(pos) {}
+  add(item) {
 
-  max() {}
+      this.items.push(item);
+      this.items.sort(function(a, b){
+        return a - b;
+    });
+    this.length +=1;
+  }
 
-  min() {}
 
-  sum() {}
+  get(pos) {
+    if (this.items[pos] === undefined) {
+      throw new Error("OutOfBounds");    
+    }
+    else {
+      return this.items[pos];
+    }
+  }
 
-  avg() {}
-}
+
+  max() {
+    if (this.items.length === 0) {
+      throw new Error("EmptySortedList");
+    }
+    else {
+      let maxNum = Math.max(...this.items);
+      return maxNum;
+    }
+  }
+
+
+  min() {
+    if (this.items.length === 0) {
+      throw new Error("EmptySortedList");
+    }
+    else {
+      let minNum = Math.min(...this.items);
+      return minNum;
+    }
+  }
+  
+
+  sum() {
+    if (this.items.length === 0) {
+      return 0;
+    }
+    else {
+      // let array = this.items;
+      let sumArr = this.items.reduce( (acc, curr) => acc + curr );
+      return sumArr;
+    }
+  }
+
+  avg() {
+    if (this.items.length === 0) {
+      return EmptySortedList;
+    }
+    else {
+      // let array = this.items;
+      let sumArr = this.items.reduce( (acc, curr) => acc + curr );
+      return sumArr / this.items.length;
+    }
+  }
+  }
+
 
 module.exports = SortedList;
