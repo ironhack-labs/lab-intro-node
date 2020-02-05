0266b41e5c35c7693e3c6a5ad9e6a73e56287aea
doveriko <abelmorpe@gmail.com>
2020-02-05 14:37:01 +0100

    done

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,17 +1,51 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort( (a, b) => a-b);
+    this.length = this.items.length;
+  };
 
-  get(pos) {}
+  get(pos) {
+    if (this.items[pos] === undefined) {
+      throw new Error("OutOfBounds");
+    } else {
+      return this.items[pos];
+    } 
+  }
 
-  max() {}
+  max() {
+    if (this.items.length === 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      return this.items[this.items.length-1];
+    }
+  }
 
-  min() {}
+  min() {
+    if (this.items.length === 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      return this.items[0];
+    }
+  }
 
-  sum() {}
+  sum() {
+    return this.items.reduce((a, b) => a + b, 0)
+  }
 
-  avg() {}
+  avg() {
+    let sum = this.items.reduce((a, b) => a + b, 0);
+    if (this.items.length === 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      return sum/this.items.length;
+    }
+  }
 }
 
 module.exports = SortedList;
