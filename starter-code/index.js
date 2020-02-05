b308c914a56d5bb0cd20d92b4a190e9bc6aa846c
Val GZ <valmgisbert@gmail.com>
2020-02-05 13:35:06 +0100

    done

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,17 +1,62 @@
 class SortedList {
-  constructor() {}
+  constructor(items = [], length = items.length) {
+    this.items = items;
+    this.length = length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort( (a, b) => a - b);
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos >= this.length) {
+      throw new Error("OutOfBounds");
+    }
+    else {
+      return this.items.indexOf(pos);
+    }
+  }
 
-  max() {}
+  max() {
+    if (this.length == 0) {
+      throw new Error("EmptySortedList");
+    }
+    else {
+      return this.items.slice(-1);
+    }
+  }
 
-  min() {}
+  min() {
+    if (this.length == 0) {
+      throw new Error("EmptySortedList");
+    }
+    else {
+      return this.items[0];
+    }
+  }
 
-  sum() {}
+  sum() {
+    let sum = 0;
+    this.items.forEach( (element) => {
+      sum += element;
+    })
+    return sum;
+  }
 
-  avg() {}
+  avg() {
+    if (this.length == 0) {
+      throw new Error("EmptySortedList");
+    }
+    else {
+    let sum = 0;
+    this.items.forEach( (element) => {
+      sum += element;
+    })
+    return sum/this.length;
+    }
+  }
 }
 
 module.exports = SortedList;
