85ec815db15304d321e81c1cb70fcd947af8464e
valentin-muller <58423332+valentin-muller@users.noreply.github.com>
2020-02-05 14:36:55 +0100

    final

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,17 +1,63 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+  this.items.push(item);
+  this.items.sort((a, b) =>  a - b);
+  this.length = this.items.length;
+  };
+  
 
-  get(pos) {}
+  get(pos) {
+    if (this.items[pos] === undefined) {
+      throw new Error("OutOfBounds");
+    } else {
+      return this.items[pos];
+    }
+   
+  }
 
-  max() {}
+  max() {
 
-  min() {}
+    let highestNum = this.items.sort((a, b) => b - a);
 
-  sum() {}
+    if (this.items.length === 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      return highestNum[0];
+  };
+}
 
-  avg() {}
+  min() {
+
+    let highestNum = this.items.sort((a, b) => b - a);
+
+    if (this.items.length === 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      return highestNum[this.items.length-1];
+  };
+  }
+
+  sum() {
+
+    let reducer = (accumulator, currentValue) => accumulator + currentValue;
+    if(this.items.length == 0) {
+      return 0;
+    } else {
+    
+    return this.items.reduce(reducer);
+    };
+  }
+
+  avg() {
+    let reducer = (accumulator, currentValue) => accumulator + currentValue;
+    let totalSum = this.items.reduce(reducer);
+    return totalSum / this.items.length; 
+  }
 }
 
 module.exports = SortedList;
