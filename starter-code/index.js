a45df637242e358213f06b29922c751e95b7f632
Thibault-d <thibaultdelfaud@gmail.com>
2020-02-11 10:00:54 +0100

    done, only 1 error remaining : should add a single value to SortedList

--- starter-code/index.js
+++ starter-code/index.js
@@ -6,20 +6,50 @@ class SortedList {
 
   add(item) {
     this.items.push(item);
-    this.items.sort(function(a,b){
+    this.items.sort(function (a, b) {
       return a - b;
     })
   }
 
-  get(pos) {}
+  get(pos) {
+    if (pos <= this.items.length && pos >= 0) {
+      return this.items[pos];
+    } else {
+      throw new Error("OutOfBounds");
+    }
+  }
 
-  max() {}
+  max() {
+    if (this.items.length > 0) {
+      return Math.max(...this.items);
+    } else {
+      throw new Error("EmptySortedList");
+    }
+  }
 
-  min() {}
+  min() {
+    if (this.items.length > 0) {
+      return Math.min(...this.items);
+    } else {
+      throw new Error("EmptySortedList");
+    }
+  }
 
-  sum() {}
+  sum() {
+    if (this.items.length > 0) {
+      return this.items.reduce((a, b) => a + b, 0);
+    } else {
+      return 0;
+    }
+  }
 
-  avg() {}
+  avg() {
+    if (this.items.length > 0) {
+      return this.items.reduce((a, b) => a + b, 0) / this.items.length;
+    } else {
+      throw new Error("EmptySortedList");
+    }
+  }
 }
 
 module.exports = SortedList;
