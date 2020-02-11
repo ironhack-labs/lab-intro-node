9ffbcba59457b5e044899fd56663935337f81beb
Marc Serra <mserrahidalgo@gmail.com>
2020-02-11 23:08:45 +0100

    done

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,17 +1,50 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a, b) => a - b)
+    this.length++;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (!this.items[pos]) {
+      throw new Error("outOfBoundException")
+    }
+    return this.items[pos]
+  }
 
-  max() {}
+  max() {
+    if (this.length == 0) {
+      throw new Error("EmptySortedList")
+    }
+    return this.items[this.length - 1]
+  }
 
-  min() {}
+  min() {
+    if (this.length == 0) {
+      throw new Error("EmptySortedList")
+    }
+    return this.items[0]
+  }
 
-  sum() {}
+  sum() {
+    if (this.length == 0) {
+      return 0
+    }
+    return this.items.reduce((accumulator, currentValue) => accumulator + currentValue);
 
-  avg() {}
+  }
+
+  avg() {
+    if (this.length == 0) {
+      throw new Error("EmptySortedList")
+    }
+    return this.items.reduce((accumulator, currentValue) => accumulator + currentValue) / this.items.length;
+  }
 }
 
 module.exports = SortedList;
