7b25aa173bac52c72bd7e27cf55aa3f575f942bf
Javier Varela <jvarela@strands.com>
2020-02-11 15:59:07 +0100

    everything but avg

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,15 +1,62 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items=[];
+    this.length=0;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort(function(a, b) {
+      return a - b;
+    });
+    this.length = this.items.length;
+  }
+   
+  get(pos) {
+    let result;
+    if (pos > this.length-1){
+      throw new Error("OutOfBounds");
+    } else {
+      this.items.forEach(function (el, i){
+        if (pos === i) {
+          result = el;
+        } 
+      });
+    }
+    return result;
+  }
 
-  get(pos) {}
+  max() {
+    let max;
+    if (this.length === 0){
+      throw new Error("EmptySortedList");
+    } else {
+      max = this.items.reduce(function(a, b) {
+        return Math.max(a, b);
+      });
+    }
+    return max;
+  }
 
-  max() {}
+  min() {
+    let min;
+    if (this.length === 0){
+      throw new Error("EmptySortedList");
+    } else {
+      min = this.items.reduce(function(a, b) {
+        return Math.min(a, b);
+      });
+    }
+    return min;
+  }
 
-  min() {}
-
-  sum() {}
+  sum() {
+    let sum=0;
+    sum = this.items.reduce(function(a, b) {
+      return a+b
+    },0);
+    return sum;
+  }
 
   avg() {}
 }
