0f3bf32ec183d16c0ddeb6ee440f1ce7d42682f8
Baenas <dbg94dani@gmail.com>
2020-02-12 18:42:36 +0100

    don

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,17 +1,65 @@
 class SortedList {
-  constructor() {}
+  constructor() {
 
-  add(item) {}
 
-  get(pos) {}
+    this.items = []
+    this.length = this.items.length;
+  }
 
-  max() {}
+  add(item) {
 
-  min() {}
+    this.items.push(item)
+    this.items.sort(function (a, b) { return a - b });
+    this.length += 1;
+  }
 
-  sum() {}
+  get(pos) {
 
-  avg() {}
+    if (pos < 0 || pos >= this.items.length) {
+
+      throw new Error("OutofBounds");
+
+    } else {
+      return this.items[pos]
+    }
+  }
+
+  max() {
+
+    if (this.items.length == 0) {
+      throw new Error("EmptySortedList");
+
+    } else {
+      return Math.max.apply(null, this.items);
+    }
+  }
+
+  min() {
+
+    if (this.items.length == 0) {
+      throw new Error("EmptySortedList");
+
+    } else {
+      return Math.min.apply(null, this.items);
+    }
+  }
+
+  sum() {
+    return this.items.reduce((a, b) => a + b, 0)
+  }
+
+  avg() {
+    if (this.items.length == 0) {
+
+      throw new Error("EmptySortedList")
+    } else {
+      return this.items.reduce((a, b) => a + b, 0) / this.items.length;
+
+
+    }
+
+
+  }
 }
 
 module.exports = SortedList;
