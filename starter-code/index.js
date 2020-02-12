6ccfedac09ff230e5f7eeff37cf02894580eb361
alexfc96 <alex@LAPTOP-ELMBT3E0.localdomain>
2020-02-12 20:11:07 +0100

    done

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,17 +1,59 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort();
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos <= this.length) {
+      return this.items[pos]
+    } else {
+      throw new Error("OutOfBounds");
+    }
+  }
 
-  max() {}
+  max() {
+    if (this.items && this.items.length) {
+      return Math.max(...this.items);
+    } else {
+      throw new Error("EmptySortedList");
+    }
+  }
 
-  min() {}
+  min() {
+    if (this.items && this.items.length) {
+      return Math.min(...this.items);
+    } else {
+      throw new Error("EmptySortedList");
+    }
 
-  sum() {}
+  }
 
-  avg() {}
+  sum() {
+    if (this.items && this.items.length) {
+      const reducer = (accumulator, currentValue) => accumulator + currentValue;
+      return this.items.reduce(reducer);
+    } else {
+      return 0;
+    }
+
+  }
+
+  avg() {
+    if (this.items && this.items.length) {
+      const sum = this.sum();
+      return sum / this.length;
+    } else {
+      throw new Error("EmptySortedList");
+    }
+
+  }
 }
 
 module.exports = SortedList;
