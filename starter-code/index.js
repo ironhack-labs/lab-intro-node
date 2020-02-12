ccc7a5234d5b22acfcd49c7d8e7b5a598053cc92
Jaime <jaimemoravallejo@gmail.com>
2020-02-12 14:38:44 +0100

    done

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,17 +1,61 @@
 class SortedList {
-  constructor() {}
+  constructor(items, length) {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.length++;
+    this.items.sort((a, b) => {
+      return a - b;
+    });
+  }
 
-  get(pos) {}
+  get(pos) {
+    if(!this.items[pos]){
+      throw new Error("OutOfBounds");
+    }else {
+      return this.items[pos];
+    }
+  }
 
-  max() {}
+  max() {
+    if(this.length === 0){
+      throw new Error("EmptySortedList");
+    } else {
+      return this.items[this.length -1];
+    }
+  }
 
-  min() {}
+  min() {
+    if (this.length === 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      return this.items[0];
+    }
+  }
 
-  sum() {}
+  sum() {
+    let total = 0;
+    this.items.forEach(element => {
+      total += element;    
+    });
+    return total;
+  }
 
-  avg() {}
+  avg() {
+    if (this.length == 0) {
+      throw new Error("EmptySortedList")
+    }
+    let avg = 0;
+    let total = 0;
+    this.items.forEach(element => {
+      total += element;
+    });
+    avg = total / this.length;
+    return avg;
+  }
 }
 
 module.exports = SortedList;
