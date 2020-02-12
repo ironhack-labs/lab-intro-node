0ad6aa179bbcbeaf756a7cccbe49516adc9948a8
raquelmu <raquel.muniesa@gmail.com>
2020-02-12 23:44:15 +0100

    iteration 1,2,3,4,5

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,17 +1,54 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = []
+    this.length = 0
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item)
+    this.items.sort(function(a,b){
+      return a-b
+    })
+    this.length += 1
+  }
 
-  get(pos) {}
+  get(pos) {
 
-  max() {}
+    if(this.items[pos] === undefined){
+      throw new Error ("OutOfBounds");
+    }
+    else{
+      return this.items[pos]
+    }
+  }
 
-  min() {}
+  max() {
+    if(this.items.length === 0){
+      throw new Error("EmptySortedList");
+    }
+    else{
+      return Math.max(...this.items)
+    }
 
-  sum() {}
+  }
 
-  avg() {}
+  min() {
+    if(this.items.length === 0){
+      throw new Error("EmptySortedList");
+    }
+    else{
+      return Math.min(...this.items)
+    }
+
+  }
+
+  sum() {
+
+  }
+
+  avg() {
+
+  }
 }
 
 module.exports = SortedList;
