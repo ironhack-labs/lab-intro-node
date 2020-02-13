4105acf4ab20a0e3978f937ec28065128d792fc8
alejandro <alejimenezgitgithub@gmail.com>
2020-02-13 21:13:10 +0100

    finish

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,17 +1,58 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = 0;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort(function (a, b){ return a - b; });
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if((this.length) >= this.items[pos])
+      return this.items[pos];
+    else 
+      throw new Error("OutOfBounds");
+  }
 
-  max() {}
+  max() {
+    if(!this.length == 0){
+      const max = this.items.sort(function(a, b){return b-a});
+      return max[0];
+    }
+    else
+      throw new Error("EmptySortedList");
+  }
 
-  min() {}
+  min() {
+    if(!this.length == 0){
+      const max = this.items.sort(function(a, b){return a-b});
+      return max[0];
+    }
+    else
+      throw new Error("EmptySortedList");
+  }
 
-  sum() {}
+  sum() {
+    if(!this.length == 0){
+      const reducer = (accumulator, currentValue) => accumulator + currentValue;
+      return this.items.reduce(reducer);
+    }
+    else
+      return 0;
+  }
 
-  avg() {}
+  avg() {
+    if(!this.length == 0){
+      const reducer = (accumulator, currentValue) => accumulator + currentValue;
+      const reduce =  this.items.reduce(reducer);
+      return reduce / this.items.length;
+    }
+    else
+      throw new Error("EmptySortedList");
+  }
 }
 
 module.exports = SortedList;
