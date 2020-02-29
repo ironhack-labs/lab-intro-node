8faf4244aadd1f4536d791dfbd8b77e219c87aa2
Rajmohan <Rajmohankct2003@gmail.com>
2020-02-29 12:24:50 +0100

    Changes completed

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,17 +1,64 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = 0;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a,b) => {
+      if (a < b){
+        return -1 ;
+      } else {
+        return 1;
+      }
+    });
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    const index = this.items.indexOf(pos);
+    if (index < 0 ){
+      throw new Error("OutOfBounds");
+    } else {
+      return index;
+    }
+  }
 
-  max() {}
+  max() {
+    if (this.length === 0 ){
+      throw new Error("EmptySortedList");
+    } else {
+      return this.items[this.length - 1];
+    }
+  }
 
-  min() {}
+  min() {
+    if (this.length === 0 ){
+      throw new Error("EmptySortedList");
+    } else {
+      return this.items[0];
+    }
+  }
 
-  sum() {}
+  sum() {
+    let totalSum = 0;
+    this.items.forEach( item => {
+       totalSum = totalSum + item;
+    })
+    return totalSum;
+  }
 
-  avg() {}
+  avg() {
+    let avg = 0;
+    if(this.length > 0 ) {
+      avg = this.sum() / this.length;
+      return avg;
+    } else {
+      throw new Error("EmptySortedList");
+    }
+    
+  }
 }
 
 module.exports = SortedList;
