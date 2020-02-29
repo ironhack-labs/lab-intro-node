3b82390c2d6b6a3e22d8759d5dd7f8e23ed32128
Daniel Hoogerwerf <45287356+danielhoogerwerf@users.noreply.github.com>
2020-02-29 12:22:03 +0100

    Done

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,17 +1,57 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a, b) => a - b);
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (this.items.indexOf(pos) > 0) {
+      return this.items.indexOf(pos);
+    } else {
+      throw new Error("OutOfBounds");
+    }
+  }
 
-  max() {}
+  max() {
+    if (this.items.length === 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      return Math.max(...this.items);
+    }
+  }
 
-  min() {}
+  min() {
+    if (this.items.length === 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      return Math.min(...this.items);
+    }
+  }
 
-  sum() {}
+  sum() {
+    if (this.items.length === 0) {
+      return 0;
+    } else {
+      const reducer = (acc, value) => acc + value;
+      return this.items.reduce(reducer);
+    }
+  }
 
-  avg() {}
+  avg() {
+    if (this.items.length <= 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      let result = 0;
+      this.items.forEach(item => result += item)
+      return result / this.items.length;
+    }
+  }
 }
 
 module.exports = SortedList;
