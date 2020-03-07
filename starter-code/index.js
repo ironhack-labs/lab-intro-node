9750dc71e29245110d2a934a49bc125c9413e3b9
Joao Juliatti <joaopjuliatti@gmail.com>
2020-03-07 11:59:32 -0300

    done

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,17 +1,45 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = []
+    this.length = 0
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item)
+    this.length++
+    this.items.sort((a,b)=>a-b)
+  }
+  get(pos) {
+    if(pos<0 || pos>this.length-1)
+      throw new Error('OutOfBounds')
+    return this.items[pos]
+  }
 
-  get(pos) {}
+  max() {
+    if(this.length===0)
+      throw new Error('EmptySortedList')
+    return Math.max(...this.items)
+  }
 
-  max() {}
+  min() {
+    if(this.length===0)
+      throw new Error('EmptySortedList')
+    return Math.min(...this.items)
+  }
 
-  min() {}
+  sum() {
+    let sumNumbers = 0
+    for(let i=0;i<this.length;i++){
+      sumNumbers +=this.items[i]
+    }
+    return sumNumbers
+  }
 
-  sum() {}
-
-  avg() {}
+  avg() {
+    if(this.length===0)
+      throw new Error(`EmptySortedList`)
+    return this.sum()/this.length
+  }
 }
 
 module.exports = SortedList;
