2408333f15de9cf2dd8c81221e6d84714d64310e
Ricardo Sorana <ricardosorana@gmail.com>
2020-03-07 12:29:15 -0300

    done

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,17 +1,58 @@
 class SortedList {
-  constructor() {}
+  constructor(items) {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items = this.items.sort(function(a, b){return a-b});
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if(pos >= this.items.length) {
+      throw new Error("OutOfBounds");
+    }
+    return (this.items[pos]);
+  }
 
-  max() {}
+  max() {
+    if(this.items.length === 0) {
+      throw new Error("EmptySortedList");
+    }
+    return this.items.reduce(function(a, b) {
+      return Math.max(a, b);
+    });
+  }
 
-  min() {}
+  min() {
+    if(this.items.length === 0) {
+      throw new Error("EmptySortedList");
+    }
+    return this.items.reduce(function(a, b) {
+      return Math.min(a, b);
+    });
+  }
 
-  sum() {}
+  sum() {
+    if(this.items.length === 0) {
+      return 0;
+    }
+    return this.items.reduce(function(a, b) {
+      return a + b;
+    });
+  }
 
-  avg() {}
+  avg() {
+    if(this.items.length === 0) {
+      throw new Error("EmptySortedList");
+    }
+    let sum = this.items.reduce(function(a, b) {
+      return a + b;
+    });
+    return sum / this.length;
+  }
 }
 
 module.exports = SortedList;
