1a2a5f1bdeeda50821a4f14e4f2945cf5178c2f8
Andre <andre.talpo@gmail.com>
2020-03-07 12:30:22 -0300

    Jeito facil

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,17 +1,38 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = 0;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a, b) => a - b);
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (this.items[pos] === undefined) throw new OutOfBounds();
+    return this.items[pos];
+  }
 
-  max() {}
+  max() {
+    if (this.length === 0) throw new Error("EmptySortedList");
+    return this.get(this.length - 1);
+  }
 
-  min() {}
+  min() {
+    if (this.length === 0) throw new Error("EmptySortedList");
+    return this.get(0);
+  }
 
-  sum() {}
+  sum() {
+    return this.items.reduce((antes, atual) => antes + atual, 0);
+  }
 
-  avg() {}
+  avg() {
+    if (this.length === 0) throw new Error("EmptySortedList");
+    return this.sum() / this.length;
+  }
 }
 
 module.exports = SortedList;
