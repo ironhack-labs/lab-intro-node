606c31fbac9ff75224ec732170d3b8371750b529
Grazielli Pozzi <graziellipozzi@gmail.com>
2020-03-08 16:12:35 -0300

    Parcial

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,17 +1,97 @@
 class SortedList {
-  constructor() {}
+  constructor(items) {
+    this.items = items;
+    this.length = this.items.length;
+    this.soma;
+  }
 
-  add(item) {}
+  add(item) {
+    let count = this.length;
+    for (let i = 0; i < this.items.length; i++) {
+      if (this.items.length === 0) {
+        this.items.push(item);
+        this.length = this.length + 1;
+        break;
+      }
+      if (this.items[i] > item) {
+        this.items.splice(i - 1, 0, item);
+        this.length = this.length + 1;
+        count = count + 1;
+        break;
+      } if (count === this.length && i === this.length - 1) {
+        this.items.push(item);
+        this.length = this.length + 1;
+        break;
+      }
+    }
+  }
 
-  get(pos) {}
+  get(pos) {
+    for (let i = 0; i < this.items.length; i++) {
+      if (pos > this.items.length - 1) {
+        throw new Error("OutOfBounds");
+      }
+      if (i === pos) {
+        return this.items[i];
+      }
+    }
+  }
 
-  max() {}
+  max() {
+    let maximo;
+    if (this.length === 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      for (let i = 0; i < this.length; i++) {
+        if (i === 0) {
+          maximo = this.items[i];
+        } else if (this.items[i] > maximo) {
+          maximo = this.items[i];
+        }
+      }
+      return maximo;
+    }
+  }
 
-  min() {}
+  min() {
+    let minimo;
+    if (this.length === 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      for (let i = 0; i < this.length; i++) {
+        if (i === 0) {
+          minimo = this.items[i];
+        } else if (this.items[i] < minimo) {
+          minimo = this.items[i];
+        }
+      }
+      return minimo;
+    }
 
-  sum() {}
+  }
 
-  avg() {}
+  sum() {
+    this.soma = 0;
+    this.items.forEach(element => {
+      this.soma = this.soma + element;
+    });
+    return this.soma;
+
+  }
+
+    avg() {
+      return this.soma/this.length;
+
+    }
 }
 
-module.exports = SortedList;
+module.exports = new SortedList();
+const list = new SortedList([6, 5]);
+list.add(7);
+console.log(list);
+list.get(1);
+list.max();
+list.min();
+list.sum();
+list.avg();
+
