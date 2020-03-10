dedb32a39fbd4995905822b25c920cab7dfcc30a
Alberto Quiñonero Ayllon <albertoquinoneroayllon@MacBook-Pro-de-Alberto-2.local>
2020-03-10 12:38:04 +0100

    done

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,17 +1,62 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a, b) => a - b);
+    this.length++;
+  }
 
-  get(pos) {}
+  get(pos) {
+    this.items.indexOf(pos);
+    if (this.items.indexOf(pos) === -1) {
+      throw new Error("OutOfBounds");
+    }
+    return this.items[pos];
+  }
 
-  max() {}
+  max() {
+    if (this.length === 0) {
+      return EmptySortedList;
+    } else {
+      let max = Math.max(...this.items);
+      return max;
+    }
+  }
 
-  min() {}
+  min() {
+    if (this.length === 0) {
+      return EmptySortedList;
+    } else {
+      let max = Math.min(...this.items);
+      return max;
+    }
+  }
 
-  sum() {}
+  sum() {
+    if (this.length === 0) {
+      return 0;
+    } else {
+      let total = this.items.reduce(function(a, b) {
+        return a + b;
+      });
+      return total;
+    }
+  }
 
-  avg() {}
+  avg() {
+    if (this.length === 0) {
+      return EmptySortedList;
+    } else {
+      let total = this.items.reduce(function(a, b) {
+        return a + b;
+      });
+      return total / this.items.length;
+    }
+  }
 }
 
 module.exports = SortedList;
