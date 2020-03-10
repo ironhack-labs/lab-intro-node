28562c0d85846c4f79a6fb4dc1d87702f012c658
Cristina Garcia <cristinagarciagrillo@gmail.com>
2020-03-10 12:45:13 +0100

    Finished Lab-intro-node

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,17 +1,50 @@
 class SortedList {
-  constructor() {}
+	constructor() {
+		this.items = [];
+		this.length = this.items.length;
+	}
 
-  add(item) {}
+	add(item) {
+		this.items.push(item);
+		this.items.sort((a, b) => a - b);
+		this.length = this.items.length;
+	}
 
-  get(pos) {}
+	get(pos) {
+		if (pos > this.items.length-1 || pos < 0) {
+			throw new Error('OutOfBounds')
+		} else {
+			return this.items[pos];
+		}
+	}
 
-  max() {}
+	max() {
+		if (this.items[0] === undefined) {
+			throw new Error('EmptySortedList');
+		}
+		return Math.max.apply(Math, this.items);
+	}
 
-  min() {}
+	min() {
+	  if (this.items[0] === undefined) {
+        throw new Error("EmptySortedList");
+      }
+	  return Math.min.apply(Math, this.items);
+	}
 
-  sum() {}
+	sum() {
+	  if (this.items[0] === undefined) {
+	    return 0;
+      }
+	  return this.items.reduce((a, b) => a + b);
+	}
 
-  avg() {}
+	avg() {
+	  if (this.items[0] === undefined) {
+        throw new Error("EmptySortedList");
+      }
+	  return this.sum() / this.length;
+	}
 }
 
 module.exports = SortedList;
