2a95577657a364e9c98e54fa351922c2c51ed84a
Miguel Dominguez Chamorro <mdchamorro7@gmail.com>
2020-03-10 12:51:36 +0100

    finish

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,17 +1,52 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort(function(a, b) {
+      return a - b;
+    });
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos > this.items.length-1) {
+      throw new Error("OutOfBounds");
+    } else {
+      return this.items[pos];
+    }
+  }
 
-  max() {}
+  max() {
+    if (this.items.length === 0) {
+      throw new Error("EmptySortedList");
+    } 
+    return this.items[this.length -1];
+  }
 
-  min() {}
+  min() {
+    if (this.items.length === 0) {
+      throw new Error("EmptySortedList");
+    } 
+    return this.items[0];
+  }
+  
+  sum() {
+    if (this.items.length === 0) {
+      return 0;
+    } 
+    return this.items.reduce((a,b) => a + b)
+  }
 
-  sum() {}
-
-  avg() {}
+  avg() {
+    if (this.items.length === 0) {
+      throw new Error("EmptySortedList");
+    } 
+    return this.sum()/this.length;
+  }
 }
 
 module.exports = SortedList;
