15b47705237e9d1cac69cbcee906f4ef3ae145bf
Adrian Vide <adrian.vide.paris@gmail.com>
2020-03-10 12:51:24 +0100

    done

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,17 +1,55 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a,b) => a - b);
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos > this.items.length) {
+      throw new Error("OutOfBounds")
+    } else {
+      return this.items[pos];
+    }
+    
+    // pos > this.items.length ? {throw new Error("OutOfBounds")} :  this.items[pos];
+  }
 
-  max() {}
+  max() {
+    if (this.length === 0) {
+      throw new Error("EmptySortedList");
+    }
+      return Math.max(...this.items);
+  }
 
-  min() {}
+  min() {
+    if (this.length === 0) {
+      throw new Error("EmptySortedList");
+    }
+      return Math.min(...this.items);
+  }
+  
 
-  sum() {}
+  sum() {
 
-  avg() {}
+    if (this.length === 0 ) {
+      return 0;
+    } else {
+      return this.items.reduce((accumulator, value) => accumulator + value);
+    }
+  }
+
+  avg() {
+    if (this.length === 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      return this.sum() / this.length;
+    }
+  }
 }
-
 module.exports = SortedList;
