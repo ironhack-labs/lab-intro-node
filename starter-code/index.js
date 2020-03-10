474a62f329f7278d08b55ff824a8cfa3034e6fe1
Paola <paolavee@gmail.com>
2020-03-10 13:10:01 +0100

    done

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,17 +1,58 @@
 class SortedList {
-  constructor() {}
+  constructor(items, length) {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a,b)=> 
+      a-b);
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if(this.items.find((e)=> e === pos)){
+      return this.items[pos];
+    } else {
+    throw new Error("OutOfBounds");
+  }
+}
 
-  max() {}
+  max() {
+    if(this.items.length === 0){
+      throw new Error("EmptySortedList");
+    } else {
+      return this.items[this.items.length-1];
+    }
+  }
 
-  min() {}
+  min() {
+    if(this.items.length === 0){
+      throw new Error("EmptySortedList");
+    } else {
+      return this.items[0];
+    }
+  }
 
-  sum() {}
+  sum() {
+    
+    if(this.items.length === 0){
+      return 0;
+  } else {
+    let somma = this.items.reduce((a,b)=> a+b)
+    return somma;
+  }
+}
 
-  avg() {}
+  avg() {
+    if(this.items.length === 0){
+      throw new Error("EmptySortedList");
+    } else {
+      let average = this.sum()/this.items.length
+      return average;
+  }
+}
 }
 
 module.exports = SortedList;
