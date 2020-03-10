a999974cfd2b58dd8c79844efff51655d53cd87a
Diablobait <thor.stg@gmail.com>
2020-03-10 12:17:52 +0100

    final release

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,17 +1,54 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort(function compare(a, b) {
+      if (a < b) {
+        return -1;
+      }
+      if (a > b) {
+        return 1;
+      }
+      return 0;
+    });
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos > this.items.length-1 || pos < 0){
+      throw new Error(`_OutOfBounds_`);
+    } 
+    return this.items[pos];
+  }
 
-  max() {}
+  max() {
+    if (this.items[0] === undefined ){
+      throw new Error(`EmptySortedList`);
+    }
+    return Math.max.apply(Math,this.items);
+  }
 
-  min() {}
+  min() {
+    if (this.items[0] === undefined ){
+      throw new Error(`EmptySortedList`)
+    }
+    return Math.min.apply(Math,this.items);
+  }
 
-  sum() {}
+  sum() {
+    if (this.items[0] === undefined ){
+      return 0;
+    }
+    return this.items.reduce((accumulator, currentValue) => {return accumulator + currentValue;});
+  }
 
-  avg() {}
+  avg() {
+    return (this.items.reduce((accumulator, currentValue) => {return accumulator + currentValue;})/this.items.length);
+  }
 }
 
 module.exports = SortedList;
