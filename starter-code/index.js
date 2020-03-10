ca8763ddc8b05fac9113413c772da03b95440520
Unai Gonzalez <unaigoe91@gmail.com>
2020-03-10 12:50:34 +0100

    BCN-FT-Unai Gonzalez

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,17 +1,66 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items  = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
 
-  get(pos) {}
+  add(item) {
+   this.items.push(item)
+   this.items.sort((a,b)=> a-b)
+   this.length = this.items.length;
 
-  max() {}
+  }
 
-  min() {}
+  get(pos) {
 
-  sum() {}
+    if (pos > this.items.length ) {
+      throw new Error("OutOfBounds");
+    }
 
-  avg() {}
+    return this.items[pos]
+   
+  }
+
+  max() {
+
+    if (this.length === 0){
+      throw new Error("EmptySortedList");
+    }
+    
+    return Math.max(...this.items)
+  }
+
+  min() {
+    if (this.length === 0){
+      throw new Error("EmptySortedList");
+    }
+
+    return Math.min(...this.items)
+  }
+
+  sum() {
+  
+    
+    if (this.length === 0) {
+      return 0;
+    } 
+
+    let sum = this.items.reduce((acc, currentv) => acc + currentv)
+    return sum
+
+    
+
+  }
+
+  avg() {
+    if (this.length === 0){
+      throw new Error("EmptySortedList");
+    }
+    let average = this.items.reduce((acc, currentv) => acc + currentv)/this.length;
+    return average
+  }
 }
 
 module.exports = SortedList;
+
