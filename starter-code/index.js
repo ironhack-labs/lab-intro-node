a275aea1c8a1fea0ebb170da96fdac90a0c46a45
Sofia Santa Maria <yolaresofia@gmail.com>
2020-03-10 12:50:40 +0100

    ready

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,17 +1,52 @@
 class SortedList {
-  constructor() {}
+  constructor(items, length) {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.length = this.length + 1;
+    this.items.sort((a,b) => a-b);
+    console.log(this.items);
+  }
 
-  get(pos) {}
+  get(pos) {
+    if(this.items[pos]) {
+      return this.items[pos];
+    } else {
+      throw new Error("OutOfBounds");
+    }
+  }
 
-  max() {}
+  max() {
+    if(this.items[this.length-1]){
+      return this.items[this.length-1];
+    } else {
+      throw new Error("EmptySortedList");
+    }
+  }
 
-  min() {}
+  min() {
+    if(this.items[0]){
+      return this.items[0];
+    } else {
+      throw new Error("EmptySortedList");
+    }
+  }
 
-  sum() {}
+  sum() {
+    if (this.items.length === 0){
+      return 0;
+    } else {
+      return this.items.reduce((a,b) => (a+b));
+    }
+  }
 
-  avg() {}
-}
+  avg() {
+      return this.items.reduce((a,b) => (a+b)) / this.items.length;
+    }
+
+  }
 
 module.exports = SortedList;
