b4df1480d8b35a3137ae3addc1285fdc1da787bb
Raul Marchan <alountk@gmail.com>
2020-03-10 10:17:34 +0100

    [BCN-WDFT-RAUL]-done

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,17 +1,72 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items=[];
+    this.length=this.items.length;
 
-  add(item) {}
+  }
 
-  get(pos) {}
+  add(item) {
+    // var newArra=this.items;
+    // newArra.push(item);
+    this.length++;
+    this.items.push(item);
+    // newArra.sort((a, b) => a - b);
+    this.items.sort((a, b) => a - b);
+    // return newArra;
+    return this.items;
+  }
 
-  max() {}
+  get(pos) {
+    if(this.items.find(element => element === pos)){
+      return this.items[pos];
+    }else{
+      return OutOfBounds;
+    }
+  }
 
-  min() {}
+  max() {
+    let arra=this.items;
+    if( this.length === 0){
+      return EmptySortedList;
+    }else{
+      var max = Math.max(...arra);
+      return max;
+    } 
+  }
 
-  sum() {}
+  min() {
+    var arra=this.items;
+    if(this.length === 0){
+      return EmptySortedList;
+    }else{
+      var min = Math.min(...arra);
+      return min;
+    }
+  }
 
-  avg() {}
+  sum() {
+    var arra=this.items;
+    if(this.length === 0){
+      return 0;
+    }else{
+      var total = arra.reduce((acu,next)=>{
+        return acu+next;
+      });
+    return total;
+    }
+  }
+
+  avg() {
+    var arra=this.items;
+    if(this.length === 0){
+      return EmptySortedList;
+    }else{
+      var total = arra.reduce((acu,next)=>{
+        return acu+next;
+      });
+      return total/this.length;
+    }
+  }
 }
 
 module.exports = SortedList;
