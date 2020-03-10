d81bd6735d55db031f695085991d21d18e8ba1dc
Pau Larrea <paularrea12@hotmail.com>
2020-03-10 12:44:02 +0100

    node

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,17 +1,50 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a,b)=>a-b);
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if(pos >= this.length){
+      throw new error ('OutOfBounds');
+    }
+      return this.items[pos];
+}
 
-  max() {}
+  max() {
+    if(this.length === 0){
+      return EmptySortedList;
+    }
+      return this.items[this.length-1];
+  }
 
-  min() {}
+  min() {
+    if(this.length === 0){
+      return EmptySortedList;
+    }
+      return this.items[0]
+  }
 
-  sum() {}
+  sum() {
+    if (this.length === 0){
+      return 0
+    }
+    return this.items.reduce((a, b) => a + b, 0);
+    }
 
-  avg() {}
+
+  avg() {
+    if(this.length === 0){
+      return EmptySortedList;
+    }
+    return (this.items.reduce((a, b) => a + b, 0)/this.length)
+  }
 }
 
 module.exports = SortedList;
