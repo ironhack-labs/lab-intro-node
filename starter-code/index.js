3a0453dd2d38f66eae08045d3a9e8ae0d7319145
Paula Marsal <paula.mz_376@live.com>
2020-03-10 13:09:58 +0100

    I win

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,17 +1,57 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item)
+    this.items.sort((a,b) => a - b);
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (this.items.find(element => element === pos )) {
+      return this.items[pos];
+      
+    } else {
+      throw new Error("OutOfBounds");
+    }
+  }
+  
+  max() {
+    if (this.length !== 0) {
+      return this.items[this.length-1];
+      
+    } else {
+      throw new Error("EmptySortedList");
+    }
+  }
+  
+  min() {
+    if (this.length !== 0) {
+      return this.items[0];
+      
+    } else {
+      throw new Error("EmptySortedList");
+    } 
+  }
 
-  max() {}
-
-  min() {}
-
-  sum() {}
-
-  avg() {}
+  sum() {
+    if(this.length === 0){
+      return 0;
+    }
+    return this.items.reduce((a,b) => a+b);
+  }
+  
+  avg() {
+    if(this.length === 0){
+      throw new Error("EmptySortedList");
+    }
+    return this.sum()/this.length;
+  }
 }
+
+
 
 module.exports = SortedList;
