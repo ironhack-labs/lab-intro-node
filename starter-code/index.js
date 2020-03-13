4f934a7f2cb2391e801eb706f52afe8b9277130b
Caio Andreolli <caioandreolli@gmail.com>
2020-03-13 00:58:28 -0300

    Lab Finalizado

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,17 +1,54 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.length = this.items.length;
+    this.items.sort((a,b) => { return a-b });
+  }
 
-  get(pos) {}
+  get(pos) {
+    if(!this.items[pos]) {
+      throw new Error("OutOfBounds");
+    } else {
+      return this.items[pos];
+    }
+  }
 
-  max() {}
+  max() {
+    if(this.length > 0){
+      return Math.max(...this.items);
+    } else {
+      throw new Error("EmptySortedList");
+    }
+  }
 
-  min() {}
+  min() {
+    if(this.length > 0){
+      return Math.min(...this.items);
+    } else {
+      throw new Error("EmptySortedList");
+    }
+  }
 
-  sum() {}
+  sum() {
+    if(this.length > 0){
+      return this.items.reduce((a,b) => a+b, 0);
+    } else {
+      return 0;
+    }
+  }
 
-  avg() {}
+  avg() {
+    if(this.length > 0){
+      return (this.sum())/this.length;
+    } else {
+      throw new Error("EmptySortedList");
+    }
+  }
 }
 
 module.exports = SortedList;
