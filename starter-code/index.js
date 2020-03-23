1788eb113b49ee4358d717553f3a5e723195b7ed
“Marcela <marcelavieirafreire@gmail.com>
2020-03-23 16:04:15 -0300

    Exercise done

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,17 +1,58 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = 0;
 
-  add(item) {}
+  }
+  add(item) {
+    //  return this.items.sort((a, b) => a - b).push(item);
+    this.length = this.items.push(item)
+    this.items.sort((a, b) => a - b);
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos > this.length) {
+      throw new Error("OutOfBounds");
+    } else {
+      return this.items[pos];
+    }
+  }
 
-  max() {}
+  max() {
+if (this.length !== 0) {
+  return this.items[this.length -1];
+} else {
+  throw new Error("EmptySortedList");
+}
+  
+  }
 
-  min() {}
+  min() {
+    if (this.length !== 0) {
+      return this.items[0];
+    } else {
+      throw new Error("EmptySortedList");
+    }
 
-  sum() {}
+  }
 
-  avg() {}
+  sum() {
+    if (this.length > 0) {
+    return this.items.reduce((a, b) => a + b);
+    } else {
+      return 0;
+    }
+  }
+
+  avg() {
+    if (this.length > 0) {
+      return this.items.reduce((a, b) => a + b) / this.length;
+      } else {
+        throw new Error("EmptySortedList");
+      }
+  }
 }
 
 module.exports = SortedList;
+
+const list = new SortedList;
