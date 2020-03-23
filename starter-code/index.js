ccc3a386e8ab3d0e8b9b7c28571e0e24e380b9c1
Gabriel Bohn <gabrielcbohn@gmail.com>
2020-03-23 14:41:11 -0300

    Done

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,17 +1,56 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items = this.items.sort((a, b) => a - b);
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos >= this.length) {
+      throw new Error("OutOfBounds");
+    } else {
+      return this.items[pos];
+    }
+  }
 
-  max() {}
+  max() {
+    let maxNumber = this.items;
+    if (this.length < 1) {
+      throw new Error("EmptySortedList");
+    } else {
+      return Math.max(...maxNumber);
+    }
+  }
 
-  min() {}
+  min() {
+    let minNumber = this.items;
+    if (this.length < 1) {
+      throw new Error("EmptySortedList");
+    } else {
+      return Math.min(...minNumber);
+    }
+  }
 
-  sum() {}
+  sum() {
+    let sum = 0;
+    this.items.forEach(item => {
+      sum += item;
+    });
+    return sum;
+  }
 
-  avg() {}
+  avg() {
+    if (this.length < 1) {
+      throw new Error("EmptySortedList");
+    } else {
+      return this.sum() / this.length;
+    }
+  }
 }
 
 module.exports = SortedList;
