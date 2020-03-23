27d5813f5fa689f8be9afbaecac0a30d91522de4
Rafael Freitas <rafaelborges1985@hotmail.com>
2020-03-23 15:03:51 -0300

    Finish implementing SortedList and passed all tests

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,17 +1,51 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [1];//array
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);//includes in the array
+    this.items.sort((a,b) => a - b);
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if(this.items[pos] === undefined){
+      throw new Error("OutOfBounds");
+    } else {
+      return this.items[pos];
+    }
+  }
 
-  max() {}
+  max() {
+    if(this.items.length === 0){
+      throw new Error("EmptySortedList");
+    } else {
+      return this.items[this.length - 1];
+    }
+  }
+  
+  min() {
+    if(this.items.length === 0){
+      throw new Error("EmptySortedList");
+    } else {
+      return this.items[0];
+    }
+  }
 
-  min() {}
+  sum() {
+    return this.items.reduce((acc, cur) => acc + cur, 0);
+  }
 
-  sum() {}
-
-  avg() {}
+  avg() {
+    let sum = this.sum();
+    if(this.length === 0){
+      throw new Error('EmptySortedList');
+    } else {
+      return sum / this.length;
+    }
+  }
 }
 
 module.exports = SortedList;
