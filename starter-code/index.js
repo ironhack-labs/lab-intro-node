ba76891f119e3f81467e0d80d35d5880768a9aa7
PiaG <piagmdf@gmail.com>
2020-03-28 16:36:40 +0100

    done, including bonus

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,17 +1,76 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = []
+    this.length = this.items.length
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item)
+    this.items.sort((first, last) => {
+      return first - last
+    })
+    this.length += 1
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos > this.length) {
+      throw new Error("OutOfBounds")
+    } else {
+      return this.items[pos]
+    }
+  }
 
-  max() {}
+  max() {
+    if (this.length === 0) {
+      throw new Error("EmptySortedList")
+    } else {
+      this.items.sort((first, last) => {
+        first - last
+      })
+      return this.items[this.length - 1]
+    }
+  }
 
-  min() {}
+  min() {
+    if (this.length === 0) {
+      throw new Error("EmptySortedList")
+    } else {
+      this.items.sort((first, last) => {
+        first - last
+      })
+      return this.items[0]
+    }
+  }
 
-  sum() {}
+  sum() {
+    let sumArray = 0
+    if (this.length === 0) {
+      return sumArray = 0
+    } else {
+      for (let i = 0; i < this.length; i++) {
+        sumArray += this.items[i]
+      }
+      return sumArray
+    }
+  }
 
-  avg() {}
+  avg() {
+    if (this.length === 0) {
+      throw new Error("EmptySortedList")
+    } else {
+      // let sumArray = 0
+      // if (this.length === 0) {
+      //   sumArray = 0
+      // } else {
+      //   for (let i = 0; i < this.length; i++) {
+      //     sumArray += this.items[i]
+      //   }
+      //   sumArray
+      // }
+      let avgArray = (this.sum() / this.length)
+      return avgArray
+    }
+  }
 }
 
 module.exports = SortedList;
