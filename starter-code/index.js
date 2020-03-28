23f64ba9022ffcf982c2867aee9317d59bdc1678
Solange Ohana <solangeohana@gmail.com>
2020-03-28 17:41:54 +0100

    BER_PT2020-Solange

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,17 +1,60 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = []
+    this.length = this.items.length
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item)
+    this.length = this.items.length
+    this.items.sort((a, b) => {return (a-b)})
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (!this.items[pos]) {
+      throw new Error("OutOfBounds");
+    }
+    else {
+      return this.items[pos]
+    }
+  }
 
-  max() {}
+  max() {
+    if (this.length === 0){
+      throw new Error("EmptySortedList");
+    }
+    else {
+      return Math.max(...this.items)
+    }
+  }
 
-  min() {}
 
-  sum() {}
+  min() {
+    if (this.length === 0){
+      throw new Error("EmptySortedList");
+    }
+    else {
+      return Math.min(...this.items)
+    }
+  }
 
-  avg() {}
+  sum() {
+    if(this.length > 0){
+      return this.items.reduce((a, b) => (a + b), 0)
+    }
+    else {
+      return 0;
+    }
+  }
+
+  avg() {
+    if (this.length > 0){
+      return this.sum()/this.length
+    }
+    else {
+      throw new Error("EmptySortedList");
+    }
+  }
 }
 
 module.exports = SortedList;
