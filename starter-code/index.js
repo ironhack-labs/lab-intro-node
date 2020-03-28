ebb3d310b90c8e57b1365b1d6e4d8081ee0e3bf3
Francesco Saccone <francescosaccone95@live.it>
2020-03-28 16:48:19 +0100

    Francesco WEBDEVPARTIME BERLIN Intronodelab finished

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,17 +1,71 @@
 class SortedList {
-  constructor() {}
+  constructor() 
+  {
+     this.items=[]
+     this.length=this.items.length
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item)
+    this.items.sort(function(a,b) {return a - b})
+    this.length=this.items.length
+  }
 
-  get(pos) {}
+  get(pos) {
+    if( pos > this.items.length)
+    {
+      throw new Error("OutOfBounds");
+    }
+    else
+    {
+      return this.items[pos]
+    }
+  }
 
-  max() {}
+  max() 
+  {
+    if (this.items.length === 0)
+    {
+      throw new Error("EmptySortedList");
 
-  min() {}
+    }
+    else
+    {
+      return this.items[this.length - 1]
+    }
+  }
 
-  sum() {}
+  min() 
+  {
+    if (this.items.length === 0)
+    {
+      throw new Error("EmptySortedList");
 
-  avg() {}
-}
+    }
+    else
+    {
+      return this.items[0]
+    }
+  }
+
+  sum() {
+    return this.items.reduce((sum, number) => {
+      return sum + number
+    },0)
+  }
+
+  avg() 
+  {
+    if( this.length === 0)
+    {
+      throw new Error("EmptySortedList")
+    }
+    else
+    {
+    return this.sum()/this.length
+    }
+    }
+    
+  }
 
 module.exports = SortedList;
