c6d52b3e4ae0b59f379e51b02ab3b9eadaaa65c0
Sjard Geiersbach <sjard.g@gmail.com>
2020-03-28 17:00:26 +0100

    WDPT Berlin Sjard

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,17 +1,39 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = []
+    this.length = 0
+  }
 
-  add(item) {}
+  add(item) {
+this.items.push(item)
+this.length = this.items.length
+this.items.sort((a,b) => a-b)
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos > this.length) {throw new Error("OutOfBounds");}
+    return this.items[pos]
+  }
 
-  max() {}
+  max() {
+    if (this.length === 0) {throw new Error("EmptySortedList");}
+   return Math.max.apply(Math, this.items)
+  }
 
-  min() {}
+  min() {
+    if (this.length === 0) {throw new Error("EmptySortedList");}
+    return Math.min.apply(Math, this.items)
+  }
 
-  sum() {}
+  sum() {
+    if (this.length === 0) {return 0}
+    return this.items.reduce((a, b) => a + b, 0)
+  }
 
-  avg() {}
+  avg() {
+    if (this.length === 0) {throw new Error("EmptySortedList");}
+    return (this.items.reduce((a, b) => a + b, 0)/this.length)
+  }
 }
 
 module.exports = SortedList;
