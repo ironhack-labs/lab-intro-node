b7254d572e32e49dfff3236a894076d59e91fdd2
ian-kerins <hello@iankerins.com>
2020-03-28 15:53:05 +0000

    done

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,17 +1,74 @@
 class SortedList {
-  constructor() {}
+  constructor(items, length) {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a, b) => {
 
-  get(pos) {}
+     if(a > b){
+       return 1
+     } if (a < b){
+       return -1
+     } else {
+       return 0
+     }
+    })
 
-  max() {}
+    this.length = this.items.length;    
+  }
 
-  min() {}
+  get(pos) {
+    if(this.items[pos] == null){
+      throw new Error("OutOfBounds");
+    } else{
+      return this.items[pos];
+    }
+    
+  }
 
-  sum() {}
+  max() {
+    if(this.items.length == 0){
+       throw new Error("EmptySortedList");
+    } else {
+      return Math.max(...this.items);
+    }
+  }
 
-  avg() {}
+  min() {
+    if(this.items.length == 0){
+      throw new Error("EmptySortedList");
+   } else {
+     return Math.min(...this.items);
+   }
+  }
+
+  sum() {
+    if(this.items.length == 0){
+      return 0
+    } else {
+    let sum = this.items.reduce((sum, value) => {sum += value; return sum});
+    return sum;
+    }
+  }
+
+  avg() {
+    if(this.length == 0){
+      throw new Error("EmptySortedList");
+    } else {
+      let sum = this.items.reduce((sum, value) => {sum += value; return sum});
+
+      return sum/this.length;
+    }
+    
+  }
 }
 
 module.exports = SortedList;
+
+// let list = new SortedList();
+// list.add(2);
+// list.add(1);
+// console.log(list);
