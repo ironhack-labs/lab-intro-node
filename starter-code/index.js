bdd4ce55de07b55db51d844fecbe3df0eedf6022
dkoushha <dimakoushha@gmail.com>
2020-03-29 17:24:31 +0200

    done

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,17 +1,55 @@
 class SortedList {
-  constructor() {}
+  constructor(items, length) {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a, b) => a - b);
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos <= this.items.length) {
+      return this.items.indexOf(pos);
+    } else {
+      throw new Error("OutOfBounds");
+    }
+  }
 
-  max() {}
+  max() {
+    if (this.items.length > 0) {
+      return Math.max(...this.items);
+    } else {
+      throw new Error("EmptySortedList");
+    }
+  }
 
-  min() {}
+  min() {
+    if (this.items.length > 0) {
+      return Math.min(...this.items);
+    } else {
+      throw new Error("EmptySortedList");
+    }
+  }
 
-  sum() {}
+  sum() {
+    let sum = 0;
+    if (this.items.length <= 0) {
+      return sum;
+    } else {
+      return this.items.reduce((sum, item) => sum + item, 0);
+    }
+  }
 
-  avg() {}
+  avg() {
+    if (this.items.length <= 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      return this.items.reduce((sum, item) => sum + item, 0) / this.items.length
+    }
+  }
 }
 
 module.exports = SortedList;
