8feec20e6020c068d8d528471ab15f578bed2029
rod-amador <rodrigo_betan@hotmail.com>
2020-03-30 14:38:57 -0600

    WEB-MEX-PT2 RODRIGO AMADOR

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,17 +1,42 @@
 class SortedList {
-  constructor() {}
+  constructor(items, length) {
+    this.items = []
+    this.length = 0
+  }
 
-  add(item) {}
+  add(item) {
+   
+    this.length = this.items.push(item);
+    this.items.sort(  (a,b) => a-b  )
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos > this.items.length) {throw new Error("OutOfBounds");}
+    else                         {return this.items[pos]} 
+          }
 
-  max() {}
+  max() {
+    if (this.items.length == 0) {throw new Error("EmptySortedList");}
+    else                        {return Math.max(...this.items)} 
+          }
 
-  min() {}
+  min() {
+    if (this.items.length == 0) {throw new Error("EmptySortedList");}
+    else                        {return Math.min(...this.items)} 
+  }
 
-  sum() {}
+  sum() {
+    if (this.items.length == 0) {return 0;}
+    else                        {return this.items.reduce( (a,b) => a+b, 0  )} 
+  }
 
-  avg() {}
+  avg() {
+    if (this.items.length == 0) {throw new Error("EmptySortedList");}
+    else {let sum = (this.items.reduce( (a,b) => a+b, 0  ))/ this.items.length
+    
+      return  sum}
+    
+  }
 }
 
 module.exports = SortedList;
