9dd106e3cbbef7ae1e814ac249fd4b2e88c9032f
Edison Abdiel <edison.abdiel@soundcloud.com>
2020-03-31 19:26:04 +0200

    Done

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,17 +1,45 @@
 class SortedList {
-  constructor() {}
+  constructor(items, length) {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item)
+    this.length++
+    this.items.sort((a, b) => {
+      return a - b
+    })
+   
 
-  get(pos) {}
+  }
 
-  max() {}
+  get(i) { 
+      if ( i > this.length - 1) {
+        throw new Error("OutOfBounds")
+      } else {
+        return this.items[i]
+      }
+  }
+  
+  max() {
+    if (this.length === 0) { throw new Error('EmptySortedList') }
+    return this.items[this.items.length -1]
+   }
 
-  min() {}
+  min() {
+    if (this.length === 0) { throw new Error('EmptySortedList') }
+    return this.items[0]
+   }
 
-  sum() {}
+  sum() {
+    return this.items.reduce((sum, el) => sum + el, 0)
+   }
 
-  avg() {}
+  avg() { 
+    if (this.length === 0) { throw new Error('EmptySortedList') }
+        return  this.sum / this.length
+    }
 }
 
 module.exports = SortedList;
