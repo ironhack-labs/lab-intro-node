228f8cfe832d44a0747d0d6d55bd2c4481ae0fb9
Josafat Solis <jsolis@macroaplicaciones.com>
2020-03-31 06:58:53 -0600

    done

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,17 +1,49 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a,b) => a - b);
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos >= this.items.length) throw new Error("OutOfBounds");
+    // Sí contiene el ítem
+    return this.items[pos];
+  }
 
-  max() {}
+  max() {
+    if (this.items.length === 0) throw new Error("EmptySortedList");
+    return Math.max(...this.items);
+  }
 
-  min() {}
+  min() {
+    if (this.items.length === 0) throw new Error("EmptySortedList");
+    return Math.min(...this.items);
+  }
 
-  sum() {}
+  sum() {
+    if (this.items.length === 0) return 0;
+    return this.items.reduce((acc, val) => acc += val);
+  }
 
-  avg() {}
+  avg() {
+    if (this.items.length === 0) throw new Error("EmptySortedList");
+    return this.items.reduce((acc, val) => acc += val) / this.items.length;
+  }
 }
 
 module.exports = SortedList;
+
+// const sl = new SortedList();
+// sl.add(1);
+// sl.add(3);
+// sl.add(2);
+// console.log(sl.items);
+
+// a = sl.max();
+// console.log(a);
