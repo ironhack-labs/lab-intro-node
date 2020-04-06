91cf36a0057ad353954084c06445a414e0d0e8e6
Bruno <bpais@outlook.pt>
2020-04-06 22:57:03 +0100

    done

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,17 +1,57 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    // if(typeof(item) !== 'number'){
+    //   item.forEach(element => {      
+    //     this.items.push(item);
+    //     this.items.sort();
+    //   });
+    // }
+    this.items.push(item);
+    this.items.sort(
+      function(a,b) {
+        return a-b;
+      }
+    ); this.length++;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if(pos > this.items.length){
+      throw new Error("OutOfBounds");
+    }
 
-  max() {}
+    return this.items[pos];
+  }
 
-  min() {}
+  max() {
+    if(this.items.length === 0) {
+      throw new Error("EmptySortedList");
+    }
+    return Math.max(...this.items);
+  }
 
-  sum() {}
+  min() {
+    if(this.items.length === 0) {
+      throw new Error("EmptySortedList");
+    }
+    return Math.min(...this.items);
+  }
 
-  avg() {}
+  sum() {
+    return this.items.reduce((a,b)=>{return a+b}, 0);
+  }
+
+  avg() {
+    if(this.length === 0) {
+      throw new Error("EmptySortedList");
+    }
+
+    return this.items.reduce((a,b)=>{return a+b}, 0)/this.length;
+  }
 }
 
 module.exports = SortedList;
