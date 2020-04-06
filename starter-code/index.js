71abc70dda11e6eff023430767d4ad0836d1412d
Mihaela Mateescu <miha.mateescu@gmail.com>
2020-04-06 21:50:49 +0100

    done

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,17 +1,49 @@
 class SortedList {
-  constructor() {}
+  constructor(items, length) {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.length = this.items.length;
+    this.items.sort((a,b) => a-b);
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos > this.length) {
+      throw new Error("OutOfBounds")
+    }
+    return this.items[pos]
+  }
 
-  max() {}
+  max() { 
+if (!this.length) {
+  throw new Error('EmptySortedList')
+} 
+return Math.max(...this.items)
+  }
 
-  min() {}
+  min() {
+    if (!this.length) {
+      throw new Error('EmptySortedList')
+    } 
+    return Math.min(...this.items)
+      }
 
-  sum() {}
+  sum() {
+    if (!this.items.length) {
+      return 0;
+    }
+    return this.items.reduce((a,b) => a+b);
+  }
 
-  avg() {}
+  avg() {
+if (!this.length) {
+  throw new Error ('EmptySortedList');
+}
+    return this.sum() / this.length  
+  }
 }
 
 module.exports = SortedList;
