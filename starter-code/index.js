93658614a1e6610efcd2583be399269fed387a5f
Yai <myd.rio@hotmail.com>
2020-04-13 16:15:22 +0200

    done

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,17 +1,59 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = []
+    this.length = 0
+  }
+  add(item) {
+    this.items.push(item)
+    this.items.sort((a, b) => a - b)
+    
+    return this.length = this.items.length
+  }
 
-  add(item) {}
+  get(pos) {
+    if (pos > this.length) {
+      throw new Error("OutOfBounds");
+    }
+    return this.items[pos]
+  }
 
-  get(pos) {}
+  max() {
+    if (this.length === 0) {
+      throw new Error("EmptySortedList")
+    }
+    this.items.sort((a, b) => b - a)
+    return this.items[0]
+  }
 
-  max() {}
+  min() {
+    if (this.length === 0) {
+      throw new Error("EmptySortedList")
+    }
+    this.items.sort((a, b) => a - b)
+    return this.items[0]
 
-  min() {}
+  }
 
-  sum() {}
+  sum() {
+    return this.length === 0 ? 0 : this.items.reduce((acc, elm) => (acc + elm), 0);
+  }
 
-  avg() {}
+  avg() {
+    if (this.length === 0) {
+      throw new Error("EmptySortedList")
+    }
+
+    return this.sum() / this.length
+  }
+
 }
 
-module.exports = SortedList;
+module.exports = SortedList
+
+let prueba = new SortedList()
+prueba.add(1)
+prueba.add(4)
+prueba.add(7)
+console.log(prueba.sum());
+console.log(prueba.length);
+console.log(prueba.avg());
