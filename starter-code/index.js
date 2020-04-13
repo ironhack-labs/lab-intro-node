b0519be4ecccd0a276027995796117e9edc4aea8
Elvira Ramírez <elvirarp92@gmail.com>
2020-04-13 17:15:42 +0200

    define SortedList.add(item) method

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,7 +1,16 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = []
+    this.length = this.items.length
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item)
+    this.items.sort((a, b) => {
+      return a - b
+    })
+    this.length = this.items.length
+  }
 
   get(pos) {}
 
@@ -14,4 +23,4 @@ class SortedList {
   avg() {}
 }
 
-module.exports = SortedList;
+module.exports = SortedList
