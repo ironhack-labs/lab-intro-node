bc9401519a2ee0dcb63e1d5fdea0ea06c4d4015d
Samuel Littlefair-Wallace <sam@littlefairwallace.ca>
2020-04-13 10:47:31 +0200

    Refactor

--- starter-code/index.js
+++ starter-code/index.js
@@ -5,44 +5,32 @@ class SortedList {
   }
 
   add(item) {
-    this.items.push(item);
-    this.items = this.items.sort(function (a, b) {
-      return a - b;
-    });
-    this.length = this.items.length;
+    this.length = this.items.push(item);
+    this.items = this.items.sort((a, b) => a - b);
   }
 
   get(pos) {
-    if (pos > this.length - 1) {
-      throw new Error("OutOfBounds");
-    }
+    if (pos > this.length - 1) throw new Error("OutOfBounds");
     return this.items[pos];
   }
 
   max() {
-    if (this.length === 0) {
-      throw new Error("EmptySortedList");
-    }
+    if (this.length === 0) throw new Error("EmptySortedList");
     return this.items[this.length - 1];
   }
 
   min() {
-    if (this.length === 0) {
-      throw new Error("EmptySortedList");
-    }
+    if (this.length === 0) throw new Error("EmptySortedList");
     return this.items[0];
   }
 
   sum() {
-    return this.items.reduce(function (accumulator, currentValue) {
-      return (currentValue += accumulator);
-    }, 0);
+    const reducer = (acc, cur) => cur + acc;
+    return this.items.reduce(reducer, 0);
   }
 
   avg() {
-    if (this.length === 0) {
-      throw new Error("EmptySortedList");
-    }
+    if (this.length === 0) throw new Error("EmptySortedList");
     return this.sum() / this.length + 0;
   }
 }
