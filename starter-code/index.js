a2ec7e09700060a39b25b2c0377b7ac9e2e5be4d
Gabriela Gallango <ga.gallango@gmail.com>
2020-04-13 20:01:00 +0200

    add

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,17 +1,60 @@
 class SortedList {
-  constructor() {}
+  constructor(items) {
+    this.items = []
+    this.length = this.items.length
+  }
 
-  add(item) {}
 
-  get(pos) {}
+  add(item) {
 
-  max() {}
+    this.length = 1
+    this.items.push(item)
+    this.items.sort(function (a, b) {
+      return a - b
+    })
 
-  min() {}
+  }
 
-  sum() {}
+  get(pos) {
 
-  avg() {}
+    if (pos >= this.items.length) {
+      throw new Error("OutOfBounds")
+    } else {
+      return this.items.slice(pos, pos + 1)
+    }
+
+  }
+
+  max() {
+
+    if (this.items.length === 0) {
+      throw new Error("EmptySortedList")
+    } else {
+      return this.items.slice(-1)
+    }
+
+  }
+
+  min() {
+    if (this.items.length === 0) {
+      throw new Error("EmptySortedList")
+    } else {
+      return this.items.slice(0, 1)
+    }
+
+  }
+
+  sum() {
+    return this.items.length === 0 ? 0 : this.items.reduce((a, b) => (a + b), 0)
+  }
+
+  avg() {
+    if (this.items.length === 0) {
+      throw new Error("EmptySortedList");
+    }
+
+    return this.sum() / this.items.length
+  }
 }
 
 module.exports = SortedList;
