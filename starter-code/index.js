e4e0b475f16f2fb711f7caae3b67ae1fc9feda2a
Madalin George <madalin.lingurar@gmail.com>
2020-04-13 16:08:25 +0200

    done

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,17 +1,87 @@
 class SortedList {
-  constructor() {}
+    constructor() {
 
-  add(item) {}
+        this.items = []
+        this.length = this.items.length
 
-  get(pos) {}
+    }
 
-  max() {}
+    add(item) {
 
-  min() {}
+        this.length++
+            this.items.push(item)
+        this.items.sort((a, b) => a - b)
 
-  sum() {}
 
-  avg() {}
+    }
+
+    get(pos) {
+
+        if (pos < this.length) {
+            return this.items[pos]
+        } else {
+            throw new Error('OutOfBounds')
+        }
+
+
+    }
+
+    max() {
+
+        if (this.length < 1) {
+            throw new Error('OutOfBounds')
+
+        } else {
+
+            return Math.max(...this.items)
+
+
+
+        }
+
+    }
+
+    min() {
+
+        if (this.length < 1) {
+            throw new Error('OutOfBounds')
+
+        } else {
+
+            return Math.min(...this.items)
+
+
+
+        }
+
+    }
+
+    sum() {
+        if (this.length < 1) {
+            return 0
+
+        } else {
+
+            return this.items.reduce((a, b) => a + b, 0)
+
+
+
+        }
+
+    }
+
+    avg() {
+        if (this.length < 1) {
+            throw new Error('EmptySortedList')
+
+        } else {
+
+            return this.items.reduce((a, b) => a + b, 0) / this.items.length
+
+
+
+        }
+    }
 }
 
 module.exports = SortedList;
