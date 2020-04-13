302cea64ab1acff7ee288b4e15e5022ff0caf9c9
Julian Bajo <jbajopinedo@gmail.com>
2020-04-13 19:58:43 +0200

    Ejercicio completado, con algunos dificultades con la maquina de testeo al hacer la refactorizacion

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,17 +1,59 @@
 class SortedList {
-  constructor() {}
+  constructor() {
 
-  add(item) {}
+    this.items = []
+    this.length = this.items.length
+  }
 
-  get(pos) {}
+  add(item) {
 
-  max() {}
+    this.items.push(item)
+    this.items.sort((a, b) => a - b)
 
-  min() {}
+    return this.length++
+  }
 
-  sum() {}
+  get(pos) {
 
-  avg() {}
+    if (pos > this.length) throw new Error("OutOfBounds")
+
+    return this.items[pos]
+
+    //throw (pos > this.length) ? new Error("OutOfBounds") : this.items[pos]
+
+  }
+
+  max() {
+
+    if (this.length <= 0) throw new Error("EmptySortedList")
+
+    // Math.max.apply(null, this.items)   
+
+    return Math.max(...this.items)
+  }
+
+  min() {
+
+    if (this.length <= 0) throw new Error("EmptySortedList")
+
+    return Math.min(...this.items)
+  }
+
+  sum() {
+    // Math.sum(this.items)
+
+    return this.length > 0 ? this.items.reduce((a,b) => a + b, 0) : 0
+  }
+
+  avg() {
+
+
+    if (this.length <= 0) throw new Error("EmptySortedList")
+
+    return this.sum()/this.length
+    
+    //return this.length > 0 ? this.sum()/this.length : 0
+  }
 }
 
 module.exports = SortedList;
