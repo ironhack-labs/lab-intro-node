9691aa367e490f23016ead915124dbfdee311554
dionisio ugalde <dionisiougalde@iMac-de-dionisio-8.local>
2020-04-14 14:30:02 +0200

    done

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,17 +1,57 @@
 class SortedList {
-  constructor() {}
+  constructor(items, length) {
+    (this.items = []), (this.length = this.items.length);
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
 
-  get(pos) {}
+    this.items.sort(function (a, b) {
+      return a - b;
+    });
 
-  max() {}
+    this.length = this.items.length;
+  }
 
-  min() {}
+  get(pos) {
+    if (pos < this.items.length) {
+      return this.items[pos];
+    } else {
+      throw new Error("OutOfBounds");
+    }
+  }
 
-  sum() {}
+  max() {
+    if (this.items.length > 0) {
+      return Math.max(...this.items);
+    } else {
+      throw new Error("EmptySortedList");
+    }
+  }
 
-  avg() {}
+  min() {
+    if (this.items.length > 0) {
+      return Math.min(...this.items);
+    } else {
+      throw new Error("EmptySortedList");
+    }
+  }
+
+  sum() {
+    if (this.items.length > 0) {
+      return this.items.reduce((a, b) => a + b, 0);
+    } else {
+      return 0;
+    }
+  }
+
+  avg() {
+    if (this.items.length > 0) {
+      return this.items.reduce((a, b) => a + b, 0) / this.length;
+    } else {
+      throw new Error("EmptySortedList");
+    }
+  }
 }
 
 module.exports = SortedList;
