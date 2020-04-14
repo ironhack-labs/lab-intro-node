dcc6c635f1877efadafbfb358c399740d2da8a78
Phillip Paul <phillippaul@gmx.de>
2020-04-14 14:21:09 +0200

    done

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,17 +1,53 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a, b) => {
+      return a - b;
+    });
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos >= this.length) {
+      throw new Error("OutOfBounds");
+    }
+    return this.items[pos];
+  }
 
-  max() {}
+  max() {
+    if (this.length == 0) {
+      throw new Error("EmptySortedList");
+    }
+    return Math.max(...this.items); //prevents NaN and returns highest also with programmatically created arrays like in this lab
+  }
 
-  min() {}
+  min() {
+    if (this.length == 0) {
+      throw new Error("EmptySortedList");
+    }
+    return Math.min(...this.items); // spread operator, see max() above
+  }
 
-  sum() {}
+  sum() {
+    if (this.length == 0) {
+      return 0;
+    }
+    return this.items.reduce((acc, val) => {
+      return acc + val;
+    }, 0);
+  }
 
-  avg() {}
+  avg() {
+    if (this.length == 0) {
+      throw new Error("EmptySortedList");
+    }
+    return this.sum() / this.length;
+  }
 }
 
 module.exports = SortedList;
