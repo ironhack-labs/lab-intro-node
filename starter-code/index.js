2d127ea59478f5df0e372a6b846a36da7ced7d0b
paulamoellendorf <61729055+paulamoellendorf@users.noreply.github.com>
2020-04-14 22:02:50 +0200

    done

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,17 +1,56 @@
+
 class SortedList {
-  constructor() {}
+  constructor() {
+   this.items=[];
+   this.length=this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort(function(a, b){return a-b });
+    return this.length=this.items.length;
+  }
 
-  get(pos) {}
 
-  max() {}
+  get(pos) {
+    if(pos>=this.length){
+      throw new Error("OutOfBounds");
+    }
+     return this.items[pos];
+  }
 
-  min() {}
+  max() {
+    if(this.length===0){
+      throw new Error("EmptySortedList");
+    } else{
+      return this.items[this.length - 1];
+    }
+  }
 
-  sum() {}
+  min() {
+    if (this.length === 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      return this.items[0];
+    } 
+  }
 
-  avg() {}
-}
+
+    sum() {
+      return this.items.reduce((acc, val) => {
+        return acc + val;
+      }, 0);
+    }
+  
+
+    avg() {
+      if (this.length === 0) {
+        throw new Error("EmptySortedList");
+      } else {
+        return this.sum() / this.length;
+      }
+    }
+  }
+
 
 module.exports = SortedList;
