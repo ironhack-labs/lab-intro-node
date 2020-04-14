cd2e5d2d053a096489a63723d0dd5f16412f63d0
Yosra Yousfi <yousfi.yosra@gmail.com>
2020-04-14 13:23:19 +0200

    lab-node

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,17 +1,42 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+  }
+  get length() {
+    return this.items.length;
+  }
 
-  add(item) {}
+  add = (item) => {
+    this.items.push(item);
+    this.items.sort((a, b) => a - b);
+  };
 
-  get(pos) {}
+  get = (pos) => {
+    this.check(pos > this.length, "OutOfBounds");
+    return this.items[pos];
+  };
 
-  max() {}
+  max = () => {
+    this.check(this.length === 0, "EmptySortedList");
+    return Math.max(...this.items);
+  };
 
-  min() {}
+  min = () => {
+    this.check(this.length === 0, "EmptySortedList");
+    return Math.min(...this.items);
+  };
 
-  sum() {}
+  sum = () => this.items.reduce((acc, val) => acc + val, 0);
 
-  avg() {}
+  avg = () => {
+    this.check(this.length === 0, "EmptySortedList");
+    return this.sum() / this.length;
+  };
+
+  //utils:
+  check = (condition, message) => {
+    if (condition) throw new Error(message);
+  };
 }
 
 module.exports = SortedList;
