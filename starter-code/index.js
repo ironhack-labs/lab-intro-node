74c484dd1dd603aec002702fdaaa75535c7850cf
Rebecca Norén <rebecca.noren@iteam.se>
2020-04-14 15:35:52 +0200

    done

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,17 +1,52 @@
 class SortedList {
-  constructor() {}
+  constructor(items, length) {
+    this.items = [];
+    this.length = 0;
+  }
 
-  add(item) {}
+  add(item) {
+    this.length = this.items.push(item);
+    this.items.sort((a, b) => {
+      return a - b
+    });
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (this.items[pos] <= this.length) {
+      return this.items[pos]
+    }
+    throw new Error("OutOfBounds");
+  }
 
-  max() {}
+  max() {
+    if (this.items.length > 0) {
+      return this.items[this.length - 1]
+    }
+    throw new Error("EmptySortedList");
+  }
 
-  min() {}
+  min() {
+    if (this.items.length > 0) {
+      return this.items[0]
+    }
+    throw new Error("EmptySortedList");
+  }
 
-  sum() {}
+  sum() {
+    if (!this.length) return 0;
+    return this.items.reduce(function (a, b) {
+      return a + b
+    }, 0);
+  }
 
-  avg() {}
+  avg() {
+    if (this.items.length > 0) {
+      return this.items.reduce(function (a, b) {
+        return a + b
+      }, 0) / this.length
+    }
+    throw new Error("EmptySortedList");
+  }
 }
 
 module.exports = SortedList;
