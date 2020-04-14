d858c6e073d6e33d9d683b8ed854ccfde47c233b
William Hermozo <william.hermozo@protonmail.com>
2020-04-14 14:19:10 +0200

    done

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,17 +1,47 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = 0;
+  }
 
-  add(item) {}
+  add(item) {
+    this.length = this.items.push(item);
+    this.items.sort((a, b) => a - b);
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (this.items[pos]) {
+      return this.items[pos];
+    }
+    throw new Error('OutOfBounds');
+  }
 
-  max() {}
+  max() {
+    if (!this.length) {
+      throw new Error('EmptySortedList');
+    }
+    // return Math.max(...this.items);
+    return this.items[this.length - 1];
+  }
 
-  min() {}
+  min() {
+    if (!this.length) {
+      throw new Error('EmptySortedList');
+    }
+    // return Math.min(...this.items);
+    return this.items[0];
+  }
 
-  sum() {}
+  sum() {
+    return this.items.reduce((r, i) => (r += i), 0);
+  }
 
-  avg() {}
+  avg() {
+    if (!this.length) {
+      throw new Error('EmptySortedList');
+    }
+    return this.sum() / this.length;
+  }
 }
 
 module.exports = SortedList;
