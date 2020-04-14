cfe18fc3a12a9e4352c648db5eb29d66e2ed9e9a
vlp90 <vlp90@DESKTOP-VUTPP2C.localdomain>
2020-04-14 15:11:26 +0200

    exercice done

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,17 +1,53 @@
 class SortedList {
-  constructor() {}
+	constructor(items, length) {
+		this.items = [];
+		this.length = this.items.length;
+	}
 
-  add(item) {}
+	add(item) {
+		if (typeof item === 'number') {
+			this.items.push(item);
+			this.length += 1;
+			this.items.sort((a, b) => a - b);
+		}
+	}
 
-  get(pos) {}
+	get(pos) {
+		if (pos < this.items.length) {
+			return this.items[pos];
+		} else {
+			throw Error('OutOfBounds');
+		}
+	}
 
-  max() {}
+	max() {
+		if (this.length === 0) {
+			throw Error('EmptySortedList');
+		} else {
+			return this.items[this.length - 1];
+		}
+	}
 
-  min() {}
+	min() {
+		if (this.length === 0) {
+			throw Error('EmptySortedList');
+		} else {
+			return this.items[0];
+		}
+	}
 
-  sum() {}
+	sum() {
+		if (this.length === 0) return 0;
+		return this.items.reduce((acc, curr) => (acc += curr), 0);
+	}
 
-  avg() {}
+	avg() {
+		if (this.length === 0) {
+			throw Error('EmptySortedList');
+		} else {
+			return this.items.reduce((acc, curr) => (acc += curr), 0) / this.items.length;
+		}
+	}
 }
 
 module.exports = SortedList;
