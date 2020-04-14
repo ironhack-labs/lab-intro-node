29eb8f0351e9bc33b59d30bfb494adb80ec8baf5
Aleah Connley <aleahkc@gmail.com>
2020-04-14 23:11:48 +0200

    done

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,17 +1,76 @@
 class SortedList {
-  constructor() {}
+  constructor(items, length) {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    let sortedList = this.items.sort(function (a, b) {
+      return a - b;
+    });
+    this.length += sortedList.length;
+    return sortedList;
+  }
+  /*
+    add(item) {
+    this.items.sort(function (a, b) {
+      return a - b;
+    }
+    if (item) {
+    this.items.push(item);
+    this.items.sort(function (a, b) {
+      return a - b;
+    });
+  } return this.items;
+  }
+  */
 
-  get(pos) {}
+  get(pos) {
+    if (pos > this.items.length) {
+      throw new Error("OutOfBounds");
+    } else return this.items[pos];
+  }
 
-  max() {}
+  max() {
+    if (this.items.length === 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      let SortedList = this.items.sort(function (a, b) {
+        return a - b;
+      });
+      return Math.max(...SortedList);
+    }
+  }
 
-  min() {}
+  min() {
+    if (this.items.length === 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      let SortedList = this.items.sort(function (a, b) {
+        return a - b;
+      });
+      return Math.min(...SortedList);
+    }
+  }
 
-  sum() {}
+  sum() {
+    if (this.items.length === 0) {
+      return 0;
+    } else {
+      return this.items.reduce((total, amount) => total + amount);
+    }
+  }
 
-  avg() {}
+  avg() {
+    if (this.items.length === 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      return (
+        this.items.reduce((total, amount) => total + amount) / this.items.length
+      );
+    }
+  }
 }
 
 module.exports = SortedList;
