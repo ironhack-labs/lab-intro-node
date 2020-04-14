3bc95ee6f57dc5aac6baf8f3817344b148728142
Karl Secen <karl.secen@gmail.com>
2020-04-14 14:30:16 +0200

    done

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,17 +1,39 @@
 class SortedList {
-  constructor() {}
+  constructor(items, length) {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a,b) => a - b);
+    this.length++;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if(this.items.indexOf(pos) === -1) throw new Error("EmptySortedList");
+    return this.items.indexOf(pos);
+  }
 
-  max() {}
+  max() {
+    if (this.length === 0) throw new Error("EmptySortedList");
+    return Math.max(...this.items);
+  }
 
-  min() {}
+  min() {
+    if (this.length === 0) throw new Error("EmptySortedList");
+    return Math.min(...this.items);
+  }
 
-  sum() {}
+  sum() {
+    if (this.length === 0) return 0;
+    return this.items.reduce((acc, val) => acc + val);
+  }
 
-  avg() {}
+  avg() {
+    if (this.length === 0) throw new Error("EmptySortedList");
+    return (this.items.reduce((acc, val) => acc + val))/this.length;
+  }
 }
 
 module.exports = SortedList;
