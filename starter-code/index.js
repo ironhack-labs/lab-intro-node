5630b60230de529e96aee56a8492eac174aa7b42
Nate <mcfwesh@gmail.com>
2020-04-14 14:14:20 +0200

    done

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,17 +1,46 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    // console.log(sorted.sort());
+    // this.items.push(item);
+    this.items.sort((a, b) => a - b);
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos > this.length - 1) {
+      throw new Error("OutOfBounds");
+    }
+    return this.items[pos];
+  }
 
-  max() {}
+  max() {
+    if (!this.items.length) throw new Error("EmptySortedList");
 
-  min() {}
+    return Math.max(...this.items);
+  }
 
-  sum() {}
+  min() {
+    if (!this.items.length) throw new Error("EmptySortedList");
 
-  avg() {}
+    return Math.min(...this.items);
+  }
+
+  sum() {
+    return this.items.reduce((a, c) => a + c, 0);
+  }
+
+  avg() {
+    if (!this.items.length) throw new Error("EmptySortedList");
+    return this.sum() / this.length;
+  }
 }
 
 module.exports = SortedList;
+let newOne = new SortedList();
+// console.log(newOne.item);
