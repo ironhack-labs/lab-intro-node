1d5536dca258550fdc63cdca5375421508ae23d1
Benjamin Chimoy <benjaminchimoy@benjamins-mbp.localdomain>
2020-04-15 19:37:31 +0200

    done

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,17 +1,49 @@
 class SortedList {
-  constructor() {}
+  constructor(items, length) {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a, b) => a - b);
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (this.items.indexOf(pos) == -1) {
+      throw new Error("OutOfBounds");
+    }
+    return this.items.indexOf(pos);
+  }
 
-  max() {}
+  max() {
+    if (this.items.length == 0) {
+      throw new Error("EmptySortedList");
+    }
+    return this.items[this.items.length - 1];
+  }
 
-  min() {}
+  min() {
+    if (this.items.length == 0) {
+      throw new Error("EmptySortedList");
+    }
+    return this.items[0];
+  }
 
-  sum() {}
+  sum() {
+    if (this.items.length == 0) {
+      return 0;
+    }
+    return this.items.reduce((a, b) => a + b);
+  }
 
-  avg() {}
+  avg() {
+    if (this.items.length === 0) {
+      throw new Error("EmptySortedList");
+    }
+    return this.items.reduce((a, b) => a + b, 0) / this.items.length;
+  }
 }
 
 module.exports = SortedList;
