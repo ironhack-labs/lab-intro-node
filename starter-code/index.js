16116acc06973ad42d1269218e222d10f2a36171
sdubois92 <seb.dubois15@free.fr>
2020-04-20 22:45:48 +0200

    WDFT PARIS MARCH 2020 Sébastien DUBOIS

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,17 +1,55 @@
+// the goal is to create a class that maintains a sorted list of numbers in ascending order
+
 class SortedList {
-  constructor() {}
+  constructor(items, length) {
+    this.items = [];
+    this.length = 0;
+  }
 
-  add(item) {}
+  add(item) {
+    if (typeof item === "number") {
+      this.items.push(item);
+      this.length++;
+      this.items.sort((a,b) => a - b);
+    }
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos < this.items.length) {
+      return this.items[pos];
+    } else {
+      throw new Error ("OutOfBounds");
+    }
+   
+  }
 
-  max() {}
+  max() {
+    if (!this.length) {
+      throw new Error("EmptySortedList");
+    }
+    return this.items[this.length-1];
+  }
 
-  min() {}
+  min() {
+    if (!this.length) {
+      throw new Error("EmptySortedList");
+    }
+    return this.items[0];
+  }
 
-  sum() {}
+  sum() {
+    if (!this.length) {
+      return 0;
+    }
+    return this.items.reduce((acc,curr) => (acc += curr), 0);
+  }
 
-  avg() {}
+  avg() {
+    if (!this.length) {
+     throw new Error ("EmptySortedList");
+    }
+    return (this.items.reduce((acc, curr) => (acc +=curr), 0) / this.items.length);
+  }
 }
 
 module.exports = SortedList;
