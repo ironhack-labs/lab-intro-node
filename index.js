83e9a3ac478119357bcfc04aed71c98725f4c33c
Juli <juli@nexumlabs.com>
2020-05-12 18:31:15 +0200

    Lab done 100%

--- index.js
+++ index.js
@@ -1,17 +1,49 @@
 class SortedList {
-  constructor() {}
+  constructor(items, length) {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a, b) => a - b);
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos > this.length - 1) {
+      throw new Error('OutOfBounds');
+    }
+    return this.items[pos];
+  }
 
-  max() {}
+  max() {
+    if (!this.length) {
+      throw new Error('EmptySortedList');
+    }
+    return Math.max(...this.items);
+  }
 
-  min() {}
+  min() {
+    if (!this.length) {
+      throw new Error('EmptySortedList');
+    }
+    return Math.min(...this.items);
+  }
 
-  sum() {}
+  sum() {
+    if (!this.length) {
+      return 0;
+    }
+    return this.items.reduce((acc, val) => acc + val);
+  }
 
-  avg() {}
+  avg() {
+    if (!this.length) {
+      throw new Error("EmptySortedList");
+    }
+    return this.sum() / this.length;
+  }
 }
 
 module.exports = SortedList;
