3a00484d8f8fe01fa51670a16b11db865b9a3904
Marc De Mena <mdemena@gmail.com>
2020-05-12 22:11:34 +0200

    Iterations 1 - to 4 finished

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = 0;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items = this.items.sort((r1, r2) => r1 - r2);
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos >= this.length) {
+      throw new Error("OutOfBounds");
+    } else {
+      return this.items[pos];
+    }
+  }
 
-  max() {}
+  max() {
+    if (this.length === 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      return this.items[this.length - 1];
+    }
+  }
 
   min() {}
 
