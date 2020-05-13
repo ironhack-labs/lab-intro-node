f56f158150c1c6168bf7000d837af871b7c5df41
Santiago Casa <santiagoacasa@gmail.com>
2020-05-13 19:32:46 +0200

    Done 1-7

--- index.js
+++ index.js
@@ -1,17 +1,55 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items = this.items.sort((a,b) => {return a - b})
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos <= this.length - 1){
+      return this.items[pos];
+    } else {
+      throw new Error('OutOfBounds')
+    }
+  }
 
-  max() {}
+  max() {
+    if(this.length != 0){
+      return Math.max(...this.items);
+    } else {
+      throw new Error('EmptySortedList');
+    }
+    
+  }
 
-  min() {}
+  min() {
+    if (this.length !== 0) {
+      return Math.min(...this.items);
+    } else {
+      throw new Error('EmptySortedList');
+    }
+  }
 
-  sum() {}
+  sum() {
+    if (this.length === 0){
+      return 0
+    } else {
+       return this.items.reduce((acc,val) => acc + val); 
+    }
+  }
 
-  avg() {}
+  avg() {
+    if(this.length != 0){
+      return this.sum(this.items) / this.length;
+    } else {
+      throw new Error('EmptySortedList')
+    }
+  }
 }
 
 module.exports = SortedList;
