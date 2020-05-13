a0f1eeb2b2ab3b7a31e1f1e07fc1bc60f89835b2
Antoine Rolland <antoine-rolland@hotmail.fr>
2020-05-13 13:05:25 +0200

    done

--- index.js
+++ index.js
@@ -1,17 +1,45 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a,b) => a-b);
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  get(pos) {
+    if (pos<0 || pos>this.items.length-1) {
+      throw new Error('OutOfBounds');
+    }
+    return this.items[pos]
+  }
 
-  get(pos) {}
+  max() {
+    if (this.items.length === 0) {
+      throw new Error('EmptySortedList');
+    }
+    return Math.max.apply(null,this.items)
+  }
 
-  max() {}
+  min() {
+    if (this.items.length === 0) {
+      throw new Error('EmptySortedList');
+    }
+    return Math.min.apply(null,this.items);
+  }
 
-  min() {}
+  sum() {
+    return this.items.reduce((acc,curr) => acc + curr,0);
+  }
 
-  sum() {}
-
-  avg() {}
+  avg() {
+    if (this.items.length === 0) {
+      throw new Error('EmptySortedList');
+    }
+    return this.sum(this.items)/this.items.length;
+  }
 }
 
 module.exports = SortedList;
