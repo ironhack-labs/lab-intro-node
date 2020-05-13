6f66fc095dd4a36d6c967c22dfbfa6fe5bc55e51
Alba Olveros <x8a.018@gmail.com>
2020-05-13 18:40:39 +0200

    Iteration 1 - 7 completed.

--- index.js
+++ index.js
@@ -1,17 +1,48 @@
 class SortedList {
-  constructor() {}
-
-  add(item) {}
-
-  get(pos) {}
-
-  max() {}
-
-  min() {}
-
-  sum() {}
-
-  avg() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a, b) => a - b);
+    this.length = this.items.length;
+  }
+  get(pos) {
+    if(this.items[pos] === undefined) {
+      throw new Error('OutOfBounds');
+    } else {
+      return this.items[pos];
+    }
+  }
+  max() {
+    if(this.items.length === 0) {
+      throw new Error('EmptySortedList');
+    } else {
+      return this.items[this.items.length - 1];
+    }
+  }
+  min() {
+    if(this.items.length === 0) {
+      throw new Error('EmptySortedList');
+    } else {
+      return this.items[0];
+    }
+  }
+  sum() {
+    if(this.items.length === 0) {
+      return 0;
+    } else {
+      return this.items.reduce((total, i) => total + i); 
+    }
+  }
+  avg() {
+    if(this.items.length === 0) {
+      throw new Error('EmptySortedList');
+    } else {
+      return this.sum(this.items)/this.length;
+    }
+  }
 }
 
 module.exports = SortedList;
