c3866d396f20876dc6fccac56bc79df6939d7c7a
David de Lamo <d.lamo.valverde@gmail.com>
2020-05-13 19:01:27 +0200

    iteration 3

--- index.js
+++ index.js
@@ -19,7 +19,13 @@ class SortedList {
     }
   }
 
-  get(pos) {}
+  get(pos) {
+    if (pos > this.length) {
+      throw new Error('OutOfBounds');
+    } else {
+      return this.items[pos];
+    }
+  }
 
   max() {}
 
