4d5fa977b917165cf47927b1a3b53b1b722e065e
alex gera <alex.ger93@gmail.com>
2020-05-13 10:36:50 +0200

    iter 2 - add item

--- index.js
+++ index.js
@@ -3,8 +3,18 @@ class SortedList {
     this.items = [];
     this.length = this.items.length;
   }
+  sortItems (arr) {
+    return arr.sort((a,b) => a - b);
+  }
+  add(item) {
+    // return this.items.push(item).sort((a, b) => a - b);
+    this.items.push(item);
+    this.sortItems(this.items);
+    console.log(this.items);
+    this.length = this.items.length;
+    console.log(this.length);
+  }
 
-  add(item) {}
 
   get(pos) {}
 
@@ -16,5 +26,12 @@ class SortedList {
 
   avg() {}
 }
+
+
+let sortedList = new SortedList();
+sortedList.add(4);
+sortedList.add(6);
+sortedList.add(8);
+sortedList.add(0);
 
 module.exports = SortedList;
