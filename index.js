cc9dace98cf975d9791c5c1995b9663c1c2a3d75
Robert Ledezma <Robert@MacBook-Pro-de-Robert-2.local>
2020-05-14 00:18:10 +0200

    Donde pero creo que con unos errores

--- index.js
+++ index.js
@@ -1,17 +1,60 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  };
 
-  add(item) {}
+  add(items) {
+    this.items.push()
+    // this.items.sort(function (a, b) {
+    //   return a - b;
+    // });
+    this.items.sort((a, b) => a - b);
 
-  get(pos) {}
+    this.length = this.items.length
+  };
 
-  max() {}
+  get(pos) {
+    if (this.items[pos] === undefined) {
+      throw new Error('OutOfBounds');
+    } else {
+      return this.items[pos]
+    }
+  };
 
-  min() {}
+  max() {
+    if (this.items.length <= 0){
+      throw new Error('EmptySortedList');
+    } else {
+      return Math.max.apply(null, this.items);
+    }
+  };
 
-  sum() {}
+  min() {
 
-  avg() {}
+    if (this.items.length <= 0){
+      throw new Error('EmptySortedList');
+    } else {
+      return Math.min.apply(null, this.items);
+    }
+
+  };
+
+  sum() {
+   if (this.items.length <= 0){
+     return 0
+   } else{
+     return this.items.reduce((a,b)=> a+b)
+   }
+  };
+
+  avg() {
+    if (this.items.length <= 0){
+      throw new Error('EmptySortedList');
+    } else {
+      return (this.items.reduce((a,b)=> a+b)) / this.items.length
+    }
+  }
 }
 
 module.exports = SortedList;
