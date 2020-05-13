763f1082ceab618d6fdb13047c83cd2510adafaa
alex gera <alex.ger93@gmail.com>
2020-05-13 13:39:50 +0200

    done

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 class SortedList {
   constructor(items, length) {
-    this.items = [1,3,5,7];
+    this.items = [];
     this.length = this.items.length;
   }
   sortItems (arr) {
@@ -10,32 +10,51 @@ class SortedList {
     // return this.items.push(item).sort((a, b) => a - b);
     this.items.push(item);
     this.sortItems(this.items);
-    console.log(this.items);
     this.length = this.items.length;
-    console.log(this.length);
   }
-
 
   get(pos) {
     if (pos < this.items.length){
-      console.log(this.items[pos])
     return this.items[pos];
     } else {
       throw new Error('OutOfBounds');
     }
   }
 
-  max() {}
+  max() {
+    if (this.length) {
+      let newArr = this.sortItems(this.items);
+      return newArr[newArr.length - 1];
+    } else {
+      throw new Error('EmptySortedList');
+    }
+  }
 
-  min() {}
+  min() {
+    if (this.length) {
+      let newArr = this.sortItems(this.items);
+      return newArr[0];
+    } else {
+      throw new Error('EmptySortedList');
+    }
+  }
 
-  sum() {}
+  sum() {
+    if (this.length){
+    return this.items.reduce((a,b) => a + b);
+    } else {
+      return 0;
+    }
+  }
 
-  avg() {}
+  avg() {
+    if (this.length){
+      let total = this.sum();
+      return total / this.length;
+      } else {
+        throw new Error('EmptySortedList');
+      }
+    }
 }
-
-
-let sortedList = new SortedList();
-sortedList.get(0);
 
 module.exports = SortedList;
