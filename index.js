50d56135fc7c994e529fba6d5741c06cdefe1513
David de Lamo <d.lamo.valverde@gmail.com>
2020-05-13 18:55:04 +0200

    iteration 2

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = 0;
+  }
 
-  add(item) {}
+  add(item) {
+    if (this.length === 0 || item > this.items[this.items.length - 1]) {
+      this.items.push(item);
+      this.length++;
+    } else {
+      for (let i = 0; i < this.items.length; i++) {
+        if (item < this.items[i]) {
+          this.items.splice(i, 0, item);
+          this.length++;
+          return;
+        }
+      }
+    }
+  }
 
   get(pos) {}
 
