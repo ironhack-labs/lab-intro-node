503ac297b6b149e2e4d36908ce0e5dfad9124ed1
irene villanueva <irens.vm@gmail.com>
2020-05-13 22:07:25 +0200

    13passing, 1 fail

--- index.js
+++ index.js
@@ -1,22 +1,49 @@
 class SortedList {
-  constructor() {
+  constructor(items, lenght) {
     this.items = [];
-    this.lenght = this.items.lenght;
+    this.length = this.items.length;
+    this.lenght === 0;
 
 
   }
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a, b) => a - b);
+    this.length = this.items.length;
+  }
+  get(pos) {
+    if (pos > this.length) {
+      throw new Error('OutOfBounds');
+    } else {
+      return this.items[pos];
+    }
+  }
 
-  add(item) {}
+  max() {
+    if (this.items.length === 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      return this.items[this.items.length - 1];
+    }
+  }
+  min() {
+    if (this.items.length === 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      return this.items[0];
+    }
+  }
+  sum() {
+    return this.items.reduce((suma, i) => suma + i);
+  }
 
-  get(pos) {}
-
-  max() {}
-
-  min() {}
-
-  sum() {}
-
-  avg() {}
+  avg() {
+    if (this.items.length === 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      return this.sum(this.items) / this.length;
+    }
+  }
 }
 
 module.exports = SortedList;
