7e1a8dbe38e674db6e94ff9558e845d1556e3320
Laura Ramirez <lau.ramirez.encinas@gmail.com>
2020-05-14 16:17:49 +0200

    done

--- index.js
+++ index.js
@@ -1,17 +1,60 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a, b) => a - b);
+    this.length = this.items.length;
+    return this.items;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos >= this.items.length) {
+      throw new Error('OutOfBounds');
+    }
+    return this.items[pos]
+  }
 
-  max() {}
+  max() {
+    let ultimoNumero = this.items.length - 1;
+    if (this.items.length === 0) {
+      throw new Error('EmptySortedList');
+    }
+    return this.items[ultimoNumero];
+  }
 
-  min() {}
+  min() {
+    if (this.items.length === 0) {
+      throw new Error('EmptySortedList');
+    }
+    return this.items[0];
+  }
 
-  sum() {}
+  sum() {
+    let suma = 0;
+    if (this.items.length === 0) {
+      return 0;
+    }
+    else {
+      this.items.forEach(function (numero) {
+        suma += numero;
+      })
+    }
+    return suma;
+  }
 
-  avg() {}
+  avg() {
+    if (this.items.length === 0) {
+      throw new Error('EmptySortedList');
+    }
+    let resultado = this.sum();
+    let media = resultado / this.items.length
+    return media
+  }
 }
 
 module.exports = SortedList;
+
