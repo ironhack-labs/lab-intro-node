f1f4b436d5212378b4b90ab51aa8304b2f841079
Justo Rivera <justogdrmonterrey@gmail.com>
2020-05-25 16:09:08 +0200

    done, all tests passing

--- index.js
+++ index.js
@@ -1,17 +1,44 @@
 class SortedList {
-  constructor() {}
+  constructor(...args) {
+    this.items = [...args]
+    this.length = args.length
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item)
+    this.sort()
+    this.length++
+  }
 
-  get(pos) {}
+  get(pos) {
+    if(pos < 0 || pos >= this.length){
+      throw Error('OutOfBonds')
+    }
+    return this.items[pos]
+  }
 
-  max() {}
+  max() {
+    if(this.length === 0) throw Error('EmptySortedList')
+    return [...this.items].pop()
+  }
 
-  min() {}
+  min() {
+    if(this.length === 0) throw Error('EmptySortedList')
+    return [...this.items].shift()
+  }
 
-  sum() {}
+  sum() {
+    return this.items.reduce( (acc, elem) => acc + elem, 0)
+  }
 
-  avg() {}
+  avg() {
+    if(this.length === 0) throw Error('EmptySortedList')
+    return this.items.reduce( (acc, elem) => acc+elem, 0)/this.length
+  }
+
+  sort() {
+    this.items.sort((a,b) => a-b)
+  }
 }
 
 module.exports = SortedList;
