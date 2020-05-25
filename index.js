639f62583eeec1c5339a8926a4a377a4596aec18
James Dawson <63356869+polymurph13@users.noreply.github.com>
2020-05-25 15:08:17 +0100

    completed

--- index.js
+++ index.js
@@ -1,17 +1,63 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = 0;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.sort();
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if(pos >= this.items.length || pos < 0) {
+      throw new Error('OutOfBounds');
+    } else {
+      return this.items[pos];
+    }
+  }
 
-  max() {}
+  max() {
+    if(this.items.length <= 0) {
+      throw new Error('EmptySortedList');
+    } else {
+      return this.items.slice(-1);
+    }
+  }
 
-  min() {}
+  min() {
+    if(this.items.length <= 0) {
+      throw new Error('EmptySortedList');
+    } else {
+      return this.items[0];
+    }
+  }
 
-  sum() {}
+  sum() {
+    if(this.items.length <= 0) {
+      return 0;
+    } else {
+      return this.items.reduce((sum, el) => {
+        return sum += el;
+      });
+    }
+  }
 
-  avg() {}
+  avg() {
+    if(this.length <= 0) {
+      throw new Error('EmptySortedList');
+    } else {
+      return this.sum() / this.length;
+    }
+  }
+
+  sort() {
+    this.items.sort((a, b) => {
+      return a - b;
+    });
+  }
+
 }
 
 module.exports = SortedList;
