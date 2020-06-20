d8fc6140292d16b6196683b2e56b44cf75bedf44
Jaime Baro <jaimebaroval@gmail.com>
2020-06-20 11:54:07 +0200

    iteractions ok

--- index.js
+++ index.js
@@ -1,17 +1,74 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = []
+    this.length = 0
 
-  add(item) {}
+  }
 
-  get(pos) {}
+  add(item) {
+    this.items.push(item)
+    this.items.sort(function (a, b) {
+      return a - b;
+    });
+    this.length = this.items.length
+  }
 
-  max() {}
+  get(pos) {
+    const item = this.items[pos]
+    if (this.items[pos]) {
+      return item
+    } else {
+      throw new Error('OutOfBounds');
+    }
+  }
 
-  min() {}
+  max() {
+    if (this.items.length === 0) {
+      throw new Error('EmptySortedList');
+    }
 
-  sum() {}
+    return this.items.pop()
+  }
 
-  avg() {}
+  min() {
+    if (this.items.length === 0) {
+      throw new Error('EmptySortedList');
+    }
+
+    return this.items[0]
+  }
+
+  sum() {
+    let sumItems = 0
+
+    if (this.items.length === 0) {
+      return 0;
+    }
+
+    this.items.forEach(e => {
+      console.log(e);
+      sumItems += e
+    })
+
+    console.log(sumItems);
+
+    return sumItems
+  }
+
+  avg() {
+    let sumItems = 0
+
+    if (this.items.length === 0) {
+      throw new Error('EmptySortedList');
+    }
+
+    this.items.forEach(e => {
+      console.log(e);
+      sumItems += e
+    })
+
+    return sumItems / this.length
+  }
 }
 
 module.exports = SortedList;
