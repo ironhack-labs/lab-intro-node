d2fe870d217fc4ca4e6f71f31c5796006aa53c19
aitorgb <aitorguerrero9@gmail.com>
2020-06-20 11:30:06 +0200

    exercise completed

--- index.js
+++ index.js
@@ -1,17 +1,61 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = []
+    this.length = 0
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item)
+    this.length = this.items.length
+    if (this.length > 1) {
+    this.items.sort((a,b) => a - b)
+    }
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos < this.length) {
+        return this.items[pos]
+    } else {
+      throw new Error ('OutOfBounds')
+    }
 
-  max() {}
+  }
 
-  min() {}
+  max() {
+    if (this.length !== 0) {
+      return this.items[this.length - 1]
+    } else {
+      throw new Error ('EmptySortedList')
+    }
 
-  sum() {}
 
-  avg() {}
+  }
+
+  min() {
+    if (this.length !== 0) {
+      return this.items[0]
+    } else {
+      throw new Error ('EmptySortedList')
+    }
+  }
+
+  sum() {
+    return this.items.reduce((accumulator, currentValue) => {
+      return accumulator + currentValue
+    }, 0)
+
+  }
+
+  avg() {
+    if (this.length !== 0){
+    const total = this.items.reduce((accumulator, currentValue) => {
+      return accumulator + currentValue
+    }, 0)
+    return total / this.length
+    } else {
+      throw new Error ('EmptySortedList')
+    }
+  }
 }
 
 module.exports = SortedList;
