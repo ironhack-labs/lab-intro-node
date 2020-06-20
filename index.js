5454907a73495d67fe4de98ef4d5e62e197bce6f
Rodrigo Poblacion <rodrigopoblacion@gmail.com>
2020-06-20 11:40:54 +0200

    Hemos ido a la peluqueria y nos ha salid genial

--- index.js
+++ index.js
@@ -1,17 +1,55 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item)
+    this.length = this.items.length
+    this.items.sort((a, b) => a - b);
+  }
 
-  get(pos) {}
+  get(pos) {
+    
+    if (this.items.indexOf(pos) >= 0) {
+      return this.items.indexOf(pos);
+    } else {
+      throw new Error('OutOfBounds');
+    }
+  }
 
-  max() {}
+  max() {
+    if (this.items.length === 0) {
+      throw new Error('EmptySortedList');
+    } else {
+      return Math.max.apply(null, this.items);
+    }
+  }
 
-  min() {}
+  min() {
+    if (this.items.length === 0) {
+      throw new Error('EmptySortedList');
+    } else {
+      return Math.min.apply(null, this.items);
+    }
+  }
 
-  sum() {}
+  sum() {
+    if (this.items.length === 0) {
+      return 0;
+    } else {
+      return this.items.reduce((acc, curr) => acc + curr);
+    }
+  }
 
-  avg() {}
+  avg() {
+    if (this.items.length === 0) {
+      throw new Error('EmptySortedList');
+    } else {
+      return this.items.reduce((acc, curr) => acc + curr) / this.items.length;
+    }
+  }
 }
 
 module.exports = SortedList;
