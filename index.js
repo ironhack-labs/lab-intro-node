9c566fe654c16134056998ff5a253c81638cc6c0
TL Garcia <tomas.bl.garcia@gmail.com>
2020-06-20 11:53:58 +0200

    Done

--- index.js
+++ index.js
@@ -1,17 +1,58 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+    this.items.sort((a, b) => a - b);
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.length = this.items.length;
+    this.items.sort((a, b) => a - b);
+  }
 
-  get(pos) {}
+  get(pos) {
+    const num = this.items[pos];
+    if (num) {
+      return this.items[pos];
+    }
+    throw new Error('OutOfBounds');
+  }
 
-  max() {}
+  max() {
+    if (this.length === 0) {
+      throw new Error('EmptySortedList');
+    }
 
-  min() {}
+    const max = Math.max(...this.items);
 
-  sum() {}
+    return max;
+  }
 
-  avg() {}
+  min() {
+    if (this.length === 0) {
+      throw new Error('EmptySortedList');
+    }
+
+    const min = Math.min(...this.items);
+
+    return min;
+  }
+
+  sum() {
+    return this.items.reduce((acc, value) => acc + value, 0);
+  }
+
+  avg() {
+    if (this.length === 0) {
+      throw new Error('EmptySortedList');
+    }
+    const sum = this.items.reduce((acc, value) => acc + value, 0);
+    const result = sum / this.length;
+
+    return result
+  }
 }
 
 module.exports = SortedList;
+
