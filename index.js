c5c8a25df274a4eae53b96261f36f133c4293de2
Jose <josemaciasbarredo@outlook.es>
2020-06-20 11:39:15 +0200

    completed

--- index.js
+++ index.js
@@ -1,17 +1,57 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = 0;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item)
 
-  get(pos) {}
+    this.items.sort((a, b) => {
+      return a - b
+    })
 
-  max() {}
+    this.length++
+  }
 
-  min() {}
+  get(pos) {
+    if (pos > this.length) {
+      throw new Error('OutOfBounds')
+    }
+    return this.items.indexOf(pos)
+  }
 
-  sum() {}
+  max() {
+    if (this.length === 0) {
+      throw new Error('EmptySortedList')
+    }
+    return Math.max(...this.items)
 
-  avg() {}
+    // this.items.reduce((acc, cur) => {
+    //   Math.max()
+    // }, 0)
+  }
+
+  min() {
+    if (this.length === 0) {
+      throw new Error('EmptySortedList')
+    }
+    return Math.min(...this.items)
+  }
+
+  sum() {
+    return this.items.reduce((acc, cur) => {
+      return acc + cur
+    }, 0)
+
+  }
+
+  avg() {
+    if (this.length === 0) {
+      throw new Error('EmptySortedList')
+    }
+    return this.sum()/ this.items.length
+  }
 }
 
 module.exports = SortedList;
