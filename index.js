3be776f37c898ebffa70f4117b2b17f0f53e156d
Old <onelifedesigning@gmail.com>
2020-06-20 11:47:15 +0200

    solved all

--- index.js
+++ index.js
@@ -1,17 +1,56 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = []
+    this.length = this.items.length
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item)
+    this.items.sort((a, b) => a - b)
+    this.length = this.items.length
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (this.items[pos]) {
+      return this.items[pos]
+    } else {
+      throw Error('OutOfBounds')
+    }
+  }
 
-  max() {}
+  max() {
+    if (!this.length) {
+      throw Error('EmptySortedList')
+    } else {
+      return this.items[this.length - 1]
+    }
+  }
 
-  min() {}
+  min() {
+    if (!this.length) {
+      throw Error('EmptySortedList')
+    } else {
+      return this.items[0]
+    }
+  }
 
-  sum() {}
+  sum() {
+    if (this.length) {
+      return this.items.reduce((a, b) => {
+        return a += b
+      }, 0)
+    } else {
+      return this.length
+    }
+  }
 
-  avg() {}
+  avg() {
+    if (!this.length) {
+      throw Error('EmptySortedList')
+    } else {
+      return this.sum(this.items) / this.length
+    }
+  }
 }
 
 module.exports = SortedList;
