8f97a43fe8754bdc8f64afa430a99071a7231fff
miguelbachi5a <miguelbachi5a@gmail.com>
2020-06-20 11:03:22 +0200

    Iteration 5

--- index.js
+++ index.js
@@ -1,13 +1,39 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = []
+    this.length = 0
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item)
+    this.items = this.items.sort((a, b) => a - b)
 
-  get(pos) {}
+    this.length++
+  }
 
-  max() {}
+  get(pos) {
+    if (pos < 0 || pos >= this.length) {
+      throw new Error('OutOfBounds');
+    } else {
+      return this.items[pos]
+    }
+  }
 
-  min() {}
+  max() {
+    if (this.length === 0) {
+      throw new Error('EmptySortedList');
+    } else {
+      return this.items[this.length - 1]
+    }
+  }
+
+  min() {
+    if (this.length === 0) {
+      throw new Error('EmptySortedList');
+    } else {
+      return this.items[0]
+    }
+  }
 
   sum() {}
 
