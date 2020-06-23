e667f80fd2b18c3d07e6d43e6e6f9d37644d308a
Vitor Pavan <vipavani@hotmail.com>
2020-06-23 13:02:06 +0200

    Lab complete

--- index.js
+++ index.js
@@ -1,17 +1,76 @@
 class SortedList {
-  constructor() {}
+  constructor(items, length) {
 
-  add(item) {}
+    this.items = [];
+    this.length= this.items.length;
 
-  get(pos) {}
+  }
 
-  max() {}
+  add(item) {
+        
+    this.items.push(item);
+    if( this.items.length > 1 ){
+      this.items.sort(function(a, b){return a-b});
+    }    
+    
+    this.length = this.items.length;
+  }
 
-  min() {}
+  get(pos) {
 
-  sum() {}
+    if ( pos < this.length){
+      return this.items[pos];
+    } else {
+      throw new Error('OutOfBounds');
+    }
+  }
 
-  avg() {}
+  max() {
+    if( this.length === 0 ){
+      throw new Error('EmptySortedList');
+    }
+
+    return Math.max(...this.items);
+
+  }
+
+  min() {
+
+    if( this.length === 0 ){
+      throw new Error('EmptySortedList');
+    }
+
+    return Math.min(...this.items);
+
+  }
+
+  sum() {
+
+    if( this.length === 0 ){
+      return 0;
+    }
+
+    let totalSum=0;
+    for (let item in this.items){
+      totalSum+=this.items[item];
+    }
+    return totalSum;
+  }
+
+  avg() {
+    if( this.length === 0 ){
+      throw new Error('EmptySortedList');
+    }
+
+    let totalSum=0;
+    for (let item in this.items){
+      totalSum+=this.items[item];
+    }
+    return totalSum/this.length;
+
+
+
+  }
 }
 
 module.exports = SortedList;
