f47df0e27ffb0673c482680d5dfba9cbba2ef784
Gabriel Palladini <gmpalladini@gmail.com>
2020-06-23 16:11:17 +0200

    Gabriel WebDeb Berlin jun 2020

--- index.js
+++ index.js
@@ -1,13 +1,33 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+      this.items = [];
+      this.length = this.items.length; 
+    }
+  add(item) {
+    
+    this.items.push(item);
+    this.items.sort((a, b) => a - b);
+    this.length =  this.items.length;
 
-  add(item) {}
+  }
 
-  get(pos) {}
+  get(pos) {
+    
+    if(pos > this.items.length) {
+      throw new Error('OutOfBounds')
+    } else return this.items[pos];
+  }
 
-  max() {}
+  max() {
+     if (SortedList = []) {
+      throw new Error('EmptySortedList')
+    } else return Math.max(...this.items)
 
-  min() {}
+  }
+
+  min() {
+
+  }
 
   sum() {}
 
