0c525e26d0cfcb3f000f74cbeda47ec65757a5aa
Yuri La Torre Ortiz <yuri.la.torre.ortiz@protonmail.ch>
2020-06-23 14:30:36 +0200

    done

--- index.js
+++ index.js
@@ -1,17 +1,51 @@
 class SortedList {
-  constructor() {}
+  constructor(items) {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.length++;
+    return this.items.sort((a, b) => a - b), this.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos > this.items.length -1){
+      throw new Error('OutOfBounds');
+    }
+    return this.items[pos];
+  }
 
-  max() {}
+  max() {
+    if (this.items === undefined || this.items.length === 0) {
+      throw new Error('EmptySortedList');
+    }
+   let max = this.items.reduce(function(a, b) {
+      return Math.max(a, b)
+    });
+    return max;
+  }
 
-  min() {}
+  min() {
+    if (this.items === undefined || this.items.length === 0) {
+      throw new Error('EmptySortedList');
+    }
+    return Math.min(...this.items);
+  }
 
-  sum() {}
+  sum() {
+    if (this.items.length === 0) return 0;
+    const sum = arr => arr.reduce((a,b) => a + b, 0);
+    return sum(this.items);
+  }
 
-  avg() {}
+  avg() {
+    if (this.items === undefined || this.items.length === 0) {
+      throw new Error('EmptySortedList');
+    }
+    return this.sum()/this.items.length
+  }
 }
 
 module.exports = SortedList;
