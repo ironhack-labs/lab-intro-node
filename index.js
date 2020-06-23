35acbce055bb5fd6656cfaa8e70c110fadf1395f
Benoit Hage <hage.benoit@gmail.com>
2020-06-23 13:01:56 +0200

    done + bonus

--- index.js
+++ index.js
@@ -1,17 +1,50 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;    
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.length = this.items.length;
+    this.items.sort((a, b) => a - b);
+  }
 
-  get(pos) {}
+  get(pos) {
+    this.pos = this.items[pos];
+    if (pos > this.length) {
+      throw new Error('OutOfBounds');
+    }
+    return this.items[pos];
+  }
 
-  max() {}
+  max() {
+    this.element = this.items;
+    if (this.length === 0) {
+      throw new Error('EmptySortedList');
+    }
+    return Math.max(...this.element);
+  }
 
-  min() {}
+  min() {
+    this.element = this.items;
+    if (this.length === 0) {
+      throw new Error('EmptySortedList');
+    }
+    return Math.min(...this.element);
+  }
 
-  sum() {}
+  sum() {
+    this.element = this.items
+    return this.element.reduce((accumulator, currentValue) => accumulator + currentValue, 0) 
+  }
 
-  avg() {}
+  avg() {
+    if (this.items.length === 0) {
+      throw new Error('EmptySortedList');
+    }
+    return this.sum() / this.items.length;
+  }
 }
 
 module.exports = SortedList;
