984ba27b83a88640db1cf80caebf0938d449db94
Fanny Sabin <fannysabin.fs@gmail.com>
2020-06-30 16:33:52 +0200

    done

--- index.js
+++ index.js
@@ -1,17 +1,54 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.length = this.items.push(item);
+    this.items.sort(function (a, b) {
+      return a - b;
+    });
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos > this.length - 1) {
+      throw new Error('OutOfBounds');
+    } else {
+      for (let i = 0; i < this.length; i++) {
+        return this.items[pos];
+      }
+    }
+  }
 
-  max() {}
+  max() {
+    if (this.length === 0) {
+      throw new Error('EmptySortedList');
+    } else {
+      return Math.max.apply(null, this.items);
+    }
+  }
 
-  min() {}
+  min() {
+    if (this.length === 0) {
+      throw new Error('EmptySortedList');
+    } else {
+      return Math.min.apply(null, this.items);
+    }
+  }
 
-  sum() {}
+  sum() {
+    var sum = this.items.reduce((cur, acc) => cur + acc, 0);
+    return sum;
+  }
 
-  avg() {}
+  avg() {
+    if (this.length === 0) {
+      throw new Error('EmptySortedList');
+    } else {
+      return this.sum() / this.length
+    }
+  }
 }
 
 module.exports = SortedList;
