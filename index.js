e66aa5a42b5377752eee4fe060b29ca351e428ac
menhaj safi <minhaj.sahil01@gmail.com>
2020-06-30 21:08:22 +0200

    done

--- index.js
+++ index.js
@@ -1,17 +1,48 @@
 class SortedList {
-  constructor() {}
+  constructor(items, length) {
+    this.items = [];
+    this.length = 0;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.length++;
+    this.items.sort((a, b) => {
+      return a - b;
+    });
+  }
+  
 
-  get(pos) {}
+  get(pos) {
+   if (pos > this.items.length - 1 || pos < 0) throw new Error('OutOfBounds');
+   return this.items[pos];
+  }
 
-  max() {}
+  max() {
+    if (this.items.length === 0) throw new Error('EmptySortedList');
+    else {
+      let number = this.items.length - 1;
+      return this.items[number];
+    }
+  }
 
-  min() {}
+  min() {
+    if (this.items.length === 0) throw new Error('EmptySortedList');
+    else {
+      return this.items[0];
+    }
+  }
 
-  sum() {}
+  sum() {
+    return this.items.reduce((total, acc) => {
+         return total + acc;
+       },0);
+  }
 
-  avg() {}
+  avg() {
+    return ((this.items.reduce((total, acc) => total + acc)) / this.length)
+  }
+  
 }
 
 module.exports = SortedList;
