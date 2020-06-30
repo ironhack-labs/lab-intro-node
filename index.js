1d4ba90d55430ca1e99f6da5aab117b9d79bae0d
malaprince <malaminebah91@gmail.com>
2020-06-30 17:19:01 +0200

    done

--- index.js
+++ index.js
@@ -1,17 +1,59 @@
 class SortedList {
-  constructor() {}
+  constructor(items, length) {
+    this.items =[];
+    this.length = this.items.length
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort(function(a,b){
+      return a -b
+    });
+    this.length = this.items.length
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos > this.length) {
+      throw new Error('OutOfBounds');
+    };
+    return this.items[pos];
+    
 
-  max() {}
+  }
 
-  min() {}
+  max() {
+    if (this.length === 0){
+      throw new Error('EmptySortedList');
+    }
+      return this.items[this.length -1]
+      
+    }
+  
 
-  sum() {}
+  min() {
+    if (this.length === 0){
+      throw new Error('EmptySortedList');
+    }
+      return this.items[0];
+      
+    }
 
-  avg() {}
+  sum() {
+    if (this.length === 0){
+      return 0;
+    }
+    const total = this.items.reduce((ac, b) => ac + b, 0 );
+    return total;
+
+  }
+
+  avg() {
+    if (this.lenght === 0){
+      throw new Error('EmptySortedList');
+    }else{
+    return this.sum() / this.items.length;
+  }
+}
 }
 
 module.exports = SortedList;
