26144713b26f2147a31b8d3c446461a2f89e696d
Spyros Kyritsis <spyros.kyritsis@gmail.com>
2020-06-30 16:31:57 +0200

    done

--- index.js
+++ index.js
@@ -1,17 +1,54 @@
 class SortedList {
-  constructor() {}
+  constructor(items, length) {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a, b) => a - b);
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos > this.length) {
+      throw new Error("OutOfBounds");
+    }
 
-  max() {}
+    return this.items[pos];
+  }
 
-  min() {}
+  max() {
+    if (this.items.length === 0) {
+      throw new Error("EmptySortedList");
+    }
 
-  sum() {}
+    return this.items[this.items.length - 1];
+  }
 
-  avg() {}
+  min() {
+    if (this.items.length === 0) {
+      throw new Error("EmptySortedList");
+    }
+
+    return this.items[0];
+  }
+
+  sum() {
+    if (this.items.length === 0) {
+      return 0;
+    }
+
+    return this.items.reduce((acc, b) => acc + b, 0);
+  }
+
+  avg() {
+    if (this.items.length === 0) {
+      throw new Error("EmptySortedList");
+    }
+
+    return this.sum() / this.items.length;
+  }
 }
 
 module.exports = SortedList;
