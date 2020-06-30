7c84d448d32f1581dd7b6b8ac8e45db94f8c585c
cchakibb <chakibbachir88@gmail.com>
2020-06-30 17:36:30 +0200

    [PAR-FT-0620] Chakib BACHIR

--- index.js
+++ index.js
@@ -1,17 +1,60 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
 
-  get(pos) {}
+    this.items.sort((a, b) => a - b);
+    this.length = this.items.length;
+  }
 
-  max() {}
+  get(pos) {
+    if (this.items[pos] !== undefined) {
+      return this.items[pos];
+    } else {
+      throw new Error("OutOfBounds");
+    }
+  }
 
-  min() {}
+  max() {
+    if (this.items.length > 0) {
+      return this.items.pop();
+    } else {
+      throw new Error("EmptySortedList");
+    }
+  }
 
-  sum() {}
+  min() {
+    if (this.items.length > 0) {
+      return this.items.shift();
+    } else {
+      throw new Error("EmptySortedList");
+    }
+  }
 
-  avg() {}
+  sum() {
+    let total = 0;
+    for (let i = 0; i < this.items.length; i++) {
+      total += this.items[i];
+    }
+    if (this.items.length > 0) return total;
+    return 0;
+  }
+
+  avg() {
+    if (this.items.length === 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      let total = 0;
+      for (let i = 0; i < this.items.length; i++) {
+        total += this.items[i];
+      }
+      return total / this.items.length;
+    }
+  }
 }
 
 module.exports = SortedList;
