5e4b223ad414d65a981734d230f990cf50aecb5b
Manon Morgaut <manonmorgaut@gmail.com>
2020-06-30 17:09:36 +0200

    missing last iteration of exercise

--- index.js
+++ index.js
@@ -1,17 +1,78 @@
 class SortedList {
-  constructor() {}
+  constructor(items,length) {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+let array = this.items;
+array.push(item);
+array.sort( (a,b) => {
+  return a - b
+})
+this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if(pos > this.items.length - 1 || pos < 0) throw new OutOfBounds("foo");
+    // continue l'algo
+    return this.items[pos];
+  }
 
-  max() {}
-
-  min() {}
-
-  sum() {}
-
-  avg() {}
+  max() {
+    if(this.items.length===0) throw new Error('EmptySortedList');
+else {
+  let i=this.items.length-1;
+  return this.items[i];
 }
+  }
+
+  min() {
+        if(this.items.length===0) throw new Error('EmptySortedList');
+else {
+  return this.items[0];
+}
+  }
+
+  sum() {
+    if(this.items.length===0) return 0;
+ 
+      return this.items.reduce(
+        (accumulator, currentValue) => accumulator + currentValue
+        )
+  }
+  avg() {
+     if(this.items.length===0) throw new Error('EmptySortedList');
+else {
+  let sum = this.items.sum();
+  let total = this.items.length;
+  let average;
+  return average = sum/total;
+}
+  }
+
+}
+
+/*
+class Cat {
+  constructor(name, kids) {
+    this.name = name
+    this.kids = kids
+  }
+
+  getName() {
+    return this.name;
+  }
+
+  getNbrKids() {
+    return this.kids.length
+  }
+}
+
+var kitty = new Cat("kitty", ["toto", "titi"]);
+var cat2 = new Cat("félix", []);
+console.log(cat2.getName());
+
+*/
 
 module.exports = SortedList;
