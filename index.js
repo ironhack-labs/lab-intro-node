90f75d82a697251b5090a426390ea3e515f0f17a
MarieAmp <marymary09@hotmail.com>
2020-06-30 18:59:00 +0200

    done

--- index.js
+++ index.js
@@ -1,17 +1,60 @@
 class SortedList {
-  constructor() {}
+  constructor(items, length) {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    
+    this.length = this.items.push(item);
+    this.items.sort((a, b) => a - b);
+  }
 
-  get(pos) {}
+  // getItemsLength() {
+  //   return this.items.length;
+  // }
 
-  max() {}
+  get(pos) {
+       if(pos<=this.length) {
+      return this.items[pos];
+    } else {
+      throw new Error("OutofBounds");
+    }
+  }
 
-  min() {}
+  max() {
+    if (this.length === 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      let i = this.length-1;
+      return this.items[i];
+    }
+  }
 
-  sum() {}
+  min() {
+    if (this.length < 1) {
+      throw new Error("EmptySortedList");
+    } else {
+      return this.items[0];
+    }
+  }
 
-  avg() {}
+  sum() {
+    let somme=this.items.reduce((acc,currentValue)=>acc+currentValue,0);
+    return somme
+  }
+  
+  avg() {
+    
+    let som=this.sum()
+    let nb=this.length;
+    if (this.length === 0) {
+      throw new Error("EmptySortedList")
+    }else{
+      return som/nb
+    }
+    
+  }
 }
 
 module.exports = SortedList;
