2327b38aed61fd0d56807a329143740ed6f47c82
Maya Despretz <mayadespretz@gmail.com>
2020-06-30 16:04:24 +0200

    lab done

--- index.js
+++ index.js
@@ -1,17 +1,58 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length
+  }
 
-  add(item) {}
+  add(item) {
 
-  get(pos) {}
+    this.items.push(item);
+    this.items.sort((a,b) => {
+      return a - b
+    });
+    this.length = this.items.length;
 
-  max() {}
+  }
 
-  min() {}
+  get(pos) {
+  if (pos > this.length) {
+    throw new Error('OutOfBounds');
+  };
+  return this.items[pos];
+  }
 
-  sum() {}
+  max() {
+  if (this.length === 0) {
+    throw new Error('EmptySortedList');
+  }
+  return this.items[this.length - 1];
+  }
 
-  avg() {}
+  min() {
+    if (this.length === 0) {
+      throw new Error('EmptySortedList');
+    }
+    return this.items[0];
+    }
+  
+
+  sum() {
+    // if (this.length === 0) {
+    //   return 0
+    // }
+    let res = 0
+    for (let i=0; i< this.length; i++) {
+      res += this.items[i]
+    }
+    return res;
+  }
+
+  avg() {
+    if (this.length === 0) {
+      throw new Error('EmptySortedList');
+    }
+    return this.sum()/this.length;
+  }
 }
 
 module.exports = SortedList;
