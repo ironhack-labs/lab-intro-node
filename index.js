31ecddebc3cf2b07490d5114b22a7184531a16e2
Alice Prigent <aliprigent@gmail.com>
2020-06-30 16:12:40 +0200

    [PAR-FT-062020] Alice Prigent

--- index.js
+++ index.js
@@ -1,17 +1,59 @@
 class SortedList {
-  constructor() {}
+  constructor(items, length) {
+    this.items = [],
+    this.length = this.items.length
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item)
+    this.items.sort(function(a,b){
+      return a - b
+    });
+    this.length = this.items.length
+  }
 
-  get(pos) {}
+  get(pos) {
+    if(pos > this.items.length){
+      throw new Error('OutOfBounds');
+    } else {
+      return this.items[pos]
+    }
+  }
 
-  max() {}
+  max() {
+    if(this.items.length === 0){
+      throw new Error('EmptySortedList');
+    } else {
+        let i = this.items.length - 1
+        return this.items[i]
+      }
+  }
 
-  min() {}
+  min() {
+    if(this.items.length === 0){
+      throw new Error('EmptySortedList');
+    } else {
+        return this.items[0]
+      }
+  }
 
-  sum() {}
+  sum() {
+    if(this.items.length === 0){
+      return 0
+    } else {
+      var sum = this.items.reduce((a, b)=> a + b,0)
+    } 
+    return sum
+  }
 
-  avg() {}
+  avg() {
+    if(this.items.length === 0){
+      throw new Error('EmptySortedList')
+    } else {
+      var sum = this.items.reduce((a, b)=> a + b,0)
+    } 
+    return sum / this.items.length
+  }
 }
 
 module.exports = SortedList;
