8243a43f42c7d6e455f2534a1b0d2090864d81c4
Hanna Murai <annmuray75@gmail.com>
2020-07-19 12:26:10 -0500

    hanna_m[ftwd2020june]

--- index.js
+++ index.js
@@ -1,17 +1,39 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = 0;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a, b) => a - b);
+    this.length++;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (this.length > pos) return this.items[pos];
+    else throw new Error("OutOfBounds");
+  }
 
-  max() {}
+  max() {
+    if (this.length > 0) return Math.max(...this.items);
+    else throw new Error("EmptySortedList");
+  }
 
-  min() {}
+  min() {
+    if (this.length > 0) return Math.min(...this.items);
+    else throw new Error("EmptySortedList");
+  }
+  sum() {
+    if (this.length > 0) return this.items.reduce((a, b) => a + b);
+    if (this.length === 0) return 0;
+  }
 
-  sum() {}
-
-  avg() {}
+  avg() {
+    if (this.length > 0)
+      return this.items.reduce((a, b) => a + b) / this.length;
+    else throw new Error("EmptySortedList");
+  }
 }
 
 module.exports = SortedList;
