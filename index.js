9e86cc8afbc4a5377fa6dd2255937e3a94a84ef8
Stephen Z <mayistep@icloud.com>
2020-07-20 11:48:07 -0400

    done

--- index.js
+++ index.js
@@ -1,17 +1,51 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = 0
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a, b) => a - b);
+    this.length++
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (this.items.length < pos) {
+       throw new Error('OutOfBounds');
+    } else {
+    return this.items[pos]
+  }
+}
 
-  max() {}
+  max() {
+    if (this.items.length > 0) {
+      return Math.max(...this.items)
+    } eles 
+      throw new Error("EmptySortedList");
+    
+  }
 
-  min() {}
+  min() {
+    if (this.items.length > 0) {
+      return Math.min(...this.items);
+    } else 
+    throw new Error("EmptySortedList");
+  }
 
-  sum() {}
+  sum() {
+    if (this.items.length > 0) {
+      return this.items.reduce((a, b) => a + b)
+    } else if (this.items.length == 0) {
+      return 0  
+    }
+  }
 
-  avg() {}
+  avg() {
+    if (this.items.length > 0)
+      return this.items.reduce((a, b) => a + b) / this.length;
+      else throw new Error("EmptySortedList");
+  }
 }
 
 module.exports = SortedList;
