541192b103ea89e88506899c8387d8afbdf2711d
Roy Gutierrez <roymg29@gmail.com>
2020-07-20 16:15:25 -0400

    done

--- index.js
+++ index.js
@@ -1,17 +1,50 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = 0;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a, b) => a - b);
+    this.length++;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos <= this.length) {
+      return this.items[pos];
+    }
+    throw new Error("OutOfBounds");
+  }
 
-  max() {}
+  max() {
+    if (this.items.length !== 0) {
+      return Math.max(...this.items);
+    }
+    throw new Error("EmptySortedList");
+  }
 
-  min() {}
+  min() {
+    if (this.items.length !== 0) {
+      return Math.min(...this.items);
+    }
+    throw new Error("EmptySortedList");
+  }
 
-  sum() {}
+  sum() {
+    if (this.items.length == 0) {
+      return 0;
+    } else {
+      return this.items.reduce((a, b) => a + b, 0);
+    }
+  }
 
-  avg() {}
+  avg() {
+    if (this.items.length !== 0) {
+      return this.items.reduce((a, b) => a + b, 0) / this.items.length;
+    }
+    throw new Error("EmptySortedList");
+  }
 }
 
 module.exports = SortedList;
