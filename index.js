72cb9b1e72075205227ebf528bbc0761b41a3623
Shane Schipper <shane99@vt.edu>
2020-07-20 12:06:08 -0400

    done

--- index.js
+++ index.js
@@ -1,17 +1,45 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = []
+    this.length = 0
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item)
+    this.items.sort((a, b) => a - b)
+    this.length++
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos < this.items.length && pos > -1) { return this.items[pos] }
+    throw new Error('OutOfBounds');
+  }
 
-  max() {}
+  max() {
+    if (this.length > 0) {
+      return this.items[this.length - 1]
+    }
+    throw new Error('EmptySortedList');
+  }
 
-  min() {}
+  min() {
+    if (this.length > 0) {
+      return this.items[0]
+    }
+    throw new Error('EmptySortedList');
+  }
 
-  sum() {}
+  sum() {
+    return this.items.reduce((a, v) => a + v, 0)
+  }
 
-  avg() {}
+  avg() {
+    if (this.length !== 0) {
+      return this.items.reduce((a, v) => a + v, 0) / this.length
+    }
+    throw new Error('EmptySortedList');
+  }
 }
 
 module.exports = SortedList;
+
