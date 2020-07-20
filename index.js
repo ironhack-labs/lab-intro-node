9b30625b340ba2b58753889c7eef7dcc8bbde37b
Daniel Perz <dpere316@fiu.edu>
2020-07-20 12:31:18 -0400

    done

--- index.js
+++ index.js
@@ -1,17 +1,62 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = []
+    this.length = 0
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item)
+    this.items.sort((a, b) => a - b)
+    this.length++
+  }
 
-  get(pos) {}
+  get(pos) {
 
-  max() {}
+    if(pos <= this.length)
+    {
+      return this.items[pos]
+    }
+   
+      throw new Error('OutOfBounds');
+    
+  }
 
-  min() {}
+  max() { 
+    if(this.length !== 0)
+    {
+      return Math.max(...this.items)
+      
+    }
+     throw new Error('EmptySortedList');
+  }
 
-  sum() {}
+  min() {
+    if(this.length !== 0)
+    {
+      return Math.min(...this.items)
+    }
+    throw new Error('EmptySortedList');
+    
+  }
 
-  avg() {}
+  sum() {
+    if(this.length === 0)
+    {
+      return 0
+    }
+    else{
+      return this.items.reduce((a,b)=> a + b,0)
+    }
+  }
+
+  avg() {
+    if(this.length !== 0)
+    {
+      return this.items.reduce((a,b)=> a + b,0) / this.length      
+    }
+    throw new Error('EmptySortedList');
+    
+  }
 }
 
 module.exports = SortedList;
