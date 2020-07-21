fbf67b64189216a9276a788f29b9f495e1f55a68
krankie <foresvives10@gmail.com>
2020-07-21 12:09:59 +0200

    'lab'

--- index.js
+++ index.js
@@ -1,17 +1,55 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+  this.items = [];
+  this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.length++;
+    this.items.sort((a,b) => a-b
+    )
 
-  get(pos) {}
+  }
 
-  max() {}
+  get(pos) {
+    if(pos <= this.length){
+    var number = this.items[pos]
+    console.log(number)
+    return number
+  }else{
+    throw new Error('OutOfBounds');
+  }
+  }
 
-  min() {}
+  max() {
+    if(this.length > 0){
+      return Math.max(...this.items)
+    }else{
+      throw new Error('EmptySortedList');
+    }
+  }
 
-  sum() {}
+  min() {
+    if(this.length > 0){
+      return Math.min(...this.items)
+    }else{
+      throw new Error("EmptySortedList");
+    }
+  } 
 
-  avg() {}
+
+  sum() {
+
+    if(this.length > 0){
+    return this.items.reduce((a,b)=> a+b)
+  }else return 0
+  }
+
+  avg() {
+    var total = this.items.reduce((a,b)=> a+b)
+    return total/this.length
+  }
 }
 
 module.exports = SortedList;
