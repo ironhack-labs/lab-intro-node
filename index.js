941938ed22fdc8d5cd4b0fca8b0c5e33735d61be
David Espinosa <davesespinosa@gmail.com>
2020-07-21 11:52:58 +0200

    Done

--- index.js
+++ index.js
@@ -1,17 +1,56 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    //iterar en el array items
+    /* añadir item
+    ordenar por orden ascendente */
+    this.items.push(item);
+    this.length++;
+    this.items.sort((a, b) => a - b);
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (this.length >= pos) {
+      return this.items[pos];
+    }
+    throw new Error("OutOfBounds");
+  }
 
-  max() {}
+  max() {
+    if (this.length !== 0) {
+      return this.items[this.length - 1];
+    }
+    throw new Error("EmptySortedList");
+  }
 
-  min() {}
+  min() {
+    if (this.length !== 0) {
+      return this.items[0];
+    }
+    throw new Error("EmptySortedList");
+  }
 
-  sum() {}
+  sum() {
+    if (this.length === 0) {
+      return 0;
+    }
+    var sum = this.items.reduce(
+      (accumulator, currentValue) => accumulator + currentValue
+    );
+    return sum;
+  }
 
-  avg() {}
+  avg() {
+    if (this.length === 0) {
+      throw new Error("EmptySortedList");
+    }
+    var result = this.sum() / this.length;
+    return result;
+  }
 }
 
 module.exports = SortedList;
