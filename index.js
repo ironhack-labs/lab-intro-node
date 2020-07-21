46cd3ab8f7ab653e3eb712934dc49af2bcb45807
victoriabarbero88 <victoriabarbero88@gmail.com>
2020-07-21 12:12:03 +0200

    Lab done

--- index.js
+++ index.js
@@ -1,17 +1,55 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
+  
+  add(item) {
+    
+    this.items.push(item);
+    this.length =this.items.length;
+    this.items.sort((a, b) => a-b);
+  }
 
-  add(item) {}
+  get(pos) {
+    if (pos > this.items.length) {
+      throw new Error('OutOfBounds') 
+    }else {
+      return this.items[pos]
+    } 
+    }
+   
+  max() {
+    if (this.items.length === 0) {
+      throw new Error('EmptySortedList') 
+    }else {
+      return this.items[this.length - 1]
+    } 
 
-  get(pos) {}
+  }
 
-  max() {}
+  min() {if (this.items.length === 0) {
+    throw new Error('EmptySortedList') 
+  }else {
+    return this.items[0]
+  } }
 
-  min() {}
+  sum() {
+    if (this.items.length === 0) {
+      return 0; 
+    } else {
+      return this.items.reduce((accumulator, currentValue) => accumulator + currentValue);
+    }
+  }
 
-  sum() {}
+  avg() {
+    if (this.items.length === 0) {
+      throw new Error('EmptySortedList');
+    } else {
+      return (this.items.reduce((accumulator, currentValue) => accumulator + currentValue))/this.length;
+    }
 
-  avg() {}
+  }
 }
 
 module.exports = SortedList;
