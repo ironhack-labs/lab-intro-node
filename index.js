a0aa27efc1ac4763ea8edf2f221204ad534578af
puigmar <ferran.puigmar@gmail.com>
2020-07-21 11:32:23 +0200

    Node lab

--- index.js
+++ index.js
@@ -1,17 +1,51 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a, b) => a - b)
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (this.items[pos] === undefined) {
+      throw new Error('OutOfBounds');
+    }
+    return this.items[pos];
+  }
 
-  max() {}
+  max() {
+    if(this.items.length === 0){
+      throw new Error('EmptySortedList');
+    }
+    //return Math.max(...this.items);
+    return this.items[this.length - 1];
+  }
 
-  min() {}
+  min() {
+    if(this.items.length === 0){
+      throw new Error('EmptySortedList');
+    }
+    //return Math.min(...this.items);
+    return this.items[0];
+  }
 
-  sum() {}
+  sum() {
+    if(this.items.length === 0){
+      return 0
+    }
+    return this.items.reduce( (acc, num) => acc + num);
+  }
 
-  avg() {}
+  avg() {
+    if(this.items.length === 0){
+      throw new Error('EmptySortedList');
+    }
+    return this.sum() / this.length
+  }
 }
 
 module.exports = SortedList;
