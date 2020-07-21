b080269903ec443a2aea139ca96ab848bf9a5c1c
Sergi Botargues <sergibotargues@MacBook-Air-de-Sergi.local>
2020-07-21 12:10:34 +0200

    finished

--- index.js
+++ index.js
@@ -1,17 +1,62 @@
 class SortedList {
-  constructor() {}
+constructor() {
+  this.items = [];
+  this.length = this.items.length;
+}
+add(item) {
+  
+  this.items.push(item);
+  this.length++;
 
-  add(item) {}
-
-  get(pos) {}
-
-  max() {}
-
-  min() {}
-
-  sum() {}
-
-  avg() {}
+  this.items.sort((a,b)=>{return a-b});
 }
 
+get(pos){
+  if (this.items.length === 0 || pos > this.length) {
+    throw new Error("OutOfBounds");
+  }
+  let positionValue = this.items[pos];
+  return positionValue;
+  
+}
+
+max() {
+
+  if (this.length == 0) {
+    throw new Error("EmptySortedList");
+  } else {
+    let max = Math.max(...this.items);
+    return max;
+  }
+
+}
+min() {
+
+  if (this.length == 0) {
+    throw new Error("EmptySortedList");
+  } else {
+    return Math.min(...this.items);
+  }
+}
+  sum() {
+    if (this.length == 0) {
+      return 0
+    } else {
+     let reducer = (acc, cv) => acc + cv;
+     return this.items.reduce(reducer);
+  }
+  }
+
+  avg() {
+    if (this.length == 0) {
+      throw new Error("EmptySortedList");
+    } else {
+    let avgArr = this.sum()/this.items.length;
+    return avgArr;
+  }
+
+  }
+
+
+}
 module.exports = SortedList;
