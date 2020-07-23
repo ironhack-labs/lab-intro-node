322823f1c1c34cc4c9ea6174c97c17f39dc204d3
Kevin Hernandez <kevin.hernandez001@gmail.com>
2020-07-22 21:30:36 -0400

    iterations 1-4

--- index.js
+++ index.js
@@ -4,11 +4,27 @@ class SortedList {
     this.length = this.items.length;
   }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a, b) => a - b);
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos >= this.length){
+      throw new Error('OutOfBounds');
+    } else{
+      return this.items[pos];
+    }
+  }
 
-  max() {}
+  max() {
+    if (this.length === 0){
+      throw new Error('EmptySortedList');
+    } else {
+      return this.items.reduce((a, b) => a > b ? a : b);
+    }
+  }
 
   min() {}
 
