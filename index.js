93b98f367989f053066f6668705390fe670ebe1b
Kevin Hernandez <kevin.hernandez001@gmail.com>
2020-07-22 21:36:46 -0400

    done

--- index.js
+++ index.js
@@ -26,11 +26,25 @@ class SortedList {
     }
   }
 
-  min() {}
+  min() {
+    if (this.length === 0){
+      throw new Error('EmptySortedList');
+    } else {
+      return this.items.reduce((a, b) => a < b ? a : b);
+    }
+  }
 
-  sum() {}
+  sum() {
+     return this.items.reduce((a, b) => a + b, 0);
+  }
 
-  avg() {}
+  avg() {
+    if (this.length === 0){
+      throw new Error('EmptySortedList');
+    } else {
+      return this.sum() / this.length;
+    } 
+  }
 }
 
 module.exports = SortedList;
