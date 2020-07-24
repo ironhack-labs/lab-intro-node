73b1951783cc3deb8679ecefe807d1904fc9efcf
Pedro Magalhães Lamy <pedrolamy@Air-de-Pedro.lan>
2020-07-25 00:44:08 +0100

    ex

--- index.js
+++ index.js
@@ -1,17 +1,41 @@
 class SortedList {
-  constructor() {}
-
-  add(item) {}
-
-  get(pos) {}
-
-  max() {}
-
+  constructor(){}
+  constructor(){
+    this.items = [];
+    this.length = this.items.length;
+  }
+  add(item){}
+  add(item){
+    this.items.push(item);
+    this.items.sort((a, b) => a - b)
+    this.length = this.items.length;
+  }
+  get(pos){}
+  get(pos){
+    if (pos <= this.length){
+      return this.items[pos];
+    }
+      throw new Error('OutOfBounds');
+    }
+    max(){}
+    max(){
+    if (this.items.length === 0){
+      throw new Error('EmptySortedList');
+    } else {
+      return Math.max(...this.items);
+    }
+  }
   min() {}
-
+  min() {
+    if (this.length === 0) {
+      throw new Error('EmptySortedList');
+    }
+    return this.items[0];
+  }
   sum() {}
-
   avg() {}
 }
-
 module.exports = SortedList;
+// new SortedList 
+items (array);
+length (elements in the array)
