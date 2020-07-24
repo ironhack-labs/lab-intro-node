6a845d0f4dbabcfbf902412ba4fde0ed96cc16ec
Raquel Nascimento <raquel.fm.nascimento@gmail.com>
2020-07-24 18:09:22 +0100

    done

--- index.js
+++ index.js
@@ -1,17 +1,60 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = 0
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort(function(a, b){return a-b});
+    this.length =+1;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if(this.items.includes(this.items[pos])){
+      return this.items[pos]
+    } else {
+      throw new Error('OutOfBounds')
+    }
+  }
 
-  max() {}
+  max() {
+    if (this.length>0) {
+      var max = this.items.reduce(function(a, b) {
+        return Math.max(a, b);
+    });
+    return max
+    } else {
+      throw new Error('EmptySortedList')
+    }
+  }
 
-  min() {}
+  min() {
+    if (this.length>0) {
+      var min = this.items.reduce(function(a, b) {
+        return Math.min(a, b);
+    });
+    return min
+    } else {
+      throw new Error('EmptySortedList')
+    }
+  }
 
-  sum() {}
+  sum() {
+    const sum = this.items.reduce (function (accumulator, currentValue) {
+      return accumulator + currentValue
+    },0)
+    return sum
+  }
 
-  avg() {}
+  avg() {
+    if(this.length==0) {
+      throw new Error (`EmptySortedList`)
+    } else {
+      let totalAvg = (this.sum()/this.items.length).toFixed(2);
+      return parseFloat(totalAvg)
+    }
+  }
 }
 
 module.exports = SortedList;
