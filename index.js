cfee029fe8f634ba7927562d7492ff99109d8eb9
Rodrigohernandezsan <rodrigoherandezsan@gamil.com>
2020-07-24 10:58:47 +0200

    Done

--- index.js
+++ index.js
@@ -1,17 +1,46 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a, b) => a - b);
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
 
-  max() {}
+    if (pos >= this.length) throw new Error('OutOfBounds');
+    return this.items[pos];
+  }
 
-  min() {}
+  max() {
 
-  sum() {}
+    if (this.length === 0) throw new Error('EmptySortedList');
+    return this.items[this.length - 1];
 
-  avg() {}
+  }
+
+  min() {
+    if (this.length === 0) throw new Error('EmptySortedList');
+    return this.items[0];
+  }
+
+  sum() {
+
+    if (this.length === 0) return 0;
+    return this.items.reduce((acc, curVal) => acc + curVal);
+  }
+
+  
+
+  avg() {
+    if (this.length === 0) throw new Error('EmptySortedList');
+    return this.sum()/this.length;
+  }
+  
 }
 
 module.exports = SortedList;
