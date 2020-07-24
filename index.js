9738c99b1175ccef969fb1652d802ae8ae7e437d
Lynn W-B <l.welshboucard@yahoo.co.uk>
2020-07-24 23:14:06 +0200

    Done

--- index.js
+++ index.js
@@ -1,17 +1,50 @@
 class SortedList {
-  constructor() {}
+  constructor(items, length) {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a,b) => a - b);
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos < this.length && pos >= 0){
+    return this.items[pos];
+  } else {
+    throw new Error('OutOfBounds');
+  };
+}
 
-  max() {}
+  max() {
+    if (this.length === 0){
+      throw new Error ('EmptySortedList');
+    } else {
+    return Math.max(...this.items);
+  };
+}
 
-  min() {}
+  min() {
+    if (this.length === 0){
+      throw new Error ('EmptySortedList');
+    } else {
+    return Math.min(...this.items);
+  };
+  }
 
-  sum() {}
+  sum() {
+    return this.items.reduce((accum, current) => accum + current, 0);
+  }
 
-  avg() {}
+  avg() {
+    if (this.items.length === 0){
+      throw new Error ('EmptySortedList');
+    } else {
+      return this.sum()/this.length;
+    }
+  }
 }
 
 module.exports = SortedList;
