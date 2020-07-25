ab8d7c6c449fe0376bc73c86f41cea1a08f65f70
Tomas Bennasar <tomas.bennasar@gmail.com>
2020-07-24 20:04:49 -0400

    completed

--- index.js
+++ index.js
@@ -1,17 +1,70 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort(function(a,b){return a-b});
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if(pos>this.items.length){
+      throw new Error('OutOfBounds');
+    }else{
+      return this.items[pos];
+    }
+  }
 
-  max() {}
+  max() {
+    if(this.length === 0){
+      throw new Error('EmptySortedList');
+    }else{
+      let highest = 0;
+      this.items.forEach(ele =>{
+        if(ele>highest){
+          highest = ele;
+        }
+      })
+      return highest;
+    }
+  }
 
-  min() {}
+  min() {
+    if(this.length === 0){
+      throw new Error('EmptySortedList');
+    }else{
+      let lowest = 1000000;
+      this.items.forEach(ele =>{
+        if(ele<lowest){
+          lowest = ele;
+        }
+      })
+      return lowest;
+    }
+  }
 
-  sum() {}
+  sum() {
+    if(this.length === 0){
+      return 0;
+    }else{
+      let reducer = (accumulator, currentValue) => accumulator + currentValue;
+      return this.items.reduce(reducer) 
+    }
+  }
 
-  avg() {}
+  avg() {
+    if(this.length === 0){
+      throw new Error('EmptySortedList');
+    }else{
+      let reducer = (accumulator, currentValue) => accumulator + currentValue;
+      return this.items.reduce(reducer)/this.length 
+    }
+  }
+
+  
 }
 
 module.exports = SortedList;
