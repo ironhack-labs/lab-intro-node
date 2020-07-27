6d24131f8fa7cd8fad67468ed39c58d03f148cc1
koladeg <tayokay@yahoo.com>
2020-07-27 16:37:29 +0100

    Done

--- index.js
+++ index.js
@@ -1,17 +1,45 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [],
+    this.length = 0
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item)
+    this.length += 1
+    this.items = this.items.sort((a,b) => a - b )
+  }
 
-  get(pos) {}
+  get(pos) {
+    if( pos <= this.items.length){
+      return this.items[pos]
+    }else throw new Error('OutofBounds')
+  }
 
-  max() {}
+  max() {
+    if( this.items.length > 0){
+      return Math.max(...this.items)
+    } throw new Error('EmptySortedList')
+  }
 
-  min() {}
+  min() {
+    if( this.items.length > 0){
+      return Math.min(...this.items)
+    } throw new Error('EmptySortedList')
+  }
 
-  sum() {}
+  sum() {
+    if( this.items.length > 0){
+      let sum = this.items.reduce((a,b) => a + b)
+      return sum
+    } return 0 
+  }
 
-  avg() {}
+  avg() {
+    if( this.items.length > 0){
+      return this.sum()/this.length
+    } throw new Error('EmptySortedList')
+  }
 }
 
 module.exports = SortedList;
