c0a99b9f52ab5354dcc63d291da15e05f48b3d03
gonzalo-development <gperalcorrales@gmail.com>
2020-07-28 17:41:39 +0200

    git commit

--- index.js
+++ index.js
@@ -1,17 +1,67 @@
 class SortedList {
-  constructor() {}
+ 
+  constructor() {
+      this.items = [];
+      this.length= this.items.length;
 
-  add(item) {}
+  }
 
-  get(pos) {}
+  add(item) {
+   
+  this.items.push(item);
+  this.items.sort(function(a,b){
+    return a-b
+  })
+  this.length= this.items.length
+  }
 
-  max() {}
+  get(pos) {
+    //if(!this.items.includes)
+    if(pos > this.length){
+      throw new Error('OutOfBounds');
+    }else{
+      return this.items[pos];
+    };
+    
+    }
 
-  min() {}
 
-  sum() {}
+  max() {
+    
+      if(this.items.length == 0){
+        throw new Error('EmptySortedList');
+      }else{
+        return this.items[this.items.length-1];
+      }
+  }
 
-  avg() {}
+  min() {
+
+    if(this.items.length == 0){
+      throw new Error('EmptySortedList');
+    }else{
+      return this.items[0];
+    }
+
+  }
+
+  sum() {
+    var sumTotal=0;
+    for(var i=0; i< this.items.length; i++){
+      if(this.items.length == 0){
+        return 0;
+      }
+      sumTotal += this.items[i]
+    }
+    return sumTotal;
+  }
+
+  avg() {
+    if(this.items.length == 0){
+      throw new Error('EmptySortedList');
+}
+return this.sum()/this.items.length;
+}
 }
 
 module.exports = SortedList;
