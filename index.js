54b7f6fa59ff69d607657736bfc34a9720667d12
Alejandro Hernández <alejandro_hj.11@outlook.com>
2020-07-28 15:32:54 +0100

    Done

--- index.js
+++ index.js
@@ -1,17 +1,43 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [],
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a,b)=>{
+      return a - b;
+    });
+    this.length ++;
+    return this.items;
+  }
 
-  get(pos) {}
+  get(pos) {
+      if(this.items[pos]) return this.items[pos]
+      else throw new Error('OutOfBounds');
+  }
 
-  max() {}
+  max() {
+    if(this.items.length !=0) return this.items[this.items.length - 1]
+    else throw new Error('EmptySortedList');
+  }
 
-  min() {}
+  min() {
+    if(this.items.length !=0) return this.items[0]
+    else throw new Error('EmptySortedList');
+  }
 
-  sum() {}
-
-  avg() {}
+  sum() {
+    let sum = this.items.reduce((acc,elem)=>{
+        return acc + elem;
+    }, 0);
+    return sum;
+  }
+  avg() {
+    if(this.items.length !=0) return this.sum()/this.items.length;
+    else throw new Error('EmptySortedList');
+  }
 }
 
 module.exports = SortedList;
