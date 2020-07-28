48d941d2f752e39f1d2c59877cccd49ce7c7a293
MartaLourido <martalouri@gmail.com>
2020-07-28 16:56:33 +0200

    Done by Marta and Bodei

--- index.js
+++ index.js
@@ -1,17 +1,59 @@
 class SortedList {
-  constructor() {}
+  constructor(items, length) {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a,b) => a-b);
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos <= this.items.length){
+      return this.items[pos];
+    }else{
+      throw new Error('OutOfBounds');
+    }
+  }
 
-  max() {}
+  max() {
+    if (this.items.length === 0){
+      throw new Error('EmptySortedList');
+    }else{
+     return Math.max(...this.items);
+    }
+  }
 
-  min() {}
+  min() {  if (this.items.length == 0){
+    throw new Error('EmptySortedList');
+  }else{
+    return Math.min(...this.items);
+  }
+}
 
-  sum() {}
+  sum() {
+    if (this.items.length === 0){
+      return 0
+    }else{
+      let sum = this.items.reduce(function (accumulator, currentValue) {  
+        return accumulator + currentValue
+        }, 0)
+      return sum;   
+    }
+  }
 
-  avg() {}
+  avg() {
+    if (this.items.length === 0){
+      throw new Error('EmptySortedList');
+    }else{
+      let sum = this.items.reduce(function (accumulator, currentValue) {  
+        return accumulator + currentValue
+        }, 0)
+      return sum/this.items.length;   
+    }
+  }
 }
 
 module.exports = SortedList;
