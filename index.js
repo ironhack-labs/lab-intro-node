92df1c60be992be16945101b99f2bca288539d30
psophielam <psophielam92@gmail.com>
2020-08-07 00:06:37 +0200

    all done

--- index.js
+++ index.js
@@ -48,7 +48,13 @@ class SortedList {
   }
 
 
-  avg() {}
+  avg() {
+    if (this.length == 0) {
+      throw new Error('EmptySortedList');
+    } else {
+      return this.sum()/this.length;
+    }
+  }
 }
 
 module.exports = SortedList;
