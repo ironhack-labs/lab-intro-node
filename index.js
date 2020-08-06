35e677118d6c2a4ff62280a1b36d06389811cbcd
James <jamescarclan@yahoo.com>
2020-08-06 22:09:43 +0200

    itération3done

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = 0;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort(function (a, b) {
+      return a - b;
+    });
+    this.length++;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos <= this.items.length) {
+      return this.items[pos];
+    } else {
+      throw new Error("OutOfBounds");
+    }
+  }
 
   max() {}
 
