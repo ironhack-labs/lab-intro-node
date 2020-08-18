ffafc1fd4d9e86d9632ddc8e1da8b597037e8a09
Maria Sharpar <masha.sharpar@gmail.com>
2020-08-18 14:28:30 +0200

    done with bonuses

--- index.js
+++ index.js
@@ -1,17 +1,67 @@
 class SortedList {
-  constructor() {}
+  constructor(){
+    this.items=[];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+   this.items.push(item);
+   this.length++;
+   this.items.sort(function(a, b){return a-b});
+   
+  }
+  get(pos) {
+    if(!this.items[pos]){
+      throw new Error('OutOfBounds');
+    }else {
+      return this.items[pos]; 
+    }
 
-  get(pos) {}
+  }
 
-  max() {}
 
-  min() {}
+  max() {
+    if (this.length === 0 ){
+      throw new Error('EmptySortedList');
+    }else {
+       return  Math.max.apply(Math, this.items);
+    }
+  }
 
-  sum() {}
+  
+  min() {
+    if (this.length === 0 ){
+      throw new Error('EmptySortedList');
+    }else {
+       return  Math.min.apply(Math, this.items);
+    }
+  }
 
-  avg() {}
+  sum() {
+    if (this.length === 0 ){
+      return 0;
+    }else {
+      let sum = this.items.reduce(function(a, b){
+        return a + b;
+    }, 0);
+    return sum;
+    }  
+  }
+
+
+
+  avg() {
+    if (this.length === 0 ){
+      throw new Error('EmptySortedList');;
+    }else {
+      let avg = this.items.reduce(function(a, b){
+        return a + b;
+    }, 0)/this.length;
+    return avg;
+    }  
+
+  }
 }
+
 
 module.exports = SortedList;
