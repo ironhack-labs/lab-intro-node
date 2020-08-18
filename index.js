1842110e76108a5c89a78438bf935a255562cb42
ViktorJT <viktor.jensentorp@hyperisland.se>
2020-08-18 16:20:58 +0200

    done

--- index.js
+++ index.js
@@ -1,17 +1,50 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a,b) => a - b);
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos < this.length) {
+      return this.items[pos];
+    } else {
+      throw new Error('OutOfBounds');
+    }
+  }
 
-  max() {}
+  max() {
+    if (this.length > 0) {
+      return this.items.reduce((a,b) => Math.max(a,b))
+    } else {
+      throw new Error('EmptySortedList');
+    }
+  }
 
-  min() {}
+  min() {
+    if (this.length > 0) {
+      return this.items.reduce((a,b) => Math.min(a,b));
+    } else {
+      throw new Error('EmptySortedList');
+    }
+  }
 
-  sum() {}
+  sum() {
+    return (this.length > 0) ? this.items.reduce((a,b) => a + b) : 0;
+  }
 
-  avg() {}
+  avg() {
+    if (this.length > 0) {
+      return this.items.reduce((a,b) => a + b, 0) / this.items.length;
+    } else {
+      throw new Error('EmptySortedList');
+    }
+  }
 }
 
 module.exports = SortedList;
