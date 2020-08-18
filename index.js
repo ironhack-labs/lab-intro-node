f223a837706e64e07c8a8f3561908ae6d5566cfd
Esra Ozel <esra.ozel_@hotmail.com>
2020-08-18 14:28:12 +0200

    done

--- index.js
+++ index.js
@@ -1,17 +1,63 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.length++;
+    this.items.sort(function(a,b){return a- b});
+   
+  }
 
-  get(pos) {}
+  get(pos) {
+    if(!this.items[pos]){
+      throw new Error('OutOfBounds');
+    }else {
+      return this.items[pos]; 
+    }
+  }
 
-  max() {}
+  max() {
+    if (this.length === 0 ){
+      throw new Error('EmptySortedList');
+    }else {
+       return  Math.max.apply(Math, this.items);
+    }
+  }
 
-  min() {}
+  min() {
+    if (this.length === 0 ){
+      throw new Error('EmptySortedList');
+    }else {
+       return  Math.min.apply(Math, this.items);
+    }
+  }
 
-  sum() {}
+  sum() {
+    if ( this.length === 0) {
+      return 0 ;
+    } else {
+      let sum = this.items.reduce(function(a, b){
+        return a + b;
+      }, 0);
+      return sum;
+    }
+  }
 
-  avg() {}
+  avg() {
+    if ( this.length === 0 ){
+      throw new Error('EmptySortedList');
+    } else {
+      let sum = this.items.reduce(function(a, b){
+        return a + b;
+      }, 0);
+      let avg = sum / this.length;
+      return avg;
+    }
+   
+  }
 }
 
 module.exports = SortedList;
