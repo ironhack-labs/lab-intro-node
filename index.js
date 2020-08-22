3522aabca5a14ce33a850516ac85ae3d3a6391d3
Yuka Yeh <yukaka210@gmail.com>
2020-08-22 11:56:43 +0200

    done

--- index.js
+++ index.js
@@ -1,17 +1,57 @@
 class SortedList {
-  constructor() {}
+  constructor(items,length) {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item)
+    this.items.sort((a,b)=>a-b)
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos <= this.items.length){ 
+      const position = this.items.indexOf(pos)
+      return position
+    } else {
+      throw new Error('OutOfBounds');
+    }
+  }
 
-  max() {}
+  max() {
+    if (this.items.length == 0) {
+      throw new Error('EmptySortedList');
+    } else {
+      return Math.max(...this.items)
+    }
+  }
 
-  min() {}
+  min() {
+    if (this.items.length == 0) {
+      throw new Error('EmptySortedList');
+    } else {
+      return Math.min(...this.items)
+    }
+  }
 
-  sum() {}
+  sum() {
+    if (this.items.length == 0) {
+      return 0
+    } else {
+      return this.items.reduce((a,b) => a + b, 0)
+    }
+  }
 
-  avg() {}
+  avg() {
+    if (this.items.length == 0) {
+      throw new Error('EmptySortedList');
+    } else {
+      return this.items.reduce((a,b) => a + b,0)/this.items.length
+    }
+  }
 }
 
 module.exports = SortedList;
+
+
