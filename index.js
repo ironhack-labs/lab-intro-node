21dcc0d9786d9d29a1348152f60430212f6ad88e
edvicaty <edvicaty@hotmail.com>
2020-08-24 13:11:58 -0500

    done

--- index.js
+++ index.js
@@ -1,17 +1,62 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a, b) => {
+      return a - b;
+    });
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (this.items[pos] === undefined) {
+      throw new Error("OutOfBounds");
+    } else {
+      return this.items[pos];
+    }
+  }
 
-  max() {}
+  max() {
+    if (this.length === 0) {
+      throw new Error("OutOfBounds");
+    } else {
+      return this.items[this.length - 1];
+    }
+  }
 
-  min() {}
+  min() {
+    if (this.length === 0) {
+      throw new Error("OutOfBounds");
+    } else {
+      return this.items[0];
+    }
+  }
 
-  sum() {}
+  sum() {
+    let sum = 0;
+    this.items.forEach((number) => {
+      sum = sum + number;
+      console.log(`the number is ${number}`);
+    });
+    console.log(`the sum is ${sum}`);
+    return Number(sum);
+  }
 
-  avg() {}
+  avg() {
+    if (this.length === 0) {
+      throw new Error("OutOfBounds");
+    } else {
+      let sum = 0;
+      this.items.forEach((number) => {
+        sum = sum + number;
+      });
+      return Number(sum / this.length);
+    }
+  }
 }
 
 module.exports = SortedList;
