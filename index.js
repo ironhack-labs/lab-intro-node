2d229d209ffca37e7e2236499ce59cc93f5e32ae
Yvan Menuet <yvan.menuet@gmail.com>
2020-08-24 13:06:03 -0500

    done

--- index.js
+++ index.js
@@ -1,17 +1,41 @@
 class SortedList {
-  constructor() {}
+    constructor() {
+        this.items = [];
+        this.length = this.items.length;
+    }
 
-  add(item) {}
+    add(item) {
+        let index = 0;
+        this.items.forEach((el, i) => {
+            if (el < item) index = i + 1
+        })
+        this.items.splice(index, 0, item);
+        this.length = this.items.length;
+    }
 
-  get(pos) {}
+    get(pos) {
+        if (pos > this.length - 1) throw new Error('OutOfBounds');
+        else return this.items[pos];
+    }
 
-  max() {}
+    max() {
+        if (this.length) return Math.max(...this.items)
+        else throw new Error('EmptySortedList');
+    }
 
-  min() {}
+    min() {
+        if (this.length) return Math.min(...this.items)
+        else throw new Error('EmptySortedList');
+    }
 
-  sum() {}
+    sum() {
+        return this.items.reduce((a, b) => a + b, 0);
+    }
 
-  avg() {}
+    avg() {
+        if (this.length) return this.items.reduce((a, b) => a + b) / this.length;
+        else throw new Error('EmptySortedList');
+    }
 }
 
 module.exports = SortedList;
