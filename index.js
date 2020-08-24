33488f21eb35fb1510edad868b1bc9f3131a35f9
eliot <eliot@pop-os.localdomain>
2020-08-24 14:01:06 -0500

    Done

--- index.js
+++ index.js
@@ -1,17 +1,60 @@
 class SortedList {
-  constructor() {}
+    constructor(item) {
+        this.items = [];
+        this.length = this.items.length;
+    }
 
-  add(item) {}
+    add(item) {
+        this.items.push(item)
+        this.items.sort(function(a, b) {
+            return a - b;
+        });
+        this.length = this.items.length;
+    }
 
-  get(pos) {}
+    get(pos) {
+        if (pos > this.items.length) {
+            throw new Error('OutOfBounds');
+        } else {
+            return this.items[pos];
+        }
+    }
 
-  max() {}
+    max() {
+        if (this.items.length === 0) {
+            throw new Error('EmptySortedList');
+        } else {
+            return this.items[Number(this.items.length - 1)];
+        }
+    }
 
-  min() {}
+    min() {
+        if (this.items.length === 0) {
+            throw new Error('EmptySortedList');
+        } else {
+            return this.items[0];
+        }
+    }
 
-  sum() {}
+    sum() {
+        if (this.items.length === 0) {
+            return 0;
+        } else {
+            return this.items.reduce((total, num) => {
+                return total + num;
+            })
+        }
+    }
 
-  avg() {}
+    avg() {
+        if (this.items.length === 0) {
+            throw new Error('EmptySortedList');
+        } else {
+            return this.items.reduce((total, num) => {
+                return total + num;
+            }) / this.items.length;
+        }
+    }
 }
 
 module.exports = SortedList;
