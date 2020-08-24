4591e16108073f17cf096dade9208e8ea72520c8
Mai Yamawaki <joooooy031006@icloud.com>
2020-08-24 13:48:45 -0500

    done

--- index.js
+++ index.js
@@ -1,17 +1,53 @@
 class SortedList {
-  constructor() {}
+  constructor(items, length){
+    this.items=[]
+    this.length=this.items.length
+  }
+  add(item){
+    while(this.items.sort()){
+      this.items.push(item)
+    }
+  }
+  get(pos){
+    if(pos>this.items.length){
+      throw new Error('OutOfBounds');
+    }
+    return this.items[pos]
+  }
+  max(){
+    if(!this.items.length){
+      throw new Error('EmptySortedList')
+    }
 
-  add(item) {}
+    return Math.max.apply(null, this.items)
+  }
+  min(){
+    if(!this.items.length){
+      throw new Error('EmptySortedList')
+    }
 
-  get(pos) {}
+    return Math.min.apply(null, this.items)
+  }
+  sum(){
+    let total=0;
+    for(let i=0; i<this.items.length; i++){
+      total += this.items[i]
+    }
+    return total;
+  }
+  avg(){
+    if(!this.items.length){
+      throw new Error('EmptySortedList')
+    }
 
-  max() {}
-
-  min() {}
-
-  sum() {}
-
-  avg() {}
+    let total=0;
+    for(let i=0; i<this.items.length; i++){
+      total += this.items[i]
+    }
+    let avg=total/this.items.length;
+    return avg
+  }
 }
 
 module.exports = SortedList;
+
