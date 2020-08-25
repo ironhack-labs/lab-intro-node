3480becb147ef9394caf31d975e63f289891ff07
Alba Mulero Otón <albamulero@MacBook-Pro-de-Alba.local>
2020-08-24 20:02:52 -0500

    Fin

--- index.js
+++ index.js
@@ -1,13 +1,39 @@
 class SortedList {
-  constructor() {}
 
-  add(item) {}
+  // Inicio el constructor...
+  constructor() {
+    this.items = new Array()
+    this.length = 0
+  }
 
-  get(pos) {}
+  add(item) {
+    
+    // Puseamos el item que nos pasan
+    this.items.push(item)
 
-  max() {}
+    // Una vez pueseado ordenamos la lista
+    this.items.sort(function(a,b){return a-b})
 
-  min() {}
+    // Actualizamos la longitud
+    this.length = this.items.length
+   
+  }
+
+  get(pos) {
+    if(pos > this.length) throw new Error ('OutOfBounds')
+    return this.items[pos]
+  
+  }
+
+  max() {
+    if(this.items == "") throw new Error('EmptySortedList')
+    return Math.max(...this.items)
+  }
+
+  min() {
+    if(this.items == "") throw new Error('EmptySortedList')
+    return Math.min(...this.items)
+  }
 
   sum() {}
 
