7352152e4b0160b478a0e8977ca60b2e36097d59
Cynthia Takiishi <cynthiaakemit@gmail.com>
2020-08-26 23:13:41 -0300

    Mocha test done

--- index.js
+++ index.js
@@ -1,17 +1,55 @@
+/* eslint-disable no-plusplus */
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    if (this.length === 0) {
+      this.items.push(item);
+      this.length = this.items.length;
+    } else {
+      for (let i = 0; i < this.length; i++) {
+        if (item < this.items[i]) {
+          this.items.splice(i, 0, item);
+          this.length = this.items.length;
+        }
+      }
+    }
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos < this.lenght || pos >= 0 || this.lenght === 0) {
+      return this.items[pos];
+    }
+    throw new Error('OutOfBounds');
+  }
 
-  max() {}
+  max() {
+    if (this.items.length === 0 || this.length === 'undefined') {
+      throw new Error('EmptySortedList');
+    }
+    const max = this.items.reduce((a, b) => Math.max(a, b));
+    return max;
+  }
 
-  min() {}
+  min() {
+    if (this.length === 0 || this.length === 'undefined') {
+      throw new Error('EmptySortedList');
+    }
+    const min = this.items.reduce((a, b) => Math.min(a, b));
+    return min;
+  }
 
-  sum() {}
+  sum() {
+    const sum = this.items.reduce((acc, cur) => acc + cur);
+    return sum;
+  }
 
-  avg() {}
+  avg() {
+    return this.sum() / this.length;
+  }
 }
 
 module.exports = SortedList;
