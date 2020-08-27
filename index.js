40b1e72946e1cf91bd782971642ddb1790237c1a
Rodrigo Muniz <ramunizbh@yahoo.com.br>
2020-08-26 22:31:39 -0300

    almost done

--- index.js
+++ index.js
@@ -1,17 +1,34 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = 0;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items = this.items.sort((a,b)=>a-b);
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
 
-  max() {}
+  }
 
-  min() {}
+  max() {
 
-  sum() {}
+  }
 
-  avg() {}
+  min() {
+
+  }
+
+  sum() {
+
+  }
+
+  avg() {
+
+  }
 }
 
 module.exports = SortedList;
