4c99ef8a9fde8f91e0c6db694db92460dd8788f4
unimatrix2 <dnny.nis@gmail.com>
2020-08-27 01:26:48 -0300

    finished everything

--- index.js
+++ index.js
@@ -1,17 +1,43 @@
+/* eslint-disable max-len */
+
 class SortedList {
-  constructor() {}
+    constructor() {
+        this.items = [];
+        this.length = 0;
+    }
 
-  add(item) {}
+    add(item) {
+        this.items.push(item);
+        this.items.sort((a, b) => a - b);
+        this.length += 1;
+    }
 
-  get(pos) {}
+    get(pos) {
+        if (this.items[pos]) {
+            return this.items[pos];
+        }
+        throw new Error('OutOfBounds');
+    }
 
-  max() {}
+    max() {
+        if (this.items.length !== 0) { return Math.max(...this.items); } // I didn't believe using spread here would work
+        throw new Error('EmptySortedList');
+    }
 
-  min() {}
+    min() {
+        if (this.items.length !== 0) { return Math.min(...this.items); }
+        throw new Error('EmptySortedList');
+    }
 
-  sum() {}
+    sum() {
+        if (this.items.length !== 0) { return this.items.reduce((a, b) => a + b); }
+        return 0;
+    }
 
-  avg() {}
+    avg() {
+        if (this.items.length !== 0) { return this.items.reduce((a, b) => a + b) / this.items.length; }
+        throw new Error('EmptySortedList');
+    }
 }
 
 module.exports = SortedList;
