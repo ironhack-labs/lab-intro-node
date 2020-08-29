a0ce9884ff10faeb6ead87dbb210a8f6a1d4dd45
Grazielli Pozzi <graziellipozzi@gmail.com>
2020-08-29 09:52:12 -0300

    done

--- index.js
+++ index.js
@@ -1,17 +1,83 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    if (this.length === 0) {
+      this.items.push(item);
+      this.length++;
+    } else {
+      for (let i = 0; i < this.length; i++) {
+        if (item > this.items[i]) {
+          this.items.splice(i - 1, 0, item);
+          this.length++;
+        } else {
+          this.items.push(item);
+          this.length++;
+        }
+      }
+    }
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos < this.length) {
+      return this.items[pos];
+    } else {
+      throw new Error('OutOfBounds');
+    }
+  }
 
-  max() {}
+  max() {
+    let maxNumber = this.items[0];
+    if (this.items.length === 0) {
+      throw new Error('EmptySortedList');
+    } else {
+      for (let i = 0; i < this.items.length; i++) {
+        if (this.items[i] > maxNumber) {
+          maxNumber = this.items[i];
+        }
+      }
+      return maxNumber;
+    }
+  }
 
-  min() {}
+  min() {
+    let minNumber = this.items[0];
+    if (this.items.length === 0) {
+      throw new Error('EmptySortedList');
+    } else {
+      for (let i = 0; i < this.items.length; i++) {
+        if (this.items[i] < minNumber) {
+          minNumber = this.items[i];
+        }
+      }
+      return minNumber;
+    }
+   }
 
-  sum() {}
+  sum() {
+    let sum = 0;
+    if (this.items === 0) {
+      return 0;
+    } else {
+      for (let i=0; i<this.items.length; i++) {
+        sum += this.items[i];
+      }
+    }
+    return sum;
+   }
 
-  avg() {}
+  avg() {
+    let average = 0;
+    if (this.items.length === 0) {
+      throw new Error('EmptySortedList');
+    } else {
+      average = this.sum/(this.items.length - 1);
+      return average;
+    }
+   }
 }
 
 module.exports = SortedList;
