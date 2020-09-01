9f3e26194a4a7a0b5267870bc1b8d06a3fdb2299
Veronica De Leon <v.dleon@gmail.com>
2020-09-01 14:46:41 +0200

    lab done

--- index.js
+++ index.js
@@ -1,17 +1,46 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items = [...this.items, item];
+    // this.items.push(item);
+    this.items.sort((a, b) => a - b);
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos > this.items.length - 1) throw new Error("OutOfBounds");
+    else return this.items[pos];
+  }
 
-  max() {}
+  max() {
+    if (this.length === 0) throw new Error("EmptySortedList");
+    else return this.items[this.length - 1];
+  }
 
-  min() {}
+  min() {
+    if (this.length === 0) throw new Error("EmptySortedList");
+    else return this.items[0];
+  }
 
-  sum() {}
+  sum() {
+    if (this.length === 0) return 0;
+    else {
+      const sum = this.items.reduce(function (acc, val) {
+        acc += val;
+        return acc;
+      }, 0);
+      return sum;
+    }
+  }
 
-  avg() {}
+  avg() {
+    if (this.length === 0) throw new Error("EmptySortedList");
+    return this.sum() / this.length;
+  }
 }
 
 module.exports = SortedList;
