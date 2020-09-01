769c6ef0dc29a4731d3dfdb538150f02046dfa63
Khushboo Goyal <khushboo026@gmail.com>
2020-09-01 13:36:12 +0200

    Khushboo

--- index.js
+++ index.js
@@ -1,17 +1,48 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+     this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    const arr = this.items.sort(function(a,b){return a-b});
+    this.length = this.items.length
+  }
 
-  get(pos) {}
+  get(pos) {
+    if(pos<=(this.length-1)){
+      return this.items[pos];
+    }else throw new Error ('OutOfBounds')
+  }
 
-  max() {}
+  max() {
+    if(this.length>0){
+      return this.items[this.length-1];
+    }throw new Error ('EmptySortedList')
+  }
 
-  min() {}
+  min() {
+    if(this.length>0) return this.items[0]
+    throw new Error ('EmptySortedList')
+  }
 
-  sum() {}
+  sum() {
+    if(this.length>0){
+    const sum = this.items.reduce(function(acc,val){
+      return acc+=val
+    },0)
+    return sum
+    }
+    return 0
+  }
 
-  avg() {}
+  avg() {
+    if(this.length>0){
+      return this.sum()/this.length
+    }
+    throw new Error('EmptySoretdList')
+  }
 }
 
 module.exports = SortedList;
