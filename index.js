a4a8c8bd40f0cadd4c88ad7039277e91d5c7b871
macman <hayrapetyan.hamlet@gmail.com>
2020-09-01 13:47:48 +0200

    finish 74 out of 76 tests

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = []
+    this.length = 0
+    }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item)
+    this.length ++
+    this.items.sort(((a,b) => {
+      return a - b
+    }))
+  }
+    
+  get(pos) {
+    if (pos+1 > this.items.length) {
+      throw new Error('OutOfBounds');
+    }
+    return this.items[pos]
+  }
 
-  get(pos) {}
-
-  max() {}
+  max() {
+    if (this.items.length === 0) {
+      throw new Error('EmptySortedList');
+    }
+    return Math.max(...this.items)
+  }
 
   min() {}
 
