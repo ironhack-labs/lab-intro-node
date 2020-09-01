5aa213c498b799823726cb9c43a1530a5b7bf797
Diana <d.thueroff@gmail.com>
2020-09-01 14:03:34 +0200

    all done

--- index.js
+++ index.js
@@ -1,17 +1,55 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
 
-  add(item) {}
+  }
 
-  get(pos) {}
+  add(item) {
+    this.items.push(item);
+    const arr = this.items.sort(function (a, b) {
+      return a - b;
+    });
+    this.length = this.items.length;
+  }
 
-  max() {}
+  get(pos) {
+    if (pos <= this.length - 1) {
+      return this.items[pos];
+    }
+    throw new Error('OutOfBounds');
+  }
 
-  min() {}
+  max() {
+    if (this.length === 0) {
+      throw new Error('EmptySortedList');
+    }
+    return this.items[this.length - 1];
+  }
 
-  sum() {}
+  min() {
+    if (this.length === 0) {
+      throw new Error('EmptySortedList');
+    }
+    return this.items[0];
+  }
 
-  avg() {}
+  sum() {
+    if (this.length > 0){
+   const sum = this.items.reduce(function (acc, val) {
+      return acc+=val;
+    }, 0);
+    return sum;
+  } else {
+    return 0;
+  }
+  }
+
+  avg() {
+    if (this.length > 0){
+     return this.sum()/this.length;
+    } throw Error('EmptySortedList');
+  }
 }
 
 module.exports = SortedList;
