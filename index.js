2bff194935cf5ef4796321f1b6d0f1a8943ddc13
Kevin Klein <pkevinklein88@gmail.com>
2020-09-01 14:45:57 +0200

    node-lab

--- index.js
+++ index.js
@@ -1,17 +1,58 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a, b) => a - b);
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (this.items[pos]) {
 
-  max() {}
+      return this.items[pos];
+    } else {
+   throw new Error('OutOfBounds');
+    }
+  }
 
-  min() {}
+  max() {
+    if (this.length == 0) {
+      throw new Error('EmptySortedList');
+    } else {
+      let max = this.items.reduce((acc,currVal) => Math.max(acc,currVal))
+    return max;
+    }
+  }
 
-  sum() {}
+  min() {
+    if (this.length == 0) {
+      throw new Error('EmptySortedList');
+    } else {
+      let min = this.items.reduce((acc,currVal) => Math.min(acc,currVal))
+    return min;
+    }
+  }
 
-  avg() {}
+  sum() {
+    if (this.length == 0) {
+      return 0
+    } else {
+      let total = this.items.reduce((acc,currVal) => acc + currVal)
+    return total;
+    }
+  }
+
+  avg() {
+    if (this.length == 0) {
+      throw new Error('EmptySortedList');
+    } else {
+    return this.sum() / this.length
+    }
+  }
 }
 
 module.exports = SortedList;
