34de7860d5ad70450973d8bb59f9c674875688a9
“Lorie <lorie.rocher@gmail.com>
2020-09-01 14:44:17 +0200

    iteration 4 done

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
 class SortedList {
-  constructor() {}
+  constructor(items, length) {
+    this.items = [],
+      this.length = this.items.length
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item)
+    this.items.sort((a, b) => a - b);
+    this.length = this.items.length
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos > this.length || pos < 0) {
+      throw new Error('OutOfBounds')
+    }
+    return this.items[pos]
 
-  max() {}
+  }
+
+
+  max() {
+    if (this.length === 0) {
+      throw new Error('EmptySortedList')
+    } else {
+      return this.items[this.length - 1]
+    }
+  }
 
   min() {}
 
