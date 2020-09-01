9560739a08636c39b7b9f6f09329a4736fa6b1c7
Habid B Espinosa <hab.bad@hotmail.com>
2020-09-01 14:56:23 +0200

    Done

--- index.js
+++ index.js
@@ -1,17 +1,56 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [] 
+    this.length = this.items.length
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item)
+    const arr = this.items.sort((a, b)=>{
+      return a-b
+    })
+    this.length = this.items.length
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos <= this.length -1){
+      return this.items[pos]
+    }else{
+      throw new Error('OutOfBounds')
+    }
+  }
 
-  max() {}
+  max() {
+    if (this.length === 0) {
+      throw new Error('EmptySortedList');
+    }
+    return this.items[this.length - 1];
+  }
 
-  min() {}
+  min() {
+    if (this.length === 0) {
+      throw new Error('EmptySortedList');
+    }
+    return this.items[0];
+  }
 
-  sum() {}
+  sum() {
+    if (this.length > 0){
+   const sum = this.items.reduce(function (acc, val) {
+      return acc+=val;
+    }, 0);
+    return sum;
+  } else {
+    return 0;
+  }
+  }
 
-  avg() {}
+  avg() {
+    if (this.length > 0){
+     return this.sum()/this.length;
+    } throw Error('EmptySortedList');
+  }
 }
+
 
 module.exports = SortedList;
