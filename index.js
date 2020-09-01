29859827ecc340ba0666cb8dda611747442fd634
Olesya Steinmetz <newsabome@icloud.com>
2020-09-01 13:36:55 +0200

    WDFT BERLIN Olesya Done

--- index.js
+++ index.js
@@ -1,17 +1,57 @@
 class SortedList {
-  constructor() {}
+  constructor(items, length) {
+    this.items = [],
+    this.length = this.items.length
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a,b) => {
+      return a-b
+    });
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+      if (this.items[pos]){
+        return this.items[pos]
+      } else {
+        throw new Error('OutOfBounds');
+      }
+  }
 
-  max() {}
+  max() {
+    if(this.items.length > 1){
+      return this.items[this.items.length -1];
+    }else {
+      throw new Error('OutOfBounds');
+    }
+  }
 
-  min() {}
+  min() {
+    if(this.items.length > 1){
+      return this.items[0];
+    }else {
+      throw new Error('OutOfBounds');
+    }
+  }
 
-  sum() {}
+  sum() {
+    if (this.items.length === 0){
+      return 0
+    }
+    return this.items.reduce((acc,val) => {
+      return acc + val;
+    })
+  }
 
-  avg() {}
+  avg() {
+    if (this.items.length === 0){
+      throw new Error('OutOfBounds');
+    }
+    return this.sum() / this.items.length;
+  }
 }
 
+//const list = new SortedList([3, 2, 4, 5, 6])
 module.exports = SortedList;
