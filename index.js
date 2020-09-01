fecc7ec844525d5a9f18655bf297506b11ee7d4d
Marly <marlenak90@gmail.com>
2020-09-01 15:12:11 +0200

    all finished

--- index.js
+++ index.js
@@ -1,17 +1,51 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items=[];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort ((a,b) => a - b);
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    this.items[pos];
+    if (pos<=this.length-1) {
+      return this.items[pos];
+    }
+    throw new Error('OutOfBounds');
+  }
 
-  max() {}
+  max() {
 
-  min() {}
+    if (this.length==0) {
+      throw new Error('EmptySortedList');
+    }
+    return Math.max(...this.items);
+    }
 
-  sum() {}
+  min() {
+    if (this.length==0) {
+      throw new Error('EmptySortedList');
+    }
+    return Math.min(...this.items);
+  }
 
-  avg() {}
+  sum() {
+    if (this.length==0) {
+      return 0;
+    }
+    return (this.items.reduce((a,b) => a+b))
+  }
+
+  avg() {
+    if (this.length==0) {
+      throw new Error('EmptySortedList');
+    }
+    return this.sum()/this.length;
+  }
 }
 
 module.exports = SortedList;
