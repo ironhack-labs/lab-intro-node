569db20311bdd24ea6707d58abb8e658ef420b1a
Rodrigo Muniz <ramunizbh@yahoo.com.br>
2020-08-31 21:22:53 -0300

    finalizado

--- index.js
+++ index.js
@@ -11,23 +11,45 @@ class SortedList {
   }
 
   get(pos) {
+    if (pos+1 >this.items.length){
+      throw new Error('OutOfBounds');
+    }
 
+    return this.items[pos];
   }
 
   max() {
+    if (this.items.length ===0){
+      throw new Error('EmptySortedList');
+    }
 
+    return this.items[this.items.length-1];
   }
 
   min() {
+    if (this.items.length ===0){
+      throw new Error("EmptySortedList");
+    }
+
+    return this.items[0];
 
   }
 
   sum() {
-
+    if (this.items.length===0){
+      return 0;
+    }
+    return this.items.reduce(function(acumulador, valorAtual) {
+      return acumulador + valorAtual;
+    });
   }
 
   avg() {
+    if (this.items.length ===0){
+      throw new Error("EmptySortedList");
+    }
 
+    return this.items.reduce(function(acumulador, valorAtual) {return acumulador + valorAtual}) / this.items.length;
   }
 }
 
