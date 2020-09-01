b362d5763b3193cd9b01c74b5e25f7fd8da542e4
basvandersluis <bas_vd_sluis@outlook.com>
2020-09-01 18:05:09 +0200

    Finished iterations 1 - 5. Still have to do Bonus iterations

--- index.js
+++ index.js
@@ -1,13 +1,41 @@
 class SortedList {
-  constructor() {}
+  constructor(items, length) {
+    this.items = [],
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    let items = [];
+    this.items.push(item);
+    this.items.sort(function(a, b){
+      return a-b;
+    })
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    const arrLength = this.items.length;
+    if (arrLength < pos) {
+      throw new Error('OutOfBounds');
+    }
+    return this.items[pos];
+  }
 
-  max() {}
+  max() {
+    const highestVal = this.items.length;
+    if (highestVal === 0) {
+      throw new Error('EmptySortedList');
+    }
+    return Math.max(...this.items);
+  }
 
-  min() {}
+  min() {
+    const lowestVal = this.items.length;
+    if (lowestVal === 0) {
+      throw new Error('EmptySortedList');
+    }
+    return Math.min(...this.items);
+  }
 
   sum() {}
 
