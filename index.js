540575dd9ba41064facccfb1504be17164704376
shinyo627 <shinyo627@gmail.com>
2020-09-01 13:36:54 +0200

    done

--- index.js
+++ index.js
@@ -1,17 +1,62 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item)
+    this.items.sort(function(a, b){
+      return a - b
+    })
+    this.length++
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (this.length >= pos) {
+      return this.items[pos]
+    }
 
-  max() {}
+    throw new Error('OutOfBounds');
+  }
 
-  min() {}
+  max() {
+    if(this.items.length === 0) {
+      throw new Error('EmptySortedList');
+    }
+    let max = this.items.reduce(function(a, b) {
+      return Math.max(a, b);
+  });
+    return max;
+  }
 
-  sum() {}
+  min() {
+    if(this.items.length === 0) {
+      throw new Error('EmptySortedList');
+    }
+    let min = this.items.reduce(function(a, b) {
+      return Math.min(a, b);
+  });
+    return min;
+  }
 
-  avg() {}
+  sum() {
+    if(this.items.length === 0) {
+      return 0
+    }
+    
+    let sum = this.items.reduce((a,b) => {
+      return a + b
+    })
+    return sum
+  }
+
+  avg() {
+    if(this.items.length === 0) {
+      throw new Error('EmptySortedList');
+    }
+    return this.sum() / this.length;
+  }
 }
 
 module.exports = SortedList;
