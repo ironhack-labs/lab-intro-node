8c7cd1408a378308a50923d522b475041adb9973
Rubem Diogo <rubemdiogo@gmail.com>
2020-09-09 10:35:59 -0400

    done

--- index.js
+++ index.js
@@ -1,17 +1,54 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
+  add(item) {
+    this.items.push(items);
+    this.items.sort((a, b) => a - b);
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  get(pos) {
+    if (pos <= this.length - 1) {
+      return this.items[pos];
+    } else {
+      throw new Error("OutOfBounds");
+    }
+  }
 
-  get(pos) {}
+  max() {
+    if (this.length === 0) {
+      throw new Error("EmptySortedList");
+    }
+    return this.items[this.length - 1];
+  }
 
-  max() {}
+  min() {
+    if (this.length === 0) {
+      throw new Error("EmptySortedList");
+    }
+    return this.items[0];
+  }
 
-  min() {}
+  sum() {
+    if (this.length > 0) {
+      const sum = this.items.reduce(function (acc, val) {
+        return (acc += val);
+      }, 0);
+      return sum;
+    } else {
+      return 0;
+    }
+  }
 
-  sum() {}
-
-  avg() {}
+  avg() {
+    if (this.length > 0) {
+      return this.sum() / this.length;
+    } else {
+      throw new Error("EmptySortedList");
+    }
+  }
 }
 
 module.exports = SortedList;
