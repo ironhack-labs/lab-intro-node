2138b364e612d8637b3268b676a18eeb11d43ad2
Camila Fassarella <fassarella.camila@gmail.com>
2020-09-09 11:59:27 -0300

    done

--- index.js
+++ index.js
@@ -1,17 +1,50 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    //roda só uma vez
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a, b) => a - b);
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos !== this.items[pos]) {
+      throw new Error("OutOfBounds");
+    }
+    return this.items[pos];
+  }
 
-  max() {}
+  max() {
+    if (this.items.length === 0) {
+      throw new Error("EmptySortedList");
+    }
+    return Math.max(...this.items);
+  }
 
-  min() {}
+  min() {
+    if (this.items.length === 0) {
+      throw new Error("EmptySortedList");
+    }
+    return Math.min(...this.items);
+  }
 
-  sum() {}
+  sum() {
+    let array = this.items;
+    let sum = array.reduce((a, b) => a + b, 0); //a = accumulator acc, b = current value cv
+    return sum;
+  }
 
-  avg() {}
+  avg() {
+    if (this.items.length === 0) {
+      throw new Error("EmptySortedList");
+    }
+    let array = this.items;
+    let avg = array.reduce((a, b) => a + b, 0) / array.length;
+    return avg;
+  }
 }
-
 module.exports = SortedList;
