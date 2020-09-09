a77777036aec89d9300816f9bd9983787109533b
Victor Monteiro <victor_monteiro@live.com>
2020-09-09 13:23:46 -0300

    lab solved

--- index.js
+++ index.js
@@ -1,17 +1,49 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a, b) => a - b);
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos < this.length) {
+      return this.items[pos];
+    }
+    throw new Error('OutOfBounds');
+  }
 
-  max() {}
+  max() {
+    if (this.length > 0) {
+      return Math.max(...this.items);
+    }
+    throw new Error('EmptySortedList');
+  }
 
-  min() {}
+  min() {
+    if (this.length > 0) {
+      return Math.min(...this.items);
+    }
+    throw new Error('EmptySortedList');
+  }
 
-  sum() {}
+  sum() {
+    if (this.length > 0) {
+      return [...this.items].reduce((acc, cv) => acc + cv, 0);
+    }
+    return 0;
+  }
 
-  avg() {}
+  avg() {
+    if (this.length > 0) {
+      return this.sum() / this.length;
+    }
+    throw new Error('EmptySortedList');
+  }
 }
 
 module.exports = SortedList;
