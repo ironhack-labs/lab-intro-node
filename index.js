939c88e6d0133f4ef2447fe1a7c022b623e0d424
carolinacervelaf <carolinacervelaf@gmail.com>
2020-09-12 17:14:45 -0300

    done

--- index.js
+++ index.js
@@ -1,17 +1,50 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a, b) => a - b);
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos < 0 || pos > this.length) {
+      throw new Error('OutOfBounds');
+    }
+    return this.items[pos];
+  }
 
-  max() {}
+  max() {
+    if (!this.items.length) {
+      throw new Error('EmptySortedList');
+    }
 
-  min() {}
+    return this.items[this.length - 1]
+  }
 
-  sum() {}
+  min() {
+    if (!this.items.length) {
+      throw new Error('EmptySortedList');
+    }
+    return this.items[0]
+  }
 
-  avg() {}
+  sum() {
+    return this.items.reduce((acc, cv) => acc + cv, 0)
+  }
+
+  avg() {
+    if (!this.items.length) {
+      throw new Error('EmptySortedList')
+    }
+    return this.sum / this.length;
+  }
 }
 
 module.exports = SortedList;
+
+
+
