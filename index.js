202202efb6ebf700f847a3ebcf8580107ffa4cb4
Sant <santiagoandres@outlook.de>
2020-10-06 19:37:22 +0200

    All iterations.

--- index.js
+++ index.js
@@ -1,17 +1,72 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = 0;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
 
-  get(pos) {}
+    this.items.sort((a, b) => {
+      if (a > b) {
+        return 1;
+      } else if (a < b) {
+        return -1;
+      } else {
+        return 0;
+      }
+    });
 
-  max() {}
+    this.length++;
+  }
 
-  min() {}
+  get(pos) {
+    if (pos < this.length) {
+      return this.items[pos];
+    } else {
+      throw new Error("OutOfBounds");
+    }
+  }
 
-  sum() {}
+  max() {
+    if (!this.length) {
+      throw new Error("EmptySortedList");
+    } else {
+      return this.items[this.length - 1];
+    }
+  }
 
-  avg() {}
+  min() {
+    if (!this.length) {
+      throw new Error("EmptySortedList");
+    } else {
+      return this.items[0];
+    }
+  }
+
+  sum() {
+    /* First Solution */
+    // let total = 0;
+    // for (let n of this.items) {
+    //   total += n;
+    // }
+    // return total;
+
+    /* Second Solution */
+    if (!this.length) {
+      return 0;
+    } else {
+      return this.items.reduce((acc, curr) => acc + curr);
+    }
+  }
+
+  avg() {
+    if (!this.length) {
+      throw new Error("EmptySortedList");
+    } else {
+      return this.sum() / this.length;
+    }
+  }
 }
 
 module.exports = SortedList;
