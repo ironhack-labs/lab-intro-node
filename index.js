9746a78bada78ee0f8d04e1c873acc74da0753ca
Aleksandra Tanasijenko <alekstana@gmail.com>
2020-10-06 17:30:26 +0200

    almost there

--- index.js
+++ index.js
@@ -1,17 +1,65 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [],
+    this.length = this.items.length
+  }
 
-  add(item) {}
+  add(item) {
+   this.items.push(item)
+   this.length++
+   this.items.sort((a,b) => {
+     if (a>b){
+       return 1;
+     } 
+     else if (a<b){
+       return -1;
+     }
+     else{
+       return 0;
+     }
+   })
+ 
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos< this.items.length && pos >= 0){
+      return this.items[pos]
+    }else {
+      throw new Error('OutOfBounds');
+    }
+  }
 
-  max() {}
+  max() {
+    if (this.items.length !== 0){
+      return this.items[this.items.length - 1]
+    } else {
+      throw new Error('EmptySortedList');
+    }
+  }
 
-  min() {}
+  min() {
+    if (this.items.length !== 0){
+      return this.items[0]
+    } else {
+      throw new Error('EmptySortedList');
+    }
+  }
 
-  sum() {}
+  sum() {
+    let sumArr = this.items.reduce((a,b) => {
+      return a+b
+    },0)
+    return sumArr
+  }
 
-  avg() {}
+  avg() {
+    if (this.items.length > 0){
+      return ((sumArr/this.items.length))
+    }
+    else{
+      throw new Error('EmptySortedList');
+    }
+  }
 }
 
 module.exports = SortedList;
