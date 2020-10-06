d082ac7b7245a771e4f6c5667d73affa491a6606
Vykintas Petrikas <vykintas.petrikas@gmail.com>
2020-10-06 17:47:05 +0300

    Completed all iterations and bonuses

--- index.js
+++ index.js
@@ -1,17 +1,91 @@
 class SortedList {
-  constructor() {}
+  constructor(items) {
+    this.items = [],
+    this.length = this.items.length
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item)
+    this.items.sort((a, b)=>{
+      if (a > b){
+        return 1
+      } else if ( a < b){
+        return -1
+      } else {
+        return 0
+      }
+    })
+    this.length = this.items.length
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (this.length < pos){
+      console.log('error')
+      throw new Error('OutOfBounds');
+    }
+    console.log(this.items[pos])
+    return this.items[pos]
+  }
 
-  max() {}
+  max() {
+    if (!this.items.length){
+      throw new Error('EmptySortedList')
+    }
+    this.items.sort((a, b)=>{
+      if(a>b){
+        return -1
+      } else if(a<b) {
+        return 1
+      } else {
+        return 0
+      }
+    })
+    return this.items[0]
+  }
 
-  min() {}
+  min() {
+    if (!this.items.length){
+      throw new Error('EmptySortedList')
+    }
+    this.items.sort((a, b)=>{
+      if(a>b){
+        return 1
+      } else if(a<b) {
+        return -1
+      } else {
+        return 0
+      }
+    })
+    return this.items[0]
+  }
 
-  sum() {}
+  sum() {
+    let sum = 0
+    this.items.map((elem)=>{
+      return sum+=elem
+    })
+    return sum
+  }
 
-  avg() {}
+  avg() {
+    if (!this.items.length) {
+      throw new Error('EmptySortedList')
+    }
+    let avg = 0
+    this.items.map((elem)=>{
+      return avg+=elem
+    })
+    return avg/this.items.length
+  }
 }
 
 module.exports = SortedList;
+
+// Some tests
+// let list = new SortedList
+
+// list.add(1)
+// list.add(5)
+// list.add(2)
+// console.log(list.items)
+// list.get(4)
