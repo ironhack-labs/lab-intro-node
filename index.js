04f4f699ae26c8d768bafc556a6ba91d8e5a65f3
Ute Wiemer <upwiemer@gmail.com>
2020-10-17 16:08:31 +0200

    done

--- index.js
+++ index.js
@@ -1,17 +1,45 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = []
+    this.length = 0
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort(function(a, b){return a-b});
+    this.length++
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos < this.length) {return this.items[pos]}
+    else {throw new Error('OutOfBounds')}
+  }
 
-  max() {}
+  max() {
+    if(this.length > 0) {return Math.max(...this.items)}
+    else {throw new Error('EmptySortedList');}
+  }
 
-  min() {}
+  min() {
+    if(this.length > 0) {return Math.min(...this.items)}
+    else {throw new Error('EmptySortedList');}
+  }
 
-  sum() {}
+  sum() {
+    return this.items.reduce(function(a, b){
+      return a + b;
+  }, 0);
+  }
 
-  avg() {}
+  avg() {
+    let sumOfItems = this.sum()
+    if(this.length > 0) {return(sumOfItems/this.length)}
+    else {throw new Error('EmptySortedList');}
+  }
+
+
 }
 
 module.exports = SortedList;
+
+
