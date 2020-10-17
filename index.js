9640db25bc46aa6631f35bc73032d390bc626b32
phil-legal-tech <philtu1287@gmail.com>
2020-10-17 16:08:41 +0200

    done

--- index.js
+++ index.js
@@ -1,17 +1,61 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.length = 0;
+    this.items = [];
+  }
+  add(item) {
+    this.items.push(item);
+    this.items.sort(
+      function (a, b) {
+        return a - b
+      }
+    );
+    this.length = this.items.length
+  }
 
-  add(item) {}
+  get(pos) {
+    if (this.items.length >= pos) {
+      return this.items[pos]
+    } else {
+      throw new Error('OutOfBounds');
+    }
+  };
 
-  get(pos) {}
+  max() {
+    if (this.items.length > 0) {
+      let highest = this.items[this.items.length - 1];
+      return highest;
+    } else if (this.items.length === 0) {
+      throw new Error('EmptySortedList');
+    }
+  }
 
-  max() {}
+  min() {
+    if (this.items.length > 0) {
+      return this.items[0];
+    } else if (this.items.length === 0) {
+      throw new Error('EmptySortedList');
+    }
+  }
+  
+  sum() {
+    let sum = 0;
+    if (this.items.length > 0) {
+      for (let i = 0; i < this.items.length; i++) {
+        sum += this.items[i];
+      } return sum;
+    } else if (this.items.length === 0) {
+      return 0;
+    }
+  }
 
-  min() {}
-
-  sum() {}
-
-  avg() {}
+  avg() { 
+    if (this.items.length > 0) {
+      return this.sum() / this.items.length;
+    } else if (this.items.length === 0) {
+      throw new Error('EmptySortedList');
+    }
+  }
 }
 
 module.exports = SortedList;
