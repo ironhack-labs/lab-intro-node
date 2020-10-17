eaa332dcdecaf79222c5f63c975f3de30aa6b270
Marie Kleinschmidt <maklein9@googlemail.com>
2020-10-17 16:15:08 +0200

    done

--- index.js
+++ index.js
@@ -1,17 +1,63 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.length = 0
+    this.items = []
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item)
+    this.items.sort((a, b) => (a - b))
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos <= this.items.length - 1) {
+      return this.items[pos];
+    } else {
+      throw new Error('OutOfBounds');
+    }
+  }
 
-  max() {}
+  max() {
+    if (this.items.length > 0) {
+      return this.items[this.items.length - 1];
+    } else {
+      throw new Error('EmptySortedList');
+    }
+  }
 
-  min() {}
 
-  sum() {}
+  min() {
+    if (this.items.length > 0) {
+      return this.items[0];
+    } else {
+      throw new Error('EmptySortedList');
+    }
+  }
 
-  avg() {}
+  sum() {
+    if (this.items.length > 0) {
+      let sum = 0
+      for (let i = 0; i < this.items.length; i++) {
+        sum += this.items[i]
+      }
+      return sum;
+
+    }
+    else {
+      return 0;
+    }
+  }
+
+  avg() {
+    if (this.items.length > 0) {
+      return this.sum() / this.items.length;
+    }
+    else {
+      throw new Error('EmptySortedList');
+    }
+  }
+
 }
 
 module.exports = SortedList;
