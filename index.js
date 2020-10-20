a7380e1eb519ff46515f6f8c14f3ffbc4dd9ca55
Jonathan Jimenez <jonathanajp@gmail.com>
2020-10-20 15:11:27 +0200

    done

--- index.js
+++ index.js
@@ -1,17 +1,58 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+   this.items.push(item);
+   this.items.sort(function(a, b) {
+    return a - b;
+  });
+  this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if(pos < this.length){
+      return this.items[pos]
+    }else{
+      throw new Error('OutOfBounds');
+    }
+  }
 
-  max() {}
+  max() {
+    if(this.items == 0){
+      throw new Error('EmptySortedList');
+    }else{
+      return Math.max(...this.items) 
+    }
+  }
 
-  min() {}
+  min() {
+    if(this.items == 0){
+      throw new Error('EmptySortedList');
+    }else{
+      return Math.min(...this.items) 
+    }
+  }
 
-  sum() {}
+  sum() {
+    if(this.items == 0){
+      return 0;
+    }else{
+      return this.items.reduce(function(accumulator, currentValue){
+        return accumulator + currentValue;
+    },0)
+    }
 
-  avg() {}
+  }
+
+  avg() {
+    if(this.items == 0){
+      throw new Error('EmptySortedList');
+    }else{
+      return this.sum() / this.length
+    }
 }
-
+}
 module.exports = SortedList;
