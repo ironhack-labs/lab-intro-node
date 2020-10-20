93a8325b9cdcf8f8453de8081ece40dc85c65df8
Claudia D <claudia.do.gallego@gmail.com>
2020-10-20 15:22:46 +0200

    done

--- index.js
+++ index.js
@@ -1,17 +1,53 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items = this.items.concat(item).sort((a, b) => {return a - b;});
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    
+    if (this.items[pos]) {
+      return this.items[pos];
+    } else {
+      throw new Error('OutOfBounds');;
+    }
+  }
 
-  max() {}
+  max() {
+    if (this.items.length>0){
+      return Math.max(...this.items)
+    }else{
+      throw new Error('EmptySortedList');
+    }
+  }
 
-  min() {}
+  min() {
+    if (this.items.length>0){
+      return Math.min(...this.items)
+    }else{
+      throw new Error('EmptySortedList');
+    }
+  }
 
-  sum() {}
+  sum() {
+     return this.items.reduce((a, b)=> a + b, 0)
+    
+  }
 
-  avg() {}
+  avg() {
+    if(this.items.length>0){
+      return (this.items.reduce((a, b)=> a + b, 0))/this.items.length;
+    }else{
+      throw new Exception('EmptySortedList'); 
+    }
+  }
 }
+
+
 
 module.exports = SortedList;
