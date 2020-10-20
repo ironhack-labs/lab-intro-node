7e05163d2f74f662ab1224100dc4d43b1bbcc1ba
Esteve Martin <esteve.martin@mpasolutions.es>
2020-10-20 15:20:27 +0200

    Finished

--- index.js
+++ index.js
@@ -1,17 +1,60 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items=[];
+    this.length=0;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a,b)=>{return a-b;});
+    this.length=this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos>this.items.length-1){
+      throw new Error('OutOfBounds');
+    } else {
+      return this.items[pos];
+    }
+    
+  }
 
-  max() {}
+  max() {
+    if (this.items.length===0){
+      throw new Error('EmptySortedList');
+    } else {
+      return Math.max(...this.items);
+    }
+  }
 
-  min() {}
+  min() {
+    if (this.items.length===0){
+      throw new Error('EmptySortedList');
+    } else {
+      return Math.min(...this.items);
+    }
+  }
 
-  sum() {}
+  sum() {
+    if (this.items.length===0){
+      return 0;
+    } else{
+      return this.items.reduce((acc,red)=>red+acc)
 
-  avg() {}
+    }
+  }
+
+  avg() {
+    if (this.items.length===0){
+      throw new Error('EmptySortedList');
+    } else {
+      // console.log(this.items.sum())
+      // console.log(this.items.length)
+      // console.log(this.items.sum()/this.items.length)
+
+      return this.sum()/this.length;
+    }
+  }
 }
 
 module.exports = SortedList;
