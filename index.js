d43d2c866e02ded01691ee9efa811157aaa31f77
Alex Olle <alex.olle.93@gmail.com>
2020-10-20 12:43:09 +0200

    Done

--- index.js
+++ index.js
@@ -1,17 +1,64 @@
 class SortedList {
-  constructor() {}
+  constructor(items) {
+    this.items = [],
+    this.length = this.items.length
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort( function (a, b) {
+      return a - b;
+    })
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if ( pos < this.length ) {
+      return this.items[pos];
+    } else {
+      throw new Error('OutOfBounds');
+    }
+  }
 
-  max() {}
+  max() {
+    if ( this.length === 0 ) {
+      throw new Error('EmptySortedList');
+    } else {
+      return this.items[this.length - 1];
+    }
+  }
 
-  min() {}
+  min() {
+    if ( this.length === 0 ) {
+      throw new Error('EmptySortedList');
+    } else {
+      return this.items[0];
+    }
+  }
 
-  sum() {}
+  sum() {
+    if ( this.length === 0 ) {
+      return 0;
+    } else {
+      let sum = 0;
+      this.items.forEach( function (elem) {
+        sum += elem;
+      })
+      return sum;
+    }
+  }
 
-  avg() {}
+  avg() {
+    if ( this.length === 0 ) {
+      throw new Error('EmptySortedList')
+    } else {
+      let sum = 0;
+      this.items.forEach( function (elem) {
+        sum += elem;
+      })
+      return sum / this.length;
+    }
+  }
 }
 
 module.exports = SortedList;
