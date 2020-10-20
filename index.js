a5ceb39fad5b6d05ae8920a4ff3cafe6b23fe4ce
Sara Amantegui <saiduna@gmail.com>
2020-10-20 18:26:15 +0200

    done

--- index.js
+++ index.js
@@ -1,17 +1,50 @@
 class SortedList {
-  constructor() {}
+  constructor(items) {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
   add(item) {}
+    this.items = this.items.concat8item).sort(a(a,b) => {return a-b})
+      this.length = this.items.length
+    }
 
-  get(pos) {}
+  get(pos) {
+    if(this.items[pos] === undefined ){
+      throw new Error('OutOfBounds')
+    } else {
+      return this.items[pos]}
 
-  max() {}
+     }
 
-  min() {}
+  max() {
+    if(this.items.length > 0){
+      return Math.max(...this.items)
+    } else {
+      throw new Error('EmptySortedList';)
+    }
+  }
 
-  sum() {}
+  min() {
+    if(this.items.length > 0) {
+      return Math.min(...this.items)
+    } else {
+      throw new Error('EmptySortedList');
+    }
+  }
 
-  avg() {}
+  sum() {
+    return this.items.reduce((a,b) => a + b, 0)
+
+  }
+
+  avg() {
+    if(this.items.length > 0){
+      return this.items.reduce((a,b) => a + b, 0)/this.items.length
+    } else {
+      throw new Exception('EmptySortedList')
+    }
+  }
 }
 
 module.exports = SortedList;
