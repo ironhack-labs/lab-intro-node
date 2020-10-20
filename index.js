38bbc709a42065396b89fbe4ff06b395accfb5d0
Marta Camacho <martacamachovargas@gmail.com>
2020-10-20 14:46:10 +0200

    [FT-BCN-0920] Marta Camacho

--- index.js
+++ index.js
@@ -1,17 +1,49 @@
 class SortedList {
-  constructor() {}
+  constructor(items, length) {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item)
+    this.length++
+    this.items.sort (function (a,b){return a-b})
+  }
 
-  get(pos) {}
+  get(pos) {
+    if(pos > this.length){
+      throw new Error('OutOfBounds');
+    } else {
+    return this.items[pos]
+  }
+  }
 
-  max() {}
+  max(items) {
+    if(this.length > 0){
+    return Math.max(...this.items)
+  } else {
+    throw new Error('EmptySortedList');
+  }
+  }
 
-  min() {}
+  min(items) {
+    if(this.length > 0){
+    return Math.min(...this.items)
+  } else {
+    throw new Error('EmptySortedList');
+  }}
 
-  sum() {}
+  sum(items) {
+    return this.items.reduce((a, b) => a + b, 0)
+  }
 
-  avg() {}
+  avg(length) {
+    if(this.length === 0){
+      throw new Error('EmptySortedList');
+    } else {
+      return (this.items.reduce((a, b) => a + b, 0))/this.length;
+    }
+  }
 }
 
 module.exports = SortedList;
