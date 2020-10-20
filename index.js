6fdc5ac48ce04acdc3deef1e1e47e924e7708794
Ona Solani <onasolani@gmail.com>
2020-10-20 12:54:37 +0200

    done

--- index.js
+++ index.js
@@ -1,17 +1,58 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = 0;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a,b) => (a-b))
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos <= this.items.length - 1) {
+      return this.items[pos];
+    } else {
+      throw new Error('OutOfBounds');
+    }
+  }
 
-  max() {}
+  max() {
+    if(this.items.length > 0) {
+      return this.items[this.items.length - 1];
+    } else {
+      throw new Error('EmptySortedList')
+    }
+  }
 
-  min() {}
+  min() {
+    if(this.items.length > 0){
+      return this.items[0];
+    } else {
+      throw new Error('EmptySortedList')
+    }
+  }
 
-  sum() {}
+  sum() {
+    if (this.items.length > 0) {
+      let sum = 0
+      for (let i = 0; i < this.items.length; i++) {
+        sum += this.items[i]
+      }
+      return sum;
+    } else {
+      return 0;
+    }
+  }
 
-  avg() {}
+  avg() {
+    if (this.items.length > 0){
+      return this.sum()/this.items.length;
+    } else {
+      throw new Error ('EmptySortedList')
+    }
+  }
 }
 
 module.exports = SortedList;
