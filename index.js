62b6667326fea4f57b25c4b2e7a080cc29c81f74
GitSkynet <carlos.curtido@gmail.com>
2020-10-20 14:59:50 +0200

    Done

--- index.js
+++ index.js
@@ -1,17 +1,64 @@
 class SortedList {
-  constructor() {}
+  constructor(){
+    this.items = [];
+    this.length = 0;
+  }
 
-  add(item) {}
+  add(item){
+    this.items.push(item);
+    this.items.sort(function (a, b){
+      return a - b;
+  });
+    this.length++;
+  }
 
-  get(pos) {}
+  get(pos){
+    if(this.length< pos){
+      throw new Error('OutOfBounds');
+    }else{
+      return this.items[pos];
+    }
+  }
 
-  max() {}
+  max(){
+    if(this.length === 0){
+      throw new Error('EmptySortedList');
+    }else{
+      return Math.max.apply(null, this.items);
+    }
+  }
 
-  min() {}
+  min(){
+    if(this.length === 0){
+      throw new Error('EmptySortedList');
+    }else{
+      return Math.min.apply(null, this.items);
+    }
+  }
 
-  sum() {}
+  sum(){
+    let total=0;
+    let numeros = this.items;
+    for(let i of numeros){
+      total+=i;
+    }
+    if(this.length === 0){
+      return 0;
+    }
+    return total;
+  }
 
-  avg() {}
+  avg(){
+    if(this.length === 0){
+      throw new Error('EmptySortedList');
+    }
+    var sum = this.items.reduce(function(acumulator, nextValue){
+      acumulator += nextValue;
+    }, 0);
+    return sum / this.length;
+    
+  }
 }
+
 
 module.exports = SortedList;
