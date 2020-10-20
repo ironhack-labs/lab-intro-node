18504678950cae0f61b963561862ee1cbbaf6d64
Mathilde Congiu <mathilde.congiu@hotmail.com>
2020-10-20 20:04:15 +0200

    Done

--- index.js
+++ index.js
@@ -1,17 +1,74 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [],
+    this.length = this.items.length
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort(function(a, b){return a-b});
+    this.length = this.items.length;
 
-  get(pos) {}
+  }
 
-  max() {}
+  get(pos) {
 
-  min() {}
+    if (pos > this.length) {
+      throw new Error('OutOfBounds');
+    }
+    else {
+      return this.items[pos];
+    }
 
-  sum() {}
+  }
 
-  avg() {}
+  max() {
+    if (this.length > 0) {
+      return Math.max(...this.items);
+      
+    }
+    else {
+      throw new Error('EmptySortedList');
+    }
+  }
+
+  min() {
+    if (this.length == 0) {
+      throw new Error('EmptySortedList');
+    }
+    else {
+      return Math.min(...this.items);
+    }
+  }
+
+  sum() {if (this.length == 0) {
+    return 0;
+  }
+  else {
+    return this.items.reduce((a,b) => a+b);
+  }}
+
+  avg() {
+    {if (this.length == 0) {
+      throw new Error('EmptySortedList');
+    }
+    else {
+      let sum = this.sum();
+      return sum/this.length
+    }}
+  
+  }
 }
 
 module.exports = SortedList;
+
+
+// let list = new SortedList
+
+// list.add(9);
+
+// list.add(35);
+// list.add(45);
+// list.add(85);
+
+// list.min()
