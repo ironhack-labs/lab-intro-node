6df1bb367f1fb38b86ad83c9dd09fb38b3a0b171
Oscar M <empanaisimo@gmail.com>
2020-10-20 12:00:20 +0100

    done with bonuses

--- index.js
+++ index.js
@@ -1,17 +1,58 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = 0;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort(function(a, b) {
+      return a - b;
+    });
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if(pos > this.items.length -1 ) {
+      throw new Error('OutOfBounds');
+    }
+    return this.items[pos];
+  }
 
-  max() {}
+  max() {
+    if(!this.items[0]){
+      throw new Error('EmptySortedList');
+    }
+    return this.items[this.items.length-1];
+  }
 
-  min() {}
+  min() {
+    if(!this.items[0]){
+      throw new Error('EmptySortedList');
+    }
+    return this.items[0];
+  }
 
-  sum() {}
+  sum() {
+    let sum = 0;
+  this.items.forEach(num => {
+      sum += num;
+    });
+    return sum;
+  }
 
-  avg() {}
+  avg() {
+
+    if(!this.items[0]){
+      throw new Exception('EmptySortedList');
+    }
+
+    let sum = 0;
+    this.items.forEach(num => {
+      sum += num;
+    });
+    return sum/this.items.length;
+  }
 }
 
 module.exports = SortedList;
