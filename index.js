1c5d1a8e2eab199c4ad966331aabcc687a38edc6
jose luis <joseluisblascoortiz@hotmail.com>
2020-10-20 13:45:01 +0200

    DONE

--- index.js
+++ index.js
@@ -1,17 +1,65 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items=[]
+    this.length=this.items.length
+  }
 
-  add(item) {}
+  add(item) {
+   this.items.push(item)
+   this.length++
+   this.items.sort((a,b)=>{
+     return a-b;
+   }
 
-  get(pos) {}
+   )
+  }
 
-  max() {}
+  get(pos) {
+    if(pos>this.length){
+      throw new Error('OutOfBounds');
+    }else{
+      return this.items[pos]
+    }
 
-  min() {}
+  }
 
-  sum() {}
+  max() {
+    if(this.items==0){
+       throw new Error('EmptySortedList')
+    }else{
+     return( Math.max(...this.items));
+    }
+  }
 
-  avg() {}
+  min() {
+    if(this.items==0){
+      throw new Error('EmptySortedList')
+    }else{
+      return( Math.min(...this.items));
+    }
+  }
+  
+
+  sum() {
+    if(this.items==0){
+      return 0
+    }else{
+      let sum=0
+      for(let i=0;i<this.items.length;i++){
+        sum+=this.items[i]
+      }
+      return sum
+    }
+    
+  }
+
+  avg() {
+    if(this.items==null){
+      throw new Error('EmptySortedList')
+    }else{
+    }
+    
+  }
 }
 
 module.exports = SortedList;
