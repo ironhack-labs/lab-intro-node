75112c0576d6210a543f780e53f0ff922e3c699c
marcurto <mcurtogilabert@gmail.com>
2020-10-20 13:21:43 +0200

    all done

--- index.js
+++ index.js
@@ -1,17 +1,60 @@
 class SortedList {
-  constructor() {}
+  constructor(items, length) {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a, b) =>{
+     return a-b;
+    });
+    this.length++
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (this.length > pos){
+      return this.items[pos]
+    } else {
+      throw new Error('OutOfBounds')
+    }
+  }
 
-  max() {}
+  max() {
+    if(this.length > 1){
+      return Math.max(...this.items)
+    } else {
+      throw new Error('EmptySortedList')
+    }
+  }
 
-  min() {}
+  min() {
+    if(this.length > 1){
+      return Math.min(...this.items)
+    } else {
+      throw new Error('EmptySortedList')
+    }
+  }
 
-  sum() {}
+  sum() {
+    if(this.length > 1){
+      const suma = this.items.reduce(function (acc, curr) {
+        return acc + curr;
+      }, 0)
+      return suma
+    } else {
+      return 0;
+    }
 
-  avg() {}
+  }
+
+  avg() {
+    if (this.sum() === 0){
+      throw new Error('EmptySortedList')
+    } else {
+      return this.sum() / this.length;
+    }
+  }
 }
 
 module.exports = SortedList;
