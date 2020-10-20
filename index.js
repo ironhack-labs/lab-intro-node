4bcd71981a972a3bf83c9f515bbd6cc83a9afe29
Raiza Escobar <raizaescobar@gmail.com>
2020-10-20 14:20:50 +0200

    [FT-BCN-0920] Raiza Escobar

--- index.js
+++ index.js
@@ -1,17 +1,56 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items=[];
+    this.length=0;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort(function(a,b){
+      return a-b;
+    });
+    this.length++
+  }
 
-  get(pos) {}
+  get(pos) {
+    if(pos>=this.items.length){
+      throw new Error('OutOfBounds');
+    }
+    return this.items[pos];
+  }
 
-  max() {}
+  max() {
+    if(this.length===0){
+      throw new Error('EmptySortedList');
+    }
+    return this.items[this.length-1]
+  }
 
-  min() {}
+  min() {
+    if(this.length===0){
+      throw new Error('EmptySortedList');
+    }
+    return this.items[0]
+  }
 
-  sum() {}
+  sum() {
+    if(this.length===0){
+      return 0;
+    }
+    let sum= this.items.reduce(function (acc,cv){
+      return acc+cv
+    });
+    return sum;
+  }
 
-  avg() {}
+  avg() {
+    if(this.length===0){
+      throw new Error('EmptySortedList');
+    }
+    let avg=this.sum()/this.length
+    return avg;
+  }
+  
 }
 
 module.exports = SortedList;
