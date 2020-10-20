9f4e05c26e509662a9738a3a706c7eb7b4e877b8
Cristina Castro <cristinacastro87@gmail.com>
2020-10-20 14:57:07 +0200

    done

--- index.js
+++ index.js
@@ -1,17 +1,62 @@
 class SortedList {
-  constructor() {}
+  constructor(items, length) {
+  this.items = []
+  this.length =this.items.length
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item)
+    this.items.sort(function(a,b){return a-b})
+    this.length =this.items.length
+  }
 
-  get(pos) {}
+  get(pos) {
+    
+      if (pos >this.length){
+        throw new Error('OutOfBounds');
+      } else {
+       return this.items[pos]
+      }
+  }
+  
 
-  max() {}
+  max() {
 
-  min() {}
+    if (this.length > 1){
+      return Math.max(...this.items)
+    } else {
+      throw new Error('EmptySortedList');
+    }
+  }
 
-  sum() {}
+  min() {
+ 
+    if (this.length > 1){
+      return Math.min(...this.items)
+    } else {
+      throw new Error('EmptySortedList');
+    }
+  }
 
-  avg() {}
+  
+  sum() {
+    
+    const total = this.items.reduce(function(acc,curr){
+      return acc + curr;
+    }, 0)
+    return total
+    }
+    
+
+  avg() {
+    if (this.length > 1){
+      return this.sum()/this.length
+    } else {
+      throw new Error('EmptySortedList');
+    }
+  }
+    
+  
 }
 
 module.exports = SortedList;
