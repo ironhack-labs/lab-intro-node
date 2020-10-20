9e0f5a4ca2b767f526f1c03d97c78040a00e530f
Andreu Martínez <martinez.andreu@gmail.com>
2020-10-20 13:16:00 +0200

    [WDFT-BCN-0920] Andreu Mtez

--- index.js
+++ index.js
@@ -1,17 +1,60 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = 0;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    let toSort = this.items;
+    this.items = toSort.sort(function(a,b) { return a - b});
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos > this.items.length - 1) {
+      throw new Error('OutOfBounds');
+    }
+    return (this.items[pos]);
+  }
 
-  max() {}
+  max() {
 
-  min() {}
+    if (this.items.length == 0) {
+      throw new Error('EmptySortedList');
+    }
+    return (Math.max(...this.items));
+  }
 
-  sum() {}
+  min() {
+    if (this.items.length == 0) {
+      throw new Error('EmptySortedList');
+    }
+    return (Math.min(...this.items));
+  }
 
-  avg() {}
+  sum() {
+    if (this.items.length == 0) {
+      return 0;
+    }
+    return (this.items.reduce((a, b) => a + b, 0));
+  }
+
+  avg() {
+    if (this.items.length == 0) {
+      throw new Error('EmptySortedList');
+    }
+    let sum = 0;
+    for (let i = 0; i < this.items.length; i++) {
+      sum += this.items[i];
+    }
+    return sum/this.items.length;
+  }
 }
+
+// let items = [];
+// let length = items.length;
+
+// let sortedList = new SortedList(items, length);
 
 module.exports = SortedList;
