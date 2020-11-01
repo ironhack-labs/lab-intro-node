add4dcac13bfeecfcee2800be5c5690f33cb0d88
Ivan Rubio <ivanrubioangeles@gmail.comn>
2020-11-01 16:47:20 -0600

    throw new Error('EmptySortedList') breaking test

--- index.js
+++ index.js
@@ -1,25 +1,27 @@
 class SortedList {
   constructor() {
     this.items = [],
-    this. length = this.items.length;
+      this.length = this.items.length;
 
   }
 
   add(item) {
     this.items.push(item)
     this.length = this.items.length
-    this.items.sort((a, b)=>{return a-b});
+    this.items.sort((a, b) => {
+      return a - b
+    });
   }
-// will get the value at index pos in the list.: if an instance of SortedList has elements: [2, 5, 7], when get(2) called, return should be 7 since this is element in that position in the array. Check the tests to see more examples.
+  // will get the value at index pos in the list.: if an instance of SortedList has elements: [2, 5, 7], when get(2) called, return should be 7 since this is element in that position in the array. Check the tests to see more examples.
   get(pos) {
     let res = ""
-    if ( pos > this.items.length) {
+    if (pos > this.items.length) {
       res = 'OutOfBounds'
-      throw new Error('EmptySortedList');
+      throw new Error('OutOfBounds');
 
-    }else {
+    } else {
 
-       res = this.items[pos]
+      res = this.items[pos]
     }
     // In addition, make sure you throw an error with the message OutOfBounds if a user tries to get an element in the non-existing position (e.g. if the array has 5 elements and we are trying to get the element on the position 7).
 
@@ -28,14 +30,17 @@ class SortedList {
   }
   //The max() method should return the highest value of the array.
   max() {
-    return this.items[this.items.length-1]
+    // let res = ""
     // In case you have an empty SortedList, you must throw an error with the message "EmptySortedList". For this, you can use:
-    //
-    // throw new Error('EmptySortedList');The max() method should return the highest value of the array.
-    //
-    // In case you have an empty SortedList, you must throw an error with the message "EmptySortedList". For this, you can use:
-    //
-    // throw new Error('EmptySortedList');
+    if (this.items.length === 0) {
+      throw new Error('EmptySortedList');
+
+
+    } else {
+
+      return this.items[this.items.length - 1]
+    }
+    // return res
   }
 
   min() {}
@@ -48,10 +53,10 @@ class SortedList {
 module.exports = SortedList;
 
 let list = new SortedList;
-list.add(2)
-list.add(5)
-list.add(7)
-list.add(7)
+// list.add(2)
+// list.add(5)
+// list.add(7)
+// list.add(7)
 
 
 console.log(list.max());
