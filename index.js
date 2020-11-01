707e4a6012028f84b941399b72e1ec4bca5e66f8
Ivan Rubio <ivanrubioangeles@gmail.comn>
2020-11-01 14:54:57 -0600

    ✓ should add a third value to SortedList       ✓ should add a value while keeping the list sorted

--- index.js
+++ index.js
@@ -4,8 +4,15 @@ class SortedList {
     this.length = 0
 
   }
+//The add(item) method should add the value item to the items array, ensuring that the items array stays sorted in ascending order. What does this mean? Well, if an array of items has these elements: [2, 5, 7], and if 6 is added, the array of items should be as follows: [2, 5, 6, 7].
 
-  add(item) {}
+//Here you should also make sure that the length property gets updated accordingly when new items are added to the list.
+
+
+  add(item) {
+    this.items.push(item)
+    this.items.sort((a, b)=>{return a-b});
+  }
 
   get(pos) {}
 
