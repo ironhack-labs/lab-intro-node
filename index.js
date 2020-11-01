cd924e68fa5b04705a02a1956f6aaf0d89396710
Ivan Rubio <ivanrubioangeles@gmail.comn>
2020-11-01 15:02:24 -0600

    ✓ should add a single value to SortedList ✓ should add a third value to SortedList ✓ should add a value while keeping the list sorted

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 class SortedList {
   constructor() {
     this.items = [],
-    this.length = 0
+    this.length = 0;
 
   }
 //The add(item) method should add the value item to the items array, ensuring that the items array stays sorted in ascending order. What does this mean? Well, if an array of items has these elements: [2, 5, 7], and if 6 is added, the array of items should be as follows: [2, 5, 6, 7].
@@ -11,6 +11,8 @@ class SortedList {
 
   add(item) {
     this.items.push(item)
+    this. length = this.items.length
+    console.log();
     this.items.sort((a, b)=>{return a-b});
   }
 
@@ -26,3 +28,9 @@ class SortedList {
 }
 
 module.exports = SortedList;
+
+let list = new SortedList;
+list.add(1)
+list.add(1)
+list.add(1)
+console.log(list.length);
