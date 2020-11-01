ba28c85f1a73527c8743e533b7c5fb37fd5d175b
Ivan Rubio <ivanrubioangeles@gmail.comn>
2020-11-01 15:24:53 -0600

    functtion on console no test

--- index.js
+++ index.js
@@ -10,12 +10,19 @@ class SortedList {
     this. length = this.items.length
     this.items.sort((a, b)=>{return a-b});
   }
-//The get(pos) method will get the value at index pos in the list.
-// Example: if an instance of SortedList has elements: [2, 5, 7], when get(2) called, return should be 7 since this is element in that position in the array. Check the tests to see more examples.
-//
+
 // In addition, make sure you throw an error with the message OutOfBounds if a user tries to get an element in the non-existing position (e.g. if the array has 5 elements and we are trying to get the element on the position 7).
   get(pos) {
-    return this.items.indexOf(pos)
+    let res = this.items.indexOf(pos)
+    // console.log(res);
+    if (res === -1) {
+      res = 'OutOfBounds'
+    }
+    // else {
+    //   res = this.items.indexOf(pos)
+    //
+    // }
+    return res
 
   }
 
@@ -30,8 +37,9 @@ class SortedList {
 
 module.exports = SortedList;
 
-// let list = new SortedList;
-// list.add(1)
-// list.add(1)
-// list.add(1)
-// console.log(list.length);
+let list = new SortedList;
+list.add(1)
+list.add(2)
+list.add(3)
+
+console.log(list.get(7));
