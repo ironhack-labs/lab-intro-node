a40bd8f253fc5e03c796ca67327187969dfae73f
Ivan Rubio <ivanrubioangeles@gmail.comn>
2020-11-01 15:15:03 -0600

    ✓ should return the element in that position

--- index.js
+++ index.js
@@ -1,22 +1,23 @@
 class SortedList {
   constructor() {
     this.items = [],
-    this.length = 0;
+    this. length = this.items.length;
 
   }
-//The add(item) method should add the value item to the items array, ensuring that the items array stays sorted in ascending order. What does this mean? Well, if an array of items has these elements: [2, 5, 7], and if 6 is added, the array of items should be as follows: [2, 5, 6, 7].
-
-//Here you should also make sure that the length property gets updated accordingly when new items are added to the list.
-
 
   add(item) {
     this.items.push(item)
     this. length = this.items.length
-    console.log();
     this.items.sort((a, b)=>{return a-b});
   }
+//The get(pos) method will get the value at index pos in the list.
+// Example: if an instance of SortedList has elements: [2, 5, 7], when get(2) called, return should be 7 since this is element in that position in the array. Check the tests to see more examples.
+//
+// In addition, make sure you throw an error with the message OutOfBounds if a user tries to get an element in the non-existing position (e.g. if the array has 5 elements and we are trying to get the element on the position 7).
+  get(pos) {
+    return this.items.indexOf(pos)
 
-  get(pos) {}
+  }
 
   max() {}
 
@@ -29,8 +30,8 @@ class SortedList {
 
 module.exports = SortedList;
 
-let list = new SortedList;
-list.add(1)
-list.add(1)
-list.add(1)
-console.log(list.length);
+// let list = new SortedList;
+// list.add(1)
+// list.add(1)
+// list.add(1)
+// console.log(list.length);
