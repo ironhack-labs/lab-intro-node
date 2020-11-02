6f4265d77b87532afc17dbc7271b45ba2f0164b8
KarenRoth <karenrothc@gmail.com>
2020-11-02 17:38:34 -0300

    done

--- index.js
+++ index.js
@@ -1,17 +1,52 @@
 class SortedList {
-  constructor() {}
+  constructor(items = [], length = 0) {
+    this.items= items;
+    this.length= length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items = this.items.sort((a,b) => a-b);
+    this.length++;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (!this.items[pos]) {
+      throw new Error('OutOfBounds');
+    }
+    return this.items[pos]
+  }
 
-  max() {}
+  max() {
+    if (!this.items.length) {
+      throw new Error('EmptySortedList');
+    }
+    if (this.items.length > 0){
+      return Math.max(...this.items)
+    }
+  }
 
-  min() {}
+  min() {
+    if (!this.items.length) {
+      throw new Error('EmptySortedList');
+    }
+    return this.items[0]
+  }
 
-  sum() {}
+  sum() {
+    return this.items.reduce((a, b) => a+b, 0); {
+    }
+  }
 
-  avg() {}
+  avg() {
+  if (this.items.length > 0){
+    return (this.items.reduce((a,b) => a+b, 0)/this.items.length);
+   }else{
+      throw new Exception('EmptySortedList'); 
+    }
+  }
+      
 }
+
 
 module.exports = SortedList;
