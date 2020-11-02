1e84dfe399dc5e8343e986b6f5a62fa7bbbeb296
Gabriela Polanco <gabrielapolancoferreyra@gmail.com>
2020-11-02 09:41:32 -0500

    done with bonus

--- index.js
+++ index.js
@@ -1,17 +1,59 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items= [];
+    this.length= this.items.length;
+   }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item)
+    this.items.sort((a , b)=> a - b )
+    this.length= this.items.length
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos > this.items.length) {
+    throw new Error('OutOfBounds');
+  } 
+  else{
+    return this.items[pos]
+  }
+  }
 
-  max() {}
+  max() {
+    if(this.length > 0)
+    {
+   return this.items.reduce((a,b)=> {
+      return Math.max(a,b)
+    })
+    }
+    else { 
+      throw new Error('EmptySortedList');
+    }
+  }
 
-  min() {}
+  min() {
+    if(this.length > 0 )
+    {
+      return this.items.reduce((a,b)=>{
+        return Math.min(a,b)
+      })
+      }
+      else{
+        throw new Error('EmptySortedList');
+    }
+  }
 
-  sum() {}
+  sum() {
+    return this.items.reduce((a, b) => { return a + b},0)
+  }
 
-  avg() {}
+  avg() {
+    if(this.length > 0){
+     return this.items.reduce((a,b)=> {return a + b},0) / this.length
+  } else {
+    throw new Error('EmptySortedList');
+  }
+}
 }
 
 module.exports = SortedList;
