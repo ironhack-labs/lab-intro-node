f87963ad669f84e024f34d692383deb70dc642a1
Abbie <abbie@summers.gg>
2020-11-02 22:54:13 +0100

    5 iterations complete

--- index.js
+++ index.js
@@ -7,14 +7,29 @@ class SortedList {
   add(item) {
     this.items.push(item)
     this.length++
-    this.items.sort;
+    this.items.sort(function(a,b){return a-b})
   }
 
-  get(pos) {}
+  get(pos) {
+    if (pos < this.items.length) {
+      return this.items [pos]
+    } else {
+    throw new Error('OutOfBounds'); 
+  }}
 
-  max() {}
+  max() {
+    if (this.items.length > 0) {
+      return Math.max(...this.items);
+    } else{
+    throw new Error('EmptySortedList');
+  }}
 
-  min() {}
+  min() {
+    if (this.items.length > 0) {
+      return Math.min(...this.items);
+    } else{
+    throw new Error('EmptySortedList');
+  }}
 
   sum() {}
 
