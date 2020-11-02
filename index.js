d9f11760b88e0428ff2065b85970dd6a8466991e
Mauricio Figueroa <mfigueroapa@hotmail.com>
2020-11-02 12:19:55 -0600

    done

--- index.js
+++ index.js
@@ -1,17 +1,51 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = []
+    this.length = this.items.length
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item)
+    this.items.sort((a, b) => {
+      return a - b
+    })
+    this.length = this.items.length
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos > this.items.length - 1) {
+      throw new Error('OutOfBounds')
+    }
+    return this.items[pos]
+  }
 
-  max() {}
+  max() {
+    if (this.items.length < 1) throw new Error('EmptySortedList')
+    return Math.max(...this.items)
+  }
 
-  min() {}
+  min() {
+    if (this.items.length < 1) throw new Error('EmptySortedList')
+    return Math.min(...this.items)
+  }
 
-  sum() {}
+  sum() {
+    if (this.items.length < 1) return 0
+    let sum = 0
+    this.items.forEach(n => {
+      sum += n
+    })
+    return sum
+  }
 
-  avg() {}
+  avg() {
+    if (this.items.length < 1) throw new Error('EmptySortedList')
+    let sum = 0
+    this.items.forEach(n => {
+      sum += n
+    })
+    return sum / this.length
+  }
 }
 
 module.exports = SortedList;
