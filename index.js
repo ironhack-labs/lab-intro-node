6251a69d44af5c4a7ed4c626dd185f3e3cb6e5d7
Nina van den Braak <ninavandenbraak@gmail.com>
2020-11-02 15:56:22 +0100

    did first five iterations

--- index.js
+++ index.js
@@ -1,17 +1,55 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = []
+    this.length = this.items.length
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item)
+    return this.items = this.items.sort(function (a, b) {
+      return a - b
+    })
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (!this.items[pos]) {
+      throw new Error('OutOfBounds');
+    }
+    return this.items[pos]
+  }
 
-  max() {}
+  max() {
+    if (!this.items.length) {
+      throw new Error('EmptySortedList');
+    }
+    let descending = this.items.sort(function (a, b) {
+      return b - a
+    })
+    return descending[0]
+  }
 
-  min() {}
+  min() {
+    if (!this.items.length) {
+      throw new Error('EmptySortedList');
+    }
+    return this.items[0]
+  }
 
-  sum() {}
+  sum() {
+    if (!this.items.length) {
+      return 0
+    }
+    return this.items.reduce(function (a, b) {
+      return a + b
+    })
+  }
 
-  avg() {}
+  avg() {
+    if (!this.items.length) {
+      throw new Error('EmptySortedList');
+    }
+    return this.sum() / this.items.length
+  }
 }
 
 module.exports = SortedList;
