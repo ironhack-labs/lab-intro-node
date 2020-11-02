0a775bf90baf9e31038e4aded921fd540c9b6603
adao <adaodcoelho@gmail.com>
2020-11-02 17:26:03 +0000

    done without bonus

--- index.js
+++ index.js
@@ -1,15 +1,49 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort(function(a, b) {
+      return a - b;
+  });
+    this.length = this.items.length;
 
-  get(pos) {}
+}
 
-  max() {}
+  get(pos) {
+    if (this.items.length <= pos) {
+      throw new Error('OutOfBounds');
+    } else {
+        return this.items[pos]; 
+    }
+  }
 
-  min() {}
+  max() {
+    if (this.items.length === 0) {
+    throw new Error('EmptySortedList'); 
+    } else {
+    return this.items[this.items.length-1]
 
-  sum() {}
+    }
+  }
+
+
+  min() {
+    if (this.items.length === 0) {
+      throw new Error('EmptySortedList'); 
+      } else {
+      return this.items[0]
+  
+      }
+    }
+  
+
+  sum() {
+    
+  }
 
   avg() {}
 }
