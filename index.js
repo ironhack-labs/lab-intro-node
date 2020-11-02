25acc8d089c2df4baedba2aaf6a87e084303d182
mahavinay <mahavinay28@gmail.com>
2020-11-02 16:48:53 +0100

    Completed without bonus

--- index.js
+++ index.js
@@ -1,15 +1,53 @@
+
+
 class SortedList {
-  constructor() {}
 
-  add(item) {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+   }
 
-  get(pos) {}
+  add(item) {
+    this.items.push(item);
+    if (!this.items.length){
+      throw new Error('EmptySortedList');
+    } else {
+    return this.items.sort;
+    }
+  }
 
-  max() {}
+  get(pos) {
+    
+    if (this.items.indexOf(pos) === -1){
+      throw new Error('OutOfBounds')
+    }
+    return this.items.indexOf(pos);
 
-  min() {}
+  }
 
-  sum() {}
+  max() {
+    if (!this.items.length){
+      throw new Error('EmptySortedList');
+    } else {
+    return Math.max(...this.items);
+    }
+  }
+
+  min() {
+    if (!this.items.length){
+      throw new Error('EmptySortedList');
+    } else {
+    return Math.min(...this.items);
+    }
+  }
+
+  sum() {
+    if (!this.items.length){
+      return 0;
+    } else {
+    this.items.reduce(function (a, b)  { return a + b; });
+    }
+   }
 
   avg() {}
 }
