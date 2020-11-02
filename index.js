f8ff537e5da83644707f9b6513fbb7fd9a959c42
Maria Reyna <mereynag@gmail.com>
2020-11-02 15:23:05 -0400

    Ejercicio completado

--- index.js
+++ index.js
@@ -1,17 +1,64 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = []
+    this.length = this.items.length
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item)
+    this.items.sort((a,b) => a - b)  
+    this.length = this.items.length
+  }
 
-  get(pos) {}
+  get(pos) {
+    let position = this.items[pos]
+    if (pos > this.length-1){
+      throw new Error('OutOfBounds')
+    } else {
+      return position
+    }
+  }
 
-  max() {}
+  max() {
+    if (this.length == 0){
+      throw new Error('EmptySortedList')
+    } else {
+      return Math.max(...this.items)
+    }
+  }
 
-  min() {}
+  min() {
+    if (this.length == 0){
+      throw new Error('EmptySortedList')
+    } else {
+      return Math.min(...this.items)
+    }
+  }
 
-  sum() {}
+  sum() {
+    let sum = this.items.reduce((acc, cv) => {
+        return acc + cv
+        })
+    if (this.length == 0){
+      return 0
+    } else {
+    return sum
+    }
+  }
 
-  avg() {}
+  avg() {
+    // let average = this.items.reduce((acc, cv) => {
+    //   return acc + cv
+    // }, 0)/this.length
+    let avg = this.items.reduce((acc, cv) => {
+      return acc + cv
+      }, 0)/this.length
+    if (this.length == 0){
+      throw new Error('EmptySortedList')
+    } else {
+    return avg
+    }
+  }
 }
 
 module.exports = SortedList;
