e67e329a60a7bf1a122497d194acb295f11c890c
Irene <ihatepineapple@gmail.com>
2020-11-02 15:56:13 +0100

    done

--- index.js
+++ index.js
@@ -1,17 +1,60 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [],
+    this.length = this.items.length
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item)
+    this.items.sort(
+      function(a, b) {
+        return a - b
+      }
+    )
+    this.length = this.items.length
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos < this.items.length) {
+      return this.items[pos]
+    } else {
+      throw new Error('OutOfBounds')
+    }
 
-  max() {}
+  }
 
-  min() {}
+  max() {
+    if (this.items.length > 0) {
+      return Math.max( ...this.items )
+    } else {
+      throw new Error('EmptySortedList')
+    }
+  }
 
-  sum() {}
+  min() {
+    if (this.items.length > 0) {
+      return Math.min( ...this.items )
+    } else {
+      throw new Error('EmptySortedList')
+    }
+  }
 
-  avg() {}
+  sum() {
+  if (this.items.length > 0) {
+    return this.items.reduce((a, b) => a + b, 0)
+  } else {
+    return 0
+  }
+}
+
+  avg() {
+  if (this.items.length > 0) {
+    return (this.items.reduce((a, b) => a + b, 0) / this.items.length)
+  } else {
+    throw new Error('EmptySortedList')
+  }
+
+ }
 }
 
 module.exports = SortedList;
