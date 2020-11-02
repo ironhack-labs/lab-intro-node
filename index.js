e52f71b5f8197b36688fc826a17ff4d42973ac3c
Emad Ghnim <emad.ghnim@outlook.com>
2020-11-02 16:31:17 +0100

    done

--- index.js
+++ index.js
@@ -1,17 +1,58 @@
 class SortedList {
-  constructor() {}
+  constructor(items , length) {
+    this.items = [];
+    this.length = 0;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort(function (a,b){
+      return a - b;
+    });
+    this.length ++;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (this.length < pos){
+      throw new Error('OutOfBounds');
+    } else {
+      return this.items[pos];
+    }
+  }
 
-  max() {}
+  max(){
+    if(this.length === 0){
+      throw new Error('EmptySortedList');
+    }else{
+      return Math.max.apply(null, this.items);
+    }
+  }
 
-  min() {}
+  min() {
+    if(this.length === 0){
+      throw new Error('EmptySortedList');
+    }else{
+      return Math.min.apply(null, this.items);
+    }
+  }
 
-  sum() {}
-
-  avg() {}
+  sum() {
+    let total;
+    if (this.length === 0){
+      return 0 ;
+    } else {
+      return this.items.reduce(function (accumulator, currentValue) {
+        return accumulator + currentValue;
+      }, 0)
+  }
+}
+  avg() {
+    if (this.length === 0){
+      throw Error ('EmptySortedList');
+    } else{
+      return this.sum() / this.length;
+  }
+}
 }
 
 module.exports = SortedList;
