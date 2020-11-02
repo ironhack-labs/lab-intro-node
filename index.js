3a682e6d074df92807c4d81038a416d4dd5c9faf
francisco-vivas <francisco.vivas95@gmail.com>
2020-11-02 13:16:05 -0500

    done

--- index.js
+++ index.js
@@ -1,17 +1,39 @@
 class SortedList {
-  constructor() {}
+  constructor(items = [], length = 0) {
+    this.items = items;
+    this.length = length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items = this.items.sort((a,b) => a -b);
+    this.length++;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos < 0 || pos > this.length) throw new Error('OutOfBounds');
+    return this.items[pos];
+  }
 
-  max() {}
+  max() {
+    if (!this.length) throw new Error('EmptySortedList');
+    return this.items[this.length - 1];
+  }
 
-  min() {}
+  min() {
+    if (!this.length) throw new Error('EmptySortedList');
+    return this.items[0];
+  }
 
-  sum() {}
+  sum() {
+    if (!this.length) return 0;
+    return this.items.reduce((acc,cv) => acc + cv);
+  }
 
-  avg() {}
+  avg() {
+    if (!this.length) throw new Error('EmptySortedList');
+    return this.sum()/this.length;
+  }
 }
 
 module.exports = SortedList;
