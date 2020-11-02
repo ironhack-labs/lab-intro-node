90cb1832f6820c7cfa1b27e86eabf8b553012c9e
sophiepa <sophiepa@hotmail.com>
2020-11-02 16:15:06 +0100

    first 5 interations done

--- index.js
+++ index.js
@@ -1,13 +1,46 @@
 class SortedList {
-  constructor() {}
+  constructor() {    
+    this.items = []; 
+    this.length = 0; 
+  }
 
-  add(item) {}
 
-  get(pos) {}
+  add(item) {
+      this.items.push(item); 
+      this.items.sort(      
+        function (a,b) {
+        return a-b
+      }
+      );
+    
+      this.length = this.items.length;  
+    }
+    
+    
 
-  max() {}
+  get(pos) {
+    if(pos < this.items.length){
+    return this.items[pos];
+    }else{
+      throw new Error('OutOfBounds');
+    }
+  } 
 
-  min() {}
+  max() {
+    if(this.items.length === 0){
+      throw new Error('EmptySortedList');
+    } else{
+      return Math.max(... this.items);
+    }
+  }
+
+  min() {
+    if(this.items.length > 0){
+      return Math.min(... this.items);
+    }else{
+      throw new Error('EmptySortedList');
+    }
+  }
 
   sum() {}
 
