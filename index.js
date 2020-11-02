60c31dad05fc4b9d69677b7ce1df75514d9d07ce
Alex Pliakas <pliakasbenitez@gmail.com>
2020-11-02 16:39:17 +0100

    done

--- index.js
+++ index.js
@@ -1,17 +1,53 @@
 class SortedList {
-  constructor() {}
+  constructor(items, length) {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a, b) => a - b);
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos >= this.length) {
+      throw new Error('OutOfBounds');
+    }
+    return this.items[pos];
+  }
 
-  max() {}
+  max() {
+    if (this.length === 0) {
+      throw new Error('EmptySortedList');
+    }
+    var max = this.items.reduce(function(a, b) {
+      return Math.max(a, b);
+    });
+    return max;
+  }
 
-  min() {}
+  min() {
+    if (this.length === 0) {
+      throw new Error('EmptySortedList');
+    }
+    return Math.min(...this.items);
+  }
 
-  sum() {}
+  sum() {
+    if (this.length === 0) {
+      return 0;
+    }
+    return this.items.reduce((acc, currVal) => {return acc + currVal;} );
+  }
 
-  avg() {}
+  avg() {
+    if (this.length === 0) {
+      throw new Error('EmptySortedList');
+    }
+    let total = this.items.reduce((acc, currVal) => {return acc + currVal});
+    return Math.floor(total / this.length);
+  }
 }
 
 module.exports = SortedList;
