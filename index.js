64a4d75a8aae477e6649da572d46173c132f3619
Elia Chiarucci <elia.chiarucci@gmail.com>
2020-11-04 11:56:22 +0100

    All assignments done

--- index.js
+++ index.js
@@ -1,17 +1,43 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = 0;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a,b) => a-b);
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos < this.items.length) {
+      return this.items[pos];
+    } else {
+      throw new Error('OutOfBounds');
+    }
+  }
 
-  max() {}
+  max() {
+    if(!this.items.length) throw new Error('EmptySortedList');
+    return this.items[this.items.length-1];
+  }
 
-  min() {}
+  min() {
+    if(!this.items.length) throw new Error('EmptySortedList');
+    return this.items[0];
+  }
 
-  sum() {}
+  sum() {
+    if(!this.items.length) return 0;
+    return this.items.reduce((accumulator, currentValue) => accumulator + currentValue);
+  }
 
-  avg() {}
+  avg() {
+    if(!this.items.length) throw new Error('EmptySortedList');
+    let sum = this.sum();
+    return sum / this.items.length;
+  }
 }
 
 module.exports = SortedList;
