26a733de562cc2b956763bbe80e9d29632aa1b4d
Gala Torres <galatorresrovelo@gmail.com>
2020-11-07 14:31:45 -0600

    Terminado Wohooo

--- index.js
+++ index.js
@@ -1,17 +1,48 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items =[];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+  this.items.push(item);
+  this.length = this.items.length;
+  this.items.sort((a,b)=>a-b);
+  }
 
-  get(pos) {}
+  get(pos) {
+    if(pos>this.items.length || this.items.length === 0 || this.items[pos] === ''){
+    throw new Error('OutOfBounds');}
+     return [this.items[pos]];
+  }
 
-  max() {}
+  max() {
+    if(this.items.length === 0){
+    throw new Error('EmptySortedList');
+    } return Math.max(...this.items);
+  }
 
-  min() {}
+  min() {
+    if(this.items.length === 0){
+      throw new Error('EmptySortedList');
+      } return Math.min(...this.items);
+  }
 
-  sum() {}
+  sum() {
+  let total= 0;
+  for(let i=0; i < this.items.length; i++){
+    total += this.items[i];
+  } return total;
+  }
 
-  avg() {}
-}
+  avg() {
+    if(this.items.length === 0){
+      throw new Error('EmptySortedList');
+      } let total = 0; for(let i=0; i < this.items.length; i++){
+        total += this.items[i];
+      } let average = total/this.items.length;
+       return average;
+    }
+  }
 
 module.exports = SortedList;
