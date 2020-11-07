25a4b90b0cb64a287c1daf9254cc1107b4f84f11
Daniela Gutiérrez <dangtz14@gmail.com>
2020-11-07 14:24:51 -0600

    Web-Mex-PT2 Daniela

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.lenght = this.items.length;
+  }
+  
+  add(item) {
+    this.items.push(item);
+    this.lenght = this.items.length;
+    this.items.sort((a, b) => a - b);
+  }
 
-  add(item) {}
+  get(pos) {
+    if (pos > this.items.length) {
+      throw new Error ('OutOfBounds');
+    } else {
+      return this.items(pos);
+    }
+  }
 
-  get(pos) {}
-
-  max() {}
+  max() {
+    if (this.length === 0) {
+      throw new Error ('EmptySortedList');
+    } else {
+      return Math.max(...this.items);
+    }
+  }
 
   min() {}
 
