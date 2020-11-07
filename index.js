ea36e7f113dd21d2d6a61fae9259b50e5b75a644
JuanBatkis <juanbatkis@gmail.com>
2020-11-07 15:51:20 -0300

    all done

--- index.js
+++ index.js
@@ -1,17 +1,50 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a,b)=>a-b);
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (this.items[pos] !== undefined) {
+      return this.items[pos]
+    } else {
+      throw new Error('OutOfBounds');
+    }
+  }
 
-  max() {}
+  max() {
+    if (this.length === 0) {
+      throw new Error('EmptySortedList');
+    } else {
+      return this.items[this.length - 1];
+    }
+  }
 
-  min() {}
+  min() {
+    if (this.length === 0) {
+      throw new Error('EmptySortedList');
+    } else {
+      return this.items[0];
+    }
+  }
 
-  sum() {}
+  sum() {
+    return (this.length === 0) ? 0 : this.items.reduce((accumulator, currentValue) => accumulator + currentValue);
+  }
 
-  avg() {}
+  avg() {
+    if (this.length === 0) {
+      throw new Error('EmptySortedList');
+    } else {
+      return this.sum()/this.length;
+    }
+  }
 }
 
 module.exports = SortedList;
