4603deb6909c7597c223f2640f2589ff33c8755e
“monica.jm <monicajm@Monicas-MacBook-Pro.local>
2020-11-07 14:06:20 -0600

    Done

--- index.js
+++ index.js
@@ -1,17 +1,54 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.lenght = this.items.length; 
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a,b)=>a-b);
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos > this.items.length){
+      throw new Error('OutOfBounds');
+      }else{
+      return this.items[pos];
+      }
+  }
 
-  max() {}
+  max() {
+    if (this.length === 0 ){
+      throw new Error('EmptySortedList');
+    }else {
+      return Math.max(...this.items)
+    }
+  }
 
-  min() {}
+  min() {
+    if (this.length === 0 ){
+      throw new Error('EmptySortedList');
+    }else {
+      return Math.min(...this.items)
+    }
+  }
 
-  sum() {}
+  sum() {
+    if (this.length === 0 ){
+      return 0
+    }else {
+      return this.items.reduce((a, b) => a + b, 0)
+    }
+  }
 
-  avg() {}
+  avg() {
+    if (this.length === 0){
+      return 0
+    }else{
+      this.items.reduce((a,b) => a + b)/this.items.lenght;
+    }
+  }
 }
 
 module.exports = SortedList;
