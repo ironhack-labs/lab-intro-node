97cf28dd8024f83fce59c6c2e3b2defcff70de7a
Dennis Deuling <dennis.deuling@gmail.com>
2020-11-08 13:10:36 +0100

    all iterations are done

--- index.js
+++ index.js
@@ -1,17 +1,55 @@
 class SortedList {
-  constructor() {}
+    constructor(items) {
+        this.items = [];
+        this.length = this.items.length;
+    }
 
-  add(item) {}
+    add(item) {
+        this.items.push(item);
+        this.items.sort((a, b) => a - b);
+        this.length = this.items.length;
+    }
 
-  get(pos) {}
+    get(pos) {
+        if (this.items[pos] != undefined) {
+            return this.items[pos];
+        } else {
+            throw new Error('OutOfBounds');
+        }
+    }
 
-  max() {}
+    max() {
+        if (this.length != 0) {
+            return Math.max(...this.items);
+        } else {
+            throw new Error('EmptySortedList');
+        }
+    }
 
-  min() {}
+    min() {
+        if (this.length != 0) {
+            return Math.min(...this.items);
+        } else {
+            throw new Error('EmptySortedList');
+        }
 
-  sum() {}
+    }
 
-  avg() {}
+    sum() {
+        if (this.length != 0) {
+            return this.items.reduce((accu, curr) => accu + curr);
+        } else {
+            return 0;
+        }
+    }
+
+    avg() {
+        if (this.length != 0) {
+            return this.items.reduce((accu, curr) => accu + curr) / this.length;
+        } else {
+            throw new Error('EmptySortedList');
+        }
+    }
 }
 
 module.exports = SortedList;
