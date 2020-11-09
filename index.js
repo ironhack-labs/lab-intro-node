a47b3d17c287956441bc2ffbc20ca7050cede800
Patrick Brito <patrickbrito95@gmail.com>
2020-11-09 19:36:40 -0300

    done

--- index.js
+++ index.js
@@ -1,13 +1,40 @@
 class SortedList {
-  constructor() {}
+  constructor(items, length) {
+    this.items = [],
+    this.length = this.items.length
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item); 
+    this.items.sort((a, b) => a - b);
+    this.lenght = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (!this.items[pos]){
+      throw new Error("OutOfBounds");
+    } else {
+      return this.items[pos];
+    }
+  }
 
-  max() {}
+  max() {
+    if (this.lenght == 0){
+      throw new Error("EmptySortedList");
+    } else {
+      return Math.max(...this.items);
+    }
+    
+    
+  }
 
-  min() {}
+  min() {
+    if (this.length === 0){
+      throw new Error("EmptySortedList");
+    } else {
+      return Math.min(...this.items);
+    }
+  }
 
   sum() {}
 
