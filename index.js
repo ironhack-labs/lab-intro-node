c394d7c68e672571494b39b099186b6fa0e18e9d
Rodrigo Matos <rodrigomatosrj@gmail.com>
2020-11-09 18:56:12 -0300

    done

--- index.js
+++ index.js
@@ -1,17 +1,50 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a,b)=>a-b);
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if(pos < this.length){
+      return this.items[pos];
+    }else{
+      throw new Error('OutOfBounds');
+    }
+  }
 
-  max() {}
+  max() {
+    if(this.length){
+      return this.items[this.length-1];
+    }else{
+      throw new Error('EmptySortedList');
+    }
+  }
 
-  min() {}
+  min() {
+    if(this.length){
+      return this.items[0];
+    }else{
+      throw new Error('EmptySortedList');
+    }
+  }
 
-  sum() {}
+  sum() {
+    return this.items.reduce((a,b)=>a+b,0);
+  }
 
-  avg() {}
+  avg() {
+    if(this.length){
+      return this.sum()/this.length;
+    }else{
+      throw new Error('EmptySortedList');
+    }  
+  }
 }
 
 module.exports = SortedList;
