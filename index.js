4b8a58df08fa196b22dce5026a54e79e3af80567
thaliaberger <thaliaberger@hotmail.com>
2020-11-09 20:04:06 -0300

    done

--- index.js
+++ index.js
@@ -1,17 +1,61 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+     this.items.push(item);
+     this.items.sort((a, b) => a - b);
+     this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos < this.length){
+      return this.items[pos];
+    }else {
+      throw new Error('OutOfBounds');
+    }
+  }
 
-  max() {}
+  max() {
+    if (this.length === 0){
+      throw new Error('EmptySortedList');
+    } else {
+      return Math.max.apply(null, this.items);
+    }
+  }
 
-  min() {}
+  min() {
+    if (this.length === 0){
+      throw new Error('EmptySortedList');
+    } else {
+      return Math.min.apply(null, this.items);
+    }
+  }
 
-  sum() {}
 
-  avg() {}
+  sum() {
+    if (this.length === 0){
+      return 0;
+    } else {
+      return this.items.reduce((a, b) => {
+        return a + b;
+      });
+    }
+  }
+
+
+  avg() {
+    if (this.length === 0){
+      throw new Error('EmptySortedList');
+    } else {
+      let sum = this.items.reduce((a, b) => {
+        return a + b;
+      });
+      return sum / this.length;
+    }
+  }
 }
 
 module.exports = SortedList;
