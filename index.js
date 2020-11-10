eba93f7da322825125601499a4cc251c659a59d6
lighteronn <mezora197@gmail.com>
2020-11-10 20:56:47 +0100

    done

--- index.js
+++ index.js
@@ -1,17 +1,55 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = []
+    this.length = 0
+  }
 
-  add(item) {}
+  add(item) {
+    
+    this.items.push(item)
+     this.length++
+    this.items = this.items.sort((a,b) => a-b)
+    
+    return this.items
+  };
 
-  get(pos) {}
+  get(pos) {
+     if(this.items[pos]) {
+       return this.items[pos];
+     } else {
+      throw new Error('OutOfBounds');
+     }
+};
 
-  max() {}
+  max() {
+    const max = Math.max( ...this.items );
+    if(this.items.length == 0) {
+      throw new Error('EmptySortedList');
+    } else {
+      return max
+    }
 
-  min() {}
+   
+  }
 
-  sum() {}
+  min() {
+    const min = Math.min( ...this.items );
+    if(this.items.length == 0) {
+      throw new Error('EmptySortedList');
+    } else {
+      return min
+    }
+  
+  }
 
-  avg() {}
+  sum() {
+    return this.items.reduce((acc,val) => {return acc+val},0)
+  }
+
+  avg() {
+    if(this.length == 0) {throw new Error('EmptySortedList')}
+    return this.items.reduce((acc,val) => {return acc+val}) / this.length
+  }
 }
 
 module.exports = SortedList;
