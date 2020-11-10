70519298bc24466be59502a7749cd553b3bbaa37
paulinemss <masse.pauline@gmail.com>
2020-11-10 14:57:56 +0100

    Lab completed

--- index.js
+++ index.js
@@ -1,17 +1,46 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length; 
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a, b) => a - b); 
+    this.length = this.items.length; 
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos > this.length || pos < 0) {
+      throw new Error('OutOfBounds'); 
+    }
+    return this.items[pos]; 
+  }
 
-  max() {}
+  max() {
+    if (this.length === 0) {
+      throw new Error('EmptySortedList');
+    }
+    return Math.max(...this.items);
+  }
 
-  min() {}
+  min() {
+    if (this.length === 0) {
+      throw new Error('EmptySortedList');
+    }
+    return Math.min(...this.items);
+  }
 
-  sum() {}
+  sum() {
+    return this.items.reduce((acc, cur) => acc + cur, 0); 
+  }
 
-  avg() {}
+  avg() {
+    if (this.length === 0) {
+      throw new Error('EmptySortedList');
+    }
+    return this.sum() / this.length; 
+  }
 }
 
 module.exports = SortedList;
