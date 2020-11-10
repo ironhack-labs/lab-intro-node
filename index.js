083eeb97b9dfe81baf972369c221caec379d5d75
Marcel Niebylski <mnblski@gmail.com>
2020-11-10 14:12:32 +0000

    all done

--- index.js
+++ index.js
@@ -1,17 +1,62 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a, b) => a - b);
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos > this.length || pos < 0) {
+      throw new Error("OutOfBounds");
+    } else {
+      return this.items[pos];
+    }
+  }
 
-  max() {}
+  max() {
+    if (this.length > 0) {
+      return this.items[this.length - 1];
+    } else {
+      throw new Error("EmptySortedList");
+    }
+  }
 
-  min() {}
+  min() {
+    if (this.length > 0) {
+      return this.items[0];
+    } else {
+      throw new Error("EmptySortedList");
+    }
+  }
 
-  sum() {}
+  sum() {
+    let sum = 0;
 
-  avg() {}
+    for (let i = 0; i < this.length; i++) {
+      sum += this.items[i];
+    }
+
+    return sum;
+  }
+
+  avg() {
+    let sum = 0;
+
+    for (let i = 0; i < this.length; i++) {
+      sum += this.items[i];
+    }
+
+    if (this.length === 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      return sum / this.length;
+    }
+  }
 }
 
 module.exports = SortedList;
