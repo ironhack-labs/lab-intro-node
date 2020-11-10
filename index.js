3db5e55e881e4d7596d6536fd04d92c9972f3c44
“Anna <aniawaszak@yahoo.com>
2020-11-10 14:37:28 +0100

    done

--- index.js
+++ index.js
@@ -1,17 +1,38 @@
 class SortedList {
-  constructor() {}
+  constructor(items, length) {
+    this.items = [];
+    this.length = 0;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item)
+    this.items.sort((a, b) => a - b);
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (this.items[pos]) return this.items[pos];
+    else throw new Error('OutOfBounds');
+  }
 
-  max() {}
+  max() {
+    if (this.length === 0) throw new Error('EmptySortedList')
+    else return Math.max(...this.items);
+  }
 
-  min() {}
+  min() {
+    if (this.length === 0) throw new Error('EmptySortedList')
+    else return Math.min(...this.items);
+  }
 
-  sum() {}
+  sum() {
+    return this.items.reduce((a, b) => a + b, 0);
+  }
 
-  avg() {}
+  avg() {
+    if (this.length === 0) throw new Error('EmptySortedList')
+    else return (this.sum() / this.length);
+  }
 }
 
 module.exports = SortedList;
