ee00a2a1cd3016d0e07ab1d9a0eb3fcfefbf3736
GosiaMas <malgorzata.maslowska81@gmail.com>
2020-11-10 14:34:06 +0000

    finished lab

--- index.js
+++ index.js
@@ -1,17 +1,54 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a, b) => a - b);
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos >= this.length) {
+      throw new Error("OutOfBounds");
+    } else {
+      return this.items[pos];
+    }
+  }
 
-  max() {}
+  max() {
+    if (!this.items.length) {
+      throw new Error("EmptySortedList");
+    } else {
+      return Math.max(...this.items);
+    }
+  }
 
-  min() {}
+  min() {
+    if (!this.items.length) {
+      throw new Error("EmptySortedList");
+    } else {
+      return Math.min(...this.items);
+    }
+  }
 
-  sum() {}
+  sum() {
+    if (!this.items.length) {
+      return 0;
+    } else {
+      return this.items.reduce((acc, el) => acc + el);
+    }
+  }
 
-  avg() {}
+  avg() {
+    if (!this.items.length) {
+      throw new Error("EmptySortedList");
+    } else {
+      return this.items.reduce((acc, el) => acc + el) / this.items.length;
+    }
+  }
 }
 
 module.exports = SortedList;
