ec459bb8d3375038e894d3090a0a5b304e76acdc
Xiaomei <xiaomei@Xiaomeis-MacBook-Air.local>
2020-11-10 17:08:01 +0100

    done

--- index.js
+++ index.js
@@ -1,17 +1,67 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a,b) => a - b);
+    this.length++;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if(this.length > pos) {
+      return this.items[pos];
+    } else {
+      throw new Error('OutOfBounds')
+    }
+  }
+  max() {
+    let max = this.items[0];
+    if(this.length !== 0) {
+      for (let i = 0; i < this.length; i++) {
+        if (this.items[i] > max) {
+          max = this.items[i];
+        }
+      }
+      return max;
+    } else {
+      throw new Error('EmptySortedList')
+    }
+  }
 
-  max() {}
+  min() {
+    let min = this.items[0];
+    if(this.length !== 0) {
+      for (let i = 0; i < this.length; i++) {
+        if (this.items[i] < min) {
+          min = this.items[i];
+        }
+      }
+      return min;
+    } else {
+      throw new Error('EmptySortedList')
+    }
+  }
 
-  min() {}
+  sum() {
+    let sum = 0;
+    if(this.length === 0) return 0;
+    else {
+      for (let i = 0; i < this.length; i++) {
+          sum += this.items[i];
+      };
+      return sum;
+    }
+}
 
-  sum() {}
-
-  avg() {}
+  avg() {
+    if(this.length === 0) throw new Error('EmptySortedList');
+    else {
+      return this.sum() / this.length;
+  }
+}
 }
 
 module.exports = SortedList;
