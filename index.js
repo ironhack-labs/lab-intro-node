0ceef04b28dddbb9c794360b6532224179b5f682
denisonamaral <denison.amaral@outlook.com>
2020-11-10 00:00:14 -0300

    Upload exercise intro-node

--- index.js
+++ index.js
@@ -1,17 +1,57 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = "";
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.length = this.items.length;
+    this.items.sort((a, b) => {
+      return a - b;
+    });
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (this.items.indexOf(pos) === -1) {
+      throw new Error("OutOfBounds");
+    } else {
+      return this.items.indexOf(pos);
+    }
+  }
 
-  max() {}
+  max() {
+    let max = this.items.reduce((a, b) => Math.max(a, b));
+    if (typeof max === "number") {
+      return max;
+    } else {
+      throw new Error("EmptySortedList");
+    }
+  }
 
-  min() {}
+  min() {
+    if (!this.items) {
+      return new Error("EmptySortedList");
+    }
+    let min = Math.min(...this.items);
+    if (typeof min === "number") {
+      return min;
+    }
+  }
 
-  sum() {}
+  sum() {
+    if (!this.items.length) {
+      return 0;
+    }
+    return this.items.reduce((a, b) => a + b);
+  }
 
-  avg() {}
+  avg() {
+    if (!this.items) {
+      new Error("EmptySortedList");
+    }
+    return this.items.reduce((a, b) => a + b) / this.items.length;
+  }
 }
 
 module.exports = SortedList;
