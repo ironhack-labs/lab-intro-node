d69bd75882eecebd489bd3c0bbeb68af5a314089
Hortencia Jorquera <hortencia.pj@gmail.com>
2020-11-10 14:37:35 +0100

    lab intro node

--- index.js
+++ index.js
@@ -1,17 +1,48 @@
 class SortedList {
-  constructor() {}
+  constructor(items,length) {
+    this.items=[];
+    this.length=0;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort(function(a, b){return a-b});
+    this.length=this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if(this.items[pos])
+      return this.items[pos];
+    else
+      throw new Error('OutOfBounds');
+  }
 
-  max() {}
+  max() {
+    if(this.items[this.items.length-1])
+      return this.items[this.items.length-1];
+    else
+      throw new Error('EmptySortedList');
 
-  min() {}
+  }
 
-  sum() {}
+  min() {
+    if(this.items[0])
+      return this.items[0];
+    else
+      throw new Error('EmptySortedList');
 
-  avg() {}
+  }
+
+  sum() {
+    return this.items.reduce((acc,val)=>acc+val,0);
+  }
+
+  avg() {
+    if(this.length!==0)
+      {return this.sum()/this.length}
+    else
+      {throw new Error('EmptySortedList');}
+  }
 }
 
 module.exports = SortedList;
