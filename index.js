e0786d41a36b9a0011b203a3dc19cc9ba9b36428
VincenzoDeLucia <vincenzo.delucia@icloud.com>
2020-11-10 15:01:02 +0100

    Completed

--- index.js
+++ index.js
@@ -1,17 +1,48 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a, b) => a - b);
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos >= this.length) {
+      throw new Error("OutOfBounds");
+    }
+    return this.items[pos];
+  }
 
-  max() {}
+  max() {
+    if (this.length) {
+      return Math.max(...this.items);
+    } else {
+      throw new Error("EmptySortedList");
+    }
+  }
 
-  min() {}
+  min() {
+    if (this.length) {
+      return Math.min(...this.items);
+    } else {
+      throw new Error("EmptySortedList");
+    }
+  }
 
-  sum() {}
+  sum() {
+    return this.items.reduce((acc, el) => acc + el, 0);
+  }
 
-  avg() {}
+  avg() {
+    if (this.length) {
+      return this.sum() / this.length;
+    }
+    throw new Error("EmptySortedList");
+  }
 }
 
 module.exports = SortedList;
