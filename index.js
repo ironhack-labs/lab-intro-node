9d03820bdaede0c97fc1f30a4c769877108dd6a3
Katie Gracey <gracey.kd@gmail.com>
2020-11-10 14:29:21 +0100

    done

--- index.js
+++ index.js
@@ -1,17 +1,55 @@
 class SortedList {
-  constructor() {}
+  constructor(items, length) {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort(function (a, b) {
+      return a - b;
+    });
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (!this.items[pos]) {
+      // console.log(this.items[pos]);
+      throw new Error('OutOfBounds');
+    } else {
+      return this.items[pos];
+    }
+  }
 
-  max() {}
+  max() {
+    if(!this.length) {
+      throw new Error('EmptySortedList');
+    } else {
+      return Math.max(...this.items)
+    }
+  }
 
-  min() {}
+  min() {
+    if(!this.length) {
+      throw new Error('EmptySortedList');
+    } else {
+      return Math.min(...this.items)
+    }
+  }
 
-  sum() {}
+  sum() {
+    let sum = this.items.reduce((a,b) => a + b, 0)
+    return sum;
+  }
 
-  avg() {}
+  avg() {
+    if(!this.length) {
+      throw new Error('EmptySortedList');
+    } else {
+      let avg = this.sum()/this.length;
+      return avg
+    }
+  }
 }
 
 module.exports = SortedList;
