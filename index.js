5127d27f806292d3f5573af62511f77d0d0cdf49
DenisM20 <denis.gmta@gmail.com>
2020-11-10 13:06:38 +0000

    done

--- index.js
+++ index.js
@@ -1,17 +1,61 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = []
+    this.length = 0
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item)
+    this.items.sort((a,b) => a - b)
+    this.length = this.items.length
 
-  get(pos) {}
+  }
 
-  max() {}
+  get(pos) {
+    if (pos === null || pos > this.items.length){
+      throw new Error('OutOfBounds');
+    } else{
+      return this.items[pos];
+    }
+  }
+  max() {
+    {
+      if(this.length ===0){
+        throw new Error('EmptySortedList');
+      }
+      else{
+        return this.items[this.length-1];
+      }
+     }
+    }
 
-  min() {}
+  min() {
 
-  sum() {}
+    if(this.length ===0){
+      throw new Error('EmptySortedList');
+    }
+    else{
+      return this.items[0];
+  }
+}
 
-  avg() {}
+  sum() {
+    if (this.items.length < 1) {
+      return 0;
+    } else {
+      return this.items.reduce((a, b) => {
+        return a + b;
+      });
+    }
+  }
+
+  avg() {
+    if (this.length == 0){
+      throw TypeError("EmptySortedList");
+      } else{
+      return this.sum()/this.items.length;
+      }
+  }
 }
 
 module.exports = SortedList;
