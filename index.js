ead3f21b972aa8585f366d2b3cd07147681578e6
gtretow <guilherme.tretow@gmail.com>
2020-11-09 21:19:35 -0300

    ex 1 até 3 feitos

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = 0;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort(function (a, b) {
+      return a - b;
+    });
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (this.length >= pos) {
+      return this.items[pos];
+    } else {
+      throw new Error("OutOfBounds");
+    }
+  }
 
   max() {}
 
