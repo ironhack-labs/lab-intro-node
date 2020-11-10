91e68b808793ccf543f135df9533e754172a2ff0
Hector Lima <hectoraugusto12@yahoo.ie>
2020-11-10 11:44:04 +0000

    done

--- index.js
+++ index.js
@@ -1,17 +1,63 @@
 class SortedList {
-  constructor() {}
+  constructor(items,length) {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a,b) =>a-b);
+    this.length = this.items.length;
 
-  get(pos) {}
+  }
 
-  max() {}
+  get(pos) {
+    if(this.length > pos) {
+     return this.items[pos];
+     } else
+      {
 
-  min() {}
+         throw new Error('OutOfBounds');
+    }
 
-  sum() {}
+  }
 
-  avg() {}
+  max() {
+    if(this.length){
+        return this.items[this.length -1];
+    } else {
+      throw new Error("EmptySortedList");
+    }
+
+
+  }
+
+  min() {
+    if(this.length) {
+      return this.items[0];
+    } else {
+      throw new Error("EmptySortedList");
+    }
+  }
+
+  sum() {
+    
+      if (this.length === 0) {
+        return 0;
+    
+    } else {
+      return  this.items.reduce((a,b) => a + b);
+    }
+  }
+
+  avg() {
+    if (this.length == 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      return this.items.reduce((a,b) => a + b) / this.length;
+    }
+
+  }
 }
 
 module.exports = SortedList;
