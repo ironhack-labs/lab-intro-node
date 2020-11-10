579cc8b3971a0f6218a6d99a7130ec105502767c
“dialmojordan” <d.andreani2@gmail.com>
2020-11-10 14:54:41 +0100

    done

--- index.js
+++ index.js
@@ -1,17 +1,50 @@
 class SortedList {
-  constructor() {}
+  constructor(item, length) {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a, b) => a - b);
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (
+      pos > this.items.length ||
+      this.items.length === 0 ||
+      this.items[pos] === ""
+    ) {
+      throw new Error("OutOfBounds");
+    }
+    return [this.items[pos]];
+  }
 
-  max() {}
+  max() {
+    if (this.items.length === 0) {
+      throw new Error("EmptySortedList");
+    }
+    return Math.max(...this.items);
+  }
 
-  min() {}
+  min() {
+    if (this.items.length === 0) {
+      throw new Error("EmptySortedList");
+    }
+    return Math.min(...this.items);
+  }
 
-  sum() {}
-
-  avg() {}
+  sum() {
+    return this.items.reduce((acc, val) => acc + val, 0);
+  }
+  //store
+  avg() {
+    if (this.items.length === 0) {
+      throw new Error("EmptySortedList");
+    }
+    return this.sum() / this.items.length;
+  }
 }
 
 module.exports = SortedList;
