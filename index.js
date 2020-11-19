41acde2b8c1da2597f78607565bedc34bb0f17b5
projav <projav23@gmail.com>
2020-11-19 13:46:31 +0100

    done with bonus

--- index.js
+++ index.js
@@ -1,17 +1,50 @@
 class SortedList {
-  constructor() {}
-
-  add(item) {}
-
-  get(pos) {}
-
-  max() {}
-
-  min() {}
-
-  sum() {}
-
-  avg() {}
+  constructor() {
+    this.items = []
+    this.length = this.items.length
+  }
+  add(item) {
+    if (!this.items){
+      this.length = 1
+    }
+    this.items.push(item)
+    this.items.sort(function(a,b){
+      return a - b;
+    })
+    this.length = this.items.length
+  }
+  get(pos) {
+    if (pos > this.length){
+      throw new Error('OutOfBounds');
+    }
+    return this.items[pos];
+  }
+  max() {
+    if (!this.length){
+      throw new Error('EmptySortedList');
+    }
+    return Math.max(...this.items)
+  }
+  min() {
+    if (!this.length){
+      throw new Error('EmptySortedList');
+    }
+    return Math.min(...this.items)
+  }
+  sum() {
+    if (!this.length){
+      return 0;
+    } else {
+      return this.items.reduce((acc, next) => acc + next);
+    }
+  }
+  avg() {
+    if (!this.length){
+      throw new Error('EmptySortedList');
+    } else {
+      return this.items.reduce((acc, next) => acc + next)/this.length;
+    }
+  }
 }
 
 module.exports = SortedList;
