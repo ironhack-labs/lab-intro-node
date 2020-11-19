bbb0ef88a4cfce693736d2e27d75aef8cf75b574
Samantha van der Put <samantha.vdput@gmail.com>
2020-11-19 15:10:36 +0100

    done

--- index.js
+++ index.js
@@ -1,17 +1,67 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort(function(a, b){
+      return a-b;
+    });
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos > this.length) {
+      throw new Error ("OutOfBounds");
+    }
+    else {
+      return this.items[pos];
+    }
+  }
 
-  max() {}
+  max() {
+    if (this.length < 1) {
+      throw new Error ("EmptySortedList");
+    }
+    else {
+      return Math.max(...this.items);
+    }
+  }
 
-  min() {}
+  min() {
+    if (this.length < 1) {
+      throw new Error ("EmptySortedList");
+    }
+    else {
+      return Math.min(...this.items);
+    }
+  }
 
-  sum() {}
+  sum() {
+    if (this.length < 1) {
+      return 0;
+    }
+    else {
+      let totalSum = this.items.reduce(function(a, b) {
+        return a + b;
+      }, 0);
+      return totalSum;
+    }
+  }
 
-  avg() {}
+  avg() {
+    if (this.length < 1) {
+      throw new Error("EmptySortedList");
+    }
+    else {
+      let totalSum = this.items.reduce(function(a, b) {
+        return a + b;
+      }, 0);
+      return totalSum / this.length;
+    }
+  }
 }
 
 module.exports = SortedList;
