e9906ccb92fa17f716bd86fb3302bb8319683f85
Marc Lopez Pujol <marclopez@MacBook-Pro-de-Marc-2.local>
2020-11-19 21:35:46 +0100

    Hasta la iteración 3

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items = this.items.sort((a, b) => {
+    return a - b;
+    });
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    const index = this.items.filter((num, i, arr) => { if (i === pos) return num });
+    if (index[0]) {
+      console.log("esto", index)
+      return index[0]
+    } else {
+      throw new Error('OutOfBounds');
+    }
+  }
 
   max() {}
 
@@ -15,3 +32,9 @@ class SortedList {
 }
 
 module.exports = SortedList;
+
+/*const list = new SortedList;
+
+list.add(2);
+console.log(list.get(1))*/
+
