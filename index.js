ceedfd90444a4e66463e7ff4937e088d3d00e80d
Tomy <tomytubert@gmail.com>
2020-11-19 10:36:04 +0100

    First try before class

--- index.js
+++ index.js
@@ -1,17 +1,54 @@
 class SortedList {
-  constructor() {}
+  constructor(items,length) {
+    this.items = [];
+    this.length = 0;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item).sort((a,b)=>{
+      return a-b
+    });
+    this.length++;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if(pos > this.length -1){
+      throw new Error("OutOfBounds")
+    } else {
+      return this.items[pos];
+    }
+  }
 
-  max() {}
+  max() {
+    if(this.length == 0){
+      throw new Error('EmptySortedList')
+    } else {
+      return Math.max(...this.items)
+    }
 
-  min() {}
+  }
 
-  sum() {}
+  min() {
+    if(this.length == 0){
+      throw new Error('EmptySortedList')
+    } else {
+      return Math.min(...this.items)
+    }
+  }
 
-  avg() {}
+  sum() {
+    if(this.length == 0){
+      throw new Error('EmptySortedList')
+    } else {
+      this.items.reduce((acc,next)=>{
+        acc+next
+      })
+    }
+  }
+
+  avg() {
+  return this.sum()/this.length;
+  }
 }
 
 module.exports = SortedList;
