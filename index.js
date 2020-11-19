90c544448ae117840e71719f89eabb80b9b0d7f7
shtesh <vshtefanuka@gmail.com>
2020-11-19 21:26:20 +0100

    Done except avg

--- index.js
+++ index.js
@@ -1,17 +1,68 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort(function (a, b) {
+      return a - b;
+    }
+  );
+  this.length++;
+}
 
-  get(pos) {}
+  get(pos) {
+    if (pos > this.length){
+    throw new Error('OutOfBounds');
+  } else {
+    return this.items[pos];
+  }
+}
 
-  max() {}
+  max() {
+    if (this.length === 0) {
+      throw new Error('EmptySortedList');
+    } else {
+      return this.items.reduce(function(a,b){
+        return Math.max(a, b);
+      }); 
+    }
+  }
 
-  min() {}
+  min() {
+    if (this.length === 0) {
+      throw new Error('EmptySortedList');
+    } else {
+      return this.items.reduce(function(a, b) {
+        return Math.min(a, b);
+      });
+  }
+}
 
-  sum() {}
+  sum() {
+    if (this.length === 0) {
+      return 0;
+    } else {
+      let sum = 0;
+      for (let i = 0, i < this.length; i++ ) {
+        sum += this.items[i];
+      }
+      return sum;
+    }
+  }
 
-  avg() {}
+  avg() {
+    if (this.length === 0) {
+      throw new Error('EmptySortedList');
+    } else {
+      let sum = 0;
+      for (let i = 0; i < this.length; i++) {
+        sum += this.items[i];
+      }
+      return sum/this.length;
+  }
 }
 
 module.exports = SortedList;
