158a147b8fde1063279ada91561836157228ba73
LucasFeli <felipe298@gmail.com>
2020-11-20 20:44:36 +0100

    done

--- index.js
+++ index.js
@@ -1,17 +1,49 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a, b) => {
+      return a - b;
+    });
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (this.items.indexOf(pos)) {
+      return pos;
+    }
+    return pos
+  }
 
-  max() {}
+  max() {
+    var maxi = this.items.reduce(function (a, b) {
+      return Math.max(a, b);
+    });
+    return maxi;
+  }
 
-  min() {}
+  min() {
+    var mini = this.items.reduce(function (a, b) {
+      return Math.min(a, b);
+    });
+    return mini;
+  }
 
-  sum() {}
+  sum() {
+    const suma = this.items.reduce((a, b) => a + b, 0);
 
-  avg() {}
+    return suma;
+  }
+
+  avg() {
+    const average = this.items.reduce((a, b) => a + b);
+
+    return average / this.items.length;
+  }
 }
 
 module.exports = SortedList;
