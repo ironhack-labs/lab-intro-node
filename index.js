6947682f94aa639c7bd923502ac8684f7f1a6682
monibix <monibixv2@gmail.com>
2020-11-20 09:23:33 +0100

    Done with bonus

--- index.js
+++ index.js
@@ -1,17 +1,54 @@
 class SortedList {
-  constructor() {}
+  constructor(items, length) {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item)
+    this.items.sort((a, b) => {
+      return a-b
+    })
+  }
 
-  get(pos) {}
+  get(pos) {
+  if (this.items.indexOf(pos) === -1) {
+    throw new Error('OutOfBounds');
+  } else {
+    return this.items.indexOf(pos)
+  }
+  }
 
-  max() {}
+  max() {
+    if (this.items.length === 0) {
+      throw new Error('EmptySortedList')
+    } else {
+      return Math.max(...this.items)
+    }
+  }
 
-  min() {}
+  min() {
+    if (this.items.length === 0) {
+      throw new Error('EmptySortedList')
+    } else {
+      return Math.min(...this.items)
+    }
+  }
 
-  sum() {}
+  sum() {
+    let sum = this.items.reduce((curr, next) => {
+      return curr + next
+    }, 0)
+    return sum
+  }
 
-  avg() {}
+  avg() {
+    if (this.items.length === 0) {
+      throw new Error('EmptySortedList')
+    } else {
+    return this.sum()/this.items.length
+    }
+  }
 }
 
 module.exports = SortedList;
