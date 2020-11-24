da67bd29ed2add121e30d079a3ee2029fcf126c8
shtesh <vshtefanuka@gmail.com>
2020-11-24 18:30:57 +0100

    Alternative solutions added

--- index.js
+++ index.js
@@ -6,15 +6,14 @@ class SortedList {
 
   add(item) {
     this.items.push(item);
-    this.items.sort(function (a, b) {
-      return a - b;
-    }
-  );
-  this.length++;
-}
+    this.length = this.items.length;
+    this.items.sort( (a, b) => a - b);
+  }
+  //this.length++;
+
 
   get(pos) {
-    if (pos > this.length){
+    if (pos > this.length-1 || pos < 0) {
     throw new Error('OutOfBounds');
   } else {
     return this.items[pos];
@@ -30,7 +29,10 @@ class SortedList {
       }); 
     }
   }
-  //return Math.max(...this.items)
+  //if(!this.length) {
+  //throw new Error('EmptySortedList');
+  //}
+  //return Math.max(...this.items);
 
   min() {
     if (this.length === 0) {
@@ -52,6 +54,11 @@ class SortedList {
       }, 0);
       return totalSum;
     }
+    //if(!this.length) {
+    //return 0;
+    //}
+    //return this.items.reduce( (acc, val) => acc + val);
+
   }
 
   avg() {
@@ -64,6 +71,11 @@ class SortedList {
       return totalSum/this.length;
   }
 }
+//if(!this.length) {
+//throw new Error('EmptySortedList');
+//}
+//return this.sum() / this.length;
+
 }
 
 module.exports = SortedList;
