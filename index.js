26e66b3a5816ff8a2147bb229cb4c791d2576be5
Xuan Tran <tran.ttxuan91@gmail.com>
2020-11-30 16:35:01 +0100

    done - max() use Math.max(...this.items)

--- index.js
+++ index.js
@@ -1,17 +1,63 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = 0;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a, b) => {
+      return a - b
+    })
 
-  get(pos) {}
+    this.length++
+  }
 
-  max() {}
+  get(pos) {
+    if (this.items[pos]) {
+      return this.items[pos];
+    } else {
+      throw new Error('OutOfBounds')
+    }
+  }
 
-  min() {}
+  max() {
+    if (this.length === 0) {
+      throw new Error('EmptySortedList');
+    } else {
+      return Math.max(...this.items)
+    }
 
-  sum() {}
+  }
 
-  avg() {}
+  min() {
+    if (this.length === 0) {
+      throw new Error('EmptySortedList');
+    } else {
+      return this.items[0]
+    }
+  }
+
+  sum() {
+    if (this.length == 0) {
+      return 0;
+    } else {
+      return this.items.reduce((acc, current) => {
+        acc += current;
+        return acc
+      });
+
+    }
+
+  }
+
+  avg() {
+    if (this.length === 0) {
+      throw new Error('EmptySortedList');
+    } else {
+      return this.sum() / this.length;
+    }
+  }
 }
 
 module.exports = SortedList;
