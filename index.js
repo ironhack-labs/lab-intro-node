09ac58d6159d584914f69422726c5cf71cc1bd5f
aguiluxo91 <apuertadelaguila@gmail.com>
2020-12-12 12:20:25 +0100

    done

--- index.js
+++ index.js
@@ -1,17 +1,50 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a, b) => a - b);
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if(pos <= this.items.length) {
+      return this.items[pos];
+    } else {
+      throw new Error('OutOfBounds');
+    }  
+  }
+  
+  max() {
+    if(this.items.length === 0 ) {
+      throw new Error('EmptySortedList');
+    } else {
+      return this.items[this.length - 1];
+    }  
+  }
 
-  max() {}
+  min() {
+    if(this.items.length === 0) {
+      throw new Error('EmptySortedList');
+    } else {
+      return this.items[0];
+    }  
+  }
 
-  min() {}
+  sum() {
+    return this.items.reduce((acc, el) => acc + el, 0);
+  }
 
-  sum() {}
-
-  avg() {}
+  avg() {
+    if (this.length === 0) {
+      throw new Error('EmptySortedList');
+    } else {
+      return this.sum() / this.length;
+    }
+  }
 }
 
 module.exports = SortedList;
