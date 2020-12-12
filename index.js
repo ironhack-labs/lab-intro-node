b486473ca5404e33257f2443b8ed0ff1f64fc136
Toni Sanmartin <tonisanmartin@MacBook-Pro-de-Toni.local>
2020-12-12 12:29:48 +0100

    feat Excercise complete

--- index.js
+++ index.js
@@ -1,17 +1,89 @@
 class SortedList {
-  constructor() {}
+  constructor() {
 
-  add(item) {}
+    this.items = []
+    this.length = this.items.length
+  }
 
-  get(pos) {}
+  add(item) {
 
-  max() {}
+    this.items.push(item);
+    this.items.sort((a,b) => a - b);
+    this.length = this.items.length
 
-  min() {}
+  }
 
-  sum() {}
+  get(pos) {
+    
+    if (!this.items[pos]) {
 
-  avg() {}
+      throw new Error('OutOfBounds');
+
+    } 
+
+    const objeto = this.items[pos]
+
+    return objeto;
+}
+
+  max() {
+
+    if (this.items.length === 0) {
+
+      throw new Error('EmptySortedList');
+
+    } 
+
+    const objetoMax = this.items[this.items.length -1]
+
+    return objetoMax;
+
+  }
+
+  min() {
+
+    if (this.items.length === 0) {
+
+      throw new Error('EmptySortedList');
+
+    } 
+
+    const objetoMin = this.items[0]
+
+    return objetoMin;
+
+  }
+
+  sum() {
+
+    let suma = 0 
+
+    for (let elemento of this.items) {
+      suma += elemento;
+    } 
+    return suma
+
+  }
+
+  avg() {
+
+    let suma = 0 
+
+    for (let elemento of this.items) {
+      suma += elemento;
+    } 
+
+    let average = suma / this.items.length;
+
+    if (this.items.length === 0) {
+
+      throw new Error('EmptySortedList');
+
+    } 
+
+    return average;
+
+  }
 }
 
 module.exports = SortedList;
