5373c05f063d76cf0f06eed34b5f0f279dcf3a62
Sebastian Montoya <seb246@gmail.com>
2020-12-12 13:21:16 -0500

    iteration 2

--- index.js
+++ index.js
@@ -4,7 +4,13 @@ class SortedList {
     this.length = this.items.length;
   }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a , b) => {
+      a - b;
+    });
+    this.length = this.items.length;
+  }
 
   get(pos) {}
 
