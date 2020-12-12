284308e45ba6267d56b57c403abb9a8f9deb937b
Kiko Quesada <franciscoquesadacabello89@gmail.com>
2020-12-12 12:31:50 +0100

    feat: iterations 1, 2 and 3

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
 class SortedList {
-  constructor() {}
 
-  add(item) {}
 
-  get(pos) {}
+  constructor(items, length) {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  max() {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a, b) => a - b);
+    this.length = this.items.length;
+  }
+
+  get(pos) {
+    for (let i = 0; i < this.items.length; i++){
+      this.pos = this.items[i];
+    }
+
+    if (this.pos > this.items.length) {
+      throw new Error('OutOfBounds');
+    }
+
+    return this.pos;
+  }
+
+  max() {
+    if(this.items.lenght === 0) {
+      throw new Error('EmptySortedList');
+    } else {
+      return Math.max(...items);
+    }
+  }
 
   min() {}
 
