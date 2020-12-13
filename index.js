a7296db1ff1d1b5bad8309315e1416d0f3cc5df0
RFeldman84 <rachel.feldman84@gmail.com>
2020-12-13 05:27:06 -0500

    finish iterations & bonuses

--- index.js
+++ index.js
@@ -1,17 +1,53 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = []
+    this.length = 0
+  }
 
-  add(item) {}
+  add(item) {
+      this.items.push(item)
+      this.items.sort((a,b)=> a - b)  
+      this.length = this.items.length 
+  }
+ 
+  get(pos) {
+    if(this.items.indexOf(pos) === -1){
+      throw new Error('OutOfBounds')
+    } else {
+      return this.items[pos]
+    }
+  
+  }
 
-  get(pos) {}
+  max() {
+    if(this.items.length === 0){
+      throw new Error('EmptySortedList')
+    } else {
+      this.items.sort((a,b)=> a - b)
+      return this.items[this.items.length-1]
+    }
+  }
 
-  max() {}
+  min() {
+    if(this.items.length === 0){
+      throw new Error('EmptySortedList')
+    } else {
+      this.items.sort((a,b)=> b - a)
+      return this.items[this.items.length-1]
+    }
+  }
 
-  min() {}
-
-  sum() {}
-
-  avg() {}
-}
+  sum() {
+    return this.items.length !== 0 ? this.items.reduce((v,i)=> v+i) : 0
+  } 
+    
+  avg() {
+    if(this.items.length === 0){
+      throw new Error('EmptySortedList')
+    } else {
+    return this.sum() / this.items.length
+    }
+  }
+};
 
 module.exports = SortedList;
