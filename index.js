190dad3e4da5f78b456fb0c86e46f62d9ebe0cf5
ida <so.kida@hotmail.com>
2020-12-13 22:22:01 -0500

    done

--- index.js
+++ index.js
@@ -1,17 +1,48 @@
 class SortedList {
-  constructor() {}
+  constructor(items, length) {
+    this.items= [];
+    this.length = this.items.length
+  }
 
-  add(item) {}
+  add(item) {
+      this.items.push(item);
+      this.length+=1;
+ return  this.items.sort((a,b)=>a-b);
+  }
 
-  get(pos) {}
+  get(pos) {
+this.items.indexOf(pos);
 
-  max() {}
+if (pos === 0 || pos < this.items.length) {
+  return this.items[pos];
+} else throw new Error("OutOfBounds");
+}
 
-  min() {}
+  
 
-  sum() {}
+  max() {
+  if (this.items.length > 0) {
+    return Math.max(...this.items);
+  } else throw new Error("EmptySortedList");
+  }
 
-  avg() {}
+
+  min() {
+  if (this.items.length > 0) {
+    return Math.min(...this.items);
+  } else throw new Error("EmptySortedList");
+  }
+
+  sum() {
+    return this.items.reduce((accum, currVal) => accum + currVal, 0);
+  }
+
+  avg() {
+    if (this.items.length > 0) {
+        return this.sum() / this.items.length;
+      } else throw new Error("EmptySortedList");
+   
+  }
 }
 
 module.exports = SortedList;
