3742f9b2ca8b4f2f75803e86222062b55022678f
Richard L <richard@11studio.co>
2020-12-15 20:53:23 +0100

    done

--- index.js
+++ index.js
@@ -1,17 +1,52 @@
 class SortedList {
-  constructor() {}
+  constructor() {
 
-  add(item) {}
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a, b) => a-b);
+    this.length = this.items.length;
+  }
 
-  max() {}
+  get(pos) {
+    if (pos < this.items.length) {
+      return this.items[pos];
+    } else {
+      throw new Error('OutOfBounds');
+    }
+  }
 
-  min() {}
+  max() {
+    if (this.length > 0) {
+      return Math.max(...this.items);
+    } else {
+      throw new Error('EmptySortedList');
+    } 
+  }
 
-  sum() {}
+  min() {
+    if (this.length > 0) {
+      return Math.min(...this.items);
+    } else {
+      throw new Error('EmptySortedList');
+    }
+  }
 
-  avg() {}
+  sum() {
+    return this.items.reduce((a,b) => a + b, 0);
+  }
+
+  avg() {
+    if (this.length > 0) {
+      return this.sum() / this.length;
+    } else {
+      throw new Error('EmptySortedList');
+    }
+    
+  }
 }
 
 module.exports = SortedList;
