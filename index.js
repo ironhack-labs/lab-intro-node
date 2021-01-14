069b9f9e9de646043125de1f5c4f42dd6f9e4139
Javier Repi <xavirepi@hotmail.com>
2021-01-14 21:04:33 +0100

    complete compulsory iterations - 1 to 5

--- index.js
+++ index.js
@@ -1,13 +1,35 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a, b) => a - b);
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (this.items[pos] === undefined) {
+      throw new Error('OutOfBounds');
+    }
+    return this.items[pos];
+  }
 
-  max() {}
+  max() {
+    if (this.items.length === 0) {
+      throw new Error('EmptySortedList');
+    }      
+    return Math.max(...this.items);
+  }
 
-  min() {}
+  min() {
+    if (this.items.length === 0) {
+      throw new Error('EmptySortedList');
+    }      
+    return Math.min(...this.items);
+  }
 
   sum() {}
 
