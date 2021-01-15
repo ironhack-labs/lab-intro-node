446bcf79873e75d4a3b0918bd95d83719c4f5c61
Thiarhes <injesusservice@hotmail.com>
2021-01-15 00:46:07 -0300

    WDPT OCT2020 Thiarhes

--- index.js
+++ index.js
@@ -1,17 +1,42 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a, b) => a - b);
+    this.length += 1;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (this.items[pos] > 0 && this.items[pos] <= this.items.length) {
+      return this.items[pos];
+    } else {
+      throw new Error('OutofBounds'); 
+    }
+  }
 
-  max() {}
+  max() {
+    if (this.items.length === 0) throw new Error('EmptySortedList');
+    return Math.max(...this.items);
+    }
+    
 
-  min() {}
+  min() {
+    if (this.items.length === 0) throw new Error('EmptySortedList');
+    return Math.min(...this.items);
+  }
 
-  sum() {}
+  sum() {
+    return this.items.reduce((a, b) => a + b, 0);
+  }
 
-  avg() {}
+  avg() {
+    if (this.items.length === 0) throw new Error('EmptySortedList');
+    return this.sum() / this.items.length;
+  }
 }
 
 module.exports = SortedList;
