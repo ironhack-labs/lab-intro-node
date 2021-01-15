760b888109ede69f9a2a9d5bdb0dc0769e874d4e
Volney Faustini <volney.faustini@gmail.com>
2021-01-14 22:00:48 -0300

    done

--- index.js
+++ index.js
@@ -1,17 +1,57 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+  this.items = [];
+  this.length = this.items.length;
 
-  add(item) {}
+  }
 
-  get(pos) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a, b) => a - b);
+    this.length = this.items.length;
+  }
 
-  max() {}
+  get(pos) {
+    if (pos >= this.items.length) {
+    throw new Error('OutOfBounds');
+    } else {
+      return this.items[pos];
+    }
 
-  min() {}
+  }
 
-  sum() {}
+  max() {
+  if (this.items.length == 0) {
+    throw new Error('EmptySortedList');
+  } else {
+    return Math.max(...this.items);
+  }
+}
 
-  avg() {}
+  min() {
+  if (this.items.length == 0) {
+    throw new Error('EmptySortedList');
+  } else {
+    return Math.min(...this.items);
+  }
+  }
+
+  sum() {
+    if (this.items.length == 0) {
+      return 0;
+    } else {
+      
+      return this.items.reduce( (accum, curr) => accum + curr );
+    }
+  }
+
+  avg() {
+    if (this.items.length == 0) {
+      throw new Error('EmptySortedList');
+    } else {
+      return this.sum() / this.items.length
+    }
+  }
 }
 
 module.exports = SortedList;
