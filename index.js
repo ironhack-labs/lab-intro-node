fbb3dc60fdfcad7321310183c4e016dcb6b5e708
henriquefb08 <fabiohenriqueftsousa@gmail.com>
2021-01-14 22:32:10 -0300

    done

--- index.js
+++ index.js
@@ -1,17 +1,63 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort(function (a, b) {
+      return a - b;
+    })
+    this.length = this.items.length
 
-  get(pos) {}
+  }
 
-  max() {}
+  get(pos) {
 
-  min() {}
+    if(pos < 0 || pos >= this.length) { 
+      throw new Error('OutOfBounds');
+    }
+    else { return this.items[pos]}
+  }
 
-  sum() {}
+  max() { 
+    if(this.length ==0) { 
+      throw new Error('EmptySortedList');
+    }
+    else {
+      return Math.max(...this.items)
+    }
+  }
 
-  avg() {}
+  min() { if(this.length ==0) { 
+    throw new Error('EmptySortedList');
+  }
+  else {
+    return Math.min(...this.items)
+  }
 }
 
+  sum() {
+    if (this.length == 0) { 
+      return 0; 
+    }
+    else { 
+      return this.items.reduce(function(acc, currValue){ 
+        return acc + currValue
+      },0)
+      
+    }
+  }
+
+  avg() {
+    if (this.length == 0){
+      throw new Error('EmptySortedList');
+    } else {
+      return this.items.reduce(function (accumulator, currentValue) {
+        return accumulator + currentValue
+      }, 0)/this.length
+    }
+  }
+}
 module.exports = SortedList;
