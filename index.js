745c3648653fb7b1792d71f29207a18bcc69a99f
luisga32 <luisga32@hotmail.com>
2021-01-16 14:20:08 +0100

    lab done

--- index.js
+++ index.js
@@ -1,17 +1,55 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items= [];
+    this.length= this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a,b) => {
+      return a - b;
+    });
+    this.length ++;
+  }
 
-  get(pos) {}
+  get(pos) {
 
-  max() {}
+    if (!this.items[pos]){
+      throw new Error('OutOfBounds');
+    }
+    return this.items[pos];
+  }
 
-  min() {}
+  max() {
+    if (this.length=== 0){
+      throw new Error('EmptySortedList');
+    }
+    return this.items[this.length -1];
+  }
 
-  sum() {}
+  min() {
+    if (this.length=== 0){
+      throw new Error('EmptySortedList');
+    }
+    return this.items[0];
+  }
 
-  avg() {}
+
+  sum() {
+    if (this.length=== 0){
+      return 0
+    }
+    return this.items.reduce((accumulator, currentValue) => accumulator + currentValue) 
+  }
+  
+
+  avg() {
+    if (this.length=== 0){
+      throw new Error('EmptySortedList');
+    }
+    return (this.sum()/this.length);
+  }
+  
 }
 
 module.exports = SortedList;
