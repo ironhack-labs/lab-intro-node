90b2e712df1f07cf2c508851232017e55fb3c1cd
Alicia Parra <alicia@redparra.com>
2021-01-16 11:22:18 +0100

    Done

--- index.js
+++ index.js
@@ -1,17 +1,55 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = []
+    this.length = this.items.length
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item)
+    this.items.sort(((a, b) => {return a-b}))
+    this.length = this.items.length
+  }
 
-  get(pos) {}
+  get(pos) {
+    if(this.items[pos] < this.items.length && pos >=0){
+      return this.items[pos]
+    }else{
+      throw new Error('OutOfBounds');
+    }
+  }
 
-  max() {}
+  max() {
+    if(this.items.length === 0){
+      throw new Error('EmptySortedList');
+    }else{
+      return this.items[this.items.length-1]
+    }
+  }
 
-  min() {}
+  min() {
+    if(this.items.length === 0){
+      throw new Error('EmptySortedList');
+    }else{
+      return this.items[0]
+    }
+  }
 
-  sum() {}
+  sum() {
+    if(this.items.length === 0){
+      return 0;
+    }else{
+      return this.items.reduce((acc,curr) => acc + curr,0)
+    }
+  }
 
-  avg() {}
+  avg() {
+    if(this.items.length === 0){
+      throw new Error('EmptySortedList');
+    }else{
+      const mySum= this.items.reduce((acc,curr) => acc + curr,0)
+      return mySum/this.items.length
+    }
+  }
 }
 
 module.exports = SortedList;
