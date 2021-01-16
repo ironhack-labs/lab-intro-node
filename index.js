f0e520f4068090813bb91342702a182e8725a943
karuvsentor <karuvsentor@gmail.com>
2021-01-16 13:47:51 +0100

    interation 5 done

--- index.js
+++ index.js
@@ -1,17 +1,41 @@
 class SortedList {
-  constructor() {}
+  constructor(items, length) {
+    this.items = []
+    this.length = this.items.length
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item)
+    this.items.sort((a, b) => a - b)
+    this.length = this.items.length
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (this.items[pos] === undefined) { throw new Error('OutOfBounds'); }
+    else {
+      return this.items[pos]
+    }
+  }
 
-  max() {}
+  max() {
+    if (this.length === 0) {
+      throw new Error('EmptySortedList');
+    } else {
+      return Math.max(...this.items)
+    }
+  }
 
-  min() {}
+  min() { 
+    if (this.items.length === 0) {
+      throw new Error('EmptySortedList');
+    } else {
+      return this.items.shift()
+    }
+  }
 
-  sum() {}
+  sum() { }
 
-  avg() {}
+  avg() { }
 }
 
 module.exports = SortedList;
