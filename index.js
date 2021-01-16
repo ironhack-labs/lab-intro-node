ec1891592b74cf5b020d2b95e28d4aa95cd6707f
Discordante <diegocerratos@gmail.com>
2021-01-16 11:43:51 +0100

    done

--- index.js
+++ index.js
@@ -1,17 +1,53 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = []
+    this.length = this.items.length
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item)
+    this.items.sort((a, b) => a - b)
+    this.length = this.items.length
+  }
 
-  get(pos) {}
+  get(pos) {
+    if(pos >= this.items.length){
+      throw new Error('OutOfBounds')
+    }
+    else{
+      return this.items[pos] 
+    }
 
-  max() {}
+  }
 
-  min() {}
+  max() {
+    if(this.items.length === 0){
+      throw new Error('EmptySortedList')
+    }
+    else{
+      return this.items[this.items.length - 1] 
+    }
+  }
 
-  sum() {}
+  min() {
+    if(this.items.length === 0){
+      throw new Error('EmptySortedList')
+    }
+    else{
+      return this.items[0] 
+    }
+  }
 
-  avg() {}
+  sum() {
+    return this.items.reduce((acc, value) => acc + value, 0);
+  }
+
+  avg() {
+    if(this.items.length === 0){
+      throw new Error('EmptySortedList')
+    }
+    return (this.items.reduce((acc, value) => acc + value, 0))/this.length;
+  }
 }
 
 module.exports = SortedList;
