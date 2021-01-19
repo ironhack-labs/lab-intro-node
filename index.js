ccf0bc2cd196a29017e31c67ab29656877a201db
Eduardo García <edugarciasb16@gmail.com>
2021-01-19 21:11:19 +0100

    lab done

--- index.js
+++ index.js
@@ -1,17 +1,56 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = []
+    this.length = this.items.length
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item)
+    this.items.sort((a,b) => a-b)
+    this.length = this.items.length
+  }
 
-  get(pos) {}
+  get(pos) {
 
-  max() {}
+    if (this.items[pos] === undefined){
+      throw new Error('OutOfBounds');
+    }else{
+      return this.items[pos]
+    }
 
-  min() {}
+  }
 
-  sum() {}
+  max() {
+    if(this.items.length === 0){
+      throw new Error('EmptySortedList');
+    }else{
+      return Math.max.apply(Math,this.items)
+    }
+  }
 
-  avg() {}
+  min() {
+    if (this.items.length === 0) {
+      throw new Error('EmptySortedList');
+    } else {
+      return Math.min.apply(Math, this.items)
+    }
+  }
+
+  sum() {
+    if(this.items.length === 0){
+      return 0
+    }
+
+    return this.items.reduce((a,c) => a + c, 0)
+  }
+
+  avg() {
+    if (this.items.length === 0) {
+      throw new Error('EmptySortedList')
+    }
+
+    return this.items.reduce((a, c) => a + c, 0) / this.items.length
+  }
 }
 
 module.exports = SortedList;
