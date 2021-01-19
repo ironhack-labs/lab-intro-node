69cc869ce47713ceee520565da1c94f0053ec91b
andrea morales <amoralesp88@gmail.com>
2021-01-18 21:52:09 -0600

    Three iteration done

--- index.js
+++ index.js
@@ -1,9 +1,32 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = 0;
+  }
 
-  add(item) {}
+  add(item) {
+    //primero función que ordene numeros
+    this.items.push(item);
+    this.items.sort();
 
-  get(pos) {}
+    //acutalizar la length
+    this.length = this.items.length;
+    
+
+  }
+
+  get(pos) {
+ // validar que el indice exista
+ 
+ if(this.items.length-1 >= pos){
+   return this.items[pos]
+ } else {throw new Error('OutOfBounds')};
+
+ // si es válido que devuelva el número del indice 
+
+ // si no es válido devuelva la excepción
+
+  }
 
   max() {}
 
