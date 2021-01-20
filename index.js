b6bd255d3cdfb3d8d980860a18b49edff287b623
andrea morales <amoralesp88@gmail.com>
2021-01-20 13:24:36 -0600

    one error to resolve

--- index.js
+++ index.js
@@ -11,30 +11,60 @@ class SortedList {
 
     //acutalizar la length
     this.length = this.items.length;
-    
+
 
   }
 
   get(pos) {
- // validar que el indice exista
- 
- if(this.items.length-1 >= pos){
-   return this.items[pos]
- } else {throw new Error('OutOfBounds')};
+    // validar que el indice exista
+    // si es válido que devuelva el número del indice 
 
- // si es válido que devuelva el número del indice 
+    // si no es válido devuelva la excepción
 
- // si no es válido devuelva la excepción
+    if (this.items.length - 1 >= pos) {
+      return this.items[pos]
+    } else { throw new Error('OutOfBounds') };
 
   }
 
-  max() {}
+  max() {
+    // devolver el valor más alto de la matriz.
+    // SortedList vacía, debe lanzar un error 
+    if (this.length === 0) {
+      throw new Error("EmptySortedList");
+    }
+    return Math.max(...this.items);
+ 
+  }
 
-  min() {}
 
-  sum() {}
+min() {
+  // devolver el valor más bajo de la matriz.
+    // SortedList vacía, debe lanzar un error 
+    if (this.length === 0) {
+      throw new Error("EmptySortedList");
+    }
+    return Math.min(...this.items);
 
-  avg() {}
+ }
+
+sum() {
+  // sumar
+  //retornar 0
+  if (this.length === 0) {
+    return 0;
+  }
+  return this.items.reduce((n1, n2) => n1 + n2);
+ }
+
+avg() {
+  // sacar porcentaje
+  //retornar error
+  if (this.length === 0) {
+    throw new Error("EmptySortedList");
+  }
+  return this.sum() / this.length;
+ }
 }
 
 module.exports = SortedList;
