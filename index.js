497ed787bf83d5bc811597a4a0cffaacdb5734d1
MCiappina <mciappops@gmail.com>
2021-02-01 18:46:51 -0300

    done

--- index.js
+++ index.js
@@ -1,17 +1,47 @@
 class SortedList {
-  constructor() {}
+    constructor() {
+        this.items = [];
+        this.length = "";
+    }
 
-  add(item) {}
+    add(item) {
+        this.items.push(item);
+        this.items = this.items.sort((a, b) => a - b);
+        this.length = this.items.length;
+    }
 
-  get(pos) {}
+    get(pos) {
+        if (pos < 0 || pos > this.length) {
+            throw new Error("OutOfBounds");
+        }
+        return this.items[pos];
+    }
 
-  max() {}
+    max() {
+        if (!this.length) {
+            throw new Error("EmptySortedList");
+        }
+        return Math.max(...this.items);
+    }
 
-  min() {}
+    min() {
+        if (!this.length) {
+            throw new Error("EmptySortedList");
+        }
+        return Math.min(...this.items);
+    }
 
-  sum() {}
+    sum() {
+        return this.items.reduce((a, b) => a + b, 0);
+    }
 
-  avg() {}
+    avg() {
+        if (!this.length) {
+            throw new Error("EmptySortedList");
+        }
+        console.log(this.items.reduce((a, b) => a + b) % this.length);
+        return this.items.reduce((a, b) => a + b) / this.length;
+    }
 }
 
 module.exports = SortedList;
