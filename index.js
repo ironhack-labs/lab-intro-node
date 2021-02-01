47a76109b7694d068b80a3db0d0976427b540aac
Javi Sastre <javisastrecal@gmail.com>
2021-02-01 13:13:47 +0100

    finished lab

--- index.js
+++ index.js
@@ -1,17 +1,65 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a, b) => a - b);
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos > this.length) {
+      throw new Error("OutOfBounds");
+    } else {
+      return this.items[pos];
+    }
+  }
 
-  max() {}
+  max() {
+    if (this.length === 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      let maxNum = this.items[0];
+      this.items.forEach((element) => {
+        if (element > maxNum) {
+          maxNum = element;
+        }
+      });
+      return maxNum;
+    }
+  }
 
-  min() {}
+  min() {
+    if (this.length === 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      let minNum = this.items[0];
+      this.items.forEach((element) => {
+        if (element < minNum) {
+          minNum = element;
+        }
+      });
+      return minNum;
+    }
+  }
 
-  sum() {}
+  sum() {
+    const total = this.items.reduce((accumulator, element) => {
+      return accumulator + element;
+    }, 0);
+    return total;
+  }
 
-  avg() {}
+  avg() {
+    if (this.length === 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      return this.sum() / this.length;
+    }
+  }
 }
 
 module.exports = SortedList;
