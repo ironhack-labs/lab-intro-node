cd82b54aed6db74e2738bbf861a69b63cac202e1
fmuniente <frd.muniente@gmail.com>
2021-02-01 12:12:26 +0100

    done

--- index.js
+++ index.js
@@ -1,17 +1,55 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a, b) => a - b);
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos > this.items.length) {
+      throw new Error("OutOfBounds");
+    } else {
+      return this.items[pos];
+    }
+  }
 
-  max() {}
+  max() {
+    if (this.items.length === 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      return this.items[this.items.length - 1];
+    }
+  }
 
-  min() {}
+  min() {
+    if (this.items.length === 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      return this.items[0];
+    }
+  }
 
-  sum() {}
+  sum() {
+    const sum = this.items.reduce(function (total, element) {
+      const updatedTotal = total + element;
+      return updatedTotal;
+    }, 0);
+    return sum;
+  }
 
-  avg() {}
+  avg() {
+    if (this.items.length === 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      const avg = this.sum() / this.items.length;
+      return avg;
+    }
+  }
 }
 
 module.exports = SortedList;
