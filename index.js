81e237aba1cfde0375d70051914499db0ffef33f
RudaF <floresta.ruda@gmail.com>
2021-02-01 17:54:09 -0300

    Finished lab

--- index.js
+++ index.js
@@ -1,17 +1,49 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = 0;
+  }
 
-  add(item) {}
+  add(item) {
+    let i = 0;
+    while (i < this.length && item > this.items[i]) {
+      i++;
+    }
+    this.items.splice(i, 0, item);
+    this.length++;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos > 0 && pos < this.length) {
+      return this.items[pos];
+    }
+    throw new Error("OutOfBounds");
+  }
 
-  max() {}
+  max() {
+    if (this.length > 0) {
+      return this.items[this.length - 1];
+    }
+    throw new Error("EmptySortedList");
+  }
 
-  min() {}
+  min() {
+    if (this.length > 0) {
+      return this.items[0];
+    }
+    throw new Error("EmptySortedList");
+  }
 
-  sum() {}
+  sum() {
+    return this.items.reduce((a, b) => a + b, 0);
+  }
 
-  avg() {}
+  avg() {
+    if (this.length > 0) {
+      return this.sum() / this.length;
+    }
+    throw new Error("EmptySortedList");
+  }
 }
 
 module.exports = SortedList;
