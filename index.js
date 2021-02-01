8085f46558f1e83de57b922d23ad49a589e15d2a
Anna Mazurek <anna-mazurek@hotmail.com>
2021-02-01 12:52:16 +0100

    lab almost finished

--- index.js
+++ index.js
@@ -1,13 +1,31 @@
 class SortedList {
-  constructor() {}
+  constructor(items) {
+    this.items = [];
+    this.length = 0;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.length++;
+    const sorted = this.items.sort(function (a, b) {
+      return a - b;
+    });
 
-  get(pos) {}
+    return sorted;
+  }
 
-  max() {}
+  get(pos) {
+    if (pos > this.items.length - 1) throw new Error("OutOfBounds");
+    return this.items[pos];
+  }
 
-  min() {}
+  max() {
+    return Math.max(...this.items);
+  }
+
+  min() {
+    return Math.min(...this.items);
+  }
 
   sum() {}
 
@@ -15,3 +33,5 @@ class SortedList {
 }
 
 module.exports = SortedList;
+
+
