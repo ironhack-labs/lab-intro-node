cde5282a0678fd87e14f0b103b909eb9b8b17618
Viviana Pulido <vivianapulido_dg@outlook.com>
2021-02-02 16:58:59 -0600

    listo

--- index.js
+++ index.js
@@ -1,17 +1,52 @@
+
 class SortedList {
-  constructor() {}
+  constructor(items, length) {
+    this.items= [];
+    this.length= this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item)
+    this.items.sort((a, b) => a - b);
+    this.length= this.items.length;
+    
+  }
 
-  get(pos) {}
+  get(pos) {
+    if(pos <= this.items.length){
+      return this.items[pos]
+  } else{
+    throw new Error('OutOfBounds');
+  }}
 
-  max() {}
+  max() {
+    if(this.items==0){
+      throw new Error('EmptySortedList');
+  } else {
+    return Math.max.apply(null, this.items)
+  }}
 
-  min() {}
+  min() {
+    if(this.items==0){
+      throw new Error('EmptySortedList');
+  } else {
+    return Math.min.apply(null, this.items)
+  }
+  }
 
-  sum() {}
+  sum() {
+    return this.items.reduce((acumulador, valorActual)=> acumulador + valorActual, 0)
+  }
 
-  avg() {}
+  avg() {
+    if(this.items.length==0){
+      throw new Error('EmptySortedList');
+    }else{
+      return this.items.reduce((acumulador, valorActual)=> acumulador + valorActual, 0)/this.items.length
+    }
+    
+  }
 }
 
 module.exports = SortedList;
+
