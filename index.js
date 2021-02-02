a6a3f3f2724561d5a0c5f690e623f923dd4d9b3e
Romulo Hipolito <hipolito.romulo12@gmail.com>
2021-02-02 00:25:48 -0300

    finished

--- index.js
+++ index.js
@@ -1,15 +1,39 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = "";
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.item = this.items.sort((a, b) => a - b);
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos < 0 || pos > this.length ){
+      throw new Error('OutOfBounds');
+    }
+    return this.items[pos];
+  }
 
-  max() {}
+  max() {
+    if (!this.length){
+      throw new Error('EmptySortedList');
+    }
+    return Math.max(...this.items);
+  }
 
-  min() {}
+  min() {
+    if (!this.length){
+      throw new Error('EmptySortedList');
+    }
+    return Math.min(...this.items);
+  }
 
-  sum() {}
+  sum() {
+    return this.items.reduce((a,b) =>  a + b, 0);
+  }
 
   avg() {}
 }
