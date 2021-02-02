ec94e246e46d19c64e73ced0749b282c96051234
Diego <diego.arronizt@gmail.com>
2021-02-02 16:38:57 -0500

    Completado

--- index.js
+++ index.js
@@ -1,17 +1,63 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = []
+    this.length = this.items.length
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item)
+    this.items.sort((a,b) => {
+      return a-b
+    })
+    
+    this.length = this.items.length
+     
+  }
 
-  get(pos) {}
+  get(pos) {
+      if(pos > this.items.length -1 || pos < 0) {
+          throw new Error('OutOfBounds')
+      } else {
+       return this.items[pos]
+        }
+}
 
-  max() {}
+  max() {
+    if(!this.items.length) {
+      throw new Error('EmptySortedList')
+    } else {
+     return this.items.reduce((a,b) => {
+       return Math.max(a,b)
+     })
+    }
+  }
 
-  min() {}
+  min() {
+    if(!this.items.length) {
+      throw new Error('EmptySortedList')
+    } else {
+      return this.items.reduce((a,b) => {
+        return Math.min(a,b)
+      })
+     }
+   }
+    
+  sum() {
+    if(!this.items.length) {
+    return 0
+  } else {
+  return this.items.reduce((a,b)=> {
+    return a+b
+  },0)
+  }}
 
-  sum() {}
-
-  avg() {}
+  avg() {
+    if(!this.items.length) {
+      throw new Error('EmptySortedList')
+    } else {
+      return (this.sum()/this.length)
+  }
+}
 }
 
 module.exports = SortedList;
