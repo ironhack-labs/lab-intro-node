e6cfdacf6470f605f8b39c85c57448c7386767e7
Mafe Bracho <mafe.bracho@gmail.com>
2021-02-02 15:17:11 +0100

    do iterations 1 to 3

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = '';
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort(function(a,b){return a-b});
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos < 0 || pos > this.items.legth) {
+      throw new Error('OutOfBounds');
+    }
+    return this.items[pos];
+  }
 
-  max() {}
+  max() {
+    if (this.items.length < 1) {
+      throw new Error('EmptySortedList');
+    }
+    return Math.max(...this.items);
+  }
 
   min() {}
 
