6652bb6dfa0a70a7458636d21fa367449ce78223
Patrick Lehmann <ptrcklehmann@gmail.com>
2021-02-02 21:23:41 +0100

    done

--- index.js
+++ index.js
@@ -1,17 +1,38 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = []
+    this.length = 0
+  }
+  add(item) {
+    this.items.push[item]
+    this.items.sort((x, y) => x-y)
+    this.length++
+  }
+    
+  get(pos) {
+    if(pos >= this.length || pos < 0) throw new Error('OutOfBounds')
+    return this.items[pos]
+  }
 
-  add(item) {}
+  max() {
+    if (this.length === 0) throw new Error('EmptySortedList')
+    return this.items[pos]
+  }
 
-  get(pos) {}
+  min() {
+    if (this.length === 0) throw new Error('EmptySortedList');
+    return this.items.reduce((a, b) => Math.min(a, b));
+  }
 
-  max() {}
+  sum() {
+    if (this.length === 0) return 0;
+    return this.items.reduce((acc, el) => acc + el, 0);
+  }
 
-  min() {}
-
-  sum() {}
-
-  avg() {}
+  avg() {
+    if (this.length === 0) throw new Error('EmptySortedList');
+    return this.sum()/this.length;
+  }
 }
 
 module.exports = SortedList;
