ad94825f478827cdcb690d5aaa8b3bb9650e2572
EduLp99 <edulp99@gmail.com>
2021-02-02 01:38:18 -0300

    done

--- index.js
+++ index.js
@@ -1,17 +1,55 @@
 class SortedList {
-  constructor() {}
+  constructor(items, length) {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a, b) => a - b);
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos > this.items.length) {
+      throw new Error("OutOfBounds")
+    } else {
+      return this.items[pos]
+    }
+  }
 
-  max() {}
+  max() {
+    if (this.items.length <= 0) {
+      throw new Error("EmptySortedList");
+    }
 
-  min() {}
+    return Math.max(...this.items);
+  }
 
-  sum() {}
+  min() {
+    if (this.items.length <= 0) {
+      throw new Error("EmptySortedList");
+    }
 
-  avg() {}
+    return Math.min(...this.items);
+  }
+
+  sum() {
+    if (this.items.length <= 0) {
+      return 0
+    }
+    return this.items.reduce((acc, val) => acc + val);
+  }
+
+  avg() {
+    if ( this.items.length <= 0) {
+      throw new Error("EmptySortedList");
+    }
+
+    return this.sum() / this.items.length;
+  }
 }
+
+
 
 module.exports = SortedList;
