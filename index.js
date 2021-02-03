305445fcd26089a9eb40197cfdfe63cda4ea60b4
Adam Tomczyk <markus.adam.tomczyk@gmail.com>
2021-02-03 10:25:53 +0100

    Adam Tomczyk

--- index.js
+++ index.js
@@ -1,17 +1,63 @@
 class SortedList {
-  constructor() {}
+  constructor(items,length) {
+    this.items  = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item)
+    this.items.sort(function(a,b){
+        return a-b;
+    });
+    this.length =  this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if(this.items.length <= pos){
+      throw new Error('OutOfBounds');
+    } else {
+      return this.items[pos];
+    }
+ }
 
-  max() {}
+  max() {
+    if(items.length === 0) {
+      throw new Error('EmptySortedList');
+    } else {
+      return this.items[this.items.length-1];
+    }
 
-  min() {}
+  }
 
-  sum() {}
+  min() {
+    if(items.length === 0) {
+      throw new Error('EmptySortedList');
+    } else {
+      return this.items[0];
+    }
+  }
 
-  avg() {}
+  sum() {
+    this.items.reduce((acc,currentVal) => {
+        return acc + currentVal;
+    },0)
+  }
+
+  avg() {
+    const allSum = this.sum();
+    return allSum / this.items.length;
+    
+  }
 }
+
+
+
+
+
+
+
+
+
+
 
 module.exports = SortedList;
