cc6eeee468a60a7262d65d73ccbdc677f0960c2e
Diego Barrera <di_barrera@hotmail.com>
2021-02-03 00:13:41 -0600

    done

--- index.js
+++ index.js
@@ -1,17 +1,48 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = []
+    this.length = this.items.length
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item)
+    this.length = this.items.length
+    this.items.sort((a, b) => a - b)
+  }
 
-  get(pos) {}
+  get(pos) {
+    this.items[pos]
+    if (this.length >= pos) {
+      return this.items[pos]
+    } else {
+      throw new Error('OutOfBounds')
+    }
+  }
 
-  max() {}
+  max() {
+    if (!this.items.length) {
+      throw new Error("EmptySortedList")
+    }
+    return this.items[this.length - 1]
+  }
 
-  min() {}
+  min() {
+    if (!this.items.length) {
+      throw new Error('EmptySortedList')
+    }
+    return this.items[0]
+  }
 
-  sum() {}
+  sum() {
+    return this.items.reduce((acc, cv) => acc + cv, 0)
+  }
 
-  avg() {}
+  avg() {
+    if (!this.items.length) {
+      throw new Error('EmptySortedList')
+    }
+    return (this.sum() / this.length)
+  }
 }
 
 module.exports = SortedList;
