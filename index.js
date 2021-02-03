9a3e2aa71c27a7faccd3ea4175dd4b7cd31f57ba
Rob TC <roberto.taboada@gmail.com>
2021-02-03 09:29:12 -0600

    Hasta el Max

--- index.js
+++ index.js
@@ -1,11 +1,42 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.unshift(item);
+    this.items.sort((a,b)=>a-b));
+    this.length=this.items.length;
+  }
+    
 
-  get(pos) {}
+  get(pos) {
+    
+    if (this.items.length>pos){
+      let getElement = this.items[pos]
+      return getElement;
+    } else {
+      throw new Error('OutOfBounds');
+    }
+    
 
-  max() {}
+  }
+
+  max() {
+    if (this.items.length===0){
+      throw new Error('EmptySortedList');
+    } else {
+      return Math.max(...this.items)
+    }
+
+    /*The max() method should return the highest value of the array.
+
+In case you have an empty SortedList, you must throw an error with the message "EmptySortedList". For this, you can use:
+
+throw new Error('EmptySortedList');
+    */ 
+  }
 
   min() {}
 
