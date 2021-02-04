82480623619b0e9d782156910281381a6b9b68b3
Rob TC <roberto.taboada@gmail.com>
2021-02-03 18:40:44 -0600

    terminado, pero con errores aparentes

--- index.js
+++ index.js
@@ -6,11 +6,10 @@ class SortedList {
 
   add(item) {
     this.items.unshift(item);
-    this.items.sort((a,b)=>a-b));
+    this.items.sort((a,b)=>a-b);
     this.length=this.items.length;
   }
     
-
   get(pos) {
     
     if (this.items.length>pos){
@@ -27,22 +26,41 @@ class SortedList {
     if (this.items.length===0){
       throw new Error('EmptySortedList');
     } else {
-      return Math.max(...this.items)
-    }
-
-    /*The max() method should return the highest value of the array.
-
-In case you have an empty SortedList, you must throw an error with the message "EmptySortedList". For this, you can use:
-
-throw new Error('EmptySortedList');
-    */ 
+      this.items.sort((a,b)=>a-b)
+      return this.items[this.length -1]
+  }
   }
 
-  min() {}
+  min() {
+    if (this.items.length===0){
+      throw new Error('EmptySortedList');
+    } else {
+      this.items.sort((a,b)=>a-b)
+      return this.items[0]
+    }
+  }
 
-  sum() {}
+  sum() {
+    if (items.length===0){
+      return 0 ;
+    } else {
+       this.items.reduce(function(acc,cv){
+        return acc + cv})
+       } 
+  }
 
-  avg() {}
+  
+
+  avg() {
+    if (items.length===0){
+      throw new Error('EmptySortedList');
+    } else {
+      let sumNumbers = this.items.reduce(function(acc,cv){
+        return acc + cv})}
+        let avgNumbers = sumNumbers/this.items.length
+        return avgNumbers
+    }
+  }
 }
 
 module.exports = SortedList;
