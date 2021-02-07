ef0c0709d796f05e67fe3d17ddf1cadd3602d177
oezge sebisteri <sebisteri@outlook.com>
2021-02-07 14:32:01 +0100

    All Done

--- index.js
+++ index.js
@@ -1,17 +1,80 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = 0;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.length += this.items.length;
+    this.items.sort((a, b) => a - b);
 
-  get(pos) {}
+  };
 
-  max() {}
+  get(pos) {
+  
+    let value = this.items[pos];
+    if (value) {
+      return value
+    } else if (pos > this.items.length) {
+      throw new Error('OutOfBounds');
+    }
+    
+   /*
+    if (pos < items.length) {
+      return this.items[pos]
+    } else if (pos >= this.items.length) {
+      throw new Error('OutOfBounds');
+    }
+    */
+  }
 
-  min() {}
+  max() {
+    let itemsCopy = [...this.items]
+    if (!(itemsCopy.length === 0)) {
+      return itemsCopy.pop()
+    } else if (itemsCopy.length === 0) {
+      throw new Error('EmptySortedList');
+    }
+  }
 
-  sum() {}
+  min() {
+    let itemsCopy = [...this.items]
+    if (!(itemsCopy.length === 0)) {
+      let returnValue = itemsCopy.splice(0, 1)
+      return returnValue[0]
+    } else if (itemsCopy.length === 0) {
+      throw new Error('EmptySortedList');
+    }
+  }
 
-  avg() {}
+  sum() {
+    let itemsCopy = [...this.items];
+    let reduced;
+    if (itemsCopy.length > 0) {
+      reduced = itemsCopy.reduce((acc, currentVal) => {
+        return acc + currentVal;
+      });
+    } else if (itemsCopy = []) {
+      reduced = 0;
+    }
+    return reduced;
+  }
+
+  avg() {
+    let itemsCopy = [...this.items];
+    let reduced;
+    let avg;
+    if (itemsCopy.length > 0) {
+      reduced = itemsCopy.reduce((acc, currentVal) => {
+      return acc + currentVal;
+    });
+    } else if (itemsCopy.length === 0) {
+      throw new Error('EmptySortedList');
+    }
+    avg = reduced / itemsCopy.length;
+    return Number(avg.toFixed(2))
+  }
 }
 
 module.exports = SortedList;
