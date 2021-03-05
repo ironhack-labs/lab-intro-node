620ece76546191e3722f71b68c09b528a6c258e9
Paula Nicioli <paulanicioli@gmail.com>
2021-03-04 22:36:58 -0300

    Lab completed

--- index.js
+++ index.js
@@ -1,17 +1,73 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = 0;
+  }
 
-  add(item) {}
+  add(item) {
+    let isInserted = false;
+    for (let i = 0; i < this.length; i++) {
+      if (item <= this.items[i]) {
+        this.items.splice(i, 1, item, this.items[i]);
+        isInserted = true;
+        break;
+      }
+    }
+    if (isInserted === false) {
+      this.items.push(item);
+    }
+    this.length++;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos >= this.length) {
+      throw new Error("OutofBounds");
+    } else {
+      return this.items[pos];
+    }
+  }
 
-  max() {}
+  max() {
+    if (this.length === 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      return this.items[this.length - 1];
+    }
+  }
 
-  min() {}
+  min() {
+    if (this.length === 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      return this.items[0];
+    }
+  }
 
-  sum() {}
+  sum() {
+    return this.length === 0
+      ? 0
+      : this.items.reduce((acc, item) => {
+          return (acc += item);
+        });
+  }
 
-  avg() {}
+  avg() {
+    if (this.length === 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      return this.sum() / this.length;
+    }
+  }
 }
 
 module.exports = SortedList;
+
+// const testList = new SortedList();
+
+// testList.add(10);
+
+// testList.add(30);
+
+// testList.add(25);
+
+// console.log(testList);
