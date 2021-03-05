4e0207aebcef0fd838fde09b5c4384b19b5183b4
mmanso <mayara.manso@mercadolivre.com>
2021-03-04 22:51:11 -0300

    done

--- index.js
+++ index.js
@@ -1,17 +1,48 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = 0;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a, b) => a - b);
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (!this.items[pos]) throw new Error('OutOfBounds')
 
-  max() {}
+    return this.items[pos]
+  }
 
-  min() {}
+  max() {
+    if (this.length === 0) throw new Error('EmptySortedList')
+    const pos = this.length - 1;
+    return this.items[pos]
+  }
 
-  sum() {}
+  min() {
+    if (this.length === 0) throw new Error('EmptySortedList')
+    return this.items[0]
+  }
 
-  avg() {}
+  sum() {
+    if (this.length === 0) return this.length;
+
+    const sum = this.items.reduce((acc, item) => {
+      return acc += item
+    })
+    return sum;
+  }
+
+  avg() {
+    if (this.length === 0) throw new Error('EmptySortedList');
+    const sum = this.items.reduce((acc, item) => {
+      return acc += item
+    })
+    return sum / this.length;
+  }
 }
 
 module.exports = SortedList;
