bd4e025160063b6325996bb67fbc0e42624691d5
Tsherculano <tico.herculano@gmail.com>
2021-03-06 07:16:14 -0300

    lab done

--- index.js
+++ index.js
@@ -1,17 +1,57 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.sortItems();
+    this.currencyLength();
+  }
 
-  get(pos) {}
+  sortItems() {
+    this.items.sort((a,b)=>a - b);
+  }
 
-  max() {}
+  currencyLength() {
+    this.length = this.items.length;
+  }
 
-  min() {}
+  get(pos) {
+    if(this.items[pos]) {
+      return pos;
+    }
+    throw new Error('OutOfBounds');
+  }
 
-  sum() {}
+  zeroError() {
+    if(this.length === 0) {
+      throw new Error('EmptySortedList');
+    }
+  }
 
-  avg() {}
+  max() {
+    this.zeroError();
+    return this.items[this.length - 1];
+  }
+
+  min() {
+    this.zeroError();
+    return this.items[0];
+  }
+
+  sum() {
+    return this.items.reduce((acc, number)=>{
+      return acc + number;
+    }, 0)
+  }
+
+  avg() {
+    this.zeroError();
+
+    return this.sum() / this.length;
+  }
 }
 
 module.exports = SortedList;
