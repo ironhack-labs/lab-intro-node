40de7e5336481c8d7c8547997583db5001f71322
Derek <shouldergiant@icloud.com>
2021-03-06 16:14:52 -0500

    done

--- index.js
+++ index.js
@@ -1,17 +1,38 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.length += 1;
+    this.items.sort((a,b) => a - b);
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos < 0 || pos >= this.length) throw new Error('OutOfBounds');
+    return this.items[pos];
+  }
 
-  max() {}
+  max() {
+    if (this.length === 0) throw new Error('EmptySortedList');
+    return this.items[this.length - 1];
+  }
 
-  min() {}
+  min() {
+    if (this.length === 0) throw new Error('EmptySortedList');
+    return this.items[0];
+  }
 
-  sum() {}
+  sum() {
+    return this.length === 0 ? 0 : this.items.reduce((sum, e) => sum + e, 0);
+  }
 
-  avg() {}
+  avg() {
+    if (this.length === 0) throw new Error('EmptySortedList');
+    return Math.floor(this.sum() / this.length);
+  }
 }
 
 module.exports = SortedList;
