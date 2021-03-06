d2f3096d57591ce753fd604a0578e3f83376f7ae
cintiathieme <cintiathieme@gmail.com>
2021-03-06 09:54:09 -0300

    done

--- index.js
+++ index.js
@@ -1,17 +1,51 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort(function(a,b){
+      return a - b
+    });
+    this.length = this.items.length;   
+  }
+  
+  get(pos) {
+    if (pos > this.items.length){
+     throw new Error('OutOfBounds')
+    }
+    return this.items[pos]    
+  }
 
-  get(pos) {}
+  max() {
+    if (this.items.length === 0){
+      throw new Error('EmptySortedList')
+     }
+     return this.items[this.length -1]
+  }
 
-  max() {}
+  min() {
+    if (this.items.length === 0){
+      throw new Error('EmptySortedList')
+     }
+    return this.items[0]
+  }
 
-  min() {}
+  sum() {
+    if (this.items.length === 0){
+      return 0
+     }
+    return this.items.reduce((acc,number) => acc + number)
+  }
 
-  sum() {}
-
-  avg() {}
+  avg() {
+    if (this.items.length === 0){
+      throw new Error('EmptySortedList')
+     }
+    return this.sum()/this.items.length;
+  }
 }
 
 module.exports = SortedList;
