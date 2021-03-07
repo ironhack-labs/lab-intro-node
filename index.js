78f4dc84b6977e2447ec1c92e65baebde0b1d9b2
Luis Henestrosa <ljoaquincuenca@gmail.com>
2021-03-07 16:27:23 +0100

    3 iterations done

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a, b) => a - b);
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (this.length > pos && pos >= 0) {
+      return this.items[pos];
+    }
+    throw new Error("OutOfBounds");
+  }
 
   max() {}
 
