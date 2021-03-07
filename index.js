6e4b12c96263e4928477fdf6490a141d4c3fd109
Frankie Eckersley-Carr <frankie.eckersley-carr@outlook.com>
2021-03-07 14:59:13 +0100

    complete iterration 1 and 2

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
 class SortedList {
-  constructor() {}
+  //Iteration 1: constructor()
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    if (this.length === 0 || item > this.items[this.length - 1]) {
+      this.items.push(item);
+    } else if (item < this.items[0]) {
+      this.items.unshift(item);
+    } else {
+      for (let i = 0; i < this.items.length; i++) {
+        if (item > this.item[i] && item < this.item[i + 1]) {
+          this.items.splice(i + 1, 0, item);
+        }
+      }
+    }
+    this.length = this.items.length;
+  }
 
   get(pos) {}
 
--- test/index.spec.js
+++ test/index.spec.js
@@ -8,8 +8,8 @@ describe("SortedList", () => {
   describe("Constructor", () => {
     it("should have items and length properties", () => {
       const list = new SortedList();
-      assert.equal(list instanceof SortedList, true);
-      assert.equal(list.length, 0);
+      assert.strictEqual(list instanceof SortedList, true);
+      assert.strictEqual(list.length, 0);
       assert(Array.isArray(list.items));
     });
   });
@@ -20,15 +20,15 @@ describe("SortedList", () => {
       list = new SortedList();
     });
     it("should add a single value to SortedList", () => {
-      assert.equal(list.length, 0);
+      assert.strictEqual(list.length, 0);
       list.add(1);
-      assert.equal(list.length, 1);
+      assert.strictEqual(list.length, 1);
     });
     it("should add a third value to SortedList", () => {
       list.add(30);
       list.add(20);
       list.add(10);
-      assert.equal(list.items.length, 3);
+      assert.strictEqual(list.items.length, 3);
     });
 
     it("should add a value while keeping the list sorted", () => {
@@ -36,10 +36,10 @@ describe("SortedList", () => {
       list.add(20);
       list.add(100);
       list.add(10);
-      assert.equal(list.items[0], 10);
-      assert.equal(list.items[1], 20);
-      assert.equal(list.items[2], 30);
-      assert.equal(list.items[3], 100);
+      assert.strictEqual(list.items[0], 10);
+      assert.strictEqual(list.items[1], 20);
+      assert.strictEqual(list.items[2], 30);
+      assert.strictEqual(list.items[3], 100);
     });
   });
 
@@ -63,7 +63,7 @@ describe("SortedList", () => {
     });
 
     it("should return the element in that position", () => {
-      assert.equal(list.get(2), 2);
+      assert.strictEqual(list.get(2), 2);
     });
   });
 
@@ -86,7 +86,7 @@ describe("SortedList", () => {
     it("should return the max (highest) value in the list", () => {
       list.add(10);
       list.add(20);
-      assert.equal(list.max(), 20);
+      assert.strictEqual(list.max(), 20);
     });
   });
 
@@ -109,7 +109,7 @@ describe("SortedList", () => {
     it("should return the min (lowest) value in the list", () => {
       list.add(10);
       list.add(20);
-      assert.equal(list.min(), 10);
+      assert.strictEqual(list.min(), 10);
     });
   });
 
@@ -123,11 +123,11 @@ describe("SortedList", () => {
       list.add(1);
       list.add(2);
       list.add(3);
-      assert.equal(list.sum(), 6);
+      assert.strictEqual(list.sum(), 6);
     });
 
     it("should return 0 for an empty sorted list", () => {
-      assert.equal(list.sum(0), 0);
+      assert.strictEqual(list.sum(0), 0);
     });
   });
 
@@ -151,7 +151,7 @@ describe("SortedList", () => {
       list.add(1);
       list.add(2);
       list.add(3);
-      assert.equal(list.avg(), 2);
+      assert.strictEqual(list.avg(), 2);
     });
   });
 });
