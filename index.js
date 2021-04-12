51d59c5e20018838ac09a0aedb2dadc6c9f6a068
Raquel Rodriguez <srtademaciel@gmail.com>
2021-04-12 16:49:04 +0200

    iteration 4

--- index.js
+++ index.js
@@ -1,15 +1,43 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = []
+    this.length = this.items.length
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item)
+    this.items.sort((a, b) => a - b)
+    this.length = this.items.length
+  }
 
-  get(pos) {}
+  get(pos) {
+   if(pos > this.items.length){
+     throw new Error('OutOfBounds');
+   } else {
+     return this.items[pos]
+   }
+  }
 
-  max() {}
+  max() {
+    if(this.items.length === 0){
+      throw new Error('EmptySortedList');
+    } else {
+      let highestValue = this.length -1
+      return this.items[highestValue]
+    }
+  }
 
-  min() {}
+  min() {
+    if(this.items.length === 0){
+      throw new Error('EmptySortedList');
+    } else {
+      return this.items[0]
+    }
+  }
 
-  sum() {}
+  sum() {
+    
+  }
 
   avg() {}
 }
