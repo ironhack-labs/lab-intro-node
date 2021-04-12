3e8ecc18fab7dcaa7a82c47a0f114e21d8b277cf
Lorgio Roda <lorgioroda@Lorgios-MacBook-Air.local>
2021-04-12 05:58:10 -0400

    1,2,3,4,5 DONE

--- index.js
+++ index.js
@@ -1,13 +1,35 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a,b) => a-b);
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if(pos > this.length -1){
+      throw new Error('OutOfBounds')
+    }
+    return this.items[pos]
+  }
 
-  max() {}
+  max() {
+    if(this.length === 0){
+      throw new Error('EmptySortedList')
+    }
+    return this.items[this.length -1]
+  }
 
-  min() {}
+  min() {
+    if(this.length === 0){
+      throw new Error('EmptySortedList')
+    }
+    return this.items[0]
+  }
 
   sum() {}
 
@@ -15,3 +37,5 @@ class SortedList {
 }
 
 module.exports = SortedList;
+
+
