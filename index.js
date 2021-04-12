effb06d0bea104e6bb7f9149d4fc325bedcdae7b
Sisco Falcó <siscofalcop@gmail.com>
2021-04-12 11:52:59 +0200

    iterations and bonus done

--- index.js
+++ index.js
@@ -1,17 +1,61 @@
 class SortedList {
-  constructor() {}
+  constructor(items, length) {
+    this.items = [],
+    this.length = this.items.length
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.length = this.items.length;
+    return this.items.sort((a, b) => {
+      return a - b;
+    });
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos > this.items.length){
+      throw new Error('OutOfBounds');
+    } else {
+    return this.items.indexOf(pos);
+    }
+  }
 
-  max() {}
+  max() {
+    if(this.items.length === 0){
+      throw new Error('EmptySortedList');
+    } else {
+      let orderedArr = this.items.sort((a,b) => {
+        return b - a;
+      });
+      return orderedArr[0];
+    }
+  }
 
-  min() {}
+  min() {
+    if(this.items.length === 0){
+      throw new Error('EmptySortedList');
+    } else {
+      let orderedArr = this.items.sort((a,b) => {
+        return a - b;
+      });
+      return orderedArr[0];
+    }
+  }
 
-  sum() {}
+  sum() {
+    let sumArr = this.items.reduce((acc, current) => {
+      return acc + current;
+    }, 0);
+    return sumArr;
+  }
 
-  avg() {}
+  avg() {
+      if(this.items.length === 0){
+        throw new Error('EmptySortedList');
+      } else {
+      return this.sum() / this.items.length;
+    }
+  }
 }
 
 module.exports = SortedList;
