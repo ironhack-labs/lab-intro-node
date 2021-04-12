1be4fa3377c7afe75625d64de8b6268f88aa521b
Jeroen van Bart <jeroenvanbart83@gmail.com>
2021-04-12 15:31:18 +0200

    done

--- index.js
+++ index.js
@@ -1,17 +1,67 @@
+
+
 class SortedList {
-  constructor() {}
+  constructor(items, length) {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort(function(a,b){
+      if (a<=b) {
+        return -1
+      }else {
+        return 1
+      }
+    });
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos >= this.length){
+      throw new Error('OutOfBounds');
+    } else {
+      return this.items[pos];
+    }
+  }
 
-  max() {}
+  max() {
+    if(this.length === 0){
+      throw new Error('EmptySortedList');
+    }else{
+      return Math.max(...this.items);
+    }
 
-  min() {}
+  }
 
-  sum() {}
+  min() {   
+  if(this.length === 0){
+    throw new Error('EmptySortedList');
+  }else{
+    return Math.min(...this.items);
+  }
+}
 
-  avg() {}
+  sum() {
+    const reducer = (accumulator, currentValue) => accumulator + currentValue;
+    if(this.length === 0){
+      return 0;
+    }else{
+      return this.items.reduce(reducer)
+    }
+  }
+
+  avg() {
+    let average = this.sum() / this.length;
+    if(this.length === 0){
+      throw new Error('EmptySortedList');
+    }else{
+      return average;
+    }
+    
+
+  }
 }
 
 module.exports = SortedList;
