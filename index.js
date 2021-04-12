a2dc63709366452df9bc36e7a9e0792dcae30504
Pablo del Barrio <pablodbb@gmail.com>
2021-04-12 14:36:08 +0200

    Iterations from 1 to 4

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a, b) => a - b);
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  get(pos) {
+    if (pos > this.items.length) {
+      throw new Error("OutOfBounds");
+    } else {
+      return this.items[pos];
+    }
+  }
 
-  get(pos) {}
-
-  max() {}
+  max() {
+    if (this.items.length === 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      let lastPos = this.length - 1;
+      return this.items[lastPos];
+    }
+  }
 
   min() {}
 
