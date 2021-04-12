69047be7806d2f3ef907b7028f08d1925a04ef09
Toni Luis <toniluiscristofol@gmail.com>
2021-04-12 22:29:18 +0200

    Finished

--- index.js
+++ index.js
@@ -1,17 +1,49 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [],
+    this.length = this.items.length;
+  }
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a, b) =>
+    {
+      return a - b;
+    });
+    this.length = this.items.length
+    } 
+  get(pos) {
+    
+    if(pos < this.items.length) {
+      return this.items[pos]
+    } else {
+      throw new Error('OutOfBounds');
+    }
+  }
+  max() {
+    this.items.sort((a, b) =>
+    {
+      return a - b;
+    });
+    if(this.items.length > 0) {
+      return this.items[this.items.length -1]
+    } else {
+      throw new Error('EmptySortedList');
+    }
 
-  add(item) {}
+  }
+  min() {
 
-  get(pos) {}
-
-  max() {}
-
-  min() {}
-
+    this.items.sort((a, b) =>
+    {
+      return a - b;
+    });
+    if(this.items.length > 0) {
+      return this.items[0]
+    } else {
+      throw new Error('EmptySortedList');
+    }
+  }
   sum() {}
-
   avg() {}
 }
-
 module.exports = SortedList;
