25518d7e08c807d9bfcbc4befcf69efd9c04e5dc
silvia rodriguez <silrodcar@gmail.com>
2021-04-12 12:44:03 +0200

    done

--- index.js
+++ index.js
@@ -1,17 +1,54 @@
 class SortedList {
-  constructor() {}
+  constructor(items, length) {
+this.items = [];
+this.length = this.items.length;
 
-  add(item) {}
+  }
 
-  get(pos) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((el1,el2) => {
+      return el1 - el2;
+    })
 
-  max() {}
+    this.length = this.items.length;
 
-  min() {}
+  }
 
-  sum() {}
+  get(pos) {
+    if(this.items[pos] === undefined) {
+      throw Error ('OutOfBounds');
+    } return this.items[pos];
+  }
 
-  avg() {}
-}
+  max() {
+    if(this.items.length == 0) {
+      throw Error ('EmptySortedList')
+    } else {return Math.max(...this.items) 
+    }
+  }
+
+  min() {
+    if(this.items.length == 0) {
+      throw Error ('EmptySortedList')
+    } else {
+      return Math.min(...this.items)
+    }
+
+  }
+
+  sum() {
+    return this.items.reduce((el1, el2) => el1 + el2, 0);
+
+  }
+
+  avg() {
+    if(this.items.length == 0){
+      throw new Error('EmptySortedList');
+    }
+    return (this.sum() / this.items.length);
+    }
+  }
+
 
 module.exports = SortedList;
