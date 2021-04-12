ff9e6df1034165efbf4efa6cb9078b556a57e145
Thornnk <Thornnk1@gmail.com>
2021-04-12 16:49:25 +0200

    Finished Iteration 5

--- index.js
+++ index.js
@@ -1,13 +1,40 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = []
+    this.length = 0
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item)
+    this.items.sort((a, b)=>{
+      return a-b
+    })
+    this.length = this.items.length
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (!this.items[pos]) {
+      throw OutOfBounds
+    } else {
+      return this.items[pos]
+    }
+  }
 
-  max() {}
+  max() {
+    if (!this.length){
+      throw EmptySortedList
+    } else {
+      return Math.max(...this.items)
+    }
+  }
 
-  min() {}
+  min() {
+    if (!this.length){
+      throw EmptySortedList
+    } else {
+      return Math.min(...this.items)
+    }
+  }
 
   sum() {}
 
@@ -15,3 +42,9 @@ class SortedList {
 }
 
 module.exports = SortedList;
+
+sortedList = new SortedList()
+sortedList.add(1)
+sortedList.add(3)
+sortedList.add(2)
+console.log(Math.max(...sortedList.items))
