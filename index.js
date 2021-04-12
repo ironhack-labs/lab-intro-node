b0fd63650bdb00a34a267b9178297b58fb60c0ac
Graziele Dantas <elentary@gmail.com>
2021-04-12 19:11:32 -0300

    done

--- index.js
+++ index.js
@@ -1,48 +1,49 @@
 class SortedList {
   constructor() {
-    this.items = []
-    this.length = 0
+    this.items = [];
+    this.length = 0;
   }
 
   add(item) {
     this.items.push(item);
     this.length++;
-    this.items.sort((a, b) => { //ou: this.items.sort((a, b) => (a - b));
-      return a - b // precisa do return pra funcionar assim!
+    this.items.sort((a, b) => {
+      //ou: this.items.sort((a, b) => (a - b));
+      return a - b; // precisa do return pra funcionar assim!
     });
-  };
+  }
 
   get(pos) {
-    if (!this.items[pos]) { //as chaves ([]) accessam o index/posição do array.
-      throw new Error('OutOfBounds');
+    if (!this.items[pos]) {
+      //as chaves ([]) accessam o index/posição do array.
+      throw new Error("OutOfBounds");
     } else {
       return this.items[pos];
-    };
-  };
+    }
+  }
 
   max() {
-    if(this.items.length === 0) throw new Error('EmptySortedList');
-    return this.items[this.length -1]; //ou: Math.max(...this.items)
+    if (this.items.length === 0) throw new Error("EmptySortedList");
+    return this.items[this.length - 1]; //ou: Math.max(...this.items)
   }
 
   min() {
-    if(this.items.length === 0) throw new Error('EmptySortedList');
+    if (this.items.length === 0) throw new Error("EmptySortedList");
     return this.items[0]; //ou: Math.min(...this.items)
   }
 
   sum() {
     if (this.length === 0) {
-      return 0
+      return 0;
     } else {
-      return this.items.reduce((a, b) => (a + b)); //ou apenas: return this.items.reduce((a, b) => (a + b), 0)
-    };
-  };
+      return this.items.reduce((a, b) => a + b); //ou apenas: return this.items.reduce((a, b) => (a + b), 0)
+    }
+  }
 
   avg() {
     if (this.length === 0) {
-      throw new Error("EmptySortedList")
-    }
-    else return (this.sum()/this.length);
+      throw new Error("EmptySortedList");
+    } else return this.sum() / this.length;
   }
 }
 
