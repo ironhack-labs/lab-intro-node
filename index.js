3cd6e10e8b717586b347087bedcf7b43cde7dc27
David Bueno <davidbuenox8@gmail.com>
2021-04-13 14:43:25 +0200

    done

--- index.js
+++ index.js
@@ -1,17 +1,54 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort(function (a, b) {
+      return a - b;
+    });
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos > this.length) {
+      throw new Error('OutOfBounds');
+    }
+    return this.items.indexOf(pos);
 
-  max() {}
+  }
 
-  min() {}
+  max() {
+    if (this.length === 0) {
+      throw new Error('EmptySortedList');
+    }
+    return Math.max(...this.items)
+  }
 
-  sum() {}
+  min() {
+    if (this.length === 0) {
+      throw new Error('EmptySortedList');
+    }
+    return Math.min(...this.items)
+  }
 
-  avg() {}
+
+  sum() {
+    if (this.length === 0) {
+      return 0;
+    }
+    const reducer = (acc, val) => acc + val;
+    return this.items.reduce(reducer);
+  }
+
+  avg() {
+    if (this.length === 0) {
+      throw new Error('EmptySortedList');
+    }
+    return this.sum() / this.length
+  }
 }
 
 module.exports = SortedList;
