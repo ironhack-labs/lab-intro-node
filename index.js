35be057998c2c4d965a0909d052865355f405195
Chocogrenouille <thestral@gmx.net>
2021-04-13 15:00:18 +0200

    done

--- index.js
+++ index.js
@@ -1,17 +1,56 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.length = this.items.length;
+    this.items.sort((a,b) => a-b);
+    //console.log(items);
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (!this.items[pos]){
+      throw new Error('OutOfBounds');
+    }
+    return this.items[pos];
+  }
 
-  max() {}
+  max() {
+    if (this.length === 0){
+      throw new Error('EmptySortedList');
+    }
+    return Math.max(...this.items);
+  }
 
-  min() {}
+  min() {
+    if (this.length === 0){
+      throw new Error('EmptySortedList');
+    }
+    return Math.min(...this.items);
+  }
 
-  sum() {}
+  sum() {
+    let sum = 0;
+    for (this.num of this.items){
+      sum += this.num;
+    }
+    return sum
+  }
 
-  avg() {}
+  avg() {
+    let sum = 0;
+    if (this.length === 0){
+      throw new Error('EmptySortedList');
+    }
+    for (this.num of this.items){
+      sum += this.num;
+    }
+    return sum/this.length;
+  }
 }
 
+//const sortedList = new SortedList();
 module.exports = SortedList;
