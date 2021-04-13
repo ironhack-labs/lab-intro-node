dcfca51ee713d619e62cb9bfcc8ecad33e23fe27
Diogo Major <diogocmajor@gmail.com>
2021-04-13 15:09:32 +0200

    done

--- index.js
+++ index.js
@@ -1,17 +1,64 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item)
+    this.items.sort((a,b)=>{
+      return a-b
+    })
+    this.length = this.items.length
+  }
 
-  get(pos) {}
+  get(pos) {
+    if(this.length<pos) {
+      throw new Error('OutOfBounds');
+    } else {
+      return this.items[pos]
+    }
+  }
 
-  max() {}
+  max() {
+    if(this.length == 0) {
+      throw new Error('EmptySortedList');
+    } else {
+      return this.items.pop();
+    }
+  }
 
-  min() {}
+  min() {
+    if (this.length == 0) {
+      throw new Error('EmptySortedList');
+    } else {
+      return this.items.shift();
+    }
+  }
 
-  sum() {}
+  sum() {
+    let total = 0;
+    if (this.length == 0) {
+      return 0;
+    } else {
+      for (let i = 0; i < this.length; ++i) {
+        total += this.items[i];
+      }
+      return total
+    }
+  }
 
-  avg() {}
+  avg() {
+    let total = 0;
+    if (this.length == 0) {
+      throw new Error('EmptySortedList');
+    } else {
+      for(let i = 0; i < this.length; i++){
+        total += this.items[i]
+      }
+    return total/this.length
+    }
+  }
 }
 
 module.exports = SortedList;
