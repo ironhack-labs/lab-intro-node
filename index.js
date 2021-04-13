f846317e24c154a87a9d460745ace51130bd0ee0
alexanderrathbone <alexander.thomas.rathbone@hotmail.com>
2021-04-13 15:22:24 +0200

    finish

--- index.js
+++ index.js
@@ -1,17 +1,52 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort(function (a, b) {
+      return a - b;
+    });
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos >= this.length) {
+      throw new Error('out of bounds');
+    }
+    return this.items[pos];;
+  }
 
-  max() {}
+  max() {
+    if (this.length === 0){
+      throw new Error('EmptySortedList');
+    }
+    return this.items[this.length - 1];
+  }
 
-  min() {}
+  min() {
+    if (this.length === 0){
+      throw new Error('EmptySortedList');
+    }
+    return this.items[0];
+  }
 
-  sum() {}
+  sum() {
+    if(this.items.length === 0){return 0;}
+    const reducer = (accumulator, currentValue) => accumulator + currentValue;
+    let sum = this.items.reduce(reducer);
+    return sum; 
+  }
 
-  avg() {}
+  avg() {
+    if (this.length === 0){
+      throw new Error('EmptySortedList');
+    }
+    let sum = this.sum();
+    return sum/this.length; 
+  }
 }
 
 module.exports = SortedList;
