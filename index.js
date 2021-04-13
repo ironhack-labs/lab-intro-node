d561148fe9df325fd9c5afc05b0cb014de2c9a13
helenetrommelen <helenetrommelen@gmail.com>
2021-04-13 14:58:20 +0200

    Lab completed. Hélène Trommelen & Rimbert Richter

--- index.js
+++ index.js
@@ -1,17 +1,69 @@
 class SortedList {
-  constructor() {}
+  constructor(items, length) {
+    this.items = [];
+    this.length = this.items.length;
 
-  add(item) {}
+  }
 
-  get(pos) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort(function(a, b) {
+      return a - b;
+    });
+    this.length = this.items.length;
+    //update length
 
-  max() {}
+  }
 
-  min() {}
+  get(pos) {
 
-  sum() {}
+    if (pos > this.length-1){
+      throw new Error('OutOfBounds');
+    }
+    return this.items[pos];
+  }
 
-  avg() {}
+  max() {
+    if (this.length === 0){
+      throw new Error('EmptySortedList');
+    }
+    return Math.max(...this.items);
+  }
+
+  min() {
+    if (this.length === 0){
+      throw new Error('EmptySortedList');
+    }
+    return Math.min(...this.items); 
+  }
+
+  sum() {
+    if (this.length === 0){
+      return 0;
+    }
+    return this.items.reduce((a, b) => a + b, 0);
+    //const reducer = (accumulator, currentValue) => accumulator + currentValue;
+  }
+
+  avg() {
+
+    if (this.length === 0){
+      throw new Error('EmptySortedList');
+    }
+    return this.sum()/this.length;
+
+  }
 }
 
 module.exports = SortedList;
+
+let list = new SortedList;
+list.add(30);
+list.add(20);
+list.add(100);
+list.add(10);
+list.get(2);
+console.log(list.sum());
+console.log(list.length);
+
+
