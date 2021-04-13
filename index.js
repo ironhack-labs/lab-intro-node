9baf8e612180c3eb31270efb359feb118ee8ff0e
Fintan Hahnefeld <fintan22@gmail.com>
2021-04-13 15:13:20 +0200

    done

--- index.js
+++ index.js
@@ -1,17 +1,50 @@
 class SortedList {
-  constructor() {}
+  constructor(items, length) {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  checkLength() {
+    if (this.length === 0) {
+      throw new Error('EmptySortedList');
+    }
+  }
 
-  get(pos) {}
+  add(item) {
+    this.items.push(item);
+    this.length++;
+    if (this.items.length > 1) {
+    this.items.sort((a,b) => a - b);
+    }
+  }
 
-  max() {}
+  get(pos) {
+    if (pos > this.length) {
+      throw new Error('OutOfBounds');
+    }
+    return this.items[pos];
+  }
 
-  min() {}
+  max() {
+    this.checkLength()
+    return Math.max(...this.items);
+  }
 
-  sum() {}
+  min() {
+    this.checkLength()
+    return Math.min(...this.items);
+  }
 
-  avg() {}
+  sum() {
+    return this.items.reduce((acc,val) => {
+      return acc + val;
+    },0);
+  }
+
+  avg() {
+    this.checkLength()
+    return this.sum() / this.length;
+  }
 }
 
 module.exports = SortedList;
