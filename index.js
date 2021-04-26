dd0a500a5fe0dc5fc2c1ef86ad51a96c1504786d
hisamparker <hisamparker@gmail.com>
2021-04-26 07:38:16 +0200

    done

--- index.js
+++ index.js
@@ -1,17 +1,40 @@
 class SortedList {
-  constructor() {}
+  constructor(arr) {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a, b) => {
+      return a - b;
+    });
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if(!this.items[pos]) throw new Error('OutOfBounds');
+    return this.items[pos];
+  }
 
-  max() {}
+  max() {
+    if (this.length === 0) throw new Error('EmptySortedList'); 
+    return Math.max(...this.items);
+  }
+  
+  min() {
+    if (this.length === 0) throw new Error('EmptySortedList'); 
+    return Math.min(...this.items);
+  }
 
-  min() {}
+  sum() {
+    return this.items.reduce((acc, num) => acc + num, 0);
+  }
 
-  sum() {}
-
-  avg() {}
+  avg() {
+    if (this.length === 0) throw new Error('EmptySortedList');
+    return this.sum(this.items)/this.length;
+  }
 }
 
 module.exports = SortedList;
