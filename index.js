313b95805f9422d2b7d2542e1a0a46dd8bac282e
ZsokaM <zsokamontvai@gmail.com>
2021-04-28 22:14:47 +0200

    done

--- index.js
+++ index.js
@@ -1,17 +1,38 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items =[];
+    this.length = this.items.length
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item)
+    this.items.sort((a,b)=> a-b)
+    this.length = this.items.length
+  }
 
-  get(pos) {}
+  get(pos) {
+    if(this.items.includes(pos)) return this.items.indexOf(pos)
+    else throw new Error('OutOfBounds')
+  }
 
-  max() {}
+  max() {
+    if(this.items.length <1) throw new Error('EmptySortedList')
+    else return Math.max(...this.items)
+  }
 
-  min() {}
+  min() {
+    if(this.items.length <1) throw new Error('EmptySortedList')
+    else return Math.min(...this.items)
+  }
 
-  sum() {}
+  sum() {
+    return this.items.reduce((accum, curr)=> {return accum + curr}, 0)
+  }
 
-  avg() {}
+  avg() {
+    if(this.items.length <1) throw new Error('EmptySortedList')
+    else return this.sum()/this.length
+  }
 }
 
 module.exports = SortedList;
