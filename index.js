9d16ed0bd7dedb3e8cd0982934f5ee781975d200
Luciano Colabraro <luciano.colabraro@gmail.com>
2021-05-03 09:59:31 +0200

    done

--- index.js
+++ index.js
@@ -1,30 +1,46 @@
 class SortedList {
   constructor() {
     this.items = [];
-    this.length = this.items.length;
+    this.length = 0;
   }
 
   add(item) {
     this.items.push(item);
-    this.length++;
+    this.length = this.items.length;
     this.items.sort((a, b) => a - b);
   }
 
   get(pos) {
-    return item[pos];
+    if (pos > this.length) {
+      throw new Error("OutOfBounds");
+    }
+    return this.items[pos];
   }
 
   max() {
+    if (this.length === 0) {
+      throw new Error("EmptySortedList");
+    }
     return this.items.sort((a, b) => a - b)[this.length - 1];
   }
 
   min() {
+    if (this.length === 0) {
+      throw new Error("EmptySortedList");
+    }
     return this.items[0];
   }
 
-  sum() {}
+  sum() {
+    return this.items.reduce((a, b) => a + b, 0);
+  }
 
-  avg() {}
+  avg() {
+    if (this.length === 0) {
+      throw new Error("EmptySortedList");
+    }
+    return this.sum() / this.length;
+  }
 }
 
 module.exports = SortedList;
