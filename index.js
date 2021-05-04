e661dd824c797b5070d74ab96c0e3d23c8b55372
dennis <dsanchez@unpluggedcorp.com>
2021-05-04 14:18:12 -0400

    ejercicios resueltos

--- index.js
+++ index.js
@@ -1,17 +1,56 @@
 class SortedList {
-  constructor() {}
+    constructor() {
+        this.items = [];
+        this.length = this.items.length
+    }
 
-  add(item) {}
+    add(item) {
+        this.items.push(item);
+        this.length = this.items.length;
+        this.items.sort((a, b) => a - b)
+    }
 
-  get(pos) {}
+    get(pos) {
 
-  max() {}
+        if (this.items.indexOf(pos) === -1) {
+            throw new Error('OutOfBounds');
+        }
 
-  min() {}
+        return this.items[pos]
 
-  sum() {}
+    }
 
-  avg() {}
+    max() {
+        this.items.length
+        if (this.items.length === 0) {
+            throw new Error('EmptySortedList');
+
+        }
+        return Math.max(...this.items);
+
+    }
+
+    min() {
+
+        this.items.length
+        if (this.items.length === 0) {
+            throw new Error('EmptySortedList');
+
+        }
+        return Math.min(...this.items);
+    }
+
+    sum() {
+        return this.items.reduce(((acc, current) => acc + current), 0)
+    }
+
+    avg() {
+        if (this.items.length === 0) {
+            throw new Error('EmptySortedList')
+        } else {
+            return this.sum() / this.length;
+        }
+    }
 }
 
 module.exports = SortedList;
