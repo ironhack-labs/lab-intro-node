5c5f1e7fffc1dc5c97d0ae917c07ce040b6ac57c
danpinon <danielpformoso@gmail.com>
2021-05-04 14:40:11 -0500

    lab intro node done

--- index.js
+++ index.js
@@ -1,17 +1,54 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = []
+    this.length = this.items.length
+    this.error = new Error('EmptySortedList')
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item)
+    this.length = this.items.length
+    this.items.sort((a, b) => a - b)
+  }
 
-  get(pos) {}
+  get(pos) {
+    if(this.items.indexOf(this.items[pos]) != -1){
+      return this.items[pos]
+    } else {
+      throw new Error('OutOfBounds')
+    }
+  }
 
-  max() {}
+  max() {
+    if(this.items.length > 0){
+      return Math.max(...this.items)
+    } else {
+      throw new Error('EmptySortedList')
+    }
+  }
 
-  min() {}
+  min() {
+    if(this.items.length > 0){
+      return Math.min(...this.items)
+    } else {
+      throw new Error('EmptySortedList')
+    }
+  }
 
-  sum() {}
+  sum() {
+    let sum = this.items.reduce(((acc, value) => acc + value), 0)
 
-  avg() {}
+    return sum    
+  }
+
+  avg() {
+    let avr = this.sum()/this.items.length
+    if(this.items.length > 0){
+      return avr
+    } else{
+      throw new Error('EmptySortedList')
+    }
+  }
 }
 
 module.exports = SortedList;
