422ab42867fdd55f6804a014e1e30c7d7149c42b
fereber1294 <freyesber@gmail.com>
2021-05-04 13:35:21 -0500

    lab done

--- index.js
+++ index.js
@@ -1,17 +1,54 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = []
+    this.length = this.items.length
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item)
+    this.length = this.items.length;
+    this.items.sort((a, b) => a - b
+    )
 
-  get(pos) {}
+  }
 
-  max() {}
+  get(pos) {
+    if (pos > this.length - 1) {
+      throw new Error('Out of Bounds')
+    }
+    return this.items[pos]
 
-  min() {}
+  }
 
-  sum() {}
+  max() {
+    if(this.length === 0){
+      throw new Error('Empty Sorted List')
+    } else {
+      return Math.max(...this.items)
+    }
+  }
 
-  avg() {}
+  min() {
+    if(this.length === 0){
+      throw new Error('Sorted List')
+    } else {
+      return Math.min(...this.items)
+    }
+  }
+
+  sum() {
+    return this.items.reduce(((acc, current) => acc + current), 0)
+
+  }
+
+  avg() {
+    if(this.items.length === 0){
+      throw new Error('EmptySortedList')
+    } else{
+      return this.sum()/this.items.length
+    }
+
+  }
 }
 
 module.exports = SortedList;
