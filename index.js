306ebf2a87106cc1fae1f8b14210898bd4e2f13c
BlancaML <blancamorenodeluna@gmail.com>
2021-05-15 15:10:42 +0200

    done

--- index.js
+++ index.js
@@ -1,17 +1,57 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = 0;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.splice(1, 0, item);
+    this.items.sort((e1, e2) => {
+      return e1 - e2;
+    });
+    this.length++;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (this.items[pos]) {
+      return this.items[pos];
+    } else {
+      throw new Error("OutOfBounds");
+    }
+  }
 
-  max() {}
+  max() {
+    if (this.length === 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      return this.items[this.length - 1];
+    }
+  }
 
-  min() {}
+  min() {
+    if (this.length === 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      return this.items[0];
+    }
+  }
 
-  sum() {}
+  sum() {
+    if (this.length === 0) {
+      return 0;
+    } else {
+      return this.items.reduce((e1, e2) => e1 + e2, 0);
+    }
+  }
 
-  avg() {}
+  avg() {
+    if(this.length === 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      return this.items.reduce((e1, e2) => e1 + e2, 0) / this.length;
+    }
+
+  }
 }
 
 module.exports = SortedList;
