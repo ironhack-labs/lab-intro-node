842ce3478a3bbcf9fa22e846dfbd28fe43393b51
Daniel Abad Noriega <Daniel.Abad.Noriega@gmail.com>
2021-05-16 11:03:51 +0200

    Done

--- index.js
+++ index.js
@@ -1,17 +1,63 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = 0;
+  }
 
-  add(item) {}
+  //Iteration 2: add(item)
+  add(item) {
+    this.items.push(item);
+    this.items = this.items.sort((i1, i2) => i1 - i2);
+    this.length++
+  }
 
-  get(pos) {}
+  //Iteration 3: get(pos)
+  get(pos) {
+    if (pos >= this.items.length) {
+      throw new Error('OutOfBounds');
+    } else {
+      return this.items[pos];
+    }
+  }
 
-  max() {}
+  //Iteration 4: max()
+  max() {
+    if(this.items.length <= 0){
+      throw new Error('EmptySortedList');
+    }else{
+      return this.items[this.items.length-1];
+    }
+  }
 
-  min() {}
+  //Iteration 5: min()
+  min() {
+    if(this.items.length <= 0){
+      throw new Error('EmptySortedList');
+    }else{
+      return this.items[0];
+    }
+  }
 
-  sum() {}
+  sum() {
+    const reducer = (accumulator, currentValue) => accumulator + currentValue;
+    if(this.items.length <=0){
+      return 0;
+    }else{
+      return this.items.reduce(reducer);
+    }
+  }
 
-  avg() {}
+  avg() {
+    if(this.items.length <= 0){
+      throw new Error('EmptySortedList');
+    }else{
+      return this.sum()/this.items.length;
+    }
+  }
 }
 
 module.exports = SortedList;
+
+/* const sortList = new SortedList([1,3,5]);
+
+console.log(sortList.add(4)) */
