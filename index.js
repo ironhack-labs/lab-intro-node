b24e77d809544e780e296a469ddbe399c1e30ae6
joserra <jrcid@mib.isdi.es>
2021-05-22 10:23:31 +0200

    done

--- index.js
+++ index.js
@@ -1,17 +1,59 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = []
+    this.length = 0
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item)
+    this.items = this.items.sort((a, b) =>  a - b);
+    this.length++
+    
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos >= this.items.length) {
+      throw new Error('OutOfBounds');
+    } else {
+      return this.items[pos];
+    }
+  }
 
-  max() {}
+  max() {
+    if (this.items.length <= 0) {
+      throw new Error('EmptySortedList')
+    } else {
+      return this.items[this.items.length-1]
+    }
+  }
 
-  min() {}
+  min() {
+    if (this.items.length <= 0) {
+      throw new Error('EmptySortedList')
+    } else {
+      return this.items[0]
+    }
+  }
 
-  sum() {}
+  sum() {
+    const reducer = (accumulator, currentValue) => accumulator + currentValue
+    if(this.items.length <=0 ) {
+      return 0;
+    } else {
+      return this.items.reduce(reducer)
+    }
+    
+  }
 
-  avg() {}
+  avg() {
+    if(this.items.length <=0 ) {
+      throw new Error('EmptySortedList')
+    } else {
+      return this.sum() / this.items.length
+    }
+    return this.sum() / this.items.length
+  }
 }
+const sortList = new SortedList()
 
 module.exports = SortedList;
