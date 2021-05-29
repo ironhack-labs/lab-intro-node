9535e49dc30309e5375713db81f509b44d2636a9
victorhhq <victorhhquezada@gmail.com>
2021-05-29 15:31:17 -0500

    done

--- index.js
+++ index.js
@@ -1,17 +1,52 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items= [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push (item);
+    this.items.sort(function(a, b){return a-b})
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos >= this.length){
+      throw new Error('OutOfBounds');
+    }else{
+      return this.items[pos];
+    }
+  }
 
-  max() {}
+  max() {
+    if (this.length === 0){
+      throw new Error('EmptySortedList');
+    }
+    return this.items[this.length - 1];
+  }
 
-  min() {}
+  min() {    if (this.length === 0){
+    throw new Error('EmptySortedList');
+  }
+  return this.items[0];}
 
-  sum() {}
+  sum() {
+    if(this.length === 0){return 0;}
+    const reducer = (accumulator, currentValue) => accumulator + currentValue;
+    let sum = this.items.reduce(reducer);
+    return sum; 
+  }
 
-  avg() {}
+  avg() {
+    const reducer = (accumulator, currentValue) => accumulator + currentValue;
+    
+    if (this.length === 0){
+      throw new Error('EmptySortedList');
+    }else{
+      let avg = this.sum()/this.length;
+      return avg;
+    }
+  }
 }
 
 module.exports = SortedList;
