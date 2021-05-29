3da0a86b588e523fddd997702ed1ccd78aa0372b
jonathanbbarbosa <jonathanbbarbosa@gmail.com>
2021-05-29 15:21:08 -0500

    done

--- index.js
+++ index.js
@@ -1,17 +1,86 @@
-class SortedList {
-  constructor() {}
+class SortedList 
+{
+  constructor() 
+  {
+    this.items=[]
+    this.length=this.items.length
+  }
 
-  add(item) {}
+  add(x) 
+  {
+    this.items.push(x)
+    this.items.sort((a,b)=>a-b)
+    this.length=this.items.length
+    
+  }
 
-  get(pos) {}
+  get(pos) 
+  {
+    
+    if (pos>this.items.length)
+    {
+      throw new Error('OutOfBounds');
+    }
+    else
+    {
+      return this.pos=this.items[pos]
+    }
+  }
 
-  max() {}
+  max()
+  {
+    
+    if (!this.items.length)
+    {
+      throw new Error('EmptySortedList');
+    }
+    else
+    {
+      return Math.max(...this.items);
+    }
+  }
 
-  min() {}
+  min() 
+  {
+    if (!this.items.length)
+    {
+      throw new Error('EmptySortedList');
+    }
+    else
+    {
+      return Math.min(...this.items);
+    }
 
-  sum() {}
+  }
 
-  avg() {}
+  sum() 
+  {
+    if(!this.items.length)
+    {
+      return 0
+    }
+    else
+    {
+      let temp=this.items.reduce((a,b)=>a+b,0)
+      return temp
+    }
+    
+  }
+
+  avg() 
+  {
+    
+    if(!this.items.length)
+    {
+      throw new Error('EmptySortedList');
+    }
+    else
+    {
+      return this.sum()/this.length
+    }
+  }
 }
+
+
 
 module.exports = SortedList;
