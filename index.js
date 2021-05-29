c02142b41a2bdc9afab245956417cceec36c2d18
Cesar Carreras <cesar.carreras95@gmail.com>
2021-05-29 15:20:56 -0500

    done

--- index.js
+++ index.js
@@ -1,17 +1,71 @@
-class SortedList {
-  constructor() {}
-
-  add(item) {}
-
-  get(pos) {}
-
-  max() {}
-
-  min() {}
-
-  sum() {}
-
-  avg() {}
+class SortedList
+{
+  constructor()
+  {
+    this.items=[]
+    this.length=this.items.length
+  }
+  add(x)
+  {
+    this.items.push(x)
+    this.items.sort((a,b)=>a-b)
+    this.length=this.items.length
+  }
+  get(pos)
+  {
+    if (pos>this.items.length)
+    {
+      throw new Error('OutOfBounds');
+    }
+    else
+    {
+      return this.pos=this.items[pos]
+    }
+  }
+  max()
+  {
+    if (!this.items.length)
+    {
+      throw new Error('EmptySortedList');
+    }
+    else
+    {
+      return Math.max(...this.items);
+    }
+  }
+  min()
+  {
+    if (!this.items.length)
+    {
+      throw new Error('EmptySortedList');
+    }
+    else
+    {
+      return Math.min(...this.items);
+    }
+  }
+  sum()
+  {
+    if(!this.items.length)
+    {
+      return 0
+    }
+    else
+    {
+      let temp=this.items.reduce((a,b)=>a+b,0)
+      return temp
+    }
+  }
+  avg()
+  {
+    if(!this.items.length)
+    {
+      throw new Error('EmptySortedList');
+    }
+    else
+    {
+      return this.sum()/this.length
+    }
+  }
 }
-
 module.exports = SortedList;
