5870f004c11f358e9ea93b2678cec700d7964c46
Mauricio Munoz <mauripedroza89@gmail.com>
2021-05-29 14:59:23 -0500

    done

--- index.js
+++ index.js
@@ -1,17 +1,52 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = []
+    this.length = this.items.length
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.length = this.items.length;
+    this.items.sort((a,b) => {
+      return a - b;
+    })
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (!this.items[pos]){
+      throw new Error('OutOfBounds');
+    }
+    return this.items[pos];
+  
+  }
 
-  max() {}
+  max() {
+    if (this.items.length === 0){
+      throw new Error('EmptySortedList');
 
-  min() {}
+    }
+    return Math.max(...this.items);
+  }
 
-  sum() {}
+  min() {
+    if (this.items.length === 0){
+      throw new Error('EmptySortedList');
 
-  avg() {}
+    }
+    return Math.min(...this.items);
+  }
+
+  sum() {
+    return this.items.reduce((a,b) => a+b,0)
+  }
+
+  avg() {
+    if (this.items.length === 0){
+      throw new Error('EmptySortedList');
+
+    }
+    return (this.sum()/this.length)
+  }
 }
 
 module.exports = SortedList;
