9e44958b62295d6d76c47346ba0b2bd60d406425
Israel Zambrana <zambrana90@gmail.com>
2021-06-05 13:44:15 +0200

    done!

--- index.js
+++ index.js
@@ -1,17 +1,41 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.length = this.items.length;
+    this.items.sort((a, b) => a - b);
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (this.items.includes(pos)) return this.items[pos];
+    else throw new Error("OutOfBounds");
+  }
 
-  max() {}
+  max() {
+    if (this.length > 0) return Math.max(...this.items);
+    else throw new Error("EmptySortedList");
+  }
 
-  min() {}
+  min() {
+    if (this.length > 0) return Math.min(...this.items);
+    else throw new Error("EmptySortedList");
+  }
 
-  sum() {}
+  sum() {
+    if ((this.length <= 0)) return 0;
+    else {
+      return this.items.reduce((acc, current) => (acc += current), 0);
+    }
+  }
 
-  avg() {}
+  avg() {
+    if (this.length > 0) return this.sum() / this.length;
+    else throw new Error("EmptySortedList");
+  }
 }
 
 module.exports = SortedList;
