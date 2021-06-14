30aab91627d7463412728e35caff8a85be4296fd
Alejandra Rubio <alejandrarubio@MacBook-Air-de-Alberto-2.local>
2021-06-14 15:01:07 +0200

    iteraciones hechas excepto los bonus

--- index.js
+++ index.js
@@ -15,3 +15,50 @@ class SortedList {
 }
 
 module.exports = SortedList;
+
+new SortedList {
+  constructor(items, length) {
+    this.items;
+    this.length;
+  }
+
+  add(item) {
+    this.items.push(item);
+    this.items = this.items.sort((a, b) => a - b);
+    this.length = this.items.length;
+  }
+
+  get(pos) {
+    if (pos = this.length) {
+
+      return this.items[pos]
+
+    } else {
+
+      throw new Error('EmptySortedList');
+    }
+  }
+
+  max() {
+    if (this.items.length === 0) {
+
+      throw new Error('EmptySortedList');
+
+    } else {
+
+      return Math.max(...this.items);
+    }
+  }
+
+  min() {
+    if (this.items.length === 0) {
+
+      throw new Error('EmptySortedList');
+
+    } else {
+
+      return Math.min(...this.items);
+    }
+  }
+
+}
