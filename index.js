1b918feeadcf3520e3841eaeaf837ec2f491b76b
Pablo Borjas <pabloborjas_0708@hotmail.com>
2021-06-14 11:09:41 +0200

    terminado

--- index.js
+++ index.js
@@ -18,13 +18,33 @@ class SortedList {
     }
   }
 
-  max() {}
+  max() {
+    if(this.items.length === 0){
+      throw new Error('EmptySortedList');
+    }else return this.items[this.items.length - 1]
+  }
 
-  min() {}
+  min() {
+    if(this.items.length === 0){
+      throw new Error('EmptySortedList');
+    }else{
+      return this.items[0]
+    }
+  }
 
-  sum() {}
+  sum() {
+    return this.items.reduce((a,b)=>{
+      return a+b
+    },0)
+  }
 
-  avg() {}
+  avg() {
+    if(this.items.length === 0){
+      throw new Error('EmptySortedList');
+    }else {
+      return (this.sum() / this.length)
+    }
+  }
 }
 
 module.exports = SortedList;
