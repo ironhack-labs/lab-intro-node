419b2e17b8d7944409b69a0ccda4737e2c8b2c0b
Your Name <you@example.com>
2021-06-22 18:12:24 +0200

    lab done

--- index.js
+++ index.js
@@ -1,17 +1,66 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = 0;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a, b) => {
+      if (a > b) {
+        return 1
+      } else if (a < b) {
+        return -1
+      }
+      return 0
+    });
 
-  get(pos) {}
+    this.length = this.items.length
+  }
 
-  max() {}
+  get(pos) {
+    if (pos < this.length) {
+      return this.items[pos]
+    } else {
+      throw new Error('OutOfBounds');
+    }
+  }
 
-  min() {}
+  max() {
+    if (this.length > 0) {
+      return Math.max(...this.items)
+    } else {
+      throw new Error('EmptySortedList');
+    }
+  }
 
-  sum() {}
+  min() {
+    if (this.length > 0) {
+      return Math.min(...this.items)
+    } else {
+      throw new Error('EmptySortedList');
+    }
+  }
 
-  avg() {}
+  sum() {
+    let sum = this.items.reduce((accum, currentValue) => {
+      return accum + currentValue
+    }, 0);
+    return sum
+  }
+
+
+
+  avg() {
+    if (this.length > 0) {
+      let sum = this.items.reduce((accum, currentValue) => {
+        return accum + currentValue
+      }, 0);
+      return sum / this.length
+    } else {
+      throw new Error('EmptySortedList');
+    }
+  }
 }
 
 module.exports = SortedList;
