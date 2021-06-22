7d55ec233dc23b7057bf357f9ba5736ae991eb6d
Zoriglon Ayush <z.ayush@gmx.de>
2021-06-22 15:04:02 +0200

    done with bonus

--- index.js
+++ index.js
@@ -1,17 +1,41 @@
 class SortedList {
-  constructor() {}
+  constructor(items, length) {
+    this.items = [];
+    this.length = 0 ;
+  }
 
-  add(item) {}
+  add(item) {
+      this.items.push(item);
+      this.items.sort((a, b) => a - b)
+      this.length++
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (this.items[pos] === undefined){throw new Error('OutOfBounds')}
+    return this.items[pos]
+  }
 
-  max() {}
+  max() {
+    if (this.items.length === 0){throw new Error('EmptySortedList')}
+    return Math.max(...this.items)
+  }
 
-  min() {}
+  min() {
+    if (this.items.length === 0){throw new Error('EmptySortedList')}
+    return Math.min(...this.items)
+  }
 
-  sum() {}
+  sum() {
+    if (this.items.length === 0){return 0}
+    let sum = this.items.reduce((acc , val) => acc + val, 0 )
+    return sum
+  }
 
-  avg() {}
+  avg() {
+    if (this.items.length === 0){throw new Error('EmptySortedList')}
+    let sum = this.items.reduce((acc , val) => acc + val, 0 )
+    return sum/this.items.length
+  }
 }
 
 module.exports = SortedList;
