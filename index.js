8af50e1ef0324dc485fea45a46f6b34bcffc2f01
Enrique Donaire <donaire.q2@gmail.com>
2021-06-22 17:41:07 +0200

    LAB-intro-node-RITA & ENRIQUE

--- index.js
+++ index.js
@@ -1,17 +1,64 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = []
+    this.length = 0
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item)
+    this.length += this.items.length
+    this.items.sort(function(a,b) {
+    return a - b;
+  })
+  console.log(`sored: ${this.items}`)
+  }
+  get(pos) {
+    
+    for (let i = 0; i < this.items.length; i++){
+      if(pos == this.items[i]){
+        return this.items.indexOf(this.items[i])
+      }
+      
+      
+    }
+    this.items.filter((item) => item.includes(pos) ? this.items.indexOf(item) : "OutOfBounds")
+  }
 
-  get(pos) {}
+  max() {
+      if(this.items.length > 0){
+      return Math.max(...this.items)
+      }
+      else {
+        throw new Error('EmptySortedList');
+      }
+  }
+  
 
-  max() {}
+  min() {
+    if(this.items.length > 0){
+      return Math.min(...this.items)
+      }
+      else {
+        throw new Error('EmptySortedList');
+      }
 
-  min() {}
+  }
 
-  sum() {}
+  sum() {
+    return  this.items.reduce((a, b) => a + b, 0)
+  }
 
-  avg() {}
+  avg() {
+    let sum = this.items.reduce((a, b) => a + b, 0)
+    if (sum > 0){
+      return sum / this.items.length
+
+    }
+    else {
+      throw new Error('EmptySortedList');
+
+    }
+  }
 }
 
 module.exports = SortedList;
