cfce097c8ad24946f03b0dceab9f90058ae1a384
Ellen Children <ellenchildren@icloud.com>
2021-06-22 16:37:07 +0200

    completed lab

--- index.js
+++ index.js
@@ -1,17 +1,60 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a,b) => a - b);
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos > this.length) {
+      throw new Error('OutOfBounds');
+    } else {
+      return this.items[pos]
+    }
+  }
 
-  max() {}
+  max() {
+    if (this.length == 0) {
+      throw new Error('EmptySortedList');
+    } else {
+      return this.items[this.length-1]
+    }
+  }
 
-  min() {}
+  min() {
+    if (this.length == 0) {
+      throw new Error('EmptySortedList');
+    } else {
+      return this.items[0]
+    }
+  }
 
-  sum() {}
+  // should return the sum of all elements in the list
+  // should return 0 for an empty sorted list
+  sum() {
+    const reducer = (accumulator, currentValue) => accumulator + currentValue;
+    if (this.length == 0) {
+      return 0
+    } else {
+      return this.items.reduce(reducer)
+    }
+  }
 
-  avg() {}
+  // should return an EmptySortedList exception if there are no elements
+  // should return the average of elements in the list
+  avg() {
+    const reducer = (accumulator, currentValue) => accumulator + currentValue;
+    if (this.length == 0) {
+      throw new Error('EmptySortedList');
+    } else {
+      return this.items.reduce(reducer) / this.length;
+    }
+  }
 }
 
 module.exports = SortedList;
