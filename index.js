a269c86a923db1a18950c761d37f0df2cedc6591
Joanne Seiler <joanne@posteo.de>
2021-06-22 16:43:52 +0200

    lab done

--- index.js
+++ index.js
@@ -1,17 +1,65 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = []
+    this.length = 0
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.length ++;
+    this.items.sort((a, b) => {
+      if (a > b) {
+        return 1
+      }
+      else if (b > a) {
+        return -1
+      }
+      else {
+        return 0
+      }
+    })
+    return this.items
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos > this.length) {
+      throw new Error('OutOfBounds');
+    }
 
-  max() {}
+    return this.items[pos]
+  }
 
-  min() {}
+  max() {
+    if (this.length === 0) {
+      throw new Error('EmptySortedList');
+    }
+    return this.items[this.length-1]
+  }
 
-  sum() {}
+  min() {
+    if (this.length === 0) {
+      throw new Error('EmptySortedList');
+    }
+    return this.items[0]
+  }
+  
+  sum() {
+    if (this.items.length === 0) {
+      return 0
+    }
 
-  avg() {}
+    return this.items.reduce((sum,elem)=> {
+      return sum + elem
+    }, 0)
+  }
+
+  avg() {
+    if (this.length === 0) {
+      throw new Error('EmptySortedList');
+    }
+    
+    return this.sum() / this.length
+  }
 }
 
 module.exports = SortedList;
