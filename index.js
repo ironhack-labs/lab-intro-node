2d5522f7d9998f839d5c0ddcd5ace91fb4167e8d
Josue Prieto-Paz <jprietopaz3@gatech.edu>
2021-06-22 16:33:12 +0200

    finished lab

--- index.js
+++ index.js
@@ -1,17 +1,69 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
+  add(item) {
+    let chosenI = 0;
+    let complete = false;
 
-  add(item) {}
+    for (let i = 0; i < this.length; i++ ){
+      if (item <= this.items[i]){
+        chosenI = i;
+        this.items.splice(chosenI, 0, item);
+        this.length = this.items.length;
+        complete = true;
+        break;
+      }
+      }
+    if (complete == false){
+      this.items.push(item);
+      this.length = this.items.length;
+    }
+    
+    
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos >= this.length){
+      throw new Error('OutOfBounds');
+    }else{
+      let chosen = this.items[pos];
+      return chosen;
+    }}
 
-  max() {}
+  max() {
+    if (this.items.length === 0){
+      throw new Error('EmptySortedList');
+    }
+    let x = Math.max(...this.items);
+    return x;
+  }
 
-  min() {}
+  min() {
+    if (this.items.length === 0){
+      throw new Error('EmptySortedList');
+    }
+    let x = Math.min(...this.items);
+    return x;
+  }
 
-  sum() {}
+  sum() {
+   let fsum =  this.items.reduce((a, b) => a + b, 0)
+   return fsum;
+  }
 
-  avg() {}
+  avg() {
+    let faverage;
+    let fsum =  this.items.reduce((a, b) => a + b, 0)
+    if (fsum === 0){
+      throw new Error('EmptySortedList');
+    }
+    else{
+     faverage = fsum/this.length;
+     return faverage;
+    }
+  }
 }
 
 module.exports = SortedList;
