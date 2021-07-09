7475fc48f9022bcc5a141f31723533830214b3f8
Tiago R <tiagobiagirebelato@gmail.com>
2021-07-09 18:53:21 -0300

    Feito

--- index.js
+++ index.js
@@ -1,17 +1,68 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = []
+    this.length = this.items.length
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item)
+    this.items.sort((a,b)=> a-b)
+    this.length = this.items.length
+  }
+  get(pos) {
+    if(this.items[pos]){
+      return this.items[pos]
+    }else{
+      throw new Error('OutOfBounds');
+    }
+  }
+  max() {
+    if(!this.items[0]){
+      throw new Error('EmptySortedList');
+      
+      
+    }else{
+      return this.items[this.items.length - 1]
+      
+      
+    }
+  }
 
-  get(pos) {}
+  min() {
+    if(!this.items[0]){
+      throw new Error('EmptySortedList');
+      
+      
+    }else{
+      return this.items[0]
+      
+      
+    }
+  }
 
-  max() {}
+  sum(){
+    if(!this.items[0]){
+    return 0
+    }else{
+    let sum = this.items.reduce((acc,item)=>{acc+=item
+    return acc},0)
+    
+    return sum 
+    }
+  }
 
-  min() {}
-
-  sum() {}
-
-  avg() {}
+  avg() {
+    if(!this.items[0]){
+      throw new Error('EmptySortedList');
+      }else{
+      let avg = this.items.reduce((acc,item)=>{acc+=item
+      return acc},0)/this.items.length
+      
+      return avg
+      }
+  }
 }
 
 module.exports = SortedList;
+
+
