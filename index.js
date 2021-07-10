ffa9ee069ba55fc839c7cb180165e7b55e5c7596
kadu <kadu999@gmail.com>
2021-07-10 00:52:04 -0300

    done

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items =[]
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  
 
-  get(pos) {}
+  add(item) {
+    this.items.push(item)
+    this.length = this.items.length;
+    this.items.sort((a, b) => {
+      return a - b;
+    });
+   
+  }
+
+  get(pos) {
+    if (pos >= this.length) {
+      throw new Error("outOfBounds");
+    }
+    return this.items[pos];
+  }
 
   max() {}
+
+  //throw new Error('EmptySortedList');
+
 
   min() {}
 
