8c1dd1671d909581f8fcd411f79adaf16436560f
P4tt1XD <patti@outlook.com.br>
2021-07-09 21:32:10 -0300

    Done

--- index.js
+++ index.js
@@ -1,17 +1,50 @@
 class SortedList {
-  constructor() {}
+  constructor(items, length) {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items = this.items.sort((a, b) => a-b)
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if(this.items[pos]){
+      return this.items[pos];
+    }else{
+      throw new Error('Out of Bounds');
+    }
+  }
 
-  max() {}
+  max() {
+    if(this.items.length === 0){
+      throw new Error('Empty Sorted List');
+    }else return this.items[this.items.length - 1];
+  }
 
-  min() {}
+  min() {
+    if(this.items.length ===0 ){
+      throw new Error('Empty Sorted List');
+    }else{
+      return this.items[0];
+    }
+  }
 
-  sum() {}
+  sum() {
+    return this.items.reduce((a, b =>{
+      return a + b;
+    }, 0))
+  }
 
-  avg() {}
+  avg() {
+    if(this.items.length === 0){
+      throw new Error('Empty Sorted List');
+    }else{
+      return(this.sum() / this.length);
+    }
+  }
 }
 
 module.exports = SortedList;
