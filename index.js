cc81a07655cf19402f0609dc8c0cccc110246896
franziska pendzialek <franziska.pendzialek@outlook.com>
2021-07-15 20:50:19 +0200

    add bonus

--- index.js
+++ index.js
@@ -6,54 +6,48 @@ class SortedList {
 
   add(item) {
     this.items.push(item);
-    this.items.sort((a,b)=> {
+    this.items.sort((a, b) => {
       return a - b;
-    })
+    });
     this.length++;
   }
 
   get(pos) {
     if (pos >= this.items.length) {
-      throw new Error('OutOfBounds');
-    } else {
-      return this.items[pos];
+      throw new Error("OutOfBounds");
     }
+    return this.items[pos];
   }
 
   max() {
     if (this.items.length === 0) {
-      throw new Error('EmptySortedList');
-    } else {
-      return Math.max(...this.items);
+      throw new Error("EmptySortedList");
     }
+    return Math.max(...this.items);
   }
 
   min() {
     if (this.items.length === 0) {
-      throw new Error('EmptySortedList');
-    } else {
-      return Math.min(...this.items);
+      throw new Error("EmptySortedList");
     }
+    return Math.min(...this.items);
   }
 
   sum() {
-    let sum;
     if (this.items.length === 0) {
       return 0;
-    } else {
-      for (let i = 0; i < this.items.length; i++) {
-        sum = sum + this.items[i];
-      }
-      return sum;
     }
+    let sumArray = this.items.reduce((accumulator, currentValue) => {
+      return accumulator + currentValue;
+    });
+    return sumArray;
   }
 
   avg() {
     if (this.items.length === 0) {
-      throw new Error('EmptySortedList');
-    } else {
-      return this.sum / this.items.length;
+      throw new Error("EmptySortedList");
     }
+    return this.sum() / this.items.length;
   }
 }
 
