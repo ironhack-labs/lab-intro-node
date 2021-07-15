25eaba381bd7f6f4c4888dff52ecf6354c54e8a2
juliahaeussler <julia@haeuszler.de>
2021-07-15 21:45:08 +0200

    done

--- index.js
+++ index.js
@@ -1,17 +1,55 @@
 class SortedList {
-  constructor() {}
+  constructor(items, length) {
+    this.items = [];
+    this.length = this.items.length
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort(function(a,b){return a-b});
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos >= this.items.length) {
+      throw new Error('OutOfBounds');
+    } 
+    return this.items[pos]
+  }
 
-  max() {}
+  max() {
+    if (this.items == 0) {
+      throw new Error('EmptySortedList');
+    }
+    return Math.max(...this.items);
+  }
 
-  min() {}
+  min() {
+    if (this.items == 0) {
+      throw new Error('EmptySortedList');
+    }
+    return Math.min(...this.items);
+  }
 
-  sum() {}
+  sum() {
+    this.sum = 0;
+    for (let i=0; i<=this.items.length; i++){
+      this.sum += i
+    }
+    return this.sum
+  }
 
-  avg() {}
+  avg() {
+    if (this.items == 0) {
+      throw new Error('EmptySortedList');
+    }
+    this.sum = 0;
+    for (let i=0; i<=this.items.length; i++){
+      this.sum += i
+    }
+    return this.sum/this.items.length;
+  }
 }
 
 module.exports = SortedList;
+
