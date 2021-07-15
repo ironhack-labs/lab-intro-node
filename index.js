b56a12f382dc9f7956e4f0b244a8a67a06ddf78f
franziska pendzialek <franziska.pendzialek@outlook.com>
2021-07-15 20:41:10 +0200

    solved lab except bonus

--- index.js
+++ index.js
@@ -1,17 +1,60 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a,b)=> {
+      return a - b;
+    })
+    this.length++;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos >= this.items.length) {
+      throw new Error('OutOfBounds');
+    } else {
+      return this.items[pos];
+    }
+  }
 
-  max() {}
+  max() {
+    if (this.items.length === 0) {
+      throw new Error('EmptySortedList');
+    } else {
+      return Math.max(...this.items);
+    }
+  }
 
-  min() {}
+  min() {
+    if (this.items.length === 0) {
+      throw new Error('EmptySortedList');
+    } else {
+      return Math.min(...this.items);
+    }
+  }
 
-  sum() {}
+  sum() {
+    let sum;
+    if (this.items.length === 0) {
+      return 0;
+    } else {
+      for (let i = 0; i < this.items.length; i++) {
+        sum = sum + this.items[i];
+      }
+      return sum;
+    }
+  }
 
-  avg() {}
+  avg() {
+    if (this.items.length === 0) {
+      throw new Error('EmptySortedList');
+    } else {
+      return this.sum / this.items.length;
+    }
+  }
 }
 
 module.exports = SortedList;
