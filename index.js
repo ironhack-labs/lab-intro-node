f96f3067695a0e1f36d706e3e8e23fa741d89a3d
Peter Mayer <peter_my@gmx.de>
2021-07-27 12:29:16 +0200

    Finished Lab

--- index.js
+++ index.js
@@ -1,17 +1,55 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = 0;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a, b) => a - b);
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if(this.items[pos] === undefined) {
+      throw new Error('OutOfBounds');
+    }
 
-  max() {}
+    return this.items[pos];
+  }
 
-  min() {}
+  max() {
+    if(this.items.length === 0) {
+      throw new Error('EmptySortedList');
+    }
 
-  sum() {}
+    return this.items[this.items.length-1];
+  }
 
-  avg() {}
+  min() {
+    if(this.items.length === 0) {
+      throw new Error('EmptySortedList');
+    }
+
+    return this.items[0];
+  }
+
+  sum() {
+    if(this.items.length === 0) {
+      return 0;
+    }
+
+    return this.items.reduce( (prev, curr) => prev + curr );
+  }
+
+  avg() {
+    if(this.items.length === 0) {
+      throw new Error('EmptySortedList');
+    }
+
+    let sum = this.items.reduce( (prev, curr) => prev + curr );
+    return sum / this.items.length;
+  }
 }
 
 module.exports = SortedList;
