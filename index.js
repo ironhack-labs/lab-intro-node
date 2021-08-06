97e974902aaae87b75ec75c695be70ae4c5d106b
kpla001 <kpla001@fiu.edu>
2021-08-05 22:14:40 -0400

    finish lab-intro-node

--- index.js
+++ index.js
@@ -1,17 +1,52 @@
 class SortedList {
-  constructor() {}
+  constructor(items, length) {
+    this.items = [];
+    this.length = this.items.length;
 
-  add(item) {}
+  }
 
-  get(pos) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a,b) => a - b);
+    this.length = this.items.length;
+  }
 
-  max() {}
+  get(pos) {
+    if (pos > this.length){
+      throw new Error('OutOfBounds');
+    }
+    return this.items[pos];
+  }
 
-  min() {}
+  max() {
+    if (this.length === 0){
+      throw new Error('EmptySortedList');
+    }
 
-  sum() {}
+    return Math.max(...this.items);
+  }
 
-  avg() {}
+  min() {
+    if (this.length === 0){
+      throw new Error('EmptySortedList');
+    }
+
+    return Math.min(...this.items);
+  }
+
+  sum() {
+    if (this.length === 0){
+      return 0
+    }
+    return this.items.reduce((acc, item) => acc + item);
+  }
+
+  avg() {
+    if (this.length === 0){
+      throw new Error('EmptySortedList');
+    }
+    return this.sum() / this.length;
+  }
 }
 
 module.exports = SortedList;
