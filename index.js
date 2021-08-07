988bc3f35c7ab05e7be8d868a6202285a09600b9
“Michael <michaelrodiles@gmail.com>
2021-08-07 11:00:41 -0400

    Solved Lab

--- index.js
+++ index.js
@@ -1,14 +1,41 @@
 class SortedList {
-  constructor() {}
+  constructor(items, length) {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a, b) => a - b);
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos > this.length) {
+      throw new Error('OutOfBounds');
+    }
+    return this.items[pos];
 
-  max() {}
 
-  min() {}
+  }
 
+  max() {
+    if (items.length === 0) {
+      throw new Error('EmptySortedList');
+    }
+
+    return Math.max(...this.items);
+  }
+
+  min() {
+    if (items.length === 0) {
+      throw new Error('EmptySortedList');
+    }
+
+    return Math.min(...this.items);
+  }
+
+  //Not Passing FOR SOME REASON???????? HELP????//
   sum() {}
 
   avg() {}
