e62088b66c0ad79ec6494e53084ecade9d5d5858
carloshoa <carloshoarantes@gmail.com>
2021-08-28 09:48:35 -0300

    Lab done

--- index.js
+++ index.js
@@ -1,17 +1,54 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a,b)=>a-b);
+    this.length = this.items.length;    
+  }
 
-  get(pos) {}
+  get(pos) {
+    if(pos>this.length){
+      throw new Error('OutOfBounds');
+    }
 
-  max() {}
+    return this.items[pos];
 
-  min() {}
+  }
 
-  sum() {}
+  max() {
+    if(!this.length){
+      throw new Error('EmptySortedList');
+    }
+    return Math.max(...this.items);
 
-  avg() {}
+  }
+
+  min() {
+    if(!this.length){
+      throw new Error('EmptySortedList');
+    }
+    return Math.min(...this.items);
+
+  }
+
+  sum() {
+   
+    return this.items.reduce((acc,item)=>acc+item,0);
+  
+  }
+
+  avg() {
+    if(!this.length){
+      throw new Error('EmptySortedList');
+    }
+
+    return this.sum()/this.length;
+   
+  }
 }
 
 module.exports = SortedList;
