1559f0b49c22e67b3d2faf6aa24e0febca2faa42
Guilherme Araújo Oliveira <guilhermearaujooliveira4@gmail.com>
2021-08-28 11:30:38 -0300

    feito

--- index.js
+++ index.js
@@ -1,17 +1,64 @@
 class SortedList {
-  constructor() {}
+    constructor() {
+        this.items = [];
+        this.length = this.items.length;
+    }
 
-  add(item) {}
+    add(item) {
+        // Add a new item - OK
+        // Sort in ascending order - OK
+        // Update length property - OK
 
-  get(pos) {}
+        this.items.push(item);
 
-  max() {}
+        this.sortList();
+        this.updateLength();
+    }
 
-  min() {}
+    sortList() {
+        this.items.sort((a, b) => a - b);
+    }
 
-  sum() {}
+    updateLength() {
+        this.length = this.items.length;
+    }
 
-  avg() {}
+    get(pos) {
+        if (this.items[pos]) return this.items[pos];
+
+        throw new Error('OutOfBounds');
+    }
+
+    verifyLength() {
+        if (this.length === 0) {
+            throw new Error('EmptySortedList');
+        }
+    }
+
+    max() {
+        this.verifyLength();
+
+        return this.items[this.length - 1];
+    }
+
+    min() {
+        this.verifyLength();
+
+        return this.items[0];
+    }
+
+    sum() {
+        return this.items.reduce(
+            (acc, number) => acc + number,
+            0,
+        );
+    }
+
+    avg() {
+        this.verifyLength();
+
+        return this.sum() / this.length;
+    }
 }
 
 module.exports = SortedList;
