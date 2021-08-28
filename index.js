20d5bb8658a0c2701fd480ff4cd5e71a77472efd
Carlos Daniel <caduccino@live.com>
2021-08-28 11:44:23 -0300

    done

--- index.js
+++ index.js
@@ -1,17 +1,51 @@
 class SortedList {
-  constructor() {}
+  constructor(items, length) {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a, b) => a - b);
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (this.items[pos]){
+      return this.items[pos]
+    } else{
+      throw new Error('OutOfBounds');  
+    } 
+  }
 
-  max() {}
+  max() {
+    if (this.items.length === 0) {
+      throw new Error('EmptySortedList');
+    }else{
+      return this.items[this.length -1];
+    }
+  }
 
-  min() {}
 
-  sum() {}
+  min() {
+    if (this.items.length === 0) {
+      throw new Error('EmptySortedList');
+    }else{
+      return this.items[0];
+    }
+  }
 
-  avg() {}
+  sum() {
+    return this.items.reduce((a, b) => a + b, 0);
+  }
+
+  avg() {
+    if (this.items.length === 0) {
+      throw new Error('EmptySortedList');
+    }else{
+      return this.sum() / this.length
+    }
+  }
 }
 
 module.exports = SortedList;
