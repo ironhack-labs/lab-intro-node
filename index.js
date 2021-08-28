ecc2961ec6c4ce5d5a0a1ce1542f7102e0e4e2b2
Renan Bartalo <renan.bartalo@outlook.com>
2021-08-28 09:32:26 -0300

    lab done

--- index.js
+++ index.js
@@ -1,17 +1,50 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = 0;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a,b) => a-b);
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if(pos > this.length){
+      throw new Error('OutOfBounds');
+    }else{
+      return this.items[pos];
+    }
+  }
 
-  max() {}
+  max() {
+    if(this.length === 0){
+      throw new Error('EmptySortedList');
+    }else{
+      return this.items[this.length -1];
+    }
+  }
 
-  min() {}
+  min() {
+    if(this.length === 0){
+      throw new Error('EmptySortedList');
+    }else{
+      return this.items[0];
+    }
+  }
 
-  sum() {}
+  sum() {
+    return this.length === 0 ? 0 : this.items.reduce((total, elem) => total + elem);
+  }
 
-  avg() {}
+  avg() {
+    if(this.length === 0){
+      throw new Error('EmptySortedList');
+    }else{
+      return this.sum() / this.length;
+    }
+  }
 }
 
 module.exports = SortedList;
