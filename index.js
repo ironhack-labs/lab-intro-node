592572e0958a7af459a8f83620a81ffe5dfebf40
AitorSantaeugenia <aitor.j.s.m@gmail.com>
2021-08-31 00:33:08 +0200

    lab done

--- index.js
+++ index.js
@@ -1,17 +1,62 @@
 class SortedList {
-  constructor() {}
+	constructor(items, length) {
+		this.items = [];
+		this.length = this.items.length;
+	}
 
-  add(item) {}
+	add(item) {
+		this.items.push(item);
+		this.items.sort((a, b) => a - b);
+		this.length = this.items.length;
+	}
 
-  get(pos) {}
+	get(pos) {
+		if (pos > this.length) {
+			throw new Error('OutOfBounds');
+		} else {
+			return this.items.indexOf(pos);
+		}
 
-  max() {}
+		// return pos
+		// 	? this.items.indexOf(pos)
+		// 	: (function() {
+		// 			throw new Error('OutOfBounds');
+		// 		})();
+	}
 
-  min() {}
+	max() {
+		if (!this.length) {
+			throw new Error('EmptySortedList');
+		}
 
-  sum() {}
+		return Math.max(...this.items);
+	}
 
-  avg() {}
+	min() {
+		if (!this.length) {
+			throw new Error('EmptySortedList');
+		}
+
+		return Math.min(...this.items);
+	}
+
+	/*
+  const reducer = (accumulator, currentValue) => accumulator + currentValue;
+
+  // 1 + 2 + 3 + 4
+  console.log(array1.reduce(reducer));
+  */
+	sum() {
+		return this.items.reduce((total, value) => total + value, 0);
+	}
+
+	avg() {
+		if (!this.length) {
+			throw new Error('EmptySortedList');
+		}
+
+		return this.sum() / this.length;
+	}
 }
 
 module.exports = SortedList;
