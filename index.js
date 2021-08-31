68ab51d6c4c0f4a4e8e3016bc3c4a9a87235352f
Esteban <esteban.github@gmail.com>
2021-08-31 12:49:12 +0200

    done

--- index.js
+++ index.js
@@ -1,17 +1,55 @@
 class SortedList {
-  constructor() {}
-
-  add(item) {}
-
-  get(pos) {}
-
-  max() {}
-
-  min() {}
-
-  sum() {}
-
-  avg() {}
+  constructor() {
+    this.items = [],
+    this.length = this.items.length
+  }
+  
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a, b) => a - b);
+    this.length = this.items.length;
+  }
+  
+  get(pos) {
+    if (pos > this.items.length-1) {
+      throw new Error('OutOfBounds');
+    } else {
+      return this.items[pos];
+    }
+  }
+  
+  max() {
+    if (this.items.length === 0) {
+      throw new Error('EmptySortedList');
+    } else {
+      return Math.max(...this.items);
+    }
+  }
+  
+  min() {
+    if (this.items.length === 0) {
+      throw new Error('EmptySortedList');
+    } else {
+      return Math.min(...this.items);
+    }
+  }
+  
+  sum() {
+    if (this.items.length === 0) {
+      return 0;
+    } else {
+      const reducer = (acc, val) => acc + val;
+      return this.items.reduce(reducer);
+    }
+  }
+  
+  avg() {
+    if (this.items.length === 0) {
+      throw new Error('EmptySortedList');
+    } else {
+    return this.sum()/this.length;
+    }
+  }
 }
 
 module.exports = SortedList;
