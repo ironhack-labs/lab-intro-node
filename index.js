c0a5a87ac2e5acb7f0de2b0bb7892c6854460f8d
Jesse Vermeulen <jessevermeulen123@gmail.com>
2021-08-31 18:01:20 +0200

    Now our tests are all in order

--- index.js
+++ index.js
@@ -1,17 +1,65 @@
+class OutOfBounds extends Error {
+
+}
+
+class EmptySortedList extends Error {
+
+}
+
 class SortedList {
-  constructor() {}
+  constructor(items = [], length = 0) {
+    this.items = items;
+    this.length = length
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item)
+    this.items = this.items.sort((a, b) => {
+      if (a < b) {
+        return -1
+      } else if (a === b) {
+        return 0
+      } else {
+        return 1
+      }
+    })
+    this.length++
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos > this.length) {
+      throw new OutOfBounds("Nope")
+    }
+    return this.items[pos]
+  }
 
-  max() {}
+  max() {
+    if (this.length === 0) {
+      throw new EmptySortedList("Nope")
+    }
+    return Math.max(...this.items)
+  }
 
-  min() {}
+  min() {
+    if (this.length === 0) {
+      throw new EmptySortedList("Nope")
+    }
+    return Math.min(...this.items)
+  }
 
-  sum() {}
+  sum() {
+    return this.items.reduce((i, acc) => {
+      acc += i
+      return acc
+    }, 0)
+  }
 
-  avg() {}
+  avg() {
+    if (this.length === 0) {
+      throw new EmptySortedList("Nope")
+    }
+    return this.sum() / this.length
+  }
 }
 
 module.exports = SortedList;
