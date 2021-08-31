84ae8a49ea7c04d9778e2ffb4f4b8c50a4bc65cd
thomaskno <thomas.kno@yahoo.com>
2021-08-31 11:54:19 -0400

    Complete lab

--- index.js
+++ index.js
@@ -1,17 +1,67 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = 0;
+  }
 
-  add(item) {}
+  add(item) {
 
-  get(pos) {}
+  this.items.push(item);
 
-  max() {}
+  this.items.sort(function(a, b) {
+      return a - b;
+    });
 
-  min() {}
+    this.length++;
+  }
 
-  sum() {}
+  get(pos) {
 
-  avg() {}
+    if(pos > this.length - 1 || pos < 0){
+      throw new Error('OutOfBounds');
+    }else{
+      return this.items[pos];
+    }
+  }
+
+  max() {
+    if(this.length == 0){
+      throw new Error('EmptySortedList');
+    }else{
+      return this.items[this.length - 1];
+    }
+  }
+
+  min() {
+    if(this.length == 0){
+      throw new Error('EmptySortedList');
+    }else{
+      return this.items[0];
+    }
+  }
+
+  sum() {
+
+    let sum = 0;
+
+    this.items.forEach(i => {
+
+      sum += i;
+      
+    });
+
+    return sum
+  }
+
+  avg() {
+
+    if(this.length == 0){
+      throw new Error('EmptySortedList');
+    }
+
+    return this.sum() / this.length;
+
+  }
 }
 
 module.exports = SortedList;
