349fba34d6d07cc0db7450bc38497b05f2e3070b
Mathijsvanginneken <mathijsvginneken@gmail.com>
2021-08-31 17:13:05 +0200

    Done

--- index.js
+++ index.js
@@ -1,17 +1,53 @@
 class SortedList {
-  constructor() {}
+  constructor(items, length) {
+    this.items = [];
+    this.length = this.items.length
+  }
 
-  add(item) {}
+  add(item) {
+  this.items.push(item)
+  this.items.sort(function (a, b) {
+    return a - b;
+  })
+  this.length = this.items.length
+  
+  }
+  get(pos) {
+    if (this.length < pos){
+      throw new Error('OutOfBounds');
+    }
+  return this.items.indexOf(pos)
+  }
 
-  get(pos) {}
+  max() {
+    if (this.length === 0){
+      throw new Error('OutOfBounds');
+    }
+  return this.items[this.items.length -1]
+  }
+  
 
-  max() {}
+  min() {
+    if (this.length === 0){
+      throw new Error('OutOfBounds');
+    }
+  return this.items[0]
+  }
+  
 
-  min() {}
+  sum() {
+    if (this.items.length === 0){
+      return 0
+    }
+    return this.items.reduce((a, b) => a + b, 0)
+  }
 
-  sum() {}
-
-  avg() {}
+  avg() {
+    if (this.items.length === 0){
+      throw new Error('EmptySortedList');
+    }
+  return this.sum() / this.length
+  }
 }
 
 module.exports = SortedList;
