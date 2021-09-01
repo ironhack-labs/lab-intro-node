009b675a4d44dc1a538898bdc357a8787606c7c3
Jon Flores <jflore61@ucsc.edu>
2021-09-01 20:17:29 +0200

    done

--- index.js
+++ index.js
@@ -1,17 +1,65 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = []
+    this.length = this.items.length
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a, b) => a - b)
+    this.length = this.items.length
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (this.items[pos] === undefined){
+      throw new Error('OutOfBounds');
+    }else{
+      return this.items[pos];
+    }
+  }
 
-  max() {}
+  max() {
+    if (this.length === 0){
+      throw new Error('EmptySortedList');
+    }else{
+      return this.items[this.length - 1]
+    }
+  }
 
-  min() {}
+  min() {
+    if (this.length === 0){
+      throw new Error('EmptySortedList');
+    }else{
+      return this.items[0]
+    }
+  }
 
-  sum() {}
+  sum() {
+    let sum =0;
+    if (this.length === 0){
+      sum = 0
+    }else{
+      for (let item of this.items){
+      sum += item
+      
+      }
+    }
+    return sum
+  }
 
-  avg() {}
+  avg() {
+    let sum =0;
+    if (this.length === 0){
+      throw new Error('EmptySortedList');
+    }else{
+      for (let item of this.items){
+      sum += item
+      
+      }
+    }
+    return sum / this.length
+  }
 }
+console.log(new SortedList)
 
 module.exports = SortedList;
