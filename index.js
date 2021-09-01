8d6b933157e0fe9bebbfabb941d45ac071cbaddd
andalucinando <58915394+andalucinando@users.noreply.github.com>
2021-09-01 12:30:57 +0200

    lab solved

--- index.js
+++ index.js
@@ -1,17 +1,58 @@
 class SortedList {
-  constructor() {}
 
-  add(item) {}
+  constructor() {
+    this.items = []; 
+    this.length = 0; 
+  }
 
-  get(pos) {}
 
-  max() {}
 
-  min() {}
+  add(item) {
+    this.items.push(item)
+    this.items.sort((a,b) => a.b)
+    this.length = this.item.length; 
+  }
 
-  sum() {}
+  get(pos) {
+    if ( pos < this.length){
+      return this.item[pos]
+    } else {
+      throw new Error('OutOfBounds');
+    }
+  } 
 
-  avg() {}
+  max() {
+    if ( this.length === 0){
+      throw new Error ('EmplySortedList');
+    } else {
+      const max = this.item.reduce((a,b) => a-b)
+    }
+
+  }
+
+  min() {
+    if ( this.length === 0){
+      throw new Error ('EmplySortedList');
+    } else {
+      const max = this.item.reduce((a,b) => a+b)
+    }
+
+  }
+
+  sum() {
+    const sum = this.item.reduce((a,b) => a+b, 0)
+    return sum; 
+  }
+
+  avg() {
+    if ( this.length === 0){
+      throw new Error ('EmplySortedList');
+    } else {
+      const avg = this.sum() / this.length
+      return avg
+    }
+
+  }
 }
 
 module.exports = SortedList;
