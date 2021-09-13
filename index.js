17a9fc2d9a8c3677df9216ff7c1c0eff98872bdb
Pool Ortega <poolortegas@gmail.com>
2021-09-13 15:23:50 -0500

    DONE

--- index.js
+++ index.js
@@ -1,17 +1,74 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items=[]
+    this.length=this.items.length
+  }
 
-  add(item) {}
+  add(item) {
 
-  get(pos) {}
+    this.items.push(item)
+    this.length=this.items.length
+    this.items.sort((a,b)=>{
+      return a-b
+    })
 
-  max() {}
+  }
 
-  min() {}
+  get(pos) {
+    if (pos>this.items.length){
+      throw new Error('OutOfBounds');
+    }
+    return this.items[pos]
+  }
 
-  sum() {}
+  max() {
+    let mayor=0
+    if(this.items.length==0){
+      throw new Error('EmptySortedList');
+    }
+    for(let i =0;i<this.items.length-1;i++){
+      if(this.items[i]>this.items[i+1]){
+        mayor=this.items[i]
+      } else {
+        mayor=this.items[i+1]
+      }
+    }
+    return mayor   
+  }
 
-  avg() {}
+  min() {
+    let menor=10;
+    if(this.items.length==0){
+      throw new Error('EmptySortedList');
+    }
+    for(let i =0; i<this.items.length-1;i++){
+      if(this.items[i]<this.items[i+1]){
+        menor=this.items[i];
+      } else{
+        menor=this.items[i+1];
+      }
+    }
+      return menor;
+  }
+
+  sum() {
+    if(this.items.length==0){
+      return [];
+    }
+   const suma= this.items.reduce((ac,av)=>{
+      return ac+av
+    },0);
+    return suma
+  }
+
+  avg() {
+    if(this.items.length==0){
+      throw new Error('EmptySortedList');
+    }
+    let ssum=this.sum(this.items);
+    let avg=ssum/this.items.length
+    return avg
+  }
 }
 
 module.exports = SortedList;
