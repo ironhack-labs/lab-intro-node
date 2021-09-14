dfcd7ae42912fadba36e7a4ff6cc730887628d22
andoni.resendiz <andoniresendiz@gmail.com>
2021-09-13 22:57:33 -0500

    lab

--- index.js
+++ index.js
@@ -1,13 +1,39 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [] //items should be an array
+    this.length = this.items.length //should be the number of elements in the array
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item) //add value item to the items array
+    this.items.sort()// ensuring that the items array stays sorted in ascending order
+    this.length = this.items.length
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (this.items[pos] === undefined){
+      throw new Error('OutOfBounds');
+    }else{
+      return this.items[pos];
+    }
 
-  max() {}
+  }
 
-  min() {}
+  max() {
+    if(this.length === 0){
+      throw new Error('EmptySortedList');
+    }else{
+      return this.items[this.length - 1]
+    }
+  }
+
+  min() {
+    if(this.length === 0){
+    throw new Error('EmptySortedList');
+    }else{
+      return this.items[0]
+    }
+  }
 
   sum() {}
 
