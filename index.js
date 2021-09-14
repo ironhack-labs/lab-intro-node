fd78ee93449b17ad8237e24565c0e22b9a5fd1af
Diana Gomez <dianaegc094@gmail.com>
2021-09-13 21:26:34 -0400

    Lab done

--- index.js
+++ index.js
@@ -1,17 +1,60 @@
 class SortedList {
-  constructor() {}
+  constructor(items,length) {
+    this.items=[];
+    this.length=this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+ this.items.push(item);// agregando el item al array
+ this.items.sort((a,b)=>a-b );//orden ascendente
+ this.length=this.items.length//
+  }
+  get(pos) {
+    if(this.items[pos]===undefined){
+      throw new Error('OutOfBounds');
+    }else{
+      return this.items[pos];
+    }   
+  }
 
-  get(pos) {}
+  max() { //  devuelve el valor mas alto
+    if(this.length===0){
+      throw new Error('EmptySortedList');
+    }else{
+       
+    return this.items[this.length-1] // para que tome la ultima posicion del array 
+    }
+  }
 
-  max() {}
+  min() {
+    if(this.length===0){
+      throw new Error('EmptySortedList');
+    }else{
+      return this.items[0]//en la primera posicion del array
+    }
+  }
 
-  min() {}
+  sum() {
+  let suma=0;
+  for(let i=0;i<this.items.length;i++){
+     suma += this.items[i]
+  }
+  
+  return suma
+  }
 
-  sum() {}
+  avg() {
+    if(this.length===0){
+      throw new Error('EmptySortedList');
+    }else {
+    let avg=0;
+    for(let i=0;i<this.items.length;i++){
+avg += this.items[i]
+    }
+  return avg/this.length;
 
-  avg() {}
+  }
+  }
 }
 
 module.exports = SortedList;
