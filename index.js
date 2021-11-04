d680867113a854d8996595b47157bd9f2211ca36
Lilli Reilly <lilli.reilly@gmail.com>
2021-11-04 21:07:23 +0100

    Completed lab

--- index.js
+++ index.js
@@ -1,17 +1,51 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
+  
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a, b) => a - b)
+    this.length = this.items.length
+  }
 
-  add(item) {}
+  get(pos) {
+    
+    if (pos > this.items.length) {
+    throw new Error('OutOfBounds');
+    }
+    return this.items[pos]
+  }
 
-  get(pos) {}
+  max() {
+    if (this.items.length === 0) {
+      throw new Error('EmptySortedlist')
+    }
+    return this.items[this.items.length - 1]
+  }
 
-  max() {}
+  min() {
+    if(this.items.length === 0) {
+      throw new Error('EmptySortedlist')
+    }
+    return this.items[0]
+  }
 
-  min() {}
+  sum() {
+    if (this.items.length === 0) {
+      return 0;
+    }
+    let sumArray = this.items.reduce((sum, el) => sum + el, 0)
+    return sumArray;
+  }
 
-  sum() {}
-
-  avg() {}
+  avg() {
+    if (this.items.length === 0) {
+      throw new Error('EmptySortedList')
+    }
+    return (this.items.reduce((sum, el) => sum + el, 0)) / this.items.length
+  }
 }
 
 module.exports = SortedList;
