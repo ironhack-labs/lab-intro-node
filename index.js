803e930906e46d475b521994089e231fc4a82cc7
phimanim <aliker.imane@gmail.com>
2021-11-04 21:00:32 +0100

    lab done

--- index.js
+++ index.js
@@ -1,17 +1,51 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = []; 
+    this.length = this.items.length; 
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a,b) => a - b);
+    this.length = this.items.length; 
+  }
 
-  get(pos) {}
+  get(pos) {
+    if ( pos < this.items.length){
+      return this.items[pos];
+    } else {
+      throw new Error('OutOfBounds');
+    }
+  }
 
-  max() {}
+  max() {
+    if (this.items.length === 0){
+      throw new Error ('EmplySortedList');
+    } else {
+      return this.items[this.items.length - 1];
+    }
+  }
 
-  min() {}
+  min() {
+    if (this.length === 0){
+      throw new Error ('EmplySortedList');
+    } else {
+      return this.items[0];
+    }
+  }
 
-  sum() {}
+  sum() {
+    const sum = this.items.reduce((a,b) => a+b, 0)
+    return sum; 
+  }
 
-  avg() {}
+  avg() {
+    if (this.length === 0){
+    throw new Error ('EmplySortedList');
+  } else {
+    const avg = this.sum() / this.length
+    return avg
+  }}
 }
 
 module.exports = SortedList;
