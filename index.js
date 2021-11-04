081d296115aa23e9c86a58a5a8cd9d5988aa6cba
Philipp Kastl <picastle@posteo.de>
2021-11-04 21:31:03 +0100

    done

--- index.js
+++ index.js
@@ -1,17 +1,53 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item)
+    this.length = this.items.length
+    this.items.sort((a, b) => a - b);
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (this.items[pos]) {
+      return this.items[pos]
+    } else {
+      throw (error("Out of Bound"))
+    }
+  }
 
-  max() {}
+  max() {
+    if (this.items.length === 0) {
+      throw (error('EmptySortedList'))
+    }
+    return this.items.pop();
+    //return Math.min(this.items)
+  }
 
-  min() {}
+  min() {
+    if (this.items.length === 0) {
+      throw (error('EmptySortedList'))
+    }
+    return this.items.shift();
+  }
 
-  sum() {}
+  sum() {
+    if (this.items.length === 0) {
+      return 0;
+    }
+    return this.items.reduce((acc, currentValue) => acc + currentValue, 0);
+  }
 
-  avg() {}
+
+  avg() {
+    if (this.items.length === 0) {
+      throw (error('EmptySortedList'))
+    }
+    return this.sum() / this.items.length;
+  }
+
 }
 
 module.exports = SortedList;
