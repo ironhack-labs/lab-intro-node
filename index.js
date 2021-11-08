3d4594065700590984ac14f86c7d4b84cce9cfa0
Santi Jimenez <jimenez.santi@gmail.com>
2021-11-08 15:53:06 +0100

    Lab done. Format done

--- index.js
+++ index.js
@@ -11,28 +11,34 @@ class SortedList {
   }
 
   get(pos) {
-    if(pos >= this.length) throw new Error('OutOfBounds');
+    if (pos >= this.length) throw new Error("OutOfBounds");
     return this.items[pos];
   }
 
   max() {
-    if(this.length === 0) throw new Error('EmptySortedList');
+    if (this.length === 0) throw new Error("EmptySortedList");
     return this.get(this.length - 1);
   }
 
   min() {
-    if(this.length === 0) throw new Error('EmptySortedList');
+    if (this.length === 0) throw new Error("EmptySortedList");
     return this.get(0);
   }
 
   sum() {
-    if(this.length === 0) return 0;
-    return this.items.reduce((accumulator, currentValue) => accumulator + currentValue);
+    if (this.length === 0) return 0;
+    return this.items.reduce(
+      (accumulator, currentValue) => accumulator + currentValue
+    );
   }
 
   avg() {
-    if(this.length === 0) throw new Error('EmptySortedList');
-    return Math.floor(this.items.reduce((accumulator, currentValue) => accumulator + currentValue/this.length));
+    if (this.length === 0) throw new Error("EmptySortedList");
+    return Math.floor(
+      this.items.reduce(
+        (accumulator, currentValue) => accumulator + currentValue / this.length
+      )
+    );
   }
 }
 
