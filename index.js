877f2ecae8643e8050d6a29d7f20481bc2fa57d1
Raquel <90280742+raqhidcor@users.noreply.github.com>
2021-11-08 17:02:00 +0100

    Iteration 5 completed

--- index.js
+++ index.js
@@ -30,6 +30,11 @@ class SortedList {
   }
 
   min() {
+    if(this.length === 0){
+      throw new Error('EmptySortedList');
+    } else{
+      return Math.min.apply(null,this.items)
+    }
   }
 
   sum() {}
