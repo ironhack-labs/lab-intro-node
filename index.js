aa5ea2234f63e430df54aca1c0ba3bd568434946
Federico Altava Redondo <fedealtava@gmail.com>
2021-11-08 16:08:30 +0100

    lab done

--- index.js
+++ index.js
@@ -1,17 +1,68 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = []
+    this.length = 0
 
-  add(item) {}
 
-  get(pos) {}
+  }
+  
+  add(item) {
 
-  max() {}
+    this.items.push(item)
+    this.items.sort((a,b)=>{
+    return a-b
+    })
+    this.length = this.items.length
+  }
 
-  min() {}
+  get(pos) {
+    if(pos < this.items.length){
+      return this.items[pos]
+    }else{
+      throw new Error('OutOfBounds')
+    }
+  }
 
-  sum() {}
+  max() {
+    if(this.items.length === 0){
+    throw new Error('EmptySortedList')
+  }else{
+    // return Math.max(...this.items)
+    return this.items.slice(-1)
+    
+  }
+  }
 
-  avg() {}
+  min() {
+    if(this.items.length === 0){
+      throw new Error('EmptySortedList')
+    }else{
+      return Math.min(...this.items)
+      // return this.items.slice(0)
+      
+    }
+  }
+
+  sum() {
+    if(this.items.length === 0){
+      return 0
+    }else{
+      return this.items.reduce((a,b) => a + b, 0)
+      
+    }
+
+  }
+
+  avg() {
+    if(this.items.length === 0){
+      throw new Error('EmptySortedList')
+    }else{
+      return this.items.reduce((a,b) => a + b, 0)/this.items.length
+      // return this.items.slice(0)
+      
+    }
+
+  }
 }
 
 module.exports = SortedList;
