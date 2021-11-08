fa596b5e065d6baa96baa20c990333bc49868118
Fede <f.gomezlara@outlook.es>
2021-11-08 15:22:23 +0100

    iteration 3

--- index.js
+++ index.js
@@ -7,12 +7,21 @@ class SortedList {
   add(item) {
     const arr = this.items //Create a reference to the items array
     arr.push(item) //Push the new item inside of it
+    this.length++
     arr.sort((a, b) => { //Then sort it
-      return a-b
+      return a - b
     })
   }
 
-  get(pos) {}
+  get(pos) {
+    const arr = this.items
+    if(typeof arr[pos] === 'undefined') { //If nothing exists in the provided position, throw error
+      throw new Error('OutOfBounds')
+    }
+    else { //If something exists, return it
+      return arr[pos]
+    }
+  }
 
   max() {}
 
