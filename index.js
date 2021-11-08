ebafa8796a09ce76d32e01f01598155ca6c7106b
Guillem Ferrer <guillemfd@gmail.com>
2021-11-08 16:13:07 +0100

    120% completed!

--- index.js
+++ index.js
@@ -1,17 +1,57 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.length = this.items.push (item)
+    this.items.sort ((a, b) => {
+      if (a < b) {
+        return -1
+      }
+    })
+  }
+ 
 
-  get(pos) {}
+  get(pos) {
+    if (pos < this.items.length) {
+      return this.items[pos]
+  } else {
+    throw new Error('OutOfBounds')
+  }
+}
 
-  max() {}
+  max() {
+    if (this.items.length === 0) {
+      throw new Error('EmptySortedList')
+    } else {
+      return this.items[this.items.length -1]
+    }
+  }
 
-  min() {}
+  min() {
+    if (this.items.length === 0) {
+      throw new Error('EmptySortedList')
+    } else {
+      return this.items[0]
+    }
+  }
 
-  sum() {}
+  sum() {
+    return this.items.reduce((accu, items) => 
+    accu + items, 0)
+  }
 
-  avg() {}
+  avg() {
+    if (this.items.length === 0) {
+      throw new Error('EmptySortedList')
+  } else {
+    return this.items.reduce((accu, items) => 
+    accu + items, 0) / this.items.length
+  }
+ }
+ 
 }
 
 module.exports = SortedList;
