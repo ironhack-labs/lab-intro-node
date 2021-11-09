af3e89f928bfe60cf7b06728aa02fd2046934fb3
abdelmalek.saksioui <abdelmalek.saksioui@cgm.com>
2021-11-09 12:28:17 +0100

    done

--- index.js
+++ index.js
@@ -1,17 +1,39 @@
 class SortedList {
-  constructor() {}
+    constructor() {
+        this.items = [];
+        this.length = 0;
+    }
 
-  add(item) {}
+    add(item) {
+        this.items.push(item);
+        this.items.sort((a, b) => a - b);
+        this.length = this.items.length;
+    }
 
-  get(pos) {}
+    get(pos) {
+        if (pos > this.items.length - 1) throw new Error('OutOfBounds');
+        return this.items[pos];
+    }
 
-  max() {}
+    max() {
+        if (this.items.length === 0) throw new Error('EmptySortedList');
+        return this.items.reduce((a, c) => c > a ? c : a);
+    }
 
-  min() {}
+    min() {
+        if (this.items.length === 0) throw new Error('EmptySortedList');
+        return this.items.reduce((a, c) => c < a ? c : a);
+    }
 
-  sum() {}
+    sum() {
+        if (this.items.length === 0) return 0;
+        return this.items.reduce((a, c) => a + c, 0);
+    }
 
-  avg() {}
+    avg() {
+        if (this.items.length === 0) throw new Error('EmptySortedList');
+        return this.sum()/this.length;
+    }
 }
 
 module.exports = SortedList;
