4ecf8665aacb6f0bddb362549f157b7f2e9a1611
Anna Morozova <a.morozova.nrw@gmail.com>
2021-11-09 12:31:54 +0100

    done

--- index.js
+++ index.js
@@ -1,17 +1,53 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = []
+    this.length = this.items.length
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item)
+    this.length++
+    this.items.sort(function (a,b) {
+      return a - b
+    })
+  }
 
-  get(pos) {}
+  get(pos) {
+    const position = this.items.indexOf(pos)
+    if(position === -1){
+      throw new Error('OutOfBounds');
+    }
+    return position
+  }
 
-  max() {}
+  max() {
+    if(this.length === 0){
+      throw new Error('EmptySortedList');
+    }
+    return Math.max(...this.items)
+  }
 
-  min() {}
+  min() {
+    if(this.length === 0){
+      throw new Error('EmptySortedList');
+    }
+    return Math.min(...this.items)
+  }
 
-  sum() {}
+  sum() {
+    let total = 0
+    for (let i = 0; i <this.length; ++i) {
+      total += this.items[i]
+ }
+ return total
+  }
 
-  avg() {}
+  avg() {
+    if(this.length === 0) {
+      throw new Error('EmptySortedList');
+    }
+    return this.sum() / this.length
+  }
 }
 
 module.exports = SortedList;
