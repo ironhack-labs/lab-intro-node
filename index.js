7bfba5bde06988a7727d55cbb2068af9cfb7ac35
Tsaikkari <kirsi.trospe@gmail.com>
2021-11-09 12:54:13 +0100

    Done

--- index.js
+++ index.js
@@ -1,17 +1,60 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.length++
+    // let i = ++SortedList.length;
+    // this.items[i] = item
+    return this.items.sort((a, b) => a - b);
+  }
 
-  get(pos) {}
+  remove(index) {
+    delete this.items[index]
+  }
 
-  max() {}
+  get(pos) {
+    const index = this.items.indexOf(pos);
+    if (this.items.indexOf(pos) === -1) {
+      throw new Error('OutOfBounds');
+    }
+    return this.items[index];
+  }
 
-  min() {}
+  max() {
+    if (this.length === 0) {
+      throw new Error('EmptySortedList');
+    } 
+    return Math.max(...this.items)
+  }
 
-  sum() {}
+  min() {
+    if (this.length === 0) {
+      throw new Error('EmptySortedList');
+    } 
+    return Math.min(...this.items)
+  }
 
-  avg() {}
+  sum() {
+    return this.items.reduce((sum, num) => {
+      return sum + num
+    }, 0)
+  }
+
+  avg() {
+    if (this.items.length === 0) {
+      throw new Error('EmptySortedList');
+    }
+    return this.sum(this.items) / this.length
+  }
 }
+
+const sortedList = new SortedList()
+sortedList.add(5)
+//sortedList.remove(0)
+console.log(sortedList.items)
 
 module.exports = SortedList;
