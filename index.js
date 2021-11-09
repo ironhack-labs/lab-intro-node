68c9cdbb5667d91c1449bf8d91a6d986871fd97c
Noam Shani <Noams156@gmail.com>
2021-11-09 12:20:27 +0100

    done

--- index.js
+++ index.js
@@ -1,17 +1,84 @@
 class SortedList {
-  constructor() {}
+  constructor() {
 
-  add(item) {}
+    this.items = []
+    this.length = 0
 
-  get(pos) {}
 
-  max() {}
+  }
 
-  min() {}
+  add(item) {
 
-  sum() {}
+    this.items.push(item);
+    this.items.sort((a,b) => a-b);
+    this.length++;
+    
 
-  avg() {}
+
+  }
+
+
+  get(pos) {
+    if (pos >= this.length ){
+
+      throw new Error ("OutOfBounds")
+
+    }
+
+    return this.items[pos]
+
+  }
+
+  max() {
+
+    if (this.length === 0 ){
+      throw new Error ("EmptySortedList")
+    }
+
+   return this.items[this.length-1]
+
+
+  }
+
+  min() {
+
+
+    if (this.length === 0 ){
+      throw new Error ("EmptySortedList")
+    }
+
+  return this.items[0]
+
+  }
+
+  sum() {
+   
+  if (this.items.length === 0){
+  return 0 
+  } 
+
+  const result = this.items.reduce(function(a,c){
+
+  return a+c   
+
+  })
+
+  return result 
+
+  }
+
+  avg() {
+   
+    if (this.length === 0 ){
+      throw new Error ("EmptySortedList")
+    }
+    
+   const result =  this.sum() / this.length 
+
+   return result 
+
+  }
+
 }
 
 module.exports = SortedList;
