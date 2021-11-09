5147bab933bffc29e70995ac68de2ace2a1ad129
Aleksandar Velichkovikj <al.velichkovikj@gmail.com>
2021-11-09 13:33:53 +0100

    done

--- index.js
+++ index.js
@@ -1,17 +1,57 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = []
+    this.length = this.items.length
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item)
+    this.items.sort((a, b) => a - b)
+    this.length++
+  }
 
-  get(pos) {}
+  get(pos) {
+    for (let i = 0; i < this.length; i++) {
+      if (pos === i) {
+        return i
+      }
+    }
+    throw new Error('OutOfBounds')
+  }
 
-  max() {}
+  max() {
+    if (this.length === 0) {
+      throw new Error('EmptySortedList')
+    } else {
+      return Math.max(...this.items)
+    }
+  }
 
-  min() {}
+  min() {
+    if (this.length === 0) {
+      throw new Error('EmptySortedList')
+    } else {
+      return Math.min(...this.items)
+    }
+  }
 
-  sum() {}
+  sum() {
+    if (this.length === 0) {
+      return 0
+    } else {
+      return this.items.reduce((acc, val) => acc + val)
+    }
+  }
 
-  avg() {}
+  avg() {
+    if (this.length === 0) {
+      throw new Error('EmptySortedList')
+    } else {
+      return Math.floor(
+        this.items.reduce((acc, val) => acc + val / this.length)
+      )
+    }
+  }
 }
 
-module.exports = SortedList;
+module.exports = SortedList
