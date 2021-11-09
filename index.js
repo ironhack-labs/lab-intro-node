14def557b7199339ce09f7927d671db8b4050e83
Johannes von Kessel <johannes.v.kessel@web.de>
2021-11-09 12:37:48 +0100

    done

--- index.js
+++ index.js
@@ -1,17 +1,65 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item)
+    this.items.sort(function(a,b){
+      return a-b
+    })
+    this.length = this.items.length
+  }
 
-  get(pos) {}
+  get(pos) {
+    
+    if (pos > this.length){
+      throw new Error('OutOfBounds');
+    } else {
+      return this.items[pos]
+    }
 
-  max() {}
+  }
 
-  min() {}
+  max() {
+    if (this.length === 0){
+      throw new Error('EmptySortedList');
+    } else {
+      return this.items[this.length-1]
+    }
+  }
 
-  sum() {}
+  min() {
+    if (this.length === 0){
+      throw new Error('EmptySortedList');
+    } else {
+      return this.items[0]
+    }
+  }
 
-  avg() {}
+  sum() {
+    if (this.length === 0){
+      return 0
+    } else {
+      return this.items.reduce(function(acc, curr){
+        return acc + curr
+      },0)
+    
+    }
+    
+  }
+
+  avg() {
+    if (this.length === 0){
+      throw new Error('EmptySortedList');
+    } else {
+      let sum = this.items.reduce(function(acc, curr){
+        return acc + curr
+      },0)
+     return sum/this.length
+    }
+  }
 }
 
 module.exports = SortedList;
