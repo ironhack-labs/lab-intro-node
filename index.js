691e1bc55dfa5cd6236f074a885ca925dc81c61d
KuraiTsukino <sandym3000@hotmail.com>
2021-11-15 16:23:56 -0600

    done

--- index.js
+++ index.js
@@ -4,17 +4,57 @@ class SortedList {
     this.length = this.items.length
   }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item)
+    this.items.sort(function(a, b) {
+      return a-b
+    })
+    this.length = this.items.length
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos > this.length) {
+      throw new Error('OutOfBounds')
+    } else {
+      return this.items [pos]
+    }
+  }
 
-  max() {}
+  max() {
+    if (this.length === 0) {
+      throw new Error ('EmptySortedList')
+    } else {
+      return Math.max.apply(null, this.items)
+    }
+  }
 
-  min() {}
+  min() {
+    if (this.length === 0) {
+      throw new Error ('EmptySortedList')
+    } else {
+      return Math.min.apply(null, this.items)
+    }
+  }
 
-  sum() {}
+  sum() {
+    if (this.items.length === 0) {
+      return 0
+    } else {
+      let total = 0
+      this.items.forEach(function(a) {total += a;})
+      return total
+    }
+  } 
 
-  avg() {}
+  avg() {
+    if (this.length === 0) {
+      throw new Error ('EmptySortedList')
+    } else {
+      let total = 0
+       this.items.forEach(function(a) {total += a;})
+      return total / this.length
+    }
+  }
 }
 
 module.exports = SortedList;
