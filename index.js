c0090bef4bd7bcf436701e6153f5813cd025a116
Rodrigo Gonzalez <rodrigogonvaz@gmail.com>
2021-11-15 16:42:25 -0600

    Lab1 Done

--- index.js
+++ index.js
@@ -1,15 +1,42 @@
 class SortedList {
-  constructor() {}
+  constructor(items,length) {
+    this.items = [],
+    this.length = this.items.length
+  }
 
-  add(item) {}
+  add(item) {
+      this.items.unshift(item)
+      this.length = this.items.length
+      this.items.sort((a,b) => a - b)
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (!this.items[pos]) {
+      throw new Error('OutOfBounds');
+    } else{
+      return this.items[pos];
+    }
+  }
 
-  max() {}
+  max() {
+    if (this.length > 0) {
+      return Math.max.apply(null, this.items)
+    } else {
+      throw new Error('EmptySortedList');
+    }
+  }
 
-  min() {}
+  min() {
+    if (this.length > 0) {
+      return Math.min.apply(null, this.items)
+    } else {
+      throw new Error('EmptySortedList');
+    }
+  }
 
-  sum() {}
+  sum() {
+
+  }
 
   avg() {}
 }
