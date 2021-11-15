022755b624660015b8ebb61d35619317e18bb388
Dulce Fabiola <dulcefabiola.rm@gmail.com>
2021-11-15 17:00:26 -0600

    done

--- index.js
+++ index.js
@@ -1,17 +1,66 @@
 class SortedList {
-  constructor() {}
+  constructor(items, length) {
+    this.items = items;
+    this.length = length;
+  }
 
-  add(item) {}
+  add(item) {
+    //Clonamos el nuevo arreglo y agregamos el nuevo item
+    // const newArray = [...this.items];
+    //agregamos elemento al array
+    this.items.push(item);
+    //ordenamos de manera ascendente
+    const sortArray = this.items.sort((a, b) => {
+      return a - b;
+    });
+    //actualizamos la propiedad de la longitud
+    this.length = sortArray.length;
+    return sortArray;
+  }
 
-  get(pos) {}
+  get(pos) {
+    //lanzar error en caso de que no se encuentre el indice
+    if (pos >= this.length || pos < 0) {
+      throw new Error("OutOfBounds");
+    }
+    return this.items[pos];
+  }
 
-  max() {}
+  max() {
+    if (this.items.length === 0) {
+      throw new Error("EmptySortedList");
+    }
+    return Math.max(...this.items);
+  }
 
-  min() {}
+  min() {
+    if (this.items.length === 0) {
+      throw new Error("EmptySortedList");
+    }
+    return Math.min(...this.items);
+  }
 
-  sum() {}
+  sum() {
+    const suma = this.items.reduce((acumulador, currentValue) => {
+      return acumulador + currentValue;
+    }, 0);
+    return suma;
+  }
 
-  avg() {}
+  avg() {
+    const promedio = this.sum() / this.length;
+    return promedio;
+  }
 }
 
 module.exports = SortedList;
+
+//Creamos instancia
+const items = [2, 5, 7];
+const array = new SortedList(items, items.length);
+array.add(6);
+array.get(2);
+array.max();
+array.min();
+array.sum();
+array.avg();
