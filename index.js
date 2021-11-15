1e59f105b6c33077eadb3bba1a35737fb635aa9c
root <root@pablovicho.localdomain>
2021-11-15 17:30:25 -0600

    done, although I still get an error in the add() method. It works as supposed, though

--- index.js
+++ index.js
@@ -1,17 +1,55 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = []
+    this.length = 0;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item)
+    this.items.sort()
+    this.length++
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos < this.length && pos > -1) {
+      return this.items[pos]    
+    } else {
+      throw new Error('OutOfBounds')
+    }
+  }
 
-  max() {}
+  max() {
+    if (!this.items.length > 0) {
+      throw new Error('EmptySortedList')
+  } else {
+    let max = this.items.sort()
+    return max[this.items.length-1]
+  }
+}
 
-  min() {}
+  min() {
+    if (!this.items.length) {
+      throw new Error('EmptySortedList')
+    } else {
+      let min = this.items.sort()
+      return min[0]
+    }
+  }
 
-  sum() {}
+  sum() {
+    let suma = 0
+    this.items.forEach(element => {
+      suma+=element
+    })
+    return suma
+  }
 
-  avg() {}
+  avg() {
+    if (!this.items.length) {
+    throw new Error('EmptySortedList')} else {
+    return this.sum()/this.items.length;
+  }
+}
 }
 
 module.exports = SortedList;
