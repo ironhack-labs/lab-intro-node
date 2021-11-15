5d74442c29e86a720dd6793dcd393d1d1919bbac
Martin Giura <martin.giura@gmail.com>
2021-11-15 16:17:24 -0600

    passing 13 tests out of 14, pending add value while keeping list sorted

--- index.js
+++ index.js
@@ -1,17 +1,53 @@
 class SortedList {
-  constructor() {}
+  constructor(items,length) {
+    this.items = []
+    this.length = 0
 
-  add(item) {}
+  }
 
-  get(pos) {}
+  add(item) {
+    this.items.push(item)
+    this.length = this.items.length
+  }
 
-  max() {}
+  get(pos) {
+    if(pos > this.length) {
+      throw new Error('OutOfBounds');
+    }
+    return this.items.indexOf(pos)
+  }
 
-  min() {}
+  max() {
+    if (this.length === 0) {
+      throw new Error('EmptySortedList');
+    }
+    return Math.max.apply(null, this.items)
+  }
 
-  sum() {}
+  min() {
+    if (this.length === 0) {
+      throw new Error('EmptySortedList');
+    }
+    return Math.min.apply(null, this.items) 
+  }
 
-  avg() {}
+  sum() {
+    if (this.length === 0) {
+      return 0
+    }
+
+    let totalSum = 0
+    this.items.forEach((element => totalSum += element))
+    return totalSum
+  }
+
+  avg() {
+    if (this.length === 0) {
+      throw new Error('EmptySortedList');
+    }
+    return this.items.reduce((a, b) => a + b) / this.length;
+    
+  }
 }
 
 module.exports = SortedList;
