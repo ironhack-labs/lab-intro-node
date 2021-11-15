6cf9f75f5a42debe5c3c9ddbdc30881166578e86
“Abril <abril.ch.26@gmail.com>
2021-11-15 17:40:08 -0600

    done

--- index.js
+++ index.js
@@ -1,17 +1,51 @@
 class SortedList {
-  constructor() {}
+  constructor(items, length) {
+    this.items = []
+    this.length = 0
+  }
+  
+  add(item) {
+    this.items.push(item)
+    this.items.sort((a,b) => (a-b))
+    this.length++
+  }
 
-  add(item) {}
 
-  get(pos) {}
+  get(pos) {
+    if (pos > this.items.length) {
+      throw new Error ("Out of Bounds")
+    } 
+      return this.items[pos]
+    }
+  
+  max() {
+    if(this.items.length === 0) {
+      throw new Error('EmptySortedList') 
+    }
+    return this.items.sort((a, b) => (a - b))[this.length - 1];
+  }
+  
+  min() {
+    if(this.items.length === 0) {
+      throw new Error('EmptySortedList')
+    }
+    return this.items.sort((a, b) => (b - a))[this.length - 1]
+  }
 
-  max() {}
+  sum() {
+    if (this.items.length === 0) {
+      return 0
+    }
+    return this.items.reduce((a,b) => a + b, 0)
+  }
 
-  min() {}
-
-  sum() {}
-
-  avg() {}
+  avg() {
+    if (this.items.length === 0) {
+      throw new Error('EmptySortedList')
+    }
+   return this.items.reduce((a, b) => a + b, 0) / this.items.length 
+  }
 }
+
 
 module.exports = SortedList;
