cff6a3ea72a2601658dac311cee8ed059c8ae930
Javier Herrera <jaaah0010@gmail.com>
2021-11-15 17:30:25 -0600

    listo

--- index.js
+++ index.js
@@ -1,17 +1,86 @@
+
+// 1. CLASE
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = 0;
+  }
 
-  add(item) {}
 
-  get(pos) {}
+  add(item) {
+    
+    this.items.push(item);
+    this.items.sort((a, b) => a - b);
+    this.length += this.items.length
+  
+  }
 
-  max() {}
+  get(pos) {
+    const index = this.items.indexOf(pos);
+    if (index === -1){
+      throw new Error('OutOfBounds');
+    }
+    return this.items[index]
+  }
 
-  min() {}
 
-  sum() {}
+  max() {
 
-  avg() {}
+    if(this.items.length === 0){
+      throw new Error('EmptySortedList');
+    }
+
+    return Math.max(...this.items)
+  }
+
+  min() {
+    if(this.items.length === 0){
+      throw new Error('EmptySortedList');
+    }
+
+    return Math.min(...this.items)
+
+  }
+
+
+  sum() {
+
+  let sumaT = 0;
+
+  for (let i = 0; i < this.items.length; i++) {
+    sumaT += this.items[i] 
+  }
+
+  return sumaT;
+
+  }
+
+
+
+  avg() {
+
+    if(this.items.length === 0){
+      throw new Error('EmptySortedList');
+    }
+
+
+    let itemAvr = this.items.reduce((a, b) => a + b ) / this.items.length
+
+    return itemAvr;
+  }
 }
 
+
+
+// 2. INSTANCIA
+
+let list = new SortedList()
+
+
+
+
+
+// 3. EXPORT
 module.exports = SortedList;
+
+
