82464bb8035deb5a0098339aee20f0a85bda22ff
Edgar Uco <edgarin@gmail.com>
2021-11-15 17:20:29 -0600

    Lab intro node done

--- index.js
+++ index.js
@@ -1,17 +1,57 @@
 class SortedList {
-  constructor() {}
+  constructor(items, length) {
+    this.items = []
+    this.length = this.items.length
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a,b) => a - b);
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    const index = this.items.indexOf(pos);
+    if (index >= 0) {
+      return this.items[index]
+    } else {
+       throw new Error('OutOfBounds');
+    }
+   
+  }
 
-  max() {}
+  max() {
+    if (this.items.length ===0) {
+      throw new Error('EmptySortedList');
+    } else {
+      return Math.max.apply(null, this.items);
+    }
+  }
 
-  min() {}
+  min() {
+    if (this.items.length === 0) {
+        throw new Error('EmptySortedList');
+    } else {
+        return Math.min.apply(null, this.items);
+    }
+  }
 
-  sum() {}
+  sum() {
+    if (this.items.length === 0) {
+      return 0
+    } else {
+      return this.items.reduce((a,b) => a + b, 0)
+    }
+  }
 
-  avg() {}
+  avg() {
+    if (this.items.length === 0) {
+        throw new Error('EmptySortedList');
+    } else {
+      return this.items.reduce((a, b) => a + b) / this.items.length;
+
+    }
+  }
 }
 
 module.exports = SortedList;
