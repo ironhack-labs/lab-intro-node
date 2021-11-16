eb59afa1cb3bcdbd1618da9f48c5dc6d2e86e7c8
SinuheJardinez <sinuhe.jh@gmail.com>
2021-11-15 18:51:10 -0600

    resuelto hasta iteracion 5

--- index.js
+++ index.js
@@ -1,15 +1,72 @@
 class SortedList {
-  constructor() {}
+  constructor(items, length) {
+    this.items = [];
+    this.length = 0;
+  }
 
-  add(item) {}
+  add(item) {
+    let newArr = this.items;
+    newArr.push(item)
+    newArr.sort((a, b)=>{return a-b})
+     this.length++
+  
+  }
 
-  get(pos) {}
+  get(pos) {
+    if(pos <= this.length-1){
+      let array = this.items;
+      for(let i=0; i <= array.length; i++){
+        if( i === pos){
+          return array[i];
+        }
+      }
+    } else{
+      throw new Error('OutOfBounds');
+    }
+  }
 
-  max() {}
+  max() {
+    let array = this.items;
+    if(array){
+      const maxNum = (array) =>{
+        const mayor = array.reduce((acum, value)=>{ 
+          return acum > value ? acum : value})
+       return mayor    
+    }
+    return maxNum(array)
+    }else if(!array){
+      throw new Error('EmptySortedList');
+    }
+  }
 
-  min() {}
+  min() {
+    let array = this.items;
+    if(array){
+      const maxNum = (array) =>{
+        const mayor = array.reduce((acum, value)=>{ 
+          return acum < value ? acum : value})
+       return mayor    
+    }
+    return maxNum(array)
+    }else if(!array){
+      throw new Error('EmptySortedList');
+    }
+  }
 
-  sum() {}
+  sum() {
+    let array = this.items;
+    if(array){
+      const maxNum = (array) =>{
+        const mayor = array.reduce((acum, value)=>{ 
+          return acum > value ? acum : value})
+       return mayor    
+    }
+    return maxNum(array)
+    }else if(!array){
+      return 0;
+    }
+
+  }
 
   avg() {}
 }
