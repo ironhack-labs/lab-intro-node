0103f2ea6e52e768c2d875156a6367a95c222e2a
Hector Biminchumo <hectorbiminchumo@gmail.com>
2021-11-15 19:59:06 -0500

    done

--- index.js
+++ index.js
@@ -1,17 +1,70 @@
 class SortedList {
-  constructor() {}
+  constructor() {
 
-  add(item) {}
+    this.items= []
+    this.length= this.items.length
 
-  get(pos) {}
+  }
 
-  max() {}
+  add(item) {
+    this.items.push(item)
+    this.items.sort((a,b)=> a-b)
+    this.length=this.items.length
 
-  min() {}
+  }
 
-  sum() {}
+  get(pos) {
 
-  avg() {}
+    if(this.items[pos]) return this.items[pos]
+
+    throw new Error('EmptySortedList')
+    
+
+  }
+
+  max() {
+
+    if(!this.items.length) throw new Error('EmptySortedList')
+    let mayor= this.items[0]
+    for(let i=1; i<this.items.length;i++){
+          if(this.items[i]>mayor) mayor=this.items[i]
+    }
+        return mayor
+
+    
+  }
+  
+  min() {
+    
+    if(!this.items.length) throw new Error('EmptySortedList')
+    let menor= this.items[0]
+    for(let i=1; i<this.items.length;i++){
+          if(this.items[i]<menor) menor=this.items[i]
+    }
+        return menor
+    
+  }
+  
+  sum() {
+    if(!this.items) return 0
+    
+    let acum=0
+    this.items.forEach((item)=>{
+      acum += item
+      
+    })
+    return acum
+    
+  }
+  
+  avg() {
+
+    if(!this.items.length) throw new Error('EmptySortedList')
+
+    return this.items.reduce((a,b) => a+b)/this.items.length
+
+  }
+  
 }
 
 module.exports = SortedList;
