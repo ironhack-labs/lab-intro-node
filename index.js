bad1e9ac40fef166371d5644d2774477c057cb5a
Miguel Garcia <gunsandgamer@hotmail.com>
2021-11-15 19:35:09 -0600

    done

--- index.js
+++ index.js
@@ -1,17 +1,52 @@
 class SortedList {
-  constructor() {}
+    constructor() {
+        this.items = [];
+        this.length = 0;
+    }
+    add(item) {
+        this.items.push(item)
+        this.items.sort(function(a, b) {
+            return a - b
+        })
+        this.length = this.items.length
+    }
 
-  add(item) {}
+    get(pos) {
+        if (pos < this.length) {
+            return this.items[pos]
+        } else {
+            throw new Error('OutOfBounds');
+        }
+    }
+    max() {
+        if (this.length === 0) {
+            throw new Error('EmptySortedList');
+        } else {
+            return Math.max(...this.items)
+        }
+    }
 
-  get(pos) {}
-
-  max() {}
-
-  min() {}
-
-  sum() {}
-
-  avg() {}
+    min() {
+        if (this.length === 0) {
+            throw new Error('EmptySortedList');
+        } else {
+            return Math.min(...this.items)
+        }
+    }
+    sum() {
+        if (this.length === 0) {
+            return 0
+        } else {
+            let total = 0
+            this.items.forEach(function(a) { total += a; })
+            return total
+        }
+    }
+    avg() {
+        if (this.length === 0) {
+            throw new Error('EmptySortedList');
+        }
+        return this.items.reduce((a, b) => a + b) / this.length;
+    }
 }
-
 module.exports = SortedList;
