d43f8b1570f2a4a73ecc97ff4fde0c2d48f77947
EvaAlonzo <la_rana9@hotmail.com>
2021-11-18 17:19:36 -0600

    5/9, 4 pendientes

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 class SortedList {
-  constructor() {}
+  constructor(items,length) {
+    this.items = []
+    this.length = this.items.length
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item)
+    this.items.sort(function(a, b){return a - b})
+    this.length = this.items.length
+  }
 
-  get(pos) {}
+  get(pos) {
+  //this.items.lenght[pos]
+    if(pos > this.items.length){
+      throw new Error('OutOfBounds');
+    }
+    this.items.length[pos]
+  }
 
   max() {}
 
