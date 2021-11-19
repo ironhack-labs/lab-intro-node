d13db020be532aa1ee0230159ea7f839039150fc
David Castillo <david@castillog.com>
2021-11-19 19:23:33 -0400

    Lab Done

--- index.js
+++ index.js
@@ -1,17 +1,46 @@
 class SortedList {
-  constructor() {}
+  constructor(items, length) {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a,b) => a-b)
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos > this.items.length - 1) throw new Error('OutOfBounds');
+    return this.items[pos];
+  }
 
-  max() {}
+  max() {
+    if(this.items.length === 0){
+      throw new Error('EmptySortedList');
+    }else{
+      return this.items[this.items.length - 1]
+    }
+  }
 
-  min() {}
+  min() {
+    if(this.items.length === 0){
+      throw new Error('EmptySortedList');
+    }else{
+     return this.items[0]
+    }
+  }
 
-  sum() {}
+  sum() {
+    if (this.items.length === 0) return 0;
+    return this.items.reduce((a, c) => a + c, 0);
+  }
 
-  avg() {}
+  avg() {
+    if (this.items.length === 0) throw new Error('EmptySortedList');
+    let avg = (this.sum() / this.length);
+    return avg
+  }
 }
 
 module.exports = SortedList;
