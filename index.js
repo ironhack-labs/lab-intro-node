988d11cc71fd39975a3f134c568ff67db6e315e9
SantiagoCortina <cortina.zapfe@gmail.com>
2021-11-20 08:59:27 -0600

    done

--- index.js
+++ index.js
@@ -1,17 +1,61 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = 0;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort(function(a, b) {
+      return a - b;
+    });
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (this.items[pos]){
+      return this.items[pos]
+    } else {
+      //return 'OutOfBounds'
+      throw new CustomException('OutOfBounds');
+    }
+  }
 
-  max() {}
+  max() {
+    if(this.length){
+      return this.items[this.length -1]
+    } else {
+      throw new CustomException('EmptySortedList');
+    }
+  }
 
-  min() {}
+  min() {
+    if(this.length){
+      return this.items[0]
+    } else {
+      throw new CustomException('EmptySortedList');
+    }
+  }
 
-  sum() {}
+  sum() {
+    if(this.length){
+      let sum = 0;
+      this.items.forEach((element) => sum += element)
+      return sum
+    } else {
+      return 0
+    }
+  }
 
-  avg() {}
+  avg() {
+    if(this.length){
+      let sum = 0;
+      this.items.forEach((element) => sum += element)
+      return sum/this.length
+    } else {
+      throw new CustomException('EmptySortedList');
+    }
+  }
 }
 
 module.exports = SortedList;
