abe3c37e340f5870a37fb044f1141b5c95871fdb
SerchPro <proyects_serch@hotmail.com>
2021-11-19 22:42:14 -0600

    done

--- index.js
+++ index.js
@@ -1,17 +1,70 @@
 class SortedList {
-  constructor() {}
+    constructor() {
+        this.items = [];
+        this.length = this.items.length;
+    }
 
-  add(item) {}
+    add(item) {
+        this.items.push(item);
+        this.items = this.items.sort(function(a, b) { return a - b });
+        this.length = this.items.length;
+        return this.items;
+    }
 
-  get(pos) {}
+    get(pos) {
+        if (this.items[pos]) {
+            return this.items[pos];
 
-  max() {}
+        } else {
+            throw new Error('OutOfBounds');
+        }
 
-  min() {}
+    }
 
-  sum() {}
+    max() {
+        if (this.items.length > 0) {
+            return Math.max.apply(null, this.items);
+        } else {
+            throw new Error('EmptySortedList');
+        }
+    }
 
-  avg() {}
+    min() {
+        if (this.items.length > 0) {
+            return Math.min.apply(null, this.items);
+        } else {
+            throw new Error('EmptySortedList');
+        }
+    }
+
+    sum() {
+        let total = 0;
+        let number;
+        if (this.items.length > 0) {
+            for (number of this.items) {
+                total += number;
+            }
+        }
+
+
+        return total;
+    }
+
+    avg() {
+        let total = 0;
+        let number, average;
+
+        if (this.items.length > 0) {
+            for (number of this.items) {
+                total += number;
+            }
+            average = total / this.items.length;
+        } else {
+            throw new Error('EmptySortedList');
+        }
+
+        return average;
+    }
 }
 
 module.exports = SortedList;
