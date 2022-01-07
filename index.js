6a79424ffc84fb27234650f0330ba213dae65702
Lucas Salomao <lucaas.afonso@hotmail.com>
2022-01-06 21:48:50 -0300

    Iteration 1, 3 e 4 - Sem npm mocha instalado

--- index.js
+++ index.js
@@ -1,13 +1,39 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items: [],
+    this.length: this.items.length
+  }
 
-  add(item) {}
+  add(item) {
+    const arrayAfterAddingItem = new Array
+    for(let i;i<this.length;i++){
+      if (this.items[i] >= item) {
+        arrayAfterAddingItem.push(this.items[i])
+      } else {
+        arrayAfterAddingItem.push(this.items[i])
+      }
+    }
+    this.items = arrayAfterAddingItem
+    this.length = arrayAfterAddingItem.length
+  }
 
   get(pos) {}
 
-  max() {}
+  max() {
+    if (this.length === 0) {
+      throw new Error('EmptySortedList')
+    } else {
+      return this.items[this.length-1]
+    }
+  }
 
-  min() {}
+  min() {
+    if (this.length === 0) {
+      throw new Error('EmptySortedList')
+    } else {
+      return this.items[0]
+    }
+  }
 
   sum() {}
 
