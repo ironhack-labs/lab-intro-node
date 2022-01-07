0846cbcfec94fecadbfab7daa7c1ace1f1c621cc
Igor Galvao Bezerra <igor@pop-os.localdomain>
2022-01-06 22:18:40 -0300

    completed

--- index.js
+++ index.js
@@ -1,17 +1,49 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a,b)=>a-b);
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if(pos<0 || pos > this.length - 1){
+      throw new error('OutOfBounds');
+    }
+    return this.items[pos];
+  }
 
-  max() {}
+  max() {
+    if(this.length === 0){
+      throw new Error('EmptySortedList')
+    }
+    return this.items[this.length - 1];
+  }
 
-  min() {}
+  min() {
+    if(this.length === 0){
+      throw new Error('EmptySortedList')
+    }
+    return this.items[0];
+  }
 
-  sum() {}
+  sum() {
+    let sum = this.items.reduce((soma,item)=>soma+item,0);
+    return sum;
+  }
 
-  avg() {}
-}
+  avg() {
+    if(this.length === 0){
+      throw new Error('EmptySortedList')
+    }
+    return this.sum()/this.length;
+  }
+};
 
 module.exports = SortedList;
+
+
