d5a9e3823a153f9fd71d6cad8276a6ec5b4f0c96
Sergio <juliwood1980@gmail.com>
2022-01-06 22:52:51 -0300

    faltam bonus

--- index.js
+++ index.js
@@ -1,15 +1,50 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  };
 
-  add(item) {}
+  
+  add(item) {
+    /*for (let i = 0; i < item.length; i+=1){
+      this.items.push(item[i]);
+    };*/
+    this.items.push(item);
+    this.items.sort((a,b) => {
+      if (a < b) return -1;
+      if (a === 0) return 0;
+      if (a > b) return 1;
+    })
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if(pos > this.length - 1 || pos < 0){
+      throw new error('OutOfBounds');
+    }
+    return this.items[pos];
+  }
 
-  max() {}
+  max() {
+    if (this.items.length === 0){
+      throw new Error('EmptySortedList');
+    }
+    return this.items[this.length - 1];
+  }
 
-  min() {}
+  min() {
+    if (this.items.length === 0){
+      throw new Error('EmptySortedList');
+    }
+    return this.items[0];
+  }
 
-  sum() {}
+  sum() {
+    if (this.items.length === 0){
+      return 0
+    }
+    
+  }
 
   avg() {}
 }
