bfb0dec4a5829d78cec52dcf0d11fcca5efee723
Andre Abre <andre@mundowap.com.br>
2022-01-06 22:38:21 -0300

    done

--- index.js
+++ index.js
@@ -1,17 +1,49 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item)
+    this.items.sort((a, b) => a - b);
+    this.length += 1;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (!this.items[pos]) {
+      throw new Error('OutOfBounds');
+    }
+    return this.items[pos];
+  }
 
-  max() {}
+  max() {
+    if (!this.length) {
+      throw new Error('EmptySortedList');
+    }
+    return Math.max(...this.items)
+  }
 
-  min() {}
+  min() {
+    if (!this.length) {
+      throw new Error('EmptySortedList');
+    }
+    return Math.min(...this.items)
+  }
 
-  sum() {}
+  sum() {
+    if (!this.length) {
+      return 0;
+    }
+    return this.items.reduce((sum, a) => sum + a)
+  }
 
-  avg() {}
+  avg() {
+    if (!this.length) {
+      return EmptySortedList;
+    }
+    return this.items.reduce((sum, a) => sum + a) / this.length
+  }
 }
 
 module.exports = SortedList;
