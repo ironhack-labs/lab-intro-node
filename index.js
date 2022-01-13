fffc4e9eb8cda18077a6c6d375af9c2a438463dd
Michael Foley <foley.p.michael@gmail.com>
2022-01-13 21:48:31 +0000

    finished first 4 iterations.

--- index.js
+++ index.js
@@ -1,13 +1,41 @@
 class SortedList {
-  constructor() {}
+  constructor(items, length) {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a, b) => a - b);
+    this.length = this.items.length;
+    return this.items;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos <= this.length) {
+      return this.items[pos];
+    } else {
+      throw new Error('OutOfBounds');
+    }
+  }
 
-  max() {}
+  max() {
+    if (this.length === 0) {
+      throw new Error('EmptySortedList');
+    } else {
+      var max = Math.max.apply(null, this.items);
+      return max;
+    }
+  }
 
-  min() {}
+  min() {
+    if (this.length === 0) {
+      throw new Error('EmptySortedList');
+    } else {
+      var min = Math.min.apply(null, this.items);
+      return min;
+    }
+  }
 
   sum() {}
 
