6b085a98e3081d91373787b8afaf71f5bf95c3f1
Diogo Carneiro <diogoascarneiro@hotmail.com>
2022-01-13 21:14:51 +0000

    all exercises done. Code is working but doesn't pass tests on Mocha for some reason

--- index.js
+++ index.js
@@ -1,17 +1,52 @@
 class SortedList {
-  constructor() {}
+  constructor(items) {
+    this.items = items;
+    this.length = items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a, b) => a - b);
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos > this.length) {
+      throw new Error('OutOfBounds');
+    } else {
+    return this.items[pos];
+    }
+  }
 
-  max() {}
+  max() {
+    if (this.length <= 0) {
+      throw new Error('EmptySortedList');
+    }
+    else {
+      let highToLowList = [...this.items];
+      highToLowList.sort((a, b) => b - a);
+      return highToLowList[0]
+    }
+  }
 
-  min() {}
+  min() {
+    if (this.length <= 0) {
+      throw new Error('EmptySortedList');
+    }
+    else {
+      return this.items[0]
+    }
+  }
 
-  sum() {}
+  sum() {
+    let result = this.items.reduce((pv, cv) => pv + cv);
+    return result;
+  }
 
-  avg() {}
+  avg() {
+    let result = this.items.reduce((pv, cv) => pv + cv);
+    return result / this.items.length;
+  }
 }
 
 module.exports = SortedList;
