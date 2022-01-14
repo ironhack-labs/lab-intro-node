8a7f3ba20539895e5d0e7f0a55d364b470f59931
AnaModesto21 <77633954+AnaModesto21@users.noreply.github.com>
2022-01-14 22:47:22 +0000

    Finished exercise
    On the bonus exercise, not getting why bonus 1 is not working properly (Help!)

--- index.js
+++ index.js
@@ -1,17 +1,51 @@
 class SortedList {
-  constructor() {}
+  constructor(items, length) {
+    this.items=[];
+    this.length=this.items.length;}
+  
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a, b) => a - b);
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  get(pos) {
+    if (pos > 0 && pos < this.length){
+      return this.items[pos];
+    } else {
+      throw new Error("OutOfBounds");
+      }
+    }
 
-  get(pos) {}
+  max() {
+    if(this.items.length === 0){
+      throw new Error('EmptySortedList');
+    }else{
+      return Math.max(...this.items);
+    }
+  }
 
-  max() {}
+  min() {
+    if(this.items.length === 0){
+    throw new Error('EmptySortedList');
+  }else{
+    return Math.min(...this.items);
+  }}
 
-  min() {}
-
-  sum() {}
-
-  avg() {}
+  sum() {
+    if(this.items.length !== 0){
+     return this.items.reduce((previousValue, currentValue) => previousValue + currentValue);
+    }else{
+      throw new Error('EmptySortedList');        
+    }
 }
-
+  avg() {
+    if(this.items.length === 0){
+      throw new Error('EmptySortedList');
+    }else{
+   // const avg = (sum / this.items.length);
+    return this.sum() / this.items.length;
+}
+}
+}
 module.exports = SortedList;
