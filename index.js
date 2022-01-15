dcc591b293d7f3bea7242f06025500ac3453edd3
Vicente Garcia <vicentegarciaayllon@gmail.com>
2022-01-15 11:39:03 +0100

    done

--- index.js
+++ index.js
@@ -1,17 +1,55 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort( (a, b) => a - b);
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (!this.items[pos]){
+      throw new Error('OutOfBounds');
+    }
+    return this.items[pos];
+  }
 
-  max() {}
+  max() {
+    if (!this.length){
+      throw new Error('EmptySortedList');
+    }
+    return this.items[this.length - 1];
+  }
 
-  min() {}
+  min() {
+    if (!this.length){
+      throw new Error('EmptySortedList');
+    }
+    return this.items[0];
+  }
 
-  sum() {}
+  sum() {
+    if (!this.length){
+      return 0;
+    }
 
-  avg() {}
+    return this.items.reduce((prev, curr) => {
+      return prev += curr;
+    });
+  }
+
+  avg() {
+    if (!this.length){
+      throw new Error('EmptySortedList');
+    }
+
+    return this.items.reduce((prev, curr) => {
+      return prev += curr;
+    }) / this.length;
+  }
 }
 
 module.exports = SortedList;
