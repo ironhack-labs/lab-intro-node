122fd225b8beed45a62bdc7bdabe2590ee0496c5
Calvin T <58273749+Calvin087@users.noreply.github.com>
2022-01-15 11:38:19 +0100

    Done

--- index.js
+++ index.js
@@ -1,17 +1,61 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = 0;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.length++;
+    this.items.sort((a, b) => {
+      return a - b;
+    });
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (!this.items[pos]) {
+      throw new Error("OutOfBounds");
+    }
 
-  max() {}
+    return this.items[pos];
+  }
 
-  min() {}
+  max() {
+    if (this.items.length <= 0) {
+      throw new Error("EmptySortedList");
+    }
 
-  sum() {}
+    let max = this.items.sort((a, b) => {
+      return a - b;
+    });
 
-  avg() {}
+    return max[max.length - 1];
+  }
+
+  min() {
+    if (this.items.length <= 0) {
+      throw new Error("EmptySortedList");
+    }
+
+    let max = this.items.sort((a, b) => {
+      return a - b;
+    });
+
+    return max[0];
+  }
+
+  sum() {
+    return this.items.reduce((acc, curr) => {
+      return acc + curr;
+    }, 0);
+  }
+
+  avg() {
+    if (this.items.length <= 0) {
+      throw new Error("EmptySortedList");
+    }
+    return this.sum() / this.items.length;
+  }
 }
 
 module.exports = SortedList;
