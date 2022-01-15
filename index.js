b130bb2ffa95c0132a5965da5affc58ecc06d02a
AlejandroGC1990 <alejandro.garcia.carmona@gmail.com>
2022-01-15 14:57:39 +0100

    Hecho

--- index.js
+++ index.js
@@ -1,17 +1,54 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items= [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a, b) => a - b);
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if(pos > 0 && pos < this.length){
+      return this.items[pos];
+    } else {
+      throw new Error("OutOfBounds");
+    }
+  }
 
-  max() {}
+  max() {
+    if(this.length === 0){
+      throw new Error('EmptySortedList');
+    } else {
+      return this.items.pop();
+    }
+  }
 
-  min() {}
+  min() {
+    if(this.length === 0){
+      throw new Error("EmptySortedList");
+    } else {
+      return this.items[0];
+    }
+  }
 
-  sum() {}
+  sum() {
+    if(this.items.length === 0){
+      return 0;
+    } else {
+      return this.items.reduce((previousValue, currentValue) => previousValue + currentValue);      
+    }
+  }
 
-  avg() {}
+  avg() {
+    if(this.items.length !== 0){
+      return (this.items.reduce((previousValue, currentValue) => previousValue + currentValue))/this.length;
+    } else {
+      throw new Error("EmptySortedList");
+    }
+  }
 }
 
 module.exports = SortedList;
