6d784822e6d429f1597ca363c4249d0b69db88c4
Pablo Mateos <pmateos805@gmail.com>
2022-01-15 11:55:56 +0100

    lab done

--- index.js
+++ index.js
@@ -1,17 +1,55 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item)
+    this.items.sort((a,b)=> a - b)
+    this.length = this.items.length
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (this.items[pos]){
+      return this.items[pos]
+    }else {
+      throw new Error('OutOfBounds');
+    }
+  }
 
-  max() {}
+  max() {
+    if(this.length <= 0){
+      throw new Error('EmptySortedList');
+    } else{
+      this.items.sort((a,b)=> b - a)
+      return this.items[0]
+    }
+  }
 
-  min() {}
+  min() {
+    if (this.length <= 0){
+      throw new Error('EmptySortedList');
+    } else {
+      this.items.sort((a,b)=> a - b)
+      return this.items[0]
+    }
 
-  sum() {}
+  }
 
-  avg() {}
+  sum() {
+    return this.items.reduce((acc, e) => {
+      return acc += e
+    }, 0)
+  }
+
+  avg() {
+    if(this.length <= 0){
+      throw new Error('EmptySortedList');
+    }else {
+      return this.sum()/this.length
+    }
+  }
 }
 
 module.exports = SortedList;
