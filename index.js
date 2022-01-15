1698f9e2e17676fe07e4c91a448a3c41f04a923e
Sergio Villabrille <sergiovillabrille@hotmail.com>
2022-01-15 12:03:19 +0100

    Lab done, might go for bonuses after this

--- index.js
+++ index.js
@@ -1,13 +1,42 @@
 class SortedList {
-  constructor() {}
+  constructor(items, length) {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item)
+    this.items.sort((a, b) =>
+       a - b
+    )
+    this.length = this.items.length
+  }
 
-  get(pos) {}
+  get(pos) {
+    if(!this.items[pos]){
+      throw new Error('OutOfBounds');
+    } else {
+      return this.items[pos]
+    }
+  }
 
-  max() {}
+  max() {
+    if (this.items.length === 0){
+       throw new Error('EmptySortedList');
+    } else {
+      var max = Math.max.apply(null, this.items);
+    }
+    return max
+  }
 
-  min() {}
+  min() {
+    if (this.items.length === 0){
+      throw new Error('EmptySortedList');
+   } else {
+    var min = Math.min.apply(null, this.items)
+  }
+  return min
+  }
 
   sum() {}
 
