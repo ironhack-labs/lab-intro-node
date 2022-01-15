9008f77a855d420421ce61678f155edae8b8615d
Robert Cabrera <cabrera.rch@gmail.com>
2022-01-15 17:18:07 +0100

    Iteration 1 & 2 done

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 class SortedList {
-  constructor() {}
+  constructor(items, length) {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((item1, item2) => item1 - item2);
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (!this.items[pos]) {
+      throw new Error('OutOfBounds');
+    } else {
+      return this.items[pos]
+    }
+  }
 
   max() {}
 
