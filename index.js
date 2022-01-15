cffd3fc3a4de5c600a9efd4f8a26948129b0fe30
Luisgp01 <73586516+Luisgp01@users.noreply.github.com>
2022-01-15 14:34:01 +0100

    Done

--- index.js
+++ index.js
@@ -1,17 +1,50 @@
 class SortedList {
-  constructor() {}
 
-  add(item) {}
+    constructor(items) {
 
-  get(pos) {}
+        this.items = [];
+        this.length = this.items.length;
+    }
 
-  max() {}
+    add(item) {
+        this.items.push(item);
+        this.items.sort((a, b) => a - b);
+        this.length = this.items.length;
+    }
 
-  min() {}
+    get(pos) {
 
-  sum() {}
+        if (pos < 0 || pos > this.length - 1) {
+            throw new Error('OutOfBounds');
+        }
+        return this.items[pos];
+    }
 
-  avg() {}
+    max() {
+        if (this.length === 0) {
+            throw new Error('EmptySortedList')
+        }
+        return this.items[this.length - 1];
+    }
+
+    min() {
+        if (this.length === 0) {
+            throw new Error('EmptySortedList')
+        }
+        return this.items[0]
+    }
+
+    sum() {
+        let sum = this.items.reduce((soma, item) => soma + item, 0)
+        return sum;
+    }
+
+    avg() {
+        if (this.length === 0) {
+            throw new Error('EmptySortedList')
+        }
+        return this.sum() / this.length;
+    }
 }
 
 module.exports = SortedList;
