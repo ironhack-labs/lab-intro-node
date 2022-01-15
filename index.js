70b53fe46012fdbadac122a06b9a6e6d67bbd772
Juan Carlos Saldaña Herrero <jcarlossaldae@gmail.com>
2022-01-15 11:33:09 +0100

    finished

--- index.js
+++ index.js
@@ -1,17 +1,59 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = []
+    this.length = this.items.length
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item)
+    this.items.sort(function(a, b){return a-b})
+    this.length = this.items.length
+  }
 
-  get(pos) {}
+  get(pos) {
+    const index = this.items.indexOf(pos)
+    if (index !== -1) {
+      return index
+    } else {
+      throw new Error('OutOfBounds')
+    }
+  }
 
-  max() {}
+  max() {
+    const maxValue = this.items[this.items.length - 1]
+    if (maxValue) {
+      return maxValue
+    } else {
+      throw new Error('EmptySortedList');
+    }
+  }
 
-  min() {}
+  min() {
+    const maxValue = this.items[0]
+    if (maxValue) {
+      return maxValue
+    } else {
+      throw new Error('EmptySortedList');
+    }
+   }
 
-  sum() {}
+  sum() { 
+    let sum = 0
+    this.items.forEach(v => {
+      sum += v
+    });
+    return sum
+  }
 
-  avg() {}
+  avg() { 
+    if(this.items.length === 0) {throw new Error('EmptySortedList') }
+    let sum = 0
+    this.items.forEach(v => {
+      sum += v
+    });
+    const avg = sum / this.items.length
+    return avg
+  }
 }
 
 module.exports = SortedList;
