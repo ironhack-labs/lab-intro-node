3c8c9a7b83cf5c997acb7c5e83bd0ab7927c1d72
guilhermefill <guilhermefill@gmail.com>
2022-01-27 21:10:03 +0100

    BER-PT-Guilherme Fill

--- index.js
+++ index.js
@@ -1,17 +1,49 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.length ++;
+    this.items.sort((a, b) => a - b);
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (this.items.length > pos) {
+      return this.items[pos];
+    } else {
+      throw new Error('OutOfBounds');
+    }
+  }
 
-  max() {}
+  max() {
+    if(this.items.length > 0) {
+      return this.items[this.items.length - 1]
+    } else {
+      throw new Error('EmptySortedList');
+    }
+  }
 
-  min() {}
+  min() {
+    if(this.items.length > 0) {
+      return this.items[0]
+    } else {
+      throw new Error('EmptySortedList');
+    }
+  }
 
-  sum() {}
-
-  avg() {}
+  sum() {
+    return this.items.reduce((acc,val) => acc + val, 0);
+  }
+  avg() {
+    if(this.items.length > 0) {
+    return this.sum() / this.length;
+    } else {
+      throw new Error('EmptySortedList');
+    }
+  }
 }
 
 module.exports = SortedList;
