86bee748a725e4bd23ab1cafa93ca1ba113f5c4c
Chris Castle <chrisjcastle93@gmail.com>
2022-01-27 20:51:26 +0100

    done

--- index.js
+++ index.js
@@ -1,17 +1,52 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a, b) => a - b);
+    this.length++;
+  }
+  get(pos) {
+    if (pos >= this.items.length || pos < 0) {
+      throw new Error("OutOfBounds");
+    } else {
+      return this.items.indexOf(pos);
+    }
+  }
+  max() {
+    if (this.items.length == 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      return this.items[this.items.length - 1];
+    }
+  }
 
-  add(item) {}
+  min() {
+    if (this.items.length == 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      return this.items[0];
+    }
+  }
 
-  get(pos) {}
+  sum() {
+    if (this.items.length == 0) {
+      return 0;
+    } else {
+      return this.items.reduce((acc, value) => {
+        return acc + value;
+    });
+    }
+  }
 
-  max() {}
-
-  min() {}
-
-  sum() {}
-
-  avg() {}
+  avg() {
+    if (this.items.length == 0) {
+      throw new Error("EmptySortedList");
+    }
+    return this.sum() / this.length;
+  }
 }
 
 module.exports = SortedList;
