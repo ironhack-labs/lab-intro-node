645a9385e66c1836c130337647438fb0650bff26
cschnitzel10 <boese.caroline@gmail.com>
2022-01-27 21:17:22 +0100

    BER/PT/Caroline

--- index.js
+++ index.js
@@ -1,17 +1,52 @@
 class SortedList {
-  constructor() {}
+  constructor(items, length) {
+    this.items = [];
+    this.length = 0;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.length += 1;
+    this.items.sort((a, b) => a-b)
+  }
 
-  get(pos) {}
+  get(pos) {
+    if(this.length < pos) {
+      throw new Error ("OutOfBounds")
+    }
+    return this.items[pos]
+  }
 
-  max() {}
+  max() {
+    if(this.length === 0) {
+      throw new Error ("EmptySortedList");
+    }
+    return Math.max(...this.items);
+  }
 
-  min() {}
+  min() {
+if (!this.length) {
+  throw new Error ("EmptySortedList");
+}
+return Math.min(...this.items);
+  }
 
-  sum() {}
+  sum() {
+    if (!this.length) {
+      return 0;
+    }
+    const total = this.items.reduce((acc, value) => {
+      return acc + value;
+    }, 0)
+return total
+  }
 
-  avg() {}
+  avg() {
+    if (!this.length) {
+      throw new Error ("EmptySortedList");
+    }
+    return this.sum() / this.length
+  }
 }
 
 module.exports = SortedList;
