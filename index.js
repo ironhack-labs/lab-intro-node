b63d468410a058ec579472b18b6aee82798ed520
Pedro Alberto Baños Folcra <pedroalbertobanosfolcra@MacBook-Pro-de-Pedro-2.local>
2022-01-30 12:38:52 +0100

    lab done including bonus

--- index.js
+++ index.js
@@ -1,17 +1,67 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = []
+    this.length = this.items.length
+  }
 
-  add(item) {}
+  add(item) {
 
-  get(pos) {}
+    if(this.length = 1){
 
-  max() {}
+    this.items.push(item)
+    this.items.sort(function (a, b) {
+      return a - b;
+    });
+    }
+    
 
-  min() {}
+  }
 
-  sum() {}
+  get(pos) { 
 
-  avg() {}
+    if(pos > this.items.length || pos < 0){   
+      throw new Error('OutOfBounds');
+    } else {
+      return this.items[pos]
+    }
+    
+  }
+
+  max() { 
+
+    if(this.items.length<=0){
+      throw new Error('EmptySortedList');
+    } else {
+      return Math.max(...this.items)
+    }
+
+  }
+
+  min() { 
+    if(this.items.length<=0){
+      throw new Error('EmptySortedList');
+    } else {
+      return Math.min(...this.items)
+    }
+
+  }
+
+  sum() { 
+    if(this.items.length<=0){
+      return 0
+    } else {
+      return this.items.reduce((prev, current) => prev + current)
+    }
+    
+  }
+
+  avg() {
+    if(this.items.length<=0){
+      throw new Error('EmptySortedList');
+    } else {
+      return this.sum()/this.items.length
+    }
+  }
 }
 
 module.exports = SortedList;
