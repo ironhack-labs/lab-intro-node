b811471b888da2fe36a159a39a0de8312b8fd052
betilla x <aclara_77@yahoo.com>
2022-01-30 17:09:21 +0000

    done

--- index.js
+++ index.js
@@ -1,17 +1,63 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+   this.items = [];
+   this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items = this.items.sort((a,b)=>{
+      return a-b;
+    });
+    this.length = this.items.length;
 
-  get(pos) {}
+  }
 
-  max() {}
+  get(pos) {
+      if (pos>(this.length-1)) {
+        throw new Error('OutOfBounds');
+      }
+      return (this.items[pos]);
 
-  min() {}
+  }
 
-  sum() {}
+  max() {
+  if (this.length===0) {
+    throw new Error('EmptySortedList');
+  } else {
+    let reverseArray = this.items.reverse();
+    return reverseArray[0];
+  }
+  }
 
-  avg() {}
+  min() {
+
+    if (this.length===0) {
+      throw new Error('EmptySortedList');
+    } 
+    return this.items[0];
+
+  }
+
+  sum() {
+    if (this.length===0) {
+      return 0;
+    }
+   
+    let sum = this.items.reduce((acc,curr)=>{
+   return acc+curr;
+    } )
+    
+    return sum;
+     
+  }
+
+  avg() {
+    if (this.length===0) {
+      throw new Error('EmptySortedList');
+    }
+  return this.sum()/this.length
+  }
 }
 
 module.exports = SortedList;
