67009233a478416314fd14e0b5b0c8fb42600c46
Emanueljuarez <zerauleuname@gmail.com>
2022-01-31 17:28:38 -0600

    iteraciones basicas

--- index.js
+++ index.js
@@ -1,14 +1,38 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+   this.items.push(item);
+   this.items.sort((a,b) => a - b);
+   this.length = this.items.length
+   
+  }
 
-  get(pos) {}
+  get(pos) {
+    if( pos > this.items.length){
+      throw new Error('OutOfBounds')
+    }
+    return this.items.splice(pos,1);
+  }
 
-  max() {}
+  max() {
+  
+  if(this.length === 0){
+    throw new Error('EmptySortedList')
+  } else {
+     return Math.max(...this.items) 
+    }
+  }
 
-  min() {}
-
+  min() {if(this.length === 0){
+    throw new Error('EmptySortedList')
+  } else {
+     return Math.min(...this.items) 
+    }}
+//
   sum() {}
 
   avg() {}
