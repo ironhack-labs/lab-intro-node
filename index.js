3ff456b9057a5c2a59c08f09a137f2242e9e9eff
Alan Cruz <alanchis@hotmail.com>
2022-01-31 17:01:17 -0600

    done

--- index.js
+++ index.js
@@ -1,17 +1,60 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+  
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort(function(a, b) {return a-b});
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    let position;
+    if (this.length < pos) {
+      throw new Error('OutOfBounds');
+    } else {
+      position = this.items.indexOf(pos);
+      return position
+    }
+  }
 
-  max() {}
+  max() {
+    if (this.items.length === 0) {
+      throw new Error('EmptySortedList');      
+    } else {
+      return Math.max(...this.items);
+    }
+   }
 
-  min() {}
+  min() {
+    if (this.items.length === 0) {
+      throw new Error('EmptySortedList');      
+    } else {
+      return Math.min(...this.items);
+    }
+     }
 
-  sum() {}
+  sum() {
+    if (this.items.length === 0) {
+      return 0;      
+    } else {
+      let sum = 0;
+      for (let i = 0; i < this.items.length; i++) {
+        sum = this.items[i] + sum
+      }
+      return sum
+      }
+    }     
+  
 
-  avg() {}
-}
+  avg() {
+
+
+}}
+
+
 
 module.exports = SortedList;
