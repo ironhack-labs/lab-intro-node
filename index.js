979eb702fdc4a6c67372abee76aa4f61534aba1e
Mauricio <mauriciomeza95@hotmail.com>
2022-01-31 17:32:01 -0600

    Not finished yet

--- index.js
+++ index.js
@@ -1,13 +1,43 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = []
+    this.length = this.items.length
+  }
+  
+  add(item) {
+    this.items.push(item)
+    this.length = this.items.length
+    this.items.sort((a,b) => {
+      return a - b
+    })
+  }
 
-  add(item) {}
+  get(pos) {
+    if (this.items.indexOf(pos) === -1) {
+      return 'OutOfBounds'
+    } else {
+      let position = this.items[pos]
+      return position
+    }
+  }
 
-  get(pos) {}
+  max() {
+    if (this.items.length === 0) {
+      return 'EmptySortedList'
+    } else {
+      let highest = Math.max(this.items)
+      return highest
+    }
+  }
 
-  max() {}
-
-  min() {}
+  min() {
+    if (this.items.length === 0) {
+      return 'EmptySortedList'
+    } else {
+      let lowest = Math.min(this.items)
+      return lowest
+    }
+  }
 
   sum() {}
 
