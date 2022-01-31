095a019d0287ccd3ca0b6dbd9ed8d5eb72c4629e
Roman <romandiazloz91@gmail.com>
2022-01-31 16:48:32 -0600

    lab-node respuestas

--- index.js
+++ index.js
@@ -1,17 +1,64 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = []
+    this.length = this.items.length
 
-  add(item) {}
+  }
 
-  get(pos) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort(function(a, b) {return a-b});
+    this.length = this.items.length;
+  }
 
-  max() {}
+  get(pos) {
 
-  min() {}
+    if(this.length< pos){
+      throw new Error('OutOfBounds');
+    }else 
+      return this.items[pos]
+  }
 
-  sum() {}
+  max() {
+    let highValue = Math.max(...this.items)
+      if(this.items.length === 0){
+        throw new Error('EmptySortedList');
+      }else{
+        return highValue
+      }
+  }
 
-  avg() {}
+  min() {
+    let highValue = Math.min(...this.items)
+    if(this.items.length === 0){
+      throw new Error('EmptySortedList');
+    }else{
+      return highValue
+    }
+  }
+
+  sum() {
+    let sum = 0
+    for( let i = 0; i< this.items.length; i++){
+      sum = this.items[i] + sum
+    }
+    return sum
+
+  }
+
+  avg() {
+    let numberValues = this.items.length
+    if(this.items.length === 0){
+      throw new Error('EmptySortedList');
+    }else{
+      let sum = 0
+      for( let i = 0; i< this.items.length; i++){
+        sum = this.items[i] + sum
+      }
+      return sum/numberValues
+    }
+
+  }
 }
 
 module.exports = SortedList;
