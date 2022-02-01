abbbc42248e4336e66cfcbfa9239af54b42d294f
Jan Gorecki <jasiekgorecki@gmail.com>
2022-02-01 15:29:18 +0100

    first push, still unsolved

--- index.js
+++ index.js
@@ -1,9 +1,30 @@
 class SortedList {
-  constructor() {}
 
-  add(item) {}
+  constructor() {
+    
+      this.items = [];
+      this.length = this.items.length
+    
+  }
 
-  get(pos) {}
+  add(item) {
+this.items.push(item)
+this.items.sort((a,b)=>a-b)
+this.length = this.items.length
+  }
+
+  get(pos) {
+
+    if (pos > this.items.length || !this.items.length) {
+        
+      throw new Error('OutOfBounds')
+     }
+
+
+   
+     return this.items[pos]
+  
+  }
 
   max() {}
 
