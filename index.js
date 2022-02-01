ae01a8d55873b887c45876bf65ed267af56b8be3
yoricktf <yorick.tenfeld@gmail.com>
2022-02-01 12:26:11 +0100

    finished all questions

--- index.js
+++ index.js
@@ -1,17 +1,56 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = []
+    this.length = this.items.length
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item)
+    this.items.sort((a,b) => a-b)
+    this.length = this.items.length
+  }
 
-  get(pos) {}
+  get(pos) {
+     if (this.items[pos] === undefined) {
+      throw new Error('OutOfBounds');
+    } else {
+      return this.items[pos]
+    }
+  }
 
-  max() {}
+  max() {
+    if (this.items.length === 0) {
+      throw new Error('EmptySortedList');
+    }
+    return this.items[this.items.length -1]
 
-  min() {}
+  }
 
-  sum() {}
+  min() {
+    if (this.items.length === 0) {
+      throw new Error('EmptySortedList');
+    }
+    return this.items[0]
+  }
 
-  avg() {}
+  sum() {
+    let sum = 0
+    for (let item of this.items) {
+      sum += item
+    }
+    return sum
+  }
+
+  avg() {
+    if (this.items.length > 0) {
+      let sum = 0
+      for (let item of this.items) {
+        sum += item
+      }
+    return sum/this.items.length
+    }
+    throw new Error('EmptySortedList');
+  }
 }
 
 module.exports = SortedList;
