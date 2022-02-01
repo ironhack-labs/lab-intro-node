5d5accfa6b4cdff8464942bf6b0aff11a980322a
Joana Carballo <joana16cruz@gmail.com>
2022-02-01 21:59:18 +0100

    done without bonus

--- index.js
+++ index.js
@@ -1,13 +1,43 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = 0;
+  }
 
-  add(item) {}
+  add(item) {
+    // finding position where to insert
+    let i = 0;
+    for(i = 0; i <= this.items.length - 1; i++){
+      if(this.items[i] > item) {
+        break;
+      }
+    }
+    // insert in right position
+    this.items.splice(i, 0, item);
+    this.length = this.length + 1;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (this.items[pos] == undefined) {
+      throw new Error('OutOfBounds');
+    }
+    return this.items[pos];
+    
+  }
 
-  max() {}
+  max() {
+    if (this.items.length == 0) {
+      throw new Error('EmptySortedList');
+    }
+    return this.items[this.items.length - 1]
+  }
 
-  min() {}
+  min() {
+    if (this.items.length == 0) {
+      throw new Error('EmptySortedList');
+    }
+    return this.items[0]
+  }
 
   sum() {}
 
