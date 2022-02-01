8b03923ce88fa5ad3886e62a990beb584184e091
Ghalia AlHasan <ghaliaalhasan12@gmail.com>
2022-02-01 14:54:55 +0200

    done

--- index.js
+++ index.js
@@ -1,17 +1,50 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item)
+    this.items.sort((a, b) => a - b)
+    this.length = this.items.length
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos >= this.length) {
+      throw new Error('OutOfBounds')
+    }
+    return this.items[pos]
 
-  max() {}
+  }
 
-  min() {}
+  max() {
+    if (this.length === 0) {
+      throw new Error('EmptySortedList')
+    }
+    return this.items[this.length - 1]
+  }
 
-  sum() {}
+  min() {
+    if (this.length === 0) {
+      throw new Error('EmptySortedList')
+    }
+    return this.items[0]
+  }
 
-  avg() {}
+  sum() {
+    const totalSum = this.items.reduce((u, v) => u + v, 0);
+    return totalSum;
+  }
+
+  avg() {
+    if (this.length === 0) {
+      throw new Error('EmptySortedList')
+    }
+    const avg = this.sum() / this.length
+    return avg;
+  }
 }
 
 module.exports = SortedList;
+const list = new SortedList
