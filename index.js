a26dfb32acb54b428bffe1cd17077e50cfed96d5
Hollie Duncan <holduncan7@gmail.com>
2022-02-08 12:30:27 +0000

    done

--- index.js
+++ index.js
@@ -1,17 +1,68 @@
 class SortedList {
-  constructor() {}
+  constructor(items, length) {
 
-  add(item) {}
+    this.items = [];
+    this.length = this.items.length
 
-  get(pos) {}
+  }
 
-  max() {}
+  add(item) {
 
-  min() {}
+  this.items.push(item);
 
-  sum() {}
+  this.length = this.items.length;
 
-  avg() {}
+  return this.items.sort((a,b) => a-b);
+  
+  }
+
+  get(pos) {
+
+    if (this.items.indexOf(pos) === -1) {
+      throw new error('OutOfBounds'); 
+    } else if (this.items.indexOf(pos)) {
+      return this.items[pos]
+
+  }}
+
+  max() {
+
+    if (this.items.length === 0) {
+      throw new Error('EmptySortedList');
+    } else {
+      return this.items[this.length-1];
+    }
+
+  }
+
+  min() {
+
+    if (this.items.length === 0) {
+      throw new Error('EmptySortedList');
+    } else {
+      return this.items[0];
+    }
+
+  }
+
+  sum() {
+
+    if (this.items.length === 0) {
+      return 0;
+    } else {
+      return this.items.reduce((a,b)=> a+b)
+
+  }}
+
+  avg() {
+
+    if (this.items.length === 0) {
+      throw new Error('EmptySortedList');
+    } else {
+      return this.items.reduce((a,b)=> a+b) / this.length;
+    }
+
+  }
 }
 
 module.exports = SortedList;
