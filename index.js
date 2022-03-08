5e62c95dd3cd6b637080b498eeaec0fbc6392da6
joeydegori <96920177+joeydegori@users.noreply.github.com>
2022-03-08 14:44:59 -0800

    done

--- index.js
+++ index.js
@@ -1,17 +1,50 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = []; 
+    this.length = this.items.length;  
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item); 
+    this.length = this.items.length; 
+    return this.items.sort((a,b) => a - b); 
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos > this.length){
+      throw new Error('OutOfBounds'); 
+    }
+    this.items.indexOf(pos)
+    return this.items[pos]
+  }
 
-  max() {}
+  max() {
+    if (this.items.length === 0){
+      throw new Error('EmptySortedList'); 
+    }
+    return this.items[this.length -1]
+  }
 
-  min() {}
+  min() {
+    if (this.items.length === 0){
+      throw new Error('EmptySortedList'); 
+    }
+    return this.items[0]
+  }
 
-  sum() {}
+  sum() {
+    if (this.length === 0){
+      return 0
+    } 
+    return this.items.reduce((a, b) => a + b)
+  }
 
-  avg() {}
+  avg() {
+    if (this.length === 0){
+      throw new Error('EmptySortedList')
+    }
+    return this.items.reduce((a, b) => a + b) / this.length
+  }
 }
 
 module.exports = SortedList;
