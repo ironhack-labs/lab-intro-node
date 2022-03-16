e15f45b68450f7fdb59a72f02306523cdc9bed1d
Isaias Paiva <isaiaslopespaiva@gmail.com>
2022-03-16 14:20:25 -0300

    feito

--- index.js
+++ index.js
@@ -1,17 +1,38 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = 0;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a, b) => a - b);
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if(pos >= this.length) throw new Error('OutOfBounds');
+    return this.items[pos]
+  }
 
-  max() {}
+  max() {
+    if (this.length === 0) throw new Error ( 'EmptySortedList' ) ;
+    return Math.max(...this.items)
+  }
 
-  min() {}
+  min() {
+    if (this.length === 0) throw new Error ( 'EmptySortedList' ) ;
+    return Math.min(...this.items)
+  }
 
-  sum() {}
+  sum() {
+    return this.items.reduce((acc, number) => acc + number, 0)
+  }
 
-  avg() {}
+  avg() {
+    if (this.length === 0) throw new Error ( 'EmptySortedList' ) ;
+    return this.sum() / this.length;
+  }
 }
 
 module.exports = SortedList;
