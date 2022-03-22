ae6ad8132160285cfb82e84a7af3032d797219d7
ilanlavi <refaelberlin@gmail.com>
2022-03-23 00:06:14 +0100

    lab completed (Iteration 1-7)

--- index.js
+++ index.js
@@ -1,17 +1,46 @@
 class SortedList {
-  constructor() {}
+  constructor(items, length) {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a, b) => a - b);
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos > this.length) throw new Error('OutOfBounds');
+    else return this.items[pos]
+  }
 
-  max() {}
+  max() {
+    if (!this.length) throw new Error('EmptySortedList');
+    else return Math.max(...this.items);
+  }
 
-  min() {}
+  min() {
+    if (!this.length) throw new Error('EmptySortedList');
+    else return Math.min(...this.items);
+  }
 
-  sum() {}
+  sum() {
+    if (!this.length) return 0;
+    const sumArray = this.items.reduce((a, b) => {
+      return a + b;
+    });
 
-  avg() {}
+    return sumArray;
+  }
+
+  avg() {
+    if (!this.length) throw new Error('EmptySortedList');
+    const avaArray = this.items.reduce((a, b) => {
+      return a + b / this.length;
+    }, 0);
+    return avaArray;
+  }
 }
 
 module.exports = SortedList;
