493ef1fbdadde2b4f4cff3e07ac7a26712748059
Arundathi Jayarathne <sandyjayarathne@gmail.com>
2022-03-24 19:06:21 +0100

    completed lab

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ class SortedList {
   add(item) {
     this.items.push(item);
     this.items.sort((a, b) => a - b);
-
     this.length += 1;
   }
 
@@ -20,18 +19,18 @@ class SortedList {
   }
 
   max() {
-    if (this.items.length <= 0) {
+    if (this.length === 0) {
       throw new Error("EmptySortedList");
     } else {
-      return Math.max(...this.items);
+      return this.items[this.length - 1];
     }
   }
 
   min() {
-    if (this.items.length <= 0) {
+    if (this.length === 0) {
       throw new Error("EmptySortedList");
     } else {
-      return Math.min(...this.items);
+      return this.items[0];
     }
   }
 
