eca183fbf08a29f4cb1efaedf544c01550ca2772
LucasPerezDev <LucasPerezDev@gmail.com>
2022-04-11 18:18:55 -0400

    finished all iterations

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
+    //iteration 1: object should have 2 properties: 
+    //items = [] && length = elements in the array. 
 class SortedList {
   constructor() {
     this.items = [];
     this.length = 0;
   }
-
+    //iteration 2: add value of item into items array, 
+    //then sort in ascending order.
   add(item) {
     this.items.push(item);
     this.length++;
@@ -12,17 +15,39 @@ class SortedList {
   }
   
 
-  get(pos) {}
+  get(pos) { 
+      if ((this.items.indexOf(pos) === -1)){
+        throw new Error('OutOfBounds');
+      } else return this.items.indexOf(pos);
+    }
+  
 
-  max() {}
+  max() {
+    if(this.items.length === 0) {
+      throw new Error('EmptySortedList');
+    } else return Math.max(...this.items);
 
-  min() {}
+  }
 
-  sum() {}
-
-  avg() {}
+  min() {
+    if(this.items.length === 0) {
+      throw new Error('EmptySortedList');
+  } else return Math.min(...this.items);
 }
 
+
+  sum() {
+    if(this.items.length === 0) {
+      return 0;
+    } else return this.items.reduce((a, b) => a + b, 0);
+}
+
+  avg() {
+    if(this.items.length === 0) {
+      throw new Error('EmptySortedList');
+  } else return this.items.reduce((a, b) => a + b, 0) / this.items.length;
+}
+}
 
 
 module.exports = SortedList;
