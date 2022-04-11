6aeb31fbf3da821a7afa546157311055c4eaf19f
Julian Rivera <julianarevircoins@gmail.com>
2022-04-11 19:37:03 -0400

    finished but still need bonus

--- index.js
+++ index.js
@@ -1,13 +1,52 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort(function (a, b) {
+      return a - b;
+    });
+    this.length = this.items;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (!this.items[pos]) {
+      throw new Error('OutOfBounds');
+    }
+    return this.items[pos];
+  }
 
-  max() {}
+  max() {
+    if (this.items.length === 0) {
+      throw new Error('EmptySortedList');
+    }
+    let number = 0;
+    this.items.forEach((item) => {
+      if (number < item) {
+        number = item;
+      }
+    });
+    return number;
+  }
 
-  min() {}
+  min() {
+    if (this.items.length === 0) {
+      throw new Error('EmptySortedList');
+    }
+
+    // let number = Math.min(...this.items);
+    // return number;
+    let number = this.items[0];
+    this.items.forEach((item) => {
+      if (number > item) {
+        number = item;
+      }
+    });
+    return number;
+  }
 
   sum() {}
 
