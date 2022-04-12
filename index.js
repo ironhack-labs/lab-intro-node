3de24b890530be2077cb56f0080c2636ae9562b9
Amanda Walker <amandajnewalker@gmail.com>
2022-04-12 10:21:11 -0400

    intro to node lab

--- index.js
+++ index.js
@@ -1,17 +1,53 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+ this.items =[];
+ this.length = 0;
+  };
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a, b) => a - b);
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
 
-  max() {}
+  get(pos) {
 
-  min() {}
+    if(pos >= this.length){
+      throw new Error('OutofBounds');
+    }
+    return this.items(pos)
+  }
 
-  sum() {}
 
-  avg() {}
-}
+  max() {
+    if(this.length === 0){
+      throw new Error('EmptySortedList');
+    }
+    return this.items(-1);
+    }
+  }
+
+
+  min() {
+    if(this.length === 0){
+      throw new Error('EmptySortedList');
+    }
+    return this.items(0);
+    }
+  }
+
+
+  sum() {
+return this.items.reduce((accumulator, element)=> accumulator + element, 0);
+  }
+
+  avg() {
+    if(this.length === 0){
+      throw new Error('EmptySortedList');
+    }
+    return this.sum() / this.length
+  }
 
 module.exports = SortedList;
+  
