6d5f8ec85394adf1963c18a85138057b488b11ce
Marco Lölkes <marcoloelkes@gmx.de>
2022-04-12 14:20:04 +0200

    done

--- index.js
+++ index.js
@@ -1,17 +1,60 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
 
-  add(item) {}
+  }
 
-  get(pos) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort(function(a,b) {
+      return a-b;
+    });
 
-  max() {}
+    this.length = this.items.length;
+  }
 
-  min() {}
+  get(pos) {
+    if (pos < 0 || pos > this.items.length - 1) {
+      throw new Error('OutOfBounds');
+    }
 
-  sum() {}
+    return this.items[pos];
+  }
 
-  avg() {}
+  max() {
+    if (this.items.length === 0) {
+      throw new Error('EmptySortedList');
+    }
+
+    return this.items[this.items.length -1]
+  }
+
+  min() {
+    if (this.items.length === 0) {
+      throw new Error('EmptySortedList');
+    }
+
+    return this.items[0]
+  }
+
+  sum() {
+    if (this.items.length === 0) {
+      return 0;
+    }
+
+    return this.items.reduce((a, b) => {
+      return a + b;
+    })
+  }
+
+  avg() {
+    if (this.items.length === 0) {
+      throw new Error('EmptySortedList');
+    }
+
+    return this.sum() / this.items.length;
+  }
 }
 
 module.exports = SortedList;
