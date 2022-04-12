948e87337d331589d3607e53575b789145374598
matigobbi <matigobbi@gmail.com>
2022-04-12 14:16:23 +0200

    done

--- index.js
+++ index.js
@@ -1,17 +1,56 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort(function(a,b) {
+      return a-b;
+    });
 
-  get(pos) {}
+    this.length = this.items.length;
+  }
 
-  max() {}
+  get(pos) {
+    if (pos < 0 || pos > this.items.length - 1) {
+      throw new Error('OutOfBounds');
+    }
 
-  min() {}
+    return this.items[pos];
 
-  sum() {}
+  }
 
-  avg() {}
+  max() {
+    if (this.items.length === 0){
+      throw new Error('EmptySortedList');
+    }
+    return this.items[this.items.length -1]
+  }
+
+  min() {
+    if (this.items.length === 0){
+      throw new Error('EmptySortedList');
+    }
+    return this.items[0]
+  }
+
+  sum() {
+    if (this.items.length === 0){
+      return 0
+    }
+    return this.items.reduce((a,b)=> {
+      return a +b
+    })
+  }
+
+  avg() {
+    if(this.items.length===0){
+      throw new Error("EmptySortedList")
+    }
+    return this.sum() / this.items.length
+  }
 }
 
 module.exports = SortedList;
