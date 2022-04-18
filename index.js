48d69cbbf0e04fc72f196edbecc49b76877b6437
Eric Aguilar <ar_eric@hotmail.com>
2022-04-18 18:49:27 -0500

    All iterations completed

--- index.js
+++ index.js
@@ -1,17 +1,54 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort(function (a, b) {
+      return a - b;
+    })
+    this.length ++
+  }
 
-  get(pos) {}
+  get(pos) {
+    if(!this.items[pos]){
+      throw new Error('OutOfBounds');
+    }
+    return this.items[pos]
+  }
 
-  max() {}
+  max() {
+    if(!this.length){
+      throw new Error('EmptySortedList');
+    }
+    return this.items[this.items.length - 1]
+  }
 
-  min() {}
+  min() {
+    if(!this.length){
+      throw new Error('EmptySortedList');
+    }
+    return this.items[0]
+  }
 
-  sum() {}
+  sum() {
+    const sum = this.items.reduce(function(a, b) {
+      return a + b;
+    }, 0)
+    return sum
+  }
 
-  avg() {}
+  avg() {
+    if(!this.length){
+      throw new Error('EmptySortedList');
+    }
+    const avg = this.items.reduce(function(a, b) {
+      return a + b;
+    }, 0) / this.items.length
+    return avg
+  }
 }
 
 module.exports = SortedList;
