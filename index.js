68710c1f94fbc9db93eed97e51d336b0d0afe646
Melissa Osorio Tavera <melissa.osorio.tavera@gmail.com>
2022-04-18 18:00:57 -0500

    done

--- index.js
+++ index.js
@@ -1,17 +1,53 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items=[];
+    this.length=0;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item)
+    this.items.sort(function(a, b){return a - b});
+    this.length=this.items.length;
+  }
+    
 
-  get(pos) {}
+  get(pos) {
+    if(!this.items[pos]){
+      throw new Error('OutOfBounds');
+    }
+    return this.items[pos]
+  }
 
-  max() {}
+  max() {
+    if(this.items.length<=0){
+      throw new Error('EmptySortedList');
+    }
+    const maxNum = Math.max.apply(null,this.items);
+    return maxNum;
+  }
 
-  min() {}
+  min() {
+    if(this.items.length<=0){
+      throw new Error('EmptySortedList');
+    }
+    const minNum=Math.min.apply(null,this.items);
+    return minNum;
+  }
 
-  sum() {}
+  sum() {
+    let initValue=0
+    const sum = this.items.reduce((prevValue,currValue)=>prevValue+currValue,initValue);
+    return sum;
+  }
 
-  avg() {}
+  avg() {
+    if(this.items.length<=0){
+      throw new Error('EmptySortedList');
+    }
+    let initValue=0
+    const avg = this.items.reduce((prevValue,currValue)=>prevValue+currValue,initValue)/this.items.length;
+    return avg;
+  }
 }
 
 module.exports = SortedList;
