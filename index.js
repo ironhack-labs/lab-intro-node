95cd5e1e16bdc4bbc61a33ccd245c64f4feba6ab
nash0x <nachoest@gmail.com>
2022-04-18 16:59:31 -0600

    done

--- index.js
+++ index.js
@@ -1,17 +1,41 @@
 class SortedList {
-  constructor() {}
+  constructor(items, length) {
+    this.items = []
+    this.length = this.items.length
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item)
+    this.items.sort(function(a,b){
+      return a-b
+    })
+    this.length = this.items.length
+  }
 
-  get(pos) {}
+  get(pos) {
+    if(!this.items[pos]) throw new Error('OutOfBounds');
+    return this.items[pos]
+  }
 
-  max() {}
+  max() {
+    if(this.length === 0) throw new Error('EmptySortedList');
+    return Math.max.apply(Math,this.items)
+  }
 
-  min() {}
+  min() {
+    if(this.length === 0) throw new Error('EmptySortedList');
+    return Math.min.apply(Math,this.items)
+  }
 
-  sum() {}
+  sum() {
+    if(this.length === 0) return 0
+    return this.items.reduce((acc, cur) => acc + cur)
+  }
 
-  avg() {}
+  avg() {
+    if(this.length === 0) throw new Error('EmptySortedList');
+    return this.sum()/this.length
+  }
 }
 
 module.exports = SortedList;
