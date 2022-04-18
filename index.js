bfbe2dfdb5c0dd9c2a93840576d2b7150dbe82ee
Georgina Aceves <aceves.georgina@hotmail.com>
2022-04-19 01:02:29 +0200

    done

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 class SortedList {
-  constructor() {}
+  constructor(items, length) {
+    this.items = [],
+    this.length = this.items.length
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item)
+    this.items.sort(function(a,b){
+      return a-b
+    })
+  }
 
-  get(pos) {}
+  get(pos) {
+    if(pos<this.item.length){
+    
+    }
+  }
 
   max() {}
 
