c2f4c0792c280ee47670e891846528fb0496090f
Leopoldo Navarrete <Leopoldo12@outlook.com>
2022-04-18 17:59:13 -0500

    Lab completo! Gracias Yani!

--- index.js
+++ index.js
@@ -1,17 +1,58 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
 
-  add(item) {}
+  }
 
-  get(pos) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a,b) => {return a-b});
 
-  max() {}
+    this.length = this.items.length;
+  }
 
-  min() {}
+  get(pos) {
+    if(!this.items[pos]){
+      throw new Error("OutOfBounds");
+    }
+    return this.items[pos]
+  }
 
-  sum() {}
+  max() {
+    if(this.items.length == 0){
+      throw new Error("EmptySortedList")
+    }
+    return this.items.pop();
+  }
 
-  avg() {}
+  min() {
+    if (this.items.length == 0){
+      throw new Error("EmptySortedList")
+    }
+    return this.items.shift();
+  }
+
+  sum() {
+    let aggregateSum = 0;
+    this.items.forEach((element)=>{
+      aggregateSum += element
+    });
+
+    return aggregateSum
+  }
+
+  avg() {
+    if(this.items.length ==0){
+      throw new Error("EmptySortedList")
+    }
+    let aggregateSum = 0;
+    this.items.forEach((element) => {
+      aggregateSum += element
+    });
+
+    return aggregateSum/this.items.length
+  }
 }
 
 module.exports = SortedList;
