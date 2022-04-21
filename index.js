c85e5b956c673e1dcf09b56e7a592bd4b693b746
Laia <laiamr77@gmail.com>
2022-04-21 21:24:48 +0200

    first commit

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items=[];
+    this.length=this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    //ensuring that the items array stays sorted in ascending order
+    this.items.sort();
+    this.length=this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {    
+    if((pos<0)||(pos>this.items.length)){
+      throw 'OutOfBounds';
+    }
+    return this.items[pos];
+  }
 
   max() {}
 
