29823eabfc01ee9a75a40ab5d70fd29c36008dce
Alfonso Cano <alfonsoheartagram@gmail.com>
2022-04-22 23:01:26 -0500

    done

--- index.js
+++ index.js
@@ -1,13 +1,38 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = []
+    this.length = this.items.length
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item)
+    this.items.sort((a, b) => a - b )
+    this.length = this.items.length
+  }
 
-  get(pos) {}
+  get(pos) {
+    if(this.items[pos]){
+      return this.items[pos]
+    }else{
+      throw new Error("OutOfBounds")
+    }
+  }
 
-  max() {}
+  max() {
+    if(this.items.length>0){
+      return  Math.max(...this.items)
+     }else{
+        throw new Error('EmptySortedList')
+     }
+  }
 
-  min() {}
+  min() {
+    if(this.items.length>0){
+      return  Math.min(...this.items)
+     }else{
+        throw new Error('EmptySortedList')
+     }
+  }
 
   sum() {}
 
