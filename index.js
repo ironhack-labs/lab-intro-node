d84f07ef4024836020b572ec30ba02c7696bb8a6
Marcelo <marcelomilhomemm@hotmail.com>
2022-05-04 00:14:16 +0100

    looks okay

--- index.js
+++ index.js
@@ -1,17 +1,56 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort();
+  }
 
-  get(pos) {}
+  get(pos) {
+    this.items[pos];
+    if (pos <= this.items.length) {
+      throw new Error("OutOfBounds");
+    }
+  }
 
-  max() {}
+  max() {
+    if (this.items === null) {
+      throw new Error("EmptySortedList");
+    }
+    return Math.max(...this.items);
+  }
 
-  min() {}
+  min() {
+    if (this.items === null) {
+      throw new Error("EmptySortedList");
+    }
+    return Math.min(...this.items);
+  }
 
-  sum() {}
+  sum() {
+    if (this.items === null) {
+      throw new Error("EmptySortedList");
+    }
+    let sum = 0;
+    for (let i = 0; i < this.length; i++) {
+      sum += this.items[i];
+    }
+    return sum;
+  }
 
-  avg() {}
+  avg() {
+    if (this.items === null) {
+      throw new Error("EmptySortedList");
+    }
+
+    const sumOfNumbers = this.items.reduce((previousValue, currentValue) => {
+      previousValue + currentValue;
+    });
+    return sumOfNumbers / this.length;
+  }
 }
 
 module.exports = SortedList;
