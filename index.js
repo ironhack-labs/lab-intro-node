96fb7c1764c106fb29caa11882804374b68b8b18
Catarina Salvado <catarinapsalvado@gmail.com>
2022-05-03 21:25:56 +0100

    almost done

--- index.js
+++ index.js
@@ -1,13 +1,35 @@
 class SortedList {
-  constructor() {}
+  constructor(items, length) {
+  this.items = [];
+  this.length = this.items.length
+  
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item)
+    this.items.sort()
 
-  get(pos) {}
+  }
 
-  max() {}
+  get(pos) {
+    
+  }
 
-  min() {}
+  max() {
+    if(this.items.length === 0){
+      throw new Error('EmptySortedList')
+  } else {
+    return Math.max(this.items)
+  }
+}
+
+  min() {
+    if(this.items.length === 0){
+      throw new Error('EmptySortedList')
+  } else {
+    return Min.max(this.items)
+  }
+  }
 
   sum() {}
 
