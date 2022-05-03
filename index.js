1b42704235d347c452e478003d4d070db16e62a4
Alexandre Alves Fernandes Junior <alexandrealvesfj@gmail.com>
2022-05-03 21:55:07 +0100

    Lab done + bonuses

--- index.js
+++ index.js
@@ -1,17 +1,55 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.length = this.items.length;
+    this.items.sort((a, b) => a - b);
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos <= this.items.length) {
+      return pos;
+    } else {
+      throw new Error("OutOfBounds");
+    }
+  }
 
-  max() {}
+  max() {
+    if (this.items.length === 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      return Math.max(...this.items);
+    }
+  }
 
-  min() {}
+  min() {
+    if (this.items.length === 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      return Math.min(...this.items);
+    }
+  }
 
-  sum() {}
+  sum() {
+    if (this.items.length === 0) {
+      return 0;
+    } else {
+      return this.items.reduce((a, b) => a + b);
+    }
+  }
 
-  avg() {}
+  avg() {
+    const sum = this.items.reduce((a, b) => a + b);
+    if (this.items.length === 0) {
+      return 0;
+    } else {
+      return sum / this.items.length;
+    }
+  }
 }
 
 module.exports = SortedList;
