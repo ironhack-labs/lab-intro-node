96be1d52a7898ea7d297e462699bce657ba22162
Matilde Costa <matildecosta@MacBook-Pro-de-Matilde.local>
2022-05-03 12:57:34 +0100

    mandatory lab done

--- index.js
+++ index.js
@@ -1,15 +1,46 @@
 class SortedList {
-  constructor() {}
+  constructor(items, length) {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a, b) => a - b);
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    this.items.indexOf(pos);
+    if (pos <= this.items.length) {
+      return this.items[pos];
+    } else {
+      throw new Error("OutOfBounds");
+    }
+  }
 
-  max() {}
+  max() {
+    if (this.items.length > 0) {
+      return Math.max(...this.items);
+    } else {
+      throw new Error("EmptySortedList");
+    }
+  }
 
-  min() {}
+  min() {
+    if (this.items.length > 0) {
+      return Math.min(...this.items);
+    } else {
+      throw new Error("EmptySortedList");
+    }
+  }
 
-  sum() {}
+  sum() {
+    const sum = this.items.reduce((sum, a) => {
+      sum + a, 0;
+    });
+    return sum;
+  }
 
   avg() {}
 }
