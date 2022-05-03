d3a07162f37f4e63559eaae056d88c1691c3994c
keven andrade <keven111299@gmail.com>
2022-05-03 22:21:33 +0100

    done

--- index.js
+++ index.js
@@ -1,17 +1,47 @@
 class SortedList {
-  constructor() {}
+    constructor() {
+        this.items = [];
+        this.length = this.items.length;
+    }
 
-  add(item) {}
+    add(item) {
+        this.items.push(item);
+        this.length = this.items.length;
+        this.items.sort((a, b) => a - b);
+    }
 
-  get(pos) {}
+    get(pos) {
+        if (this.length >= pos) {
+            return this.items[pos];
+        } else {
+            throw new Error("OutOfBounds");
+        }
+    }
 
-  max() {}
+    max() {
+        if (this.items) {
+            return Math.max(...this.items);
+        } else {
+            throw new Error("EmptySortedList");
+        }
+    }
 
-  min() {}
-
-  sum() {}
-
-  avg() {}
+    min() {
+        if (this.items) {
+            return Math.min(...this.items);
+        } else {
+            throw new Error("EmptySortedList");
+        }
+    }
 }
+
+sum() {
+    return this.items.reduce();
+}
+
+avg() {
+
+}
+
 
 module.exports = SortedList;
