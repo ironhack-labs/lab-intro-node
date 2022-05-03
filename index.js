1ca216049eca61ad92df99938c3d07179fc2c152
Mariana Pontes <marianasevinatep@gmail.com>
2022-05-03 18:16:08 +0100

    solved lab

--- index.js
+++ index.js
@@ -1,17 +1,60 @@
 class SortedList {
-  constructor() {}
+  constructor(items, length) {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.length = this.items.length;
+    this.items.sort(function (a, b) {
+      return a - b;
+    });
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos > this.items.length) {
+      throw new Error("OutOfBounds");
+    } else {
+      return this.items[pos];
+    }
+  }
 
-  max() {}
+  max() {
+    if (!this.length) {
+      throw new Error("EmptySortedList");
+    } else {
+      return Math.max(...this.items);
+    }
+  }
 
-  min() {}
+  min() {
+    if (!this.length) {
+      throw new Error("EmptySortedList");
+    } else {
+      return Math.min(...this.items);
+    }
+  }
 
-  sum() {}
+  sum() {
+    if (!this.length) {
+      return 0;
+    }
+    let result = 0;
+    for (let i = 0; i < this.items.length; i++) {
+      result = this.items[i] + this.items[i];
+    }
 
-  avg() {}
+    return result;
+  }
+
+  avg() {
+    if (!this.length) {
+      throw new Error("EmptySortedList");
+    }
+
+    return this.sum() / this.length;
+  }
 }
 
 module.exports = SortedList;
