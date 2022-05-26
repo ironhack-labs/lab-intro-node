96ff06cde52443e3b97993fce88c7b448808d1fb
ana luz martinez <almadigital.mx@gmail.com>
2022-05-26 11:59:52 -0700

    all iterations done

--- index.js
+++ index.js
@@ -8,24 +8,58 @@ class SortedList {
     this.items.push(item);
     this.length = this.items.length; //add single value to sorted list
     this.items.sort(function (a, b) {
+      //sort
       return a - b;
     });
   }
 
-  get(pos) {}
+  get(pos) {
+    //get position data
+    if (pos > this.length) {
+      //I also tried using OR lesset than but " should return the element in that position" didn't work
+      throw new Error("EmptySortedList");
+    } else {
+      return this.items[pos];
+    }
+  }
 
   max() {
-    //spread operator
-    math.max(...this.items);
+    if (!this.length) {
+      throw new Error("EmptySortedList");
+    } else {
+      //spread operator
+      return Math.max(...this.items);
+    }
   }
 
   min() {
-    min.max(...this.items);
+    if (!this.length) {
+      throw new Error("EmptySortedList");
+    } else {
+      //spread operator
+      return Math.min(...this.items);
+    }
   }
 
-  sum() {}
+  sum() {
+    if (this.length == 0) {
+      return 0;
+    } else {
+      let totalSum = 0;
+      for (const value of this.items) {
+        totalSum += value;
+      }
+      return totalSum;
+    }
+  }
 
-  avg() {}
+  avg() {
+    if (!this.length) {
+      throw new Error("EmptySortedList");
+    } else {
+      return this.sum() / this.length;
+    }
+  }
 }
 
 module.exports = SortedList;
