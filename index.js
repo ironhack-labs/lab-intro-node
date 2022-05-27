c722f70be67c8f5bc07120b3f0ba250615cb06c0
Carlos Perez <carlosa.ps@outlook.com>
2022-05-26 21:25:45 -0500

    Done

--- index.js
+++ index.js
@@ -1,17 +1,40 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort();
+  }
 
-  get(pos) {}
+  get(pos) {
+    if(pos < 0 || pos >= this.items.length) throw new Error('OutOfBounds');
+    else  return this.items[pos];
+  }
 
-  max() {}
+  max() {
+    if(this.items.length === 0) throw new Error('EmptySortedList');
+    else return Math.max(...items);
+  }
 
-  min() {}
+  min() {
+    if(this.items.length === 0) throw new Error('EmptySortedList');
+    else return Math.min(...items);
+  }
 
-  sum() {}
+  sum() {
+    let sum = this.items.reduce((acc,current)=>acc+current);
+    return sum;
+  }
 
-  avg() {}
+  avg() {
+    let avg = this.items.reduce((acc,current)=>{
+      let i = this.items.length;
+      return acc+current/i;
+    })
+  }
 }
 
 module.exports = SortedList;
