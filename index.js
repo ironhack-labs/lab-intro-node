86573b53cc4a56f120bcb3be259d0ad7d87bdf29
Ulises-lab <yaftemiranda@gmail.com>
2022-05-26 22:07:41 -0500

    done

--- index.js
+++ index.js
@@ -1,17 +1,55 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+	this.items = [];
+	this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+	this.items.push(item);
+	this.items.sort((a,b)=>a-b);
+	this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+	if(pos<this.length){
+		return this.items[pos];
+	}else{
+ 		throw new Error("OutOfBounds");
+	}
+	
+  }
 
-  max() {}
+  max() {
+	if(this.items.length>0){
+		return Math.max(...this.items);
+	}else{
+		throw new Error('EmptySortedList');
+	}
+  }
 
-  min() {}
+  min() {
+	if(this.items.length>0){
+		return Math.min(...this.items);
+	}else{
+		throw new Error('EmptySortedList');
+	}
+  }
 
-  sum() {}
+  sum() {
+	let count = 0;
+	for(let i = 0;i<this.length;i++){
+		count += this.items[i];
+	}
+	return count;
+  }
 
-  avg() {}
+  avg() {
+	if(this.length===0){
+		throw new Error('EmptySortedList');
+	}else{
+		return this.sum()/this.length;
+	}
+  }
 }
 
 module.exports = SortedList;
