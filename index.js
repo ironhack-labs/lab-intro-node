692026650a1e9720349218fb4d02936456e421a3
Azdhy Galvan <azdhy16@gmail.com>
2022-05-26 23:49:59 -0500

    WDPTMEX [Azdhy Galvan]

--- index.js
+++ index.js
@@ -1,17 +1,84 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items=[];
+    this.length=this.items.length
 
-  add(item) {}
+  }
 
-  get(pos) {}
+  add(item) {
+    this.items.push(item)
 
-  max() {}
+    this.items.sort(function(a,b){
 
-  min() {}
+    return a-b
 
-  sum() {}
+    })
 
-  avg() {}
+    this.length =this.items.length
+      
+  }
+
+  get(pos) {
+
+    if(pos < 0 || pos > this.items.length){
+      throw new Error('OutOfBounds');
+    }  
+
+    else{
+      this.items[pos]
+    }
+  }
+
+  max() {
+
+    if(this.items.length === 0){
+      throw new Error('EmptySortedList');
+    }
+    else{
+      return Math.max(...this.items);
+    }
+    
+
+  }
+
+  min() {
+
+    if(this.items.length === 0){
+      throw new Error('EmptySortedList');
+    }
+    else{
+      return Math.min(...this.items)
+    }
+
+  }
+
+  sum() {
+    let sum=0;
+
+    if (this.items.length === 0){
+      return 0;
+    }
+    else{
+      for(let i =0; i < this.items.length; i++){
+        sum += this.items[i]
+      }
+      
+    }
+    return sum 
+  }
+
+  avg() {
+   
+
+    if (this.items.length === 0){
+      throw new Error('EmptySortedList');
+    }
+    else {
+      return this.sum()/ this.length
+      }
+      
+    }
+    
 }
 
 module.exports = SortedList;
