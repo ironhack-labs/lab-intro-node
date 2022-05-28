9a6493871cd705f3ed6cb40aa8b57085ad1ccf07
Mapy Villalobos <mapyvillalobos@Mapys-MBP.bbrouter>
2022-05-28 11:15:35 -0500

    done

--- index.js
+++ index.js
@@ -1,17 +1,61 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort(function (a, b) {
+      return a - b;
+    });
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos >= this.items.length) {
+      throw new Error("OutOfBounds");
+    } else {
+      return this.items[pos];
+    }
+  }
 
-  max() {}
+  max() {
+    if ((this.length === 0)) {
+      throw new Error("EmptySortedList");
+    } else {
+      return this.items.reduce(function (a, b) {
+        return Math.max(a, b);
+      });
+    }
+  }
 
-  min() {}
+  min() {
+    if ((this.length === 0)) {
+      throw new Error("EmptySortedList");
+    } else {
+      return this.items.reduce(function (a, b) {
+        return Math.min(a, b);
+      });
+    }
+  }
 
-  sum() {}
+  sum() {
+    if (this.length === 0) {
+      return 0;
+    } else {
+      const reducer = (accumulator, curr) => accumulator + curr;
+      return this.items.reduce(reducer);
+    }
+  }
 
-  avg() {}
+  avg() {
+    if (this.length === 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      return this.sum() / this.length;
+    }
+  }
 }
 
 module.exports = SortedList;
