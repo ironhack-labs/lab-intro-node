cf96ab184e2b40c4994cdfdf4b8d68f595efdacc
ihab5520 <ihab.saleh@live.com>
2022-05-28 09:56:47 +0200

    done

--- index.js
+++ index.js
@@ -1,17 +1,90 @@
 class SortedList {
-  constructor() {}
+  constructor ()
+  {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add ( item )
+  {
+    this.items.push( item );
+    this.items.sort( function ( a, b )
+    {
+      return a - b;
+    } );
+    this.length = this.items;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (!this.items[pos]) {
+      throw new Error('OutOfBounds');
+    }
+    return this.items[pos];
+  }
 
-  max() {}
+  max() {
+    if (this.items.length === 0) {
+      throw new Error('EmptySortedList');
+    }
+    let number = 0;
+    this.items.forEach((item) => {
+      if (number < item) {
+        number = item;
+      }
+    });
+    return number;
+  }
 
-  min() {}
 
-  sum() {}
+  min() {
+    if (this.items.length === 0) {
+      throw new Error('EmptySortedList');
+    }
 
-  avg() {}
+
+
+    let number = this.items[0];
+    this.items.forEach((item) => {
+      if (number > item) {
+        number = item;
+      }
+    });
+    return number;
+  }
+
+
+  sum ( numbers )
+  {
+	  let sum = 0; 
+	  for (let i = 0; i < numbers.length; i++) {
+	    if (typeof numbers[i] === "number") {
+	      sum += numbers[i];
+	    } else if (typeof numbers[i] === "string") {
+	      sum += numbers[i].length;
+	    } else if (typeof numbers[i] === "boolean") {
+	      sum += numbers[i];
+	    } else {
+	      throw new Error("err");
+	    }
+	  }
+	  return sum; 
+	}
+
+
+  avg(numbersAvg) {
+    let average = 0; 
+  
+    if (numbersAvg.length === 0) {
+      return null; 
+    }
+  
+    for (let i = 0; i < numbersAvg.length; i++) {
+      average += numbersAvg[i];
+    }
+  
+    average = average / numbersAvg.length; 
+  
+    return average; 
+  }
 }
-
 module.exports = SortedList;
