889baef7c9252e44cacc7ca28cdc68f1dd2b21a5
Evgeniia Riaguzova <aglaya_2004@mail.ru>
2022-05-28 09:59:32 +0200

    solved with problems

--- index.js
+++ index.js
@@ -1,16 +1,55 @@
 class SortedList {
-  constructor() {}
+  constructor(items,length) {
+    this.items=[];
+    this.length= this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.item=item;
+    this.items.push(item);
+    this.length +=1;
+    this.items.sort((a,b)=>{a-b});
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos>=this.items.length) 
+    {throw new Error('OutOfBounds')}
+    else {return this.items[pos]}
+  }
 
-  max() {}
+  max() {
+    if (this.items=[])
+    {throw new Error('EmptySortedList')}
+    else {
+      let max = this.items.get(0);
 
-  min() {}
+      for (let i = 1; i < this.items.length; i++) {
+          if (this.items.get(i) > max) {
+            max = this.items.get(i);
+          }
+      }
+      //return Math.max.apply(null, this.items)
+      };
+    }
+  
 
-  sum() {}
+  min() {
+    if (this.items=[])
+    {throw new Error('EmptySortedList')}
+    else {return Math.min.apply(null, this.items)
+      };
+      
+  }
 
+  sum() {
+  /*this.summ =0;
+  if (this.items=[]) {return 0;
+  } else{
+  for (let i=0; i<this.items.length; i++){
+    summ += this.items[i]
+  return this.summ}
+  }*/
+  }
   avg() {}
 }
 
