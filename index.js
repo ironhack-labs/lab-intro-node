b74d6151c1a948d88c869295a0e9f0908db932d6
Iván Rodríguez <82571096+Ivan180687@users.noreply.github.com>
2022-06-19 20:20:19 +0200

    solved most of the test, problems with the error tests

--- index.js
+++ index.js
@@ -1,17 +1,63 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = []
+    this.length = this.items.length
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item)
+    return this.items.sort(function(a, b) {
+      return a - b
+    })
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (!this.items[pos]) {
+      return Error("OutOfBounds")
+    } else {
+      return this.items[pos]
+    }
+  }
 
-  max() {}
+  max() {
+    if (this.items.length === 0) {
+      return Error("EmptySortedList")
+    } else {
+      return this.items[this.items.length -1]
+    }
+  }
 
-  min() {}
+  min() {
+    if (this.items.length === 0) {
+      return Error("EmptySortedList")
+    } else {
+      return this.items[0]
+    }
+  }
 
-  sum() {}
+  sum() {
+    if (this.items.length === 0) {
+      return 0
+    } else {
+      let sum = 0
+      for (let i = 0; i < this.items.length; i++) {
+        sum += this.items[i]
+      }
+      return sum
+    }
+  }
 
-  avg() {}
+  avg() {
+    if (this.items.length === 0) {
+      return Error("EmptySortedList")
+    } else {
+      let sum = 0
+      for (let i = 0; i < this.items.length; i++) {
+        sum += this.items[i]
+      }
+      return sum / this.items.length
+    }
+  }
 }
 
 module.exports = SortedList;
