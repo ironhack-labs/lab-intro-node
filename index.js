9a7e85e5bd0ab0ffb492065fc6a9095488fda762
LuisEUM <luiseum95@gmail.com>
2022-06-20 19:04:25 +0200

    No me funciona ninguno de los test, pero esta resuelto todos los problemas sin lanzar los errores, lo que piden de Throw

--- index.js
+++ index.js
@@ -1,17 +1,51 @@
 class SortedList {
-  constructor() {}
+  constructor(items, length) {
+    this.items = items;
+    this.length = length
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item)
+    this.items.sort()
+    this.length = this.items.length
+  }
 
-  get(pos) {}
+  get(pos) {
+    if(pos >= this.items.length){
+      throw new Error("OutOfBounds")
+    } else {
+      return this.items[pos]
+    }
+  }
 
-  max() {}
+  max() {
+    let lastNumber = this.items.length - 1
+    return this.items[lastNumber]
+  }
 
-  min() {}
+  min() {
+    this.items.sort()
+    return this.items[0]
+  }
 
-  sum() {}
+  sum() {
+    if(this.items.length <= 0){
+      return 0
+    }
+    let sumSortedList = this.items.reduce((sum,element)=>{
+      return  sum += element
+    },0)
+    return sumSortedList 
+  }
 
-  avg() {}
+  avg() {
+    let sumSortedList = this.items.reduce((sum,element)=>{
+      return  sum += element
+    },0)
+    return sumSortedList /this.items.length
+  }
 }
 
 module.exports = SortedList;
+
+console.log(new SortedList)
