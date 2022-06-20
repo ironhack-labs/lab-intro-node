8fe9209044bd8fd8b9ab61ae72833468b8e56277
Adrian Perez <adrian.perez.conde22@gmail.com>
2022-06-20 14:31:29 +0200

    done

--- index.js
+++ index.js
@@ -1,17 +1,64 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    
+    this.items.sort((a,b) => {
+      if (a > b) {
+        return 1;
+      } else if (a < b) {
+        return -1;
+      } else {
+        return 0;
+      }
+    });
+    
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos > this.items.length) {
+      throw new Error('OutOfBounds');
+    };
 
-  max() {}
+    return this.items[pos];
+  }
 
-  min() {}
+  max() {
+    if (this.length === 0) {
+      throw new Error('EmptySortedList');
+    };
 
-  sum() {}
+    return Math.max(...this.items);
+  }
 
-  avg() {}
+  min() {
+    if (this.length === 0) {
+      throw new Error('EmptySortedList');
+    };
+
+    return Math.min(...this.items);
+  }
+
+  sum() {
+    if (this.length === 0) {
+      return 0
+    };
+
+    return this.items.reduce((prevNumber, nextNumber) => prevNumber + nextNumber, 0);
+  }
+
+  avg() {
+    if (this.length === 0) {
+      throw new Error('EmptySortedList');
+    };
+
+    return this.sum() / this.length;
+  }
 }
 
 module.exports = SortedList;
