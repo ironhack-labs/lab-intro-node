16f8998d94337407a12694a7a2b24b534f73ebe6
antonio-datahack <antoniosastre@tutanota.com>
2022-06-21 17:00:52 +0200

    done

--- index.js
+++ index.js
@@ -1,17 +1,46 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a, b) => a - b);
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos > this.items.length) throw new Error("Out of bounds");
+    return this.items[pos];
+  }
 
-  max() {}
+  max() {
+    if (this.items.length === 0) throw new Error("EmptySortedList");
+    return Math.max(...this.items);
+  }
 
-  min() {}
+  min() {
+    if (this.items.length === 0) throw new Error("EmptySortedList");
+    return Math.min(...this.items);
+  }
 
-  sum() {}
+  sum() {
+    if (this.items.length === 0) {
+      return 0;
+    } else {
+      const reducer = (acc, curr) => acc + curr;
+      return this.items.reduce(reducer);
+    }
+  }
 
-  avg() {}
+  avg() {
+    if (this.items.length === 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      return this.sum() / this.items.length;
+    }
+  }
 }
 
 module.exports = SortedList;
