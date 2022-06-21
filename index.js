b24c5423b96a489b5df72957f3d5b34eb7a087ff
WHL99 <xjv298347@gmail.com>
2022-06-21 15:57:18 +0200

    dine

--- index.js
+++ index.js
@@ -1,17 +1,64 @@
 class SortedList {
-  constructor() {}
 
-  add(item) {}
+  constructor() {
+    //this.SortedList = new SortedList()
+    this.items = []
+    this.length = this.items.length
+  }
 
-  get(pos) {}
+  add(item) {
 
-  max() {}
+    this.length = this.items.push(item)
+    //this.items.sort()
+    this.items.sort(function (a, b) {
+      return a - b
+    })
+  }
 
-  min() {}
+  get(pos) {
+    if (this.items.at(pos) === undefined) {
+      throw new Error('OutOfBounds');
+    }
+    return this.items.at(pos);
+  }
 
-  sum() {}
+  max() {
 
-  avg() {}
+    if (this.length === 0) {
+      throw new Error('EmptySortedList');
+    }
+
+    return Math.max(...this.items)
+
+  }
+  min() {
+    if (this.length === 0) {
+      throw new Error('EmptySortedList');
+    }
+    return Math.min(...this.items)
+  }
+
+
+  sum() {
+    
+    if (this.length === 0) {
+      return 0
+    }
+    
+    return this.items.reduce(function (sum, val) {
+     return sum + val
+    })
+    
+  }
+
+  avg() {
+    if (this.length === 0) {
+      throw new Error('EmptySortedList');
+    }
+
+    return this.sum() / this.length
+   
+  }
 }
 
 module.exports = SortedList;
