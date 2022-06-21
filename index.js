453f28901b534f271c476422ab5f9b943a2373eb
Deniz Saganak <saganak.deniz@gmail.com>
2022-06-21 16:27:10 +0200

    done

--- index.js
+++ index.js
@@ -1,17 +1,56 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a,b) => a-b);
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+   if(this.items[pos] === undefined){
+    throw new Error('OutOfBounds');
+   } 
+   return this.items[pos];  
+  }
 
-  max() {}
+  max() {
+    if(this.items.length === 0){
+      throw new Error('EmptySortedList');
+    }
+    return Math.max(...this.items)
+  }
 
-  min() {}
+  min() {
+    if(this.items.length === 0){
+      throw new Error('EmptySortedList');
+    }
+    return Math.min(...this.items)
+  }
 
-  sum() {}
+  sum() { 
+    let sum = 0
+    for(let i = 0; i < this.items.length; i++){
+      sum += this.items[i];
+    }
+    return sum;
+  }
 
-  avg() {}
+  avg() {
+    if(this.items.length === 0){
+      throw new Error('EmptySortedList');
+    };
+    let sum = 0;
+    let average = 0;
+    for(let i = 0; i < this.items.length; i++){
+      sum += this.items[i];
+    };
+    average = sum / this.items.length;
+    return average;
+  }
 }
 
 module.exports = SortedList;
