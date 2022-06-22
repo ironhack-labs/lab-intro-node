ecdcb4c6ee4e9abfb47498db7e7ece4f0b5d4626
Jule <bingthemachine@gmail.com>
2022-06-22 09:15:16 +0200

    finished

--- index.js
+++ index.js
@@ -1,17 +1,54 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = 0;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort(function (a, b) { return a - b });
+    this.length = this.items.length
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos <= this.length) {
+      return this.items[pos];
+    } else { }
+    throw new Error('OutOfBounds');
+  }
 
-  max() {}
+  max() {
+    if (this.length == 0) {
+      throw new Error('EmptySortedList');
+    } else {
+      let max = Math.max(...this.items);
+      return max
+    }
+  }
 
-  min() {}
+  min() {
+    if (this.length == 0) {
+      throw new Error('EmptySortedList');
+    } else {
+      return this.items[0];
+    }
+  }
 
-  sum() {}
+  sum() {
+    if (this.items.length == 0) {
+      return 0;
+    } else {
+      return this.items.reduce((a, b) => a + b, 0)
+    }
+  }
 
-  avg() {}
+  avg() {
+    if (this.items.length == 0) {
+      throw new Error('EmptySortedList');
+    } else {
+      return this.items.reduce((a, b) => a + b, 0) / this.length
+    }
+  }
 }
 
 module.exports = SortedList;
