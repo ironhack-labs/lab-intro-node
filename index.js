e72f3ec9409ee3190919bdd8cafdfc0d567a71bb
Siobhan Doherty <siobhandoherty18@gmail.com>
2022-06-26 22:53:10 +0200

    done

--- index.js
+++ index.js
@@ -1,17 +1,50 @@
 class SortedList {
-  constructor() {}
+  constructor(items, length) {
+    this.items = [] 
+    this.length = 0;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item); 
+    this.items.sort(function (a, b) {return a - b}); 
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos > this.items.length) {
+      throw new Error('OutOfBounds'); 
+    } 
+    return this.items[pos]
+  }
 
-  max() {}
+  max() {
+    if (this.length === 0) {
+      throw new Error('EmptySortedList');
+    }
+    return this.items.sort(function (a, b) {return a - b})[this.items.length - 1];
+  }
 
-  min() {}
+  min() {
+    if (this.length === 0) { 
+      throw new Error('EmptySortedList');
+    } 
+    return this.items.sort(function (a, b) { return a - b})[0];
+  }
 
-  sum() {}
+  sum() { 
+    if (this.length == 0) {
+      return 0
+    } 
+    return this.items.reduce((sum, element) => sum + element)
+  }
 
-  avg() {}
+  avg() {
+    if (this.length === 0) {
+      throw new Error('EmptySortedList'); 
+    } else {
+      return this.sum()/this.items.length;
+    }
+  }
 }
 
 module.exports = SortedList;
