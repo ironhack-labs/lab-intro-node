010eadb717040d22e7e0a8b9de4954b02d5ff37d
Fran <94910215+FranEjarque@users.noreply.github.com>
2022-06-28 17:25:23 +0200

    done

--- index.js
+++ index.js
@@ -1,17 +1,70 @@
 class SortedList {
-  constructor() {}
+  constructor(items, length) {
+      this.items = [];
+      this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort(function(a,b) {
+      return a - b;
+    })
+    this.length = this.items.length
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (this.items.length < pos) {
+      throw new Error('OutOfBounds'); 
+    } else {
+      return this.items[pos];
+    }
+  };
 
-  max() {}
+  max() {
+    if ( this.items.length ===  0 ) {
+      throw new Error('EmptySortedList');
+    } else {
+      const max = Math.max(...this.items);
 
-  min() {}
+      return max;
+    }
+  }
+     
+  
+  min() {
+    if ( this.items.length ===  0 ) {
+      throw new Error('EmptySortedList');
+    } else {
+      const min = Math.min(...this.items);
+    return min;
+    }
+  }
 
-  sum() {}
+  sum(items) {
+    let sum = 0;
+    for (let i = 0; i < this.items.length; i++)
+      sum += this.items[i];
+    return sum; 
+    }
 
-  avg() {}
+  avg() {
+  
+    if ( this.items.length ===  0 ) {
+      throw new Error('EmptySortedList');
+    } else {
+      const average = this.items.reduce((a,b) => a + b, 0) / this.items.length;
+      return average; 
+    }
+  }
 }
+
+let item = new SortedList()
+
+// item.add();
+// item.get();
+// item.min();
+// item.sum();
+// item.avg();
+
 
 module.exports = SortedList;
