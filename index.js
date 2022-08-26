920a3d8fef6ad1ef20abbb6f6946e51c1ed4b2aa
Jonathan Raposo <jonathan.raposo.luna@gmail.com>
2022-08-26 01:41:41 -0400

    lab done

--- index.js
+++ index.js
@@ -1,17 +1,59 @@
 class SortedList {
-  constructor() {}
+  constructor(items, length) {
 
-  add(item) {}
+    this.items = [];
+    this.length = this.items.length;
+  }
+   
 
-  get(pos) {}
+  add(item) {
+    this.items.push(item);
+    this.length++;
+    let sortedArr = this.item.sort( (a,b) => {
+      return a-b;
+    })
+    
+  
+   return sortedArr;
+  }
+   
+  get(pos) {
+    if(pos > this.length){
+      throw new Error('OutOfBounds');
+    }
+    return this.items[pos];
+  }
 
-  max() {}
+  max() {
 
-  min() {}
+    if(this.length === 0){
+      throw new Error('EmptySortedList');
+    } 
+      return this.items[this.length-1];
+    
+   
+  }
+   
+  min() {
 
-  sum() {}
+    if( this.length === 0 ){
+      throw new Error('EmptySortedlist')
+    } 
+    return this.items[0];
+  }
 
-  avg() {}
+  sum() {
+    let total = 0;
+    for(let i = 0 ; i < this.length; i++){
+      total += this.items[i];
+    }
+    return total;
+  }
+
+  avg() {
+    let avg =  this.sum()/this.length;
+    return avg;
+  }
 }
 
 module.exports = SortedList;
