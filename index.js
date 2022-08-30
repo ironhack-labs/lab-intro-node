6210473a1b322e288c2163496212bceaaa3c03e6
Pavel Popov <pcveykos@gmail.com>
2022-08-30 17:02:56 +0200

    Finished lab

--- index.js
+++ index.js
@@ -1,17 +1,50 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = 0;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a,b) => a-b)
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos <= this.length - 1) {
+      return this.items[pos]
+    } else {
+      throw new Error('OutOfBounds')
+    }
+  }
 
-  max() {}
+  max() {
+    if (this.length > 0) {
+      return this.items[this.length - 1]
+    } else {
+      throw new Error('EmptySortedList');
+    }
+  }
 
-  min() {}
+  min() {
+    if (this.length > 0) {
+      return this.items[0]
+    } else {
+      throw new Error('EmptySortedList')
+    }
+  }
 
-  sum() {}
+  sum() {
+    let sum;
+    this.length > 0 ?  sum = this.items.reduce((a,b) => a+b) : sum = 0
+    return sum
+  }
 
-  avg() {}
+  avg() {
+    if (this.length === 0) return new Error(EmptySortedList)
+    let avg = this.sum() / this.length
+    return avg
+  }
 }
 
 module.exports = SortedList;
