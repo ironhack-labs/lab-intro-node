6d4b7c566e0e3ac551c067d1d8e711f54167b215
Renata Inojosa <inojosa.renata@gmail.com>
2022-09-29 22:38:48 -0300

    iterations 1, 2 and 3 done

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 class SortedList {
-  constructor() {}
+  constructor(items = []) {
+    this.items = items;
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a, b) => a - b);
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos > this.length) throw new Error('OutOfBounds');
+    return this.items[pos];
+  }
 
   max() {}
 
