6a92f5b0d8559925653d4a1f731c668d0c888bc9
Walison Abram <abramwhas@gmail.com>
2022-09-29 22:52:23 -0300

    1, 2 and 3 done

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 class SortedList {
-  constructor() {}
+  constructor(items = []) {
+    this.items = items;
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a, b) => a - b);
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos > this.length) {
+      throw new Error('OutOfBounds')
+    } else {
+      return this.items[pos]
+    }
+  }
 
   max() {}
 
