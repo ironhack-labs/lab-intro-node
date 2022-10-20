ca559d8b5f06590efcabed85d5718fc5efccc1cc
Iury Flores <iury.flores@gmail.com>
2022-10-20 20:59:52 -0300

    Iteration 2 Done

--- index.js
+++ index.js
@@ -4,7 +4,13 @@ class SortedList {
     this.length = this.items.length;
   }
 
-  add(item) {}
+  add(item) {
+    if (item) {
+      this.items.push(item);
+      this.items.sort((a, b) => a - b);
+      this.length++;
+    }
+  }
 
   get(pos) {}
 
