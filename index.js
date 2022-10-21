eed565fc5f82e9de4f5347a1540cba1aa84ae95c
amandahp <amandahoffmann96@gmail.com>
2022-10-21 11:46:55 -0300

    feat: finished lab

--- index.js
+++ index.js
@@ -1,17 +1,55 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
 
-  get(pos) {}
+  add(item) {
+    this.items.push(item)
+    this.items.sort((a, b) => a - b)
+    this.length = this.items.length
+  }
 
-  max() {}
+  get(pos) {
+    if (pos <= this.items.length && pos !== 0) {
+      return this.items[pos]
+    } else {
+      throw new Error("OutOfBounds")
 
-  min() {}
+    }
+  }
 
-  sum() {}
+  max() {
+    if (this.length > 0) {
+      return this.items[this.length - 1]
+    } else {
+      throw new Error('EmptySortedList');
+    }
+  }
 
-  avg() {}
+  min() {
+    if (this.length > 0) {
+      return this.items[0]
+    } else {
+      throw new Error('EmptySortedList');
+    }
+  }
+
+  sum() {
+    if (this.length > 0) {
+      return this.items.reduce((a, b) => a + b)
+    }
+    return 0
+  }
+
+  avg() {
+    if (this.length > 0) {
+      return this.items.reduce((a, b) => a + b, 0) / this.length;
+    } else {
+      throw new Error('EmptySortedList');
+    }
+  }
 }
 
 module.exports = SortedList;
