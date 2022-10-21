7e3d7fada785509f466b127e96eda4d3078c5f91
Shania <shaniaantezana@gmail.com>
2022-10-21 15:23:40 -0400

    done

--- index.js
+++ index.js
@@ -1,17 +1,50 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item)
+    let itemsSorted = this.items.sort((a,b)=> a - b)
+    this.items = itemsSorted
+    this.length = itemsSorted.length
+  }
 
-  get(pos) {}
+  get(pos) {
+    if(pos === this.length - 1 || pos < this.length && pos >= 0 ){ 
+      return this.items[pos] 
+    } else{
+      throw new Error('OutOfBounds');
+    }
+  }
 
-  max() {}
+  max() {
+    if(this.length === 0 ) {
+      throw new Error('EmptySortedList');
+    }else{ 
+      return Math.max(...this.items)
+    }
+  }
 
-  min() {}
+  min() {
+    if(this.length === 0) {
+      throw new Error('EmptySortedList');
+    }else{ 
+      return Math.min(...this.items)
+    }
+  }
 
-  sum() {}
+  sum() {
+    return this.items.reduce((acc,cur)=> acc + cur,0)
+  }
 
-  avg() {}
+  avg() {
+    if(this.length === 0 ) {
+      throw new Error('EmptySortedList');
+    }else{ 
+      return this.sum()/this.length
+    }
+  }
 }
-
 module.exports = SortedList;
