78ba5f3334810ec3a101bbf040e03a09a4d32564
Iury Flores <iury.flores@gmail.com>
2022-10-20 21:36:47 -0300

    Iteration 3, 4, 5, 6 and 7 Done - Lab Done

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 class SortedList {
-  constructor(items, length) {
+  constructor() {
     this.items = [];
     this.length = this.items.length;
   }
@@ -12,15 +12,51 @@ class SortedList {
     }
   }
 
-  get(pos) {}
+  get(pos) {
+    if (pos > this.items.length) {
+      throw new Error("OutOfBounds");
+    }
+    return pos;
+  }
 
-  max() {}
+  max() {
+    if (this.items.length > 0) {
+      let highest = 0;
+      for (let i = 0; i < this.items.length; i++) {
+        if (highest < this.items[i]) {
+          highest = this.items[i];
+        }
+      }
+      return highest;
+    }
+    throw new Error("EmptySortedList");
+  }
 
-  min() {}
+  min() {
+    if (this.items.length > 0) {
+      return this.items[0];
+    }
+    throw new Erro("EmptySotedList");
+  }
 
-  sum() {}
+  sum() {
+    let sum = 0;
+    for (let i = 0; i < this.items.length; i++) {
+      sum += this.items[i];
+    }
+    return sum;
+  }
 
-  avg() {}
+  avg() {
+    if (this.items.length <= 0) {
+      throw new Erro("EmptySortedList");
+    }
+    let sum = 0;
+    for (let i = 0; i < this.items.length; i++) {
+      sum += this.items[i];
+    }
+    return sum / this.items.length;
+  }
 }
 
 module.exports = SortedList;
