c148813c1839a02d57487d0a56b3c3f956533a78
Alfie Collins <alfiecollins44@hotmail.co.uk>
2022-10-26 15:19:50 +0100

    done including bonuses

--- index.js
+++ index.js
@@ -1,17 +1,47 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item)
+    this.items.sort((a, b) => a - b);
+    this.length+=1
+  }
 
-  get(pos) {}
+  get(pos) {
+    if(pos<this.length && pos>=0){
+return this.items[pos]
+    }
+    else throw new Error('OutOfBounds');
+  }
 
-  max() {}
+  max() {
+    if (!this.length) throw new Error('EmptySortedList');
+    return this.items[this.length-1];
+  }
+  
 
-  min() {}
+  min() {
+    if (!this.length) throw new Error('EmptySortedList');
+    return this.items[0]
+  }
 
-  sum() {}
+  sum() {
+     this.sum =this.items.reduce(function(total, current) { 
+  return total + current
+}, 0)
+return this.sum
+}
 
-  avg() {}
+  avg() {
+    if (!this.length) throw new Error('EmptySortedList');
+    this.sum =this.items.reduce(function(total, current) { 
+      return total + current
+    }, 0)
+    return this.sum/this.length
+  }
 }
 
 module.exports = SortedList;
