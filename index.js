a770237dc941b76ef20bc609a0c3a5cf8045d615
cursedxp <anilozsoy@gmail.com>
2022-10-26 22:54:38 +0200

    Iteration 5: min()

--- index.js
+++ index.js
@@ -27,7 +27,13 @@ class SortedList {
     return this.items[this.length - 1];
   }
 
-  min() {}
+  min() {
+    if (this.length == 0) {
+      throw new Error("EmptySortedList");
+    }
+
+    return this.items[0];
+  }
 
   sum() {}
 
