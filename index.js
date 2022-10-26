ff3e6850567b33dd0586869fe69651026ed02c3a
Paulfresnel <paul.fresnel1@gmail.com>
2022-10-26 02:41:10 +0200

    Done

--- index.js
+++ index.js
@@ -1,17 +1,66 @@
+
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+    
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a,b)=>{
+      return a-b;
+    });
+    this.length = this.items.length;
+  }
+  get(pos) {
+    if (pos <=this.items.length){
+    return this.items[pos];
+    } else {
+    throw new Error('OutOfBounds')
+    }
+  }
 
-  get(pos) {}
+  max() {
+    if (this.items.length !== 0){
+    return this.items[this.items.length-1]
+    } else {
+      throw new Error('EmptySortedList')
+    }
+  }
 
-  max() {}
+  min() {
+    if (this.items.length !== 0){
+      return this.items[0];
+      } else {
+        throw new Error('EmptySortedList');
+      }
+    }
+  
 
-  min() {}
+  sum() {
+    if (this.items.length ===0){
+      return 0;
+    }
+    let sum = 0;
+    for (let i=0;i<this.items.length;i++){
+      sum += this.items[i];
+    }
+    return sum;
+  }
 
-  sum() {}
-
-  avg() {}
+  avg() {
+    if (this.items.length ===0){
+      throw new Error('EmptySortedList')
+    } else{
+    let sum = 0;
+    for (let i=0;i<this.items.length;i++){
+      sum += this.items[i];
+    }
+    return sum/this.items.length;
+  }
 }
+  }
 
 module.exports = SortedList;
+
