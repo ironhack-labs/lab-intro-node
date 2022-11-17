957acbda4463bd3565f2d64ea8814e4d395e6dbb
Cristian Gomez <gmz.mrs.cristian@gmail.com>
2022-11-17 16:18:58 -0600

    Done the class

--- index.js
+++ index.js
@@ -1,17 +1,49 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.length = this.items.length;
+    this.items.sort( (a,b) => { return a - b} );
+  }
 
-  get(pos) {}
+  get(pos) {
+    let res = this.items.indexOf(pos)
+    if(res === -1) throw new Error('OutOfBounds');
+    else return res;
+  }
 
-  max() {}
+  max() {
+    if(this.items.length === 0) throw new Error('EmptySortedList');
+    else {
+      let arr = this.items.sort((a,b) => {return b - a})
+      return arr[0];
+    }
+  }
 
-  min() {}
+  min() {
+    if(this.items.length === 0) throw new Error('EmptySortedList');
+    else {
+      let arr = this.items.sort((a,b) => {return a - b})
+      return arr[0];
+    }
+  }
 
-  sum() {}
+  sum() {
+    const suma = this.items.reduce((pv,cv) => pv + cv, 0);
+    return suma;
+  }
 
-  avg() {}
+  avg() {
+    if(this.items.length === 0) throw new Error('EmptySortedList');
+    else {
+      const suma = this.sum();
+      return suma/this.items.length;
+    }
+  }
 }
 
 module.exports = SortedList;
