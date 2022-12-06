db008dbb00ed15240e7d0254254224cc1b01a106
Henrique-mazzoleni <henrique.mazzoleni@gmail.com>
2022-12-06 21:39:10 +0100

    iteration #1 completed

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = []
+    this.length = this.items.length
+  }
 
-  add(item) {}
+  add(item) {
+    // this.items.push(item)
+  }
 
-  get(pos) {}
+  get(pos) {
+    // if (this.items[pos] === undefined) throw new Error('OutOfBounds: element not on list')
+    // return this.items[pos]
+  }
 
   max() {}
 
