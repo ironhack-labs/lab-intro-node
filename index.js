46a9d96c5914fc8579b5aacec1527ffb24353047
Thor Löfgren <lofgrenthor@gmail.com>
2022-12-07 07:27:49 +0100

    Complete iterations 1 - 7. Lab done

--- index.js
+++ index.js
@@ -1,17 +1,43 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = 0;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a, b) => a - b);
 
-  get(pos) {}
+    this.length++;
+  }
 
-  max() {}
+  get(pos) {
+    if (pos < 0 || pos > this.length - 1) throw new Error("OutOfBounds");
 
-  min() {}
+    return this.items[pos];
+  }
 
-  sum() {}
+  max() {
+    if (!this.length) throw new Error("EmptySortedList");
 
-  avg() {}
+    return this.items[this.length - 1];
+  }
+
+  min() {
+    if (!this.length) throw new Error("EmptySortedList");
+
+    return this.items[0];
+  }
+
+  sum() {
+    return this.items.reduce((total, curr) => total + curr, 0);
+  }
+
+  avg() {
+    if (!this.length) throw new Error("EmptySortedList");
+
+    return this.sum() / this.length;
+  }
 }
 
 module.exports = SortedList;
