72c696a4f1ff820b55b9a659b604728050ca9ef6
Alona Donovska <alonadonovska@gmail.com>
2022-12-07 20:09:45 +0100

    done

--- index.js
+++ index.js
@@ -1,9 +1,31 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = []
+    this.length = 0
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.length ++;
+    this.items.sort((a,b) => {
+      if(a>b) {
+        return 1
+      }
+      else if (b>a) {
+        return -1
+      }
+      else {
+        return 0
+      }
+    })
+  }
 
-  get(pos) {}
+  get(pos) {
+     if (pos>this.length){
+      throw new Error('OutOfBounds');
+     }
+     return this.items[pos]
+  }
 
   max() {}
 
