e38f3c223ad93ac27a9bb10fd5fb2dfd0dd52f68
artweise <art2wsxweise7ujm@gmail.com>
2022-12-08 15:40:30 +0100

    the 7th iteration completed, improved the min() and max() methods, sum() method and found the mistake in word length in the add() method, one test is not passing still in 2nd iteration, didn't find the issue

--- index.js
+++ index.js
@@ -4,12 +4,11 @@ class SortedList {
   }
 
   add(item) {
-    // if (!this.items.length) this.items.push(item);
     this.items.push(item);
-    this.lenght++;
     this.items.sort((a, b) => {
       a - b;
     });
+    this.length++;
   }
 
   get(pos) {
@@ -19,29 +18,36 @@ class SortedList {
 
   max() {
     if (!this.items.length) throw new Error("EmptySortedList");
-    // with reduce() can be used to find the maximum element in a numeric array, by comparing each value
-    return this.items.reduce((a, b) => Math.max(a, b));
-    // another approach, but this should only be used for arrays with relatively few elements
-    //both spread (...) and apply() will either fail or return the wrong result if the array has too many elements,
-    // because they try to pass the array elements as function parameters.
-    //The reduce solution does not have this problem.
+    // because of the sorting the array, we can return easily the last element, which will be the highest value of the array
+    return this.items[this.items.length - 1];
+    // *** another approach, when we don't sort the array in ascending order:
+    // *** with reduce() can be used to find the maximum element in a numeric array, by comparing each value
+    // return this.items.reduce((a, b) => Math.max(a, b));
+    // *** another approach, but this should only be used for arrays with relatively few elements
+    // *** both spread (...) and apply() will either fail or return the wrong result if the array has too many elements,
+    // *** because they try to pass the array elements as function parameters.
+    // *** The reduce solution does not have this problem.
     // return Math.max(...this.items);
   }
 
   min() {
     if (!this.items.length) throw new Error("EmptySortedList");
+    // *** because of the sorting the array, we can return easily the first element, which will be the lowest value of the array
+    return this.items[0];
+    // *** another approach, when we don't sort the array in ascending order:
     return this.items.reduce((a, b) => Math.min(a, b));
   }
 
   sum() {
-    if (!this.items.length) return 0;
-    return this.items.reduce(
-      (previousValue, currentValue) => previousValue + currentValue,
-      0
-    );
+    // if (!this.items.length) return 0;
+    // because of initialValue = 0, we don't need the first line
+    return Number(this.items.reduce((a, b) => a + b, 0));
   }
 
-  avg() {}
+  avg() {
+    if (!this.items.length) throw new Error("EmptySortedList");
+    return Number(this.sum() / this.items.length);
+  }
 }
 
 module.exports = SortedList;
