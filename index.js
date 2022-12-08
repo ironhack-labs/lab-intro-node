b39048c13bc0d03c6c4ea52b88dbca0481db3c45
artweise <art2wsxweise7ujm@gmail.com>
2022-12-08 16:20:36 +0100

    found the error with the 2nd iteration, all test are running

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 class SortedList {
   constructor() {
-    (this.items = []), (this.length = 0);
+    this.items = [];
+    this.length = this.items.length;
   }
 
   add(item) {
     this.items.push(item);
-    this.items.sort((a, b) => {
-      a - b;
-    });
     this.length++;
+    this.items.sort((a, b) => {
+      return a - b;
+    });
   }
 
   get(pos) {
