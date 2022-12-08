fcd9d90a688937ea14925c9f399aa258d2c9d2b4
Sirah Tabarez <sartab2011@gmail.com>
2022-12-08 18:31:53 +0100

    iteration 1-3 attempted 4 and 5

--- index.js
+++ index.js
@@ -1,15 +1,49 @@
 class SortedList {
-  constructor() {}
+  constructor(items, length) {
 
-  add(item) {}
+    this.items = [];
+    this.length = 0;
 
-  get(pos) {}
+  }
 
-  max() {}
+  add(item) {
+    this.items.push(item)
+    this.items.sort((a, b)=> a - b);
+    this.length = this.items.length
+   
+  }
+//had help with this one
+  get(pos) {
+    if(this.items[pos]){
+    return this.items[pos]
+  }else{
 
-  min() {}
+    throw new Error('OutOfBounds');
+  }
 
-  sum() {}
+}
+  
+
+  max() {
+    if(this.length > 0){
+    return Math.max(...this.items)
+    }else{
+    throw new Error('EmptySortedList');
+  }
+}
+
+  min() {
+    if(this.length > 0){
+      return Math.min(...this.items)
+      }else{
+      throw new Error('EmptySortedList');
+    }
+  }
+
+  sum() {
+    
+}
+  
 
   avg() {}
 }
