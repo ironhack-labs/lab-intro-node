0cb595fbd6900b7506fb41b54f1b5e21e9431848
artweise <art2wsxweise7ujm@gmail.com>
2022-12-08 08:53:37 +0100

    iterations 3,4 completed

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ class SortedList {
   }
 
   add(item) {
+    // if (!this.items.length) this.items.push(item);
     this.items.push(item);
     this.lenght++;
     this.items.sort((a, b) => {
@@ -11,9 +12,21 @@ class SortedList {
     });
   }
 
-  get(pos) {}
+  get(pos) {
+    if (pos < 0 || pos >= this.items.length) throw new Error("OutOfBounds");
+    return this.items[pos];
+  }
 
-  max() {}
+  max() {
+    if (!this.items.length) throw new Error("EmptySortedList");
+    // with reduce() can be used to find the maximum element in a numeric array, by comparing each value
+    return this.items.reduce((a, b) => Math.max(a, b));
+    // another approach, but this should only be used for arrays with relatively few elements
+    //both spread (...) and apply() will either fail or return the wrong result if the array has too many elements,
+    // because they try to pass the array elements as function parameters.
+    //The reduce solution does not have this problem.
+    // return Math.max(...this.items);
+  }
 
   min() {}
 
