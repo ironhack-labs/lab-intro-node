a260797d5634927e04379638a8582a1103e1dc4b
Secil <secilegen@gmail.com>
2022-12-08 18:19:47 +0100

    10 passed, 4 failed

--- index.js
+++ index.js
@@ -1,17 +1,63 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = 0;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.sort();
+    this.items.push(item);
+    this.items.sort();
+    this.length++;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (this.length >= pos+1) {
+    return this.items[pos];
+  }
+    else throw new Error('OutOfBounds')
+  }
 
-  max() {}
+  max() {
+    if (this.length > 0) {
+      for(let i=0; i<this.length; i++) {
+        let maxNumber = 0;
+        if (this.items[i]> maxNumber) {
+          maxNumber = this.items[i];
+        }
+      }
+      return maxNumber;
+    }
+    else throw new Error('EmptySortedList');
+  }
 
-  min() {}
+  min() {
+    if (this.length > 0) {
+      for(let i=0; i<this.length; i++) {
+        let minNumber = 0;
+        if (this.items[i]< minNumber) {
+          minNumber = this.items[i];
+        }
+      }
+      return minNumber;
+    }
+    else throw new Error('EmptySortedList');
+  }
 
-  sum() {}
+  sum() {
+    let total = 0
+    for(let i=0; i<this.length; i++) {
+      total += this.items[i];
+    }
+    return total;
+  }
 
-  avg() {}
+  avg() {
+    if (this.length > 0) {
+    return this.items.sum()/this.length;
+  }
+    else throw new Error('EmptySortedList')
+}
 }
 
 module.exports = SortedList;
