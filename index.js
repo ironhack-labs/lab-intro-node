6d31cc87498639b9dae08029b52cf328fe710baf
Nicole <92593875+ozzleme@users.noreply.github.com>
2022-12-09 21:41:07 +0530

    iterations 1-5

--- index.js
+++ index.js
@@ -1,17 +1,52 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = []; 
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    if (this.length >= 0) {
+    this.items.push(item); // correct 
+    ++this.length
+    this.items.sort(function(a,b) {return a-b});
+    }
+    return this.items;
+  }
 
-  get(pos) {}
+  get(pos) {
+    for(let i=0; i < this.length; i++) {
+      if (pos == this.items[i]) {
+        return this.items[i]
+      } 
+      else if (pos > this.length) {
+        throw new Error('OutOfBounds');
+      }
+    }
+  }
 
-  max() {}
+  max() {
+    if (this.length === 0) {
+      throw new Error('EmptySortedList');
+    } else if (this.length >= 0) {
+    return Math.max(...this.items)
+    } 
+  }
 
-  min() {}
+  min() {
+    if (this.length === 0) {
+      throw new Error('EmptySortedList');
+    }
+    else if (this.length >= 0) {
+      return Math.min(...this.items)
+      } 
+  }
 
   sum() {}
 
   avg() {}
 }
 
+
 module.exports = SortedList;
+
+
