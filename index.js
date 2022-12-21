344d820cf3032567a60c47101a9418e8505433b1
Jesus Gonzalez <jesusglezt27@gmail.com>
2022-12-20 19:59:27 -0600

    done

--- index.js
+++ index.js
@@ -1,17 +1,40 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = []; //numeros
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item)
+    this.items.sort((a, b) => a - b);
+    this.length++
+  }
 
-  get(pos) {}
+  get(pos) {
+    if(pos < 0 || pos > this.items.length-1) throw new Error('OutOfBounds');
+    return this.items[pos]
+  }
 
-  max() {}
+  max() {
+    if(!this.items.length) throw new Error('EmptySortedList');
+    return this.items[this.length-1];
+  }
 
-  min() {}
+  min() {
+    if(!this.items.length) throw new Error('EmptySortedList');
+    return this.items[0]
+  }
 
-  sum() {}
+  sum() {
+    return this.items.reduce((accum, curr) => accum + curr, 0); 
+    if(!this.items.length) throw new Error('EmptySortedList'); 
+  }
 
-  avg() {}
+
+  avg() {
+    if(!this.length) throw new Error("valio queso")
+    return this.sum() / this.length
+  }
 }
 
 module.exports = SortedList;
