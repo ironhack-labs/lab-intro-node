b2142e65ecca45d8c690bd0082bca0b9e8683e38
Barbara Rodrigues <barbara.rodrigues@cabify.com>
2022-12-28 17:06:47 +0100

    done

--- index.js
+++ index.js
@@ -1,17 +1,59 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+      this.items = [];
+      this.length = 0;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.length++
+    this.items.sort((a, b) => a - b);
+  }
 
-  get(pos) {}
+  get(pos) {
+    const value = this.items[pos];
+    if (pos > this.items.length || pos < this.items.length){ 
+    throw new Error('OutOfBounds');
+  } else {
+    return value;
+  }
+  }
 
-  max() {}
+  max() {
+    const maxValue = this.items.reduce((a, b) => Math.max(a, b))
+    if (this.items.length === 0) {
+      throw new Error('EmptySortedList')
+    } else {
+      return maxValue;
+    }
+  }
 
-  min() {}
+  min() {
+    const minValue = Math.min(...this.items)
+    if (this.items.length === 0) {
+      throw new Error('EmptySortedList')
+    } else {
+      return minValue;
+    }
+  }
 
-  sum() {}
+  sum() {
+    return this.items.reduce((a, b) => a + b, 0)
 
-  avg() {}
-}
+   if (this.items.length === 0) {
+      return 0;
+    }
+  }
+
+  avg() {
+    if (this.items.length === 0) {
+      throw new Error('EmptySortedList')
+      }
+      else {
+        return this.sum()/this.items.length;
+      }
+  
+    }
+  }
 
 module.exports = SortedList;
