d921451a096637e223782bf83316d508a2cafb41
Daniel Olea <oleaonize@gmail.com>
2023-01-07 12:40:39 +0100

    Done

--- index.js
+++ index.js
@@ -1,17 +1,50 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = []; 
+    this.length = this.items.length;
+  }
+  
+  add(item) {
+    this.items.push(item);
+    this.length++;
+    this.items.sort((item1,item2) => item1-item2);
+  }
 
-  add(item) {}
+  get(pos) {
+    if (pos < 0 || pos > this.items.length){
+      throw new Error('OutOfBounds');
+    } else {
+      return this.items[pos]}
+  }
 
-  get(pos) {}
+  max() {
+    if (this.length === 0){
+      throw new Error('EmptySortedList');
+    } else {
+      return this.items[this.length-1];
+    }
+  }
 
-  max() {}
+  min() {
+    if (this.length === 0){
+      throw new Error('EmptySortedList');
+    } else {
+      return this.items[0];
+    }
+  }
 
-  min() {}
+  sum() {
+    return this.items.reduce((acc, item) => acc + item, 0)
+  }
 
-  sum() {}
-
-  avg() {}
+  avg() {
+    if (this.items.length === 0) {
+      throw new Error('EmptySortedList')
+      } else {
+        return this.sum()/this.items.length
+      }
+  }
 }
 
 module.exports = SortedList;
+
