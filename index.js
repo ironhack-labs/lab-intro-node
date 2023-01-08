763aef1e26d8029f6016f211f5841bd5b8f258c1
Carlos Sosa <devcaco@simpletg.com>
2023-01-08 17:09:05 -0500

    lab - solved

--- index.js
+++ index.js
@@ -1,17 +1,44 @@
 class SortedList {
-  constructor() {}
+  constructor(items = []) {
+    this.items = items;
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a, b) => a - b);
+    this.length++;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (!pos || typeof pos !== "number" || pos > this.length) throw new Error("OutOfBounds");
 
-  max() {}
+    return this.items[pos];
+  }
 
-  min() {}
+  max() {
+    if (!this.items.length) throw new Error("EmptySortedList");
 
-  sum() {}
+    return Math.max(...this.items);
+  }
 
-  avg() {}
+  min() {
+    if (!this.items.length) throw new Error("EmptySortedList");
+
+    return Math.min(...this.items);
+  }
+
+  sum() {
+    if (!this.items.length) return 0;
+
+    return this.items.reduce((prev, curr) => prev + curr, 0);
+  }
+
+  avg() {
+    if (!this.items.length) throw new Error("EmptySortedList");
+
+    return this.sum() / this.items.length;
+  }
 }
 
 module.exports = SortedList;
