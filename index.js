67da3f9a04f0b3373648323718c8d6448bc54df0
Andrea <ironhack@Ironhacks-MacBook-Pro-2.local>
2023-01-08 08:10:09 -0400

    Casi todo

--- index.js
+++ index.js
@@ -1,17 +1,62 @@
 class SortedList {
-  constructor() {}
+  constructor(items, length) {
+  this.items = [];
+  this.length = this.items.length;
+  this.sumAct = 0;
+  }
+  
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a,b)=>a-b);
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  get(pos) {
+    if (pos <= this.length) {
+      return this.items[pos];
+    } else {
+      throw new Error('OutOfBounds');
+    }
+  }
 
-  get(pos) {}
+  max() {
+    if (this.length === 0) {
+      throw new Error('EmptySortedList');
+    } else {
+      return this.items[this.items.length - 1];
+    }
+  }
 
-  max() {}
+  min() {
+    if (this.length === 0) {
+      throw new Error('EmptySortedList');
+    } else {
+      return this.items[0];
+    }
+  }
 
-  min() {}
+  sum() {
+    // hice cosas raras porque no me pasaba la última iteración, pero igual no pude sacarla
+    if (this.length === 0) {
+      return 0;
+    } else {
+      const sum = this.items.reduce((a, b) => {
+        return a + b;
+    }
+  )
+  this.sumAct = sum;
+  return sum;
+}
+}
 
-  sum() {}
-
-  avg() {}
+  avg() {
+    if (this.length === 0) {
+      throw new Error('EmptySortedList');
+    } else {
+      const average = this.sumAct / this.length;
+    return average;
+    }
+  }
 }
 
 module.exports = SortedList;
