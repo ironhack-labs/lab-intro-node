e9cd16473be889e0a36f56bc80cd640a59c11335
hilkizun <hilkizun@gmail.com>
2023-01-09 16:16:42 +0100

    try-done-v2

--- index.js
+++ index.js
@@ -1,17 +1,51 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.length++
+    this.items.sort((a, b) => a - b);
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (this.length - 1 >= pos) {
+      return this.items[pos];
+    }
+    throw new Error("OutOfBounds");
+  }
 
-  max() {}
+  max() {
+    const maxValue = this.items.reduce((a, b) => Math.max(a, b))
+    if (this.items.length === 0) {
+      throw new Error('EmptySortedList')
+    } else {
+      return maxValue;
+    }
+  }
 
-  min() {}
+  min() {
+    const minValue = Math.min(...this.items)
+    if (this.items.length === 0) {
+      throw new Error('EmptySortedList')
+    } else {
+      return minValue;
+    }
+  }
 
-  sum() {}
+  sum() {
+    return this.items.reduce((acc, item) => acc + item, 0)
+  }
 
-  avg() {}
+  avg() {
+    if (this.items.length === 0) {
+      throw new Error('EmptySortedList')
+      } else {
+        return this.sum()/this.items.length
+      }
+  }
 }
 
 module.exports = SortedList;
