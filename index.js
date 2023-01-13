1787ca045a890b573118e5eed878a7370eaea7c0
Erin <erica@Erin.local>
2023-01-13 17:39:31 +0100

    Done

--- index.js
+++ index.js
@@ -1,17 +1,57 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = 0;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((el1, el2) => {
+      return el1 - el2;
+    });
+    this.length++;
+    // console.log(this.items);
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos > this.items.length - 1) {
+      throw new Error("OutOfBounds");
+    }
+    return this.items[pos];
+  }
 
-  max() {}
+  max() {
+    if (this.length <= 0) {
+      throw new Error("EmptySortedList");
+    }
 
-  min() {}
+    return Math.max(...this.items);
+  }
 
-  sum() {}
+  min() {
+    if (this.length === 0) {
+      throw new Error("EmptySortedList");
+    }
+    return Math.min(...this.items);
+  }
 
-  avg() {}
+  sum() {
+    if (this.length === 0) {
+      return 0;
+    }
+    return this.items.reduce((el, acc) => {
+      return el + acc;
+    }, 0);
+  }
+
+  avg() {
+    if (this.length === 0) {
+      throw new Error("EmptySortedList");
+    }
+    return this.items.reduce((el, acc) => {
+      return el + acc / this.length;
+    }, 0);
+  }
 }
 
 module.exports = SortedList;
