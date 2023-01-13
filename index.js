e5ddd533cd9eac5e4be27f8c3662f5ed8dadff47
leandropereyradev <leandro-pereyra@hotmail.es>
2023-01-13 21:04:05 +0100

    Done

--- index.js
+++ index.js
@@ -1,17 +1,44 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a, b) => a - b);
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos < this.items.length) {
+      return this.items[pos];
+    } else {
+      throw new Error("OutOfBounds");
+    }
+  }
 
-  max() {}
+  max() {
+    if (this.items.length) return this.items.sort((a, b) => b - a)[0];
 
-  min() {}
+    throw new Error("EmptySortedList");
+  }
 
-  sum() {}
+  min() {
+    if (this.items.length) return this.items.sort((a, b) => a - b)[0];
 
-  avg() {}
+    throw new Error("EmptySortedList");
+  }
+
+  sum() {
+    return this.items.reduce((a, b) => a + b, 0);
+  }
+
+  avg() {
+    if (this.items.length) return this.sum() / this.items.length;
+
+    throw new Error("EmptySortedList");
+  }
 }
 
 module.exports = SortedList;
