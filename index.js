d0ee743403f8a3a2726d0f4bb4d0ee44296f4b76
javideas <infojavideas@gmail.com>
2023-01-13 10:24:25 +0200

    finished with bonus

--- index.js
+++ index.js
@@ -1,17 +1,60 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = 0;
 
-  add(item) {}
+  }
 
-  get(pos) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a, b) => a - b);
+    this.length = this.items.length;
+  }
 
-  max() {}
+  get(pos) {
+    if (Number.isInteger(pos) && pos < this.items.length && pos >= 0 ) {
+      return this.items[pos];
+    } else { 
+      throw new Error('OutOfBounds');
+    }
+  }
 
-  min() {}
+  max() {
+    if (this.items.length === 0) {
+      throw new Error('EmptySortedList');
+    } else {
+      this.items.sort(( a, b) => a - b);
+      return this.items[this.items.length - 1];
+    }
+  }
 
-  sum() {}
+  min() {
+    if (this.items.length === 0) {
+      throw new Error('EmptySortedList');
+    } else {
+      this.items.sort(( a, b) => a - b);
+      return this.items[0];
+    }
+  }
 
-  avg() {}
+  sum() {
+    if (this.items.length === 0) {
+      return 0;
+    } else {
+      this.items.sort(( a, b) => a - b);
+      return this.items.reduce((acc, cur) => acc + cur);
+    }
+  }
+
+  avg() {
+    if (this.items.length === 0) {
+      throw new Error('EmptySortedList');
+    } else {
+      let sum = this.items.reduce((acc, cur) => acc + cur);
+      return sum/this.items.length;
+    }
+  }
 }
 
 module.exports = SortedList;
+//npm test
