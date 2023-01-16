1d04021597853cfaea0208e3d311c75d24e40a0c
Anna Soriano <annasorianotoledo@gmail.com>
2023-01-16 18:01:51 +0100

    [WD PT NOV ES] - {Anna Soriano Toledo}

--- index.js
+++ index.js
@@ -1,17 +1,56 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((i1, i2) => i1 - i2);
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos < this.items.length) {
+      return this.items[pos]
+    } else if (!this.items.includes(pos)) {
+      throw new Error('OutOfBounds');
+    }
+  }
 
-  max() {}
+  max() {
+    if (this.length > 0) {
+      return this.items[this.items.length - 1];
+    } else if (this.length === 0) {
+      throw new Error('EmptySortedList');
+    }
+  }
 
-  min() {}
+  min() {
+    if (this.length > 0) {
+      return this.items[0];
+    } else if (this.length === 0) {
+      throw new Error('SortedList');
+    }
+  }
 
-  sum() {}
+  sum() {
+    let total = 0;
+    for (let i = 0; i < this.items.length; i++) {
+      total += this.items[i];
+    }
+    return total;
+  }
 
-  avg() {}
+  avg() {
+    let totalSum = this.sum(this.items);
+    let average = totalSum / this.items.length;
+    if (this.length > 0) {
+    return average;
+    } else if (this.length === 0) {
+      throw new Error("EmptySortedList");
+    }
+  }
 }
 
 module.exports = SortedList;
