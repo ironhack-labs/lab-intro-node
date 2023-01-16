f6ab84b64dac9ff66e2229ad1561cc7706cdccf1
Javier <javiergonzalez74@hotmail.com>
2023-01-16 18:55:36 +0100

    firts commit

--- index.js
+++ index.js
@@ -1,13 +1,44 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = []
+    this.length = this.items.length
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items = this.items.sort((a, b) => a - b);
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos <= this.length) {
+      return this.items[pos];
+      
+    } else if (pos >= this.length) {
+        throw new Error('OutofBounds');
+      }
+    
+  }
 
-  max() {}
+  max() {
+    if (this.length === 0) {
+      throw new Error('EmptySortedList');
 
-  min() {}
+    } else if (this.length != 0) {
+        return this.items.reduce((max, num) => (max > num)? max:num);
+
+      }
+    
+  }
+
+  min() {
+    if (this.length === 0) {
+      throw new Error('EmptySortedList');
+
+    } else if (this.length != 0) {
+        return this.items.reduce((min, num) => (min < num)? min:num);
+
+      }
+  }
 
   sum() {}
 
