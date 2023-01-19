217169c71523b17f59e591f345dc7d8f7691dce9
sergiocataladuran <sergio.catala.duran@gmail.com>
2023-01-19 18:36:12 +0100

    Completado.

--- index.js
+++ index.js
@@ -1,17 +1,55 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = []
+    this.length = this.items.length
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item)
+    this.items.sort(function(a, b){return a-b});
+    this.length = this.items.length
+  }
 
-  get(pos) {}
+  get(i) {
+    let result = this.items[i]
+    if (result == undefined) {
+      throw new Error('OutOfBounds')
+    } else {
+      return result
+    }
+  }
 
-  max() {}
+  max() {
+    if (this.items.length != 0) {
+      return Math.max.apply(Math, this.items)
+    } else {
+      throw new Error('EmptySortedList')
+    }
+  }
 
-  min() {}
+  min() {
+    if (this.items.length != 0) {
+      return Math.min.apply(Math, this.items)
+    } else {
+      throw new Error('EmptySortedList')
+    }
+  }
 
-  sum() {}
+  sum() {
+    if (this.items.length != 0) {
+      return this.items.reduce((acc, cVal) => acc + cVal)
+    } else {
+      return 0
+    }
+  }
 
-  avg() {}
+  avg() {
+    if (this.items.length != 0) {
+      return this.sum()/this.items.length
+    } else {
+      throw new Error('EmptySortedList')
+    }
+  }
 }
 
 module.exports = SortedList;
