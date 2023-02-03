3886dd0e0df8c35657d0e578d2b991a339d128bf
Noa Leon <n.leon.carrasco@gmail.com>
2023-02-03 08:50:33 +0100

    done

--- index.js
+++ index.js
@@ -1,17 +1,54 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = []
+    this.length = 0
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item)
+    this.items.sort((a, b) => a - b)
+    this.length = this.items.length
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos < this.items.length) {
+      return this.items[pos]
+    } else throw new Error('OutOfBounds')
+  }
 
-  max() {}
+  max() {
+    if (this.length === 0) {
+      throw new Error('EmptySortedList')
+    } else return this.items[this.length - 1]
+  }
 
-  min() {}
+  min() {
+    if (this.length === 0) {
+      throw new Error('EmptySortedList')
+    } else return this.items[0]
+  }
 
-  sum() {}
+  sum() {
+    let sum = 0
+    this.items.forEach(item => {
+      sum += item
+    })
 
-  avg() {}
+    if (this.length === 0) {
+      return 0
+    } else return sum
+  }
+
+  avg() {
+    let sum = 0
+    this.items.forEach(item => {
+      sum += item
+    })
+
+    if (this.length === 0) {
+      throw new Error('EmptySortedList')
+    } else return (sum / this.length)
+  }
 }
 
 module.exports = SortedList;
