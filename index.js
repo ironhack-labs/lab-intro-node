fdc2c9c6647281049790ddcd65ed7624477b6bd5
ApollineStev <apollinecanard@gmail.com>
2023-02-03 10:52:41 +0100

    almost done

--- index.js
+++ index.js
@@ -1,17 +1,56 @@
 class SortedList {
-  constructor() {}
+  constructor(items = []) {
+    this.items = items
+    this.length = this.items.length
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item)
+    this.items.sort(function(a,b){return a-b})
+    return this.items, this.length
+  }
 
-  get(pos) {}
+  get(pos) {
+    if(this.items.indexOf(pos) != -1){
+      return this.items.indexOf(pos)
+    }else{
+      throw new Error('OutOfBounds');
+    }
+    
+  }
 
-  max() {}
+  max() {
+    if(this.items.length !== 0){
+       return Math.max(...this.items)
+    }else if (this.length == 0){
+     throw new Error('EmptySortedList');
+    }
+    
+  }
 
-  min() {}
+  min() {
+    if(this.items.length !== 0){
+      return Math.min(...this.items)
+   }else if (this.length == 0){
+    throw new Error('EmptySortedList');
+   }
+  }
 
-  sum() {}
+  sum() {
+    const initialValue = 0;
+    const sumWithInitial = this.items.reduce(
+      (accumulator, currentValue) => accumulator + currentValue,initialValue);
+      return sumWithInitial
+  }
 
-  avg() {}
+  avg() {
+    if(this.items.length !== 0){
+      let average = this.items((a,b) => a+ b, 0)
+     return average / this.length
+   }else if (this.length == 0){
+    throw new Error('EmptySortedList');
+   }
+  }
 }
 
 module.exports = SortedList;
