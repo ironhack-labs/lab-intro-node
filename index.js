61318694f5f52f20bf0987a99dad2747a7cce298
Wilkins Morales <wilkinscom@gmail.com>
2023-02-03 17:42:19 +0200

    Iteration 4: max()

--- index.js
+++ index.js
@@ -22,7 +22,13 @@ class SortedList {
     }
   }
 
-  max() {}
+  max() {
+    if (this.items.length == 0){
+      throw new Error('EmptySortedList');
+    } else {
+      return Math.max(...this.items);
+    }
+  }
 
   min() {}
 
