b9343d3c001989e7bdbb422580fe2912fd772c0d
Wilkins Morales <wilkinscom@gmail.com>
2023-02-03 17:43:16 +0200

    Iteration 5: min()

--- index.js
+++ index.js
@@ -30,7 +30,13 @@ class SortedList {
     }
   }
 
-  min() {}
+  min() {
+    if (this.items.length == 0){
+      throw new Error('EmptySortedList');
+    } else {
+      return Math.min(...this.items);
+    }
+  }
 
   sum() {}
 
