589c2f7f4045b1c461792ef70954c8095b2d25a1
LKTOKB <theodiamanttt@gmail.com>
2023-02-07 07:17:56 +0200

    Done

--- index.js
+++ index.js
@@ -1,17 +1,57 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = []
+    this.length = this.items.length
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a, b) => a - b);
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if(this.items[pos] === undefined){
+      throw new Error('OutOfBounds');
+    }
+   return this.items[pos]
+  }
 
-  max() {}
+  max() {
+    if(this.length === 0){
+      throw new Error('EmptySortedList');
+    }
 
-  min() {}
+      return Math.max(...this.items)
+   
+  }
 
-  sum() {}
+  min() {
+    if(this.length === 0){
+      throw new Error('EmptySortedList');
+    }else {
+     return Math.min(...this.items)
+    }
+  }
+ 
+  sum() {
+      if(this.length === 0){
+        return 0
+      }
+    let sum = 0
+    for (let i = 0; i < this.length; i++){
+      sum += this.items[i]
+    }
+    return sum
+  }
 
-  avg() {}
+  avg() {
+    if(this.length === 0){
+      throw new Error('EmptySortedList');
+    }
+    const average = this.items.reduce((a, b) => a + b, 0) / this.length
+    return average
+  }
 }
 
 module.exports = SortedList;
