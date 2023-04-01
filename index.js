7b607df4dd32df27c17d00ebdd46011253289d66
AZRGUI-MOHAMED <azrgui.m@gmail.com>
2023-04-01 10:54:38 +0200

    iterration2

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 class SortedList {
   constructor() {
     this.items = [];
-    this.length = this.items.length;
+    this.length = 0;
   }
 
   add(item) {
-
+    let i = 0
+    while(i<this.items.length && this.items[i]<item){
+      i++;
+    }
+    this.items.splice(i,0, item);
+    this.length = this.items.length;
   }
 
   get(pos) {}
