50de4767f4c47cc921afeb9f1afd2201ff9ac0d5
Pepe Alacid <ppalacid@gmail.com>
2023-04-01 22:34:47 +0200

    solo funcionan los errores

--- index.js
+++ index.js
@@ -1,17 +1,67 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    if (item < this.items[0]) {
+      this.items.unshift(item);
+    } else if (item > this.items[this.length - 1]) {
+      this.items.push(item);
+    } else {
+      for (let i = 0; i <= this.length; i++) {
+        if (item >= this.items[i] && item <= this.items[i + 1]) {
+          this.items.slice(i + 1, 0, item);
+        }
+      }
+    }
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos + 1 <= this.length) {
+      return this.items[pos];
+    } else throw new Error("EmptySortedList");
+  }
 
-  max() {}
+  max() {
+    let maxNum = 0;
+    if (this.length !== 0) {
+      this.items.forEach((num) => {
+        if (num > maxNum) {
+          maxNum = num;
+        }
+      });
+      return maxNum;
+    } else throw new Error("EmptySortedList");
+  }
 
-  min() {}
+  min() {
+    let minNum = this.length[0];
+    if (this.length !== 0) {
+      this.items.forEach((num) => {
+        if (num < minNum) {
+          minNum = num;
+        }
+      });
+      return minNum;
+    } else throw new Error("EmptySortedList");
+  }
 
-  sum() {}
+  sum() {
+    let sum = 0;
+    this.items.forEach((num) => {
+      sum += num;
+    });
+    return sum;
+  }
 
-  avg() {}
+  avg() {
+    if (this.length !== 0) {
+      let result = this.sum() / this.length;
+      return result;
+    } else throw new Error("EmptySortedList");
+  }
 }
 
 module.exports = SortedList;
