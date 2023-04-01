79e401c818e47c6965da9514ae6a5570c441a9a5
kobetoto <106164290+kobetoto@users.noreply.github.com>
2023-04-01 11:04:37 +0200

    iteration2

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
 
-  add(item) {}
+  }
+
+  add(item) {
+    this.items.push(item)
+
+    let sortedItems = this.items.sort(function(a,b) {
+      if (a<b) return -1
+      if (a>b) return 1
+      if (a=b) return 0
+      
+      return sortedItems
+    })
+
+    this.items.length = sortedItems.length
+  }
 
   get(pos) {}
 
