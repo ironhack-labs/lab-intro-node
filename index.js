e84d006620579b3d14428a3c862833c8bf14e84a
José Antonio Escudero García <jaeg90@gmail.com>
2023-04-01 18:40:04 +0200

    Lab done

--- index.js
+++ index.js
@@ -1,17 +1,66 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a, b) => a - b);
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos < this.length) {
+      return this.items[pos];
+    } else {
+      throw new Error("OutOfBounds");
+    }
+  }
 
-  max() {}
+  max() {
+    if (this.length > 0) {
+      let highestValue = this.items[0];
+      this.items.forEach((item) => {
+        if (highestValue < item) {
+          highestValue = item;
+        }
+      });
+      return highestValue;
+    } else {
+      throw new Error("EmptySortedList");
+    }
+  }
 
-  min() {}
+  min() {
+    if (this.length > 0) {
+      let lowestValue = this.items[0];
+      this.items.forEach((item) => {
+        if (lowestValue > item) {
+          lowestValue = item;
+        }
+      });
+      return lowestValue;
+    } else {
+      throw new Error("EmptySortedList");
+    }
+  }
 
-  sum() {}
+  sum() {
+    let result = 0;
+    this.items.forEach((item) => {
+      result += item;
+    });
+    return result;
+  }
 
-  avg() {}
+  avg() {
+    if (this.length > 0) {
+      return this.sum() / this.length;
+    } else {
+      throw new Error("EmptySortedList");
+    }
+  }
 }
 
 module.exports = SortedList;
