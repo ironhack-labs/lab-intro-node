91484759561d7f6f6f844e42c6ec028ea77b55b5
Nicolas_M <nicolas.meot@live.fr>
2023-04-01 11:07:01 +0200

    3rd and 4th iteration

--- index.js
+++ index.js
@@ -13,9 +13,16 @@ class SortedList {
     this.length = this.items.length;
   }
 
-  get(pos) {}
+  get(pos) {
+    if(this.items.length<pos){throw new Error('OutOfBounds')}
+    else {return this.items[pos]}
+  }
 
-  max() {}
+  max() {
+    if(this.items.length===0){throw new Error('EmptySortedList')}
+    else{return Math.max(...this.items)}
+    //this.items[this.items.length-1]
+  }
 
   min() {}
 
