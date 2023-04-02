b8ffb08e02d00d48dd2d35632fabdebe56150b59
Jon <jon_ander_23@protonmail.com>
2023-04-02 10:49:59 +0200

    Solved lab (with corrections)

--- index.js
+++ index.js
@@ -5,34 +5,60 @@ class SortedList {
   }
 
   add(item) {
-    this.items.push(item);
+    let start = 0;
+    let end = this.items.length;
+
+    if (item < this.items[0]) {
+      this.items.unshift(item);
+      return;
+    }
+
+    else if (this.items[this.length-1]) {
+      this.items.push(item);
+      return;
+    }
+
+    while (end - start > 1) {
+      let midpoint = Math.ceil((start + end) / 2 );
+      if (this.items[midpoint] < item) {
+        start = midpoint;
+      }
+      else {
+        end = midpoint;
+      }
+    }
+
+    this.items.splice(end, 0, item);
+    this.length = this.items.length;
   }
 
   get(pos) {
-    if (pos > this.items.length) {
+    if (pos > this.items.length - 1) {
       throw new Error('OutOfBounds');
     }
 
-    for (let i=0, i < this.items.length; i++) {
-      if (pos == i) {
-        return items[i]
+    return this.items[pos];
+
+    // for (let i=0, i < this.items.length; i++) {
+    //   if (pos == i) {
+    //     return items[i]
       }
-    }
-  }
 
   max() {
-    let max = 0;
+  //   let max = 0;
 
     if[items.length === 0] {
       throw new Error('EmptySortedList');
     }
 
-    for (let i = 0; i < this.items.length; i++) {
-      if (items[i] > max) {
-        max = items[i];
-      }
-      return max;
-    }
+    return Math.max(...this.items);
+
+    // for (let i = 0; i < this.items.length; i++) {
+    //   if (items[i] > max) {
+    //     max = items[i];
+    //   }
+    //   return max;
+    // }
   }
 
   min() {
@@ -60,6 +86,11 @@ class SortedList {
   }
 
   avg() {
+
+    if (this.items.length === 0) {
+      throw new Error ('EmptySortedList');
+    }
+
     let total = sum(items);
     let avg = total/this.items.length;
     return avg;
