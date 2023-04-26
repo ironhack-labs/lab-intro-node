21f3ed53bfdbed647d3048670866556f5f9aea51
Wiebke Lueneburg <git@fraulueneburg.de>
2023-04-26 22:23:06 +0200

    solved lab (just for fun/practice, don't even worry about it, Shaun ;-)

--- index.js
+++ index.js
@@ -1,17 +1,55 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.unshift(item);
+    this.items.sort((a, b) => a - b);
+    this.length = this.items.length;
+    return this.items;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (this.items[pos] !== undefined) {
+      return this.items[pos];
+     } else {
+      throw new Error ('OutOfBounds');
+     }
+  }
 
-  max() {}
+  max() {
+    if (this.length > 0) {
+      return Math.max.apply(null, this.items);
+    } else {
+      throw new Error ('EmptySortedList');
+    }
+  }
 
-  min() {}
+  min() {
+    if (this.length > 0) {
+      return Math.min.apply(null, this.items);
+    } else {
+      throw new Error ('EmptySortedList');
+    }
+  }
 
-  sum() {}
+  sum() {
+    if (this.length > 0) {
+      return this.items.reduce((accumulator, item) => accumulator + item);
+    } else {
+      return 0;
+    }
+  }
 
-  avg() {}
+  avg() {
+    if (this.length > 0) {
+      return this.sum() / this.length;
+    } else {
+      throw new Error('EmptySortedList');
+    }
+  }
 }
 
 module.exports = SortedList;
