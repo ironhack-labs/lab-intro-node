be2f7db20be3e4127c7fef200fa17a51d8d534ac
NabilAslam <nabil.aslam26@gmail.com>
2023-04-27 09:49:22 +0200

    lab done

--- index.js
+++ index.js
@@ -1,17 +1,63 @@
 class SortedList {
-  constructor() {}
+  constructor(items, length) {
+    this.items = [];
+    this.length = 0;
+  } 
 
-  add(item) {}
+  add(item) {
 
-  get(pos) {}
+    this.items.push(item);
+    this.items.sort((a, b) => a - b);
+    this.length = this.items.length;
 
-  max() {}
+  };
 
-  min() {}
+  get(pos) {
+      if ((pos) > this.items.length) {
+      throw new Error('OutOfBounds');
+    } else  {
+      return this.items.indexOf(pos) 
+    }
+  }
 
-  sum() {}
+  max() {
 
-  avg() {}
+    if (!this.items.length) {
+    throw new Error('EmptySortedList');
+    } else {
+      return (Math.max(...this.items));
+    }
+  };
+
+  min() {
+
+    if (!this.items.length) {
+      throw new Error('EmptySortedList');
+      } else {
+        return (Math.min(...this.items));
+      }
+  };
+
+  sum() {
+
+    if (!this.items.length) {
+      return 0
+    } else {
+      return this.items.reduce((a, b) => a + b)
+    }
+  }
+
+  avg(sum) {
+    
+    // !array.length is the same as writing array.length === 0
+    if (!this.items.length) throw new Error('EmptySortedList');
+
+    sum = 0; 
+    this.items.forEach((element) => {
+    sum += element;
+    });
+    return sum / this.items.length;
+}
 }
 
 module.exports = SortedList;
