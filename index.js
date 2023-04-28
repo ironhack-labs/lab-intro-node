eee5bf92363b84fd8bf33b665eb46dd8484c29cf
FRITZ IRONHACK <weninger.fritz@gmail.com>
2023-04-28 21:14:09 +0200

    done

--- index.js
+++ index.js
@@ -1,17 +1,50 @@
 class SortedList {
-  constructor() {}
+  constructor() {
 
-  add(item) {}
+    this.items = []
+    this.length = this.items.length
+  }
 
-  get(pos) {}
+  add(item) {
+    this.items.push(item)
+    this.items.sort((compare)=>{return a - b})
+   this.length = this.items.length
+  }
 
-  max() {}
+  get(pos) {
+    const position = this.items[pos]
+    if (!this.length) {
+      throw new Error('OutOfBounds')
+    }
 
-  min() {}
+    return  position
+  }
+
+
+  max() {
+    if(this.items.length === 0){
+      throw new Error('EmptySortedList');
+    }
+    const lastNumber = this.items.length -1 
+    return  this.items[lastNumber]
+  }
+
+  min() {
+    if(this.items.length === 0){
+      throw new Error('EmptySortedList');
+    }
+    return this.items[0]
+
+  }
 
   sum() {}
 
   avg() {}
 }
+
+const list = new SortedList ("list1")
+
+
+
 
 module.exports = SortedList;
