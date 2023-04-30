6af429b1cca5e6cb04f0e7273beb68c0b16e7e12
SalvadorBellver <salvador@salvadorbellver.com>
2023-04-30 14:33:51 +0200

    Done!

--- index.js
+++ index.js
@@ -1,17 +1,46 @@
 class SortedList {
-  constructor() {}
+    constructor() {
+        this.items = [];
+        this.length = this.items.length;
+    }
 
-  add(item) {}
+    add(item) {
+        this.items.push(item);
+        this.items.sort((a, b) => a - b);
+        this.length = this.items.length;
+    }
 
-  get(pos) {}
+    get(pos) {
+        if (pos >= this.length || pos < 0) {
+            throw new Error('OutOfBounds');
+        }
+        return this.items[pos];
+    }
 
-  max() {}
+    max() {
+        if (this.length === 0) {
+            throw new Error('EmptySortedList');
+        }
+        return this.items[this.length - 1];
+    }
 
-  min() {}
+    min() {
+        if (this.length === 0) {
+            throw new Error('EmptySortedList');
+        }
+        return this.items[0];
+    }
 
-  sum() {}
+    sum() {
+        return this.items.reduce((acc, val) => acc + val, 0);
+    }
 
-  avg() {}
+    avg() {
+        if (this.length === 0) {
+            throw new Error('EmptySortedList');
+        }
+        return this.sum() / this.length;
+    }
 }
 
 module.exports = SortedList;
