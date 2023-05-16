e128d4f9b122524b242d090caa9cc960d893c37d
Bernard Cardoso <be1.0@hotmail.com>
2023-05-16 23:45:37 +0100

    Done!

--- index.js
+++ index.js
@@ -7,14 +7,29 @@ class SortedList {
   add(item) {
       this.items.push(item);
       this.length++;
-      return this.items.sort((a, b) => a - b);
+      return this.items.sort((a, b) => a - b); // ascending order using sort
   }
 
-  get(pos) {}
+  get(pos) {
+    if(pos >= this.items.length){
+      throw new Error('OutOfBounds');
+    }
+    return this.items[pos];   // to get the position of this item
+  }
 
-  max() {}
+  max() {
+    if(this.length === 0){
+      throw new Error('EmptySortedList');
+    } 
+    return Math.max(...this.items);
+  }
 
-  min() {}
+  min() {
+    if(this.length === 0){
+      throw new Error('EmptySortedList');
+    } 
+    return Math.min(...this.items);
+  }
 
   sum() {}
 
