420a964ddd25dedeb0f7ea69deda2f3d3bd1737e
Matt Krebs <mattekrebs@gmail.com>
2023-05-17 19:27:05 +0200

    Matt Krebs

--- index.js
+++ index.js
@@ -1,17 +1,59 @@
 class SortedList {
-  constructor() {}
+  constructor(items, length) {
+    this.items = [],
+    this.length = this.items.length
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a,b) => a-b);
+    this.length = this.items.length
+  }
 
-  get(pos) {}
+  get(pos) {
+    if(pos[i]> this.length){
+      throw new Error('OutOfBounds');
+    } else {
+      return this.items[i];
+    }
+  }
 
-  max() {}
+  max() {
+    if(this.length == 0 ){
+      throw new Error('EmptySortedList');
+    } else {
+      return Math.max(this.items);
+    }
+  }
 
-  min() {}
+  min() {
+    if(this.length == 0 ){
+      throw new Error('EmptySortedList');
+    } else {
+      return this.items.min();
+    }
+  }
 
-  sum() {}
+  sum() {
+    if(this.length == 0 ){
+      return 0;
+    } else {
+      return this.items.reduce((a, b) => a + b);
+    }
+  }
 
-  avg() {}
+  avg() {
+    if(this.length == 0){
+      throw new Error('EmptySortedList');      
+    } else {    
+      function average() {
+        this.items.reduce((a, b) => (a + b)) / this.items.length;
+    }
+    return average;
+  }
+  }
 }
+
+
 
 module.exports = SortedList;
