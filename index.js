15c3778ae2e9de114cc91aec4ce8d018bed9677b
Carlos Baptista <baptista.carlos.ab@gmail.com>
2023-05-18 18:24:06 +0200

    V1.0

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = 0;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a, b) => a - b);
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos >= 0 && pos < this.items.length) {
+      return this.items[pos];
+    } else {
+      throw new Error('OutOfBounds');      
+    };
+  }
 
   max() {}
 
