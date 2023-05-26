514f73a685cc4a34736ec7ccc823e9b364e1fec3
churrundo <churrundo@gmail.com>
2023-05-26 11:44:18 -0600

    set up constructor and add() method

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 class SortedList {
-  constructor() {}
+  constructor(items, length) {
+    this.items = [];
+    this.length = 0;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.length++;
+    this.items.sort((a, b) => a - b);
+  }
 
   get(pos) {}
 
