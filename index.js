c39748aa7bf9fe72d810d5cd10277f9d33362705
Vialoha <vi62469@gmail.com>
2023-05-27 14:59:40 +0100

    done

--- index.js
+++ index.js
@@ -1,17 +1,60 @@
 class SortedList {
-  constructor() {}
+  constructor(items, length) {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+      this.items.push(item);
+      this.length += 1;
+      return this.items.sort(function(item,b) {return item-b});
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos > this.items.length){
+      throw new Error('OutOfBounds');
+    }
+    else{
+      return this.items[pos];
+    }
+  }
 
-  max() {}
+  max() {
+    if (this.items.length < 1){
+      throw new Error('EmptySortedList');
+    }
+    else {
+      return Math.max.apply(Math, this.items);
+    }
+  }
 
-  min() {}
+  min() {
+    if (this.items.length < 1){
+      throw new Error('EmptySortedList');
+    }
+    else {
+      return Math.min.apply(Math, this.items);
+    }
+  }
 
-  sum() {}
+  sum() {
+    let sum = 0;
 
-  avg() {}
+    this.items.forEach(item => {
+      sum += item;
+    });
+    return sum;
+  }
+
+  avg() {
+    if (this.items.length > 0){
+      let avg = this.sum()/this.items.length;
+      return avg;
+    }
+    else {
+      throw new Error ('EmptySortedList');
+    }
+  }
 }
 
 module.exports = SortedList;
