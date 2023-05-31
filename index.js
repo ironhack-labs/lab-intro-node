e0e7fe225b81aa840f1e04bd68b1bb663fe38766
Victor Tovar <manuelxdvic@gmail.com>
2023-05-31 14:12:08 -0600

    lab done

--- index.js
+++ index.js
@@ -1,17 +1,39 @@
 class SortedList {
-  constructor() {}
+  constructor(items, length) {
+    this.items = [],
+    this.length = this.items.length
+  }
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a,b) => a-b);
+    this.length = this.items.length
+  }
+  get(pos) {
+    if(pos[i]> this.length){
+      throw new Error('OutOfBounds');
+    } else {
+      return this.items[i];
+      return this.items(pos[i]);
+    }
+  }
 
-  add(item) {}
+  max() {
+    if(this.length == 0 ){
+      throw new Error('EmptySortedList');
+    } else {
+      return Math.max(this.items);
+      return Math.max(...this.items);
+    }
+  }
 
-  get(pos) {}
-
-  max() {}
-
-  min() {}
-
-  sum() {}
-
-  avg() {}
-}
+  min() {
+    if(this.length == 0 ){
+      throw new Error('EmptySortedList');
+    } else {
+      return this.items.min();
+    } 
+      return Math.min(...this.items);
+    }
+  }
 
 module.exports = SortedList;
