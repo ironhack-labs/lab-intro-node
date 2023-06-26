356cdffe07b6de55e9f34ae51a4191cc934270e9
Lu <jlu.fernandez90@gmail.com>
2023-06-26 22:43:17 +0200

    primeras iteraciones hechas

--- index.js
+++ index.js
@@ -1,17 +1,31 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = []
+    this.length = this.items.length
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a, b) => a - b);
+    this.length = this.items.length
+  }
 
-  get(pos) {}
+  get(pos) { 
+    if (pos <= this.length){
+      return this.items[pos];
+    }else {
+      throw new Error("OutOfBounds");
+    }
 
-  max() {}
+  }
 
-  min() {}
+  max() { }
 
-  sum() {}
+  min() { }
 
-  avg() {}
+  sum() { }
+
+  avg() { }
 }
 
 module.exports = SortedList;
