d891a3e07e4a4f5ff0d2dd953439fa672d8e6e13
0000nebu <neboa.fresco@gmail.com>
2023-07-14 12:37:56 +0200

    done against 2 points

--- index.js
+++ index.js
@@ -1,17 +1,68 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+    
+  }
 
-  add(item) {}
+  add(item) {
+   
 
-  get(pos) {}
+    this.items.push(item);
+    
+    this.items.sort(function(a, b) {
+      return a - b;
+    });
 
-  max() {}
+    this.length = this.items.length;
+     
+  }
 
-  min() {}
+  get(pos) {
+    if (pos < 0 || pos > this.length){
+      throw new Error("_OutOfBounds_");
+    }
+    return this.items[index];
 
-  sum() {}
+  }
 
-  avg() {}
+  max() {
+    if (this.length === 0){
+      throw new Error("EmptySortedList")
+    } else {
+      
+      return Math.max(...this.items)
+    }
+
+  }
+
+  min() {
+    if (this.length === 0){
+      throw new Error("EmptySortedList")
+    } else {
+      
+      return Math.min(...this.items)
+    }
+
+  }
+
+  sum() {
+    if (this.length === 0){
+      return 0
+    } else { 
+      let suma = 0;
+      this.items.forEach(function(item){suma += item;});
+      return suma
+    }
+  }
+
+  avg() {
+    if (this.length === 0){
+      throw new Error("EmptySortedList");
+    } else {
+      
+    }
+  }
 }
 
 module.exports = SortedList;
