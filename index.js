1f75c7535d0d1e06df45dc20057df767ad780b7b
LuieR-RMT-WDPT-MAY2023 <rhluisenrique@gmail.com>
2023-07-14 20:35:41 -0400

    Done Lab Intro Node

--- index.js
+++ index.js
@@ -1,17 +1,57 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    
+    this.items.push(item);
 
-  get(pos) {}
+    this.items.sort((a,b) => {
+      if( a < b ) return -1;
+      if( a > b ) return 1;
+      if( a === b ) return 0;
+    })
 
-  max() {}
+    this.length = this.items.length
 
-  min() {}
+  }
 
-  sum() {}
+  get(pos) {
 
-  avg() {}
+    if(this.items[pos] === undefined) throw new Error("OutOfBounds")
+
+    return this.items[pos]
+      
+  }
+
+  max() {
+
+    if(this.items.length == 0) throw new Error("EmptySortedList")
+
+    return this.items[this.items.length -1]
+  }
+
+  min() {
+
+    if(this.items.length == 0) throw new Error("EmptySortedList");
+
+    return this.items[0]
+
+  }
+
+  sum() {
+
+    return this.items.reduce(( a, b ) => a + b, 0)
+  }
+
+  avg() {
+
+    if(this.items.length == 0) throw new Error("EmptySortedList");
+    return this.sum() / this.items.length
+
+  }
 }
 
 module.exports = SortedList;
