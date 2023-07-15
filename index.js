e6c67a1fb9e306972ab562898b372eb4b1a9503f
Andrews <andreeews2k12@gmail.com>
2023-07-15 02:19:35 +0200

    feat: all iterations, lab finish

--- index.js
+++ index.js
@@ -22,18 +22,52 @@ class SortedList {
     }
 
     this.length = this.items.length;
-}
+  }
 
+  get(pos) {
+    if (pos > this.items.length - 1) {
+      throw new Error("OutOfBounds");
+    } else {
+      return this.items[pos];
+    }
+  }
 
-  get(pos) {}
+  max() {
+    if (this.items.length === 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      return Math.max(...this.items);
+    }
+  }
 
-  max() {}
+  min() {
+    if (this.items.length === 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      return Math.min(...this.items);
+    }
+  }
 
-  min() {}
+  sum() {
+    let sumResult = 0;
+    if (this.items.lenth === 0) {
+      return (sumResult = 0);
+    } else {
+      return this.items.reduce((acc, current) => (acc += current), sumResult);
+    }
+  }
 
-  sum() {}
+  avg() {
+    let avg = 0;
+    let sum = 0;
 
-  avg() {}
+    if (this.items.length === 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      sum = this.items.reduce((acc, current) => (acc += current), sum);
+      return (avg = sum / this.items.length);
+    }
+  }
 }
 
 module.exports = SortedList;
