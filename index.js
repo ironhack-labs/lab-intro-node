9767162b3e27ed6b76395e1f150ff002d8b2caac
AlvaroGC95 <107547881+AlvaroGC95@users.noreply.github.com>
2023-07-19 18:39:01 +0200

    index.js

--- index.js
+++ index.js
@@ -1,15 +1,45 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = []
+    this.lenght = 0;
+  }
 
-  add(item) {}
+  add(item) {
+    let index = 0 ;
+    while (index < this.lenght && item > this.item[index]){
+      index ++
+    }
+    this.items.splice(index, 0, item);
+    this.lenght++;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos < 0 || pos >= this.length) {
+      throw new Error("OutOfBounds");
+    }
+    return this.items[pos];
+  }
 
-  max() {}
+  max() {
+    if (this.length === 0) {
+      throw new Error("EmptySortedList");
+    }
+    return this.items[this.length - 1];
+  }
 
-  min() {}
+  min() {
+    if (this.length === 0) {
+    throw new Error("EmptySortedList");
+  }
+  return this.items[0];
+  }
 
-  sum() {}
+  sum() {
+     if (this.length > 0) {
+      return this.items.reduce((accumaltor, currentValue) => accumaltor + currentValue, 0);
+    } else {
+      return 0;
+  }
 
   avg() {}
 }
