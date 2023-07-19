c97156155bd9dc434c6fc32c64e7d758c7a67309
AlvaroGC95 <alvarogarciacarmona95@gmail.com>
2023-07-19 17:00:27 +0200

    donde

--- index.js
+++ index.js
@@ -1,17 +1,53 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = []
+    this.lenght = 0;
+  }
 
-  add(item) {}
+  add(item) {
+    let index = 0 ;
+    while (index < this.lenght && item > this.item[index]){
+      index ++
+    }
+    this.items.splice(index, 0, item);
+    this.lenght++;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos < 0 || pos >= this.length) {
+      throw new Error("OutOfBounds");
+    }
+    return this.items[pos];
+  }
 
-  max() {}
+  max() {
+    if (this.length === 0) {
+      throw new Error("EmptySortedList");
+    }
+    return this.items[this.length - 1];
+  }
 
-  min() {}
-
-  sum() {}
-
-  avg() {}
+  min() {if (this.length === 0) {
+    throw new Error("EmptySortedList");
+  }
+  return this.items[0];
 }
+
+  sum() {
+    if (this.length > 0) {
+      return this.items.reduce((acc, curr) => acc + curr, 0);
+    } else {
+      return 0;
+    }
+  }
+
+  avg() {  if (this.length > 0) {
+    return this.sum() / this.length;
+  }else {
+    throw new Error ('EmptySortedList');
+  }
+}
+}
+
 
 module.exports = SortedList;
