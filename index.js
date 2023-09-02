99b91d337ff1f11517346876797c0f884db11a81
dutuka <39162585+dutuka@users.noreply.github.com>
2023-09-02 20:26:00 +0200

    done

--- index.js
+++ index.js
@@ -1,17 +1,57 @@
 class SortedList {
-  constructor() {}
+  constructor(items, length) {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a, b) => a - b);
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (this.items[pos] === undefined) {
+      throw new Error("OutOfBounds");
+    } else {
+      return this.items[pos];
+    }
+  }
 
-  max() {}
+  max() {
+    if ((SortedList.length = 0)) {
+      throw new Error("EmptySortedList");
+    } else {
+      // return the max (highest) value in the list
+      return Math.max(...this.items);
+    }
+  }
 
-  min() {}
+  min() {
+    if ((SortedList.length = 0)) {
+      throw new Error("EmptySortedList");
+    } else {
+      // return the min (lowest) value in the list
+      return Math.min(...this.items);
+    }
+  }
 
-  sum() {}
+  sum() {
+    if ((this.items.length = 0)) {
+      return 0;
+    } else {
+      //return the sum of all elements in the list
+      return this.items.reduce((a, b) => a + b, 0);
+    }
+  }
 
-  avg() {}
+  avg() {
+    if ((SortedList.length = 0)) {
+      throw new Error("EmptySortedList");
+    } else {
+      //return the average of elements in the list
+      return this.sum / this.items.length;
+    }
+  }
 }
-
 module.exports = SortedList;
