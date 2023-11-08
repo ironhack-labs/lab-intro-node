2f4e2ab94d1130845118ed43bfe3892bfcab608e
paussaanz <paula@sanz.eu>
2023-11-08 23:02:18 +0100

    lab done

--- index.js
+++ index.js
@@ -1,17 +1,53 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item)
+    this.items.sort((a, b) => a - b)
+    this.length = this.items.length
+  }
 
-  get(pos) {}
+  get(pos) {
+    if(pos >= 0 && pos < this.length){
+    return this.items[pos]
+  }
+  else {
+    throw new Error("OutOfBounds");
+  }
+  }
 
-  max() {}
+  max() {
+    if(this.items.length > 0){
+      const highestItem = this.items.length - 1
+      return this.items[highestItem]
+    }
+    else{
+      throw new Error("EmptySortedList") 
+    }
+  }
 
-  min() {}
+  min() {
+    if(this.items.length > 0){
+      return this.items[0]
+    }
+    else{
+      throw new Error("EmptySortedList") 
+    }
+  }
 
-  sum() {}
+  sum() {
+    return this.items.reduce((acc, currentValue) => acc + currentValue, 0);
+  }
 
-  avg() {}
+  avg() {
+    if(this.items.length > 0){
+    return this.sum() / this.items.length
+  }
+    throw new Error("EmptySortedList") 
+  }
 }
 
 module.exports = SortedList;
