953dd94e2c423257fc34595bb6fdd9b2d807daef
HisokaHX <luca.tobiello92@gmail.comgit config --global user.email luca.tobiello92@gmail.com>
2023-11-10 18:03:57 +0100

    WD-PT-MAD-1123 LUCA TOBIELLO

--- index.js
+++ index.js
@@ -1,17 +1,60 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a, b) => {
+      return a - b;
+    })
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos > this.items.length) {
+      throw new Error("OutOfBounds");
+    } else {
+      return this.items[pos];
+    }
 
-  max() {}
+  }
 
-  min() {}
+  max() {
+    if (this.items.length === 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      const highestElem = this.items.length - 1;
+      return this.items[highestElem];
+    }
+  }
 
-  sum() {}
+  min() {
+    if (this.items.length === 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      return this.items[0];
+    }
+  }
 
-  avg() {}
+  sum() {
+    if (this.items.length === 0) {
+      return 0;
+    } else {
+      const sum = this.items.reduce((acc, value) => acc + value, 0);
+      return sum;
+    }
+  }
+
+  avg() {
+    if (this.items.length === 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      const sum = this.items.reduce((acc, value) => acc + value, 0);
+      return sum / this.items.length;
+    }
+  }
 }
 
 module.exports = SortedList;
