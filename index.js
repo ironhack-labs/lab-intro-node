ca6b65dcee480bf30f9f97b09f78eb9f277cc69d
Vladimir Ilovaca <vladoilovaca@gmail.com>
2023-11-10 20:17:35 +0100

    Done

--- index.js
+++ index.js
@@ -1,17 +1,55 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a, b) => {
+      return a - b;
+    });
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+   if (this.items[pos]) {
+    return this.items[pos];
+   } else {
+    throw new Error("OutOfBounds")
+   }
+  }
 
-  max() {}
+  max() { 
+    if (this.items.length === 0) {
+      throw new Error("EmptySortedList")
+    } else {
+      return this.items[this.items.length - 1];
+    }
+  }
 
-  min() {}
+  min() {
+    if (this.items.length === 0) {
+      throw new Error("EmptySortedList");
+    }
+    return this.items[0];
+  }
 
-  sum() {}
+  sum() {
+    if (this.items.length === 0) {
+      return 0;
+    }
+    const sum = this.items.reduce((acc, value) => acc + value, 0);
+    return sum;
+  }
 
-  avg() {}
+  avg() {
+    if (this.items.length === 0) {
+      throw new Error("EmptySortedList");
+    }
+    const avarage = this.items.reduce((acc, value) => acc + value, 0);
+    return avarage / this.items.length;
+  }
 }
 
 module.exports = SortedList;
