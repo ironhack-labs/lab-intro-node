b360650923a86d6596069272f3f23694a039b7f1
Victor Junquera <victorjunquera@eventalista.es>
2023-11-12 13:47:20 +0100

    lab done

--- index.js
+++ index.js
@@ -1,17 +1,60 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = []
+    this.length = this.items.length
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a, b) => a - b);
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos in this.items) {
+      return this.items[pos]
+    } else {
+      throw new Error ("OutOfBounds")
+    }
+  }
 
-  max() {}
+  max() {
+    if (!this.items.length) {
+      throw new Error ("EmptySortedList");
+    } else {    
+    let value = 0;
+    for (let i = 0; i < this.items.length; i++) {
+      if (this.items[i] > value) {
+        value = this.items[i]
+      }
+    } return value
+  } 
+}
 
-  min() {}
+  min() {
+    if (!this.items.length) {
+      throw new Error ("EmptySortedList");
+    } else {
+      return Math.min(...this.items);
+    }
+  }
 
-  sum() {}
+  sum() {
+    if (!this.items.length) {
+      return 0;
+    } else {
+      return this.items.reduce((acc, value) => acc + value)
+    }
+  }
 
-  avg() {}
+  avg() {
+    if (!this.items.length) {
+      throw new Error ("EmptySortedList");
+    } else {
+      let total = this.items.reduce ((acc,value) => acc + value);
+      return total / this.items.length;
+    }
+  }
 }
 
 module.exports = SortedList;
