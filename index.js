592d6f2efa4c3af9263c8a07cc02de974d4a2beb
Vitor193 <vitormiguel135@gmail.com>
2023-11-29 11:52:58 +0000

    lab done

--- index.js
+++ index.js
@@ -1,17 +1,58 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a,b) => a -b);
+    this.length = this.items.length;
 
-  get(pos) {}
+  }
 
-  max() {}
+  get(pos) {
+    if (pos === 0 || pos > this.items.length){
+      throw new Error("OutofBounds");
+    }
+    else{
+      return this.items[pos]
+    }
+  }
 
-  min() {}
+  max() {
+    if(this.items.length > 0){
+      const highestVal = this.items.length -1
+      return this.items[highestVal]
+    }
+    else {
+      throw new Error("EmptySortedList")
+    }
+  }
 
-  sum() {}
+  min() {
+    if(this.items.length > 0){
+      return this.items[0]
+    }
+    else{
+      throw new Error("EmptySortedList")
+    }
+  }
 
-  avg() {}
+  sum() {
+    if(this.items.length > 0){
+    return this.items.reduce((acc,currentVal) => acc + currentVal, 0)}
+    else { return 0}
+  }
+
+  avg() {
+    if(this.items.length > 0){
+      return this.sum()/ this.items.length
+    }
+    else{
+      throw new Error("EmptySortedlist")
+    }
+  }
 }
 
 module.exports = SortedList;
