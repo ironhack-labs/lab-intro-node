d9a435ce1f836d0ace2869ba291385baec3fbf24
lucasfajersztejn <96478667+lucasfajersztejn@users.noreply.github.com>
2024-02-02 00:37:04 +0100

    task 1 and 2 solved

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a, b) => a - b);
+    this.length = this.items.length;
+  }
 
   get(pos) {}
 
