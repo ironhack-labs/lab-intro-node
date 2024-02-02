34a5210aee5cdc11fe2053cb5e79c29351cc87e1
RaquelMelow <raquelmelo949@gmail.com>
2024-02-02 23:13:48 +0000

    [REM-PT-122023] Raquel Melo

--- index.js
+++ index.js
@@ -1,13 +1,36 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = 0;
+  }
+  add(item) {
+    let index = 0;
+    while (index < this.length && this.items[index] < item) {
+      index++;
+    }
+    this.items.splice(index, 0, item);
+    this.length = this.items.length;
+  }
+  get(pos) {
+    if (pos < 0 || pos >= this.length) {
+      throw new Error('OutOfBounds');
+    }
+    return this.items[pos];
 
-  add(item) {}
+  }
+  max() {
+    if (this.length === 0) {
+      throw new Error('EmptySortedList');
+    }
+    return this.items[this.length - 1];
+  }
 
-  get(pos) {}
-
-  max() {}
-
-  min() {}
+  min() {
+    if (this.length === 0) {
+      throw new Error('EmptySortedList');
+    }
+    return this.items[0];
+  }
 
   sum() {}
 
