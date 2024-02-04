9aa162203ef003a20e3d87b3f3494302546410e9
Jesús Sánchez <jsxexu38@gmail.com>
2024-02-04 11:15:31 +0100

    feat: iteration 1,2,3,4 & 5

--- index.js
+++ index.js
@@ -1,13 +1,46 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = []
+    this.length = 0
+  }
 
-  add(item) {}
+  add(item) {
 
-  get(pos) {}
+    this.items.push(item)
+    this.items.sort((a, b) => a - b)
 
-  max() {}
 
-  min() {}
+    this.length = this.items.length
+
+  }
+
+  get(pos) {
+  
+    if (pos > this.items.length) {
+      throw new Error("OutOfBounds")
+    } else {
+      return this.items[pos]
+    }
+
+  }
+
+  max() {
+
+    if (this.items.length === 0) {
+      throw new Error("EmptySortedList")
+    } else {
+      return this.items[this.items.length - 1]
+    }
+  }
+
+  min() {
+
+    if (this.items.length === 0) {
+      throw new Error("Empty SortedList")
+    } else {
+      return this.items[0]
+    }
+  }
 
   sum() {}
 
