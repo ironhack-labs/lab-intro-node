b7fd49e496453a82a5b4c357a9d7b9ff5e74e4fc
José Ramón Mosquera Farré <136368059+Theindicud@users.noreply.github.com>
2024-02-05 12:17:53 +0100

    Solved Lab

--- index.js
+++ index.js
@@ -1,17 +1,59 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.length += 1;
+    this.items.sort((a, b) => a - b);
+  }
 
-  get(pos) {}
+  get(pos) {
+    if(this.items[pos]) {
+      return this.items[pos];
+    } else {
+      throw new Error('OutOfBounds');
+    }
+  }
 
-  max() {}
+  max() {
+    if (this.items.length === 0) {
+      throw new Error ('EmptySortedList');
+    } else {
+      this.items.sort((a, b) => a - b);
+      return this.items[this.items.length - 1];
+    }
+  }
 
-  min() {}
+  min() {
+    if (this.items.length) {
+      this.items.sort((a, b) => a - b);
+      return this.items[0];
+    } else {
+      throw new Error('EmptySortedList')
+    }
+  } 
 
-  sum() {}
+  sum() {
+    if (this.items.length === 0) {
+      return 0;
+    } else {
+      return this.items.reduce(
+        (accumulatedSum, currentNumber) => accumulatedSum + currentNumber, 0);
+    }
+  }
 
-  avg() {}
+  avg() {
+    if (this.items.length === 0) {
+      throw new Error('EmptySortedList');
+    } else {
+      return (this.items.reduce(
+        (accumulatedSum, currentNumber) => accumulatedSum + currentNumber, 0) /
+        this.items.length);
+    }
+  }
 }
 
 module.exports = SortedList;
