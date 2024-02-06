3d2930e44a384f05a9f397b0aac61239c32a63c0
Raulpsdos <raulpscoder@gmail.com>
2024-02-06 12:19:22 +0100

    V.2

--- index.js
+++ index.js
@@ -1,17 +1,46 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length =  this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort('(a, b) => a - b');
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos > this.items.length) {
+      throw new Error('OutOfBounds');
+    } else {
+      return this.items[pos];
+    }
+  }
 
-  max() {}
+  max() {
+    if (this.items.length === 0) {
+      throw new Error('EmptySortedList');
+    } else {
+      return Math.max(...this.items);
+    }
+  }
 
-  min() {}
+  min() {
+    if (this.items.length === 0) {
+      throw new Error('EmptySortedList');
+    } else {
+      return Math.min(...this.items);
+    }
+  }
 
-  sum() {}
+  sum() {
+    return this.items.reduce((total, currentValue) => total + currentValue, 0);
+  }
 
-  avg() {}
+  avg() {
+    const sum = this.items.reduce((total, currentValue) => total + currentValue, 0);
+    return sum / this.items.length;
+  }
 }
 
 module.exports = SortedList;
