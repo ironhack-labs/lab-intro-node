ab68a406c0c30ff7b80ea5e11804a233ca009f5a
Alejandro Martínez <alejandromartinezvarel@gmail.comgit config --global user.name Alejandro>
2024-02-06 17:38:32 +0100

    done

--- index.js
+++ index.js
@@ -1,17 +1,62 @@
 class SortedList {
-  constructor() {}
+  
+  constructor() {
 
-  add(item) {}
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  add(item) {
 
-  max() {}
+    this.items.push(item);
+    this.items.sort((a, b) => a - b);
+    this.length = this.items.length;
+  }
 
-  min() {}
+  get(pos) {
 
-  sum() {}
+    if (pos > this.items.length - 1) {
+      throw new Error ("OutOfBounds");
+    } else {
+      return this.items[pos];
+    }
+  }
 
-  avg() {}
+  max() {
+
+    if (this.items.length === 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      return this.items[this.items.length - 1];
+    }
+  }
+
+  min() {
+
+    if (this.items.length === 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      return this.items[0];
+    }
+  }
+
+  sum() {
+
+    if (this.items.length === 0) {
+      return 0;
+    } else {
+      return this.items.reduce((acc, item) => acc + item);
+    }
+  }
+
+  avg() {
+
+    if (this.items.length === 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      return this.sum() / this.items.length;
+    }
+  }
 }
 
 module.exports = SortedList;
